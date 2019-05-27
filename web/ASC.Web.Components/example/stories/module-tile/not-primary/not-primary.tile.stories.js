24db20fbeffaefbe01fc5abc01d7f07211ef6fe8
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-05-27 17:01:52 +0300

    web: Added new ModuleTile stories

---
+++ web/ASC.Web.Components/example/stories/module-tile/not-primary/not-primary.tile.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Container, Row, Col } from 'reactstrap';
+import { ModuleTile } from 'asc-web-components';
+import withReadme from 'storybook-readme/with-readme';
+import { withKnobs, boolean, text, select } from '@storybook/addon-knobs/react';
+import Readme from './README.md';
+
+const rowStyle = { marginTop: 8 };
+
+storiesOf('Components|ModuleTile', module)
+    // To set a default viewport for all the stories for this component
+    .addParameters({ viewport: { defaultViewport: 'responsive' } })
+    .addDecorator(withKnobs)
+    .addDecorator(withReadme(Readme))
+    .add('not-primary', () => (
+        <Container>
+            <Row style={rowStyle}>
+                <Col>
+                    <ModuleTile
+                        title={text("title", "Documents")}
+                        imageUrl="./modules/documents240.png"
+                        link="/products/files/"
+                        description={text("description", "Create, edit and share documents. Collaborate on them in real-time. 100% compatibility with MS Office formats guaranteed.")}
+                        isPrimary={boolean("isPrimary", false)}
+                        onClick={action("onClick")}
+                    />
+                </Col>
+            </Row>
+        </Container>
+    ));
