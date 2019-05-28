7d6ada9ec0ca9d4ba04a10b31dd6b14e5e212e30
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-05-28 13:45:12 +0300

    web: Changed styles of ModuleTile

--- web/ASC.Web.Components/src/components/module-tile/index.js
+++ web/ASC.Web.Components/src/components/module-tile/index.js
@@ -7,38 +7,44 @@ const PrimaryImage = styled(CardImg)`
     border: none;
     height: 241px;
     width: 240px;
+    cursor: pointer;
 `;
 
 const NotPrimaryImage = styled(CardImg)`
     border: none;
     height: 100px;
     width: 100px;
+    cursor: pointer;
 `;
 
 const DescriptionText = styled(CardTitle)`
-    font-size: 18px; 
-    color: black;
+    font-size: 12px; 
+    
     text-decoration: none;
+    line-height: 20px;
     clear: both;
 `;
 
 const PrimaryTitle = styled(CardTitle)`
-    font-size: 36px; 
-    color: black;
+    font-size: 36px;
+    margin: 46px 0 11px 0;
+    cursor: pointer;
 `;
 
 const NotPrimaryTitle = styled(CardTitle)`
-    font-size: 13px; 
-    color: black;
+    font-size: 18px;
+    cursor: pointer;
+    margin: 14px 0 14px 0;
 `;
 
 const TileCard = styled(Card)`
+    font-family: 'Open Sans', sans-serif;
+    color: #333;
     border: none;
 
     &:hover {
         .selectable {
             text-decoration: underline;
-            cursor: pointer;
         }
     }
 `;
@@ -56,7 +62,7 @@ const ModuleTile = (props) => {
                     <Col md="6" className="align-middle">
                         <CardBody>
                             <PrimaryTitle className="selectable" onClick={event => onClick(event, link) }>{title}</PrimaryTitle>
-                            <DescriptionText>{description}</DescriptionText>
+                            <DescriptionText className="description">{description}</DescriptionText>
                         </CardBody>
                     </Col>
                 </Row>
