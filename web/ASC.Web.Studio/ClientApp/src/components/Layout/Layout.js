3ee5a11be60314e8242df625ba6f5b6a8b65ee65
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-05-30 18:09:49 +0300

    web: Added profile and logout dropdown after login

--- web/ASC.Web.Studio/ClientApp/src/components/Layout/Layout.js
+++ web/ASC.Web.Studio/ClientApp/src/components/Layout/Layout.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Container } from 'reactstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { withRouter } from "react-router";
+import { Container, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { NavMenu } from 'asc-web-components';
+import setAuthorizationToken from '../../utils/setAuthorizationToken';
 
 const Layout = props => {
-    const { children } = props;
+    const { auth, children, history } = props;
+
+    const logout = () => {
+        setAuthorizationToken();
+        history.push('/');
+    };
 
     return (
         <>
             <header>
-                <NavMenu logoUrl="images/light_small_general.svg" href="/" />
+                <NavMenu logoUrl="images/light_small_general.svg" href="/">
+                    {auth.isAuthenticated && (
+                        <UncontrolledDropdown inNavbar size="sm">
+                            <DropdownToggle caret nav style={{ fontFamily: '"Open Sans", sans-serif', fontSize: '12px', color: '#c5c5c5',  }}>
+                                {auth.user.displayName}
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem href="/profile">Profile</DropdownItem>
+                                <DropdownItem onClick={logout}>Sign Out</DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                        )}
+                </NavMenu>
             </header>
             <Container>
                 {children}
@@ -17,4 +38,14 @@ const Layout = props => {
     )
 };
 
-export default Layout;
+Layout.propTypes = {
+    auth: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        auth: state.auth
+    };
+}
+
+export default connect(mapStateToProps)(withRouter(Layout));
