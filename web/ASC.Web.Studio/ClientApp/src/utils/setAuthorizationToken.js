4941f730fa73cb8c95f445019e3cf82ac049ab3a
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-06-03 11:14:58 +0300

    web: Changed work with auth token to request Header 'Authorization'

--- web/ASC.Web.Studio/ClientApp/src/utils/setAuthorizationToken.js
+++ web/ASC.Web.Studio/ClientApp/src/utils/setAuthorizationToken.js
@@ -6,24 +6,20 @@ export default function setAuthorizationToken(token) {
     const cookies = new Cookies();
 
     if (token) {
-        //localStorage.setItem(AUTH_KEY, token);
-        //axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+        axios.defaults.headers.common["Authorization"] = token;
+
         const current = new Date();
         const nextYear = new Date();
 
         nextYear.setFullYear(current.getFullYear() + 1);
 
-        axios.defaults.withCredentials = true;
         cookies.set(AUTH_KEY, token, {
             path: '/',
             expires: nextYear,
         });
     }
     else {
-        axios.defaults.withCredentials = false
+        delete axios.defaults.headers.common["Authorization"];
         cookies.remove(AUTH_KEY);
-        //localStorage.removeItem(AUTH_KEY, token);
-        //delete axios.defaults.headers.common["Authorization"];
-        //setCookie(AUTH_KEY);
     }
 }
