48217aedd8f786896a94c0299c003f947e7bc244
pavelbannov <pavel.bannov@onlyoffice.com>
2019-06-21 11:03:42 +0300

    External config

--- web/ASC.Web.Api/Program.cs
+++ web/ASC.Web.Api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace ASC.Web.Api
 {
@@ -18,8 +19,19 @@ namespace ASC.Web.Api
                     webBuilder.UseStartup<Startup>();
                 })
             .ConfigureAppConfiguration((hostingContext, config) => {
-                config.AddJsonFile("autofac.json");
-                config.AddJsonFile("storage.json");
+                var buided = config.Build();
+                var path = buided["pathToConf"];
+                if (!Path.IsPathRooted(path))
+                {
+                    path = Path.GetFullPath(Path.Combine(hostingContext.HostingEnvironment.ContentRootPath, path));
+                }
+
+                config.SetBasePath(path);
+                config
+                    .AddJsonFile("appsettings.json")
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true)
+                    .AddJsonFile("autofac.api.json")
+                    .AddJsonFile("storage.json");
             });
     }
 }
--- web/ASC.Web.Studio/Program.cs
+++ web/ASC.Web.Studio/Program.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,7 +19,15 @@ namespace ASC.Web.Studio
                     w.UseStartup<Startup>();
                 })
                 .ConfigureAppConfiguration((hostingContext, config) => {
+                    var buided = config.Build();
+                    var path = buided["pathToConf"];
+                    if (!Path.IsPathRooted(path)) {
+                        path = Path.GetFullPath(Path.Combine(hostingContext.HostingEnvironment.ContentRootPath, path));
+                    }
+                    config.SetBasePath(path);
                     config
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true)
                         .AddJsonFile("autofac.json")
                         .AddJsonFile("storage.json");
                 });
