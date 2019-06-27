781677432c6c34307f17b02442b45f7356d848c1
Andrey Savihin <andrey.savihin@onlyoffice.com>
2019-06-27 15:47:32 +0300

    web: Added ModalDialog component

---
+++ web/ASC.Web.Components/example/stories/modal-dialog/modal-dialog.stories.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { Value } from 'react-value';
+import { withKnobs, boolean } from '@storybook/addon-knobs/react';
+import withReadme from 'storybook-readme/with-readme';
+import Readme from './README.md';
+import Section from '../../.storybook/decorators/section';
+import { ModalDialog, Button } from 'asc-web-components';
+
+const header = "Header text";
+
+const body = <p>{"Body text"}</p>;
+
+const footer = [
+  <Button label="Ok" onClick={()=>{}} primary/>,
+  <Button label="Cancel" onClick={()=>{}} style={{marginLeft:"8px"}}/>
+];
+
+storiesOf('Components|ModalDialog', module)
+  .addDecorator(withKnobs)
+  .addDecorator(withReadme(Readme))
+  .add('Modal dialog', () => (
+    <Section>
+      <Value
+        defaultValue={true}
+        render={(isOpen, onChange) => (
+          <ModalDialog
+            isOpen={boolean('isOpen', isOpen)}
+            headerContent={header}
+            bodyContent={body}
+            footerContent={footer}
+            onClose={event => {
+              action('onClose')(event);
+              onChange(false);
+            }}
+          />
+        )}
+      />  
+    </Section>
+  ));
