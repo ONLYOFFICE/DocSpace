9412d80555254052c2c200131e563e0ac77a4fd6
Andrey Savihin <andrey.savihin@onlyoffice.com>
2019-06-28 17:26:19 +0300

    web: Changed DateInput component

--- web/ASC.Web.Components/src/components/date-input/index.js
+++ web/ASC.Web.Components/src/components/date-input/index.js
@@ -2,28 +2,32 @@ import React from 'react'
 import styled from 'styled-components';
 import TextInput from '../text-input'
 import DatePicker from "react-datepicker";
+import { Icons } from '../icons'
 import "react-datepicker/dist/react-datepicker.css";
 
-//TODO: replace svgUrl with real icon url
-
-const svg = `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M14.4444 1H12.6667V0H11.1111V1H4.88889V0H3.33333V1H1.55556C0.696134 1 0.00780207 1.71599 0.00780207 2.60001L0 14.4C0 15.284 0.696097 16 1.55556 16H14.4444C15.3039 16 16 15.284 16 14.4V2.59997C16 1.71599 15.3039 1 14.4444 1ZM14 14H2V5H14V14Z" fill="#A3A9AE"/>
-</svg>`;
-
-const svgUrl = encodeURIComponent(svg.replace(new RegExp('"', 'g'), '\''));
+const StyledOuter = styled.div`
+  position: relative;
+  display: inline-block;
+`;
 
 const StyledTextInput = styled(TextInput)`
-  background-image: url("data:image/svg+xml;utf8,${svgUrl}");
-  background-position: 82px center;
-  background-repeat: no-repeat;  
   padding-right: 32px;
   width: 106px;
+`;
+
+const StyledIcon = styled(Icons.CalendarEmptyIcon)`
+  position: absolute;
+  top: 6px;
+  right: 6px;
 `;
 
 class CustomInput extends React.Component {
   render () {
     return (
-      <StyledTextInput type="text" {...this.props}/>
+      <StyledOuter onClick={this.props.onClick}>
+        <StyledTextInput type="text" {...this.props}/>
+        <StyledIcon size="medium"/>
+      </StyledOuter>
     )
   }
 }
