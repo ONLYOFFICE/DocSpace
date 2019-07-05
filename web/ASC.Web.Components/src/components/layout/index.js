f2bb6da4472ad27ced00f8ed5fec111a6f731f38
Andrey Savihin <andrey.savihin@onlyoffice.com>
2019-07-05 18:32:13 +0300

    web: ProfileActions component changed to stateful

--- web/ASC.Web.Components/src/components/layout/index.js
+++ web/ASC.Web.Components/src/components/layout/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components';
 import device from './sub-components/device'
