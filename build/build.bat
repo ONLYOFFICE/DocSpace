d1c6a7d82e69dc8b9fc174b58058a134185f68b4
pavelbannov <pavel.bannov@onlyoffice.com>
2019-07-09 18:04:47 +0300

    fixed build

--- build/build.bat
+++ build/build.bat
@@ -16,12 +16,9 @@ echo "ASC.Web.sln"
 cd ../../
 call dotnet build ASC.Web.sln  /fl1 /flp1:LogFile=build/ASC.Web.log;Verbosity=Normal
 
-rem echo "ASC.People"
-rem call dotnet build products/ASC.People --self-contained -r win10-x64 -o build/deploy/products/people  /fl1 /flp1:LogFile=build/ASC.People.log;Verbosity=Normal
-
-rem echo "ASC.Web.Api"
-rem call dotnet publish web/ASC.Web.Api --self-contained -r win10-x64 -o build/deploy/www/api  /fl1 /flp1:LogFile=build/ASC.Web.Api.log;Verbosity=Normal
-
-rem xcopy config\*.* build\deploy\config\ /E /R /Y
+echo "ASC.People"
+cd products/ASC.People/ClientApp
+call npm ci
+call npm run build
 
 pause
