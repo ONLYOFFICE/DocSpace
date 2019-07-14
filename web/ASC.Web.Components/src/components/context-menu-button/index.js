4cbbfe66a48bda160934d6da0dd656f09ca486ec
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-07-14 21:23:55 +0300

    web: components: Re-written contex-menu-button to class style (issue: useState, useEffect break React functional components rules of usage)

--- web/ASC.Web.Components/src/components/context-menu-button/index.js
+++ web/ASC.Web.Components/src/components/context-menu-button/index.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import DropDownItem from '../drop-down-item'
 import DropDown from '../drop-down'
 import { Icons } from '../icons'
+import IconButton from '../icon-button'
 
 const StyledOuther = styled.div`
   display: inline-block;
@@ -11,41 +12,75 @@ const StyledOuther = styled.div`
   cursor: pointer;
 `;
 
-const useOuterClickNotifier = (onOuterClick, ref) => {
-  useEffect(() => { 
-      const handleClick = (e) => !ref.current.contains(e.target) && onOuterClick(e);
+class ContextMenuButton extends React.Component {
+  constructor(props) {
+    super(props);
 
-      if (ref.current) {
-          document.addEventListener("click", handleClick);
-      }
+    this.ref = React.createRef();
+    this.iconNames = Object.keys(Icons);
 
-      return () => document.removeEventListener("click", handleClick);
-  },
-  [onOuterClick, ref]
-  );
-}
+    this.state = {
+      isOpen: props.opened,
+      data: props.data
+    };
+  }
 
-const ContextMenuButton = (props) => {
+  handleClick = (e) => !this.ref.current.contains(e.target) && this.toggle(false);
+  stopAction = (e) => e.preventDefault();
+  toggle = (isOpen) => this.setState({ isOpen: isOpen });
 
-  const [data, setState] = useState(props.data);
-  const [opened, toggle] = useState(props.opened);
-  const iconNames = Object.keys(Icons);
-  const ref = useRef(null);
+  componentDidMount() {
+    if (this.ref.current) {
+      document.addEventListener("click", this.handleClick);
+    }
+  }
 
-  useOuterClickNotifier((e) => { toggle(false) }, ref);
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handleClick)
+  }
 
-  return (
-      <StyledOuther title={props.title} onClick={() => { setState(props.getData()); toggle(!opened); }} ref={ref}>
-        {
-          iconNames.includes(props.iconName) && React.createElement(Icons[props.iconName], {size: props.size, color: props.color})
-        }
-        <DropDown direction={props.direction || 'left'} isOpen={opened}>
+  componentDidUpdate(prevProps) {
+    // Store prevId in state so we can compare when props change.
+    // Clear out previously-loaded data (so we don't render stale stuff).
+    if (this.props.opened !== prevProps.opened) {
+      this.toggle(this.props.opened);
+    }
+  }
+
+  render() {
+    return (
+      <StyledOuther ref={this.ref}>
+        <IconButton
+          color={this.props.color}
+          size={this.props.size}
+          iconName={this.props.iconName}
+          isFill={false}
+          isDisabled={this.props.isDisabled}
+          onClick={
+            !this.props.isDisabled
+              ? () => { 
+                this.setState({ data: this.props.getData()});
+                this.toggle(!this.state.isOpen);
+              }
+              : this.stopAction
+          }
+        />
+        <DropDown direction={this.props.direction || 'left'} isOpen={this.state.isOpen}>
           {
-            data.map(item => <DropDownItem {...item}/>)
+            this.state.data.map(item => 
+              <DropDownItem 
+                {...item}
+                onClick={() => { 
+                  item.onClick && item.onClick();
+                  this.toggle(!this.state.isOpen);
+                }} 
+              />
+            )
           }
         </DropDown>
       </StyledOuther>
-  );
+    );
+  };
 }
 
 ContextMenuButton.propTypes = {
@@ -55,7 +90,8 @@ ContextMenuButton.propTypes = {
   title: PropTypes.string,
   iconName: PropTypes.string,
   size: PropTypes.oneOf(['small', 'medium', 'big', 'scale']),
-  color: PropTypes.string
+  color: PropTypes.string,
+  isDisabled: PropTypes.bool
 };
 
 ContextMenuButton.defaultProps = {
@@ -63,7 +99,8 @@ ContextMenuButton.defaultProps = {
   data: [],
   title: '',
   iconName: 'VerticalDotsIcon',
-  size: 'medium'
+  size: 'medium',
+  isDisabled: false
 };
 
 export default ContextMenuButton
