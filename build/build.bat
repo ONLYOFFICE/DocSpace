a7148cb7935ef59fda1bba30a44abca023b02226
pavelbannov <pavel.bannov@onlyoffice.com>
2019-07-17 16:46:08 +0300

    npm install changed to yarn

--- build/build.bat
+++ build/build.bat
@@ -3,16 +3,16 @@ call start\stop.bat
 
 PUSHD %~dp0..
 echo "ASC.Web.Components"
-call npm ci --prefix web/ASC.Web.Components
+call yarn install --cwd web/ASC.Web.Components --frozen-lockfile > build\ASC.Web.Components.log
 
 echo "ASC.Web.Storybook"
-call npm ci --prefix web/ASC.Web.Storybook
+call yarn install --cwd web/ASC.Web.Storybook --frozen-lockfile  > build\ASC.Web.Storybook.log
 
 echo "ASC.Web.Client"
-call npm ci --prefix web/ASC.Web.Client
+call yarn install --cwd web/ASC.Web.Client --frozen-lockfile > build\ASC.Web.Client.log
 
 echo "ASC.Web.People.Client"
-call npm ci --prefix products/ASC.People/Client
+call yarn install --cwd products/ASC.People/Client --frozen-lockfile > build\ASC.Web.People.Client.log
 
 xcopy build\cra\*.* products\ASC.People\Client\node_modules\ /E /R /Y
 
--- build/rebuild.bat
+++ build/rebuild.bat
@@ -2,36 +2,26 @@ PUSHD %~dp0
 call start\stop.bat
 
 PUSHD %~dp0..
-echo "REMOVE npm-local"
+
 del /s /q web\npm-local
 
-PUSHD %~dp0..
 echo "ASC.Web.Components"
-cd web/ASC.Web.Components
-call npm i
+call yarn install --cwd web/ASC.Web.Components > build\ASC.Web.Components.log
 
-PUSHD %~dp0..
 echo "ASC.Web.Storybook"
-cd web\ASC.Web.Storybook
-call npm i
+call yarn install --cwd web/ASC.Web.Storybook  > build\ASC.Web.Storybook.log
 
-PUSHD %~dp0..
 echo "ASC.Web.Client"
-cd web\ASC.Web.Client
-call npm i
+call yarn install --cwd web/ASC.Web.Client > build\ASC.Web.Client.log
 
-PUSHD %~dp0..
 echo "ASC.Web.People.Client"
-cd products/ASC.People/Client 
-call npm i
+call yarn install --cwd products/ASC.People/Client > build\ASC.Web.People.Client.log
 
-PUSHD %~dp0..
 xcopy build\cra\*.* products\ASC.People\Client\node_modules\ /E /R /Y
 
 echo "ASC.Web.sln"
 call dotnet build ASC.Web.sln  /fl1 /flp1:LogFile=build/ASC.Web.log;Verbosity=Normal
 
-PUSHD %~dp0
-start /b call start\start.bat
+start /b call build\start\start.bat
 
 pause
