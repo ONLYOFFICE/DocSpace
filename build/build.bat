c1ea615a187922960684bf1569243235a36dd007
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-07-18 10:57:35 +0300

    build: Added yarn link to asc-web-components

--- build/build.bat
+++ build/build.bat
@@ -5,13 +5,19 @@ PUSHD %~dp0..
 echo "ASC.Web.Components"
 call yarn install --cwd web/ASC.Web.Components --frozen-lockfile > build\ASC.Web.Components.log
 
+echo "ASC.Web.Components"
+call yarn link --cwd web/npm-local/asc-web-components
+
 echo "ASC.Web.Storybook"
+call yarn link "asc-web-components" --cwd web/ASC.Web.Storybook
 call yarn install --cwd web/ASC.Web.Storybook --frozen-lockfile  > build\ASC.Web.Storybook.log
 
 echo "ASC.Web.Client"
+call yarn link "asc-web-components" --cwd web/ASC.Web.Client
 call yarn install --cwd web/ASC.Web.Client --frozen-lockfile > build\ASC.Web.Client.log
 
 echo "ASC.Web.People.Client"
+call yarn link "asc-web-components" --cwd products/ASC.People/Client
 call yarn install --cwd products/ASC.People/Client --frozen-lockfile > build\ASC.Web.People.Client.log
 
 xcopy build\cra\*.* products\ASC.People\Client\node_modules\ /E /R /Y
