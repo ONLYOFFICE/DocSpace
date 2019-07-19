9e7701238429fd230223f84222a8e3e7462f0dcd
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-07-19 12:07:22 +0300

    web: People: Added filter to getUserList api call

--- products/ASC.People/Client/src/utils/api.js
+++ products/ASC.People/Client/src/utils/api.js
@@ -1,6 +1,8 @@
 
 import axios from 'axios';
 import * as fakeApi from './fakeApi';
+import isEmpty from 'lodash/isEmpty';
+import { objectToUrlQuery } from './converter'
 
 const PREFIX = 'api';
 const VERSION = '2.0';
@@ -29,8 +31,9 @@ export function getUser() {
     return IS_FAKE ? fakeApi.getUser() :  axios.get(`${API_URL}/people/@self.json`);
 };
 
-export function getUserList() {
-    return IS_FAKE ? fakeApi.getUsers() :  axios.get(`${API_URL}/people`);
+export function getUserList(filterOptions = {}) {
+    const filter = !isEmpty(filterOptions) ? `/filter.json?${objectToUrlQuery(filterOptions)}` : ""; // /filter.json?StartIndex=0&Count=25&sortby=lastname&sortorder=ascending
+    return IS_FAKE ? fakeApi.getUsers() :  axios.get(`${API_URL}/people${filter}`);
 };
 
 export function getGroupList() {
---
+++ products/ASC.People/Client/src/utils/converter.js
@@ -0,0 +1,11 @@
+export const objectToUrlQuery = obj => {
+  let str = "";
+  for (var key in obj) {
+    if (str !== "") {
+      str += "&";
+    }
+    str += key + "=" + encodeURIComponent(obj[key]);
+  }
+
+  return str;
+};
