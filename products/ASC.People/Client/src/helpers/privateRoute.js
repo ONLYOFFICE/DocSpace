31f253c210f30de4c315670ab02f0e0f5829d939
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-07-19 14:41:54 +0300

    web: People: Refactoring of routing

--- products/ASC.People/Client/src/helpers/privateRoute.js
+++ products/ASC.People/Client/src/helpers/privateRoute.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { AUTH_KEY } from './constants';
-import Cookies from 'universal-cookie';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Redirect, Route } from "react-router-dom";
+import { PageLayout, Loader } from "asc-web-components";
 
-export const PrivateRoute = ({ component: Component, ...rest }) => {
-    return (
-        <Route
-            {...rest}
-            render={props =>
-                (new Cookies()).get(AUTH_KEY) ? (
-                    <Component {...props} />
-                ) : (
-                        <Redirect
-                            to={{
-                                pathname: "/login",
-                                state: { from: props.location }
-                            }}
-                        />
-                    )
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { isAuthenticated, isLoaded } = rest;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        !isLoaded ? (
+          <PageLayout
+            sectionBodyContent={
+              <Loader className="pageLoader" type="rombs" size={40} />
             }
-        />
-    )
+          />
+        ) : isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
 };
+
+PrivateRoute.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+    isLoaded: state.auth.isLoaded
+  };
+}
+
+export default connect(mapStateToProps)(PrivateRoute);
