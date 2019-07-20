b31d8a2f1a8ccbd0a7fe30ec021fa21f11cf9e9f
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-07-20 21:13:42 +0300

    web: People: Added check on isAdmin/isOwner for MainButton render

--- products/ASC.People/Client/src/components/Article/MainButton/index.js
+++ products/ASC.People/Client/src/components/Article/MainButton/index.js
@@ -1,42 +1,52 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import {
     MainButton,
     DropDownItem,
-  } from "asc-web-components";
+} from "asc-web-components";
 
-const ArticleMainButtonContent = () => (
-    <MainButton
-        isDisabled={false}
-        isDropdown={true}
-        text={"Actions"}
-        clickAction={() => console.log("MainButton clickAction")}
-      >
-        <DropDownItem
-          label="New employee"
-          onClick={() => console.log("New employee clicked")}
-        />
-        <DropDownItem
-          label="New quest"
-          onClick={() => console.log("New quest clicked")}
-        />
-        <DropDownItem
-          label="New department"
-          onClick={() => console.log("New department clicked")}
-        />
-        <DropDownItem isSeparator />
-        <DropDownItem
-          label="Invitation link"
-          onClick={() => console.log("Invitation link clicked")}
-        />
-        <DropDownItem
-          label="Invite again"
-          onClick={() => console.log("Invite again clicked")}
-        />
-        <DropDownItem
-          label="Import people"
-          onClick={() => console.log("Import people clicked")}
-        />
-      </MainButton>
+const ArticleMainButtonContent = ({ isAdmin }) => (
+    isAdmin ?
+        <MainButton
+            isDisabled={false}
+            isDropdown={true}
+            text={"Actions"}
+            clickAction={() => console.log("MainButton clickAction")}
+        >
+            <DropDownItem
+                label="New employee"
+                onClick={() => console.log("New employee clicked")}
+            />
+            <DropDownItem
+                label="New quest"
+                onClick={() => console.log("New quest clicked")}
+            />
+            <DropDownItem
+                label="New department"
+                onClick={() => console.log("New department clicked")}
+            />
+            <DropDownItem isSeparator />
+            <DropDownItem
+                label="Invitation link"
+                onClick={() => console.log("Invitation link clicked")}
+            />
+            <DropDownItem
+                label="Invite again"
+                onClick={() => console.log("Invite again clicked")}
+            />
+            <DropDownItem
+                label="Import people"
+                onClick={() => console.log("Import people clicked")}
+            />
+        </MainButton>
+        :
+        <></>
 );
 
-export default ArticleMainButtonContent;
+const mapStateToProps = (state) => {
+    return {
+        isAdmin: state.auth.user.isAdmin || state.auth.user.isOwner
+    }
+}
+
+export default connect(mapStateToProps)(ArticleMainButtonContent);
