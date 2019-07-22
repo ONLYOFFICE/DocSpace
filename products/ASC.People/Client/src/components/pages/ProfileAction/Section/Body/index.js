3290f5578e0f1bebd9460b75da35cfc22d0915dc
Andrey Savihin <andrey.savihin@onlyoffice.com>
2019-07-22 11:57:47 +0300

    web: People: start ProfileAction

--- products/ASC.People/Client/src/components/pages/ProfileAction/Section/Body/index.js
+++ products/ASC.People/Client/src/components/pages/ProfileAction/Section/Body/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import { Avatar } from 'asc-web-components';
 
-const SectionBodyContent = () => {
+
+const SectionBodyContent = (props) => {
+  const {profile, history} = props;
+
+  const isEditProfile = profile && profile.id;
+
+  const getUserRole = (user) => {
+    if(user.isOwner) return "owner";
+    if(user.isAdmin) return "admin";
+    if(user.isVisitor) return "guest";
+    return "user";
+  };
+
+  const getEditAvatarLabel = () => {
+    return isEditProfile ? "Edity photo" : "Add photo";
+  }
+
+  const onEditAvatar = () => {};
+
   return (
-    <span>
-      ProfileAction SectionBodyContent
-    </span>
+    <div>
+      <Avatar
+          size="max"
+          role={getUserRole(profile)}
+          source={profile.avatarBig}
+          userName={profile.userName}
+          editing={true}
+          editLabel={getEditAvatarLabel()}
+          editAction={onEditAvatar}
+        />
+    </div>
   );
 };
 
+SectionBodyContent.propTypes = {
+  profile: PropTypes.object.isRequired,
+  isLoaded: PropTypes.bool
+};
 
 export default SectionBodyContent;
--- products/ASC.People/Client/src/components/pages/ProfileAction/index.js
+++ products/ASC.People/Client/src/components/pages/ProfileAction/index.js
@@ -7,29 +7,28 @@ import { PageLayout } from "asc-web-components";
 import {ArticleHeaderContent, ArticleBodyContent} from '../../Article';
 import {SectionHeaderContent, SectionBodyContent} from './Section';
 
-const ProfileAction = ({profile}) => {
-
-    return (
-        <PageLayout
-            articleHeaderContent={<ArticleHeaderContent />}
-            articleBodyContent={<ArticleBodyContent />}
-            sectionHeaderContent={<SectionHeaderContent /> }
-            sectionBodyContent={<SectionBodyContent /> }
-        />
-    );
+const ProfileAction = (props) => {
+  return (
+      <PageLayout
+          articleHeaderContent={<ArticleHeaderContent />}
+          articleBodyContent={<ArticleBodyContent />}
+          sectionHeaderContent={<SectionHeaderContent {...props} />}
+          sectionBodyContent={<SectionBodyContent {...props} /> }
+      />
+  );
 };
 
 ProfileAction.propTypes = {
-    profile: PropTypes.object.isRequired,
-    history: PropTypes.object.isRequired,
-    isLoaded: PropTypes.bool
-  };
+  profile: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+  isLoaded: PropTypes.bool
+};
 
-  function mapStateToProps(state) {
-    return {
-      profile: {},
-      isLoaded: state.auth.isLoaded
-    };
-  }
+function mapStateToProps(state) {
+  return {
+    profile: {},
+    isLoaded: state.auth.isLoaded
+  };
+}
 
 export default connect(mapStateToProps)(withRouter(ProfileAction));
