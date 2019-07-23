92f4c142d89d27d1cd815bc72c09bb56e9755d9e
Ilya Oleshko <oleshko.ilya@gmail.com>
2019-07-23 15:50:32 +0300

    Web: People: Profile: Added profile viewer

--- products/ASC.People/Client/src/components/pages/Profile/Section/Body/index.js
+++ products/ASC.People/Client/src/components/pages/Profile/Section/Body/index.js
@@ -1,10 +1,161 @@
 import React from 'react';
+import _ from "lodash";
+import { Text, Avatar, Button, ToggleContent, IconButton } from 'asc-web-components';
 
-const SectionBodyContent = () => {
+const profileWrapper = {
+  display: "flex",
+  alignItems: "flex-start",
+  flexDirection: "row",
+  flexWrap: "wrap"
+};
+
+const avatarWrapper = {
+  marginRight: "32px",
+  marginBottom: "24px"
+};
+
+const infoWrapper = {
+  display: "inline-flex",
+  marginBottom: "24px"
+};
+
+const titlesWrapper = {
+  marginRight: "8px"
+};
+
+const restMargins = {
+  marginBottom: "0",
+  marginBlockStart: "5px",
+  marginBlockEnd: "0px",
+};
+
+const notesWrapper = {
+  display: "block",
+  marginTop: "24px",
+  width: "100%"
+};
+
+let socialProfiles = [
+  { type: "facebook", value: "", icon:"ShareFacebookIcon" },
+  { type: "livejournal", value: "", icon:"LivejournalIcon" },
+  { type: "myspace", value: "", icon:"MyspaceIcon" },
+  { type: "twitter", value: "", icon:"ShareTwitterIcon" },
+  { type: "blogger", value: "", icon:"BloggerIcon" },
+  { type: "yahoo", value: "", icon:"YahooIcon" }
+];
+
+let contacts = [
+  { type: "mail", value: "", icon:"MailIcon" },
+  { type: "phone", value: "", icon:"PhoneIcon" },
+  { type: "mobphone", value: "", icon:"MobileIcon" },
+  { type: "gmail", value: "", icon:"GmailIcon" },
+  { type: "skype", value: "", icon:"SkypeIcon" },
+  { type: "msn", value: "", icon:"WindowsMsnIcon" },
+  { type: "icq", value: "", icon:"IcqIcon" },
+  { type: "jabber", value: "", icon:"JabberIcon" },
+  { type: "aim", value: "", icon:"AimIcon" }
+];
+
+let userContacts = [];
+let userSocialProfiles = [];
+
+const getUserRole = (user) => {
+  if (user.isOwner) return "owner";
+  if (user.isAdmin) return "admin";
+  if (user.isVisitor) return "guest";
+  return "user";
+};
+
+const getFormattedDate = (date) => {
+  if (!date) return;
+  let d = date.slice(0, 10).split('-');
+  return d[1] + '/' + d[2] + '/' + d[0];
+};
+
+const getFormattedContacts = (profile) => {
+  let filledUserContacts = _.merge({}, contacts, profile.contacts);
+  userContacts = _.reject(filledUserContacts, (o) => { return o.icon === undefined; });
+
+  let filledSocialProfiles = _.merge({}, socialProfiles, profile.contacts);
+  userSocialProfiles = _.reject(filledSocialProfiles, (o) => { return o.icon === undefined; });
+
+};
+
+const createContacts = (contacts) => {
+  return contacts.map((contact, index) => {
+    if (contact.value) {
+      return (
+        <div key={index} style={{ display: "inline-flex", width: "300px" }}>
+          <IconButton color="#333333" size={16} iconName={contact.icon} isFill={true} onClick={() => { }} />
+          <div style={{ marginLeft: "8px" }}>{contact.value}</div>
+        </div>
+      );
+    }
+  })
+};
+
+const SectionBodyContent = (props) => {
+  const { profile } = props;
+
+  getFormattedContacts(profile);
+
   return (
-    <span>
-      Profile SectionBodyContent
-    </span>
+    <div style={profileWrapper}>
+      <div style={avatarWrapper}>
+        <Avatar
+          size="max"
+          role={getUserRole(profile)}
+          source={profile.avatar}
+          userName={profile.displayName}
+        />
+        <Button style={{marginTop: "16px", width: '160px' }} size="big" label="Edit profile" onClick={() => console.log('Edit action')} />
+      </div>
+      <div style={infoWrapper}>
+        <div style={titlesWrapper}>
+          <Text.Body style={restMargins} color='lightGray' title='Type'>Type:</Text.Body>
+          {profile.email && <Text.Body style={restMargins} color='lightGray' title='E-mail'>E-mail:</Text.Body>}
+          {profile.department && <Text.Body style={restMargins} color='lightGray' title='Department'>Department:</Text.Body>}
+          {profile.title && <Text.Body style={restMargins} color='lightGray' title='Position'>Position:</Text.Body>}
+          {profile.mobilePhone && <Text.Body style={restMargins} color='lightGray' title='Phone'>Phone:</Text.Body>}
+          {profile.sex && <Text.Body style={restMargins} color='lightGray' title='Sex'>Sex:</Text.Body>}
+          {profile.workFrom && <Text.Body style={restMargins} color='lightGray' title='Employed since'>Employed since:</Text.Body>}
+          {profile.birthday && <Text.Body style={restMargins} color='lightGray' title='Date of birth'>Date of birth:</Text.Body>}
+          {profile.location && <Text.Body style={restMargins} color='lightGray' title='Location'>Location:</Text.Body>}
+        </div>
+        <div>
+          <Text.Body style={restMargins}>{profile.isVisitor ? "Guest" : "Employee"}</Text.Body>
+          <Text.Body style={restMargins}>{profile.email}</Text.Body>
+          <Text.Body style={restMargins}>{profile.department}</Text.Body>
+          <Text.Body style={restMargins}>{profile.title}</Text.Body>
+          <Text.Body style={restMargins}>{profile.mobilePhone}</Text.Body>
+          <Text.Body style={restMargins}>{profile.sex}</Text.Body>
+          <Text.Body style={restMargins}>{getFormattedDate(profile.workFrom)}</Text.Body>
+          <Text.Body style={restMargins}>{getFormattedDate(profile.birthday)}</Text.Body>
+          <Text.Body style={restMargins}>{profile.location}</Text.Body>
+        </div>
+      </div>
+      <div style={{ width: "100%" }}>
+        <ToggleContent label="Comment" style={notesWrapper} isOpen={true}>
+          <Text.Body tag="span">
+            {profile.notes}
+          </Text.Body>
+        </ToggleContent>
+      </div>
+      <div style={{ width: "100%", marginTop: "24px" }}>
+        <ToggleContent label="Contact information" style={notesWrapper} isOpen={true}>
+          <Text.Body tag="span">
+            {createContacts(userContacts)}
+          </Text.Body>
+        </ToggleContent>
+      </div>
+      <div style={{ width: "100%", marginTop: "24px" }}>
+        <ToggleContent label="Social profiles" style={notesWrapper} isOpen={true}>
+          <Text.Body tag="span">
+            {createContacts(userSocialProfiles)}
+          </Text.Body>
+        </ToggleContent>
+      </div>
+    </div>
   );
 };
 
--- products/ASC.People/Client/src/components/pages/Profile/index.js
+++ products/ASC.People/Client/src/components/pages/Profile/index.js
@@ -1,34 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
 import _ from "lodash";
-import { PageLayout } from "asc-web-components";
-import {ArticleHeaderContent, ArticleBodyContent} from '../../Article';
-import {SectionHeaderContent, SectionBodyContent} from './Section';
+import { PageLayout, Loader } from "asc-web-components";
+import { ArticleHeaderContent, ArticleBodyContent } from '../../Article';
+import { SectionHeaderContent, SectionBodyContent } from './Section';
+import { getUser } from '../../../utils/api';
 
 const Profile = (props) => {
-    console.log("Profile userId", props.match.params.userId);
-    return (
-        <PageLayout
-            articleHeaderContent={<ArticleHeaderContent />}
-            articleBodyContent={<ArticleBodyContent />}
-            sectionHeaderContent={<SectionHeaderContent /> }
-            sectionBodyContent={<SectionBodyContent /> }
-        />
-    );
+  const { auth, history, match } = props;
+  const { userId } = match.params;
+  const [profile, setProfile] = useState(props.profile);
+  const [isLoaded, setLoaded] = useState(props.isLoaded);
+
+  useEffect(() => {
+    if (userId === "@self") {
+      setProfile(auth.user);
+      setLoaded(true);
+    } else {
+      getUser(userId)
+        .then((res) => {
+          if (res.data.error)
+            throw (res.data.error);
+
+          setProfile(res.data.response);
+          setLoaded(true);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  }, []);
+
+  return (
+    isLoaded ?
+      <PageLayout
+        articleHeaderContent={<ArticleHeaderContent />}
+        articleBodyContent={<ArticleBodyContent />}
+        sectionHeaderContent={<SectionHeaderContent profile={profile} history={history} />}
+        sectionBodyContent={<SectionBodyContent profile={profile} />}
+      />
+      : <PageLayout
+        sectionBodyContent={<Loader className="pageLoader" type="rombs" size={40} />}
+      />
+  );
 };
 
 Profile.propTypes = {
-    history: PropTypes.object.isRequired,
-    match: PropTypes.object.isRequired,
-    auth: PropTypes.object
-  };
+  auth: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired,
+  profile: PropTypes.object,
+  isLoaded: PropTypes.bool
+};
 
-  function mapStateToProps(state) {
-    return {
-      auth: state.auth
-    };
-  }
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
 
 export default connect(mapStateToProps)(withRouter(Profile));
