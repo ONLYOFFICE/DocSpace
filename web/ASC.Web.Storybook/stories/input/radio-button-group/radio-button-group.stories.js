33cb3d443ccb0c2d7708aaf05d3e409ff423743f
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2019-07-25 19:24:30 +0300

    Web: components: RadioButtonGroup: fixed story, added manually input data

--- web/ASC.Web.Storybook/stories/input/radio-button-group/radio-button-group.stories.js
+++ web/ASC.Web.Storybook/stories/input/radio-button-group/radio-button-group.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import { RadioButtonGroup } from 'asc-web-components';
 import Section from '../../../.storybook/decorators/section';
-import {withKnobs} from '@storybook/addon-knobs/react';
+import {withKnobs, text, boolean, number} from '@storybook/addon-knobs/react';
 import Readme from './README.md';
 
 storiesOf('Components|Input', module)
@@ -13,30 +13,15 @@ storiesOf('Components|Input', module)
     return (
       <Section>
         <>
-        <div> First list</div>
-          <RadioButtonGroup
-           selected='apple'
-           spaceBtwnElems={50} 
-           name="test" 
-           options={
-                    [
-                      { value: 'apple', label: 'Apple' },
-                      { value: 'mandarin', label: 'Mandarin'}
-                    ]
-                  } 
-            />
-          <div> Second list</div>
-          <RadioButtonGroup
-            selected='veh' 
-            name="test2" 
-            options={
-                      [
-                        { value: 'Car' },
-                        { value: 'veh', label: 'Vehicle', disabled: true },
-                        { value: 'veh1', label: 'Vehicle1' },
-                        { value: 'veh2', label: 'Vehicle2', disabled: true }
-                      ]
-                    } 
+          <RadioButtonGroup selected={text('selected radio button(value)', 'second')} spaceBtwnElems={number('spaceBtwnElems', 33)}  name={text('name of your group', 'group')} options={
+                [
+                  { value: text('value 1st radiobutton', 'first'), label: text('label 1st radiobutton', 'First radiobtn'), disabled: boolean('isDisabled 1st radiobutton', false) },
+                  { value: text('value 2nd radiobutton', 'second'), label: text('label 2nd radiobutton', 'Second radiobtn'), disabled: boolean('isDisabled 2nd radiobutton', true) },
+                  { value: text('value 3rd radiobutton', 'third'), label: text('label 3rd radiobutton', '3nd radiobtn'), disabled: boolean('isDisabled 3rd radiobutton', false) },
+                  { value: text('value 4th radiobutton', 'fourth'), label: text('label 4th radiobutton', '4th radiobtn'), disabled: boolean('isDisabled 4th radiobutton', true) },
+                  
+                ]
+              } 
             />
         </>
       </Section>
