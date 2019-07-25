49d384f14d1561ed79c6318092a4147c48c7ca77
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2019-07-25 18:14:52 +0300

    Web: components: RadioButtonGroup: fixed story

--- web/ASC.Web.Storybook/stories/input/radio-button-group/radio-button-group.stories.js
+++ web/ASC.Web.Storybook/stories/input/radio-button-group/radio-button-group.stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@storybook/react';
 import withReadme from 'storybook-readme/with-readme';
 import { RadioButtonGroup } from 'asc-web-components';
 import Section from '../../../.storybook/decorators/section';
-import {text, boolean, withKnobs, select, number } from '@storybook/addon-knobs/react';
+import {withKnobs} from '@storybook/addon-knobs/react';
 import Readme from './README.md';
 
 storiesOf('Components|Input', module)
@@ -15,18 +15,29 @@ storiesOf('Components|Input', module)
       <Section>
         <>
         <div> First list</div>
-          <RadioButtonGroup spaceBtwnElems='33' selected='apple' name="test" options={
-            [
-              { value: 'apple', label: 'Яблоко' },
-              { value: 'mandarin', label: 'Мандарин'}]} />
+          <RadioButtonGroup
+           selected='apple' 
+           name="test" 
+           options={
+                    [
+                      { value: 'apple', label: 'Apple' },
+                      { value: 'mandarin', label: 'Mandarin'}
+                    ]
+                  } 
+            />
           <div> Second list</div>
-          <RadioButtonGroup spaceBtwnElems='55' selected='veh' name="test2" options={
-            [
-              { value: 'Car' },
-              { value: 'veh', label: 'Vehicle', disabled: true },
-              { value: 'veh1', label: 'Vehicle1' },
-              { value: 'veh2', label: 'Vehicle2', disabled: true }
-            ]} />
+          <RadioButtonGroup
+            selected='veh' 
+            name="test2" 
+            options={
+                      [
+                        { value: 'Car' },
+                        { value: 'veh', label: 'Vehicle', disabled: true },
+                        { value: 'veh1', label: 'Vehicle1' },
+                        { value: 'veh2', label: 'Vehicle2', disabled: true }
+                      ]
+                    } 
+            />
         </>
       </Section>
     );
