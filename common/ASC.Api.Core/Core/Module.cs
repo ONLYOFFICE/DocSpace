aad452174ed7ff27d5abf6c7db9c1f28d0d4f99a
pavelbannov <pavel.bannov@onlyoffice.com>
2019-07-29 13:04:43 +0300

    Api: added module id

--- common/ASC.Api.Core/Core/Module.cs
+++ common/ASC.Api.Core/Core/Module.cs
@@ -1,9 +1,11 @@
-﻿using ASC.Web.Core;
+﻿using System;
+using ASC.Web.Core;
 
 namespace ASC.Api.Core
 {
     public class Module
     {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Link { get; set; }
         public string ImageUrl { get; set; }
@@ -12,6 +14,7 @@ namespace ASC.Api.Core
 
         public Module(Product product, bool isPrimary = false)
         {
+            Id = product.ProductID;
             Title = product.Name;
             Description = product.Description;
             ImageUrl = product.Context.LargeIconFileName;
