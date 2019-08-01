ca7818996f942c7f7c0dde8477a3d16c7b89981e
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2019-08-01 18:51:07 +0300

    Web: components: Re-written Link component to function style. Removed dropdown functionality.

--- web/ASC.Web.Components/src/components/link/index.js
+++ web/ASC.Web.Components/src/components/link/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
 import PropTypes from 'prop-types';
-import { Icons } from '../icons';
-import DropDown from '../drop-down';
-import DropDownItem from '../drop-down-item';
 import { Text } from '../text';
 
 const SimpleLink = ({ rel, isBold, fontSize, isTextOverflow,
     isHovered, isSemitransparent, type, color, title,
-    dropdownType, data, ...props }) => <a {...props}></a>;
-
-const getColor = color => {
-    switch (color) {
-        case 'gray':
-            return '#A3A9AE';
-        case 'blue':
-            return '#316DAA';
-        default:
-            return '#333333';
-    }
-}
+    ...props }) => <a {...props}></a>;
 
 const opacityCss = css`
     opacity: ${props =>
@@ -27,7 +13,11 @@ const opacityCss = css`
 `;
 
 const colorCss = css`
-    color: ${props => getColor(props.color)};
+    color: ${props =>
+        (props.color === 'black' && '#333333') ||
+        (props.color === 'gray' && '#A3A9AE') ||
+        (props.color === 'blue' && '#316DAA')
+    };
 `;
 
 const hoveredCss = css`
@@ -43,22 +33,6 @@ const visitedCss = css`
 const dottedCss = css`
     border-bottom: 1px dotted;
 `;
-const ExpanderDownIcon = ({ isSemitransparent, dropdownType, type, ...props }) => <Icons.ExpanderDownIcon {...props} />;
-
-const Caret = styled(ExpanderDownIcon)`
-    width: 10px;
-    margin-left: 5px;
-    margin-top: -4px;
-    ${opacityCss};
-    opacity: ${props => (props.type === 'action' && props.dropdownType === 'appearDottedAfterHover') && '0'};
-    display: ${props => !(props.type === 'action' && props.dropdownType !== 'none') && 'none'};
-    position: ${props => (props.type === 'action' && props.dropdownType === 'appearDottedAfterHover') && 'absolute'};
-
-    path {
-        fill: ${props => getColor(props.color)};
-    }
-
-`;
 
 const StyledLink = styled(SimpleLink).attrs((props) => ({
     href: props.href,
@@ -67,6 +41,7 @@ const StyledLink = styled(SimpleLink).attrs((props) => ({
     ${opacityCss};
     text-decoration: none;
     user-select: none;
+    cursor: pointer;
 
         &:hover { 
             ${hoveredCss};
@@ -87,8 +62,7 @@ const StyledLink = styled(SimpleLink).attrs((props) => ({
 
 ${props => (props.isHovered && hoveredCss)}
 
-${props => (props.type === 'action' &&
-        (props.isHovered || props.dropdownType === 'alwaysDotted') &&
+${props => (props.type === 'action' && props.isHovered &&
         dottedCss)}
 
 ${props => (props.isTextOverflow && css`
@@ -99,148 +73,38 @@ ${props => (props.isTextOverflow && css`
 
 `;
 
-const StyledSpan = styled.span`
-    cursor: pointer;
-
-    :hover {
-        svg {  
-        position: static;
-        opacity: ${props => (props.isSemitransparent ? '0.5' : '1')};
-        }
-    }
-`;
-
 const SimpleText = ({ color, fontSize, ...props }) => <Text.Body {...props} />
 const StyledText = styled(SimpleText)`
     ${colorCss};
     font-size: ${props => props.fontSize}px;
 `;
 
-const DataDropDown = ({ data, type, color, fontSize, ...props }) => <DropDown {...props}></DropDown>;
-
-class Link extends React.PureComponent {
-
-    constructor(props) {
-        super(props);
-
-        this.ref = React.createRef();
-
-        this.state = {
-            isOpen: false,
-            isDropdown: props.dropdownType != 'none',
-            data: props.data
-        };
-
-        this.handleClick = this.handleClick.bind(this);
-        this.stopAction = this.stopAction.bind(this);
-        this.toggleDropdown = this.toggleDropdown.bind(this);
-        this.onDropDownItemClick = this.onDropDownItemClick.bind(this);
+const Link = props => {
+    const onClick = (e) => {
+        !props.href && e.preventDefault();
+        props.onClick && props.onClick(e);
     }
 
-    handleClick = (e) => !this.ref.current.contains(e.target) && this.toggleDropdown(false);
-    stopAction = (e) => !this.props.href && e.preventDefault();
-    toggleDropdown = (isOpen) => this.setState({ isOpen: isOpen });
-    clickToDropdown = () => {
-        this.setState({
-            data: this.props.data,
-            isOpen: !this.state.isOpen
-        });
-    }
-    clickAction = (e) => {
-        this.stopAction(e);
-        this.props.hasOwnProperty("onClick") && this.props.onClick(e);
-    }
+    console.log("Link render");
 
-    componentDidMount() {
-        if (this.ref.current) {
-            document.addEventListener("click", this.handleClick);
-        }
-    }
-
-    componentWillUnmount() {
-        document.removeEventListener("click", this.handleClick)
-    }
-
-    componentDidUpdate(prevProps) {
-        // Store prevId in state so we can compare when props change.
-        // Clear out previously-loaded data (so we don't render stale stuff).
-        if (this.props.dropdownType !== prevProps.dropdownType) {
-            if (this.props.isOpen !== prevProps.isOpen) {
-                this.setState({
-                    isDropdown: this.props.dropdownType != 'none',
-                    isOpen: this.props.isOpen
-                });
-            }
-            else {
-                this.setState({ isDropdown: this.props.dropdownType != 'none' });
-            }
-        }
-        else if (this.props.isOpen !== prevProps.isOpen) {
-            this.toggleDropdown(this.props.isOpen);
-        }
-    }
-
-    onDropDownItemClick = (item) => {
-        item.onClick && item.onClick();
-        this.toggleDropdown(!this.state.isOpen);
-    }
-
-    render() {
-        console.log("Link render");
-        return (
-            <>
-                <StyledSpan
-                    ref={this.ref}
-                    isSemitransparent={this.props.isSemitransparent}
-                    onClick={
-                        this.state.isDropdown ?
-                            this.clickToDropdown
-                            : this.clickAction
-                    }>
-                    <StyledLink {...this.props}>
-                        <StyledText
-                            fontSize={this.props.fontSize}
-                            color={this.props.color}
-                            isBold={this.props.isBold}
-                            title={this.props.title}
-                            tag='span'
-                        >
-                            {this.props.children}
-                        </StyledText>
-                    </StyledLink>
-
-                    <Caret
-                        isSemitransparent={this.props.isSemitransparent}
-                        size='small'
-                        isfill={true}
-                        color={this.props.color}
-                        type={this.props.type}
-                        dropdownType={this.props.dropdownType}
-                    />
-
-                </StyledSpan>
-
-                {this.state.isDropdown &&
-                    <DataDropDown isOpen={this.state.isOpen} {...this.props}>
-                        {
-                            this.state.data.map(item =>
-                                <DropDownItem
-                                    {...item}
-                                    onClick={this.onDropDownItemClick.bind(this.props, item)}
-                                />
-                            )
-                        }
-                    </DataDropDown>}
-            </>
-
-        );
-    };
+    return (
+        <StyledLink {...props}>
+            <StyledText
+                onClick={onClick}
+                fontSize={props.fontSize}
+                color={props.color}
+                isBold={props.isBold}
+                title={props.title}
+                tag='span'
+            >
+                {props.children}
+            </StyledText>
+        </StyledLink>
+    );
 }
 
 Link.propTypes = {
     color: PropTypes.oneOf(['gray', 'black', 'blue']),
-    data: PropTypes.array,
-    dropdownType: PropTypes.oneOf(['alwaysDotted', 'appearDottedAfterHover', 'none']),
     fontSize: PropTypes.number,
     href: PropTypes.string,
     isBold: PropTypes.bool,
@@ -256,8 +120,6 @@ Link.propTypes = {
 
 Link.defaultProps = {
     color: 'black',
-    data: [],
-    dropdownType: 'none',
     fontSize: 12,
     href: undefined,
     isBold: false,
--- web/ASC.Web.Storybook/stories/link/base/base.link.stories.js
+++ web/ASC.Web.Storybook/stories/link/base/base.link.stories.js
@@ -2,73 +2,42 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Link } from 'asc-web-components';
 import Readme from './README.md';
-import {text, boolean, withKnobs, select, number } from '@storybook/addon-knobs/react';
+import { text, boolean, withKnobs, select, number } from '@storybook/addon-knobs/react';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../../../.storybook/decorators/section';
-import {Col} from 'reactstrap';
+import { Col } from 'reactstrap';
 import { action } from '@storybook/addon-actions';
 
 const type = ['action', 'page'];
 const colors = ['black', 'gray', 'blue'];
 const target = ['_blank', '_self', '_parent', '_top'];
-const dropdownType = ['alwaysDotted', 'appearDottedAfterHover', 'none'];
-
 
 function clickActionLink(e) {
   action('actionClick')(e);
 }
 
-const dropdownItems = [
-  {
-    key: 'key1',
-    label: 'Button 1',
-    onClick: () => console.log('Button1 action'),
-  },
-  {
-    key: 'key2',
-    label: 'Button 2',
-    onClick: () => console.log('Button2 action'),
-  },
-  {
-    key: 'key3',
-    isSeparator: true
-  },
-  {
-    key: 'key4',
-    label: 'Button 3',
-    onClick: () => console.log('Button3 action'),
-  },
-];
-
 storiesOf('Components|Link', module)
-.addDecorator(withKnobs)
-.addDecorator(withReadme(Readme))
-.add('base', () => {
-let linkType=`${select('type', type, 'page')}`;
-const userProps = linkType === "action" ? {
-  dropdownType: `${select('dropdownType', dropdownType, 'none')}`,
-  onClick: clickActionLink
-  } : {};
-return (
-  <Section>
-    <Col>
-      <Link
-      type={linkType}
-      color={select('color', colors, 'black')}
-      fontSize={number('fontSize', 12)}
-      href={text('href', undefined)}
-      isBold={boolean('isBold', false)}
-      title={text('title', undefined )}
-      target={select('target', target, '_top')}
-      isTextOverflow={boolean('isTextOverflow', false)}
-      isHovered={boolean('isHovered', false)}
-      isSemitransparent={boolean('isSemitransparent', false)}
-      data={dropdownItems}
-      {...userProps}
-      >
-      {text('text', 'Simple link')}
-      </Link>
-    </Col>
-  </Section>
-);
-});
+  .addDecorator(withKnobs)
+  .addDecorator(withReadme(Readme))
+  .add('base', () => (
+    <Section>
+      <Col>
+        <Link
+          type={select('type', type, 'page')}
+          color={select('color', colors, 'black')}
+          fontSize={number('fontSize', 12)}
+          href={text('href', undefined)}
+          isBold={boolean('isBold', false)}
+          title={text('title', undefined)}
+          target={select('target', target, '_top')}
+          isTextOverflow={boolean('isTextOverflow', false)}
+          isHovered={boolean('isHovered', false)}
+          isSemitransparent={boolean('isSemitransparent', false)}
+          onClick={clickActionLink}
+        >
+          {text('text', 'Simple link')}
+        </Link>
+      </Col>
+    </Section>
+  )
+  );
