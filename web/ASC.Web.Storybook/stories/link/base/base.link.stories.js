3d3b37cf0fd7c11de8668424403f0eea182cefd0
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-08-12 12:41:53 +0300

    web: components: Link refactoring

--- web/ASC.Web.Storybook/stories/link/base/base.link.stories.js
+++ web/ASC.Web.Storybook/stories/link/base/base.link.stories.js
@@ -5,7 +5,6 @@ import Readme from './README.md';
 import { text, boolean, withKnobs, select, number } from '@storybook/addon-knobs/react';
 import withReadme from 'storybook-readme/with-readme';
 import Section from '../../../.storybook/decorators/section';
-import { Col } from 'reactstrap';
 import { action } from '@storybook/addon-actions';
 
 const type = ['action', 'page'];
@@ -19,25 +18,32 @@ function clickActionLink(e) {
 storiesOf('Components|Link', module)
   .addDecorator(withKnobs)
   .addDecorator(withReadme(Readme))
-  .add('base', () => (
-    <Section>
-      <Col>
+  .add('base', () => {
+
+    const href = text('href', "http://onlyoffice.com");
+
+    const actionProps = (href && href.length > 0) ? { href } : { onClick: clickActionLink };
+
+    const label = text('text', 'Simple link');
+
+    const isTextOverflow=boolean('isTextOverflow', false);
+
+   return (
+   <Section>
         <Link
           type={select('type', type, 'page')}
           color={select('color', colors, 'black')}
           fontSize={number('fontSize', 13)}
-          href={text('href', undefined)}
           isBold={boolean('isBold', false)}
           title={text('title', undefined)}
-          target={select('target', target, '_top')}
-          isTextOverflow={boolean('isTextOverflow', false)}
+          target={select('target', target, '_blank')}
+          isTextOverflow={isTextOverflow}
           isHovered={boolean('isHovered', false)}
           isSemitransparent={boolean('isSemitransparent', false)}
-          onClick={clickActionLink}
+          {...actionProps}
         >
-          {text('text', 'Simple link')}
+          {label}
         </Link>
-      </Col>
     </Section>
-  )
+   )}
   );
