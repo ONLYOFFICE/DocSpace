7c28197679c61aa4c9a6b024509a98901e4158ca
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2019-08-19 14:25:46 +0300

    Web: Client: translated existing words in current version, Layout [EN]

--- web/ASC.Web.Client/src/i18n.js
+++ web/ASC.Web.Client/src/i18n.js
@@ -15,9 +15,10 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    lng: "en",
+    lng: "ru",
     fallbackLng: 'en',
     debug: true,
+    ns: ['translation', 'Resource'],
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
