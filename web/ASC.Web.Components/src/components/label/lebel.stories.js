fce2be3a31b27d97e719b544fefa5ea462c7b3eb
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-09-08 17:01:14 +0300

    web: components: Moved story for Label + added base test

--- web/ASC.Web.Components/src/components/label/lebel.stories.js
+++ web/ASC.Web.Components/src/components/label/lebel.stories.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, boolean, withKnobs } from '@storybook/addon-knobs/react';
-import { Label } from 'asc-web-components';
+import Label from '.';
 import Section from '../../../.storybook/decorators/section';
 import withReadme from 'storybook-readme/with-readme';
 import Readme from './README.md';
