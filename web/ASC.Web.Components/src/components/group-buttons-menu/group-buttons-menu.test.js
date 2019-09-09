81e614bdc3115a96399ebeb68f859485ce53496e
Ilya Oleshko <oleshko.ilya@gmail.com>
2019-09-09 16:11:10 +0300

    web: Components: Fixed tests for GroupButtonsMenu component.

--- web/ASC.Web.Components/src/components/group-buttons-menu/group-buttons-menu.test.js
+++ web/ASC.Web.Components/src/components/group-buttons-menu/group-buttons-menu.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { mount } from 'enzyme';
-// import GroupButtonsMenu from '.';
-// import DropDownItem from '../drop-down-item';
+import GroupButtonsMenu from '.';
+import DropDownItem from '../drop-down-item';
 
-//TODO: Fix test run
 describe('<GroupButtonsMenu />', () => {
     it('renders without error', () => {
-        /*const menuItems = [
+        const menuItems = [
             {
               label: 'Select',
               isDropdown: true,
@@ -18,27 +17,23 @@ describe('<GroupButtonsMenu />', () => {
                 <DropDownItem key='bbb' label='bbb' />,
                 <DropDownItem key='ccc' label='ccc' />,
               ],
-              onSelect: (a) => console.log(a)
+              onSelect: () => {}
             },
             {
               label: 'Menu item 1',
               disabled: false,
-              onClick: () => console.log('Menu item 1 action')
+              onClick: () => {}
             },
             {
               label: 'Menu item 2',
               disabled: true,
-              onClick: () => console.log('Menu item 2 action')
+              onClick: () => {}
             }
           ];
 
         const wrapper = mount(         
-          <GroupButtonsMenu checked={false} menuItems={menuItems} visible={true} />
-        );*/
-
-        const wrapper = mount(         
-            <div>FIX ME</div>
-          );
+          <GroupButtonsMenu checked={false} menuItems={menuItems} visible={true} moreLabel='More' closeTitle='Close' />
+        );
 
         expect(wrapper).toExist();
     });
