5866b0517bbab89282aa8ea5b2f892bef64081a1
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-09-09 09:48:30 +0300

    web: components: Fixed Combobox test warning

--- web/ASC.Web.Components/src/components/combobox/combobox.test.js
+++ web/ASC.Web.Components/src/components/combobox/combobox.test.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import ComboBox from '.';
+import DropDownItem from '../drop-down-item';
 
 describe('<ComboBox />', () => {
   it('renders without error', () => {
+    const advancedOptions = (
+      <>
+        <DropDownItem>
+          <span>Some text</span>
+        </DropDownItem>
+      </>
+    );
+
     const wrapper = mount(
       <ComboBox
         options={[]} // An empty array will enable advancedOptions
-        advancedOptions={[]}
+        advancedOptions={advancedOptions}
         onSelect={option => console.log("Selected option", option)}
         selectedOption={{
           key: 0,
