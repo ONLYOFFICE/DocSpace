c1f320cfb2d4203759d2486c1a144fd346fac298
Ilya Oleshko <oleshko.ilya@gmail.com>
2019-09-10 11:53:12 +0300

    web: Components: Added containerWidth property for RowContent component. Fixed story and README.

--- web/ASC.Web.Components/src/components/row-content/row-content.stories.js
+++ web/ASC.Web.Components/src/components/row-content/row-content.stories.js
@@ -19,9 +19,9 @@ storiesOf('Components|RowContent', module)
             <Icons.CatalogSpamIcon size='small' isfill={true} color='#3B72A7' />
           </>
           <Link type='page' title='Demo' fontSize={12} color='#A3A9AE' >Demo</Link>
-          <Link type='action' title='Demo' fontSize={12} color='#A3A9AE' >Demo</Link>
+          <Link containerWidth='160px' type='action' title='Demo' fontSize={12} color='#A3A9AE' >Demo</Link>
           <Link type='page' title='0 000 0000000' fontSize={12} color='#A3A9AE' >0 000 0000000</Link>
-          <Link type='page' title='demo@demo.com' fontSize={12} color='#A3A9AE' >demo@demo.com</Link>
+          <Link containerWidth='160px' type='page' title='demo@demo.com' fontSize={12} color='#A3A9AE' >demo@demo.com</Link>
         </RowContent>
         <RowContent>
           <Link type='page' title='Demo Demo' isBold={true} fontSize={15} color='#333333' >Demo Demo</Link>
@@ -29,17 +29,17 @@ storiesOf('Components|RowContent', module)
             <Icons.CatalogSpamIcon size='small' isfill={true} color='#3B72A7' />
           </>
           <></>
-          <Link type='action' title='Demo Demo' fontSize={12} color='#A3A9AE' >Demo Demo</Link>
+          <Link containerWidth='160px' type='action' title='Demo Demo' fontSize={12} color='#A3A9AE' >Demo Demo</Link>
           <Link type='page' title='0 000 0000000' fontSize={12} color='#A3A9AE' >0 000 0000000</Link>
-          <Link type='page' title='demo.demo@demo.com' fontSize={12} color='#A3A9AE' >demo.demo@demo.com</Link>
+          <Link containerWidth='160px' type='page' title='demo.demo@demo.com' fontSize={12} color='#A3A9AE' >demo.demo@demo.com</Link>
         </RowContent>
         <RowContent>
           <Link type='page' title='Demo Demo Demo' isBold={true} fontSize={15} color='#333333' >Demo Demo Demo</Link>
           <></>
           <></>
-          <Link type='action' title='Demo Demo Demo' fontSize={12} color='#A3A9AE' >Demo Demo Demo</Link>
+          <Link containerWidth='160px' type='action' title='Demo Demo Demo' fontSize={12} color='#A3A9AE' >Demo Demo Demo</Link>
           <Link type='page' title='0 000 0000000' fontSize={12} color='#A3A9AE' >0 000 0000000</Link>
-          <Link type='page' title='demo.demo.demo@demo.com' fontSize={12} color='#A3A9AE' >demo.demo.demo@demo.com</Link>
+          <Link containerWidth='160px' type='page' title='demo.demo.demo@demo.com' fontSize={12} color='#A3A9AE' >demo.demo.demo@demo.com</Link>
         </RowContent>
         <RowContent>
           <Link type='page' title='Demo Demo Demo Demo' isBold={true} fontSize={15} color='#333333' >Demo Demo Demo Demo</Link>
@@ -47,9 +47,9 @@ storiesOf('Components|RowContent', module)
             <Icons.SendClockIcon size='small' isfill={true} color='#3B72A7' />
           </>
           <Link type='page' title='Demo' fontSize={12} color='#A3A9AE' >Demo</Link>
-          <Link type='action' title='Demo Demo Demo Demo' fontSize={12} color='#A3A9AE' >Demo Demo Demo Demo</Link>
+          <Link containerWidth='160px' type='action' title='Demo Demo Demo Demo' fontSize={12} color='#A3A9AE' >Demo Demo Demo Demo</Link>
           <Link type='page' title='0 000 0000000' fontSize={12} color='#A3A9AE' >0 000 0000000</Link>
-          <Link type='page' title='demo.demo.demo.demo@demo.com' fontSize={12} color='#A3A9AE' >demo.demo.demo.demo@demo.com</Link>
+          <Link containerWidth='160px' type='page' title='demo.demo.demo.demo@demo.com' fontSize={12} color='#A3A9AE' >demo.demo.demo.demo@demo.com</Link>
         </RowContent>
       </>
     );
