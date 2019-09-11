c05d6e01b371163cade00a135059dc4dcb52d0a1
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2019-09-11 15:23:18 +0300

    Web.Client: added PublicRoute component

---
+++ web/ASC.Web.Client/src/helpers/publicRoute.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import { AUTH_KEY } from './constants';
+import Cookies from 'universal-cookie';
+
+export const PublicRoute = ({ component: Component, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                (new Cookies()).get(AUTH_KEY) ? (
+                    <Redirect
+                        to={{
+                            pathname: "/",
+                            state: { from: props.location }
+                        }}
+                    />
+                ) : (
+                        <Component {...props} />
+                    )
+            }
+        />
+    )
+};
