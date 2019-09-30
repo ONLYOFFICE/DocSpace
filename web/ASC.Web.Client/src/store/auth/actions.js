2f909d7ff0cda7007f4453751c8cb1a998507f2b
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2019-09-30 13:47:24 +0300

    Web.Client: Confirm: createUser: added changing data on confirm activation link

--- web/ASC.Web.Client/src/store/auth/actions.js
+++ web/ASC.Web.Client/src/store/auth/actions.js
@@ -165,6 +165,11 @@ export function changeEmail(userId, email, key) {
 
 export function activateConfirmUser(personalData, loginData, key, userId, activationStatus) {
     const data = Object.assign({}, personalData, loginData);
+    const changedData = {
+        id: userId,
+        FirstName: personalData.firstname,
+        LastName: personalData.lastname
+    }
     return dispatch => {
         return api.changePassword(userId, data, key)
             .then(res => {
@@ -182,6 +187,11 @@ export function activateConfirmUser(personalData, loginData, key, userId, activa
                 checkResponseError(res);
                 const token = res.data.response.token;
                 setAuthorizationToken(token);
+                return api.updateUser(changedData);
+            })
+            .then(res => {
+                console.log("user data updated, result:", res);
+                checkResponseError(res);
                 return getUserInfo(dispatch);
             });
     };
