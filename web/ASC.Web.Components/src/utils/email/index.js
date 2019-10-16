8ecd9fab29ea768f4d5b65d8c165351b78b632bc
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2019-10-16 15:05:27 +0300

    Web.Components: utils: email: added tests

--- web/ASC.Web.Components/src/utils/email/index.js
+++ web/ASC.Web.Components/src/utils/email/index.js
@@ -1,2 +1,2 @@
-export {  parseAddress } from './email';
+export { parseAddress, Email, isEqualEmail, isValidDomainName, parseAddresses } from './email';
 export { EmailSettings, checkAndConvertEmailSettings, isEqualEmailSettings } from './emailSettings';
