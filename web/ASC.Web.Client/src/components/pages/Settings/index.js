5d4341105d56521339ded553726c9a70afa958a6
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2019-10-23 20:23:06 +0300

    Web.Client: Settings: deleted unused code

--- web/ASC.Web.Client/src/components/pages/Settings/index.js
+++ web/ASC.Web.Client/src/components/pages/Settings/index.js
@@ -1,5 +1,4 @@
-import React, { Suspense, lazy } from "react";
-import { connect } from "react-redux";
+import React, { Suspense } from "react";
 import { Loader, PageLayout } from "asc-web-components";
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
@@ -7,14 +6,10 @@ import {
   ArticleHeaderContent,
   ArticleBodyContent
 } from "./Article";
-import { SectionHeaderContent, SectionBodyContent } from './Section'
+import { SectionHeaderContent, SectionBodyContent } from './Section';
 
-
-const Settings = ({ match, language }) => {
-
-  i18n.changeLanguage(language);
-
-  //console.log("Settings render");
+const Settings = () => {
+  console.log("Settings render");
   return (
     <I18nextProvider i18n={i18n}>
       <Suspense
@@ -33,10 +28,4 @@ const Settings = ({ match, language }) => {
   );
 };
 
-function mapStateToProps(state) {
-  return {
-    language: state.auth.user.cultureName || state.auth.settings.culture,
-  };
-}
-
-export default connect(mapStateToProps)(Settings);
+export default Settings;
