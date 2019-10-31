5b9d02bcc37bf672f89d4bf830dc50fb207fc82c
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-10-31 17:33:55 +0300

    web: Components: Formatted code

--- web/ASC.Web.Components/src/components/advanced-selector/sub-components/row.js
+++ web/ASC.Web.Components/src/components/advanced-selector/sub-components/row.js
@@ -3,31 +3,43 @@ import PropTypes from "prop-types";
 import Checkbox from "../../checkbox";
 import Link from "../../link";
 
-const ADSelectorRow = props => {
-  const { label, isChecked, style, onChange, onSelect, isMultiSelect, isSelected } = props;
+class ADSelectorRow extends React.Component {
+  render() {
+    const {
+      label,
+      isChecked,
+      style,
+      onChange,
+      onSelect,
+      isMultiSelect,
+      isSelected
+    } = this.props;
 
-  return (
-    <div className={`option ${isSelected && 'selected'}`} style={style}>
-      {isMultiSelect ? (
-        <Checkbox
-          label={label}
-          isChecked={isChecked}
-          className="option_checkbox"
-          onChange={onChange}
-        />
-      ) : (
-        <Link
-          as="span"
-          truncate={true}
-          className="option_link"
-          onClick={onSelect}
-        >
-          {label}
-        </Link>
-      )}
-    </div>
-  );
-};
+    console.log("ADSelectorRow render", label, isChecked);
+
+    return (
+      <div className={`option ${isSelected && "selected"}`} style={style}>
+        {isMultiSelect ? (
+          <Checkbox
+            label={label}
+            isChecked={isChecked}
+            className="option_checkbox"
+            onChange={onChange}
+          />
+        ) : (
+          <Link
+            as="span"
+            truncate={true}
+            className="option_link"
+            onClick={onSelect}
+          >
+            {label}
+          </Link>
+        )}
+      </div>
+    );
+  }
+}
 
 ADSelectorRow.propTypes = {
   label: PropTypes.string,
