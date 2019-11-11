1c7b86777be65a022c50233acc31e8e7bdc67569
Ilya Oleshko <oleshko.ilya@gmail.com>
2019-11-11 15:46:54 +0300

    web: Components: Added tests in Row component

--- web/ASC.Web.Components/src/components/row/row.test.js
+++ web/ASC.Web.Components/src/components/row/row.test.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Row from '.';
+
+const baseProps = {
+  checked: false,
+  element: <span>1</span>,
+  contextOptions: [{key: '1', label: 'test'}],
+  children: <span>Some text</span>
+}
 
 describe('<Row />', () => {
   it('renders without error', () => {
-    const wrapper = mount(     
-      <Row 
-          checked={false}
-          contextOptions={[]}
-      >
-          <span>Some text</span>
-      </Row>
-    );
+    const wrapper = mount(<Row {...baseProps} />);
 
     expect(wrapper).toExist();
+  });
+
+  it('call changeCheckbox(e)', () => {
+    const onSelect = jest.fn();
+    const wrapper = shallow(<Row {...baseProps} onChange={onSelect} onSelect={onSelect} data={{test: 'test'}} />);
+
+    wrapper.simulate('change', { target: {checked: true} });
+
+    expect(onSelect).toHaveBeenCalled();
   });
 });
