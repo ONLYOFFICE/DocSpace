a7ed0e89108e6c42358ef3808d5ce6ded7ccd2dc
Ilya Oleshko <oleshko.ilya@gmail.com>
2019-11-11 11:12:30 +0300

    web: Components: Fixed story of ContextMenu component

--- web/ASC.Web.Components/src/components/context-menu/context-menu.stories.js
+++ web/ASC.Web.Components/src/components/context-menu/context-menu.stories.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import ContextMenu from '.';
+import RowContainer from '../row-container';
+import RowContent from '../row-content';
 import Row from '../row';
 import Section from '../../../.storybook/decorators/section';
 import Readme from './README.md';
 import withReadme from 'storybook-readme/with-readme';
+
+const getRndString = (n) => Math.random().toString(36).substring(2, (n + 2));
 
 storiesOf('Components|ContextMenu', module)
   .addDecorator(withReadme(Readme))
   .add('base', () => {
     const array = Array.from(Array(10).keys());
 
-    let localState = [];
-
-    const getOptions = options => {
-      return localState[0] = options;
-    };
-
     return (
       <Section>
-        <div id='rowContainer'>
-        {array.map(item => {
-          const context = [{ key: item, label: item },{ key: item+1, label: item+1 },{ key: item+2, label: item+2 }];
-          return (
-            <Row key={item} checked={false} contextOptions={context} onContextClick={getOptions} >
-              <span>Context me {item}</span>
-            </Row>
-          );
-        })}
-        </div>
-        <ContextMenu targetAreaId='rowContainer' options={localState} />
+        <RowContainer manualHeight='300px'>
+          {array.map((item, index) => {
+            return (
+              <Row
+                key={`${item + 1}`}
+                contextOptions={index !== 3 ? [
+                  { key: 1, label: getRndString(5) },
+                  { key: 2, label: getRndString(5) },
+                  { key: 3, label: getRndString(5) },
+                  { key: 4, label: getRndString(5) }
+                ] : []}
+              >
+                <RowContent>
+                  <span>{getRndString(5)}</span>
+                  <></>
+                </RowContent>
+              </Row>
+            );
+          })}
+        </RowContainer>
       </Section>
     );
   });
