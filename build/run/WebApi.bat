fcc4e4605f52ed0de228f6fa5777133b19bbd691
pavelbannov <pavel.bannov@onlyoffice.com>
2019-11-15 11:47:56 +0300

    Common nlog config

--- build/run/WebApi.bat
+++ build/run/WebApi.bat
@@ -1,2 +1,2 @@
 echo "RUN ASC.Web.Api"
-call dotnet run --project ..\..\web\ASC.Web.Api\ASC.Web.Api.csproj --no-build --$STORAGE_ROOT=..\..\Data
+call dotnet run --project ..\..\web\ASC.Web.Api\ASC.Web.Api.csproj --no-build --$STORAGE_ROOT=..\..\Data --log__dir=..\..\Logs --log__name=api
