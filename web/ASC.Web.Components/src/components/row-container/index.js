b71faa8cb182bed7979a4ff83906b8fc8507f909
Andrey Savihin <andrey.savihin@onlyoffice.com>
2019-11-19 13:49:46 +0300

    web: components: row-container: fix context-menu position

--- web/ASC.Web.Components/src/components/row-container/index.js
+++ web/ASC.Web.Components/src/components/row-container/index.js
@@ -10,6 +10,7 @@ import ContextMenu from '../context-menu';
 const StyledRowContainer = styled.div`
   height: ${props => props.useReactWindow ? props.manualHeight ? props.manualHeight : '100%' : 'auto'};
   margin: 16px 0;
+  position: relative;
 `;
 
 class RowContainer extends React.PureComponent {
