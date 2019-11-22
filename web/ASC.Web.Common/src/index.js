6070dd0ba63e6a4d94b1a41b908560d3d82f45e0
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-11-22 15:31:48 +0300

    web: Common: Added export of common errors components

--- web/ASC.Web.Common/src/index.js
+++ web/ASC.Web.Common/src/index.js
@@ -4,3 +4,4 @@ export { default as history } from './history';
 export * from './components';
 export * as constants  from './constants';
 export * as utils from './utils';
+export * from './components/errors';
