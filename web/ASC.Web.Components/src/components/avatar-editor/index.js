f54a8b45e78967a3bdf997db77e5d8bac8097e5d
Ilya Oleshko <oleshko.ilya@gmail.com>
2019-11-22 16:19:55 +0300

    web: Components: Added id, className and style property for AvatarEditor component.

--- web/ASC.Web.Components/src/components/avatar-editor/index.js
+++ web/ASC.Web.Components/src/components/avatar-editor/index.js
@@ -105,6 +105,9 @@ class AvatarEditor extends React.Component {
                     />
                 ]}
                 onClose={this.onClose}
+                className={this.props.className}
+                id={this.props.id}
+                style={this.props.style}
             />
         );
     }
@@ -127,7 +130,10 @@ AvatarEditor.propTypes = {
     onLoadFileError: PropTypes.func,
     unknownTypeError: PropTypes.string,
     unknownError: PropTypes.string,
-    displayType: PropTypes.oneOf(['auto', 'modal', 'aside'])
+    displayType: PropTypes.oneOf(['auto', 'modal', 'aside']),
+    className: PropTypes.string,
+    id: PropTypes.string,
+    style: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
 };
 
 AvatarEditor.defaultProps = {
