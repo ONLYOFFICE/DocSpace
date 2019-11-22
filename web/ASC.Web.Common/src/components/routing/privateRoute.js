59d46748850cba9555e8179dcf37c6cb0f7475ad
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-11-22 14:41:57 +0300

    web: Common: Added PrivateRoute

--- web/ASC.Web.Common/src/components/routing/privateRoute.js
+++ web/ASC.Web.Common/src/components/routing/privateRoute.js
@@ -1,68 +1,95 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React, { useCallback } from "react";
+import { Redirect, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { PageLayout, Loader } from "asc-web-components";
-import { store, constants } from 'asc-web-common';
-const { isAdmin, isMe } = store.auth.selectors;
-const { AUTH_KEY } = constants;
+import { isAdmin, isMe } from "../../store/auth/selectors.js";
+import { AUTH_KEY } from "../../constants";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    const { isAuthenticated, isLoaded, isAdmin, restricted, allowForMe, currentUser, computedMatch } = rest;
-    const { userId } = computedMatch.params;
+  const {
+    isAuthenticated,
+    isLoaded,
+    isAdmin,
+    restricted,
+    allowForMe,
+    currentUser,
+    computedMatch
+  } = rest;
+  const { userId } = computedMatch.params;
+  const token = localStorage.getItem(AUTH_KEY);
 
-    const token = localStorage.getItem(AUTH_KEY);
+  const renderComponent = useCallback(
+    props => {
+      if (!token || (isLoaded && !isAuthenticated)) {
+        return (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        );
+      }
 
-    // console.log("PrivateRoute render", rest);
-    return (
-        <Route
-            {...rest}
-            render={props =>
-                token ? (
-                    !isLoaded ? (
-                        <PageLayout
-                             sectionBodyContent={
-                                 <Loader className="pageLoader" type="rombs" size={40} />
-                             }
-                        />
-                    ) : isAuthenticated ? (
-                        restricted
-                            ? (isAdmin || allowForMe && userId && isMe(currentUser, userId)
-                                ? <Component {...props} />
-                                : <Redirect
-                                        to={{
-                                            pathname: "/error=401",
-                                            state: { from: props.location }
-                                        }}
-                                    />
-                            )
-                            : <Component {...props} />
-                    ) : (
-                                <Redirect
-                                    to={{
-                                        pathname: "/login",
-                                        state: { from: props.location }
-                                    }}
-                                />
-                            )) : (
-                        <Redirect
-                            to={{
-                                pathname: "/login",
-                                state: { from: props.location }
-                            }}
-                        />
-                    )
+      if (!isLoaded) {
+        return (
+          <PageLayout
+            sectionBodyContent={
+              <Loader className="pageLoader" type="rombs" size={40} />
             }
+          />
+        );
+      }
+
+      if (
+        !restricted ||
+        isAdmin ||
+        (allowForMe && userId && isMe(currentUser, userId))
+      )
+        return <Component {...props} />;
+
+      if (restricted) {
+        <Redirect
+          to={{
+            pathname: "/error=401",
+            state: { from: props.location }
+          }}
+        />;
+      }
+
+      return (
+        <Redirect
+          to={{
+            pathname: "/error=404",
+            state: { from: props.location }
+          }}
         />
-    );
+      );
+    },
+    [
+      token,
+      isAuthenticated,
+      isLoaded,
+      isAdmin,
+      restricted,
+      allowForMe,
+      currentUser,
+      userId
+    ]
+  );
+
+  // console.log("PrivateRoute render", rest);
+  return <Route {...rest} render={renderComponent} />;
 };
 
 function mapStateToProps(state) {
-    return {
-        isAuthenticated: state.auth.isAuthenticated,
-        isLoaded: state.auth.isLoaded,
-        isAdmin: isAdmin(state.auth.user)
-    };
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+    isLoaded: state.auth.isLoaded,
+    isAdmin: isAdmin(state.auth.user),
+    currentUser: state.auth.user
+  };
 }
 
 export default connect(mapStateToProps)(PrivateRoute);
