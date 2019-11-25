6dcd699f212f7a4f8927ac088b3579afa5a03d84
Andrey Savihin <andrey.savihin@onlyoffice.com>
2019-11-25 17:52:18 +0300

    ASC.Web.Client: Settings: revert e617c00a4107dfebd4c041feffda07b9a467ffd2 + added Suspense

--- web/ASC.Web.Client/src/components/pages/Settings/index.js
+++ web/ASC.Web.Client/src/components/pages/Settings/index.js
@@ -1,30 +1,34 @@
-import React from "react";
+import React, { lazy,Suspense } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { withRouter } from "react-router";
 import Layout from './Layout';
-import CommonSettings from "./categories/common";
-import SecuritySettings from "./categories/security";
+import { Loader } from "asc-web-components";
+
+const CommonSettings = lazy(() => import("./categories/common"));
+const SecuritySettings = lazy(() => import("./categories/security"));
 
 const Settings = () => {
   const basePath = '/settings';
-
+  
   return (
     <Layout key='1'>
-      <Switch>
-        <Route
-          path={`${basePath}/security`}
-          component={SecuritySettings}
-        />
-        <Route
-          path={[`${basePath}/common`, basePath]}
-          component={CommonSettings}
-        />
-        <Redirect
-          to={{
-            pathname: "/error/404",
-          }}
-        />
-      </Switch>
+      <Suspense fallback={<Loader className="pageLoader" type="rombs" size={40} />}>
+        <Switch>
+          <Route
+            path={`${basePath}/security`}
+            component={SecuritySettings}
+          />
+          <Route
+            path={[`${basePath}/common`, basePath]}
+            component={CommonSettings}
+          />
+          <Redirect
+            to={{
+              pathname: "/error/404",
+            }}
+          />
+        </Switch>
+      </Suspense>
     </Layout>
   );
 };
