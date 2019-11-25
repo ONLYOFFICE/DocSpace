da03e4462397fc8a8829afaeada6bfe23bd9bea0
Andrey Savihin <andrey.savihin@onlyoffice.com>
2019-11-25 11:28:33 +0300

    web: components: field-container: merge stories

--- web/ASC.Web.Components/src/components/field-container/field-container.stories.js
+++ web/ASC.Web.Components/src/components/field-container/field-container.stories.js
@@ -20,60 +20,14 @@ storiesOf("Components|FieldContainer", module)
     >
       {({ value, set }) => (
         <Section>
-          <FieldContainer
-            isVertical={boolean("isVertical", false)}
-            isRequired={boolean("isRequired", false)}
-            hasError={boolean("hasError", false)}
-            labelText={text("labelText", "Name:")}
-            horLabelWidth={text("horLabelWidth", "110px")}
-          >
-            <TextInput
-              value={value}
-              hasError={boolean("hasError", false)}
-              className="field-input"
-              onChange={e => {
-                set(e.target.value);
-              }}
-            />
-          </FieldContainer>
-        </Section>
-      )}
-    </StringValue>
-  ))
-  .add("with tooltip", () => (
-    <StringValue
-      onChange={e => {
-        action("onChange")(e);
-      }}
-    >
-      {({ value, set }) => (
-        <Section>
           <div style={{ marginTop: 100, marginLeft: 50 }}>
             <FieldContainer
               isVertical={boolean("isVertical", false)}
               isRequired={boolean("isRequired", false)}
               hasError={boolean("hasError", false)}
               labelText={text("labelText", "Name:")}
-              tooltipContent={"Paste you tooltip content here"}
-              place="top"
-            >
-              <TextInput
-                value={value}
-                hasError={boolean("hasError", false)}
-                className="field-input"
-                onChange={e => {
-                  set(e.target.value);
-                }}
-              />
-            </FieldContainer>
-          </div>
-          <div style={{ marginTop: 200, marginLeft: 50 }}>
-            <FieldContainer
-              isVertical={boolean("isVertical", false)}
-              isRequired={boolean("isRequired", false)}
-              hasError={boolean("hasError", false)}
-              labelText={text("labelText", "Name:")}
-              tooltipContent={"Paste you tooltip content here"}
+              horLabelWidth={text("horLabelWidth", "110px")}
+              tooltipContent={text("tooltipContent", "Paste you tooltip content here")}
               place="top"
             >
               <TextInput
