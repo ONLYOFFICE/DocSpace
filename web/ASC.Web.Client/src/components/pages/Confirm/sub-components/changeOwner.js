9bf9a3dbbef741bb71364bbfb2fea5c19c1a359e
gopienkonikita <nikita.gopienko@onlyoffice.com>
2019-11-26 14:25:41 +0300

    Web: Client: fixed merge 4e69ebebd2fc711ef2acf6af9fc95ec8617f142a

--- web/ASC.Web.Client/src/components/pages/Confirm/sub-components/changeOwner.js
+++ web/ASC.Web.Client/src/components/pages/Confirm/sub-components/changeOwner.js
@@ -4,11 +4,11 @@ import { withTranslation } from "react-i18next";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-//import { Container, Row, Col, Card, CardTitle, CardImg } from "reactstrap";
+import { Container, Row, Col, Card, CardTitle, CardImg } from "reactstrap";
 import { Button, PageLayout, Text, toastr } from "asc-web-components";
 //import {  } from "../../../../../src/store/auth/actions";
 
-/*const BodyStyle = styled(Container)`
+const BodyStyle = styled(Container)`
   margin-top: 70px;
 
   .buttons-style {
@@ -43,46 +43,6 @@ import { Button, PageLayout, Text, toastr } from "asc-web-components";
   .confirm-text {
     margin-top: 32px;
   }
-`;*/
-
-const BodyStyle = styled.div`
-  .wrapper {
-    display: grid;
-    grid-template-columns: repeat(10, 1fr);
-    grid-gap: 10px;
-    grid-auto-rows: minmax(90px, auto);
-  }
-  .one {
-    margin-left: 10px;
-
-    grid-column: 4 / 10;
-    grid-row: 2 / 3;
-
-    .card-img {
-      max-width: 216px;
-      max-height: 35px;
-    }
-  }
-  .two {
-    grid-column: 1 / 4;
-    grid-row: 1 / 3;
-  }
-  .three {
-    grid-column: 1;
-    grid-row: 2 / 5;
-  }
-  .four {
-    grid-column: 3;
-    grid-row: 3;
-  }
-  .five {
-    grid-column: 2;
-    grid-row: 4;
-  }
-  .six {
-    grid-column: 3;
-    grid-row: 4;
-  }
 `;
 
 class Form extends React.PureComponent {
@@ -111,44 +71,6 @@ class Form extends React.PureComponent {
     const mdOptions = { size: 6, offset: 2 };
 
     return (
-      <BodyStyle>
-        <div class="wrapper">
-          <div class="one">
-            <img
-              class="card-img"
-              src="images/dark_general.png"
-              alt="Logo"
-            ></img>
-          </div>
-
-          <div class="two">Two</div>
-          <div class="three">Three</div>
-          <div class="four">Four</div>
-          <div class="five">Five</div>
-          <div class="six">Six</div>
-        </div>
-      </BodyStyle>
-    );
-  }
-}
-
-Form.propTypes = {};
-
-Form.defaultProps = {};
-
-const ChangePasswordForm = props => (
-  <PageLayout sectionBodyContent={<Form {...props} />} />
-);
-
-function mapStateToProps(state) {
-  return { greetingTitle: state.auth.settings.greetingSettings };
-}
-
-export default connect(
-  mapStateToProps,
-  {}
-)(withRouter(withTranslation()(ChangePasswordForm)));
-/*
       <BodyStyle>
         <Row className="password-row">
           <Col sm="12" md={mdOptions}>
@@ -202,4 +124,23 @@ export default connect(
           </Row>
         )}
       </BodyStyle>
-*/
+    );
+  }
+}
+
+Form.propTypes = {};
+
+Form.defaultProps = {};
+
+const ChangePasswordForm = props => (
+  <PageLayout sectionBodyContent={<Form {...props} />} />
+);
+
+function mapStateToProps(state) {
+  return { greetingTitle: state.auth.settings.greetingSettings };
+}
+
+export default connect(
+  mapStateToProps,
+  {}
+)(withRouter(withTranslation()(ChangePasswordForm)));
