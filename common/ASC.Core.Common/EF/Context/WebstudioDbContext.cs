d081c18c76696ae5bb2fd07ae01dc7b9a5af3cde
pavelbannov <pavel.bannov@onlyoffice.com>
2019-12-04 12:04:32 +0300

    EF: settings

---
+++ common/ASC.Core.Common/EF/Context/WebstudioDbContext.cs
@@ -0,0 +1,30 @@
+﻿using ASC.Core.Common.EF.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace ASC.Core.Common.EF.Context
+{
+    public class WebstudioDbContext : BaseDbContext
+    {
+        public DbSet<DbWebstudioSettings> WebstudioSettings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.AddWebstudioSettings();
+        }
+    }
+
+    public static class WebstudioDbExtension
+    {
+        public static IServiceCollection AddWebstudioDbContextService(this IServiceCollection services)
+        {
+            services.TryAddScoped<DbContextManager<WebstudioDbContext>>();
+            services.TryAddScoped<IConfigureOptions<WebstudioDbContext>, ConfigureDbContext>();
+            services.TryAddScoped<WebstudioDbContext>();
+
+            return services;
+        }
+    }
+}
---
+++ common/ASC.Core.Common/EF/Model/DbWebstudioSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASC.Core.Common.EF.Model
+{
+    [Table("webstudio_settings")]
+    public class DbWebstudioSettings
+    {
+        public int TenantId { get; set; }
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Data { get; set; }
+    }
+
+    public static class WebstudioSettingsExtension
+    {
+        public static void AddWebstudioSettings(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DbWebstudioSettings>()
+                .HasKey(c => new { c.TenantId, c.Id, c.UserId });
+        }
+    }
+}
