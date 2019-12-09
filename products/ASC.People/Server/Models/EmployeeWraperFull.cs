94581bdd39970c98cfe1b5b0aebd0580cfcd2b9f
NikolayRechkin <nikolaynikcher@gmail.com>
2019-12-09 15:59:34 +0300

    People: deleted unused code

--- products/ASC.People/Server/Models/EmployeeWraperFull.cs
+++ products/ASC.People/Server/Models/EmployeeWraperFull.cs
@@ -90,9 +90,6 @@ namespace ASC.Web.Api.Models
         public string AvatarMax { get; set; }
 
         [DataMember(Order = 20)]
-        public string AvatarDefault { get; set; }
-
-        [DataMember(Order = 20)]
         public string AvatarMedium { get; set; }
 
         [DataMember(Order = 20)]
@@ -251,11 +248,6 @@ namespace ASC.Web.Api.Models
             }
 
             var userInfoLM = userInfo.LastModified.GetHashCode();
-
-            if (Context.Check("avatarDefault"))
-            {
-                result.AvatarDefault = Convert.ToBase64String(UserManager.GetUserPhoto(userInfo.ID));
-            }
 
             if (Context.Check("avatarMax"))
             {
