7ebca49738124135279e4a0e708d8a1218c97586
pavelbannov <pavel.bannov@onlyoffice.com>
2019-12-12 18:24:47 +0300

    EF: MobileAppInstall

---
+++ common/ASC.Core.Common/EF/Context/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using ASC.Core.Common.EF.Model;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace ASC.Core.Common.EF.Context
+{
+    public class DbContext : BaseDbContext
+    {
+        public DbSet<MobileAppInstall> MobileAppInstall { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.AddMobileAppInstall();
+        }
+    }
+}
