27ee4325bef6ab2362cf4cfe0c5ff915a4e15dab
Andrey Savihin <andrey.savihin@onlyoffice.com>
2019-12-20 14:53:07 +0300

    Grid component updated

--- web/ASC.Web.Components/src/components/grid/grid.stories.js
+++ web/ASC.Web.Components/src/components/grid/grid.stories.js
@@ -4,16 +4,70 @@ import withReadme from 'storybook-readme/with-readme';
 import Readme from './README.md';
 import Grid from '.';
 import Box from '../box';
+import Text from "../text";
+
+const gridProps = {
+  marginProp: "0 0 20px 0"
+};
+
+const boxProps = {
+  paddingProp: "10px",
+  displayProp: "flex",
+  alignItems: "center",
+  justifyContent: "center"
+};
 
 storiesOf('Components|Grid', module)
   .addDecorator(withReadme(Readme))
-  .add('base', () => (
+  .add("Columns", () => (
+    <>
+      <Grid {...gridProps} columnsProp={["200px", ["100px","1fr"], "auto"]}>
+        <Box {...boxProps} backgroundProp="#F4991A"><Text>200px</Text></Box>
+        <Box {...boxProps} backgroundProp="#F2EAD3"><Text>minmax(100px,1fr)</Text></Box>
+        <Box {...boxProps} backgroundProp="#F9F5F0"><Text>auto</Text></Box>
+      </Grid>
+
+      <Grid {...gridProps} columnsProp="25%">
+        <Box {...boxProps} backgroundProp="#F4991A"><Text>25%</Text></Box>
+        <Box {...boxProps} backgroundProp="#F2EAD3"><Text>25%</Text></Box>
+        <Box {...boxProps} backgroundProp="#F9F5F0"><Text>25%</Text></Box>
+      </Grid>
+      
+      <Grid {...gridProps} columnsProp={{ count: 3, size: "100px" }}>
+        <Box {...boxProps} backgroundProp="#F4991A"><Text>100px</Text></Box>
+        <Box {...boxProps} backgroundProp="#F2EAD3"><Text>100px</Text></Box>
+        <Box {...boxProps} backgroundProp="#F9F5F0"><Text>100px</Text></Box>
+      </Grid>
+
+      <Grid {...gridProps} columnsProp={{ count: 3, size: ["100px", "1fr"] }}>
+        <Box {...boxProps} backgroundProp="#F4991A"><Text>minmax(100px,1fr)</Text></Box>
+        <Box {...boxProps} backgroundProp="#F2EAD3"><Text>minmax(100px,1fr)</Text></Box>
+        <Box {...boxProps} backgroundProp="#F9F5F0"><Text>minmax(100px,1fr)</Text></Box>
+      </Grid>
+    </>  
+  ))
+  .add("Rows", () => (
+    <>
+      <Grid {...gridProps} rowsProp={["100px", ["100px","1fr"], "auto"]}>
+        <Box {...boxProps} backgroundProp="#F4991A"><Text>100px</Text></Box>
+        <Box {...boxProps} backgroundProp="#F2EAD3"><Text>minmax(100px,1fr)</Text></Box>
+        <Box {...boxProps} backgroundProp="#F9F5F0"><Text>auto</Text></Box>
+      </Grid>
+
+      <Grid {...gridProps} rowsProp="50px">
+        <Box {...boxProps} backgroundProp="#F4991A"><Text>50px</Text></Box>
+        <Box {...boxProps} backgroundProp="#F2EAD3"><Text>50px</Text></Box>
+        <Box {...boxProps} backgroundProp="#F9F5F0"><Text>50px</Text></Box>
+      </Grid>
+    </>  
+  ))
+  .add('Layout', () => (
     <Grid
-      widthProp="100%"
+      widthProp="100vw"
       heightProp="100vh"
       gridGap="10px"
       rowsProp={["auto", "1fr", "auto"]}
-      columnsProp={[["100px","1fr"], "3fr", "100px"]}
+      columnsProp={[["100px","1fr"], "3fr", ["100px","1fr"]]}
       areasProp={[
         { name: "header", start: [0, 0], end: [2, 0] },
         { name: "navbar", start: [0, 1], end: [0, 1] },
@@ -22,58 +76,10 @@ storiesOf('Components|Grid', module)
         { name: "footer", start: [0, 2], end: [2, 2] }
       ]}
     >
-      <Box
-        gridArea="header"
-        displayProp="flex"
-        alignItems="center"
-        justifyContent="center"
-        backgroundProp="#F4991A"
-        paddingProp="30px"
-      >
-        header
-      </Box>
-      <Box
-        gridArea="navbar"
-        displayProp="flex"
-        alignItems="center"
-        justifyContent="center"
-        backgroundProp="radial-gradient(red, blue)"
-      >
-        navbar
-      </Box>
-      <Box
-        gridArea="main"
-        borderProp={{ 
-          style: "dashed solid double dotted",
-          width: "2em 1rem 1px 2%",
-          color: "red yellow green blue",
-          radius: "10% 30% 50% 70%"
-        }}
-        displayProp="flex"
-        alignItems="center"
-        justifyContent="center"
-        backgroundProp="#F9F5F0"
-      >
-        main
-      </Box>
-      <Box
-        gridArea="sidebar"
-        displayProp="flex"
-        alignItems="center"
-        justifyContent="center"
-        backgroundProp="#F2EAD3"
-      >
-        sidebar
-      </Box>
-      <Box
-        gridArea="footer"
-        displayProp="flex"
-        alignItems="center"
-        justifyContent="center"
-        backgroundProp="#F4991A"
-        marginProp="30px"
-      >
-        footer
-      </Box>
+      <Box {...boxProps} gridArea="header" backgroundProp="#F4991A"><Text>header</Text></Box>
+      <Box {...boxProps} gridArea="navbar" backgroundProp="#F2EAD3"><Text>navbar</Text></Box>
+      <Box {...boxProps} gridArea="main" backgroundProp="#F9F5F0"><Text>main</Text></Box>
+      <Box {...boxProps} gridArea="sidebar" backgroundProp="#F2EAD3"><Text>sidebar</Text></Box>
+      <Box {...boxProps} gridArea="footer" backgroundProp="#F4991A"><Text>footer</Text></Box>
     </Grid>
   ));
