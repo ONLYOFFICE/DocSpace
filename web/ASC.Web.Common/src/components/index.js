3a08d9196f53e7a801e6a8c88c6c912fb7a9262d
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2019-12-20 10:45:59 +0300

    web: Common: Refactoring: Renamed component folder

--- web/ASC.Web.Common/src/components/index.js
+++ web/ASC.Web.Common/src/components/index.js
@@ -1,7 +1,7 @@
 export { default as PrivateRoute } from "./routing/privateRoute";
 export { default as PublicRoute } from "./routing/publicRoute";
 export { default as StudioLayout } from "./layout";
-export { default as ModuleTile } from "./module-tile";
+export { default as ModuleTile } from "./ModuleTile";
 export { default as Headline } from "./headline";
 export { default as PeopleSelector } from "./PeopleSelector";
 export { default as GroupSelector } from "./GroupSelector";
