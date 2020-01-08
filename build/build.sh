7ad678fabaad55b8974c4452e78dbfa3f0cd125e
pavelbannov <pavel.bannov@onlyoffice.com>
2020-01-08 15:04:59 +0300

    Added build.sh

---
+++ build/build.sh
@@ -0,0 +1,12 @@
+cd ..
+echo "ASC.Web.Components"
+build/scripts/components.sh
+
+echo "ASC.Web.Common"
+build/scripts/common.sh
+
+echo "ASC.Web.Client"
+build/scripts/client.sh
+
+echo "ASC.Web.People.Client"
+build/scripts/people.sh
