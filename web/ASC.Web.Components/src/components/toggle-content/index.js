6ee2d883307585a5322a804f4671ecc5e28a298e
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2020-01-13 19:01:09 +0300

    Web.Components: ToggleContent: refactoring

--- web/ASC.Web.Components/src/components/toggle-content/index.js
+++ web/ASC.Web.Components/src/components/toggle-content/index.js
@@ -4,11 +4,29 @@ import { Icons } from '../icons'
 import Heading from '../heading'
 import PropTypes from 'prop-types'
 
+const StyledContainer = styled.div`
+
+  .span-toggle-content {
+    cursor: pointer;
+    user-select: none;
+  }
+
+  .heading-toggle-content {
+    height: 24px;
+    line-height: 26px;
+    box-sizing: border-box;
+    font-style: normal;
+
+    &:hover {
+    border-bottom: 1px dashed;
+    }
+  }
+`;
 
 const StyledContent = styled.div`
 color: #333;
 display: ${props => props.isOpen ? 'block' : 'none'};
-padding-top: 9px;
+padding-top: 6px;
 `;
 
 // eslint-disable-next-line react/prop-types, no-unused-vars
@@ -21,74 +39,69 @@ const Arrow = styled(IconArrow)`
   transform: ${props => props.isOpen && 'rotate(180deg)'};
 `;
 
-const StyledSpan = styled.span`
-
-  cursor: pointer;
-  user-select: none;
-`;
-
-const StyledText = styled(Heading)`
-  height: 24px;
-  line-height: 26px;
-  box-sizing: border-box;
-  font-style: normal;
-    &:hover{
-    border-bottom: 1px dashed;
-  }
-`;
-
 class ToggleContent extends React.Component {
 
   constructor(props) {
     super(props);
 
+    const { isOpen } = props;
+
     this.state = {
-      isOpen: this.props.isOpen
+      isOpen
     };
   }
 
-  toggleContent = (isOpen) => this.setState({ isOpen: isOpen });
+  toggleContent = () => this.setState({ isOpen: !this.state.isOpen });
 
   componentDidUpdate(prevProps) {
-    if (this.props.isOpen !== prevProps.isOpen) {
-      this.setState({ isOpen: this.props.isOpen });
+    const { isOpen } = this.props;
+    if (isOpen !== prevProps.isOpen) {
+      this.setState({ isOpen });
     }
   }
 
   render() {
-    //console.log("ToggleContent render");
+    // console.log("ToggleContent render");
+
+    const {
+      children,
+      className,
+      id,
+      label,
+      style
+    } = this.props;
+
+    const { isOpen } = this.state;
+
     return (
-      <div
-        className={this.props.className}
-        id={this.props.id}
-        style={this.props.style}
+      <StyledContainer
+        className={className}
+        id={id}
+        style={style}
       >
-        <StyledSpan onClick={() => {
-          this.toggleContent(!this.state.isOpen);
-          this.props.onChange && this.props.onChange(!this.state.isOpen);
-        }}>
-          <Arrow color="#333333" isfill={true} size='medium' isOpen={this.state.isOpen} />
-          <StyledText level={2} size='small' isInline={true}>{this.props.label}</StyledText>
-        </StyledSpan>
-        <StyledContent isOpen={this.state.isOpen}>{this.props.children}</StyledContent>
-      </div>
+        <span className='span-toggle-content' onClick={this.toggleContent}>
+          <Arrow color="#333" isfill={true} size='medium' isOpen={isOpen} />
+          <Heading className='heading-toggle-content' level={2} size='small' isInline={true}>{label}</Heading>
+        </span>
+        <StyledContent isOpen={isOpen}>{children}</StyledContent>
+      </StyledContainer>
     )
   }
 }
 
 ToggleContent.propTypes = {
-  label: PropTypes.string.isRequired,
-  isOpen: PropTypes.bool,
-  onChange: PropTypes.func,
-  className: PropTypes.string,
   children: PropTypes.any,
+  className: PropTypes.string,
   id: PropTypes.string,
+  isOpen: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
   style: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
 }
 
 ToggleContent.defaultProps = {
   isOpen: false,
-  label: "Some label"
+  label: ""
 }
 
 export default ToggleContent;
