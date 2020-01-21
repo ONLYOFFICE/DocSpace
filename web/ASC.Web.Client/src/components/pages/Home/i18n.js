da1d88643da7a66dd041b5e926b4f2a79bd08ee4
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2020-01-21 12:01:53 +0300

    Web.Client: Home: changed redux to localStorage for storing language value

--- web/ASC.Web.Client/src/components/pages/Home/i18n.js
+++ web/ASC.Web.Client/src/components/pages/Home/i18n.js
@@ -1,5 +1,7 @@
 import i18n from "i18next";
 import Backend from "i18next-xhr-backend";
+import { constants } from 'asc-web-common';
+const { LANGUAGE } = constants;
 
 const newInstance = i18n.createInstance();
 
@@ -7,7 +9,7 @@ if (process.env.NODE_ENV === "production") {
   newInstance
     .use(Backend)
     .init({
-      lng: 'en',
+      lng: localStorage.getItem(LANGUAGE) || 'en',
       fallbackLng: "en",
       debug: false,
 
@@ -36,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
 
   newInstance.init({
     resources: resources,
-    lng: 'en',
+    lng: localStorage.getItem(LANGUAGE) || 'en',
     fallbackLng: "en",
     debug: true,
 
