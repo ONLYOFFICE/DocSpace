8713184d568cb86b04b74443f7fd19d81fb85ece
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2020-01-26 15:05:26 +0300

    Web.Client: removed useless i18n config

--- web/ASC.Web.Client/src/components/pages/Confirm/i18n.js
+++ web/ASC.Web.Client/src/components/pages/Confirm/i18n.js
@@ -15,10 +15,6 @@ if (process.env.NODE_ENV === "production") {
 
       interpolation: {
         escapeValue: false, // not needed for react as it escapes by default
-        format: function (value, format) {
-          if (format === 'lowercase') return value.toLowerCase();
-          return value;
-        }
       },
 
       react: {
@@ -47,10 +43,6 @@ if (process.env.NODE_ENV === "production") {
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-      format: function (value, format) {
-        if (format === 'lowercase') return value.toLowerCase();
-        return value;
-      }
     },
 
     react: {
