d0bff4f2917bb915eb76177b3e5060cd409c091c
Ilya Oleshko <oleshko.ilya@gmail.com>
2020-02-03 15:06:45 +0300

    Web: Components: DropDown: Fixed checkPosition function

--- web/ASC.Web.Components/src/components/drop-down/index.js
+++ web/ASC.Web.Components/src/components/drop-down/index.js
@@ -50,7 +50,6 @@ const Row = memo(({ data, index, style }) => {
 });
 
 class DropDown extends React.PureComponent {
-
   constructor(props) {
     super(props);
 
@@ -83,12 +82,10 @@ class DropDown extends React.PureComponent {
       else {
         this.props.disableOnClickOutside();
       }
-
     }
   }
 
   handleClickOutside = e => {
-    //console.log(`DropDown handleClickOutside`, e);
     this.toggleDropDown(e);
   };
 
@@ -101,10 +98,8 @@ class DropDown extends React.PureComponent {
 
     const rects = this.dropDownRef.current.getBoundingClientRect();
     const container = { width: window.innerWidth, height: window.innerHeight };
-
     const left = rects.left < 0;
-    const right = rects.right > container.width;
-
+    const right = rects.right > container.width || container.width < rects.right + rects.left;
     let newDirection = {};
 
     newDirection.directionX = left ? 'left' : right ? 'right' : this.props.directionX;
