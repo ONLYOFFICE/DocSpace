42d9258d997953b43a0d3ff11baeda89484ef6a1
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2020-02-05 10:58:57 +0300

    Web.Common: replaced changeLanguage function, removed connect HOC

--- web/ASC.Web.Common/src/pages/errors/520/index.js
+++ web/ASC.Web.Common/src/pages/errors/520/index.js
@@ -1,26 +1,20 @@
 import React, { useEffect } from 'react';
-import { connect } from "react-redux";
 import ErrorContainer from '../../../components/ErrorContainer';
 import { useTranslation } from 'react-i18next';
 import i18n from './i18n';
+import { changeLanguage } from '../../../utils';
 
-const Error520Container = ({language}) => {
+const Error520Container = () => {
   const { t } = useTranslation('translation', { i18n });
 
   useEffect(() => {
-    i18n.changeLanguage(language);
-  }, [language]);
+    changeLanguage(i18n);
+  }, []);
 
   return <ErrorContainer headerText={t("Error520Text")} />;
 };
 
-function mapStateToProps(state) {
-  return {
-      language: state.auth.user.cultureName || state.auth.settings.culture,
-  };
-}
-
-const Error404 = connect(mapStateToProps)(Error520Container);
+const Error404 = Error520Container;
 
 export default Error404;
 
