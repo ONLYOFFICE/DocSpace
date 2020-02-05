dec059457ca4e1a7a5a467426673eb31c1faf12c
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2020-02-05 12:33:31 +0300

    People.Client: Import, Reassign:fixed i18n config

--- products/ASC.People/Client/src/components/pages/Import/i18n.js
+++ products/ASC.People/Client/src/components/pages/Import/i18n.js
@@ -1,6 +1,8 @@
 import i18n from "i18next";
 import Backend from "i18next-xhr-backend";
 import config from "../../../../package.json";
+import { constants } from 'asc-web-common';
+const { LANGUAGE } = constants;
 
 const newInstance = i18n.createInstance();
 
@@ -8,7 +10,7 @@ if (process.env.NODE_ENV === "production") {
   newInstance
     .use(Backend)
     .init({
-      lng: 'en',
+      lng: localStorage.getItem(LANGUAGE) || 'en',
       fallbackLng: "en",
       debug: true,
 
@@ -33,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 
   newInstance.init({
     resources: resources,
-    lng: 'en',
+    lng: localStorage.getItem(LANGUAGE) || 'en',
     fallbackLng: "en",
     debug: true,
 
