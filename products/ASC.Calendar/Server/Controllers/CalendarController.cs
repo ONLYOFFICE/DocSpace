be5e8454388ad5465e680bf42caf2fe4f8032eee
NikolayRechkin <nikolaynikcher@gmail.com>
2020-02-12 10:58:31 +0300

    init Calendar

---
+++ products/ASC.Calendar/Server/Controllers/CalendarController.cs
@@ -0,0 +1,70 @@
+﻿
+
+using System.Collections.Generic;
+
+using ASC.Api.Core;
+using ASC.Common.Logging;
+using ASC.Common.Utils;
+using ASC.Core;
+using ASC.Core.Common.Settings;
+using ASC.Core.Tenants;
+using ASC.Core.Users;
+using ASC.Data.Reassigns;
+using ASC.FederatedLogin;
+using ASC.MessagingSystem;
+using ASC.Calendar.Models;
+using ASC.Security.Cryptography;
+using ASC.Web.Api.Routing;
+using ASC.Web.Core;
+using ASC.Web.Core.Users;
+using ASC.Web.Studio.Core;
+using ASC.Web.Studio.Core.Notify;
+using ASC.Web.Studio.UserControls.Statistics;
+using ASC.Web.Studio.Utility;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+using SecurityContext = ASC.Core.SecurityContext;
+
+namespace ASC.Calendar.Controllers
+{
+    [DefaultRoute]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        public Tenant Tenant { get { return ApiContext.Tenant; } }
+        public ApiContext ApiContext { get; }
+        public ILog Log { get; }
+
+        public CalendarController(
+           
+            ApiContext apiContext,
+            
+            IOptionsMonitor<ILog> option)
+        {
+            Log = option.Get("ASC.Api");
+            ApiContext = apiContext;
+           
+        }
+
+        [Read("info")]
+        public Module GetModule()
+        {
+            var product = new CalendarProduct();
+            product.Init();
+            return new Module(product, true);
+        }
+
+    }
+
+    public static class PeopleControllerExtention
+    {
+        public static IServiceCollection AddCalendarController(this IServiceCollection services)
+        {
+            return services
+                .AddApiContextService();
+        }
+    }
+}
---
+++ products/ASC.Calendar/Server/Startup.cs
@@ -0,0 +1,158 @@
+
+using System;
+
+using ASC.Api.Core.Auth;
+using ASC.Api.Core.Core;
+using ASC.Api.Core.Middleware;
+using ASC.Common.DependencyInjection;
+using ASC.Common.Logging;
+using ASC.Common.Threading.Progress;
+using ASC.Common.Threading.Workers;
+using ASC.Data.Reassigns;
+using ASC.Calendar.Controllers;
+using ASC.Web.Core.Users;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace ASC.Calendar
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IHostEnvironment HostEnvironment { get; }
+
+        public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment)
+        {
+            Configuration = configuration;
+            HostEnvironment = hostEnvironment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpContextAccessor();
+
+            services.AddControllers().AddControllersAsServices()
+                .AddNewtonsoftJson()
+                .AddXmlSerializerFormatters();
+
+            services.AddTransient<IConfigureOptions<MvcNewtonsoftJsonOptions>, CustomJsonOptionsWrapper>();
+
+            services.AddMemoryCache();
+
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
+            services.AddAuthentication("cookie")
+                .AddScheme<AuthenticationSchemeOptions, CookieAuthHandler>("cookie", a => { })
+                .AddScheme<AuthenticationSchemeOptions, ConfirmAuthHandler>("confirm", a => { });
+
+            var builder = services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+                config.Filters.Add(new TypeFilterAttribute(typeof(TenantStatusFilter)));
+                config.Filters.Add(new TypeFilterAttribute(typeof(PaymentFilter)));
+                config.Filters.Add(new TypeFilterAttribute(typeof(IpSecurityFilter)));
+                config.Filters.Add(new TypeFilterAttribute(typeof(ProductSecurityFilter)));
+                config.Filters.Add(new CustomResponseFilterAttribute());
+                config.Filters.Add(new CustomExceptionFilterAttribute());
+                config.Filters.Add(new TypeFilterAttribute(typeof(FormatFilter)));
+
+                config.OutputFormatters.RemoveType<XmlSerializerOutputFormatter>();
+                config.OutputFormatters.Add(new XmlOutputFormatter());
+            });
+
+            services
+                //.AddConfirmAuthHandler()
+                .AddCookieAuthHandler()
+                .AddCultureMiddleware()
+                .AddIpSecurityFilter()
+                .AddPaymentFilter()
+                .AddProductSecurityFilter()
+                .AddTenantStatusFilter();
+
+           /* services.Configure<WorkerQueue<ResizeWorkerItem>>(r =>
+            {
+                r.workerCount = 2;
+                r.waitInterval = (int)TimeSpan.FromSeconds(30).TotalMilliseconds;
+                r.errorCount = 1;
+                r.stopAfterFinsih = true;
+            });
+
+            services.Configure<ProgressQueue<ReassignProgressItem>>(r =>
+            {
+                r.workerCount = 1;
+                r.waitInterval = (int)TimeSpan.FromMinutes(5).TotalMilliseconds;
+                r.removeAfterCompleted = true;
+                r.stopAfterFinsih = false;
+                r.errorCount = 0;
+            });
+
+            services.Configure<ProgressQueue<RemoveProgressItem>>(r =>
+            {
+                r.workerCount = 1;
+                r.waitInterval = (int)TimeSpan.FromMinutes(5).TotalMilliseconds;
+                r.removeAfterCompleted = true;
+                r.stopAfterFinsih = false;
+                r.errorCount = 0;
+            });*/
+
+            services.AddNLogManager("ASC.Api", "ASC.Web");
+
+            services
+                .AddCalendarController();
+
+            services.AddAutofac(Configuration, HostEnvironment.ContentRootPath);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseCors(builder =>
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod());
+
+            app.UseRouting();
+
+            app.UseSession();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.UseCultureMiddleware();
+
+            app.UseDisposeMiddleware();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapCustom();
+            });
+
+            app.UseStaticFiles();
+        }
+    }
+}
