dffa17672b5c4d752ba86752220b9605782e061d
Ilya Oleshko <oleshko.ilya@gmail.com>
2020-02-14 12:20:46 +0300

    Web: Components: LinkWithDropDown: Added max-height for DropDown

--- web/ASC.Web.Components/src/components/link-with-dropdown/index.js
+++ web/ASC.Web.Components/src/components/link-with-dropdown/index.js
@@ -208,6 +208,7 @@ class LinkWithDropdown extends React.Component {
           open={this.state.isOpen}
           withArrow={false}
           clickOutsideAction={this.onClose}
+          maxHeight={200}
           {...rest}
         >
           {data.map(item => (
