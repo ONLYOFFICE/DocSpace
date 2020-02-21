30d952b946004a250fdc89560962e4131ec4815f
pavelbannov <pavel.bannov@onlyoffice.com>
2020-02-21 15:38:04 +0300

    fix

--- common/ASC.Core.Common/EF/Model/Feed/FeedAggregate.cs
+++ common/ASC.Core.Common/EF/Model/Feed/FeedAggregate.cs
@@ -29,6 +29,6 @@ namespace ASC.Core.Common.EF.Model
         public string Json { get; set; }
         public string Keywords { get; set; }
 
-        internal override object[] GetKeys() => new object[] { Id };
+        public override object[] GetKeys() => new object[] { Id };
     }
 }
