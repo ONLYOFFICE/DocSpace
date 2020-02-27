8010b81e8cea10c2e3ea8c18308cc5eafb57a280
Daniil Senkiv <daniil.senkiv@onlyoffice.com>
2020-02-27 16:22:48 +0300

    Files.Client: Home: Header: add context menu

--- products/ASC.Files/Client/src/components/pages/Home/Section/Header/index.js
+++ products/ASC.Files/Client/src/components/pages/Home/Section/Header/index.js
@@ -1,41 +1,29 @@
-import React from "react";
+import React, { useCallback } from "react";
 import styled, { css } from "styled-components";
 import { withRouter } from "react-router";
-import { Headline } from 'asc-web-common';
+import { Headline, store } from 'asc-web-common';
 import { connect } from "react-redux";
 import { withTranslation } from "react-i18next";
+import {
+  toastr,
+  ContextMenuButton
+} from "asc-web-components";
+
+const { isAdmin } = store.auth.selectors;
 
 const StyledContainer = styled.div`
+
+  position: relative;
+  display: flex;
+  align-items: center;
+  max-width: calc(100vw - 32px);
 
   @media (min-width: 1024px) {
     ${props => props.isHeaderVisible && css`width: calc(100% + 76px);`}
   }
 
-  .group-button-menu-container {
-    margin: 0 -16px;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    
-    @media (max-width: 1024px) {
-      & > div:first-child {
-      position: absolute;
-      top: 56px;
-      z-index: 180;
-      }
-    }
-
-    @media (min-width: 1024px) {
-      margin: 0 -24px;
-    }
-  }
-
-  .header-container {
-    position: relative;
-
-    display: flex;
-    align-items: center;
-    max-width: calc(100vw - 32px);
-
     .action-button {
+      margin-bottom: -1px;
       margin-left: 16px;
 
       @media (max-width: 1024px) {
@@ -47,20 +35,98 @@ const StyledContainer = styled.div`
         }
       }
     }
-  }
 `;
 
 const SectionHeaderContent = props => {
 
+  const { t } = props;
+
+  const createDocument = useCallback(
+    () => toastr.info("New Document click"),
+    []
+  );
+
+  const createSpreadsheet = useCallback(
+    () => toastr.info("New Spreadsheet click"),
+    []
+  );
+
+  const createPresentation = useCallback(
+    () => toastr.info("New Presentation click"),
+    []
+  );
+
+  const createFolder = useCallback(
+    () => toastr.info("New Folder click"),
+    []
+  );
+
+  const uploadToFolder = useCallback(
+    () => toastr.info("Upload To Folder click"),
+    []
+  );
+
+  const getContextOptionsPlus = useCallback(() => {
+    return [
+      {
+        key: "new-document",
+        label: t('NewDocument'),
+        onClick: createDocument
+      },
+      {
+        key: "new-spreadsheet",
+        label: t('NewSpreadsheet'),
+        onClick: createSpreadsheet
+      },
+      {
+        key: "new-presentation",
+        label: t('NewPresentation'),
+        onClick: createPresentation
+      },
+      {
+        key: "new-folder",
+        label: t('NewFolder'),
+        onClick: createFolder
+      },
+      { key: "separator", isSeparator: true },
+      {
+        key: "make-invitation-link",
+        label: t('UploadToFolder'),
+        onClick: uploadToFolder
+      }
+    ];
+  }, [
+    t,
+    createDocument,
+    createSpreadsheet,
+    createPresentation,
+    createFolder,
+    uploadToFolder
+  ]);
+
   return (
     <StyledContainer isHeaderVisible={true}>
-        <Headline className='headline-header' type="content" truncate={true}>Files sample header</Headline>
+      <Headline className='headline-header' type="content" truncate={true}>Files sample header</Headline>
+      {isAdmin && (
+        <>
+          <ContextMenuButton
+            className="action-button"
+            directionX="right"
+            iconName="PlusIcon"
+            size={16}
+            color="#657077"
+            getData={getContextOptionsPlus}
+            isDisabled={false}
+          />
+        </>
+      )}
     </StyledContainer>
   );
 };
 
 const mapStateToProps = state => {
   return {
+    isAdmin: isAdmin(state.auth.user),
   };
 };
 
