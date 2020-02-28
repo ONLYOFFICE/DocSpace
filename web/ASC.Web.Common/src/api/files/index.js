49845fbb6cf86a3f9466f5ba60aa9612e60f10f7
Ilya Oleshko <oleshko.ilya@gmail.com>
2020-02-28 17:21:15 +0300

    Web: Common: Api: Files: Added getCommonFolderList and getProjectsFolderList

--- web/ASC.Web.Common/src/api/files/index.js
+++ web/ASC.Web.Common/src/api/files/index.js
@@ -15,6 +15,30 @@ export function getMyFolderList(filter = Filter.getDefault(), fake = false) {
   });
 }
 
+export function getCommonFolderList(filter = Filter.getDefault(), fake = false) {
+
+  if (fake) {
+    return fakeFiles.getFakeElements(filter, "Common Documents");
+  }
+
+  return request({
+    method: "get",
+    url: `/files/@common.json`
+  });
+}
+
+export function getProjectsFolderList(filter = Filter.getDefault(), fake = false) {
+
+  if (fake) {
+    return fakeFiles.getFakeElements(filter, "Project Documents");
+  }
+
+  return request({
+    method: "get",
+    url: `/files/@projects.json`
+  });
+}
+
 export function getTrashFolderList(filter = Filter.getDefault(), fake = true) {
 
   if (fake) {
