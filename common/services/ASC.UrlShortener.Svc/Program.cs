ef806e6c2bcdc01d7b1d924f9d4c3af15fc56c27
Andrey Savihin <andrey.savihin@onlyoffice.com>
2020-03-05 14:46:19 +0300

    Added url shortener service launcher

---
+++ common/services/ASC.UrlShortener.Svc/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+ *
+ * (c) Copyright Ascensio System Limited 2010-2020
+ *
+ * This program is freeware. You can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License (GPL) version 3 as published by the Free Software Foundation (https://www.gnu.org/copyleft/gpl.html). 
+ * In accordance with Section 7(a) of the GNU GPL its Section 15 shall be amended to the effect that 
+ * Ascensio System SIA expressly excludes the warranty of non-infringement of any third-party rights.
+ *
+ * THIS PROGRAM IS DISTRIBUTED WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE. For more details, see GNU GPL at https://www.gnu.org/copyleft/gpl.html
+ *
+ * You can contact Ascensio System SIA by email at sales@onlyoffice.com
+ *
+ * The interactive user interfaces in modified source and object code versions of ONLYOFFICE must display 
+ * Appropriate Legal Notices, as required under Section 5 of the GNU GPL version 3.
+ *
+ * Pursuant to Section 7 § 3(b) of the GNU GPL you must retain the original ONLYOFFICE logo which contains 
+ * relevant author attributions when distributing the software. If the display of the logo in its graphic 
+ * form is not reasonably feasible for technical reasons, you must include the words "Powered by ONLYOFFICE" 
+ * in every copy of the program you distribute. 
+ * Pursuant to Section 7 § 3(e) we decline to grant you any rights under trademark law for use of our trademarks.
+ *
+*/
+
+
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+using ASC.Common;
+using ASC.Common.DependencyInjection;
+using ASC.Common.Logging;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace ASC.UrlShortener.Svc
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    var buided = config.Build();
+                    var path = buided["pathToConf"];
+                    if (!Path.IsPathRooted(path))
+                    {
+                        path = Path.GetFullPath(Path.Combine(hostContext.HostingEnvironment.ContentRootPath, path));
+                    }
+                    config.SetBasePath(path);
+                    var env = hostContext.Configuration.GetValue("ENVIRONMENT", "Production");
+                    config
+                        .AddInMemoryCollection(new Dictionary<string, string>
+                            {
+                                {"pathToConf", path }
+                            }
+                        )
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile($"appsettings.{env}.json", true)
+                        .AddJsonFile($"appsettings.services.json", true)
+                        .AddJsonFile("storage.json")
+                        .AddJsonFile("notify.json")
+                        .AddJsonFile("kafka.json")
+                        .AddJsonFile($"kafka.{env}.json", true)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var diHelper = new DIHelper(services);
+                    diHelper.AddNLogManager("ASC.UrlShortener.Svc");
+                    services.AddHostedService<UrlShortenerServiceLauncher>();
+                    diHelper.AddUrlShortenerServiceLauncher();
+
+                    //services.AddAutofac(hostContext.Configuration, hostContext.HostingEnvironment.ContentRootPath);
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            using (host)
+            {
+                // Start the host
+                await host.StartAsync();
+
+                // Wait for the host to shutdown
+                await host.WaitForShutdownAsync();
+            }
+        }
+    }
+}
