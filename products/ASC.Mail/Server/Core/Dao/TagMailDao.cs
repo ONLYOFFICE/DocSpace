18b564024ddee13a360a1713e8d2a49faaad08e7
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2020-03-17 16:53:03 +0300

    mail: core: Re-written TagMailDao to EF core

--- products/ASC.Mail/Server/Core/Dao/TagMailDao.cs
+++ products/ASC.Mail/Server/Core/Dao/TagMailDao.cs
@@ -24,32 +24,24 @@
 */
 
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using ASC.Common.Data;
-//using ASC.Common.Data.Sql;
-//using ASC.Common.Data.Sql.Expressions;
-//using ASC.Mail.Core.Dao.Interfaces;
-//using ASC.Mail.Core.DbSchema;
-//using ASC.Mail.Core.DbSchema.Interfaces;
-//using ASC.Mail.Core.DbSchema.Tables;
+using System.Collections.Generic;
+using System.Linq;
+using ASC.Api.Core;
+using ASC.Core;
+using ASC.Core.Common.EF;
+using ASC.Mail.Core.Dao.Entities;
+using ASC.Mail.Core.Dao.Interfaces;
 
 namespace ASC.Mail.Core.Dao
 {
-    /*public class TagMailDao : BaseDao, ITagMailDao
+    public class TagMailDao : BaseDao, ITagMailDao
     {
-        protected static ITable table = new MailTableFactory().Create<TagMailTable>();
-
-        protected string CurrentUserId { get; private set; }
-
-        public TagMailDao(IDbManager dbManager, int tenant, string user)
-            : base(table, dbManager, tenant)
+        public TagMailDao(DbContextManager<MailDbContext> dbContext,
+            ApiContext apiContext,
+            SecurityContext securityContext)
+            : base(apiContext, securityContext, dbContext)
         {
-            CurrentUserId = user;
         }
-
-        private delegate SqlInsert CreateInsertDelegate();
 
         public void SetMessagesTag(IEnumerable<int> messageIds, int tagId)
         {
@@ -57,74 +49,56 @@ namespace ASC.Mail.Core.Dao
             if (!idMessages.Any())
                 return;
 
-            CreateInsertDelegate createInsertQuery = ()
-                => new SqlInsert(TagMailTable.TABLE_NAME)
-                    .IgnoreExists(true)
-                    .InColumns(TagMailTable.Columns.MailId,
-                        TagMailTable.Columns.TagId,
-                        TagMailTable.Columns.Tenant,
-                        TagMailTable.Columns.User);
-
-            var insertQuery = createInsertQuery();
-
+            var items = new List<MailTagMail>();
             int i, messagessLen;
             for (i = 0, messagessLen = idMessages.Count; i < messagessLen; i++)
             {
                 var messageId = idMessages[i];
 
-                insertQuery
-                    .Values(messageId, tagId, Tenant, CurrentUserId);
+                items.Add(new MailTagMail
+                {
+                    Tenant = Tenant,
+                    IdUser = UserId,
+                    IdMail = messageId,
+                    IdTag = tagId
+                });
 
                 if ((i % 100 != 0 || i == 0) && i + 1 != messagessLen) 
                     continue;
 
-                Db.ExecuteNonQuery(insertQuery);
+                MailDb.MailTagMail.AddRange(items);
 
-                insertQuery = createInsertQuery();
+                MailDb.SaveChanges();
+
+                items = new List<MailTagMail>();
             }
         }
 
         public int CalculateTagCount(int id)
         {
-            var query = new SqlQuery(TagMailTable.TABLE_NAME)
-                .SelectCount()
-                .Where(TagMailTable.Columns.Tenant, Tenant)
-                .Where(TagMailTable.Columns.User, CurrentUserId)
-                .Where(TagMailTable.Columns.TagId, id);
+            var count = MailDb.MailTagMail
+                .Where(t => t.Tenant == Tenant && t.IdUser == UserId && t.IdTag == id)
+                .Count();
 
-            return Db.ExecuteScalar<int>(query);
+            return count;
         }
 
         public Dictionary<int, List<int>> GetMailTagsDictionary(List<int> mailIds)
         {
-            var query = new SqlQuery(TagMailTable.TABLE_NAME)
-                .Select(TagMailTable.Columns.MailId, TagMailTable.Columns.TagId)
-                .Where(TagMailTable.Columns.Tenant, Tenant)
-                .Where(TagMailTable.Columns.User, CurrentUserId)
-                .Where(Exp.In(TagMailTable.Columns.MailId, mailIds));
-
-            var dictionary = Db.ExecuteList(query)
-                .ConvertAll(r => new
-                {
-                    MailId = Convert.ToInt32(r[0]),
-                    TagId = Convert.ToInt32(r[1])
-                })
-                .GroupBy(t => t.MailId)
-                .ToDictionary(g => g.Key, g => g.Select(t => t.TagId).ToList());
+            var dictionary = MailDb.MailTagMail
+                .Where(t => t.Tenant == Tenant && t.IdUser == UserId && mailIds.Contains(t.IdMail))
+                .Select(t => new { t.IdMail, t.IdTag })
+                .GroupBy(t => t.IdMail)
+                .ToDictionary(g => g.Key, g => g.Select(t => t.IdTag).ToList());
 
             return dictionary;
         }
 
         public List<int> GetTagIds(List<int> mailIds)
         {
-            var query = new SqlQuery(TagMailTable.TABLE_NAME)
-                .Select(TagMailTable.Columns.TagId)
-                .Where(TagMailTable.Columns.Tenant, Tenant)
-                .Where(TagMailTable.Columns.User, CurrentUserId)
-                .Where(Exp.In(TagMailTable.Columns.MailId, mailIds));
-
-            var tagIds = Db.ExecuteList(query)
-                .ConvertAll(r => Convert.ToInt32(r[0]))
+            var tagIds = MailDb.MailTagMail
+                .Where(t => t.Tenant == Tenant && t.IdUser == UserId && mailIds.Contains(t.IdMail))
+                .Select(t => t.IdTag)
                 .Distinct()
                 .ToList();
 
@@ -133,12 +107,14 @@ namespace ASC.Mail.Core.Dao
 
         public List<int> GetTagIds(int mailboxId)
         {
-            var query = string.Format("select t.{0} from {1} t inner join {2} m on t.{3} = m.{4} where m.{5} = @mailbox_id",
-                    TagMailTable.Columns.TagId, TagMailTable.TABLE_NAME, MailTable.TABLE_NAME, TagMailTable.Columns.MailId,
-                    MailTable.Columns.Id, MailTable.Columns.MailboxId);
-
-            var tagIds = Db.ExecuteList(query, new { mailbox_id = mailboxId })
-                .ConvertAll(r => Convert.ToInt32(r[0]))
+            var tagIds = MailDb.MailTagMail
+                .Join(MailDb.MailMail, tm => tm.IdMail, m => m.Id, 
+                (tm, m) => new { 
+                    TagMail = tm,
+                    Mail = m
+                })
+                .Where(t => t.Mail.IdMailbox == mailboxId)
+                .Select(t => t.TagMail.IdTag)
                 .Distinct()
                 .ToList();
 
@@ -147,43 +123,59 @@ namespace ASC.Mail.Core.Dao
 
         public int Delete(int tagId, List<int> mailIds)
         {
-            var query = new SqlDelete(TagMailTable.TABLE_NAME)
-                .Where(TagMailTable.Columns.TagId, tagId)
-                .Where(Exp.In(TagMailTable.Columns.MailId, mailIds))
-                .Where(TagMailTable.Columns.Tenant, Tenant)
-                .Where(TagMailTable.Columns.User, CurrentUserId);
+            var deleteQuery = MailDb.MailTagMail
+                .Where(t => t.Tenant == Tenant && t.IdUser == UserId && t.IdTag == tagId)
+                .Where(t => mailIds.Contains(t.IdMail));
 
-            return Db.ExecuteNonQuery(query);
+            MailDb.MailTagMail.RemoveRange(deleteQuery);
+
+            var result = MailDb.SaveChanges();
+
+            return result;
         }
 
         public int DeleteByTagId(int tagId)
         {
-            var query = new SqlDelete(TagMailTable.TABLE_NAME)
-                .Where(TagMailTable.Columns.TagId, tagId)
-                .Where(TagMailTable.Columns.Tenant, Tenant)
-                .Where(TagMailTable.Columns.User, CurrentUserId);
+            var deleteQuery = MailDb.MailTagMail
+                .Where(t => t.Tenant == Tenant && t.IdUser == UserId)
+                .Where(t => t.IdTag == tagId);
 
-            return Db.ExecuteNonQuery(query);
+            MailDb.MailTagMail.RemoveRange(deleteQuery);
+
+            var result = MailDb.SaveChanges();
+
+            return result;
         }
-
-        private static readonly string QueryDeleteFormat =
-            string.Format("delete t from {0} t inner join {1} m on t.{2} = m.{3} where m.{4} = @mailbox_id",
-                TagMailTable.TABLE_NAME, MailTable.TABLE_NAME, TagMailTable.Columns.MailId, MailTable.Columns.Id,
-                MailTable.Columns.MailboxId);
 
         public int DeleteByMailboxId(int mailboxId)
         {
-            return Db.ExecuteNonQuery(QueryDeleteFormat, new { mailbox_id = mailboxId });
+            var deleteQuery = MailDb.MailTagMail
+               .Join(MailDb.MailMail, tm => tm.IdMail, m => m.Id,
+                (tm, m) => new {
+                    TagMail = tm,
+                    Mail = m
+                })
+                .Where(t => t.Mail.IdMailbox == mailboxId)
+                .Select(t => t.TagMail);
+
+            MailDb.MailTagMail.RemoveRange(deleteQuery);
+
+            var result = MailDb.SaveChanges();
+
+            return result;
         }
 
         public int DeleteByMailIds(List<int> mailIds)
         {
-            var query = new SqlDelete(TagMailTable.TABLE_NAME)
-                .Where(TagMailTable.Columns.Tenant, Tenant)
-                .Where(TagMailTable.Columns.User, CurrentUserId)
-                .Where(Exp.In(TagMailTable.Columns.MailId, mailIds));
+            var deleteQuery = MailDb.MailTagMail
+                .Where(t => t.Tenant == Tenant && t.IdUser == UserId)
+                .Where(t => mailIds.Contains(t.IdMail));
 
-            return Db.ExecuteNonQuery(query);
+            MailDb.MailTagMail.RemoveRange(deleteQuery);
+
+            var result = MailDb.SaveChanges();
+
+            return result;
         }
-    }*/
+    }
 }
