e199ff703a06101f5b67aa9bfdf3f16bddcccec1
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-03-31 13:50:28 +0300

    Web: Common: removed unused styles from AdvancedSelector

--- web/ASC.Web.Common/src/components/AdvancedSelector/sub-components/StyledFooter.js
+++ web/ASC.Web.Common/src/components/AdvancedSelector/sub-components/StyledFooter.js
@@ -13,18 +13,6 @@ const StyledFooter = styled.div`
       padding: 16px 0;
     `}
 
-  .ad-selector_combo-box {
-    margin-left: 8px;
-
-    .combo-button {
-      height: 36px;
-    }
-
-    .combo-button-label {
-      margin: 0;
-    }
-  }
-
   ${props =>
     !props.isVisible &&
     css`
