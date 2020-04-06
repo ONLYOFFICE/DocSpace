ef530cbfb8dc22d496c89c0a8ba88599abd06d11
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2020-04-06 16:18:43 +0300

    mail: API: Added method CreateAccountSimple in MailAccountController

--- products/ASC.Mail/Server/Controllers/MailAccountController.cs
+++ products/ASC.Mail/Server/Controllers/MailAccountController.cs
@@ -1,8 +1,10 @@
 ﻿using ASC.Mail.Core.Dao.Expressions.Mailbox;
+using ASC.Mail.Enums;
 using ASC.Mail.Exceptions;
 using ASC.Mail.Extensions;
 using ASC.Mail.Models;
 using ASC.Web.Api.Routing;
+using ASC.Web.Mail.Resources;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -61,6 +63,111 @@ namespace ASC.Mail.Controllers
                 throw new LoginException(loginResult);
 
             return accountInfo.ToAccountData().FirstOrDefault();
+        }
+
+        /// <summary>
+        ///    Creates an account based on email and password.
+        /// </summary>
+        /// <param name="email">Account email in string format like: name@domain</param>
+        /// <param name="password">Password as plain text.</param>
+        /// <exception cref="ArgumentException">Exception happens when in parameters is invalid. Text description contains parameter name and text description.</exception>
+        /// <exception cref="Exception">Exception contains text description of happened error.</exception>
+        /// <returns>Created account</returns>
+        /// <short>Create new account by email and password</short> 
+        /// <category>Accounts</category>
+        [Create(@"accounts/simple")]
+        public MailAccountData CreateAccountSimple(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentException(@"Empty email", "email");
+
+            //Thread.CurrentThread.CurrentCulture = CurrentCulture;
+            //Thread.CurrentThread.CurrentUICulture = CurrentCulture;
+
+            string errorText = null;
+
+            try
+            {
+                var account = AccountEngine.CreateAccountSimple(email, password, out List<LoginResult> loginResults);
+
+                if (account != null)
+                    return account.ToAccountData().FirstOrDefault();
+
+                var i = 0;
+
+                foreach (var loginResult in loginResults)
+                {
+                    errorText += string.Format("#{0}:<br>", ++i);
+
+                    if (!loginResult.IngoingSuccess)
+                    {
+                        errorText += GetFormattedTextError(loginResult.IngoingException,
+                            loginResult.Imap ? ServerType.Imap : ServerType.Pop3, false) + "<br>";
+                    }
+
+                    if (!loginResult.OutgoingSuccess)
+                    {
+                        errorText += GetFormattedTextError(loginResult.OutgoingException, ServerType.Smtp, false) +
+                                     "<br>";
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //TODO: change AttachmentsUnknownError to common unknown error text
+                errorText = GetFormattedTextError(ex, MailApiResource.AttachmentsUnknownError);
+            }
+
+            throw new Exception(errorText);
+        }
+
+        private static string GetFormattedTextError(Exception ex, ServerType mailServerType, bool timeoutFlag = true)
+        {
+            var headerText = string.Empty;
+            var errorExplain = string.Empty;
+
+            switch (mailServerType)
+            {
+                case ServerType.Imap:
+                case ServerType.ImapOAuth:
+                    headerText = MailApiResource.ImapResponse;
+                    if (timeoutFlag)
+                        errorExplain = MailApiResource.ImapConnectionTimeoutError;
+                    break;
+                case ServerType.Pop3:
+                    headerText = MailApiResource.Pop3Response;
+                    if (timeoutFlag)
+                        errorExplain = MailApiResource.Pop3ConnectionTimeoutError;
+                    break;
+                case ServerType.Smtp:
+                    headerText = MailApiResource.SmtRresponse;
+                    if (timeoutFlag)
+                        errorExplain = MailApiResource.SmtpConnectionTimeoutError;
+                    break;
+            }
+
+            return GetFormattedTextError(ex, errorExplain, headerText);
+
+        }
+
+        //TODO: Remove HTML tags from response data
+        private static string GetFormattedTextError(Exception ex, string errorExplain = "", string headerText = "")
+        {
+            if (!string.IsNullOrEmpty(headerText))
+                headerText = string.Format("<span class=\"attempt_header\">{0}</span><br/>", headerText);
+
+            if (string.IsNullOrEmpty(errorExplain))
+                errorExplain = ex.InnerException == null ||
+                                string.IsNullOrEmpty(ex.InnerException.Message)
+                                    ? ex.Message
+                                    : ex.InnerException.Message;
+
+            var errorText = string.Format("{0}{1}",
+                          headerText,
+                          errorExplain);
+
+            return errorText;
         }
     }
 }
