53fcbfc4412bb71eda9ffe955409f80a149e07a9
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2020-04-08 16:03:42 +0300

    mail: API: Added method GetFilteredConversations in MailController (+ added new partial class MailConversationController)

---
+++ products/ASC.Mail/Server/Controllers/MailConversationController.cs
@@ -0,0 +1,97 @@
+﻿using ASC.Api.Core;
+using ASC.Mail.Enums;
+using ASC.Mail.Models;
+using ASC.Web.Api.Routing;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ASC.Mail.Controllers
+{
+    public partial class MailController : ControllerBase
+    {
+        /// <summary>
+        ///    Returns filtered conversations, if were changes since last check date
+        /// </summary>
+        /// <param optional="true" name="folder">Folder ID - integer. 1 - inbox, 2 - sent, 3 - drafts, 4 - trash, 5 - spam.</param>
+        /// <param optional="true" name="unread">Message unread status. bool flag. Search in unread(true), read(false) or all(null) messages.</param>
+        /// <param optional="true" name="attachments">Message attachments status. bool flag. Search messages with attachments(true), without attachments(false) or all(null) messages.</param>
+        /// <param optional="true" name="period_from">Begin search period date</param>
+        /// <param optional="true" name="period_to">End search period date</param>
+        /// <param optional="true" name="important">Message has importance flag. bool flag.</param>
+        /// <param optional="true" name="from_address">Address to find 'From' field</param>
+        /// <param optional="true" name="to_address">Address to find 'To' field</param>
+        /// <param optional="true" name="mailbox_id">Recipient mailbox id.</param>
+        /// <param optional="true" name="tags">Messages tags. Id of tags linked with target messages.</param>
+        /// <param optional="true" name="search">Text to search in messages body and subject.</param>
+        /// <param optional="true" name="page_size">Count of messages on page</param>
+        /// <param name="sortorder">Sort order by date. String parameter: "ascending" - ascended, "descending" - descended.</param> 
+        /// <param optional="true" name="from_date">Date from wich conversations search performed</param>
+        /// <param optional="true" name="from_message">Message from wich conversations search performed</param>
+        /// <param optional="true" name="with_calendar">Message has calendar flag. bool flag.</param>
+        /// <param optional="true" name="user_folder_id">id of user's folder</param>
+        /// <param name="prev_flag"></param>
+        /// <returns>List of filtered chains</returns>
+        /// <short>Gets filtered conversations</short>
+        /// <category>Conversations</category>
+        [Read(@"conversations")]
+        public IEnumerable<MailMessageData> GetFilteredConversations(int? folder,
+            bool? unread,
+            bool? attachments,
+            long? period_from,
+            long? period_to,
+            bool? important,
+            string from_address,
+            string to_address,
+            int? mailbox_id,
+            IEnumerable<int> tags,
+            string search,
+            int? page_size,
+            string sortorder,
+            ApiDateTime from_date,
+            int? from_message,
+            bool? prev_flag,
+            bool? with_calendar,
+            int? user_folder_id)
+        {
+            var primaryFolder = user_folder_id.HasValue
+                ? FolderType.UserFolder
+                : folder.HasValue ? (FolderType)folder.Value : FolderType.Inbox;
+
+            var filter = new MailSearchFilterData
+            {
+                PrimaryFolder = primaryFolder,
+                Unread = unread,
+                Attachments = attachments,
+                PeriodFrom = period_from,
+                PeriodTo = period_to,
+                Important = important,
+                FromAddress = from_address,
+                ToAddress = to_address,
+                MailboxId = mailbox_id,
+                CustomLabels = new List<int>(tags),
+                SearchText = search,
+                PageSize = page_size.GetValueOrDefault(25),
+                Sort = Defines.ORDER_BY_DATE_CHAIN,
+                SortOrder = sortorder,
+                WithCalendar = with_calendar,
+                UserFolderId = user_folder_id,
+                FromDate = from_date,
+                FromMessage = from_message.GetValueOrDefault(0),
+                PrevFlag = prev_flag.GetValueOrDefault(false)
+            };
+
+            var conversations = MessageEngine.GetConversations(filter, out bool hasMore);
+
+            if (hasMore)
+            {
+                ApiContext.SetTotalCount(page_size.GetValueOrDefault(25) + 1);
+            }
+            else
+            {
+                ApiContext.SetTotalCount(conversations.Count);
+            }
+
+            return conversations;
+        }
+    }
+}
