437b6ce2403e1864586a748ed34fb799735db367
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2020-04-24 15:56:23 +0300

    mail: core: Fixed all TemplateEngineTests issues

--- products/ASC.Mail/Tests/Common/Engine/TemplateEngineTests.cs
+++ products/ASC.Mail/Tests/Common/Engine/TemplateEngineTests.cs
@@ -1,130 +1,259 @@
-///*
-// *
-// * (c) Copyright Ascensio System Limited 2010-2020
-// *
-// * This program is freeware. You can redistribute it and/or modify it under the terms of the GNU 
-// * General Public License (GPL) version 3 as published by the Free Software Foundation (https://www.gnu.org/copyleft/gpl.html). 
-// * In accordance with Section 7(a) of the GNU GPL its Section 15 shall be amended to the effect that 
-// * Ascensio System SIA expressly excludes the warranty of non-infringement of any third-party rights.
-// *
-// * THIS PROGRAM IS DISTRIBUTED WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR
-// * FITNESS FOR A PARTICULAR PURPOSE. For more details, see GNU GPL at https://www.gnu.org/copyleft/gpl.html
-// *
-// * You can contact Ascensio System SIA by email at sales@onlyoffice.com
-// *
-// * The interactive user interfaces in modified source and object code versions of ONLYOFFICE must display 
-// * Appropriate Legal Notices, as required under Section 5 of the GNU GPL version 3.
-// *
-// * Pursuant to Section 7 § 3(b) of the GNU GPL you must retain the original ONLYOFFICE logo which contains 
-// * relevant author attributions when distributing the software. If the display of the logo in its graphic 
-// * form is not reasonably feasible for technical reasons, you must include the words "Powered by ONLYOFFICE" 
-// * in every copy of the program you distribute. 
-// * Pursuant to Section 7 § 3(e) we decline to grant you any rights under trademark law for use of our trademarks.
-// *
-//*/
+/*
+ *
+ * (c) Copyright Ascensio System Limited 2010-2020
+ *
+ * This program is freeware. You can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License (GPL) version 3 as published by the Free Software Foundation (https://www.gnu.org/copyleft/gpl.html). 
+ * In accordance with Section 7(a) of the GNU GPL its Section 15 shall be amended to the effect that 
+ * Ascensio System SIA expressly excludes the warranty of non-infringement of any third-party rights.
+ *
+ * THIS PROGRAM IS DISTRIBUTED WITHOUT ANY WARRANTY; WITHOUT EVEN THE IMPLIED WARRANTY OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE. For more details, see GNU GPL at https://www.gnu.org/copyleft/gpl.html
+ *
+ * You can contact Ascensio System SIA by email at sales@onlyoffice.com
+ *
+ * The interactive user interfaces in modified source and object code versions of ONLYOFFICE must display 
+ * Appropriate Legal Notices, as required under Section 5 of the GNU GPL version 3.
+ *
+ * Pursuant to Section 7 § 3(b) of the GNU GPL you must retain the original ONLYOFFICE logo which contains 
+ * relevant author attributions when distributing the software. If the display of the logo in its graphic 
+ * form is not reasonably feasible for technical reasons, you must include the words "Powered by ONLYOFFICE" 
+ * in every copy of the program you distribute. 
+ * Pursuant to Section 7 § 3(e) we decline to grant you any rights under trademark law for use of our trademarks.
+ *
+*/
 
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using ASC.Core;
-//using ASC.Core.Users;
-//using ASC.Mail.Core;
-//using ASC.Mail.Models;
-//using ASC.Mail.Enums;
-//using ASC.Mail.Utils;
-//using NUnit.Framework;
-//using System.IO;
-//using ASC.Mail.Aggregator.Tests.Common.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ASC.Core;
+using ASC.Core.Users;
+using ASC.Mail.Aggregator.Tests.Common.Utils;
+using ASC.Mail.Core;
+using ASC.Mail.Models;
+using ASC.Mail.Enums;
+using ASC.Mail.Exceptions;
+using ASC.Mail.Utils;
+using NUnit.Framework;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ASC.Common;
+using ASC.Api.Core.Auth;
+using ASC.Api.Core.Middleware;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Autofac;
+using ASC.Mail.Core.Engine;
+using ASC.ElasticSearch;
+using ASC.Common.Logging;
+using ASC.Api.Core;
 
-//namespace ASC.Mail.Aggregator.Tests.Common.Engine
-//{
-//    [TestFixture]
-//    internal class TemplateEngineTests
-//    {
-//        private const int CURRENT_TENANT = 0;
-//        public const string PASSWORD = "123456";
-//        public const string DOMAIN = "gmail.com";
+namespace ASC.Mail.Aggregator.Tests.Common.Engine
+{
+    [TestFixture]
+    internal class TemplateEngineTests
+    {
+        private const int CURRENT_TENANT = 0;
+        public const string PASSWORD = "123456";
+        public const string DOMAIN = "gmail.com";
 
-//        private static readonly string TestFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-//           @"..\..\Data\");
-//        private const string EML1_FILE_NAME = @"bad_encoding.eml";
-//        private static readonly string Eml1Path = TestFolderPath + EML1_FILE_NAME;
+        public UserInfo TestUser { get; private set; }
+        private MailBoxData TestMailbox { get; set; }
 
-//        public UserInfo TestUser { get; private set; }
-//        private EngineFactory _engineFactory;
-//        private MailBoxData _testMailbox;
+        private static readonly string TestFolderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+           @"..\..\..\Data\");
+        private const string EML1_FILE_NAME = @"bad_encoding.eml";
+        private static readonly string Eml1Path = TestFolderPath + EML1_FILE_NAME;
 
-//        [SetUp]
-//        public void SetUp()
-//        {
-//            CoreContext.TenantManager.SetCurrentTenant(CURRENT_TENANT);
-//            SecurityContext.AuthenticateMe(ASC.Core.Configuration.Constants.CoreSystem);
+        IServiceProvider ServiceProvider { get; set; }
+        IHost TestHost { get; set; }
 
-//            TestUser = TestHelper.CreateNewRandomEmployee();
+        [OneTimeSetUp]
+        public void Prepare()
+        {
+            var args = new string[] { };
 
-//            _engineFactory = new EngineFactory(CURRENT_TENANT, TestUser.ID.ToString());
+            TestHost = Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostContext, config) =>
+                {
+                    var buided = config.Build();
+                    var path = buided["pathToConf"];
+                    if (!Path.IsPathRooted(path))
+                    {
+                        path = Path.GetFullPath(Path.Combine(hostContext.HostingEnvironment.ContentRootPath, path));
+                    }
 
-//            var mailboxSettings = _engineFactory.MailBoxSettingEngine.GetMailBoxSettings(DOMAIN);
+                    config.SetBasePath(path);
 
-//            var testMailboxes = mailboxSettings.ToMailboxList(TestUser.Email, PASSWORD, CURRENT_TENANT, TestUser.ID.ToString());
+                    config
+                        .AddInMemoryCollection(new Dictionary<string, string>
+                        {
+                        {"pathToConf", path}
+                        })
+                        .AddJsonFile("appsettings.json")
+                        .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", true)
+                        .AddJsonFile("storage.json")
+                        .AddJsonFile("kafka.json")
+                        .AddJsonFile($"kafka.{hostContext.HostingEnvironment.EnvironmentName}.json", true)
+                        .AddEnvironmentVariables();
 
-//            _testMailbox = testMailboxes.FirstOrDefault();
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHttpContextAccessor();
 
-//            if (_testMailbox == null || !_engineFactory.MailboxEngine.SaveMailBox(_testMailbox))
-//            {
-//                throw new Exception(string.Format("Can't create mailbox with email: {0}", TestUser.Email));
-//            }
-//            SecurityContext.AuthenticateMe(TestUser.ID);
-//        }
+                    var diHelper = new DIHelper(services);
 
-//        [TearDown]
-//        public void CleanUp()
-//        {
-//            if (TestUser == null || TestUser.ID == Guid.Empty)
-//                return;
+                    diHelper
+                        .AddCookieAuthHandler()
+                        .AddCultureMiddleware()
+                        .AddIpSecurityFilter()
+                        .AddPaymentFilter()
+                        .AddProductSecurityFilter()
+                        .AddTenantStatusFilter();
 
-//            CoreContext.TenantManager.SetCurrentTenant(CURRENT_TENANT);
+                    diHelper.AddNLogManager("ASC.Api", "ASC.Web");
 
-//            SecurityContext.AuthenticateMe(ASC.Core.Configuration.Constants.CoreSystem);
+                    diHelper
+                        .AddTenantManagerService()
+                        .AddUserManagerService()
+                        .AddSecurityContextService()
+                        .AddMailBoxSettingEngineService()
+                        .AddMailboxEngineService()
+                        .AddApiContextService()
+                        .AddApiHelperService()
+                        .AddFolderEngineService()
+                        .AddUserFolderEngineService()
+                        .AddFactoryIndexerHelperService()
+                        .AddFactoryIndexerService()
+                        .AddFactoryIndexerService<MailWrapper>()
+                        .AddMailGarbageEngineService()
+                        .AddTestEngineService()
+                        .AddDraftEngineService()
+                        .AddTemplateEngineService()
+                        .AddMessageEngineService()
+                        .AddCoreSettingsService();
 
-//            CoreContext.UserManager.DeleteUser(TestUser.ID);
+                    var builder = new ContainerBuilder();
+                    var container = builder.Build();
 
-//            // Clear TestUser1 mail data
-//            var eraser = _engineFactory.MailGarbageEngine;
+                    services.TryAddSingleton(container);
 
-//            eraser.ClearUserMail(TestUser.ID, CoreContext.TenantManager.GetCurrentTenant());
-//        }
+                    //services.AddAutofac(hostContext.Configuration, hostContext.HostingEnvironment.ContentRootPath);
+                })
+                .UseConsoleLifetime()
+                .Build();
 
-//        [Test]
-//        public void CreateTemplate()
-//        {
-//            var folders = _engineFactory.FolderEngine.GetFolders();
+            TestHost.Start();
 
-//            Assert.IsNotEmpty(folders);
+            ServiceProvider = TestHost.Services;
+        }
 
-//            var templateItem = new MailTemplateData(0, _testMailbox, "test@gmail.com", new List<string>(), 
-//                new List<string>(), new List<string>(), "subject", MailUtil.CreateMessageId(), null, false, 
-//                null, "Test body", MailUtil.CreateStreamId(), new List<MailAttachmentData>());
+        [SetUp]
+        public void SetUp()
+        {
+            using var scope = ServiceProvider.CreateScope();
+            var userManager = scope.ServiceProvider.GetService<UserManager>();
+            var tenantManager = scope.ServiceProvider.GetService<TenantManager>();
+            var securityContext = scope.ServiceProvider.GetService<SecurityContext>();
+            var mailBoxSettingEngine = scope.ServiceProvider.GetService<MailBoxSettingEngine>();
+            var mailboxEngine = scope.ServiceProvider.GetService<MailboxEngine>();
+            var apiHelper = scope.ServiceProvider.GetService<ApiHelper>();
 
-//            var data = _engineFactory.TemplateEngine.Save(templateItem);
+            tenantManager.SetCurrentTenant(CURRENT_TENANT);
+            securityContext.AuthenticateMe(ASC.Core.Configuration.Constants.CoreSystem);
 
-//            Assert.Greater(data.Id, 0);
+            TestUser = TestHelper.CreateNewRandomEmployee(userManager, securityContext, tenantManager, apiHelper);
 
-//            folders = _engineFactory.FolderEngine.GetFolders();
+            var mailboxSettings = mailBoxSettingEngine.GetMailBoxSettings(DOMAIN);
 
-//            var templateFolder = folders.FirstOrDefault(f => f.id == FolderType.Templates);
+            var testMailboxes = mailboxSettings.ToMailboxList(TestUser.Email, PASSWORD, CURRENT_TENANT, TestUser.ID.ToString());
 
-//            Assert.IsNotNull(templateFolder);
-//            Assert.AreEqual(1, templateFolder.totalMessages);
-//            Assert.AreEqual(0, templateFolder.unreadMessages);
-//            Assert.AreEqual(0, templateFolder.total);
-//            Assert.AreEqual(0, templateFolder.unread);
+            TestMailbox = testMailboxes.FirstOrDefault();
 
-//            var savedTemplateData = _engineFactory.MessageEngine.GetMessage(data.Id, new MailMessageData.Options());
+            if (TestMailbox == null || !mailboxEngine.SaveMailBox(TestMailbox))
+            {
+                throw new Exception(string.Format("Can't create mailbox with email: {0}", TestUser.Email));
+            }
+        }
 
-//            Assert.AreEqual("subject", savedTemplateData.Subject);
-//            Assert.AreEqual("test@gmail.com", savedTemplateData.From);
-//        }
-//    }
-//}
+        [TearDown]
+        public void CleanUp()
+        {
+            if (TestUser == null || TestUser.ID == Guid.Empty)
+                return;
+
+            using var scope = ServiceProvider.CreateScope();
+
+            var tenantManager = scope.ServiceProvider.GetService<TenantManager>();
+            var securityContext = scope.ServiceProvider.GetService<SecurityContext>();
+
+            tenantManager.SetCurrentTenant(CURRENT_TENANT);
+            securityContext.AuthenticateMe(ASC.Core.Configuration.Constants.CoreSystem);
+
+            // Remove TestUser profile
+            var userManager = scope.ServiceProvider.GetService<UserManager>();
+            userManager.DeleteUser(TestUser.ID);
+
+            // Clear TestUser mail index
+            var factoryIndexer = scope.ServiceProvider.GetService<FactoryIndexer<MailWrapper>>();
+            var factoryIndexerHelper = scope.ServiceProvider.GetService<FactoryIndexerHelper>();
+
+            var t = ServiceProvider.GetService<MailWrapper>();
+            if (factoryIndexerHelper.Support(t))
+                factoryIndexer.DeleteAsync(s => s.Where(m => m.UserId, TestUser.ID)).Wait();
+
+            // Clear TestUser mail data
+            var mailGarbageEngine = scope.ServiceProvider.GetService<MailGarbageEngine>();
+            mailGarbageEngine.ClearUserMail(TestUser.ID, tenantManager.GetCurrentTenant());
+        }
+
+        [Test]
+        public void CreateTemplate()
+        {
+            using var scope = ServiceProvider.CreateScope();
+            var userManager = scope.ServiceProvider.GetService<UserManager>();
+            var tenantManager = scope.ServiceProvider.GetService<TenantManager>();
+            var securityContext = scope.ServiceProvider.GetService<SecurityContext>();
+
+            tenantManager.SetCurrentTenant(CURRENT_TENANT);
+            securityContext.AuthenticateMe(TestUser.ID);
+
+            var folderEngine = scope.ServiceProvider.GetService<FolderEngine>();
+            var coreSettings = scope.ServiceProvider.GetService<CoreSettings>();
+            var templateEngine = scope.ServiceProvider.GetService<TemplateEngine>();
+            var messageEngine = scope.ServiceProvider.GetService<MessageEngine>();
+
+            var folders = folderEngine.GetFolders();
+
+            Assert.IsNotEmpty(folders);
+
+            var templateItem = new MailTemplateData(0, TestMailbox, "test@gmail.com", new List<string>(),
+                new List<string>(), new List<string>(), "subject", 
+                MailUtil.CreateMessageId(tenantManager, coreSettings), null, false,
+                null, "Test body", MailUtil.CreateStreamId(), new List<MailAttachmentData>());
+
+            var data = templateEngine.Save(templateItem);
+
+            Assert.Greater(data.Id, 0);
+
+            folders = folderEngine.GetFolders();
+
+            var templateFolder = folders.FirstOrDefault(f => f.id == FolderType.Templates);
+
+            Assert.IsNotNull(templateFolder);
+            Assert.AreEqual(1, templateFolder.totalMessages);
+            Assert.AreEqual(0, templateFolder.unreadMessages);
+            Assert.AreEqual(0, templateFolder.total);
+            Assert.AreEqual(0, templateFolder.unread);
+
+            var savedTemplateData = messageEngine.GetMessage(data.Id, new MailMessageData.Options());
+
+            Assert.AreEqual("subject", savedTemplateData.Subject);
+            Assert.AreEqual("test@gmail.com", savedTemplateData.From);
+        }
+    }
+}
