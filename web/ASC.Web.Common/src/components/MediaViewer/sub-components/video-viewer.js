62fee1ecfe1cd6e48f6e117a8f9a678b2b0a669c
NikolayRechkin <nikolaynikcher@gmail.com>
2020-04-30 11:20:30 +0300

    web: common: MediaViewer. refactoring video viewer

--- web/ASC.Web.Common/src/components/MediaViewer/sub-components/video-viewer.js
+++ web/ASC.Web.Common/src/components/MediaViewer/sub-components/video-viewer.js
@@ -37,43 +37,45 @@ const StyledVideoControlBtn = styled.div`
 `;
 
 const VideoControlBtn = props => {
-    return (
-        <StyledVideoControlBtn {...props} >
-            {props.children}
-        </StyledVideoControlBtn>
-    );
+  return (
+    <StyledVideoControlBtn {...props} >
+      {props.children}
+    </StyledVideoControlBtn>
+  );
+}
+VideoControlBtn.propTypes = {
+  children: PropTypes.any
 }
 const Controls = props => {
   return (
     <StyledControls {...props} >
-        {props.children}
+      {props.children}
     </StyledControls>
   );
 }
-
-class PlayBtn extends Component{
-  constructor(props) {
-      super(props);
-  }
-  render(){
-      return(
-          <VideoControlBtn  onClick={this.props.onClick}>
-              {this.props.playing ? <Icons.ItemPausedIcon size="medium" isfill={true} color="#fff" /> : <Icons.ItemActiveIcon size="medium" isfill={true} color="#fff" />}
-          </VideoControlBtn>
-      );
-  }
+Controls.propTypes = {
+  children: PropTypes.any
 }
-class FullScreenBtn extends Component{
-  constructor(props) {
-      super(props);
-  }
-  render(){
-      return(
-          <VideoControlBtn  onClick={this.props.onClick}>
-              <Icons.ExternalLinkIcon size="medium" isfill={true} color="#fff" />
-          </VideoControlBtn>
-      );
-  }
+const PlayBtn = props => {
+  return (
+    <VideoControlBtn onClick={props.onClick}>
+      {props.playing ? <Icons.ItemPausedIcon size="medium" isfill={true} color="#fff" /> : <Icons.ItemActiveIcon size="medium" isfill={true} color="#fff" />}
+    </VideoControlBtn>
+  );
+}
+PlayBtn.propTypes = {
+  playing: PropTypes.bool,
+  onClick: PropTypes.func
+}
+const FullScreenBtn = props => {
+  return (
+    <VideoControlBtn onClick={props.onClick}>
+      <Icons.ExternalLinkIcon size="medium" isfill={true} color="#fff" />
+    </VideoControlBtn>
+  );
+}
+FullScreenBtn.propTypes = {
+  onClick: PropTypes.func
 }
 
 const StyledValumeContainer = styled.div`
@@ -137,31 +139,41 @@ const StyledVideoViewer = styled.div`
     }
 `;
 
-class ValumeBtn extends Component{
+class ValumeBtn extends Component {
+  
   constructor(props) {
     super(props);
   }
 
-  render(){
-      return(
-        <StyledValumeContainer>
-          <div className="muteConteiner">
-            <Progress
-                className="mute"
-                width = {this.props.width}
-                value = {this.props.volume}
-                onMouseDown={this.props.onMouseDown}
-                onChange={this.props.onChange}
-                onMouseUp={this.props.handleSeekMouseUp}
-              />
-          </div>
-          
-          <VideoControlBtn  onClick={this.props.onChangeMute}>
-              {this.props.muted ? <Icons.ToggleButtonIcon size="medium" /> : <Icons.ToggleButtonCheckedIcon size="medium" />}
-          </VideoControlBtn>
-        </StyledValumeContainer>
-      );
+  render() {
+    return (
+      <StyledValumeContainer>
+        <div className="muteConteiner">
+          <Progress
+            className="mute"
+            width={this.props.width}
+            value={this.props.volume}
+            onMouseDown={this.props.onMouseDown}
+            onChange={this.props.onChange}
+            onMouseUp={this.props.handleSeekMouseUp}
+          />
+        </div>
+
+        <VideoControlBtn onClick={this.props.onChangeMute}>
+          {this.props.muted ? <Icons.ToggleButtonIcon size="medium" /> : <Icons.ToggleButtonCheckedIcon size="medium" />}
+        </VideoControlBtn>
+      </StyledValumeContainer>
+    );
   }
+}
+ValumeBtn.propTypes = {
+  width: PropTypes.number,
+  volume: PropTypes.number,
+  muted: PropTypes.bool,
+  onMouseDown: PropTypes.func,
+  onChange: PropTypes.func,
+  handleSeekMouseUp: PropTypes.func,
+  onChangeMute: PropTypes.func
 }
 
 
@@ -223,7 +235,7 @@ class VideoViewer extends Component {
   }
 
   handleVolumeChange = e => {
-    this.setState({ 
+    this.setState({
       volume: parseFloat(e.target.value),
       muted: false
     })
@@ -236,7 +248,7 @@ class VideoViewer extends Component {
   handleSetPlaybackRate = e => {
     this.setState({ playbackRate: parseFloat(e.target.value) })
   }
- 
+
   handleTogglePIP = () => {
     this.setState({ pip: !this.state.pip })
   }
@@ -310,42 +322,42 @@ class VideoViewer extends Component {
 
   resizePlayer = (videoSize, screenSize) => {
     var ratio = videoSize.h / videoSize.w;
-    
+
     if (videoSize.h > screenSize.h) {
-        videoSize.h = screenSize.h;
-        videoSize.w = videoSize.h / ratio;
+      videoSize.h = screenSize.h;
+      videoSize.w = videoSize.h / ratio;
     }
     if (videoSize.w > screenSize.w) {
-        videoSize.w = screenSize.w;
-        videoSize.h = videoSize.w * ratio;
+      videoSize.w = screenSize.w;
+      videoSize.h = videoSize.w * ratio;
     }
-    
+
     return {
-        width: videoSize.w,
-        height: videoSize.h
+      width: videoSize.w,
+      height: videoSize.h
     };
-    
+
   };
 
-  render () {
+  render() {
     const { url, playing, controls, light, volume, muted, loop, played, loaded, duration, playbackRate, pip } = this.state
-  
+
     var screenSize = {
-      w : window.innerWidth,
-      h : window.innerHeight
+      w: window.innerWidth,
+      h: window.innerHeight
     };
     screenSize.h -= 48 + 48 + 40;
 
-    let width = screenSize.w ;
-    let height = screenSize.h ;
+    let width = screenSize.w;
+    let height = screenSize.h;
 
     var centerAreaOx = screenSize.w / 2 + document.documentElement.scrollLeft;
     var centerAreaOy = screenSize.h / 2 + document.documentElement.scrollTop;
 
-    if(document.getElementsByTagName('video')[0]){
-      width = this.props.isVideo ? document.getElementsByTagName('video')[0].videoWidth || 480 : screenSize.w - 300 ;
+    if (document.getElementsByTagName('video')[0]) {
+      width = this.props.isVideo ? document.getElementsByTagName('video')[0].videoWidth || 480 : screenSize.w - 300;
       height = this.props.isVideo ? document.getElementsByTagName('video')[0].videoHeight || 270 : 0;
-      console.log(width,height)
+      console.log(width, height)
       let resize = this.resizePlayer(
         {
           w: width,
@@ -359,72 +371,72 @@ class VideoViewer extends Component {
     }
 
     let left = centerAreaOx - width / 2;
-    let top = this.props.isVideo ? centerAreaOy - height / 2 + 48  : centerAreaOy + 48;
+    let top = this.props.isVideo ? centerAreaOy - height / 2 + 48 : centerAreaOy + 48;
 
     return (
-        <StyledVideoViewer
-            isVideo = {this.props.isVideo}
-            width = {width}
-            height = {height}
-            left = {left}
-            top = {top}
-        >
-          <div>
-            <div className='playerWrapper'>
-              <ReactPlayer
-                ref={this.ref}
-                className='react-player'
-                width='100%'
-                height='100%'
-                url={url}
-                pip={pip}
-                playing={playing}
-                controls={controls}
-                light={light}
-                loop={loop}
-                playbackRate={playbackRate}
-                volume={volume}
-                muted={muted}
-                onReady={() => console.log('onReady')}
-                onStart={() => console.log('onStart')}
-                onPlay={this.handlePlay}
-                onEnablePIP={this.handleEnablePIP}
-                onDisablePIP={this.handleDisablePIP}
-                onPause={this.handlePause}
-                onBuffer={() => console.log('onBuffer')}
-                onSeek={e => console.log('onSeek', e)}
-                onEnded={this.handleEnded}
-                onError={e => console.log('onError', e)}
-                onProgress={this.handleProgress}
-                onDuration={this.handleDuration}
-              />
-            </div>
-            <Controls
-              left = {left}
-              top = {top+height}
-              isVideo = {this.props.isVideo}
-            >
-              <PlayBtn onClick={this.handlePlayPause} playing={playing} />
-              <Progress
-                value={played}
-                width={width - 220}
-                onMouseDown={this.handleSeekMouseDown}
-                onChange={this.handleSeekChange}
-                onMouseUp={this.handleSeekMouseUp}
-              />
-              <StyledDuration>-<Duration seconds={duration * (1 - played)} /></StyledDuration>
-              <ValumeBtn
-                width={64}
-                muted={muted}
-                volume={muted ? 0 : volume}
-                onChangeMute={this.handleToggleMuted}
-                onChange={this.handleVolumeChange}
-              />
-              {this.props.isVideo && <FullScreenBtn onClick={this.handleClickFullscreen} />}
-
-            </Controls>
+      <StyledVideoViewer
+        isVideo={this.props.isVideo}
+        width={width}
+        height={height}
+        left={left}
+        top={top}
+      >
+        <div>
+          <div className='playerWrapper'>
+            <ReactPlayer
+              ref={this.ref}
+              className='react-player'
+              width='100%'
+              height='100%'
+              url={url}
+              pip={pip}
+              playing={playing}
+              controls={controls}
+              light={light}
+              loop={loop}
+              playbackRate={playbackRate}
+              volume={volume}
+              muted={muted}
+              onReady={() => console.log('onReady')}
+              onStart={() => console.log('onStart')}
+              onPlay={this.handlePlay}
+              onEnablePIP={this.handleEnablePIP}
+              onDisablePIP={this.handleDisablePIP}
+              onPause={this.handlePause}
+              onBuffer={() => console.log('onBuffer')}
+              onSeek={e => console.log('onSeek', e)}
+              onEnded={this.handleEnded}
+              onError={e => console.log('onError', e)}
+              onProgress={this.handleProgress}
+              onDuration={this.handleDuration}
+            />
           </div>
-        </StyledVideoViewer>
+          <Controls
+            left={left}
+            top={top + height}
+            isVideo={this.props.isVideo}
+          >
+            <PlayBtn onClick={this.handlePlayPause} playing={playing} />
+            <Progress
+              value={played}
+              width={width - 220}
+              onMouseDown={this.handleSeekMouseDown}
+              onChange={this.handleSeekChange}
+              onMouseUp={this.handleSeekMouseUp}
+            />
+            <StyledDuration>-<Duration seconds={duration * (1 - played)} /></StyledDuration>
+            <ValumeBtn
+              width={64}
+              muted={muted}
+              volume={muted ? 0 : volume}
+              onChangeMute={this.handleToggleMuted}
+              onChange={this.handleVolumeChange}
+            />
+            {this.props.isVideo && <FullScreenBtn onClick={this.handleClickFullscreen} />}
+
+          </Controls>
+        </div>
+      </StyledVideoViewer>
     )
   }
 }
