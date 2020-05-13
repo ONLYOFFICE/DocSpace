1205f03a5aaa55949e1eebf9d2e229d67b564ddb
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-05-13 10:00:04 +0300

    Web: Files: added download as dialog

--- products/ASC.Files/Client/src/components/panels/SharingPanel/SharingRow.js
+++ products/ASC.Files/Client/src/components/panels/SharingPanel/SharingRow.js
@@ -29,7 +29,9 @@ const SharingRow = (props) => {
     onShowEmbeddingPanel,
   } = props;
 
-  const linkVisible = selection && selection.length === 1 && item.shareLink;
+  const linkVisible =
+    (selection && selection.length === 1 && item.shareLink) ||
+    (!selection.length && item.shareLink);
 
   const onCopyInternalLink = () => {
     const internalLink = item.shareLink.split("&");
