059994458527992cc002fadd1c25786bc091b961
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-05-29 09:30:49 +0300

    Web: Files: added dropzone uploading, code refactoring

--- products/ASC.Files/Client/src/components/Article/MainButton/index.js
+++ products/ASC.Files/Client/src/components/Article/MainButton/index.js
@@ -2,28 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
-import { MainButton, DropDownItem, toastr, utils } from "asc-web-components";
+import { MainButton, DropDownItem } from "asc-web-components";
 import { withTranslation, I18nextProvider } from "react-i18next";
-import {
-  setAction,
-  fetchFiles,
-  setTreeFolders,
-} from "../../../store/files/actions";
-import { isCanCreate, loopTreeFolders } from "../../../store/files/selectors";
-import store from "../../../store/store";
+import { setAction } from "../../../store/files/actions";
+import { isCanCreate } from "../../../store/files/selectors";
 import i18n from "../i18n";
-import { utils as commonUtils, constants, api } from "asc-web-common";
+import { utils as commonUtils, constants } from "asc-web-common";
 
 const { changeLanguage } = commonUtils;
 const { FileAction } = constants;
 
 class PureArticleMainButtonContent extends React.Component {
-  state = {
-    files: [],
-    uploadedFiles: 0,
-    totalSize: 0,
-    percent: 0
-  };
 
   onCreate = (format) => {
     this.props.setAction({
@@ -34,199 +23,13 @@ class PureArticleMainButtonContent extends React.Component {
   };
 
   onUploadFileClick = () => this.inputFilesElement.click();
-
   onUploadFolderClick = () => this.inputFolderElement.click();
 
-  updateFiles = () => {
-    const { onLoading, filter, currentFolderId, treeFolders, setTreeFolders } = this.props;
-
-    onLoading(true);
-    const newFilter = filter.clone();
-    fetchFiles(currentFolderId, newFilter, store.dispatch, treeFolders)
-      .then((data) => {
-        const path = data.selectedFolder.pathParts;
-        const newTreeFolders = treeFolders;
-        const folders = data.selectedFolder.folders;
-        const foldersCount = data.selectedFolder.foldersCount;
-        loopTreeFolders(path, newTreeFolders, folders, foldersCount);
-        setTreeFolders(newTreeFolders);
-      })
-      .catch((err) => toastr.error(err))
-      .finally(() => {
-        onLoading(false);
-      });
-  };
-
-  sendChunk = (files, location, requestsDataArray, isLatestFile, indexOfFile) => {
-    const sendRequestFunc = (index) => {
-      let newState = {};
-      api.files
-        .uploadFile(location, requestsDataArray[index])
-        .then((res) => {
-          let newPercent = this.state.percent;
-          const percent = (newPercent +=
-            (files[indexOfFile].size / this.state.totalSize) * 100);
-          if (res.data.data && res.data.data.uploaded) {
-            files[indexOfFile].uploaded = true;
-            newState = { files, percent };
-          }
-          if (index + 1 !== requestsDataArray.length) {
-            sendRequestFunc(index + 1);
-          } else if (isLatestFile) {
-            this.updateFiles();
-            newState = Object.assign({}, newState, {
-              uploadedFiles: this.state.uploadedFiles + 1,
-            });
-            return;
-          } else {
-            newState = Object.assign({}, newState, {
-              uploadedFiles: this.state.uploadedFiles + 1,
-            });
-            this.startSessionFunc(indexOfFile + 1);
-          }
-        })
-        .catch((err) => toastr.error(err))
-        .finally(() => {
-          if (
-            newState.hasOwnProperty("files") ||
-            newState.hasOwnProperty("percent") ||
-            newState.hasOwnProperty("uploadedFiles")
-          ) {
-            let progressVisible = true;
-            let uploadedFiles = newState.uploadedFiles;
-            let percent = newState.percent;
-            if (newState.uploadedFiles === files.length) {
-              percent = 100;
-              newState.percent = 0;
-              newState.uploadedFiles = 0;
-              progressVisible = false;
-            }
-            this.setState(newState, () => {
-              this.props.setProgressValue(percent);
-              this.props.setProgressLabel(
-                this.props.t("UploadingLabel", {
-                  file: uploadedFiles,
-                  totalFiles: files.length,
-                })
-              );
-              if (!progressVisible) {
-                this.props.setProgressVisible(false);
-              }
-            });
-          }
-        });
-    };
-
-    sendRequestFunc(0);
-  };
-
-  startSessionFunc = indexOfFile => {
-    const { files } = this.state;
-    const { currentFolderId } = this.props;
-    const file = files[indexOfFile];
-    const isLatestFile = indexOfFile === files.length - 1;
-
-    const fileName = file.name;
-    const fileSize = file.size;
-    const relativePath = file.webkitRelativePath
-      ? file.webkitRelativePath.slice(0, -file.name.length)
-      : "";
-
-    let location;
-    const requestsDataArray = [];
-    const chunkSize = 1024 * 1023; //~0.999mb
-    const chunks = Math.ceil(file.size / chunkSize, chunkSize);
-    let chunk = 0;
-
-    api.files
-      .startUploadSession(currentFolderId, fileName, fileSize, relativePath)
-      .then((res) => {
-        location = res.data.location;
-        while (chunk < chunks) {
-          const offset = chunk * chunkSize;
-          //console.log("current chunk..", chunk);
-          //console.log("file blob from offset...", offset);
-          //console.log(file.slice(offset, offset + chunkSize));
-
-          const formData = new FormData();
-          formData.append("file", file.slice(offset, offset + chunkSize));
-          requestsDataArray.push(formData);
-          chunk++;
-        }
-      })
-      .then(() =>
-        this.sendChunk(
-          files,
-          location,
-          requestsDataArray,
-          isLatestFile,
-          indexOfFile
-        )
-      )
-      .catch((err) => {
-        this.props.setProgressVisible(false, 0);
-        toastr.error(err);
-      });
-  };
-
-  onFileChange = (e) => {
-    const { t, setProgressVisible, setProgressLabel } = this.props;
-    const files = e.target.files;
-    //console.log("files", files);
-    const newFiles = [];
-    if(files) {
-      let total = 0;
-      for (let item of files) {
-        if (item.size !== 0) {
-          newFiles.push(item);
-          total += item.size;
-        } else {
-          toastr.error(t("ErrorUploadMessage"));
-        }
-      }
-
-      if (newFiles.length > 0) {
-        this.setState({ files: newFiles, totalSize: total }, () => {
-          setProgressVisible(true);
-          setProgressLabel(
-            this.props.t("UploadingLabel", {
-              file: 0,
-              totalFiles: newFiles.length,
-            })
-          );
-          this.startSessionFunc(0);
-        });
-      }
-    }
-  };
-
-  onInputClick = e => {
-    e.target.value = null;
-  }
+  onFileChange = (e) => this.props.startUpload(e.target.files);
+  onInputClick = e => e.target.value = null;
 
   shouldComponentUpdate(nextProps, nextState) {
-    const { files, uploadedFiles, totalSize, percent } = this.state;
-    if (nextProps.isCanCreate !== this.props.isCanCreate) {
-      return true;
-    }
-
-    if (!utils.array.isArrayEqual(nextState.files, files)) {
-      return true;
-    }
-
-    if (nextState.uploadedFiles !== uploadedFiles) {
-      return true;
-    }
-
-    if (nextState.totalSize !== totalSize) {
-      return true;
-    }
-
-    if (nextState.percent !== percent) {
-      return true;
-    }
-
-    return false;
+    return nextProps.isCanCreate !== this.props.isCanCreate;
   }
 
   render() {
@@ -309,18 +112,14 @@ ArticleMainButtonContent.propTypes = {
 };
 
 const mapStateToProps = (state) => {
-  const { selectedFolder, filter, treeFolders } = state.files;
-  const { settings, user } = state.auth;
+  const { selectedFolder } = state.files;
+  const { user } = state.auth;
 
   return {
-    settings,
-    isCanCreate: isCanCreate(selectedFolder, user),
-    currentFolderId: selectedFolder.id,
-    filter,
-    treeFolders,
+    isCanCreate: isCanCreate(selectedFolder, user)
   };
 };
 
-export default connect(mapStateToProps, { setAction, setTreeFolders })(
+export default connect(mapStateToProps, { setAction })(
   withRouter(ArticleMainButtonContent)
 );
