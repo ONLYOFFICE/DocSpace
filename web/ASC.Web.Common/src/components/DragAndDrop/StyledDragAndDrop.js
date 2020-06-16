22a7df50b7b4200b4a47542751fe48fbfd1abc93
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-06-16 11:49:22 +0300

    Web: Common: added DnD styles

--- web/ASC.Web.Common/src/components/DragAndDrop/StyledDragAndDrop.js
+++ web/ASC.Web.Common/src/components/DragAndDrop/StyledDragAndDrop.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 const StyledDragAndDrop = styled.div`
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
   user-select: none;
   border: ${props => props.drag ? "1px dashed #bbb" : "1px solid transparent"};
   background: ${props => props.dragging ? "#F8F7BF" : "none !important"};
