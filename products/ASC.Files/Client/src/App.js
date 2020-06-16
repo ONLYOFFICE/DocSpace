2bdbb60545d1deaa19df527be7512058488e1695
Ilya Oleshko <oleshko.ilya@gmail.com>
2020-06-16 19:28:09 +0300

    Web: Files: App: Use Studio layout only if we need it

--- products/ASC.Files/Client/src/App.js
+++ products/ASC.Files/Client/src/App.js
@@ -6,24 +6,25 @@ import Home from "./components/pages/Home";
 import DocEditor from "./components/pages/DocEditor";
 import { history, PrivateRoute, PublicRoute, Login, Error404, StudioLayout, Offline } from "asc-web-common";
 
+const withStudioLayout = Component => props => <StudioLayout><Component {...props} /></StudioLayout>;
+
 const App = ({ settings }) => {
   const { homepage } = settings;
+  
   return (
     navigator.onLine ? 
     <Router history={history}>
-      <StudioLayout>
         <Suspense
           fallback={<Loader className="pageLoader" type="rombs" size='40px' />}
         >
           <Switch>
             <Redirect exact from="/" to={`${homepage}`} />
-            <PrivateRoute exact path={[homepage, `${homepage}/filter`]} component={Home} />
+            <PrivateRoute exact path={[homepage, `${homepage}/filter`]} component={withStudioLayout(Home)} />
             <PrivateRoute exact path={`${homepage}/doceditor`} component={DocEditor} />
             <PublicRoute exact path={["/login","/login/error=:error", "/login/confirmed-email=:confirmedEmail"]} component={Login} />
-            <PrivateRoute component={Error404} />
+            <PrivateRoute component={withStudioLayout(Error404)} />
           </Switch>
         </Suspense>
-      </StudioLayout>
     </Router>
     : <Offline/>
   );
