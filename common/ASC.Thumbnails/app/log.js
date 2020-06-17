f4592ed73d67e178fe46969bcd27e4c38ef2e084
pavelbannov <pavel.bannov@onlyoffice.com>
2020-06-17 14:36:09 +0300

    Thumb: fix

--- common/ASC.Thumbnails/app/log.js
+++ common/ASC.Thumbnails/app/log.js
@@ -1,6 +1,6 @@
 /*
  *
- * (c) Copyright Ascensio System Limited 2010-2018
+ * (c) Copyright Ascensio System Limited 2010-2020
  *
  * This program is freeware. You can redistribute it and/or modify it under the terms of the GNU 
  * General Public License (GPL) version 3 as published by the Free Software Foundation (https://www.gnu.org/copyleft/gpl.html). 
@@ -53,6 +53,6 @@ const transports = [
     fileTransport
 ];
 
-winston.handleExceptions(fileTransport);
+winston.exceptions.handle(fileTransport);
 
-module.exports = new winston.Logger({ transports: transports, exitOnError: false});
+module.exports = winston.createLogger({ transports: transports, exitOnError: false});
