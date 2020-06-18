e6108694df6740f68b1e2b79d4e97c34953270aa
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2020-06-18 14:31:38 +0300

    Web: Files: VersionHistory: Added back button

--- products/ASC.Files/Client/src/components/pages/VersionHistory/Section/Header/index.js
+++ products/ASC.Files/Client/src/components/pages/VersionHistory/Section/Header/index.js
@@ -2,10 +2,23 @@ import React from "react";
 import styled, { css } from "styled-components";
 import { withRouter } from "react-router";
 import { Headline } from 'asc-web-common';
+import { IconButton } from "asc-web-components";
 import { connect } from "react-redux";
 import { withTranslation } from "react-i18next";
 
 const StyledContainer = styled.div`
+
+  display: flex;
+  align-items: center;
+
+  .arrow-button {
+    margin-right: 16px;
+
+    @media (max-width: 1024px) {
+      padding: 8px 0 8px 8px;
+      margin-left: -8px;
+    }
+  }
 
   @media (min-width: 1024px) {
     ${props => props.isHeaderVisible && css`width: calc(100% + 76px);`}
@@ -54,8 +67,22 @@ const SectionHeaderContent = props => {
 
   const { title } = props;
 
+  const onClickBack = () => {
+    const { history, settings } = props;
+    history.push(settings.homepage);
+  };
+
   return (
     <StyledContainer isHeaderVisible={true}>
+        <IconButton
+          iconName="ArrowPathIcon"
+          size="17"
+          color="#A3A9AE"
+          hoverColor="#657077"
+          isFill={true}
+          onClick={onClickBack}
+          className="arrow-button"
+        />
         <Headline className='headline-header' type="content" truncate={true}>{title}</Headline>
     </StyledContainer>
   );
@@ -63,6 +90,7 @@ const SectionHeaderContent = props => {
 
 const mapStateToProps = state => {
   return {
+    settings: state.auth.settings,
   };
 };
 
