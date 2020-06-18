235e10a6458f32e2c36f984e352a388d36b41a57
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2020-06-18 17:49:07 +0300

    Web: Files: VersionHistory: Added simple version viewer

--- products/ASC.Files/Client/src/components/pages/VersionHistory/index.js
+++ products/ASC.Files/Client/src/components/pages/VersionHistory/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
-import { RequestLoader } from "asc-web-components";
-import { PageLayout, utils } from "asc-web-common";
-import { withTranslation, I18nextProvider } from 'react-i18next';
+import { RequestLoader, Loader } from "asc-web-components";
+import { PageLayout, utils, api } from "asc-web-common";
+import { withTranslation, I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
 import {
@@ -12,10 +12,7 @@ import {
   ArticleBodyContent,
   ArticleMainButtonContent
 } from "../../Article";
-import {
-  SectionHeaderContent,
-  SectionBodyContent,
-} from "./Section";
+import { SectionHeaderContent, SectionBodyContent } from "./Section";
 
 const { changeLanguage } = utils;
 
@@ -30,17 +27,38 @@ class PureVersionHistory extends React.Component {
     this.state = {
       isLoading: false,
       fileId: props.match.params.fileId,
-      file: found && found[0]
+      file: found && found[0],
+      versions: null
     };
   }
 
+  componentDidMount() {
+    const { match, t } = this.props;
+    const { fileId } = match.params;
+
+    //document.title = `${t("GroupAction")} – ${t("People")}`;
+
+    if (fileId) {
+      //fetchGroup(fileId);
+      api.files.getFileVersionInfo(fileId)
+      .then((versions) => {
+        console.log("getFileVersionInfo result", versions);
+        this.setState({
+          versions
+        })
+      })
+
+      console.log("Loading file versions", fileId);
+    }
+  }
+  
   onLoading = status => {
     this.setState({ isLoading: status });
   };
 
   render() {
-    const { file } = this.state;
-    const { t } = this.props;
+    const { file, versions } = this.state;
+    const { t, settings } = this.props;
 
     console.log(`FileId ${file.id}`);
 
@@ -49,40 +67,47 @@ class PureVersionHistory extends React.Component {
         <RequestLoader
           visible={this.state.isLoading}
           zIndex={256}
-          loaderSize='16px'
+          loaderSize="16px"
           loaderColor={"#999"}
-          label={`${t('LoadingProcessing')} ${t('LoadingDescription')}`}
-          fontSize='12px'
+          label={`${t("LoadingProcessing")} ${t("LoadingDescription")}`}
+          fontSize="12px"
           fontColor={"#999"}
         />
-        <PageLayout
-          withBodyScroll={true}
-          withBodyAutoFocus={true}
-          articleHeaderContent={<ArticleHeaderContent />}
-          articleMainButtonContent={
-            <ArticleMainButtonContent
-              onLoading={this.onLoading}
-              startUpload={this.startUpload}
-              setProgressVisible={this.setProgressVisible}
-              setProgressValue={this.setProgressValue}
-              setProgressLabel={this.setProgressLabel}
-            />
-          }
-          articleBodyContent={
-            <ArticleBodyContent
-              onLoading={this.onLoading}
-              isLoading={this.state.isLoading}
-            />
-          }
-          sectionHeaderContent={
-            <SectionHeaderContent title={file.title} />
-          }
-          sectionBodyContent={
-            <SectionBodyContent
-              onLoading={this.onLoading}
-            />
-          }
-        />
+        {versions ? (
+          <PageLayout
+            withBodyScroll={true}
+            withBodyAutoFocus={true}
+            articleHeaderContent={<ArticleHeaderContent />}
+            articleMainButtonContent={
+              <ArticleMainButtonContent
+                onLoading={this.onLoading}
+                startUpload={this.startUpload}
+                setProgressVisible={this.setProgressVisible}
+                setProgressValue={this.setProgressValue}
+                setProgressLabel={this.setProgressLabel}
+              />
+            }
+            articleBodyContent={
+              <ArticleBodyContent
+                onLoading={this.onLoading}
+                isLoading={this.state.isLoading}
+              />
+            }
+            sectionHeaderContent={<SectionHeaderContent title={file.title} />}
+            sectionBodyContent={
+              <SectionBodyContent onLoading={this.onLoading} versions={versions} culture={settings.culture} />
+            }
+          />
+        ) : (
+          <PageLayout
+            articleHeaderContent={<ArticleHeaderContent />}
+            articleMainButtonContent={<ArticleMainButtonContent />}
+            articleBodyContent={<ArticleBodyContent />}
+            sectionBodyContent={
+              <Loader className="pageLoader" type="rombs" size="40px" />
+            }
+          />
+        )}
       </>
     );
   }
@@ -90,10 +115,14 @@ class PureVersionHistory extends React.Component {
 
 const VersionHistoryContainer = withTranslation()(PureVersionHistory);
 
-const VersionHistory = (props) => { 
+const VersionHistory = props => {
   changeLanguage(i18n);
-  return (<I18nextProvider i18n={i18n}><VersionHistoryContainer {...props}/></I18nextProvider>); 
-}
+  return (
+    <I18nextProvider i18n={i18n}>
+      <VersionHistoryContainer {...props} />
+    </I18nextProvider>
+  );
+};
 
 VersionHistory.propTypes = {
   files: PropTypes.array,
@@ -104,10 +133,9 @@ VersionHistory.propTypes = {
 function mapStateToProps(state) {
   return {
     files: state.files.files,
+    settings: state.auth.settings,
     isLoaded: state.auth.isLoaded
   };
 }
 
-export default connect(
-  mapStateToProps,
-)(withRouter(VersionHistory));
+export default connect(mapStateToProps)(withRouter(VersionHistory));
