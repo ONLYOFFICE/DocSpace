ddf497bac4df5a9cf5dd5958f9d3ec1992e15285
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-07-28 10:24:53 +0300

    Merge differences from master after header fix

--- web/ASC.Web.Common/src/components/Layout/sub-components/header.js
+++ web/ASC.Web.Common/src/components/Layout/sub-components/header.js
@@ -4,8 +4,6 @@ import styled from "styled-components";
 import NavItem from "./nav-item";
 import Headline from "../../Headline";
 import { utils } from "asc-web-components";
-import RecoverAccess from "../sub-components/recover-access";
-import HeaderLogo from "../sub-components/header-logo";
 const { desktop } = utils.device;
 
 const backgroundColor = "#0F4071";
@@ -18,79 +16,62 @@ const Header = styled.header`
   position: absolute;
   width: 100vw;
   height: 56px;
-
+  .header-logo-wrapper {
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
   .header-module-title {
     display: block;
     font-size: 21px;
     line-height: 0;
-
     @media ${desktop} {
       display: none;
     }
   }
-
-  /* .header-logo {
-    margin-left: 200px;
-  } */
-  /* .recover-container {
-    padding: 0 16.67% 0 0;
-
-    @media(max-width: 768px) {
-      padding: 0 18.75% 0 0;
+  .header-logo-min_icon {
+    display: none;
+    cursor: pointer;
+    
+    width: 24px;
+    height: 24px;
+    @media (max-width: 620px) {
+      padding: 0 12px 0 0;
+      display: ${props => props.module && "block"};
     }
-    @media(max-width: 375px) {
-      padding: 0 8.53% 0 0;
-    }
-  } */
-
-  /* .header-logo-icon {
+  }
+  .header-logo-icon {
     width: 146px;
     height: 24px;
     position: relative;
-    padding: 4px 20px 0 ${props => (props.logged ? "6px" : "145%")};
+    padding: 4px 20px 0 6px;
     cursor: pointer;
-
-    @media (max-width: 768px) {
-      padding: 4px 20px 0 ${props => (props.logged ? "6px" : "145%")};
-    }
-
     @media (max-width: 620px) {
       display: ${props => (props.module ? "none" : "block")};
-      padding: 0px 20px 0 ${props => (props.logged ? "6px" : "145%")};
+      padding: 0px 20px 0 6px;
     }
-
-    @media (max-width: 375px) {
-      padding: 4px 20px 0 ${props => (props.logged ? "6px" : "145%")};
-    }
-  } */
+  }
 `;
 
 const HeaderComponent = React.memo(props => {
   //console.log("Header render");
   const currentModule = props.currentModule && props.currentModule.title;
-  const isAuthKey = localStorage.getItem("asc_auth_key") ? true : false;
-
   return (
     <Header module={currentModule}>
-
-      {props.currentUser && <NavItem
+      <NavItem
         iconName="MenuIcon"
         badgeNumber={props.badgeNumber}
         onClick={props.onClick}
         noHover={true}
-      />}
-
-      <HeaderLogo module={module} logged={isAuthKey} className="header-logo" />
-
-      {!isAuthKey && <RecoverAccess
-        t={props.t}
-        className="recover-container"
-      />}
-
+      />
+      <a className="header-logo-wrapper" href="/">
+        <img className="header-logo-min_icon" src="images/nav.logo.react.svg" />
+        <img
+          className="header-logo-icon"
+          src="images/nav.logo.opened.react.svg"
+        />
+      </a>
       <Headline className="header-module-title" type="header" color="#FFF">
         {props.currentModule && props.currentModule.title}
       </Headline>
-
     </Header>
   );
 });
@@ -101,9 +82,7 @@ HeaderComponent.propTypes = {
   badgeNumber: PropTypes.number,
   onClick: PropTypes.func,
   onLogoClick: PropTypes.func,
-  currentModule: PropTypes.object,
-  currentUser: PropTypes.object,
-  t: PropTypes.func.isRequired
+  currentModule: PropTypes.object
 };
 
 export default HeaderComponent;
