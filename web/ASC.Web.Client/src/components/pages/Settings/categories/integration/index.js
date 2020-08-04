a1f53b16c4143ba98eb4b12f8b9044e2c84e616a
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-08-04 09:52:58 +0300

    TPS init

---
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/integration/index.js
@@ -0,0 +1,24 @@
+import React, { lazy, Suspense } from "react";
+import { Route, Switch } from "react-router-dom";
+import { withRouter } from "react-router";
+import { Loader } from "asc-web-components";
+
+const ThirdPartyServices = lazy(() => import("./third-party-services"));
+
+const Integration = ({ match }) => {
+    const basePath = '/settings/integration';
+
+    return (
+        <Suspense fallback={<Loader className="pageLoader" type="rombs" size='40px' />}>
+            <Switch>
+                <Route
+                    exact
+                    path={[`${basePath}/third-party-services`, '/integration', match.path]}
+                    component={ThirdPartyServices}
+                />
+            </Switch>
+        </Suspense>
+    );
+};
+
+export default withRouter(Integration);
---
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/integration/third-party-services.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ThirdParty extends React.Component {
+
+    render() {
+        return (
+            <div>tfs settings</div>
+        )
+    }
+}
+
+
+export default ThirdParty;
