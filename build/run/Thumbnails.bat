112a378d28c6a943699c8cce68cb76ad13a46d91
SuhorukovAnton <62381554+SuhorukovAnton@users.noreply.github.com>
2020-08-09 11:34:09 +0300

    socket: add bat

--- build/run/Thumbnails.bat
+++ build/run/Thumbnails.bat
@@ -1,2 +1,2 @@
-echo "RUN ASC.Web.Studio"
+echo "RUN ASC.Thumbnails.Svc"
 call dotnet run --project ..\..\common\services\ASC.Thumbnails.Svc\ASC.Thumbnails.Svc.csproj --no-build --$STORAGE_ROOT=..\..\Data  --log__dir=..\..\Logs --log__name=thumbnails
