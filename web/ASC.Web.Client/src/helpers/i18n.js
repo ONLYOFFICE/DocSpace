a1c4a8a9f530ea2dd9a15388630f727a5fff8eca
AlexeySafronov <alexey.safronov@onlyoffice.com>
2020-08-17 10:54:42 +0300

    Web Client: refactoring

--- web/ASC.Web.Client/src/helpers/i18n.js
+++ web/ASC.Web.Client/src/helpers/i18n.js
@@ -2,14 +2,6 @@ import i18n from "i18next";
 import Backend from "i18next-xhr-backend";
 import { constants } from "asc-web-common";
 const { i18nBaseSettings } = constants;
-//const fs = require("fs");
-const path = require("path");
-
-// function getFolderList(dir) {
-//   return fs
-//     .readdirSync(dir)
-//     .filter(file => fs.lstatSync(path.join(dir, file)).isDirectory());
-// }
 
 /**
  * @description create i18n instance
@@ -30,31 +22,11 @@ export const createI18N = function(options) {
   } else if (process.env.NODE_ENV === "development") {
     const resources = {};
 
-    const path = require("path");
-
-    //const folders = getFolderList(localesPath);
-
-    /* 
-
-
-        translation: require("../components/pages/About/locales/en/translation.json")
-                              ../components/pages/About/locales/en/translation.json
-*/
-
     i18nBaseSettings.supportedLngs.forEach(name => {
       resources[name] = {
-        translation: require(`../components/${localesPath}/locales/${name}/translation.json`) //`${localesPath}/${name}/translation.json`)
+        translation: require(`../components/${localesPath}/locales/${name}/translation.json`)
       };
     });
-
-    // const resources = {
-    //   en: {
-    //     translation: require("./locales/en/translation.json")
-    //   },
-    //   ru: {
-    //     translation: require("./locales/ru/translation.json")
-    //   }
-    // };
 
     newInstance.init({ ...i18nBaseSettings, resources });
   }
