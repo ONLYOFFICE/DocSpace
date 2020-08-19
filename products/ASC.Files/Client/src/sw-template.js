8d11e1b0c885efb1b880556fafbc89a859e46fec
AlexeySafronov <alexey.safronov@onlyoffice.com>
2020-08-19 20:00:41 +0300

    Web: Clients: Changed interaction with serviceWorker

---
+++ products/ASC.Files/Client/src/sw-template.js
@@ -0,0 +1,93 @@
+if ("function" === typeof importScripts) {
+  importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+  );
+  /* global workbox */
+  if (workbox) {
+    console.log("Workbox is loaded");
+
+    // Force development builds -> { debug: true } or production builds { debug: false }
+    workbox.setConfig({ debug: true });
+
+    //`generateSW` and `generateSWString` provide the option
+    // to force update an exiting service worker.
+    // Since we're using `injectManifest` to build SW,
+    // manually overriding the skipWaiting();
+    // self.addEventListener("install", event => {
+    //   self.skipWaiting();
+    //   window.location.reload();
+    // });
+
+    /* injection point for manifest files.  */
+    workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
+
+    // Image caching
+    workbox.routing.registerRoute(
+      // Cache image files.
+      ({ request }) => request.destination === "image",
+      // Use the cache if it's available.
+      new workbox.strategies.CacheFirst({
+        // Use a custom cache name.
+        cacheName: "image-cache",
+        plugins: [
+          new workbox.expiration.ExpirationPlugin({
+            // Cache only 60 images.
+            maxEntries: 60,
+            // Cache for a maximum of a month.
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+          })
+        ]
+      })
+    );
+
+    // Font caching
+    workbox.routing.registerRoute(
+      new RegExp("https://fonts.(?:.googlepis|gstatic).com/(.*)"),
+      new workbox.strategies.CacheFirst({
+        cacheName: "googleapis",
+        plugins: [
+          new workbox.expiration.ExpirationPlugin({
+            // Cache only 60 images.
+            maxEntries: 60,
+            // Cache for a maximum of a month.
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+          })
+        ]
+      })
+    );
+
+    // CSS caching
+    workbox.routing.registerRoute(
+      // Cache style resources, i.e. CSS files.
+      ({ request }) => request.destination === "style",
+      // Use cache but update in the background.
+      new workbox.strategies.StaleWhileRevalidate({
+        // Use a custom cache name.
+        cacheName: "css-cache"
+      })
+    );
+
+    // scripts caching
+    workbox.routing.registerRoute(
+      // Cache style resources, i.e. CSS files.
+      ({ request }) => request.destination === "script",
+      // Use cache but update in the background.
+      new workbox.strategies.StaleWhileRevalidate({
+        // Use a custom cache name.
+        cacheName: "script-cache"
+      })
+    );
+
+    // translations caching
+    workbox.routing.registerRoute(
+      /\translation.json$/,
+      // Use cache but update in the background.
+      new workbox.strategies.StaleWhileRevalidate({
+        // Use a custom cache name.
+        cacheName: "translation-cache"
+      })
+    );
+  } else {
+    console.log("Workbox could not be loaded. No Offline support");
+  }
+}
