667b3f0032a3cc73a6e766be5694aaa35b064236
AlexeySafronov <alexey.safronov@onlyoffice.com>
2020-08-25 16:28:14 +0300

    web: common: Fixed Cannot update a component from inside the function body of a different component (issue: call of i18n.changeLanguage without useEffect)

--- web/ASC.Web.Common/src/pages/login/index.js
+++ web/ASC.Web.Common/src/pages/login/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
 import {
@@ -33,11 +33,11 @@ const LoginContainer = styled.div`
   @media (max-width: 768px) {
     padding: 0 16px;
     max-width: 475px;
-    }
+  }
   @media (max-width: 375px) {
     margin: 72px auto 0 auto;
     max-width: 311px;
-    }
+  }
 
   .greeting-title {
     width: 100%;
@@ -56,11 +56,11 @@ const LoginContainer = styled.div`
 
     @media (max-width: 768px) {
       margin: 32px 0 0 0;
-      width: 100%
+      width: 100%;
     }
     @media (max-width: 375px) {
       margin: 32px 0 0 0;
-      width: 100%
+      width: 100%;
     }
 
     .login-forgot-wrapper {
@@ -73,48 +73,48 @@ const LoginContainer = styled.div`
 
         .login-checkbox {
           float: left;
-           span {
+          span {
             font-size: 12px;
-           }
-         }
+          }
+        }
 
-          .login-tooltip {
-            display: inline-flex;
+        .login-tooltip {
+          display: inline-flex;
 
-             @media(min-width: 1025px) {
-               margin-left: 8px;
-               margin-top: 4px;
-             }
-             @media(max-width: 1024px) {
-               padding: 4px 8px 8px 8px;
-             }
+          @media (min-width: 1025px) {
+            margin-left: 8px;
+            margin-top: 4px;
+          }
+          @media (max-width: 1024px) {
+            padding: 4px 8px 8px 8px;
+          }
+        }
       }
-    }
 
       .login-link {
         float: right;
         line-height: 16px;
       }
-  }
+    }
 
-  .login-button {
-    margin-bottom: 16px;
-  }
+    .login-button {
+      margin-bottom: 16px;
+    }
 
-  .login-button-dialog {
-    margin-right: 8px;
-  }
+    .login-button-dialog {
+      margin-right: 8px;
+    }
 
-  .login-bottom-border {
-    width: 100%;
-    height: 1px;
-    background: #ECEEF1;
-  }
+    .login-bottom-border {
+      width: 100%;
+      height: 1px;
+      background: #eceef1;
+    }
 
-  .login-bottom-text {
-    margin: 0 8px;
+    .login-bottom-text {
+      margin: 0 8px;
+    }
   }
-}
 `;
 
 class Form extends Component {
@@ -174,14 +174,10 @@ class Form extends Component {
   onSendPasswordInstructions = () => {
     if (!this.state.email.trim()) {
       this.setState({ emailError: true });
-    }
-    else {
+    } else {
       this.setState({ isLoading: true });
       sendInstructionsToChangePassword(this.state.email)
-        .then(
-          res => toastr.success(res),
-          message => toastr.error(message)
-        )
+        .then(res => toastr.success(res), message => toastr.error(message))
         .finally(this.onDialogClose());
     }
   };
@@ -251,7 +247,7 @@ class Form extends Component {
     upperCase: false,
     digits: false,
     specSymbols: false
-  }
+  };
 
   render() {
     const { greetingTitle, match, t } = this.props;
@@ -276,16 +272,16 @@ class Form extends Component {
     return (
       <>
         <LoginContainer>
-
-          <Text fontSize="32px"
+          <Text
+            fontSize="32px"
             fontWeight={600}
             textAlign="center"
-            className="greeting-title">
+            className="greeting-title"
+          >
             {greetingTitle}
           </Text>
 
           <form className="auth-form-container">
-
             <FieldContainer
               isVertical={true}
               labelVisible={false}
@@ -338,19 +334,19 @@ class Form extends Component {
                   className="login-checkbox"
                   isChecked={isChecked}
                   onChange={this.onChangeCheckbox}
-                  label={<Text fontSize='13px'>{t("Remember")}</Text>}
+                  label={<Text fontSize="13px">{t("Remember")}</Text>}
                 />
                 <HelpButton
                   className="login-tooltip"
                   helpButtonHeaderContent={t("CookieSettingsTitle")}
                   tooltipContent={
-                    <Text fontSize='12px'>{t("RememberHelper")}</Text>
+                    <Text fontSize="12px">{t("RememberHelper")}</Text>
                   }
                 />
               </div>
 
               <Link
-                fontSize='13px'
+                fontSize="13px"
                 color="#316DAA"
                 className="login-link"
                 type="page"
@@ -361,7 +357,7 @@ class Form extends Component {
               </Link>
             </div>
 
-            {openDialog &&
+            {openDialog && (
               <ForgotPasswordModalDialog
                 openDialog={openDialog}
                 isLoading={isLoading}
@@ -372,7 +368,7 @@ class Form extends Component {
                 onDialogClose={this.onDialogClose}
                 t={t}
               />
-            }
+            )}
 
             <Button
               id="button"
@@ -388,25 +384,25 @@ class Form extends Component {
             />
 
             {params.confirmedEmail && (
-              <Text isBold={true} fontSize='16px'>
+              <Text isBold={true} fontSize="16px">
                 {t("MessageEmailConfirmed")} {t("MessageAuthorize")}
               </Text>
             )}
-            <Text fontSize='14px' color="#c30">
+            <Text fontSize="14px" color="#c30">
               {errorText}
             </Text>
 
-            {socialButtons.length ? (<Box displayProp="flex" alignItems="center">
-              <div className="login-bottom-border"></div>
-              <Text className="login-bottom-text" color="#A3A9AE">{t("Or")}</Text>
-              <div className="login-bottom-border"></div>
-            </Box>
+            {socialButtons.length ? (
+              <Box displayProp="flex" alignItems="center">
+                <div className="login-bottom-border"></div>
+                <Text className="login-bottom-text" color="#A3A9AE">
+                  {t("Or")}
+                </Text>
+                <div className="login-bottom-border"></div>
+              </Box>
             ) : null}
-
           </form>
-
         </LoginContainer>
-
       </>
     );
   }
@@ -436,21 +432,22 @@ const RegisterWrapper = withTranslation()(Register);
 const LoginForm = props => {
   const { language, isLoaded, enabledJoin } = props;
 
-  i18n.changeLanguage(language);
+  useEffect(() => {
+    i18n.changeLanguage(language);
+  }, [language]);
 
   return (
     <>
-      {isLoaded && <>
-        <PageLayout>
-          <PageLayout.SectionBody>
-            <FormWrapper i18n={i18n} {...props} />
-            {enabledJoin &&
-              <RegisterWrapper i18n={i18n} {...props} />
-            }
-          </PageLayout.SectionBody>
-        </PageLayout>
-      </>
-      }
+      {isLoaded && (
+        <>
+          <PageLayout>
+            <PageLayout.SectionBody>
+              <FormWrapper i18n={i18n} {...props} />
+              {enabledJoin && <RegisterWrapper i18n={i18n} {...props} />}
+            </PageLayout.SectionBody>
+          </PageLayout>
+        </>
+      )}
     </>
   );
 };
@@ -470,6 +467,7 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { login, setIsLoaded })(
-  withRouter(LoginForm)
-);
+export default connect(
+  mapStateToProps,
+  { login, setIsLoaded }
+)(withRouter(LoginForm));
