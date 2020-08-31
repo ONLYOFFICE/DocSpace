a4a9234328106c52146082e145e45044df135482
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-08-31 10:03:19 +0300

    Web: Common: added react-resize-detector package, fixed styles

--- web/ASC.Web.Common/src/components/PageLayout/sub-components/section.js
+++ web/ASC.Web.Common/src/components/PageLayout/sub-components/section.js
@@ -1,13 +1,25 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { utils } from "asc-web-components";
 const { tablet } = utils.device;
+
+const tabletProps = css`
+  .section-header_filter {
+    display: none;
+  }
+
+  .section-body_filter {
+    display: block;
+    margin: 0 0 16px;
+  }
+`;
 
 const StyledSection = styled.section`
   padding: 0 0 0 24px;
   flex-grow: 1;
   display: flex;
   flex-direction: column;
+  min-width: 360px;
 
   .layout-progress-bar {
     position: sticky;
@@ -22,17 +34,11 @@ const StyledSection = styled.section`
       display: none;
     }
 
+  ${props => props.widthProp < 737 && tabletProps};
+
   @media ${tablet} {
     padding: 0 0 0 16px;
-
-    .section-header_filter {
-      display: none;
-    }
-
-    .section-body_filter {
-      display: block;
-      margin: 0 0 16px;
-    }
+    ${tabletProps};
   }
 `;
 
