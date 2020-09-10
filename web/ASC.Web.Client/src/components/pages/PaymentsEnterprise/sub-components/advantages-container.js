2b24c97d9e12e5b8da63d811eae306d69f65f5d4
TatianaLopaeva <talopaeva@gmail.com>
2020-09-10 14:57:29 +0300

    Refactoring styles of advantages-container

--- web/ASC.Web.Client/src/components/pages/PaymentsEnterprise/sub-components/advantages-container.js
+++ web/ASC.Web.Client/src/components/pages/PaymentsEnterprise/sub-components/advantages-container.js
@@ -5,272 +5,232 @@ import styled from "styled-components";
 import { Text, utils } from "asc-web-components";
 const { tablet } = utils.device;
 const StyledBodyAdvantages = styled.div`
-  margin-top: 42px;
+  display: grid;
+  padding-left: 32px;
+  padding-top: 32px;
+  padding-right: 8px;
+  width: 880px;
+  height: 279px;
+  grid-template-areas:
+    "header header"
+    "firstI first"
+    "secondI second"
+    "thirdI third"
+    "fourI four"
+    "fiveI five";
 
-  display: flex;
-  flex-direction: column;
-  padding: 32px 32px 32px 0px;
-  width: 888px;
-  height: 247px;
-  position: static;
+  grid-template-columns: 2.7% 98.3%;
+  grid-template-rows: 11.6% 2.9% 2.8% 2.8% 2.8% 77.1%;
+  grid-row-gap: 32px;
+  grid-column-gap: 5px;
 
+  margin: 0;
   background: #f8f9f9;
-  border-radius: 6px 6px 0px 0px;
-
-  .payments-icon-enterprise {
-    position: absolute;
-    margin-left: 666px;
-    z-index: 0;
-  }
-  /*
-  .first_icon {
-    margin: 64px 0px 193px 32px;
-  } */
-  /* .second_icon {
-    margin: 104px 0px 193px 32px;
-  } */
-  /* .third_icon {
-    margin: 144px 0px 193px 32px;
-  }
-  .four_icon {
-    margin: 184px 0px 193px 32px;
-  }
-  .five_icon {
-    margin: 224px 0px 193px 32px;
-  } */
-  /* .payments-body {
-    /* margin-right: 609px; 
-    margin-bottom: 22px;
-    margin-left: 64px;
-
-    font-weight: bold;
-    font-size: 13px;
-    color: #555f65;
-  } */
-  /* .first-advantages {
-    margin-top: 37px;
-    /* width: 247px; 
-  } */
-  .second-advantages {
-    /* width: 278px; */
-  }
-  .third-advantages {
-    /* width: 124px; */
-  }
-  .four-advantages {
-    /* width: 366px; */
-  }
-  .five-advantages {
-    /* width: 276px; */
-  }
-  .advantages-enterprise {
-    width: 856px;
-    height: 30px;
-    font-weight: bold;
-    font-size: 22px;
-    line-height: 30px;
-    margin: 0px 32px 0 32px;
-  }
-  .payments-body {
+  .advantages {
     font-weight: bold;
     font-size: 13px;
     line-height: 16px;
   }
-
-  .styled-advantages {
-    margin-top: 34px;
-    margin-left: 32px;
-    display: flex;
+  .header-advantages {
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 30px;
   }
-  .styled-first-advantages {
-    margin-top: 34px;
-    margin-left: 32px;
-    display: flex;
-  }
-  .styled-second-advantages {
-    margin-top: 18px;
-    margin-left: 32px;
-    display: flex;
-  }
-
-  .styled-third-advantages {
-    margin-top: 17px;
-    margin-left: 32px;
-    display: flex;
-  }
-
-  .styled-four-advantages {
-    margin-top: 17px;
-    margin-left: 32px;
-    display: flex;
-    z-index: 1;
-  }
-
-  .styled-five-advantages {
-    margin-top: 17px;
-    margin-left: 32px;
-    display: flex;
-  }
-
-  .advantages {
-    margin-left: 8px;
-    margin-top: 4px;
-  }
-  /* .second-advantages {
-    margin-left: 8px;
-    margin-top: 4px;
+  .main_icon {
+    position: absolute;
+    margin-left: 634px;
+    z-index: 0;
   }
   .first-advantages {
-    margin-left: 8px;
-    margin-top: 4px;
+    grid-area: first;
+    z-index: 1;
+    padding: 3px;
+  }
+  .first_icon {
+    grid-area: firstI;
+  }
+  .second-advantages {
+    grid-area: second;
+    padding: 3px;
+    z-index: 1;
   }
   .third-advantages {
-    margin-left: 8px;
-    margin-top: 4px;
+    grid-area: third;
+    padding: 3px;
+    z-index: 1;
   }
   .four-advantages {
-    margin-left: 8px;
-    margin-top: 4px;
+    grid-area: four;
+    padding: 3px;
+    z-index: 1;
   }
   .five-advantages {
-    margin-left: 8px;
-    margin-top: 4px;
-  } */
+    grid-area: five;
+    padding: 3px;
+    z-index: 1;
+  }
+  .header-advantages {
+    grid-area: header;
+    z-index: 1;
+  }
+  .second_icon {
+    grid-area: secondI;
+  }
+  .third_icon {
+    grid-area: thirdI;
+  }
+  .four_icon {
+    grid-area: fourI;
+  }
+  .five_icon {
+    grid-area: fiveI;
+  }
   @media ${tablet} {
-    width: 568px;
-    height: 249px;
-    margin-top: 41px;
+    width: 560px;
+    height: 100%;
+    grid-template-columns: 2.7% 98.3%;
+    grid-template-rows: min-content min-content;
+    grid-auto-rows: min-content;
+    grid-row-gap: 12px;
+    grid-column-gap: 5px;
     .main_icon {
       display: none;
     }
-    .styled-first-advantages {
-      margin-top: 33px;
-      margin-left: 32px;
-      display: flex;
+    .header-advantages {
+      margin-bottom: 21px;
     }
-    .styled-second-advantages {
-      margin-top: 17px;
-      margin-left: 32px;
-      display: flex;
+    .first-advantages {
+      padding-left: 11px;
+      margin-bottom: 6px;
     }
-
-    .styled-third-advantages {
-      margin-top: 16px;
-      margin-left: 32px;
-      display: flex;
+    .four-advantages {
+      padding-left: 5px;
     }
-
-    .styled-four-advantages {
-      margin-top: 16px;
-      margin-left: 32px;
-      display: flex;
-      z-index: 1;
+    .second-advantages {
+      padding-left: 11px;
+      margin-bottom: 5px;
+      padding-top: 3px;
     }
-
-    .styled-five-advantages {
-      margin-top: 17px;
-      margin-left: 32px;
-      display: flex;
+    .third-advantages {
+      padding-left: 11px;
+      margin-bottom: 5px;
+      padding-top: 3px;
+    }
+    .four-advantages {
+      padding-left: 11px;
+      padding-top: 3px;
+      margin-bottom: 6px;
+    }
+    .five-advantages {
+      padding-left: 11px;
+      padding-top: 3px;
+      margin-bottom: 32px;
     }
   }
+
   @media (max-width: 632px) {
-    margin-top: 16px;
-    width: 311px;
-    height: 301px;
-    .styled-first-advantages {
-      margin-top: 33px;
-      margin-left: 32px;
-      display: flex;
-    }
-    .styled-second-advantages {
-      margin-top: 17px;
-      margin-left: 32px;
-      display: flex;
-    }
+    padding-right: 32px;
+    width: 280px;
+    height: 100%;
 
-    .styled-third-advantages {
-      margin-top: 16px;
-      margin-left: 32px;
-      display: flex;
-    }
+    grid-template-columns: 7.9% 92.1%;
+    /* grid-template-rows: 21% 2.8% 5.3% 1.5% 5.1% 40.1%; */
+    grid-template-rows: min-content auto;
+    grid-auto-rows: min-content;
 
-    .styled-four-advantages {
-      margin-top: 16px;
-      margin-left: 32px;
-      display: flex;
-      z-index: 1;
-    }
+    /* grid-template-columns: min-content auto;
+    grid-auto-columns: min-content;  */
+    /* grid-template-columns: 7.9% 92.1%; */
 
-    .styled-five-advantages {
-      margin-top: 17px;
-      margin-left: 32px;
-      display: flex;
+    /* grid-column-gap: 5px; */
+    grid-row-gap: 13px;
+    .header-advantages {
+      margin-bottom: 21px;
+    }
+    .first-advantages {
+      padding-left: 5px;
+      margin-bottom: 4px;
+    }
+    .four-advantages {
+      padding-left: 5px;
+    }
+    .second-advantages {
+      padding-left: 5px;
+      margin-bottom: 2px;
+      padding-top: 0px;
+    }
+    .third-advantages {
+      padding-left: 5px;
+      margin-bottom: 4px;
+      padding-right: 0px;
+    }
+    .four-advantages {
+      padding-left: 5px;
+      padding-top: 0px;
+    }
+    .five-advantages {
+      padding-left: 5px;
+      padding-top: 0px;
+      margin-bottom: 29px;
     }
   }
 `;
 
-const StyledFirstAdvantages = styled.div``;
-const StyledSecondAdvantages = styled.div``;
 const AdvantagesContainer = ({ t }) => {
   return (
     <StyledBodyAdvantages>
-      <Text className="advantages-enterprise">{t("SubscriptionGet")}</Text>
-      <div className="styled-first-advantages">
-        <img
-          className="first_icon"
-          src="images/payments_enterprise_cubes.svg"
-          width="24px"
-          height="23px"
-          alt="Icon"
-        />
-        <Text className="payments-body advantages">
-          {t("OnlyOfficeEditor")}
-        </Text>
-      </div>
-      <div className="styled-second-advantages">
-        <img
-          className="second_icon"
-          src="images/payments_enterprise_lock.svg"
-          width="24px"
-          height="23px"
-          alt="Icon"
-        />
-        <Text className="payments-body advantages">{t("PrivateRooom")}</Text>
-      </div>
-      <div className="styled-third-advantages">
-        <img
-          className="third_icon"
-          src="images/payments_enterprise_smartphone.svg"
-          width="24px"
-          height="23px"
-          alt="Icon"
-        />
-        <Text className="payments-body advantages">{t("WebEditors")}</Text>
-      </div>
-      <div className="styled-four-advantages">
-        <img
-          className="four_icon"
-          src="images/payments_enterprise_update.svg"
-          width="24px"
-          height="23px"
-          alt="Icon"
-        />
-        <Text className="payments-body advantages">
-          {t("FunctionalityAndSecurity")}
-        </Text>
-      </div>
-      <div className="styled-five-advantages">
-        <img
-          className="five_icon"
-          src="images/payments_enterprise_help.svg"
-          width="24px"
-          height="23px"
-          alt="Icon"
-        />
-        <Text className="payments-body advantages">
-          {t("ProfessionalTechSupport")}
-        </Text>
-      </div>
+      <Text className="header-advantages">{t("SubscriptionGet")}</Text>
+
+      <img
+        className="first_icon"
+        src="images/payments_enterprise_cubes.svg"
+        width="24px"
+        height="23px"
+        alt="Icon"
+      />
+      <Text className="first-advantages  advantages">
+        {t("OnlyOfficeEditor")}
+      </Text>
+
+      <img
+        className="second_icon"
+        src="images/payments_enterprise_lock.svg"
+        width="24px"
+        height="23px"
+        alt="Icon"
+      />
+      <Text className="second-advantages  advantages">{t("PrivateRooom")}</Text>
+
+      <img
+        className="third_icon"
+        src="images/payments_enterprise_smartphone.svg"
+        width="24px"
+        height="23px"
+        alt="Icon"
+      />
+      <Text className="third-advantages  advantages">{t("WebEditors")}</Text>
+
+      <img
+        className="four_icon"
+        src="images/payments_enterprise_update.svg"
+        width="24px"
+        height="23px"
+        alt="Icon"
+      />
+      <Text className="four-advantages  advantages">
+        {t("FunctionalityAndSecurity")}
+      </Text>
+
+      <img
+        className="five_icon"
+        src="images/payments_enterprise_help.svg"
+        width="24px"
+        height="23px"
+        alt="Icon"
+      />
+      <Text className="five-advantages  advantages">
+        {t("ProfessionalTechSupport")}
+      </Text>
 
       <img
         className="payments-icon-enterprise main_icon"
