6936c060e5dfe43532cba6c55ad1a88b466a4ea9
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-09-22 10:15:37 +0300

    Web: Common: fixed MediaViewer styles

--- web/ASC.Web.Common/src/components/MediaViewer/MediaViewer.js
+++ web/ASC.Web.Common/src/components/MediaViewer/MediaViewer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { Icons } from "asc-web-components";
+import { Icons, Text } from "asc-web-components";
 
 import ImageViewer from "./sub-components/image-viewer"
 import VideoViewer from "./sub-components/video-viewer"
@@ -352,7 +352,7 @@ class MediaViewer extends React.Component {
 
                 <div>
                     <div className="details" ref={this.detailsContainer}>
-                        <div className="title">{fileTitle}</div>
+                        <Text isBold fontSize="14px" color="#fff" className="title">{fileTitle}</Text>
                         <ControlBtn onClick={this.props.onClose && (this.props.onClose)} className="mediaPlayerClose">
                             <Icons.CrossIcon size="medium" isfill={true} color="#fff" />
                         </ControlBtn>
