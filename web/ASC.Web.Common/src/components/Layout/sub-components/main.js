6101dc32dbb6754375f0924299248521d025b929
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-09-22 15:33:12 +0300

    Web: Common: fixed main styles

--- web/ASC.Web.Common/src/components/Layout/sub-components/main.js
+++ web/ASC.Web.Common/src/components/Layout/sub-components/main.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledMain = styled.main`
-  height: 100%;
-  position: absolute;
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
   padding: ${props => (props.fullscreen ? "0" : "56px 0 0 0")};
   width: 100vw;
   z-index: 0;
@@ -13,6 +13,9 @@ const StyledMain = styled.main`
 `;
 
 const Main = React.memo(props => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
   //console.log("Main render");
   return <StyledMain {...props} />;
 });
