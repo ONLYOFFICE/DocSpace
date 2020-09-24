97539afc0276da694f3e4582a70b22013df6d1ba
AlexeySafronov <alexey.safronov@onlyoffice.com>
2020-09-24 13:42:32 +0300

    Web: People: Added HeadlineLoader

--- products/ASC.People/Client/src/components/Article/Header/index.js
+++ products/ASC.People/Client/src/components/Article/Header/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { store, Headline } from 'asc-web-common';
+import React from "react";
+import { connect } from "react-redux";
+import { store, Headline, Loaders } from "asc-web-common";
+
 const { getCurrentModule } = store.auth.selectors;
 
-const ArticleHeaderContent = ({currentModuleName}) => {
-  return <Headline type="menu">{currentModuleName}</Headline>;
-}
+const ArticleHeaderContent = ({ currentModuleName }) => {
+  return currentModuleName ? (
+    <Headline type="menu">{currentModuleName}</Headline>
+  ) : (
+    <Loaders.Headline />
+  );
+};
 
 const mapStateToProps = (state) => {
-  const currentModule = getCurrentModule(state.auth.modules, state.auth.settings.currentProductId);
+  const currentModule = getCurrentModule(
+    state.auth.modules,
+    state.auth.settings.currentProductId
+  );
   return {
-      currentModuleName: (currentModule && currentModule.title) || ""
-  }
-}
+    currentModuleName: (currentModule && currentModule.title) || "",
+  };
+};
 
 export default connect(mapStateToProps)(ArticleHeaderContent);
