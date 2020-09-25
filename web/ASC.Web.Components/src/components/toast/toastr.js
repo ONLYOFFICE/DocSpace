a8f987cca8126f2ae68ad1b0f95ee25bd1fcafe9
Alexey Kostenko <kostenko.20101994@mail.ru>
2020-09-25 09:01:05 +0300

    Web: Components: Restyle Toast

--- web/ASC.Web.Components/src/components/toast/toastr.js
+++ web/ASC.Web.Components/src/components/toast/toastr.js
@@ -2,23 +2,49 @@ import React from 'react'
 import { toast } from 'react-toastify'
 import styled from 'styled-components'
 import { Icons } from '../icons'
+import IconButton from '../icon-button'
 
 // eslint-disable-next-line react/prop-types
 const Icon = ({ type }) => (
   type === "success"
-    ? <Icons.CheckIcon color="#ffffff" isfill={true} />
+    ? <Icons.CheckToastIcon color="#333333" isfill={true} />
     : type === "error" || type === "warning"
-      ? <Icons.DangerIcon color="#ffffff" isfill={true} />
-      : <Icons.InfoIcon color="#ffffff" isfill={true} />
+      ? <Icons.DangerToastIcon color="#333333" isfill={true} />
+      : <Icons.InfoToastIcon color="#333333" isfill={true} />
 );
+
+const IconWrapper = styled.div`
+  align-self: end;
+  display: flex;
+  svg{
+    width: 16px;
+    min-width: 16px;
+    height: 16px;
+    min-height: 16px;
+  }
+`;
 
 const StyledDiv = styled.div`
   margin-left: 15px;
+  line-height: 1.3;
+  align-self: center;
+`;
+
+const StyledCloseWrapper = styled.div`
+  .closeButton{
+    opacity: 0.5;
+    padding-top: 2px;
+    &:hover{
+      opacity: 1;
+    }
+  }
 `;
 
 const ToastTitle = styled.p`
-  font-weight: bold;
-  margin: 0;
+    font-weight: bold;
+    margin: 0;
+    margin-bottom: 5px;
+    line-height: 16px;
 `;
 
 const toastr = {
@@ -29,22 +55,34 @@ const toastr = {
   warning: warning
 };
 
+const CloseButton = ({closeToast}) => (
+  <StyledCloseWrapper>
+    <IconButton
+      className="closeButton"
+      onClick={closeToast}
+      iconName="CrossIcon"
+      size={12}
+      color="#333333"
+    />
+  </StyledCloseWrapper>
+)
+
 
 const notify = (type, data, title, timeout = 5000, withCross = false,  centerPosition = false) => {
   return toast(
     <>
-      <div>
-        <Icon type={type} />
-      </div>
+      <IconWrapper>
+        <Icon size="medium" type={type} />
+      </IconWrapper>
       <StyledDiv>
-        <ToastTitle>{title}</ToastTitle>
+        {title && <ToastTitle>{title}</ToastTitle>}
         {data}
       </StyledDiv>
     </>,
     {
       type: type,
       closeOnClick: !withCross,
-      closeButton: withCross,
+      closeButton: withCross && <CloseButton/>,
       autoClose: timeout === 0 ? false : timeout < 750 ? 5000 : (timeout || 5000),
       position: centerPosition && toast.POSITION.TOP_CENTER
     }
