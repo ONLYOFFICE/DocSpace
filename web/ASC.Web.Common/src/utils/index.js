b4c144df74f5b10df6bf33247b4701b783af49f4
Artem Tarasov <artem.tarasov@onlyoffice.com>
2020-10-01 19:04:10 +0300

    Web: Common: refactoring createPasswordHash

--- web/ASC.Web.Common/src/utils/index.js
+++ web/ASC.Web.Common/src/utils/index.js
@@ -42,6 +42,20 @@ export function changeLanguage(i18n) {
 }
 
 export function createPasswordHash(password, hashSettings) {
+  if (
+    !password ||
+    !hashSettings ||
+    typeof password !== "string" ||
+    typeof hashSettings !== "object" ||
+    !hashSettings.hasOwnProperty("salt") ||
+    !hashSettings.hasOwnProperty("size") ||
+    !hashSettings.hasOwnProperty("iterations") ||
+    typeof hashSettings.size !== "number" ||
+    typeof hashSettings.iterations !== "number" ||
+    typeof hashSettings.salt !== "string"
+  )
+    throw new Error("Invalid params.");
+
   const { size, iterations, salt } = hashSettings;
 
   let bits = sjcl.misc.pbkdf2(password, salt, iterations);
