226d4c6cc58afcc1d8756dba9bf9cb7ed442c025
AlexeySafronov <alexey.safronov@onlyoffice.com>
2020-10-09 14:23:40 +0300

    Web: Files: Add and applied Filter loader

--- web/ASC.Web.Common/src/components/Loaders/index.js
+++ web/ASC.Web.Common/src/components/Loaders/index.js
@@ -2,5 +2,6 @@ import Headline from "./HeadlineLoader";
 import TreeFolders from "./TreeFolderLoader";
 import Rows from "./RowsLoader";
 import Text from "./TextLoader";
+import Filter from "./FilterLoader";
 
-export default { Headline, TreeFolders, Rows, Text };
+export default { Headline, TreeFolders, Rows, Text, Filter };
