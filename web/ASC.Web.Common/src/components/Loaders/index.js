5a5fb5b036ba78e2445362e87b6c68f766ae61e6
Artem Tarasov <artem.tarasov@onlyoffice.com>
2020-10-09 15:57:04 +0300

    Web: Common: added ProfileViewLoader

--- web/ASC.Web.Common/src/components/Loaders/index.js
+++ web/ASC.Web.Common/src/components/Loaders/index.js
@@ -2,5 +2,6 @@ import Headline from "./HeadlineLoader";
 import TreeFolders from "./TreeFolderLoader";
 import Rows from "./RowsLoader";
 import Text from "./TextLoader";
+import ProfileView from "./ProfileViewLoader";
 
-export default { Headline, TreeFolders, Rows, Text };
+export default { Headline, TreeFolders, Rows, Text, ProfileView };
