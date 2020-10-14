3ce984b37417c150cf9240c67e22ce45d1f58101
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-10-14 12:57:00 +0300

    Web: Files: removed useless code from DocEditor

--- products/ASC.Files/Client/src/components/pages/DocEditor/index.js
+++ products/ASC.Files/Client/src/components/pages/DocEditor/index.js
@@ -1,19 +1,16 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
 import { withRouter } from "react-router";
 import { /*RequestLoader,*/ Box } from "asc-web-components";
 import { utils, api, toastr } from "asc-web-common";
 import { withTranslation, I18nextProvider } from "react-i18next";
 import { createI18N } from "../../../helpers/i18n";
-import { getFiles, getFolders } from "../../../store/files/selectors";
 
 const i18n = createI18N({
   page: "DocEditor",
   localesPath: "pages/DocEditor",
 });
 
-const { changeLanguage, getObjectByLocation, hideLoader, showLoader } = utils;
+const { changeLanguage, getObjectByLocation, showLoader } = utils;
 const { files } = api;
 
 class PureEditor extends React.Component {
@@ -83,16 +80,4 @@ const DocEditor = (props) => {
   );
 };
 
-DocEditor.propTypes = {
-  files: PropTypes.array,
-  history: PropTypes.object
-};
-
-function mapStateToProps(state) {
-  return {
-    files: getFiles(state),
-    folders: getFolders(state)
-  };
-}
-
-export default connect(mapStateToProps)(withRouter(DocEditor));
+export default withRouter(DocEditor);
