e307e0c1c0d4752bef6ad33403bf2addeb992e1b
TatianaLopaeva <talopaeva@gmail.com>
2020-10-22 16:10:31 +0300

    Web: Common: Added min-height for mobile

--- web/ASC.Web.Common/src/components/Main/index.js
+++ web/ASC.Web.Common/src/components/Main/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { utils} from "asc-web-components";
-const { tablet } = utils.device;
+const { tablet,smallTablet } = utils.device;
 const StyledMain = styled.main`
   height: calc(100vh - 56px);
   height: calc(var(--vh, 1vh) * 100 - 56px);
@@ -16,6 +16,9 @@ const StyledMain = styled.main`
     min-height: 100%;
   }
 
+  @media ${smallTablet} {
+    min-height: auto;
+  }
 `;
 
 const Main = React.memo((props) => {
