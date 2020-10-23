b74a6a11a092d9ccdd9eb63173b471d180fcc741
TatianaLopaeva <talopaeva@gmail.com>
2020-10-23 12:31:58 +0300

    Web: Common: Changed Layout with full page scroll for mobile

--- web/ASC.Web.Common/src/components/Layout/index.js
+++ web/ASC.Web.Common/src/components/Layout/index.js
@@ -1,16 +1,24 @@
-import { Box } from "asc-web-components";
+
 import React from "react"
 import styled from "styled-components";
-
+import {  Scrollbar } from "asc-web-components";
+import { isMobile } from "react-device-detect";
 const StyledContainer = styled.div`
 width:100%;
-height:100%;
+height:100vh;
+
 `
 
 const Layout = ({children}) => (
     
     <StyledContainer>
+      {isMobile  
+      ? <Scrollbar  stype="mediumBlack">
         {children}
+        </Scrollbar>
+
+      :  {children}
+        }
     </StyledContainer>
 )
 
