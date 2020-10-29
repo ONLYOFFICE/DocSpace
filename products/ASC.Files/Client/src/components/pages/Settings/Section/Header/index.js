088df7adf5ffdb6eb6d0a986cb4d91ce16ab825b
Artem Tarasov <artem.tarasov@onlyoffice.com>
2020-10-29 14:23:37 +0300

    Web: Files: Settings: fixed styles of headline

--- products/ASC.Files/Client/src/components/pages/Settings/Section/Header/index.js
+++ products/ASC.Files/Client/src/components/pages/Settings/Section/Header/index.js
@@ -1,12 +1,16 @@
 import React from "react";
+import styled from "styled-components";
 import { Headline } from "asc-web-common";
 
+const StyledHeadline = styled(Headline)`
+  margin-top: -1px;
+`;
 const SectionHeaderContent = ({ setting, t }) => {
   const title = setting[0].toUpperCase() + setting.slice(1);
   return (
-    <Headline className="headline-header" type="content" truncate={true}>
+    <StyledHeadline className="headline-header" type="content" truncate={true}>
       {t(`${title}`)}
-    </Headline>
+    </StyledHeadline>
   );
 };
 
