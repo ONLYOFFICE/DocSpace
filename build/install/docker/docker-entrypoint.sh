f09f8dd3820358e8a966970d4ba24ffeef260d8a
Sergey Kirichenko <sk81.biz@gmail.com>
2020-11-01 22:49:46 +0300

    fix Dockerfile-app

--- build/install/docker/docker-entrypoint.sh
+++ build/install/docker/docker-entrypoint.sh
@@ -41,90 +41,90 @@ while [ "$1" != "" ]; do
 	shift
 done
 
-set_jwt_secret () {
-	CURRENT_JWT_SECRET="";
+# set_jwt_secret () {
+# 	CURRENT_JWT_SECRET="";
 
-	if [[ -z ${JWT_SECRET} ]]; then
-		CURRENT_JWT_SECRET=$(get_container_env_parameter "$DOCUMENT_CONTAINER_NAME" "JWT_SECRET");
+# 	if [[ -z ${JWT_SECRET} ]]; then
+# 		CURRENT_JWT_SECRET=$(get_container_env_parameter "$DOCUMENT_CONTAINER_NAME" "JWT_SECRET");
 
-		if [[ -n ${CURRENT_JWT_SECRET} ]]; then
-			JWT_SECRET="$CURRENT_JWT_SECRET";
-		fi
-	fi
+# 		if [[ -n ${CURRENT_JWT_SECRET} ]]; then
+# 			JWT_SECRET="$CURRENT_JWT_SECRET";
+# 		fi
+# 	fi
 
-	if [[ -z ${JWT_SECRET} ]]; then
-		CURRENT_JWT_SECRET=$(get_container_env_parameter "$COMMUNITY_CONTAINER_NAME" "DOCUMENT_SERVER_JWT_SECRET");
+# 	if [[ -z ${JWT_SECRET} ]]; then
+# 		CURRENT_JWT_SECRET=$(get_container_env_parameter "$COMMUNITY_CONTAINER_NAME" "DOCUMENT_SERVER_JWT_SECRET");
 
-		if [[ -n ${CURRENT_JWT_SECRET} ]]; then
-			JWT_SECRET="$CURRENT_JWT_SECRET";
-		fi
-	fi
+# 		if [[ -n ${CURRENT_JWT_SECRET} ]]; then
+# 			JWT_SECRET="$CURRENT_JWT_SECRET";
+# 		fi
+# 	fi
 
-	if [[ -z ${JWT_SECRET} ]] && [[ "$UPDATE" != "true" ]] && [[ "$USE_AS_EXTERNAL_SERVER" != "true" ]]; then
-		JWT_SECRET=$(get_random_str 12);
-	fi
-}
+# 	if [[ -z ${JWT_SECRET} ]] && [[ "$UPDATE" != "true" ]] && [[ "$USE_AS_EXTERNAL_SERVER" != "true" ]]; then
+# 		JWT_SECRET=$(get_random_str 12);
+# 	fi
+# }
 
-set_core_machinekey () {
-	CURRENT_CORE_MACHINEKEY="";
+# set_core_machinekey () {
+# 	CURRENT_CORE_MACHINEKEY="";
 
-	if [[ -z ${CORE_MACHINEKEY} ]]; then
-		if file_exists ${BASE_DIR}/CommunityServer/data/.private/machinekey; then
-			CURRENT_CORE_MACHINEKEY=$(cat ${BASE_DIR}/CommunityServer/data/.private/machinekey);
+# 	if [[ -z ${CORE_MACHINEKEY} ]]; then
+# 		if file_exists ${BASE_DIR}/CommunityServer/data/.private/machinekey; then
+# 			CURRENT_CORE_MACHINEKEY=$(cat ${BASE_DIR}/CommunityServer/data/.private/machinekey);
 
-			if [[ -n ${CURRENT_CORE_MACHINEKEY} ]]; then
-				CORE_MACHINEKEY="$CURRENT_CORE_MACHINEKEY";
-			fi
-		fi
-	fi
+# 			if [[ -n ${CURRENT_CORE_MACHINEKEY} ]]; then
+# 				CORE_MACHINEKEY="$CURRENT_CORE_MACHINEKEY";
+# 			fi
+# 		fi
+# 	fi
 
-	if [[ -z ${CORE_MACHINEKEY} ]]; then
-		CURRENT_CORE_MACHINEKEY=$(get_container_env_parameter "$CONTROLPANEL_CONTAINER_NAME" "$MACHINEKEY_PARAM");
+# 	if [[ -z ${CORE_MACHINEKEY} ]]; then
+# 		CURRENT_CORE_MACHINEKEY=$(get_container_env_parameter "$CONTROLPANEL_CONTAINER_NAME" "$MACHINEKEY_PARAM");
 
-		if [[ -n ${CURRENT_CORE_MACHINEKEY} ]]; then
-			CORE_MACHINEKEY="$CURRENT_CORE_MACHINEKEY";
-		fi
-	fi
+# 		if [[ -n ${CURRENT_CORE_MACHINEKEY} ]]; then
+# 			CORE_MACHINEKEY="$CURRENT_CORE_MACHINEKEY";
+# 		fi
+# 	fi
 
-	if [[ -z ${CORE_MACHINEKEY} ]]; then
-		CURRENT_CORE_MACHINEKEY=$(get_container_env_parameter "$COMMUNITY_CONTAINER_NAME" "$MACHINEKEY_PARAM");
+# 	if [[ -z ${CORE_MACHINEKEY} ]]; then
+# 		CURRENT_CORE_MACHINEKEY=$(get_container_env_parameter "$COMMUNITY_CONTAINER_NAME" "$MACHINEKEY_PARAM");
 
-		if [[ -n ${CURRENT_CORE_MACHINEKEY} ]]; then
-			CORE_MACHINEKEY="$CURRENT_CORE_MACHINEKEY";
-		fi
-	fi
+# 		if [[ -n ${CURRENT_CORE_MACHINEKEY} ]]; then
+# 			CORE_MACHINEKEY="$CURRENT_CORE_MACHINEKEY";
+# 		fi
+# 	fi
 
-	if [[ -z ${CORE_MACHINEKEY} ]] && [[ "$UPDATE" != "true" ]] && [[ "$USE_AS_EXTERNAL_SERVER" != "true" ]]; then
-		CORE_MACHINEKEY=$(get_random_str 12);
-	fi
+# 	if [[ -z ${CORE_MACHINEKEY} ]] && [[ "$UPDATE" != "true" ]] && [[ "$USE_AS_EXTERNAL_SERVER" != "true" ]]; then
+# 		CORE_MACHINEKEY=$(get_random_str 12);
+# 	fi
 
-	echo CORE_MACHINEKEY=$CORE_MACHINEKEY
-	exit;
+# 	echo CORE_MACHINEKEY=$CORE_MACHINEKEY
+# 	exit;
 
-get_container_env_parameter () {
-	CONTAINER_NAME=$1;
-	PARAMETER_NAME=$2;
-	VALUE="";
+# get_container_env_parameter () {
+# 	CONTAINER_NAME=$1;
+# 	PARAMETER_NAME=$2;
+# 	VALUE="";
 
-	if [[ -z ${CONTAINER_NAME} ]]; then
-		echo "Empty container name"
-		exit 1;
-	fi
+# 	if [[ -z ${CONTAINER_NAME} ]]; then
+# 		echo "Empty container name"
+# 		exit 1;
+# 	fi
 
-	if [[ -z ${PARAMETER_NAME} ]]; then
-		echo "Empty parameter name"
-		exit 1;
-	fi
+# 	if [[ -z ${PARAMETER_NAME} ]]; then
+# 		echo "Empty parameter name"
+# 		exit 1;
+# 	fi
 
-	if command_exists docker ; then
-		CONTAINER_EXIST=$(docker ps -aqf "name=$CONTAINER_NAME");
+# 	if command_exists docker ; then
+# 		CONTAINER_EXIST=$(docker ps -aqf "name=$CONTAINER_NAME");
 
-		if [[ -n ${CONTAINER_EXIST} ]]; then
-			VALUE=$(docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' ${CONTAINER_NAME} | grep "${PARAMETER_NAME}=" | sed 's/^.*=//');
-		fi
-	fi
+# 		if [[ -n ${CONTAINER_EXIST} ]]; then
+# 			VALUE=$(docker inspect --format='{{range .Config.Env}}{{println .}}{{end}}' ${CONTAINER_NAME} | grep "${PARAMETER_NAME}=" | sed 's/^.*=//');
+# 		fi
+# 	fi
 
-	echo "$VALUE"
+# 	echo "$VALUE"
 
 sed -i "s!Server=.*;Pooling=!Server=${MYSQL_HOST:-${BUILD_MYSQL_HOST}};Port=3306;Database=${MYSQL_DATABASE:-${BUILD_MYSQL_DATABASE}};User ID=${MYSQL_USER:-${BUILD_MYSQL_USER}};Password=${MYSQL_PASSWORD:-${BUILD_MYSQL_PASSWORD}};Pooling=!g" ${PATH_TO_CONF}/appsettings.${APP_DOTNET_ENV:-${BUILD_APP_DOTNET_ENV}}.json
 sed -i "s!\"base-domain\".*,!\"base-domain\": \"${APP_CORE_BASE_DOMAIN:-${BUILD_APP_CORE_BASE_DOMAIN}}\",!g" ${PATH_TO_CONF}/appsettings.${APP_DOTNET_ENV:-${BUILD_APP_DOTNET_ENV}}.json
