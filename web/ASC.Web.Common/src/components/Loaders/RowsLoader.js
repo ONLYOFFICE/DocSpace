0efd774bbad8398475b1436f9b026a8ad78392c4
Viktor Fomin <enotcode@gmail.com>
2020-11-04 01:54:44 +0300

    Web: Common: Create rows loader

--- web/ASC.Web.Common/src/components/Loaders/RowsLoader.js
+++ web/ASC.Web.Common/src/components/Loaders/RowsLoader.js
@@ -1,37 +1,67 @@
 import React from "react";
-import ContentLoader from "react-content-loader";
+import styled from "styled-components";
+import RectangleLoader from "./RectangleLoader";
 
-const RowsLoader = (props) => (
-  <div style={{ width: "100%", height: "100%" }}>
-    <ContentLoader
-      speed={2}
-      width={"100%"}
-      height={300}
-      backgroundColor="#f3f3f3"
-      foregroundColor="#ecebeb"
-      {...props}
-    >
-      <rect x="0" y="24" rx="3" ry="3" width="16" height="16" />
-      <rect x="26" y="20" rx="5" ry="5" width="24" height="24" />
-      <rect x="65" y="15" rx="5" ry="5" width="100vw" height="35" />
+import { utils } from "asc-web-components";
+const { isMobile, isTablet } = utils.device;
 
-      <rect x="0" y="82" rx="3" ry="3" width="16" height="16" />
-      <rect x="26" y="78" rx="5" ry="5" width="24" height="24" />
-      <rect x="65" y="72" rx="5" ry="5" width="100vw" height="35" />
+const StyledRow = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 16px 22px 1fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 8px;
+  margin-bottom: 32px;
+  justify-items: center;
+  align-items: center;
+`;
 
-      <rect x="0" y="140" rx="3" ry="3" width="16" height="16" />
-      <rect x="26" y="136" rx="5" ry="5" width="24" height="24" />
-      <rect x="65" y="129" rx="5" ry="5" width="100vw" height="35" />
+const StyledBox = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 16px 12px;
+  grid-row-gap: 4px;
+  justify-items: left;
+  align-items: left;
+`;
 
-      <rect x="0" y="198" rx="3" ry="3" width="16" height="16" />
-      <rect x="26" y="194" rx="5" ry="5" width="24" height="24" />
-      <rect x="65" y="186" rx="5" ry="5" width="100vw" height="35" />
+const RowBlock = () => {
+  const numberLines = !isMobile() && !isTablet() ? 1 : 2;
 
-      <rect x="0" y="255" rx="3" ry="3" width="16" height="16" />
-      <rect x="26" y="252" rx="5" ry="5" width="24" height="24" />
-      <rect x="65" y="243" rx="5" ry="5" width="100vw" height="35" />
-    </ContentLoader>
-  </div>
-);
+  if (numberLines === 1) {
+    return <RectangleLoader width="100%" height="16" />;
+  } else {
+    return (
+      <StyledBox>
+        <RectangleLoader width="80%" height="16" />
+        <RectangleLoader width="100%" height="12" />
+      </StyledBox>
+    );
+  }
+};
+
+const Row = () => {
+  const rectangleSize = !isMobile() && !isTablet() ? 22 : 32;
+  return (
+    <StyledRow>
+      <RectangleLoader width="16" height="16" />
+      <RectangleLoader width={rectangleSize} height={rectangleSize} />
+      <RowBlock />
+    </StyledRow>
+  );
+};
+
+const RowsLoader = () => {
+  return (
+    <div>
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+    </div>
+  );
+};
 
 export default RowsLoader;
