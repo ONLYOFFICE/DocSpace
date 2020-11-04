1fc0d9118273152629f66471c35f56ae31d10613
Viktor Fomin <enotcode@gmail.com>
2020-11-04 12:23:35 +0300

    Web: Files: fix header loader

--- products/ASC.Files/Client/src/components/Article/Header/index.js
+++ products/ASC.Files/Client/src/components/Article/Header/index.js
@@ -8,7 +8,7 @@ const ArticleHeaderContent = ({ currentModuleName }) => {
   return currentModuleName ? (
     <Headline type="menu">{currentModuleName}</Headline>
   ) : (
-    <Loaders.Headline />
+    <Loaders.Head />
   );
 };
 
