c29871137945d2b64e7a4d9759b0d6c54a21dd26
Viktor Fomin <enotcode@gmail.com>
2020-11-06 15:00:11 +0300

    Web: Common: fix props circle

--- web/ASC.Web.Common/src/components/Loaders/CircleLoader.js
+++ web/ASC.Web.Common/src/components/Loaders/CircleLoader.js
@@ -3,37 +3,38 @@ import ContentLoader from "react-content-loader";
 import PropTypes from "prop-types";
 import { LoaderStyle } from "../../constants/index";
 
-const CircleLoader = (props) => (
-  <ContentLoader
-    speed={props.speed}
-    width={"100%"}
-    height={32}
-    backgroundColor={props.backgroundColor}
-    foregroundColor={props.foregroundColor}
-    backgroundOpacity={props.backgroundOpacity}
-    foregroundOpacity={props.foregroundOpacity}
-    {...props}
-  >
-    <circle cx="3" cy="12" r={props.radius} />
+const CircleLoader = ({ x, y, radius, width, height, ...rest }) => (
+  <ContentLoader width={width} height={height} {...rest}>
+    <circle cx={x} cy={y} r={radius} />
   </ContentLoader>
 );
 
-CircleLoader.PropTypes = {
+CircleLoader.propTypes = {
+  x: PropTypes.string,
+  y: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
   radius: PropTypes.string,
   backgroundColor: PropTypes.string,
   foregroundColor: PropTypes.string,
   backgroundOpacity: PropTypes.number,
   foregroundOpacity: PropTypes.number,
   speed: PropTypes.number,
+  animate: PropTypes.bool,
 };
 
 CircleLoader.defaultProps = {
+  x: "3",
+  y: "12",
   radius: "12",
+  width: "100%",
+  height: "100%",
   backgroundColor: LoaderStyle.backgroundColor,
   foregroundColor: LoaderStyle.foregroundColor,
   backgroundOpacity: LoaderStyle.backgroundOpacity,
   foregroundOpacity: LoaderStyle.foregroundOpacity,
   speed: LoaderStyle.speed,
+  animate: LoaderStyle.animate,
 };
 
 export default CircleLoader;
