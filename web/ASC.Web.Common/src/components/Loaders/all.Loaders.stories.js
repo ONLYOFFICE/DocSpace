2d78e10011582de3da6ae1a29b1c07ecf2220b4f
Viktor Fomin <enotcode@gmail.com>
2020-11-06 15:51:31 +0300

    Web: Common: hide AddonPanel

--- web/ASC.Web.Common/src/components/Loaders/all.Loaders.stories.js
+++ web/ASC.Web.Common/src/components/Loaders/all.Loaders.stories.js
@@ -4,28 +4,30 @@ import Section from "../../../.storybook/decorators/section";
 
 import Loaders from ".";
 
-storiesOf("Components|Loaders", module).add("all", () => (
-  <Section>
-    <h1>Base loader</h1>
-    <h2>Rectangle Loader</h2>
-    <Loaders.Rectangle />
+storiesOf("Components|Loaders", module)
+  .addParameters({ options: { showAddonPanel: false } })
+  .add("all", () => (
+    <Section>
+      <h1>Base loader</h1>
+      <h2>Rectangle Loader</h2>
+      <Loaders.Rectangle />
 
-    <h2>Circle loader</h2>
-    <Loaders.Circle x="20" y="20" height="32" />
+      <h2>Circle loader</h2>
+      <Loaders.Circle x="20" y="20" height="32" />
 
-    <h1>Article Header Loader</h1>
-    <Loaders.ArticleHeader />
+      <h1>Article Header Loader</h1>
+      <Loaders.ArticleHeader />
 
-    <h1>Filter loader</h1>
-    <Loaders.Filter />
+      <h1>Filter loader</h1>
+      <Loaders.Filter />
 
-    <h1>Rows loader</h1>
-    <Loaders.Rows />
+      <h1>Rows loader</h1>
+      <Loaders.Rows />
 
-    <h1>Section Header loader</h1>
-    <Loaders.SectionHeader />
+      <h1>Section Header loader</h1>
+      <Loaders.SectionHeader />
 
-    <h1>Tree Folders loader</h1>
-    <Loaders.TreeFolders />
-  </Section>
-));
+      <h1>Tree Folders loader</h1>
+      <Loaders.TreeFolders />
+    </Section>
+  ));
