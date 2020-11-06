ffd23c59b4348e6410acdde0c8cc126aaac628ae
Viktor Fomin <enotcode@gmail.com>
2020-11-06 15:39:31 +0300

    Web: Common: create stories

---
+++ web/ASC.Web.Common/src/components/Loaders/ArticleHeaderLoader/ArticleHeaderLoader.stories.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import {
+  withKnobs,
+  boolean,
+  text,
+  color,
+  number,
+} from "@storybook/addon-knobs/react";
+import Section from "../../../../.storybook/decorators/section";
+
+import Loaders from "..";
+import { LoaderStyle } from "../../../constants/index";
+
+storiesOf("Components|Loaders", module)
+  .addDecorator(withKnobs)
+  .add("article header loader", () => (
+    <Section>
+      <h1>Article Header Loader</h1>
+      <Loaders.ArticleHeader
+        width={text("width", "100%")}
+        height={text("height", "32px")}
+        backgroundColor={color("backgroundColor", LoaderStyle.backgroundColor)}
+        foregroundColor={color("foregroundColor", LoaderStyle.foregroundColor)}
+        backgroundOpacity={number(
+          "backgroundOpacity",
+          LoaderStyle.backgroundOpacity
+        )}
+        foregroundOpacity={number(
+          "foregroundOpacity",
+          LoaderStyle.foregroundOpacity
+        )}
+        speed={number("speed", LoaderStyle.speed)}
+        animate={boolean("animate", LoaderStyle.animate)}
+      />
+    </Section>
+  ));
