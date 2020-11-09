adc6347d2111f33233373d18b1bbbf11b4141649
Viktor Fomin <enotcode@gmail.com>
2020-11-09 12:35:48 +0300

    Web: Common: fix story for header loader

--- web/ASC.Web.Common/src/components/Loaders/HeaderLoader/HeaderLoader.stories.js
+++ web/ASC.Web.Common/src/components/Loaders/HeaderLoader/HeaderLoader.stories.js
@@ -6,27 +6,26 @@ import {
   color,
   number,
 } from "@storybook/addon-knobs/react";
-import Section from "../../../../.storybook/decorators/section";
+import Section from "../../../../.storybook/decorators/sectionBlue";
 
 import Loaders from "..";
 import { LoaderStyle } from "../../../constants/index";
+import styled from "styled-components";
+
+const StyledH1 = styled.h1`
+  padding-left: 16px;
+`;
 
 storiesOf("Components|Loaders", module)
   .addDecorator(withKnobs)
   .add("header loader", () => (
     <Section>
-      <h1>Header Loader</h1>
+      <StyledH1>Header Loader</StyledH1>
       <Loaders.Header
         backgroundColor={color("backgroundColor", "#fff")}
         foregroundColor={color("foregroundColor", "#fff")}
-        backgroundOpacity={number(
-          "backgroundOpacity",
-          LoaderStyle.backgroundOpacity
-        )}
-        foregroundOpacity={number(
-          "foregroundOpacity",
-          LoaderStyle.foregroundOpacity
-        )}
+        backgroundOpacity={number("backgroundOpacity", 0.2)}
+        foregroundOpacity={number("foregroundOpacity", 0.25)}
         speed={number("speed", LoaderStyle.speed)}
         animate={boolean("animate", LoaderStyle.animate)}
       />
