94e875075d17b7d60bb36d88643ce472f30bb049
Viktor Fomin <enotcode@gmail.com>
2020-11-16 18:49:06 +0300

    Web: Common: fixed styles

--- web/ASC.Web.Common/src/components/FloatingButton/StyledFloatingButton.js
+++ web/ASC.Web.Common/src/components/FloatingButton/StyledFloatingButton.js
@@ -1,16 +1,52 @@
 import styled from "styled-components";
 
+const backgroundColor = "none";
+const color = "#2DA7DB";
+
 const StyledFloatingButton = styled.div`
-  position: relative;
-  width: 54px;
-  height: 54px;
-  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.13);
-  border-radius: 54px;
-  cursor: pointer;
-  background: #fff;
-  box-sizing: border-box;
-  text-align: center;
-  padding-top: 16px;
+  .circle-wrap {
+    width: 54px;
+    height: 54px;
+    background: ${backgroundColor};
+    border-radius: 50%;
+  }
+  .circle-wrap .circle .mask,
+  .circle-wrap .circle .fill {
+    width: 54px;
+    height: 54px;
+    position: absolute;
+    border-radius: 50%;
+  }
+  .circle-wrap .circle .mask {
+    clip: rect(0px, 54px, 54px, 27px);
+  }
+  .circle-wrap .circle .mask .fill {
+    clip: rect(0px, 27px, 54px, 0px);
+    background-color: ${color};
+  }
+  .circle-wrap .circle .mask.full,
+  .circle-wrap .circle .fill {
+    animation: fill ease-in-out 3s;
+    transform: rotate(${(props) => props.percent * 1.8}deg);
+  }
+  @keyframes fill {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(${(props) => props.percent * 1.8}deg);
+    }
+  }
+  .circle-wrap .inside-circle {
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    background: #fff;
+    line-height: 48px;
+    text-align: center;
+    margin: 3px;
+    position: absolute;
+  }
 `;
 
 const StyledAlertIcon = styled.div`
@@ -18,7 +54,7 @@ const StyledAlertIcon = styled.div`
   width: 12px;
   height: 12px;
   left: 26px;
-  top: 10px;
+  top: -10px;
 `;
 
 export { StyledFloatingButton, StyledAlertIcon };
