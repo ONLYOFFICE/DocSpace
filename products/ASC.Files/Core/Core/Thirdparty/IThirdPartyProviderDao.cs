121f34946bbb48a9291831afb4dac2f49310efac
pavelbannov <pavel.bannov@onlyoffice.com>
2020-11-17 11:13:53 +0300

    Files: added base "Only in TMFileDao" methods

--- products/ASC.Files/Core/Core/Thirdparty/IThirdPartyProviderDao.cs
+++ products/ASC.Files/Core/Core/Thirdparty/IThirdPartyProviderDao.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -12,11 +14,14 @@ using ASC.Files.Core.Security;
 using ASC.Files.Core.Thirdparty;
 using ASC.Security.Cryptography;
 using ASC.Web.Core.Files;
+using ASC.Web.Files.Services.DocumentService;
 using ASC.Web.Studio.Core;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+
+using FileShare = ASC.Files.Core.Security.FileShare;
 
 namespace ASC.Files.Thirdparty
 {
@@ -166,6 +171,64 @@ namespace ASC.Files.Thirdparty
         }
 
         protected abstract string MakeId(string path = null);
+
+        #region Only in TMFileDao
+
+        public void ReassignFiles(string[] fileIds, Guid newOwnerId)
+        {
+        }
+
+        public List<File<string>> GetFiles(string[] parentIds, FilterType filterType, bool subjectGroup, Guid subjectID, string searchText, bool searchInContent)
+        {
+            return new List<File<string>>();
+        }
+
+        public IEnumerable<File<string>> Search(string text, bool bunch)
+        {
+            return null;
+        }
+
+        public bool IsExistOnStorage(File<string> file)
+        {
+            return true;
+        }
+
+        public void SaveEditHistory(File<string> file, string changes, Stream differenceStream)
+        {
+            //Do nothing
+        }
+
+        public List<EditHistory> GetEditHistory(DocumentServiceHelper documentServiceHelper, string fileId, int fileVersion)
+        {
+            return null;
+        }
+
+        public Stream GetDifferenceStream(File<string> file)
+        {
+            return null;
+        }
+
+        public bool ContainChanges(string fileId, int fileVersion)
+        {
+            return false;
+        }
+
+        public string GetUniqFilePath(File<string> file, string fileTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<(File<int>, SmallShareRecord)> GetFeeds(int tenant, DateTime from, DateTime to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> GetTenantsWithFeeds(DateTime fromTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
 
         public void Dispose()
         {
