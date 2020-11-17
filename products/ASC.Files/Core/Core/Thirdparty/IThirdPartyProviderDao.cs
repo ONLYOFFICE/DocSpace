f11a11d470c77391417834802e2774c4c08936bb
pavelbannov <pavel.bannov@onlyoffice.com>
2020-11-17 17:56:36 +0300

    Thirdparty: refactoring

--- products/ASC.Files/Core/Core/Thirdparty/IThirdPartyProviderDao.cs
+++ products/ASC.Files/Core/Core/Thirdparty/IThirdPartyProviderDao.cs
@@ -25,153 +25,8 @@ using FileShare = ASC.Files.Core.Security.FileShare;
 
 namespace ASC.Files.Thirdparty
 {
-    internal abstract class ThirdPartyProviderDao<T> : IDisposable where T : class, IProviderInfo
+    internal abstract class ThirdPartyProviderDao 
     {
-        public int TenantID { get; private set; }
-        protected IServiceProvider ServiceProvider { get; }
-        protected UserManager UserManager { get; }
-        protected TenantUtil TenantUtil { get; }
-        protected FilesDbContext FilesDbContext { get; }
-        protected SetupInfo SetupInfo { get; }
-        public ILog Log { get; }
-        protected FileUtility FileUtility { get; }
-
-        public RegexDaoSelectorBase<T> DaoSelector { get; set; }
-        public T ProviderInfo { get; set; }
-        public string PathPrefix { get; private set; }
-
-        public abstract string Id { get; }
-
-        public ThirdPartyProviderDao(
-            IServiceProvider serviceProvider,
-            UserManager userManager,
-            TenantManager tenantManager,
-            TenantUtil tenantUtil,
-            DbContextManager<FilesDbContext> dbContextManager,
-            SetupInfo setupInfo,
-            IOptionsMonitor<ILog> monitor,
-            FileUtility fileUtility)
-        {
-            ServiceProvider = serviceProvider;
-            UserManager = userManager;
-            TenantUtil = tenantUtil;
-            FilesDbContext = dbContextManager.Get(FileConstant.DatabaseId);
-            SetupInfo = setupInfo;
-            Log = monitor.CurrentValue;
-            FileUtility = fileUtility;
-            TenantID = tenantManager.GetCurrentTenant().TenantId;
-        }
-
-        public void Init(BaseProviderInfo<T> providerInfo, RegexDaoSelectorBase<T> selectorBase)
-        {
-            ProviderInfo = providerInfo.ProviderInfo;
-            PathPrefix = providerInfo.PathPrefix;
-            DaoSelector = selectorBase;
-        }
-
-        protected IQueryable<TSet> Query<TSet>(DbSet<TSet> set) where TSet : class, IDbFile
-        {
-            return set.Where(r => r.TenantId == TenantID);
-        }
-
-        protected string MappingID(string id, bool saveIfNotExist = false)
-        {
-            if (id == null) return null;
-
-            string result;
-            if (id.StartsWith(Id))
-            {
-                result = Regex.Replace(BitConverter.ToString(Hasher.Hash(id.ToString(), HashAlg.MD5)), "-", "").ToLower();
-            }
-            else
-            {
-                result = FilesDbContext.ThirdpartyIdMapping
-                        .Where(r => r.HashId == id)
-                        .Select(r => r.Id)
-                        .FirstOrDefault();
-            }
-            if (saveIfNotExist)
-            {
-                var newMapping = new DbFilesThirdpartyIdMapping
-                {
-                    Id = id,
-                    HashId = result,
-                    TenantId = TenantID
-                };
-
-                FilesDbContext.ThirdpartyIdMapping.Add(newMapping);
-                FilesDbContext.SaveChanges();
-            }
-            return result;
-        }
-
-        protected Folder<string> GetFolder()
-        {
-            var folder = ServiceProvider.GetService<Folder<string>>();
-
-            InitFileEntry(folder);
-
-            folder.FolderType = FolderType.DEFAULT;
-            folder.Shareable = false;
-            folder.TotalFiles = 0;
-            folder.TotalSubFolders = 0;
-
-            return folder;
-        }
-
-        protected Folder<string> GetErrorFolder(ErrorEntry entry)
-        {
-            var folder = GetFolder();
-
-            InitFileEntryError(folder, entry);
-
-            folder.ParentFolderID = null;
-
-            return folder;
-        }
-
-        protected File<string> GetFile()
-        {
-            var file = ServiceProvider.GetService<File<string>>();
-
-            InitFileEntry(file);
-
-            file.Access = FileShare.None;
-            file.FileStatus = FileStatus.None;
-            file.Shared = false;
-            file.Version = 1;
-
-            return file;
-        }
-
-        protected File<string> GetErrorFile(ErrorEntry entry)
-        {
-            var file = GetFile();
-            InitFileEntryError(file, entry);
-            return file;
-        }
-
-        protected void InitFileEntry(FileEntry<string> fileEntry)
-        {
-            fileEntry.CreateBy = ProviderInfo.Owner;
-            fileEntry.ModifiedBy = ProviderInfo.Owner;
-            fileEntry.ProviderId = ProviderInfo.ID;
-            fileEntry.ProviderKey = ProviderInfo.ProviderKey;
-            fileEntry.RootFolderCreator = ProviderInfo.Owner;
-            fileEntry.RootFolderType = ProviderInfo.RootFolderType;
-            fileEntry.RootFolderId = MakeId();
-        }
-
-        protected void InitFileEntryError(FileEntry<string> fileEntry, ErrorEntry entry)
-        {
-            fileEntry.ID = MakeId(entry.ErrorId);
-            fileEntry.CreateOn = TenantUtil.DateTimeNow();
-            fileEntry.ModifiedOn = TenantUtil.DateTimeNow();
-            fileEntry.Error = entry.Error;
-        }
-
-        protected abstract string MakeId(string path = null);
-
         #region Only in TMFileDao
 
         public void ReassignFiles(string[] fileIds, Guid newOwnerId)
@@ -319,6 +174,154 @@ namespace ASC.Files.Thirdparty
         }
 
         #endregion
+    }
+
+    internal abstract class ThirdPartyProviderDao<T> : ThirdPartyProviderDao, IDisposable where T : class, IProviderInfo
+    {
+        public int TenantID { get; private set; }
+        protected IServiceProvider ServiceProvider { get; }
+        protected UserManager UserManager { get; }
+        protected TenantUtil TenantUtil { get; }
+        protected FilesDbContext FilesDbContext { get; }
+        protected SetupInfo SetupInfo { get; }
+        public ILog Log { get; }
+        protected FileUtility FileUtility { get; }
+
+        public RegexDaoSelectorBase<T> DaoSelector { get; set; }
+        public T ProviderInfo { get; set; }
+        public string PathPrefix { get; private set; }
+
+        public abstract string Id { get; }
+
+        public ThirdPartyProviderDao(
+            IServiceProvider serviceProvider,
+            UserManager userManager,
+            TenantManager tenantManager,
+            TenantUtil tenantUtil,
+            DbContextManager<FilesDbContext> dbContextManager,
+            SetupInfo setupInfo,
+            IOptionsMonitor<ILog> monitor,
+            FileUtility fileUtility)
+        {
+            ServiceProvider = serviceProvider;
+            UserManager = userManager;
+            TenantUtil = tenantUtil;
+            FilesDbContext = dbContextManager.Get(FileConstant.DatabaseId);
+            SetupInfo = setupInfo;
+            Log = monitor.CurrentValue;
+            FileUtility = fileUtility;
+            TenantID = tenantManager.GetCurrentTenant().TenantId;
+        }
+
+        public void Init(BaseProviderInfo<T> providerInfo, RegexDaoSelectorBase<T> selectorBase)
+        {
+            ProviderInfo = providerInfo.ProviderInfo;
+            PathPrefix = providerInfo.PathPrefix;
+            DaoSelector = selectorBase;
+        }
+
+        protected IQueryable<TSet> Query<TSet>(DbSet<TSet> set) where TSet : class, IDbFile
+        {
+            return set.Where(r => r.TenantId == TenantID);
+        }
+
+        protected string MappingID(string id, bool saveIfNotExist = false)
+        {
+            if (id == null) return null;
+
+            string result;
+            if (id.StartsWith(Id))
+            {
+                result = Regex.Replace(BitConverter.ToString(Hasher.Hash(id.ToString(), HashAlg.MD5)), "-", "").ToLower();
+            }
+            else
+            {
+                result = FilesDbContext.ThirdpartyIdMapping
+                        .Where(r => r.HashId == id)
+                        .Select(r => r.Id)
+                        .FirstOrDefault();
+            }
+            if (saveIfNotExist)
+            {
+                var newMapping = new DbFilesThirdpartyIdMapping
+                {
+                    Id = id,
+                    HashId = result,
+                    TenantId = TenantID
+                };
+
+                FilesDbContext.ThirdpartyIdMapping.Add(newMapping);
+                FilesDbContext.SaveChanges();
+            }
+            return result;
+        }
+
+        protected Folder<string> GetFolder()
+        {
+            var folder = ServiceProvider.GetService<Folder<string>>();
+
+            InitFileEntry(folder);
+
+            folder.FolderType = FolderType.DEFAULT;
+            folder.Shareable = false;
+            folder.TotalFiles = 0;
+            folder.TotalSubFolders = 0;
+
+            return folder;
+        }
+
+        protected Folder<string> GetErrorFolder(ErrorEntry entry)
+        {
+            var folder = GetFolder();
+
+            InitFileEntryError(folder, entry);
+
+            folder.ParentFolderID = null;
+
+            return folder;
+        }
+
+        protected File<string> GetFile()
+        {
+            var file = ServiceProvider.GetService<File<string>>();
+
+            InitFileEntry(file);
+
+            file.Access = FileShare.None;
+            file.FileStatus = FileStatus.None;
+            file.Shared = false;
+            file.Version = 1;
+
+            return file;
+        }
+
+        protected File<string> GetErrorFile(ErrorEntry entry)
+        {
+            var file = GetFile();
+            InitFileEntryError(file, entry);
+            return file;
+        }
+
+        protected void InitFileEntry(FileEntry<string> fileEntry)
+        {
+            fileEntry.CreateBy = ProviderInfo.Owner;
+            fileEntry.ModifiedBy = ProviderInfo.Owner;
+            fileEntry.ProviderId = ProviderInfo.ID;
+            fileEntry.ProviderKey = ProviderInfo.ProviderKey;
+            fileEntry.RootFolderCreator = ProviderInfo.Owner;
+            fileEntry.RootFolderType = ProviderInfo.RootFolderType;
+            fileEntry.RootFolderId = MakeId();
+        }
+
+        protected void InitFileEntryError(FileEntry<string> fileEntry, ErrorEntry entry)
+        {
+            fileEntry.ID = MakeId(entry.ErrorId);
+            fileEntry.CreateOn = TenantUtil.DateTimeNow();
+            fileEntry.ModifiedOn = TenantUtil.DateTimeNow();
+            fileEntry.Error = entry.Error;
+        }
+
+        protected abstract string MakeId(string path = null);
 
         public void Dispose()
         {
