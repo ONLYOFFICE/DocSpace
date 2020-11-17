0c520416fa32f9535be3df86bad68a733f9bc14c
pavelbannov <pavel.bannov@onlyoffice.com>
2020-11-17 21:17:23 +0300

    Files: remove cache

--- products/ASC.Files/Core/Core/Thirdparty/OneDrive/OneDriveProviderInfo.cs
+++ products/ASC.Files/Core/Core/Thirdparty/OneDrive/OneDriveProviderInfo.cs
@@ -157,7 +157,7 @@ namespace ASC.Files.Thirdparty.OneDrive
 
         public OneDriveStorage CreateStorage(OAuth20Token token, int id)
         {
-            if (Storage != null) return Storage;
+            if (Storage != null && Storage.IsOpened) return Storage;
 
             var onedriveStorage = ServiceProvider.GetService<OneDriveStorage>();
 
@@ -174,7 +174,7 @@ namespace ASC.Files.Thirdparty.OneDrive
             {
                 token = OAuth20TokenHelper.RefreshToken<OneDriveLoginProvider>(ConsumerFactory, token);
 
-                var dbDao = ServiceProvider.GetService<CachedProviderAccountDao>();
+                var dbDao = ServiceProvider.GetService<ProviderAccountDao>();
                 var authData = new AuthData(token: token.ToJson());
                 dbDao.UpdateProviderInfo(id, authData);
             }
