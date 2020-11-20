1f01a3b308221436c50cdf02fd2563e96c2a22bf
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-11-20 11:48:35 +0300

    Add encryptionUploadDialog

--- products/ASC.Files/Client/src/helpers/desktop.js
+++ products/ASC.Files/Client/src/helpers/desktop.js
@@ -1,3 +1,24 @@
+import store from "../store/store";
 import { getEncryptedFormats } from "../store/files/selectors";
 
-const fn = () => getEncryptedFormats;
+export function encryptionUploadDialog(callback) {
+  const state = store.getState();
+  const filter = getEncryptedFormats(state);
+  const data = {
+    cryptoEngineId: "{FFF0E1EB-13DB-4678-B67D-FF0A41DBBCEF}",
+    filter: filter,
+  };
+  window.AscDesktopEditor.cloudCryptoCommand(
+    "upload",
+    JSON.stringify(data),
+    function (obj) {
+      let bytes = obj.bytes;
+      let filename = obj.name;
+      let file = new File([bytes], filename);
+
+      if (typeof callback == "function") {
+        callback(file, obj.isCrypto !== false);
+      }
+    }
+  );
+}
