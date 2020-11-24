06342cf27ba6f95536985408079c09c41dd74b4e
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-11-24 10:11:47 +0300

    Fix upload dialog

--- products/ASC.Files/Client/src/helpers/desktop.js
+++ products/ASC.Files/Client/src/helpers/desktop.js
@@ -3,22 +3,22 @@ import { getEncryptedFormats } from "../store/files/selectors";
 
 export function encryptionUploadDialog(callback) {
   const state = store.getState();
-  const filter = getEncryptedFormats(state).map((f) => "*" + f);
+  const filter = getEncryptedFormats(state)
+    .map((f) => "*" + f)
+    .join(" ");
+
   const data = {
     cryptoEngineId: "{FFF0E1EB-13DB-4678-B67D-FF0A41DBBCEF}",
     filter: filter,
   };
-  window.AscDesktopEditor.cloudCryptoCommand(
-    "upload",
-    JSON.stringify(data),
-    function (obj) {
-      let bytes = obj.bytes;
-      let filename = obj.name;
-      let file = new File([bytes], filename);
 
-      if (typeof callback == "function") {
-        callback(file, obj.isCrypto !== false);
-      }
+  window.AscDesktopEditor.cloudCryptoCommand("upload", data, function (obj) {
+    let bytes = obj.bytes;
+    let filename = obj.name;
+    let file = new File([bytes], filename);
+
+    if (typeof callback == "function") {
+      callback(file, obj.isCrypto !== false);
     }
-  );
+  });
 }
