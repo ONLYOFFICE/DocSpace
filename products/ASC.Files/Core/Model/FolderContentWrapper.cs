b4e071081c273b0905ef2ad1ed980ac5411f8cda
pavelbannov <pavel.bannov@onlyoffice.com>
2020-11-27 16:42:27 +0300

    todo

--- products/ASC.Files/Core/Model/FolderContentWrapper.cs
+++ products/ASC.Files/Core/Model/FolderContentWrapper.cs
@@ -124,6 +124,7 @@ namespace ASC.Api.Documents
                 .Where(r => r.FileEntryType == FileEntryType.File)
                 .Select(r =>
                 {
+                    //TODO
                     FileEntryWrapper wrapper = null;
                     if (r is File<int> fol1)
                     {
