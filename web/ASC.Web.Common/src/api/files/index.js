45ca67e7b80eb7b9ec7abf5c673e5750fe1ce478
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-12-03 12:11:52 +0300

    Add form data

--- web/ASC.Web.Common/src/api/files/index.js
+++ web/ASC.Web.Common/src/api/files/index.js
@@ -628,11 +628,16 @@ export function getEncryptionAccess(fileId) {
 }
 
 export function updateFileStream(file, fileId, encrypted, forcesave) {
-  const data = { file, encrypted, forcesave };
-  console.log("updateFileStream: ", data);
+  let bodyFormData = new FormData();
+  bodyFormData.append("file", file);
+  bodyFormData.append("encrypted", encrypted);
+  bodyFormData.append("forcesave", forcesave);
+
   return request({
     method: "put",
-    url: `/files/${fileId}/update`,
-    data,
+    url: `/files/${fileId}/update.json?encrypted=${encrypted}&forcesave=${forcesave}`,
+    // url: `/files/${fileId}/update.json?encrypted=${encrypted}&forcesave=${forcesave}`,
+    data: bodyFormData,
+    headers: { "Content-Type": "multipart/form-data" },
   });
 }
