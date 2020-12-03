9ddb7c4d0eb70e06e5965fdbe881dbb4b3ba875e
pavelbannov <pavel.bannov@onlyoffice.com>
2020-12-03 21:42:39 +0300

    Files: fix upload

--- products/ASC.Files/Server/Controllers/FilesController.cs
+++ products/ASC.Files/Server/Controllers/FilesController.cs
@@ -596,27 +596,14 @@ namespace ASC.Api.Documents
         /// <param name="encrypted"></param>
         /// <returns></returns>
         /// <visible>false</visible>
-        [Update("{fileId}/update", DisableFormat = true)]
-        public FileWrapper<string> UpdateFileStreamFromBody(string fileId, [FromBody]FileStreamModel model)
-        {
-            return FilesControllerHelperString.UpdateFileStream(model.File.OpenReadStream(), fileId, model.Encrypted, model.Forcesave);
-        }
 
         [Update("{fileId}/update", DisableFormat = true)]
-        [Consumes("application/x-www-form-urlencoded")]
         public FileWrapper<string> UpdateFileStreamFromForm(string fileId, [FromForm]FileStreamModel model)
         {
             return FilesControllerHelperString.UpdateFileStream(model.File.OpenReadStream(), fileId, model.Encrypted, model.Forcesave);
         }
 
         [Update("{fileId:int}/update")]
-        public FileWrapper<int> UpdateFileStreamFromBody(int fileId, [FromBody]FileStreamModel model)
-        {
-            return FilesControllerHelperInt.UpdateFileStream(model.File.OpenReadStream(), fileId, model.Encrypted, model.Forcesave);
-        }
-
-        [Update("{fileId:int}/update")]
-        [Consumes("application/x-www-form-urlencoded")]
         public FileWrapper<int> UpdateFileStreamFromForm(int fileId, [FromForm]FileStreamModel model)
         {
             return FilesControllerHelperInt.UpdateFileStream(model.File.OpenReadStream(), fileId, model.Encrypted, model.Forcesave);
