a3bd0823c670953f96d5953f194b877df054664f
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-12-04 12:59:48 +0300

    Refactoring

--- products/ASC.Files/Client/src/helpers/desktop.js
+++ products/ASC.Files/Client/src/helpers/desktop.js
@@ -1,7 +1,7 @@
 import store from "../store/store";
 import { store as commonStore } from "asc-web-common";
 import { getEncryptedFormats } from "../store/files/selectors";
-import { Log } from "oidc-client";
+import { desktopConstants } from "asc-web-common";
 
 const domain = window.location.origin;
 const guid = "{FFF0E1EB-13DB-4678-B67D-FF0A41DBBCEF}";
@@ -14,7 +14,7 @@ export function encryptionUploadDialog(callback) {
     .join(" ");
 
   const data = {
-    cryptoEngineId: guid,
+    cryptoEngineId: desktopConstants.guid,
     filter: filter,
   };
 
@@ -36,19 +36,20 @@ export function setEncryptionAccess(file) {
         window.AscDesktopEditor.cloudCryptoCommand(
           "share",
           {
-            "cryptoEngineId": guid,
+            "cryptoEngineId": desktopConstants.guid,
             "file": [file.viewUrl],
-            "keys": [
-              {
-                "publicKey":
-                  "-----BEGIN PUBLIC KEY-----&#xAMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApEKtEiSlig1Ue3JF6ajv&#xAtlWXEDdd/zcBmKUpkVtgi3gvCbGFB2VnUZRgOBWLQ8Bx+VU5beFlg0+/jUNSIzs1&#xAMwjGFa17CV8CxaZmtwTZjDwkfozWopttwxHfRIaOV8t2ZFB2V2qoGBCC4vxeF2/t&#xAMkNOgAnhVjH8Pq3uy5oOwzlZgU5u93ly12Jpa/bl2xGiXAqJpPH8s7ceSWBe/0Ky&#xAiDRz1DtRMs2elWQ6ag+tZwBk3Ee+j+ffK62d2n/B6ksY9oZ/joyzaHzjgeKI4+3E&#xAxW0Wh4zt/EEuypc6ySVd6+3WafRRqvQm+tXpolX6NL9oeCsyj0YrQGVcg6qm7BXn&#xABQIDAQAB&#xA-----END PUBLIC KEY-----&#xA",
-                "userId": "66faa6e4-f133-11ea-b126-00ffeec8b4ef",
-              },
-            ],
+            "keys": keys,
+            // "keys": [
+            //   {
+            //     "publicKey":
+            //       "-----BEGIN PUBLIC KEY-----&#xAMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApEKtEiSlig1Ue3JF6ajv&#xAtlWXEDdd/zcBmKUpkVtgi3gvCbGFB2VnUZRgOBWLQ8Bx+VU5beFlg0+/jUNSIzs1&#xAMwjGFa17CV8CxaZmtwTZjDwkfozWopttwxHfRIaOV8t2ZFB2V2qoGBCC4vxeF2/t&#xAMkNOgAnhVjH8Pq3uy5oOwzlZgU5u93ly12Jpa/bl2xGiXAqJpPH8s7ceSWBe/0Ky&#xAiDRz1DtRMs2elWQ6ag+tZwBk3Ee+j+ffK62d2n/B6ksY9oZ/joyzaHzjgeKI4+3E&#xAxW0Wh4zt/EEuypc6ySVd6+3WafRRqvQm+tXpolX6NL9oeCsyj0YrQGVcg6qm7BXn&#xABQIDAQAB&#xA-----END PUBLIC KEY-----&#xA",
+            //     "userId": "66faa6e4-f133-11ea-b126-00ffeec8b4ef",
+            //   },
+            // ],
           },
           (obj) => {
             let file = null;
-            if (obj.isCrypto !== false) {
+            if (obj.isCrypto) {
               let bytes = obj.bytes;
               let filename = "temp_name";
               file = new File([bytes], filename);
@@ -62,5 +63,4 @@ export function setEncryptionAccess(file) {
     });
     return promise;
   });
-  // .catch((e) => console.log(e));
 }
--- web/ASC.Web.Common/src/desktop/index.js
+++ web/ASC.Web.Common/src/desktop/index.js
@@ -1,7 +1,13 @@
 import { toastr } from "asc-web-common";
 import isEmpty from "lodash/isEmpty";
 import omit from "lodash/omit";
-import { getEncryptionAccess } from "../store/auth/actions";
+//import { getEncryptionAccess } from "../store/auth/actions";
+
+export const desktopConstants = Object.freeze({
+  domain: window.location.origin,
+  provider: "AppServer",
+  guid: "{FFF0E1EB-13DB-4678-B67D-FF0A41DBBCEF}",
+});
 
 const domain = window.location.origin;
 const provider = "AppServer";
@@ -11,8 +17,8 @@ export function regDesktop(user, isEncryption, keys, setEncryptionKeys) {
   const data = {
     displayName: user.displayName,
     email: user.email,
-    domain,
-    provider,
+    domain: desktopConstants.domain,
+    provider: desktopConstants.provider,
     userId: user.id,
   };
 
@@ -22,7 +28,7 @@ export function regDesktop(user, isEncryption, keys, setEncryptionKeys) {
     extendedData = {
       ...data,
       encryptionKeys: {
-        cryptoEngineId: guid,
+        cryptoEngineId: desktopConstants.guid,
       },
     };
 
@@ -95,7 +101,7 @@ export function regDesktop(user, isEncryption, keys, setEncryptionKeys) {
 export function relogin() {
   setTimeout(() => {
     const data = {
-      domain,
+      domain: desktopConstants.domain,
       onsuccess: "reload",
     };
     window.AscDesktopEditor.execCommand("portal:logout", JSON.stringify(data));
@@ -104,7 +110,7 @@ export function relogin() {
 
 export function checkPwd() {
   const data = {
-    domain,
+    domain: desktopConstants.domain,
     emailInput: "login",
     pwdInput: "password",
   };
@@ -113,55 +119,55 @@ export function checkPwd() {
 
 export function logout() {
   const data = {
-    domain,
+    domain: desktopConstants.domain,
   };
   window.AscDesktopEditor.execCommand("portal:logout", JSON.stringify(data));
 }
 
-export function setEncryptionAccess(file, callback) {
-  getEncryptionAccess(file.id).then((keys) => {
-    console.log(
-      "%c%s",
-      "color: green; font: 1.1em/1 bold;",
-      "Fetch keys: ",
-      keys
-    );
-    window.AscDesktopEditor.cloudCryptoCommand(
-      "share",
-      {
-        "cryptoEngineId": guid,
-        "file": [file.viewUrl],
-        "keys": [
-          {
-            "publicKey":
-              "-----BEGIN PUBLIC KEY-----&#xAMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApEKtEiSlig1Ue3JF6ajv&#xAtlWXEDdd/zcBmKUpkVtgi3gvCbGFB2VnUZRgOBWLQ8Bx+VU5beFlg0+/jUNSIzs1&#xAMwjGFa17CV8CxaZmtwTZjDwkfozWopttwxHfRIaOV8t2ZFB2V2qoGBCC4vxeF2/t&#xAMkNOgAnhVjH8Pq3uy5oOwzlZgU5u93ly12Jpa/bl2xGiXAqJpPH8s7ceSWBe/0Ky&#xAiDRz1DtRMs2elWQ6ag+tZwBk3Ee+j+ffK62d2n/B6ksY9oZ/joyzaHzjgeKI4+3E&#xAxW0Wh4zt/EEuypc6ySVd6+3WafRRqvQm+tXpolX6NL9oeCsyj0YrQGVcg6qm7BXn&#xABQIDAQAB&#xA-----END PUBLIC KEY-----&#xA",
-            "userId": "66faa6e4-f133-11ea-b126-00ffeec8b4ef",
-          },
-        ],
-      },
-      (obj) => {
-        console.log(
-          "%c%s",
-          "color: green; font: 1.1em/1 bold;",
-          "obj item: ",
-          obj
-        );
-        let fileItem = null;
-        if (obj.isCrypto !== false) {
-          let bytes = obj.bytes;
-          let filename = file.title;
-          fileItem = new File([bytes], filename);
-        }
-        console.log(
-          "%c%s",
-          "color: green; font: 1.1em/1 bold;",
-          "File item: ",
-          fileItem
-        );
-        if (typeof callback == "function") {
-          callback(fileItem);
-        }
-      }
-    );
-  });
-}
+// export function setEncryptionAccess(file, callback) {
+//   getEncryptionAccess(file.id).then((keys) => {
+//     console.log(
+//       "%c%s",
+//       "color: green; font: 1.1em/1 bold;",
+//       "Fetch keys: ",
+//       keys
+//     );
+//     window.AscDesktopEditor.cloudCryptoCommand(
+//       "share",
+//       {
+//         "cryptoEngineId": guid,
+//         "file": [file.viewUrl],
+//         "keys": [
+//           {
+//             "publicKey":
+//               "-----BEGIN PUBLIC KEY-----&#xAMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApEKtEiSlig1Ue3JF6ajv&#xAtlWXEDdd/zcBmKUpkVtgi3gvCbGFB2VnUZRgOBWLQ8Bx+VU5beFlg0+/jUNSIzs1&#xAMwjGFa17CV8CxaZmtwTZjDwkfozWopttwxHfRIaOV8t2ZFB2V2qoGBCC4vxeF2/t&#xAMkNOgAnhVjH8Pq3uy5oOwzlZgU5u93ly12Jpa/bl2xGiXAqJpPH8s7ceSWBe/0Ky&#xAiDRz1DtRMs2elWQ6ag+tZwBk3Ee+j+ffK62d2n/B6ksY9oZ/joyzaHzjgeKI4+3E&#xAxW0Wh4zt/EEuypc6ySVd6+3WafRRqvQm+tXpolX6NL9oeCsyj0YrQGVcg6qm7BXn&#xABQIDAQAB&#xA-----END PUBLIC KEY-----&#xA",
+//             "userId": "66faa6e4-f133-11ea-b126-00ffeec8b4ef",
+//           },
+//         ],
+//       },
+//       (obj) => {
+//         console.log(
+//           "%c%s",
+//           "color: green; font: 1.1em/1 bold;",
+//           "obj item: ",
+//           obj
+//         );
+//         let fileItem = null;
+//         if (obj.isCrypto !== false) {
+//           let bytes = obj.bytes;
+//           let filename = file.title;
+//           fileItem = new File([bytes], filename);
+//         }
+//         console.log(
+//           "%c%s",
+//           "color: green; font: 1.1em/1 bold;",
+//           "File item: ",
+//           fileItem
+//         );
+//         if (typeof callback == "function") {
+//           callback(fileItem);
+//         }
+//       }
+//     );
+//   });
+// }
