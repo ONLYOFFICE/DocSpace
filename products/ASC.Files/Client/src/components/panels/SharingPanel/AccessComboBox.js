c3b935e6b3b5d9ce868d4e72f421d86183b18eec
gopienkonikita <nikita.gopienko@onlyoffice.com>
2020-12-09 15:39:01 +0300

    Web: Files: added AccessComboBox component

---
+++ products/ASC.Files/Client/src/components/panels/SharingPanel/AccessComboBox.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { ComboBox, Icons } from "asc-web-components";
+
+const AccessComboBox = (props) => {
+  const {
+    //t,
+    //itemId,
+    //itemAccess,
+    //accessOptions,
+    //onFullAccessClick,
+    //onReadOnlyClick,
+    //onReviewClick,
+    //onCommentClick,
+    //onFormFillingClick,
+    //onDenyAccessClick,
+    access,
+    advancedOptions,
+    directionX,
+  } = props;
+
+  const getAccessIcon = () => {
+    switch (access) {
+      case 1:
+        return "AccessEditIcon";
+      case 2:
+        return "EyeIcon";
+      case 3:
+        return "AccessNoneIcon";
+      case 5:
+        return "AccessReviewIcon";
+      case 6:
+        return "AccessCommentIcon";
+      case 7:
+        return "AccessFormIcon";
+      case 8:
+        return "CustomFilterIcon";
+      default:
+        return;
+    }
+  };
+
+  const accessIcon = getAccessIcon();
+
+  return (
+    <ComboBox
+      advancedOptions={advancedOptions}
+      options={[]}
+      selectedOption={{ key: 0 }}
+      size="content"
+      className="panel_combo-box"
+      scaled={false}
+      directionX={directionX}
+      disableIconClick={false}
+    >
+      {React.createElement(Icons[accessIcon], {
+        size: "medium",
+        className: "sharing-access-combo-box-icon",
+      })}
+    </ComboBox>
+  );
+};
+
+export default AccessComboBox;
