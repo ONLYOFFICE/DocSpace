81581d219983a2adc17312d2d365c5d5e8384178
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2020-12-11 16:34:55 +0300

    Remove unused code

--- products/ASC.Files/Client/src/helpers/desktop.js
+++ products/ASC.Files/Client/src/helpers/desktop.js
@@ -39,13 +39,6 @@ export function setEncryptionAccess(file) {
             "cryptoEngineId": desktopConstants.guid,
             "file": [file.viewUrl],
             "keys": keys,
-            // "keys": [
-            //   {
-            //     "publicKey":
-            //       "-----BEGIN PUBLIC KEY-----&#xAMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApEKtEiSlig1Ue3JF6ajv&#xAtlWXEDdd/zcBmKUpkVtgi3gvCbGFB2VnUZRgOBWLQ8Bx+VU5beFlg0+/jUNSIzs1&#xAMwjGFa17CV8CxaZmtwTZjDwkfozWopttwxHfRIaOV8t2ZFB2V2qoGBCC4vxeF2/t&#xAMkNOgAnhVjH8Pq3uy5oOwzlZgU5u93ly12Jpa/bl2xGiXAqJpPH8s7ceSWBe/0Ky&#xAiDRz1DtRMs2elWQ6ag+tZwBk3Ee+j+ffK62d2n/B6ksY9oZ/joyzaHzjgeKI4+3E&#xAxW0Wh4zt/EEuypc6ySVd6+3WafRRqvQm+tXpolX6NL9oeCsyj0YrQGVcg6qm7BXn&#xABQIDAQAB&#xA-----END PUBLIC KEY-----&#xA",
-            //     "userId": "66faa6e4-f133-11ea-b126-00ffeec8b4ef",
-            //   },
-            // ],
           },
           (obj) => {
             let file = null;
