697ef7ed69a142b9e079b49a034a6c91e87fa210
AlexeySafronov <alexey.safronov@onlyoffice.com>
2020-12-14 10:56:39 +0300

    Web: Files: Refactoring of DocEditor page

--- products/ASC.Files/Client/src/components/pages/DocEditor/index.js
+++ products/ASC.Files/Client/src/components/pages/DocEditor/index.js
@@ -1,59 +1,69 @@
 import React from "react";
 import { withRouter } from "react-router";
-import { /*RequestLoader,*/ Box } from "asc-web-components";
+import { Toast, Box } from "asc-web-components";
 import { utils, api, toastr } from "asc-web-common";
 
-const { getObjectByLocation, showLoader } = utils;
+const { getObjectByLocation, showLoader, hideLoader } = utils;
 
 class PureEditor extends React.Component {
   async componentDidMount() {
-    const urlParams = getObjectByLocation(window.location);
-    const fileId = urlParams.fileId || null;
-    const doc = urlParams.doc || null;
+    try {
+      const urlParams = getObjectByLocation(window.location);
+      const fileId = urlParams.fileId || null;
+      const doc = urlParams.doc || null;
 
-    console.log("PureEditor componentDidMount", fileId, doc);
+      console.log("PureEditor componentDidMount", fileId, doc);
 
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
 
-    showLoader();
+      showLoader();
 
-    let docApiUrl = await api.files.getDocServiceUrl();
+      const docApiUrl = await api.files.getDocServiceUrl();
 
+      const config = await api.files.openEdit(fileId, doc);
+
+      this.loadDocApi(docApiUrl, () => this.onLoad(config));
+    } catch (error) {
+      console.log(error);
+      toastr.error(error);
+    }
+  }
+
+  loadDocApi = (docApiUrl, onLoadCallback) => {
     const script = document.createElement("script");
     script.setAttribute("type", "text/javascript");
     script.setAttribute("id", "scripDocServiceAddress");
 
-    script.onload = function () {
-      console.log("PureEditor script.onload", fileId, window.DocsAPI);
-
-      api.files
-        .openEdit(fileId, doc)
-        .then((config) => {
-          if (window.innerWidth < 720) {
-            config.type = "mobile";
-          }
-          if (!window.DocsAPI) throw new Error("DocsAPI is not defined");
-
-          console.log("Trying to open file with DocsAPI", fileId);
-          window.DocsAPI.DocEditor("editor", config);
-        })
-        .catch((e) => {
-          console.log(e);
-          toastr.error(e);
-        });
-    };
+    script.onload = onLoadCallback;
 
     script.src = docApiUrl;
     script.async = true;
 
     console.log("PureEditor componentDidMount: added script");
     document.body.appendChild(script);
-  }
+  };
+
+  onLoad = (config) => {
+    try {
+      if (window.innerWidth < 720) {
+        config.type = "mobile";
+      }
+      if (!window.DocsAPI) throw new Error("DocsAPI is not defined");
+
+      hideLoader();
+
+      window.DocsAPI.DocEditor("editor", config);
+    } catch (e) {
+      console.log(e);
+      toastr.error(e);
+    }
+  };
 
   render() {
     return (
       <Box widthProp="100vw" heightProp="calc(var(--vh, 1vh) * 100)">
+        <Toast />
         <div id="editor"></div>
       </Box>
     );
