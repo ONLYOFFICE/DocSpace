16d738e5caa7d181f7db78eca50a342e97d07648
Artem Tarasov <artem.tarasov@onlyoffice.com>
2020-12-17 14:49:30 +0300

    Web: Common/Client: changePassword: added logout if user is logged in

--- web/ASC.Web.Common/src/store/auth/actions.js
+++ web/ASC.Web.Common/src/store/auth/actions.js
@@ -147,12 +147,10 @@ export function getUser(dispatch) {
 }
 
 export function getIsAuthenticated(dispatch) {
-  return api.user
-    .checkIsAuthenticated()
-    .then((success) => { 
-      dispatch(setIsAuthenticated(success));
-      return success;
-    });
+  return api.user.checkIsAuthenticated().then((success) => {
+    dispatch(setIsAuthenticated(success));
+    return success;
+  });
 }
 
 export function getPortalSettings(dispatch) {
@@ -204,13 +202,17 @@ export function login(user, hash) {
   };
 }
 
-export function logout() {
+export function logout(needRedirect = true) {
   return (dispatch) => {
     return api.user.logout().then(() => {
       setWithCredentialsStatus(false);
-      dispatch(setLogout());
+      if (!needRedirect) {
+        getPortalSettings(dispatch);
+      } else {
+        dispatch(setLogout());
 
-      history.push("/login");
+        history.push("/login");
+      }
     });
   };
 }
