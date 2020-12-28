6f8df750ba6c8421b355aa82e7f94de089acd8e8
TatianaLopaeva <talopaeva@gmail.com>
2020-12-28 16:21:59 +0300

    Web: Common: Refactoring.

--- web/ASC.Web.Common/src/components/Layout/ScrollToTop.js
+++ web/ASC.Web.Common/src/components/Layout/ScrollToTop.js
@@ -5,7 +5,9 @@ export default function ScrollToTop() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const documentScrollElement = document.getElementById("customScrollBar");
+    const documentScrollElement = document.querySelector(
+      "#customScrollBar > .scroll-body"
+    );
     documentScrollElement && documentScrollElement.scrollTo(0, 0);
   }, [pathname]);
 
