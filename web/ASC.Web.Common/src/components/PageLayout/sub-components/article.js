8e64d662390e79c5e687c102779df391c98e77b4
TatianaLopaeva <talopaeva@gmail.com>
2020-12-29 14:38:10 +0300

    WEb: Common: Fixed displaying of article header.

--- web/ASC.Web.Common/src/components/PageLayout/sub-components/article.js
+++ web/ASC.Web.Common/src/components/PageLayout/sub-components/article.js
@@ -39,7 +39,7 @@ const StyledArticle = styled.article`
           ? props.pinned
             ? `
             min-width: 240px;
-            margin-top: ${props.isLoaded ? "0" : "56px"};
+            margin-top: ${props.isLoaded ? "56px" : "0"};
             max-width: ${props.isLoaded ? "calc(100vw - 368px)" : "240px"};
             height: calc(100% - 56px)!important;
 			
