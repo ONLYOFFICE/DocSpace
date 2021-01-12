19c90010821058be718720cfa2370d6c4af2349f
pavelbannov <pavel.bannov@onlyoffice.com>
2021-01-12 18:32:05 +0300

    Files: fix test

--- products/ASC.Files/Core/Services/WCFService/FileOperations/FileOperationsManager.cs
+++ products/ASC.Files/Core/Services/WCFService/FileOperations/FileOperationsManager.cs
@@ -63,7 +63,7 @@ namespace ASC.Web.Files.Services.WCFService.FileOperations
                 foreach (var o in operations.Where(o => processlist.All(p => p.Id != o.InstanceId)))
                 {
                     o.SetProperty(FileOperation.PROGRESS, 100);
-                    //tasks.RemoveTask(o.Id);
+                    tasks.RemoveTask(o.Id);
                 }
             }
 
