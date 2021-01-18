9273fd82f49a4d0412224c60af26f264e84ccce8
TatianaLopaeva <talopaeva@gmail.com>
2021-01-18 17:30:28 +0300

    Web: Common: Fixed auto scrolling down on ios.

--- web/ASC.Web.Common/src/components/PageLayout/index.js
+++ web/ASC.Web.Common/src/components/PageLayout/index.js
@@ -22,6 +22,7 @@ import { changeLanguage } from "../../utils";
 import ReactResizeDetector from "react-resize-detector";
 import FloatingButton from "../FloatingButton";
 import { getIsTabletView } from "../../store/auth/selectors";
+import { isMobile } from "react-device-detect";
 
 const { getLanguage } = store.auth.selectors;
 const { size } = utils.device;
@@ -333,7 +334,7 @@ class PageLayoutComponent extends React.Component {
                         uploadFiles={uploadFiles}
                         setSelections={setSelections}
                         withScroll={withBodyScroll}
-                        autoFocus={!isTabletView}
+                        autoFocus={isMobile || isTabletView ? false : true}
                         pinned={this.state.isArticlePinned}
                         viewAs={viewAs}
                       >
