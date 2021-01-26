d8a17e6443bb65a3df4c35ead818b70bc42aaa3f
TatianaLopaeva <talopaeva@gmail.com>
2021-01-26 14:39:44 +0300

    Web: Common: PageLayout: Added the condition for margin top for Ipad.

--- web/ASC.Web.Common/src/components/PageLayout/sub-components/article.js
+++ web/ASC.Web.Common/src/components/PageLayout/sub-components/article.js
@@ -5,7 +5,7 @@ import { utils } from "asc-web-components";
 import { Resizable } from "re-resizable";
 import { isMobile } from "react-device-detect";
 
-const { tablet } = utils.device;
+const { tablet, isDesktop } = utils.device;
 
 const StyledArticle = styled.article`
   @media ${tablet} {
@@ -34,7 +34,7 @@ const StyledArticle = styled.article`
     }
     ${isMobile &&
     css`
-      margin-top: ${(props) => (props.pinned ? "56px;" : "0;")}
+      margin-top: ${(props) => (props.pinned || isDesktop() ? "56px;" : "0;")}
       height: calc(100% - 56px) !important;
       width: 240px !important;
     `}
