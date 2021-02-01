0c72a6a1408d07410ecb37fbc98d02d97f17d167
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2021-02-01 14:01:09 +0300

    web: add mobx provider

--- web/ASC.Web.Client/src/index.js
+++ web/ASC.Web.Client/src/index.js
@@ -5,13 +5,20 @@ import store from "./store/store";
 import "./custom.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { ErrorBoundary } from "asc-web-common";
+import { ErrorBoundary, store as commonStore } from "asc-web-common";
+import { Provider as NewProvider } from "mobx-react";
 
+const { userStore } = commonStore;
+const stores = {
+  userStore,
+};
 ReactDOM.render(
   <Provider store={store}>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <NewProvider {...stores}>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </NewProvider>
   </Provider>,
   document.getElementById("root")
 );
