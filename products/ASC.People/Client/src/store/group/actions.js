1121559cdaa61522e5771d5a0dbb44b44b64dd91
Artem Tarasov <artem.tarasov@onlyoffice.com>
2021-02-04 17:30:33 +0300

    Web: People: fixed fetchGroup action to work with state

--- products/ASC.People/Client/src/store/group/actions.js
+++ products/ASC.People/Client/src/store/group/actions.js
@@ -1,6 +1,8 @@
 import { setGroups, fetchPeople } from "../people/actions";
 import { api } from "asc-web-common";
 
+import { getGroups } from "../people/selectors";
+
 export const SET_GROUP = "SET_GROUP";
 export const CLEAN_GROUP = "CLEAN_GROUP";
 
@@ -12,16 +14,24 @@ export function setGroup(targetGroup) {
 }
 
 export function resetGroup() {
+  console.log("clean group");
   return {
     type: CLEAN_GROUP,
   };
 }
 
 export function fetchGroup(groupId) {
-  return (dispatch) => {
-    api.groups
-      .getGroup(groupId)
-      .then((group) => dispatch(setGroup(group || null)));
+  return (dispatch, getState) => {
+    const state = getState();
+    const groups = getGroups(state);
+
+    let targetGroup;
+
+    groups.filter((group) => {
+      if (group.id === groupId) targetGroup = group;
+    });
+
+    dispatch(setGroup(targetGroup || null));
   };
 }
 
