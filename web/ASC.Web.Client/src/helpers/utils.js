fb6b345734229adbe4a259befd7a8890e76f9329
AlexeySafronov <alexey.safronov@onlyoffice.com>
2021-02-05 14:22:26 +0300

    Web: Client: Fixed setDocumentTitle

--- web/ASC.Web.Client/src/helpers/utils.js
+++ web/ASC.Web.Client/src/helpers/utils.js
@@ -1,18 +1,17 @@
 import { store as commonStore } from "asc-web-common";
-import store from "../store/store";
+//import store from "../store/store";
 
-const { getCurrentProduct } = commonStore.auth.selectors;
+const { authStore } = commonStore;
+
+//const { getCurrentProduct } = commonStore.auth.selectors;
 
 export const setDocumentTitle = (subTitle = null) => {
-  const state = store.getState();
-  const { auth: commonState } = state;
-
-  const { isAuthenticated, settings } = commonState;
-  const { organizationName } = settings;
+  // const state = store.getState();
+  // const { auth: commonState } = state;
+  const { isAuthenticated, settingsStore, product: currentModule } = authStore;
+  const { organizationName } = settingsStore;
 
   let title;
-  const currentModule = getCurrentProduct(state);
-
   if (subTitle) {
     if (isAuthenticated && currentModule) {
       title = subTitle + " - " + currentModule.title;
