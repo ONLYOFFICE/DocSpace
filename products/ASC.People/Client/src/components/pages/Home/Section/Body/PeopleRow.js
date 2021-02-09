940197de705d11e2c426b9a14a7e51cd7c2aeee0
Viktor Fomin <enotcode@gmail.com>
2021-02-10 01:40:09 +0300

    Web: People: created people row

---
+++ products/ASC.People/Client/src/components/pages/Home/Section/Body/PeopleRow.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Row, Avatar } from "asc-web-components";
+import UserContent from "./userContent";
+
+const PeopleRow = ({
+  man,
+  widthProp,
+  selectGroup,
+  isAdmin,
+  currentUserId,
+  context,
+  isMobile,
+  history,
+  settings,
+  getUserContextOptions,
+  onContentRowSelect,
+  needForUpdate,
+}) => {
+  console.log("PeopleRow render");
+  const { checked, role, displayName, avatar, id, status, options } = man;
+  const sectionWidth = context.sectionWidth;
+  const showContextMenu = options && options.length > 0;
+  const contextOptionsProps =
+    (isAdmin && showContextMenu) || (showContextMenu && id === currentUserId)
+      ? {
+          contextOptions: getUserContextOptions(options, id),
+        }
+      : {};
+
+  const checkedProps = checked !== null && isAdmin ? { checked } : {};
+
+  const element = (
+    <Avatar size="min" role={role} userName={displayName} source={avatar} />
+  );
+  return (
+    <Row
+      key={id}
+      status={status}
+      data={man}
+      element={element}
+      onSelect={onContentRowSelect}
+      {...checkedProps}
+      {...contextOptionsProps}
+      needForUpdate={needForUpdate}
+      sectionWidth={sectionWidth}
+    >
+      <UserContent
+        isMobile={isMobile}
+        widthProp={widthProp}
+        user={man}
+        history={history}
+        settings={settings}
+        selectGroup={selectGroup}
+        sectionWidth={sectionWidth}
+      />
+    </Row>
+  );
+};
+export default PeopleRow;
