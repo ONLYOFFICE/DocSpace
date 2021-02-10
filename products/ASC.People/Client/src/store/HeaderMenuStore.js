673762894e54258b19bb2c077958d4b29d6ea1d0
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2021-02-10 13:30:39 +0300

    web: people: add Header Menu Store

---
+++ products/ASC.People/Client/src/store/HeaderMenuStore.js
@@ -0,0 +1,38 @@
+import { computed, makeObservable } from "mobx";
+
+class HeaderMenuStore {
+  constructor(peopleStore) {
+    this.peopleStore = peopleStore;
+    makeObservable(this, {
+      isHeaderVisible: computed,
+      isHeaderIndeterminate: computed,
+      isHeaderChecked: computed,
+    });
+  }
+
+  get isHeaderVisible() {
+    const { selection } = this.peopleStore.selectionStore;
+
+    return !!selection.length;
+  }
+
+  get isHeaderIndeterminate() {
+    const { selection } = this.peopleStore.selectionStore;
+    const { users } = this.peopleStore.usersStore;
+
+    return (
+      this.isHeaderVisible &&
+      !!selection.length &&
+      selection.length < users.length
+    );
+  }
+
+  get isHeaderChecked() {
+    const { selection } = this.peopleStore.selectionStore;
+    const { users } = this.peopleStore.usersStore;
+
+    return this.isHeaderVisible && selection.length === users.length;
+  }
+}
+
+export default HeaderMenuStore;
