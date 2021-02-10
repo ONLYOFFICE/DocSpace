de8737afcd11b1d4a33bfa787c6c966c3b805abc
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2021-02-10 20:15:40 +0300

    web: people: add deleteGroup fn

--- products/ASC.People/Client/src/store/GroupsStore.js
+++ products/ASC.People/Client/src/store/GroupsStore.js
@@ -4,7 +4,8 @@ import { api } from "asc-web-common";
 class GroupsStore {
   groups = null;
 
-  constructor() {
+  constructor(peopleStore) {
+    this.peopleStore = peopleStore;
     makeObservable(this, {
       groups: observable,
       getGroupList: action,
@@ -14,6 +15,14 @@ class GroupsStore {
   getGroupList = async () => {
     const res = await api.groups.getGroupList();
     this.groups = res;
+  };
+
+  deleteGroup = async (id) => {
+    const { filter } = this.peopleStore.filterStore;
+    await api.groups.deleteGroup(id);
+    const newData = this.groups.filter((g) => g.id !== id);
+    this.groups = newData;
+    await this.peopleStore.usersStore.getUsersList(filter);
   };
 }
 
