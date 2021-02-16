e513ad35c94eb6951762bb206b72b62da820d69c
Vladislav Makhov <vladislav.makhov@onlyoffice.com>
2021-02-16 10:01:01 +0300

    web: people: hide unused code

--- products/ASC.People/Client/src/store/profile/selectors.js
+++ products/ASC.People/Client/src/store/profile/selectors.js
@@ -5,15 +5,15 @@ const { isAdmin, getCurrentUserId } = store.auth.selectors;
 const getProfileId = (state) =>
   state.profile.targetUser && state.profile.targetUser.id;
 
-const getIsAdminProfile = (state) =>
-  state.profile.targetUser.listAdminModules &&
-  state.profile.targetUser.listAdminModules.includes("people");
+const getIsAdminProfile = (state) => true;
+// state.profile.targetUser.listAdminModules &&
+//state.profile.targetUser.listAdminModules.includes("people");
 
-export const getDisableProfileType = createSelector(
-  [isAdmin, getCurrentUserId, getProfileId, getIsAdminProfile],
-  (isAdmin, currentUserId, profileId, isAdminProfile) => {
-    return currentUserId === profileId || !isAdmin || isAdminProfile
-      ? true
-      : false;
-  }
-);
+// export const getDisableProfileType = createSelector(
+//   [isAdmin, getCurrentUserId, getProfileId, getIsAdminProfile],
+//   (isAdmin, currentUserId, profileId, isAdminProfile) => {
+//     return currentUserId === profileId || !isAdmin || isAdminProfile
+//       ? true
+//       : false;
+//   }
+// );
