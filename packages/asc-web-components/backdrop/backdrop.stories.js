7f5c4e779b5e85ec6d4a4a80da1c92691f5464c1
Artem Tarasov <artem.tarasov@onlyoffice.com>
2021-03-01 17:58:42 +0300

    Web: Components: added story of Backgrop

--- packages/asc-web-components/backdrop/backdrop.stories.js
+++ packages/asc-web-components/backdrop/backdrop.stories.js
@@ -1,40 +1,81 @@
 import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import { BooleanValue } from "react-values";
-import withReadme from "storybook-readme/with-readme";
-import { withKnobs, number } from "@storybook/addon-knobs/react";
-import Readme from "./README.md";
-import Section from "../../../.storybook/decorators/section";
-import Backdrop from ".";
+
+import Backdrop from "./";
 import Button from "../button";
 
-storiesOf("Components|Backdrop", module)
-  .addDecorator(withReadme(Readme))
-  .addDecorator(withKnobs)
-  .add("base", () => (
-    <Section>
+export default {
+  title: "Components/Backdrop",
+  component: Backdrop,
+  argTypes: {
+    visible: {
+      control: false,
+      description: "Display or not",
+    },
+    zIndex: {
+      description: "CSS z-index",
+    },
+    className: { description: "Accepts class" },
+    id: { description: "Accepts id" },
+    style: { description: "Accepts CSS style" },
+    withBackground: {
+      description:
+        "The background is not displayed if the viewport width is less than 1024, set it to true for display",
+    },
+    isAside: { description: "Must be true if used with Aside component" },
+    onClick: { action: "On Hide", table: { disable: true } },
+    onClickButton: { action: "On Show", table: { disable: true } },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: "Backdrop for displaying modal dialogs or other components",
+      },
+      source: {
+        code: `
+      import Backdrop from "@appserver/components/backdrop";
+
+<Backdrop visible={true} zIndex={200}/>`,
+      },
+    },
+  },
+  decorators: [
+    (Story, props) => (
       <BooleanValue>
-        {({ value, toggle }) => (
-          <div>
-            <Button
-              label="Show Backdrop"
-              primary={true}
-              onClick={(e) => {
-                action("onShow")(e);
-                toggle(true);
-              }}
-            />
-            <Backdrop
-              visible={value}
-              zIndex={number("zIndex", 1)}
-              onClick={(e) => {
-                action("onHide")(e);
-                toggle(false);
-              }}
-            />
-          </div>
-        )}
+        {({ value, toggle }) => {
+          props.args.value = value;
+          props.args.toggle = toggle;
+          console.log(props);
+
+          return (
+            <>
+              <Button
+                label="Show Backdrop"
+                primary
+                size="medium"
+                onClick={(e) => {
+                  props.args.onClickButton(e);
+                  toggle(true);
+                }}
+              />
+              <Story />
+            </>
+          );
+        }}
       </BooleanValue>
-    </Section>
-  ));
+    ),
+  ],
+};
+
+const Template = (args) => (
+  <Backdrop
+    {...args}
+    visible={args.value}
+    onClick={(e) => {
+      args.onClick(e);
+      args.toggle();
+    }}
+  />
+);
+
+export const Default = Template.bind({});
