5412f7b86069a1abc2661210ecfe52a6351a0224
TatianaLopaeva <talopaeva@gmail.com>
2021-03-05 11:09:23 +0300

    Web: NavMenu: Fixed icon path, deleted useless icons from modules.

--- web/ASC.Web.Client/src/components/NavMenu/sub-components/header-unauth.js
+++ web/ASC.Web.Client/src/components/NavMenu/sub-components/header-unauth.js
@@ -69,11 +69,11 @@ const HeaderUnAuth = ({
             <a className="header-logo-wrapper" href="/">
               <img
                 className="header-logo-min_icon"
-                src="images/nav.logo.react.svg"
+                src="/static/images/nav.logo.react.svg"
               />
               <img
                 className="header-logo-icon"
-                src="images/nav.logo.opened.react.svg"
+                src="/static/images/nav.logo.opened.react.svg"
               />
             </a>
           </div>
