3efc35a9faf290fcb48a391298dad634e617cb8b
Diana <diana.vahomskaya@mail.ru>
2021-03-09 20:58:40 +0300

    fix

--- products/ASC.Files/Tests/TestMigrations/20210309174659_TestMigration.cs
+++ products/ASC.Files/Tests/TestMigrations/20210309174659_TestMigration.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ASC.Core.Common.Migrations.MySql.TenantDbContextMySql
 {
-    public partial class TestMigration : Migration
+    public partial class TestMigration : Microsoft.EntityFrameworkCore.Migrations.Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
