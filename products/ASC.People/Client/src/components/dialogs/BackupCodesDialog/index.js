0fe04284a42410acfa8a98cb8adf59eeb293b1cd
Viktor Fomin <enotcode@gmail.com>
2021-04-13 12:22:40 +0300

    Web: People: print backup codes

--- products/ASC.People/Client/src/components/dialogs/BackupCodesDialog/index.js
+++ products/ASC.People/Client/src/components/dialogs/BackupCodesDialog/index.js
@@ -11,6 +11,22 @@ class BackupCodesDialogComponent extends React.Component {
     super(props);
   }
 
+  printPage = () => {
+    const { t } = this.props;
+    const printContent = document.getElementById("backup-codes-print-content");
+    const printWindow = window.open(
+      "about:blank",
+      "",
+      "toolbar=0,scrollbars=1,status=0"
+    );
+    printWindow.document.write(`<h1>${t("BackupCodesTitle")}</h1>`);
+    printWindow.document.write(printContent.innerHTML);
+    printWindow.document.close();
+    printWindow.focus();
+    printWindow.print();
+    printWindow.close();
+  };
+
   render() {
     console.log("Render BackupCodesDialog");
     const { t, visible, onClose } = this.props;
@@ -21,35 +37,38 @@ class BackupCodesDialogComponent extends React.Component {
         <ModalDialog visible={visible} onClose={onClose}>
           <ModalDialog.Header>{t("BackupCodesTitle")}</ModalDialog.Header>
           <ModalDialog.Body>
-            <Text className="text-dialog">{t("BackupCodesDescription")}</Text>
-            <Text className="text-dialog">
-              {t("BackupCodesSecondDescription")}
-            </Text>
-
-            <Trans
-              t={t}
-              i18nKey="CodesCounter"
-              ns="BackupCodesDialog"
-              count={count}
-            >
+            <div id="backup-codes-print-content">
+              <Text className="text-dialog">{t("BackupCodesDescription")}</Text>
               <Text className="text-dialog">
-                <strong>{{ count }} codes:</strong>
+                {t("BackupCodesSecondDescription")}
               </Text>
-            </Trans>
-            <Text className="text-dialog" isBold={true}>
-              code1 <br />
-              code2 <br />
-              code3 <br />
-              code4 <br />
-              code5
-            </Text>
+
+              <Trans
+                t={t}
+                i18nKey="CodesCounter"
+                ns="BackupCodesDialog"
+                count={count}
+              >
+                <Text className="text-dialog">
+                  <strong>{{ count }} codes:</strong>
+                </Text>
+              </Trans>
+              <Text className="text-dialog" isBold={true}>
+                code1 <br />
+                code2 <br />
+                code3 <br />
+                code4 <br />
+                code5
+              </Text>
+            </div>
           </ModalDialog.Body>
           <ModalDialog.Footer>
             <Button
-              key="SendBtn"
+              key="PrintBtn"
               label={t("PrintButton")}
               size="medium"
               primary={true}
+              onClick={this.printPage}
             />
             <Button
               key="RequestNewBtn"
