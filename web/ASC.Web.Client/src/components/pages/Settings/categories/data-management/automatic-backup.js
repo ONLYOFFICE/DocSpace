0ddeac00a33f21411c55b415272352287f41c3d9
TatianaLopaeva <tatiana.lopaeva@onlyoffice.com>
2021-04-28 14:38:16 +0300

    Web: Settings: Backup: Added week and month options.

--- web/ASC.Web.Client/src/components/pages/Settings/categories/data-management/automatic-backup.js
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/data-management/automatic-backup.js
@@ -2,13 +2,17 @@ import React from "react";
 import Text from "@appserver/components/text";
 import { withTranslation } from "react-i18next";
 import commonSettingsStyles from "../../utils/commonSettingsStyles";
-import styled from "styled-components";
+import { inject, observer } from "mobx-react";
 import Button from "@appserver/components/button";
 import Checkbox from "@appserver/components/checkbox";
 import FieldContainer from "@appserver/components/field-container";
 import RadioButtonGroup from "@appserver/components/radio-button-group";
 import RadioButton from "@appserver/components/radio-button";
 import DocumentsModule from "./sub-components/documentsModule";
+import ComboBox from "@appserver/components/combobox";
+import styled from "styled-components";
+import moment from "moment";
+
 const StyledComponent = styled.div`
   ${commonSettingsStyles}
   .manual-backup_buttons {
@@ -46,6 +50,11 @@ const StyledComponent = styled.div`
 class AutomaticBackup extends React.Component {
   constructor(props) {
     super(props);
+    const { t, language } = props;
+
+    this.lng = language.substring(0, language.indexOf("-"));
+    moment.locale(this.lng);
+
     this.state = {
       backupMailTemporaryStorage: false,
       backupMailDocuments: false,
@@ -61,11 +70,84 @@ class AutomaticBackup extends React.Component {
       isCheckedThirdParty: false,
       isCheckedThirdPartyStorage: false,
 
-      monthlySchedule: "",
-      dailySchedule: "",
-      weeklySchedule: "",
+      monthlySchedule: false,
+      dailySchedule: false,
+      weeklySchedule: false,
+
+      selectedOption: t("DailyPeriodSchedule"),
+      selectedWeekdayOption: "",
+      selectedTimeOption: "12:00",
+      selectedMonthOption: "1",
+      weekOptions: [],
     };
+
+    this.periodOptions = [
+      {
+        key: 1,
+        label: t("DailyPeriodSchedule"),
+      },
+      {
+        key: 2,
+        label: t("WeeklyPeriodSchedule"),
+      },
+      {
+        key: 3,
+        label: t("MonthlyPeriodSchedule"),
+      },
+    ];
+
+    this.timeOptionsArray = [];
+    this.getTimeOptions();
+    this.monthNumberOptionsArray = [];
+    this.getMonthNumbersOption();
+    this.weekdaysOptions = [];
+    this.arrayWeekdays = moment.weekdays();
   }
+
+  componentDidMount() {
+    this.getWeekdaysOptions();
+  }
+
+  getTimeOptions = () => {
+    for (let item = 0; item < 24; item++) {
+      let obj = {
+        key: item,
+        label: `${item}:00`,
+      };
+      this.timeOptionsArray.push(obj);
+    }
+  };
+
+  getMonthNumbersOption = () => {
+    for (let item = 1; item <= 31; item++) {
+      let obj = {
+        key: item + 24,
+        label: `${item}`,
+      };
+      this.monthNumberOptionsArray.push(obj);
+    }
+  };
+  getWeekdaysOptions = () => {
+    for (let item = 0; item < this.arrayWeekdays.length; item++) {
+      let obj = {
+        key: item + 4,
+        label: `${this.arrayWeekdays[item]}`,
+      };
+      this.weekdaysOptions.push(obj);
+    }
+    const isEnglishLanguage = this.lng === "en";
+
+    if (!isEnglishLanguage) {
+      const startWeek = this.weekdaysOptions[0];
+      this.weekdaysOptions.shift();
+      this.weekdaysOptions.push(startWeek);
+    }
+
+    this.setState({
+      weekOptions: this.weekdaysOptions,
+      selectedWeekdayOption: this.weekdaysOptions[0].label,
+    });
+  };
 
   onClickCheckbox = (e) => {
     const name = e.target.name;
@@ -78,10 +160,14 @@ class AutomaticBackup extends React.Component {
     if (name === "enable") {
       this.setState({
         isShowedStorageTypes: true,
+        isCheckedDocuments: true,
+        isShowDocuments: true,
       });
     } else {
       this.setState({
         isShowedStorageTypes: false,
+        isCheckedDocuments: false,
+        isShowDocuments: false,
       });
     }
   };
@@ -112,18 +198,39 @@ class AutomaticBackup extends React.Component {
     this.setState({ [name]: change });
   };
 
-  onSelect = (options) => {
+  onSelectPeriodAndWeekday = (options) => {
     console.log("options", options);
+
     const key = options.key;
     const label = options.label;
-    key === 1
-      ? this.setState({ dailySchedule: label })
-      : key === 2
-      ? this.setState({ weeklySchedule: label })
-      : this.setState({ monthlySchedule: label });
+
+    if (key <= 3) {
+      this.setState({ selectedOption: label });
+      key === 1
+        ? this.setState({ weeklySchedule: false, monthlySchedule: false })
+        : key === 2
+        ? this.setState({ weeklySchedule: true, monthlySchedule: false })
+        : this.setState({ monthlySchedule: true, weeklySchedule: false });
+    } else {
+      this.setState({
+        selectedWeekdayOption: label,
+      });
+    }
+  };
+
+  onSelectMonthNumberAndTimeOptions = (options) => {
+    const key = options.key;
+    const label = options.label;
+    if (key <= 24) {
+      this.setState({ selectedTimeOption: label });
+    } else {
+      this.setState({
+        selectedMonthOption: label,
+      });
+    }
   };
   render() {
-    const { t } = this.props;
+    const { t, language } = this.props;
     const {
       backupMailTemporaryStorage,
       backupMailDocuments,
@@ -133,8 +240,16 @@ class AutomaticBackup extends React.Component {
       isCheckedDocuments,
       isCheckedThirdParty,
       isShowDocuments,
+      weeklySchedule,
+      monthlySchedule,
+      weekOptions,
+      selectedOption,
+      selectedWeekdayOption,
+      selectedTimeOption,
+      selectedMonthOption,
     } = this.state;
-    console.log(" this.periodOptions", this.rt);
+    console.log(" this.arrayWeekdays", this.arrayWeekdays);
+    console.log("isCheckedDocuments", isCheckedDocuments);
     return (
       <StyledComponent>
         <RadioButtonGroup
@@ -159,39 +274,95 @@ class AutomaticBackup extends React.Component {
         {isShowedStorageTypes && (
           <>
             <RadioButton
-              // name={"DocumentsModule"}
-              // options={[
-              //   {
-              //     label: t("DocumentsModule"),
-              //     value: "documentsModule",
-              //   },
-              //   {
-              //     label: "Third radiobtn",
-              //     value: "third",
-              //   },
-              // ]}
-              // isDisabled={false}
-              // onClick={() => console.log("res")}
-              // orientation="vertical"
-              // selected="documentsModule"
-
               fontSize="13px"
               fontWeight="400"
               label={t("DocumentsModule")}
               name={"DocumentsModule"}
-              //onChange={this.onClickShowStorage}
               onClick={this.onClickShowStorage}
               isChecked={isCheckedDocuments}
               value="value"
               className="automatic-backup_current_storage"
             />
             {isShowDocuments && (
-              <DocumentsModule
-                isManualBackup={false}
-                onClickCheckbox={this.onClickCheckbox}
-                backupMailDocuments={backupMailDocuments}
-                onSelect={this.onSelect}
-              />
+              <div className="category-item-wrapper">
+                <Text className="category-item-description">
+                  {t("DocumentsModuleDescription")}
+                </Text>
+
+                <ComboBox
+                  options={this.periodOptions}
+                  selectedOption={{
+                    key: 0,
+                    label: selectedOption,
+                  }}
+                  onSelect={this.onSelectPeriodAndWeekday}
+                  isDisabled={false}
+                  noBorder={false}
+                  scaled={false}
+                  scaledOptions={false}
+                  dropDownMaxHeight={300}
+                  size="base"
+                  className="backup_combobox"
+                />
+                {weeklySchedule && (
+                  <ComboBox
+                    options={weekOptions}
+                    selectedOption={{
+                      key: 0,
+                      label: selectedWeekdayOption,
+                    }}
+                    onSelect={this.onSelectPeriodAndWeekday}
+                    isDisabled={false}
+                    noBorder={false}
+                    scaled={false}
+                    scaledOptions={false}
+                    dropDownMaxHeight={300}
+                    size="base"
+                    className="backup_combobox"
+                  />
+                )}
+                {monthlySchedule && (
+                  <ComboBox
+                    options={this.monthNumberOptionsArray}
+                    selectedOption={{
+                      key: 0,
+                      label: selectedMonthOption,
+                    }}
+                    onSelect={this.onSelectMonthNumberAndTimeOptions}
+                    isDisabled={false}
+                    noBorder={false}
+                    scaled={false}
+                    scaledOptions={false}
+                    dropDownMaxHeight={300}
+                    size="base"
+                    className="backup_combobox"
+                  />
+                )}
+                <ComboBox
+                  options={this.timeOptionsArray}
+                  selectedOption={{
+                    key: 0,
+                    label: selectedTimeOption,
+                  }}
+                  onSelect={this.onSelectMonthNumberAndTimeOptions}
+                  isDisabled={false}
+                  noBorder={false}
+                  scaled={false}
+                  scaledOptions={false}
+                  dropDownMaxHeight={300}
+                  size="base"
+                  className="backup_combobox"
+                />
+
+                <div className="backup-include_mail">
+                  <Checkbox
+                    name={"backupMailDocuments"}
+                    isChecked={backupMailDocuments}
+                    label={t("IncludeMail")}
+                    onChange={this.onClickCheckbox}
+                  />
+                </div>
+              </div>
             )}
             <RadioButton
               fontSize="13px"
@@ -209,4 +380,10 @@ class AutomaticBackup extends React.Component {
     );
   }
 }
-export default withTranslation("Settings")(AutomaticBackup);
+export default inject(({ auth }) => {
+  const { language } = auth;
+
+  return {
+    language,
+  };
+})(withTranslation("Settings")(observer(AutomaticBackup)));
