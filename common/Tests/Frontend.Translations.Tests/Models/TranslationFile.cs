4013d0a799b151ca560e20a8cab8544a929cdd31
AlexeySafronov <alexey.safronov@onlyoffice.com>
2021-05-17 10:46:43 +0300

    Tests: Front-end: Added NUnit tests for i18n duplicates

---
+++ common/Tests/Frontend.Translations.Tests/Models/TranslationFile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Frontend.Translations.Tests
+{
+    public class TranslationFile
+    {
+        public TranslationFile(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; }
+
+        public List<TranslationItem> Translations { get; set; }
+    }
+}
