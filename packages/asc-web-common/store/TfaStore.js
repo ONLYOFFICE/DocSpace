cce7b94d5daabe1ae6ea8d0d5d196795404923bf
Viktor Fomin <viktor.fomin@onlyoffice.com>
2021-05-17 16:52:38 +0300

    Web: Common: deleted console.log

--- packages/asc-web-common/store/TfaStore.js
+++ packages/asc-web-common/store/TfaStore.js
@@ -12,7 +12,6 @@ class TfaStore {
   }
 
   getTfaSettings = async () => {
-    console.log("getTfaSettings");
     const res = await api.settings.getTfaSettings();
     const sms = res[0].enabled;
     const app = res[1].enabled;
@@ -24,15 +23,12 @@ class TfaStore {
   };
 
   setTfaSettings = async (type) => {
-    console.log("setTfaSettings");
     const res = await api.settings.setTfaSettings(type);
     console.log(res);
     this.getTfaConfirmLink(res, type);
   };
 
   getTfaConfirmLink = async (res, type) => {
-    console.log("getTfaConfirmLink");
-
     if (res && type !== "none") {
       const link = await api.settings.getTfaConfirmLink();
       console.log(link);
@@ -45,14 +41,12 @@ class TfaStore {
   };
 
   getBackupCodes = async () => {
-    console.log("getBackupCodes");
     const backupCodes = await api.settings.getTfaNewBackupCodes();
     console.log(backupCodes);
     this.backupCodes = backupCodes;
   };
 
   loginWithCode = async (userName, passwordHash, code) => {
-    //console.log("loginWithCode");
     return api.user.loginWithTfaCode(userName, passwordHash, code);
   };
 }
