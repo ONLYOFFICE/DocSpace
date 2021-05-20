d22937945be0001b0b9ca41665d65e01fe604d82
evgeniy-antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2021-05-20 16:00:06 +0300

    Code optimization

--- build/install/common/systemd/build.sh
+++ build/install/common/systemd/build.sh
@@ -31,12 +31,11 @@ while [ "$1" != "" ]; do
   shift
 done
 
-SERVICE_SYSNAME="appserver"
-PRODUCT="onlyoffice/${SERVICE_SYSNAME}"
-BASE_DIR="/etc/${PRODUCT}"
-PATH_TO_CONF="${BASE_DIR}"
-STORAGE_ROOT="${BASE_DIR}/data"
-LOG_DIR="/var/log/${PRODUCT}"
+PRODUCT="appserver"
+BASE_DIR="/var/www/${PRODUCT}"
+PATH_TO_CONF="/etc/onlyoffice/${PRODUCT}"
+STORAGE_ROOT="${PATH_TO_CONF}/data"
+LOG_DIR="/var/log/onlyoffice/${PRODUCT}"
 DOTNET_RUN="/usr/bin/dotnet"
 APP_URLS="http://0.0.0.0"
 ENVIRONMENT=" --ENVIRONMENT=production"
@@ -67,101 +66,101 @@ reassign_values (){
   case $1 in
 	api )
 		SERVICE_PORT="5000"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/studio/api/"
+		WORK_DIR="${BASE_DIR}/studio/api/"
 		EXEC_FILE="ASC.Web.Api.dll"
 	;;
 	api-system )
 		SERVICE_PORT="5010"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.ApiSystem/"
+		WORK_DIR="${BASE_DIR}/services/ASC.ApiSystem/"
 		EXEC_FILE="ASC.ApiSystem.dll"
 	;;
 	urlshortener )
 		SERVICE_PORT="9999"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.UrlShortener/"
+		WORK_DIR="${BASE_DIR}/services/ASC.UrlShortener/"
 		EXEC_FILE="ASC.UrlShortener.Svc.dll"
 	;;
 	thumbnails )
 		SERVICE_PORT="9800"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.Thumbnails/"
+		WORK_DIR="${BASE_DIR}/services/ASC.Thumbnails/"
 		EXEC_FILE="ASC.Thumbnails.Svc.dll"
 	;;
 	socket )
 		SERVICE_PORT="9899"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.Socket.IO/"
+		WORK_DIR="${BASE_DIR}/services/ASC.Socket.IO/"
 		EXEC_FILE="ASC.Socket.IO.Svc.dll"
 	;;
 	studio-notify )
 		SERVICE_PORT="5006"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.Studio.Notify/"
+		WORK_DIR="${BASE_DIR}/services/ASC.Studio.Notify/"
 		EXEC_FILE="ASC.Studio.Notify.dll"
-		CORE=" --core:products:folder=/var/www/${SERVICE_SYSNAME}/products --core:products:subfolder=server "
+		CORE=" --core:products:folder=${BASE_DIR}/products --core:products:subfolder=server "
 	;;
 	notify )
 		SERVICE_PORT="5005"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.Notify/"
+		WORK_DIR="${BASE_DIR}/services/ASC.Notify/"
 		EXEC_FILE="ASC.Notify.dll"
-		CORE=" --core:products:folder=/var/www/${SERVICE_SYSNAME}/products --core:products:subfolder=server "
+		CORE=" --core:products:folder=${BASE_DIR}/products --core:products:subfolder=server "
 	;;
 	people-server )
 		SERVICE_PORT="5004"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/products/ASC.People/server/"
+		WORK_DIR="${BASE_DIR}/products/ASC.People/server/"
 		EXEC_FILE="ASC.People.dll"
 	;;
 	files )
 		SERVICE_PORT="5007"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/products/ASC.Files/server/"
+		WORK_DIR="${BASE_DIR}/products/ASC.Files/server/"
 		EXEC_FILE="ASC.Files.dll"
 	;;
 	files-services )
 		SERVICE_PORT="5009"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/products/ASC.Files/service/"
+		WORK_DIR="${BASE_DIR}/products/ASC.Files/service/"
 		EXEC_FILE="ASC.Files.Service.dll"
-		CORE=" --core:products:folder=/var/www/${SERVICE_SYSNAME}/products --core:products:subfolder=server"
+		CORE=" --core:products:folder=${BASE_DIR}/products --core:products:subfolder=server"
 	;;
 	studio )
 		SERVICE_PORT="5003"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/studio/server/"
+		WORK_DIR="${BASE_DIR}/studio/server/"
 		EXEC_FILE="ASC.Web.Studio.dll"
 	;;
 	backup )
 		SERVICE_PORT="5012"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.Data.Backup/"
+		WORK_DIR="${BASE_DIR}/services/ASC.Data.Backup/"
 		EXEC_FILE="ASC.Data.Backup.dll"
-		CORE=" --core:products:folder=/var/www/${SERVICE_SYSNAME}/products --core:products:subfolder=server"
+		CORE=" --core:products:folder=${BASE_DIR}/products --core:products:subfolder=server"
 	;;
 	storage-migration )
 		SERVICE_PORT="5018"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.Data.Storage.Migration/"
+		WORK_DIR="${BASE_DIR}/services/ASC.Data.Storage.Migration/"
 		EXEC_FILE="ASC.Data.Storage.Migration.dll"
 	;;
 	storage-encryption )
 		SERVICE_PORT="5019"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.Data.Storage.Encryption/"
+		WORK_DIR="${BASE_DIR}/services/ASC.Data.Storage.Encryption/"
 		EXEC_FILE="ASC.Data.Storage.Encryption.dll"
 	;;
 	projects-server )
 		SERVICE_PORT="5020"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/products/ASC.Projects/server/"
+		WORK_DIR="${BASE_DIR}/products/ASC.Projects/server/"
 		EXEC_FILE="ASC.Projects.dll"
 	;;
 	telegram-service )
 		SERVICE_PORT="51702"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/services/ASC.TelegramService/"
+		WORK_DIR="${BASE_DIR}/services/ASC.TelegramService/"
 		EXEC_FILE="ASC.TelegramService.dll"
 	;;
 	crm )
 		SERVICE_PORT="5021"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/products/ASC.CRM/server/"
+		WORK_DIR="${BASE_DIR}/products/ASC.CRM/server/"
 		EXEC_FILE="ASC.CRM.dll"
 	;;
 	calendar )
 		SERVICE_PORT="5023"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/products/ASC.Calendar/server/"
+		WORK_DIR="${BASE_DIR}/products/ASC.Calendar/server/"
 		EXEC_FILE="ASC.Calendar.dll"
 	;;
 	mail )
 		SERVICE_PORT="5022"
-		WORK_DIR="/var/www/${SERVICE_SYSNAME}/products/ASC.Mail/server/"
+		WORK_DIR="${BASE_DIR}/products/ASC.Mail/server/"
 		EXEC_FILE="ASC.Mail.dll"
 	;;
   esac
@@ -173,13 +172,13 @@ reassign_values (){
 
 write_to_file () {
   sed -i -e 's#${SERVICE_NAME}#'$SERVICE_NAME'#g' -e 's#${WORK_DIR}#'$WORK_DIR'#g' -e \
-  "s#\${EXEC_START}#$EXEC_START#g" $BUILD_PATH/${SERVICE_SYSNAME}-${SERVICE_NAME[$i]}.service
+  "s#\${EXEC_START}#$EXEC_START#g" $BUILD_PATH/${PRODUCT}-${SERVICE_NAME[$i]}.service
 }
 
 mkdir -p $BUILD_PATH
 
 for i in ${!SERVICE_NAME[@]}; do
-  cp $BASEDIR/service $BUILD_PATH/${SERVICE_SYSNAME}-${SERVICE_NAME[$i]}.service
+  cp $BASEDIR/service $BUILD_PATH/${PRODUCT}-${SERVICE_NAME[$i]}.service
   reassign_values "${SERVICE_NAME[$i]}"
   write_to_file $i
 done
