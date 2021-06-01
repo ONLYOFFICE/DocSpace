76d8ebb4592e5856ed6c3671d6959a0527db5a20
pavelbannov <pavel.bannov@onlyoffice.com>
2021-06-01 12:21:51 +0300

    Tests moved to separate projects. Removed unused dependepcies

--- common/ASC.Core.Common/Properties/AssemblyInfo.cs
+++ common/ASC.Core.Common/Properties/AssemblyInfo.cs
@@ -1,3 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("3ff04204-4b22-4899-bcde-07edfcbb5355")]
+[assembly: InternalsVisibleTo("ASC.Core.Common.Tests")]
