a3cccc404498380b281353fd8e683ddef2b034cb
Alexey Bannov <alexey.bannov@onlyoffice.com>
2021-06-28 12:42:42 +0300

    crm: migrate from FromForm to FromBody attribute

---
+++ products/ASC.CRM/Server/ApiModels/RequestsDto/SetCurrencyRatesRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using ASC.CRM.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASC.CRM.ApiModels
+{
+    public class SetCurrencyRatesRequestDto
+    {
+        public String Currency { get; set; }
+        public List<CurrencyRate> Rates { get; set; }
+    }
+}
