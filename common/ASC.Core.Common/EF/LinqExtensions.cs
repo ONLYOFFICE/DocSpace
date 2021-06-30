b6b1c7276988ef92e43ac6478cdc968fee620631
Alexey Bannov <alexey.bannov@onlyoffice.com>
2021-06-30 19:08:41 +0300

    core: added new ThenBy linq extension. crm: fixed sql

--- common/ASC.Core.Common/EF/LinqExtensions.cs
+++ common/ASC.Core.Common/EF/LinqExtensions.cs
@@ -7,15 +7,26 @@ namespace ASC.Core.Common.EF
 {
     public static class LinqExtensions
     {
-        public static IQueryable<T> OrderBy<T>(this IQueryable<T> query, string name, bool sortOrderAsc)
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> query, string name, bool sortOrderAsc)
         {
             var propInfo = GetPropertyInfo(typeof(T), name);
             var expr = GetOrderExpression(typeof(T), propInfo);
 
             var method = typeof(Queryable).GetMethods().FirstOrDefault(m => m.Name == (sortOrderAsc ? "OrderBy" : "OrderByDescending") && m.GetParameters().Length == 2);
             var genericMethod = method.MakeGenericMethod(typeof(T), propInfo.PropertyType);
-            return (IQueryable<T>)genericMethod.Invoke(null, new object[] { query, expr });
+            return (IOrderedQueryable<T>)genericMethod.Invoke(null, new object[] { query, expr });
         }
+
+        public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> query, string name, bool sortOrderAsc)
+        {
+            var propInfo = GetPropertyInfo(typeof(T), name);
+            var expr = GetOrderExpression(typeof(T), propInfo);
+
+            var method = typeof(Queryable).GetMethods().FirstOrDefault(m => m.Name == (sortOrderAsc ? "ThenBy" : "ThenByDescending") && m.GetParameters().Length == 2);
+            var genericMethod = method.MakeGenericMethod(typeof(T), propInfo.PropertyType);
+            return (IOrderedQueryable<T>)genericMethod.Invoke(null, new object[] { query, expr });
+        }
+
 
         private static PropertyInfo GetPropertyInfo(Type objType, string name)
         {
