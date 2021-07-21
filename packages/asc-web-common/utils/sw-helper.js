4e920bfe3cf523d55e360df0e24e6f19137ec474
Viktor Fomin <viktor.fomin@onlyoffice.com>
2021-07-21 07:19:08 +0300

    Web: Common: disabled snackbar

--- packages/asc-web-common/utils/sw-helper.js
+++ packages/asc-web-common/utils/sw-helper.js
@@ -1,27 +1,47 @@
 import { Workbox } from "workbox-window";
-import { Snackbar } from "@appserver/components/snackbar";
-import { useTranslation } from "react-i18next";
+import Snackbar from "@appserver/components/snackbar";
+import React from "react";
+import ReactDOM from "react-dom";
+import SnackBar from "@appserver/components/snackbar";
 
 export function registerSW(homepage) {
   if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
     const wb = new Workbox(`${homepage}/sw.js`);
-    const { t } = useTranslation("Common", {
-      useSuspense: false,
-    });
-
     //TODO: watch https://developers.google.com/web/tools/workbox/guides/advanced-recipes and https://github.com/webmaxru/prog-web-news/blob/5ff94b45c9d317409c21c0fbb7d76e92f064471b/src/app/app-shell/app-shell.component.ts
-
     const showSkipWaitingPrompt = (event) => {
       console.log(
         `A new service worker has installed, but it can't activate` +
           `until all tabs running the current version have fully unloaded.`
       );
 
+      /*
       const barConfig = {
-        textBody: t("NewVersionAvailable"),
+        parentElementId: "snackbar",
+        text: "New Version Available",
+        btnText: "Reload",
+        onAction: () => onButtonClick(),
+        opacity: 1,
+      };
+
+      SnackBar.show(barConfig);
+
+      const onButtonClick = () => {
+        // Assuming the user accepted the update, set up a listener
+        // that will reload the page as soon as the previously waiting
+        // service worker has taken control.
+        wb.addEventListener("controlling", () => {
+          window.location.reload();
+        });
+
+        // This will postMessage() to the waiting service worker.
+        wb.messageSkipWaiting();
+      };*/
+
+      /*const barConfig = {
+        textBody: "New Version Available",
         pos: "top_center",
         showSecondButton: true,
-        secondButtonText: t("Reload"),
+        secondButtonText: "Reload",
 
         onSecondButtonClick: (element) => {
           element.style.opacity = 0;
@@ -35,9 +55,7 @@ export function registerSW(homepage) {
           // This will postMessage() to the waiting service worker.
           wb.messageSkipWaiting();
         },
-      };
-
-      Snackbar.show(barConfig);
+      };*/
 
       // let snackBarRef = this.snackBar.open(
 
