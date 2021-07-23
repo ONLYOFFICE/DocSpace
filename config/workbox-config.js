675fb8de2129163012d63b265c20d65a65eab947
AlexeySafronov <alexey.safronov@onlyoffice.com>
2021-07-23 17:20:42 +0300

    Web: Fix caching remoteEntry.js

--- config/workbox-config.js
+++ config/workbox-config.js
@@ -3,6 +3,7 @@ module.exports = {
 	globPatterns: [
 		'**/*.{ico,woff2,svg,html,json,js,png}'
 	],
+    globIgnores: ['**/remoteEntry.js'],
     swSrc: 'packages/asc-web-common/utils/sw-template.js',
 	swDest: 'build/deploy/public/sw.js'
 };
