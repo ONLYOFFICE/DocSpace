d8c8d267c97c5de95c6f60c661c686bebea9f1f2
gopienkonikita <nikita.gopienko@onlyoffice.com>
2021-07-27 09:57:00 +0300

    Web: Files: added an avatar to the author's cell

--- products/ASC.Files/Client/src/pages/Home/Section/Body/TableView/sub-components/AuthorCell.js
+++ products/ASC.Files/Client/src/pages/Home/Section/Body/TableView/sub-components/AuthorCell.js
@@ -1,18 +1,33 @@
 import React from "react";
 import Text from "@appserver/components/text";
+import styled from "styled-components";
 
-const AuthorCell = ({ fileOwner, sideColor }) => {
+const StyledAuthorCell = styled.div`
+  display: flex;
+
+  .author-avatar-cell {
+    width: 16px;
+    height: 16px;
+    margin-right: 8px;
+    border-radius: 20px;
+  }
+`;
+
+const AuthorCell = ({ fileOwner, sideColor, item }) => {
   return (
-    <Text
-      as="div"
-      color={sideColor}
-      fontSize="12px"
-      fontWeight={400}
-      title={fileOwner}
-      truncate={true}
-    >
-      {fileOwner}
-    </Text>
+    <StyledAuthorCell>
+      <img src={item.createdBy.avatarSmall} className="author-avatar-cell" />
+      <Text
+        as="div"
+        color={sideColor}
+        fontSize="12px"
+        fontWeight={400}
+        title={fileOwner}
+        truncate={true}
+      >
+        {fileOwner}
+      </Text>
+    </StyledAuthorCell>
   );
 };
 
