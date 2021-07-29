bd2a5070a204d964cee59ccf2bb70c628a109b9a
gopienkonikita <nikita.gopienko@onlyoffice.com>
2021-07-29 18:13:57 +0300

    Web: Components: fixed checkbox onChange event

--- packages/asc-web-components/checkbox/index.js
+++ packages/asc-web-components/checkbox/index.js
@@ -56,6 +56,7 @@ class Checkbox extends React.Component {
   }
 
   onInputChange(e) {
+    e.stopPropagation();
     this.setState({ checked: e.target.checked });
     this.props.onChange && this.props.onChange(e);
   }
