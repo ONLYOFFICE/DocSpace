a20956e5c7ea12a4e2cc2731cfbd774246b3f47b
TatianaLopaeva <tatiana.lopaeva@onlyoffice.com>
2021-07-30 17:54:30 +0300

    Web: Editor: Fixed styles.

--- web/ASC.Web.Editor/src/StyledEditor.js
+++ web/ASC.Web.Editor/src/StyledEditor.js
@@ -1,13 +1,13 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 const StyledSelectorFolder = styled.div`
   .editor-selector-folder_text {
     color: #555f65;
   }
   .editor-selector-folder_text-input {
-    //width: 100%;
-    //height: 20px;
     margin: 8px 0;
+    margin-top: 8px;
+    margin-bottom: 16px;
   }
 `;
 
