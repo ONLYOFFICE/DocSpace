55b14255c5639adeb0879eae16947f43ba6b50ad
AlexeySafronov <alexey.safronov@onlyoffice.com>
2021-08-11 19:36:15 +0300

    Web: Fix [mobx] Out of bounds read: 1

--- products/ASC.Files/Client/src/components/Article/Body/ThirdPartyList.js
+++ products/ASC.Files/Client/src/components/Article/Body/ThirdPartyList.js
@@ -81,7 +81,7 @@ const ServiceItem = (props) => {
   const { capability, src, ...rest } = props;
 
   const capabilityName = capability[0];
-  const capabilityLink = capability[1] ? capability[1] : "";
+  const capabilityLink = capability.length > 1 ? capability[1] : "";
 
   const dataProps = {
     "data-link": capabilityLink,
