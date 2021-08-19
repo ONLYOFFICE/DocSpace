fc32619c9a9676058c1c22deae3b037602f27e8f
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2021-08-19 14:55:39 +0300

    WEb: Editor: Deleted useless code.

--- web/ASC.Web.Editor/src/Editor.jsx
+++ web/ASC.Web.Editor/src/Editor.jsx
@@ -684,7 +684,6 @@ const Editor = () => {
                 : mailMergeActionProps)}
               header={<SelectFileHeader />}
               headerName={i18n.t("SelectFileTitle")}
-              modalHeightContent="252px"
             />
           )}
 
