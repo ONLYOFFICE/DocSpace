ecc5c69722f0d39b7839e0e6bd7392e601bdf982
Viktor Fomin <viktor.fomin@onlyoffice.com>
2021-09-07 18:34:46 +0300

    Web: Files: fixed banner translations

--- products/ASC.Files/Client/src/components/Article/Body/Banner.js
+++ products/ASC.Files/Client/src/components/Article/Body/Banner.js
@@ -1,11 +1,54 @@
 import React, { useEffect, useState } from "react";
-import { useTranslation } from "react-i18next";
+import { useTranslation, withTranslation } from "react-i18next";
 
 import CampaignsBanner from "@appserver/components/campaigns-banner";
 import { ADS_TIMEOUT } from "../../../helpers/constants";
 import { LANGUAGE } from "@appserver/common/constants";
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+import { getLanguage } from "@appserver/common/utils";
 
-const Banner = ({ FirebaseHelper }) => {
+const i18nConfig = i18n.createInstance();
+
+let translationUrl;
+
+const loadLanguagePath = async () => {
+  if (!window.firebaseHelper) return;
+
+  const lng = localStorage.getItem(LANGUAGE) || "en";
+  const language = getLanguage(lng instanceof Array ? lng[0] : lng);
+
+  const campaigns = (localStorage.getItem("campaigns") || "")
+    .split(",")
+    .filter((campaign) => campaign.length > 0);
+  const index = Number(localStorage.getItem("bannerIndex") || 0);
+  const campaign = campaigns[index];
+
+  translationUrl = await window.firebaseHelper.getCampaignsTranslations(
+    campaign,
+    language
+  );
+  console.log("translationUrl", translationUrl);
+  return translationUrl;
+};
+
+i18nConfig.use(Backend).init({
+  lng: localStorage.getItem(LANGUAGE) || "en",
+  fallbackLng: "en",
+  load: "all",
+  debug: true,
+  defaultNS: "",
+
+  backend: {
+    loadPath: function () {
+      return translationUrl;
+    },
+  },
+});
+
+const bannerHOC = (WrappedComponent) => (props) => {
+  const { FirebaseHelper } = props;
+
   const campaigns = (localStorage.getItem("campaigns") || "")
     .split(",")
     .filter((campaign) => campaign.length > 0);
@@ -13,6 +56,7 @@ const Banner = ({ FirebaseHelper }) => {
   const defaultBannerName = "Cloud";
   const [bannerName, setBannerName] = useState(defaultBannerName);
   const [bannerImage, setBannerImage] = useState("");
+  const [bannerTranslation, setBannerTranslation] = useState();
 
   const updateBanner = async () => {
     console.log("update banner");
@@ -26,13 +70,6 @@ const Banner = ({ FirebaseHelper }) => {
       index++;
     }
 
-    const lng = localStorage.getItem(LANGUAGE).split("-")[0] || "en";
-    const translate = await FirebaseHelper.getCampaignsTranslations(
-      campaign,
-      lng
-    );
-    localStorage.setItem("campaignsT", translate);
-
     const image = await FirebaseHelper.getCampaignsImages(
       campaign.toLowerCase()
     );
@@ -42,32 +79,55 @@ const Banner = ({ FirebaseHelper }) => {
     setBannerName(campaign);
   };
 
-  useEffect(() => {
+  useEffect(async () => {
+    const translationUrl = await loadLanguagePath();
+    setBannerTranslation(translationUrl);
     updateBanner();
     setInterval(updateBanner, ADS_TIMEOUT);
   }, []);
 
-  const { t, i18n, ready } = useTranslation(`CampaignPersonal${bannerName}`, {
-    useSuspense: false,
-  });
+  console.log("banner hoc render", bannerTranslation);
+  if (!bannerTranslation || !bannerName || !bannerImage) return <p>Loading</p>;
+
+  return (
+    <WrappedComponent
+      bannerName={bannerName}
+      bannerImage={bannerImage}
+      {...props}
+    />
+  );
+};
+
+const Banner = (props) => {
+  console.log("Banner render", props);
+  const { t, tReady, bannerName, bannerImage, FirebaseHelper } = props;
+  const campaigns = (localStorage.getItem("campaigns") || "")
+    .split(",")
+    .filter((campaign) => campaign.length > 0);
 
   if (
     !campaigns.length ||
-    !ready ||
-    !i18n.exists(`CampaignPersonal${bannerName}:Header`)
+    !tReady /*||
+    !i18n.exists(`CampaignPersonal${bannerName}:Header`)*/
   ) {
     return <></>;
   }
 
   return (
     <CampaignsBanner
-      headerLabel={t(`CampaignPersonal${bannerName}:Header`)}
-      subHeaderLabel={t(`CampaignPersonal${bannerName}:SubHeader`)}
+      headerLabel={t("Header")}
+      subHeaderLabel={t("SubHeader")}
       img={bannerImage}
-      btnLabel={t(`CampaignPersonal${bannerName}:ButtonLabel`)}
-      link={t(`CampaignPersonal${bannerName}:Link`)}
+      btnLabel={t("ButtonLabel")}
+      link={t("Link")}
     />
   );
 };
 
-export default Banner;
+const ExtendedComponent = withTranslation()(Banner);
+
+const WrapperComponent = (props) => (
+  <ExtendedComponent i18n={i18nConfig} useSuspense={false} {...props} />
+);
+
+export default bannerHOC(WrapperComponent);
