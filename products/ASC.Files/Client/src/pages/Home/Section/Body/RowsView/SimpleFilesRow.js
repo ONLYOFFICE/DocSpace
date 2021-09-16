f2dd335c7287c33cf95300daee96a4a3da843099
Viktor Fomin <viktor.fomin@onlyoffice.com>
2021-09-16 13:43:49 +0300

    Web: Files: fixed cursor

--- products/ASC.Files/Client/src/pages/Home/Section/Body/RowsView/SimpleFilesRow.js
+++ products/ASC.Files/Client/src/pages/Home/Section/Body/RowsView/SimpleFilesRow.js
@@ -13,7 +13,7 @@ import ItemIcon from "../../../../../components/ItemIcon";
 
 const StyledSimpleFilesRow = styled(Row)`
   background: ${(props) => props.checked && "#f8f9f9"};
-  cursor: ${(props) => props.checked && "pointer"};
+  cursor: ${(props) => props.checked && "grab"};
   margin-top: -2px;
   ${(props) =>
     !props.contextOptions &&
