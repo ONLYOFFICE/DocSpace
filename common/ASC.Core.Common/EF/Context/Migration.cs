a5e1609dac934bedc59bca986f393a76b0261ec9
diana-vahomskaya <diana.vahomskaya@mail.ru>
2021-09-16 11:25:17 +0300

    two methods

---
+++ common/ASC.Core.Common/EF/Context/Migration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC.Core.Common.EF.Context
+{
+    public class Migration
+    {
+        public Migration(string name, string query, int number)
+        {
+            Number = number;
+            Name = name;
+            Query = query;
+
+        }
+
+        public readonly int Number;
+
+        public string Query { get; }
+
+        public string Name { get; }
+    }
+}
---
+++ common/ASC.Core.Common/EF/Context/Migrator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Migrations;
+using System.IO;
+using System.Linq;
+
+namespace ASC.Core.Common.EF.Context
+{
+    public class Migrator
+    {
+        internal ConnectionStringSettings ConnectionStringSettings { get; }
+        
+        //Method 1
+   
+        public void UpdateDb()
+        {
+            var configuration = new DbMigrationsConfiguration
+            {
+                TargetDatabase = new DbConnectionInfo(ConnectionStringSettings.ConnectionString, ConnectionStringSettings.ProviderName)
+            };
+
+            var migrator = new DbMigrator(configuration);
+            var local = migrator.GetLocalMigrations().ToArray();
+            migrator.Update();
+        }
+
+        //Method 2
+        public static IEnumerable<Migration> GetLocalMigrations()
+        {
+            var migrations = new List<Migration>();
+            string Migration_Path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Migrations");
+            var Migration_Files = Directory.GetFiles(Migration_Path);
+            foreach (var MigrationsFiles in Migration_Files)
+            {
+                var FileName = Path.GetFileName(MigrationsFiles);
+                var file = File.ReadAllText(MigrationsFiles);
+                var name = FileName.Split('-');
+                var success = int.TryParse(name[0], out var migrationNumber);
+                if (success)
+                {
+                    if (migrations.Any(m => m.Number == migrationNumber))
+                    {
+                        throw new ArgumentException(
+                            $"There are two migrations with this {migrationNumber}.");
+                    }
+
+                    migrations.Add(new Migration(FileName, file, migrationNumber));
+                }
+                else
+                {
+                    throw new FormatException($"The migration file must start with a number.");
+                }
+                
+            }
+            return migrations;
+        }
+    }
+}
+
