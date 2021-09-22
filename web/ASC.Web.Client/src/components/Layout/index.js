844b996a6649003f8f392693f56d85c78b1af715
gopienkonikita <nikita.gopienko@onlyoffice.com>
2021-09-22 16:37:11 +0300

    Web: Client: fixed window height for IOS (Firefox, Safari)

--- web/ASC.Web.Client/src/components/Layout/index.js
+++ web/ASC.Web.Client/src/components/Layout/index.js
@@ -127,7 +127,7 @@ const Layout = (props) => {
 
     const updateHeight = () => {
       const correctorMobileChrome = 57; // ios
-      const correctorTabletSafari = 71; // ios
+      //const correctorTabletSafari = 71; // ios
 
       clearInterval(intervalHandler);
       clearTimeout(timeoutHandler);
@@ -142,14 +142,15 @@ const Layout = (props) => {
           height = window.screen.availWidth - correctorMobileChrome;
         }
       }
-      if (isTablet && isIOS && isSafari) {
-        if (
-          window.innerHeight < window.innerWidth &&
-          window.innerWidth > 1024
-        ) {
-          height = window.screen.availHeight - correctorTabletSafari;
-        }
-      }
+
+      // if (isTablet && isIOS && isSafari) {
+      //   if (
+      //     window.innerHeight < window.innerWidth &&
+      //     window.innerWidth > 1024
+      //   ) {
+      //     height = window.screen.availHeight - correctorTabletSafari;
+      //   }
+      // }
       setContentHeight(height);
     };
     intervalHandler = setInterval(() => {
