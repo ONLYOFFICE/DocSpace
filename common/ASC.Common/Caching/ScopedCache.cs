a0e39d011266060539d2c5a5445240f5b8f0bfa4
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-09-27 19:54:01 +0300

    add ScopeCache

---
+++ common/ASC.Common/Caching/ScopedCache.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace ASC.Common.Caching
+{
+    [Scope]
+    public class ScopedCache
+    {
+        private Dictionary<string, object> cache
+            = new Dictionary<string, object>();
+
+        public T Get<T>(string key)
+        {
+            cache.TryGetValue(key, out var result);
+
+            return (T)result;
+        }
+        
+        public void Insert(string key, object obj)
+        {
+            if (cache.ContainsKey(key)) cache.Remove(key);
+
+            cache.Add(key, obj);
+        }
+        
+        public void Remove(string key)
+        {
+            cache.Remove(key);
+        }
+    }
+}
