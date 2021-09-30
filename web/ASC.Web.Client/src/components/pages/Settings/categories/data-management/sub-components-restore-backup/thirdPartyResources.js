57804746a5cb7ae586039546712c3103b9941370
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2021-09-30 09:42:06 +0300

    Web: Restore backup: Refactoring after merge.

--- web/ASC.Web.Client/src/components/pages/Settings/categories/data-management/sub-components-restore-backup/thirdPartyResources.js
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/data-management/sub-components-restore-backup/thirdPartyResources.js
@@ -1,30 +1,15 @@
 import React from "react";
-import PropTypes from "prop-types";
-
-import FileInput from "@appserver/components/file-input";
 import SelectFileInput from "files/SelectFileInput";
+
 class ThirdPartyResources extends React.Component {
   render() {
-    const {
-      onClose,
-      isPanelVisible,
-      onClickInput,
-      iconUrl,
-      filterType,
-      filterValue,
-      withSubfolders,
-      onSelectFile,
-    } = this.props;
+    const { onClose, isPanelVisible, onClickInput, onSelectFile } = this.props;
     return (
       <SelectFileInput
-        isPanelVisible={isPanelVisible}
-        onClose={onClose}
         onClickInput={onClickInput}
-        iconUrl={iconUrl}
-        filterValue={filterValue}
-        withSubfolders={withSubfolders}
-        filterType={filterType}
         onSelectFile={onSelectFile}
+        onClose={onClose}
+        isPanelVisible={isPanelVisible}
         foldersType="third-party"
       />
     );
