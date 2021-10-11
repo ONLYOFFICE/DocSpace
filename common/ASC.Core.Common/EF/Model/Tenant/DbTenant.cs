0066585554c5adf40bb3ae9e1a46dc7da9834f99
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-10-11 17:23:02 +0300

    replace DateTime.Now to fixed DateTime

--- common/ASC.Core.Common/EF/Model/Tenant/DbTenant.cs
+++ common/ASC.Core.Common/EF/Model/Tenant/DbTenant.cs
@@ -48,7 +48,7 @@ namespace ASC.Core.Common.EF.Model
                     Id = 1,
                     Alias = "localhost",
                     Name = "Web Office",
-                    CreationDateTime = DateTime.UtcNow,
+                    CreationDateTime = new DateTime(2021, 3, 9, 17, 46, 59, 97, DateTimeKind.Utc).AddTicks(4317),
                     OwnerId = Guid.Parse("66faa6e4-f133-11ea-b126-00ffeec8b4ef")
                 }
                 );
