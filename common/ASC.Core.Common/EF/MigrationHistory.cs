9541b9e6b4483e28d7c9e3421057555acceb34aa
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-10-12 12:04:24 +0300

    optimization migration

---
+++ common/ASC.Core.Common/EF/MigrationHistory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+using ASC.Common;
+
+namespace ASC.Core.Common.EF
+{
+    [Singletone]
+    public class MigrationHistory
+    {
+        private ConcurrentDictionary<string, bool> _historyStore
+            = new ConcurrentDictionary<string, bool>();
+
+        public bool TryAddMigratedContext(string contextTypeName)
+        {
+            return _historyStore.TryAdd(contextTypeName, true);
+        }
+    }
+}
