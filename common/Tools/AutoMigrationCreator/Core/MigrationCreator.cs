24f658754ffba463ecbae7c717957928a8b65758
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-10-12 17:51:13 +0300

    refactor

---
+++ common/Tools/AutoMigrationCreator/Core/MigrationCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AutoMigrationCreator
+{
+    public class MigrationCreator
+    {
+        public static void Run(Settings settings)
+        {
+            var counter = 0;
+            var ctxTypesFinder = new ContextFinder(settings);
+
+            foreach (var contextType in ctxTypesFinder.GetContextsTypes())
+            {
+                var context = DbContextActivator.CreateInstance(contextType, settings.ConnectionStringSettings);
+
+                var modelDiffChecker = new ModelDifferenceChecker(context);
+
+                if (!modelDiffChecker.IsDifferent()) continue;
+
+                context = DbContextActivator.CreateInstance(contextType, settings.ConnectionStringSettings); //Hack: refresh context
+
+                var migrationGenerator = new MigrationGenerator(context);
+                migrationGenerator.Generate();
+
+                counter++;
+            }
+
+            Console.WriteLine($"Created {counter} migrations");
+        }
+    }
+}
