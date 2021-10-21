706913275c651437995bef876062fdf48ec40ab8
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2021-10-21 13:19:28 +0300

    Web: Common: PageLayout: Fixed error "Cannot read properties of null (reading 'focus')".

--- packages/asc-web-common/components/PageLayout/sub-components/section-body.js
+++ packages/asc-web-common/components/PageLayout/sub-components/section-body.js
@@ -125,8 +125,9 @@ class SectionBody extends React.Component {
   // }
 
   componentDidMount() {
+    const { withScroll } = this.props;
     if (!this.props.autoFocus) return;
-    this.focusRef.current.focus();
+    if (withScroll) this.focusRef.current.focus();
   }
 
   componentWillUnmount() {
