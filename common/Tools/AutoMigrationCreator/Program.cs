4b2f7242fca6250f9e814e3b79ef1f22c167dc4d
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2021-10-22 20:05:40 +0300

    Tools: refactor

--- common/Tools/AutoMigrationCreator/Program.cs
+++ common/Tools/AutoMigrationCreator/Program.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using System.IO;
-
-using Microsoft.Extensions.Configuration;
-
-namespace AutoMigrationCreator
+﻿namespace AutoMigrationCreator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var configPath = Path.GetFullPath(
-               Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-               "..", "..", "..", "..", "..", "..", "config", "appsettings.json"));
-
-            var config = new ConfigurationBuilder().AddJsonFile(configPath).Build();
-            var settings = Settings.GetSettings(config);
-
-            MigrationCreator.Run(settings);
+            MigrationCreator.Run();
         }
     }
 }
