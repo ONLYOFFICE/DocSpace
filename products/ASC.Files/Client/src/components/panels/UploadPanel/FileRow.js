19252882eda1d1040f53ee4579b827f7c0b57b11
Viktor Fomin <viktor.fomin@onlyoffice.com>
2021-10-29 16:29:18 +0300

    Web: Files: delete useless

--- products/ASC.Files/Client/src/components/panels/UploadPanel/FileRow.js
+++ products/ASC.Files/Client/src/components/panels/UploadPanel/FileRow.js
@@ -44,28 +44,6 @@ const StyledFileRow = styled(Row)`
   .convert_icon {
     padding-right: 12px;
   }
-
-  .__react_component_tooltip.type-light {
-    background-color: #f8f7bf !important;
-    box-shadow: none;
-    -moz-box-shadow: none;
-    -webkit-box-shadow: none;
-  }
-  .__react_component_tooltip.place-left::after {
-    border-left: 6px solid #f8f7bf !important;
-  }
-
-  .__react_component_tooltip.place-right::after {
-    border-right: 6px solid #f8f7bf !important;
-  }
-
-  .__react_component_tooltip.place-top::after {
-    border-top: 6px solid #f8f7bf !important;
-  }
-
-  .__react_component_tooltip.place-bottom::after {
-    border-bottom: 6px solid #f8f7bf !important;
-  }
 `;
 
 const FileRow = (props) => {
