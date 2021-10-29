b15bc7b5cf10747ff06ebcf83552068f76feba88
pavelbannov <pavel.bannov@onlyoffice.com>
2021-10-29 13:23:37 +0300

    WebHook: fix service

--- build/run/WebhooksService.bat
+++ build/run/WebhooksService.bat
@@ -1,2 +1,4 @@
-echo "RUN ASC.Webhooks.Service"
-call dotnet run --project ..\..\common\services\ASC.Webhooks.Service\ASC.Webhooks.Service.csproj --no-build --$STORAGE_ROOT=..\..\..\Data --log__dir=..\..\..\Logs --log__name=webhooks
+@echo off
+
+PUSHD %~dp0..\..
+set servicepath=%cd%\common\services\ASC.Webhooks.Service\bin\Debug\ASC.Webhooks.Service.exe urls=http://0.0.0.0:5031 $STORAGE_ROOT=%cd%\Data  log:dir=%cd%\Logs log:name=webhooks pathToConf=%cd%\config core:products:folder=%cd%\products
