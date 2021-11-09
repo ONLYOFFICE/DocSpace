68413b971702eaf4e3083fdab5c94fd7652c151f
TimofeyBoyko <timofey.boyko@onlyoffice.com>
2021-11-09 15:45:50 +0800

    Web:Login: add and init codeceptJS

---
+++ web/ASC.Web.Login/codecept.conf.js
@@ -0,0 +1,35 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './test/*_tests.js',
+  output: './test/output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'ASC.Web.Login',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
