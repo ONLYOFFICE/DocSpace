7334fec09f1c698428e40d0230f3f559367534bc
TimofeyBoyko <timofey.boyko@onlyoffice.com>
2021-11-24 18:21:28 +0800

    Web:People:Tests: refactoring and rename tests

--- products/ASC.People/Client/tests/action_tests.js
+++ products/ASC.People/Client/tests/action_tests.js
@@ -6,7 +6,7 @@ const deviceType = process.env.DEVICE_TYPE || 'desktop';
 const isModel = !!process.env.MODEL;
 
 const featureName = isModel
-  ? `Create screenshots actions model on '${browser}' with '${deviceType}' dimension`
+  ? `Create screenshots actions model on '${browser}' with '${deviceType}' dimension (actions)`
   : `People actions on '${browser}' with '${deviceType}' dimension`;
 
 const form = {
@@ -27,19 +27,24 @@ Before(async ({ I }) => {
 Scenario('Create new user test', async ({ I }) => {
   I.mockEndpoint(Endpoints.group, 'empty');
   I.mockEndpoint(Endpoints.filter, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(3);
+  I.openPage();
+
   if (deviceType !== 'desktop') I.openArticle();
   I.clickArticleMainButton();
   I.click({ react: 'DropDownItem', props: { label: 'User' } });
-  if (deviceType !== 'desktop') I.forceClick('.backdrop-active');
-  I.see('User (creation)');
+
   I.checkRequest('http://localhost:8092/api/2.0/people', form, 'people', 'createUser');
   I.mockEndpoint(Endpoints.createUser, 'createUser');
   I.mockEndpoint(Endpoints.filter, 'createUser');
+
+  if (deviceType !== 'desktop') I.forceClick('.backdrop-active');
+
+  I.see('User (creation)');
+
   I.fillForm(form);
+
   I.checkText(form, 'User');
+
   I.saveScreenshot(`10.create-new-user.png`);
   if (!isModel) {
     I.seeVisualDiff(`10.create-new-user.png`, {
@@ -52,19 +57,24 @@ Scenario('Create new user test', async ({ I }) => {
 Scenario('Create new guest test', async ({ I }) => {
   I.mockEndpoint(Endpoints.group, 'empty');
   I.mockEndpoint(Endpoints.filter, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(3);
+  I.openPage();
+
   if (deviceType !== 'desktop') I.openArticle();
   I.clickArticleMainButton();
   I.click({ react: 'DropDownItem', props: { label: 'Guest' } });
-  if (deviceType !== 'desktop') I.forceClick('.backdrop-active');
-  I.see('Guest (creation)');
+
   I.checkRequest('http://localhost:8092/api/2.0/people', form, 'people', 'createGuest');
   I.mockEndpoint(Endpoints.createUser, 'createGuest');
   I.mockEndpoint(Endpoints.filter, 'createGuest');
+
+  if (deviceType !== 'desktop') I.forceClick('.backdrop-active');
+
+  I.see('Guest (creation)');
+
   I.fillForm(form);
+
   I.checkText(form, 'Guest');
+
   I.saveScreenshot(`11.create-new-guest.png`);
   if (!isModel) {
     I.seeVisualDiff(`11.create-new-guest.png`, {
@@ -77,14 +87,12 @@ Scenario('Create new guest test', async ({ I }) => {
 Scenario('Create new group test', async ({ I }) => {
   I.mockEndpoint(Endpoints.group, 'empty');
   I.mockEndpoint(Endpoints.filter, 'many');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(3);
+  I.openPage();
+
   if (deviceType !== 'desktop') I.openArticle();
   I.clickArticleMainButton();
   I.click({ react: 'DropDownItem', props: { label: 'Group' } });
-  if (deviceType !== 'desktop') I.forceClick('.backdrop-active');
-  I.see('Group (creation)');
+
   I.checkRequest(
     'http://localhost:8092/api/2.0/group.json',
     { groupName: groupName },
@@ -92,15 +100,23 @@ Scenario('Create new group test', async ({ I }) => {
     'createGroup',
   );
   I.mockEndpoint(Endpoints.group, 'createGroup');
-  I.fillField('group-name', groupName);
-  I.click('Select');
-  I.click('Administrator1');
+
   if (deviceType !== 'desktop') I.forceClick('.backdrop-active');
-  I.click('#users-selector_button');
-  I.click({ react: 'Checkbox', props: { value: '2' } });
-  I.click('Add members');
+
+  I.see('Group (creation)');
+
+  I.fillField('group-name', groupName);
+
+  I.addHeadInNewGroup();
+
+  if (deviceType !== 'desktop') I.forceClick('.backdrop-active');
+
+  I.addUserInNewGroup();
+
   I.click('Add');
+
   I.waitForText('Done', 3);
+
   if (deviceType !== 'desktop') I.openArticle();
   I.see(groupName);
 });
--- products/ASC.People/Client/tests/render_tests.js
+++ products/ASC.People/Client/tests/render_tests.js
@@ -5,7 +5,7 @@ const deviceType = process.env.DEVICE_TYPE || 'desktop';
 const isModel = !!process.env.MODEL;
 
 const featureName = isModel
-  ? `Create screenshots model on '${browser}' with '${deviceType}' dimension`
+  ? `Create screenshots model on '${browser}' with '${deviceType}' dimension (render)`
   : `Render people on '${browser}' with '${deviceType}' dimension`;
 
 Feature(featureName);
@@ -15,13 +15,13 @@ Before(async ({ I }) => {
   I.mockData();
 });
 
-Scenario('Test empty people list render', async ({ I }) => {
+Scenario('Test empty people and group lists render', async ({ I }) => {
   I.mockEndpoint(Endpoints.group, 'empty');
   I.mockEndpoint(Endpoints.filter, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.waitForText('No teammates found', 5);
+  I.openPage();
+
   I.see('No teammates found');
+
   I.saveScreenshot(`1.empty-people-list.png`);
   if (!isModel) {
     I.seeVisualDiff(`1.empty-people-list.png`, {
@@ -29,95 +29,82 @@ Scenario('Test empty people list render', async ({ I }) => {
       prepareBaseImage: false,
     });
   }
+
+  if (deviceType !== 'desktop') {
+    I.openArticle();
+
+    I.saveScreenshot(`2.empty-group-list.png`);
+    if (!isModel) {
+      I.seeVisualDiff(`2.empty-group-list.png`, {
+        tolerance: 1,
+        prepareBaseImage: false,
+      });
+    }
+  }
 });
 
-Scenario('Test one person list render', async ({ I }) => {
+Scenario('Test one person and group lists render', async ({ I }) => {
   I.mockEndpoint(Endpoints.filter, 'one');
-  I.mockEndpoint(Endpoints.group, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(5);
-  I.saveScreenshot(`2.one-person-people-list.png`);
+  I.mockEndpoint(Endpoints.group, 'one');
+  I.openPage();
+
+  I.saveScreenshot(`3.one-person-people-list.png`);
   if (!isModel) {
-    I.seeVisualDiff(`2.one-person-people-list.png`, {
+    I.seeVisualDiff(`3.one-person-people-list.png`, {
       tolerance: 1,
       prepareBaseImage: false,
     });
   }
+
+  if (deviceType !== 'desktop') {
+    I.openArticle();
+
+    I.saveScreenshot(`4.one-group-list.png`);
+    if (!isModel) {
+      I.seeVisualDiff(`4.one-group-list.png`, {
+        tolerance: 1,
+        prepareBaseImage: false,
+      });
+    }
+  }
 });
 
-Scenario('Test many people list render', async ({ I }) => {
+Scenario('Test many people and groups lists render', async ({ I }) => {
   I.mockEndpoint(Endpoints.filter, 'many');
   I.mockEndpoint(Endpoints.group, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(5);
-  I.saveScreenshot(`3.many-people-list.png`);
+  I.openPage();
+
+  I.saveScreenshot(`5.many-people-list.png`);
   if (!isModel) {
-    I.seeVisualDiff(`3.many-people-list.png`, {
+    I.seeVisualDiff(`5.many-people-list.png`, {
       tolerance: 1,
       prepareBaseImage: false,
     });
   }
-});
 
-Scenario('Test empty group list render', async ({ I }) => {
-  I.mockEndpoint(Endpoints.filter, 'empty');
-  I.mockEndpoint(Endpoints.group, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(5);
-  if (deviceType !== 'desktop') I.openArticle();
-  I.saveScreenshot(`4.empty-group-list.png`);
-  if (!isModel) {
-    I.seeVisualDiff(`4.empty-group-list.png`, {
-      tolerance: 1,
-      prepareBaseImage: false,
-    });
-  }
-});
+  if (deviceType !== 'desktop') {
+    I.openArticle();
 
-Scenario('Test one group list render', async ({ I }) => {
-  I.mockEndpoint(Endpoints.group, 'one');
-  I.mockEndpoint(Endpoints.filter, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(5);
-  if (deviceType !== 'desktop') I.openArticle();
-  I.saveScreenshot(`5.one-group-list.png`);
-  if (!isModel) {
-    I.seeVisualDiff(`5.one-group-list.png`, {
-      tolerance: 1,
-      prepareBaseImage: false,
-    });
-  }
-});
-
-Scenario('Test many groups list render', async ({ I }) => {
-  I.mockEndpoint(Endpoints.group, 'many');
-  I.mockEndpoint(Endpoints.filter, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(5);
-  if (deviceType !== 'desktop') I.openArticle();
-  I.saveScreenshot(`6.many-groups-list.png`);
-  if (!isModel) {
-    I.seeVisualDiff(`6.many-groups-list.png`, {
-      tolerance: 1,
-      prepareBaseImage: false,
-    });
+    I.saveScreenshot(`6.many-groups-list.png`);
+    if (!isModel) {
+      I.seeVisualDiff(`6.many-groups-list.png`, {
+        tolerance: 1,
+        prepareBaseImage: false,
+      });
+    }
   }
 });
 
 Scenario('Test profile menu render', async ({ I }) => {
   I.mockEndpoint(Endpoints.group, 'empty');
   I.mockEndpoint(Endpoints.filter, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(5);
+  I.openPage();
+
   I.openProfileMenu();
+
   I.seeElement({ react: 'Backdrop', props: { visible: true } });
   I.seeElement({ react: 'DropDown', props: { className: 'profile-menu' } });
+
   I.saveScreenshot(`7.profile-menu.png`);
   if (!isModel) {
     I.seeVisualDiff(`7.profile-menu.png`, {
@@ -130,15 +117,15 @@ Scenario('Test profile menu render', async ({ I }) => {
 Scenario('Test list of actions render', async ({ I }) => {
   I.mockEndpoint(Endpoints.group, 'empty');
   I.mockEndpoint(Endpoints.filter, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(5);
+  I.openPage();
+
   if (deviceType !== 'desktop') I.openArticle();
   I.clickArticleMainButton();
   I.see('User');
   I.see('Guest');
   I.see('Group');
   I.see('Invitation link');
+
   I.saveScreenshot(`8.article-main-button-list.png`);
   if (!isModel) {
     I.seeVisualDiff(`8.article-main-button-list.png`, {
@@ -151,14 +138,14 @@ Scenario('Test list of actions render', async ({ I }) => {
 Scenario('Test context menu render', async ({ I }) => {
   I.mockEndpoint(Endpoints.filter, 'one');
   I.mockEndpoint(Endpoints.group, 'empty');
-  I.amOnPage('/products/people');
-  I.refreshPage();
-  I.wait(5);
+  I.openPage();
+
   I.openContextMenu();
   I.see('Send email');
   I.see('Edit');
   I.see('Change password');
   I.see('Change email');
+
   I.saveScreenshot(`9.context-menu.png`);
   if (!isModel) {
     I.seeVisualDiff(`9.context-menu.png`, {
