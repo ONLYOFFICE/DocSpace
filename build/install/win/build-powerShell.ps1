d73dca48a65f46d044a2b0acdec6d0d12c43167d
eugene-kozyrev <eugene.kozyrev@onlyoffice.com>
2021-12-02 08:58:51 +0300

    Update build-powerShell.ps1

--- build/install/win/build-powerShell.ps1
+++ build/install/win/build-powerShell.ps1
@@ -3,21 +3,14 @@
 $AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12,Tls13'
 [System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 
-$names_array = @("nginx","zookeeper","kafka","winsw")
+$hash_table = @{"nginx-1.21.1.zip"="https://nginx.org/download/nginx-1.21.1.zip";
+                "apache-zookeeper-3.7.0-bin.tar.gz"="https://dlcdn.apache.org/zookeeper/zookeeper-3.7.0/apache-zookeeper-3.7.0-bin.tar.gz";
+                "kafka_2.12-2.8.0.tgz"="https://archive.apache.org/dist/kafka/2.8.0/kafka_2.12-2.8.0.tgz";
+                "WinSW.NET4new.exe"="https://github.com/winsw/winsw/releases/download/v2.11.0/WinSW.NET4.exe"} 
 
-$URL_hash_table = @{"nginx"="https://nginx.org/download/nginx-1.21.1.zip";
-                    "zookeeper"="https://dlcdn.apache.org/zookeeper/zookeeper-3.7.0/apache-zookeeper-3.7.0-bin.tar.gz";
-                    "kafka"="https://archive.apache.org/dist/kafka/2.8.0/kafka_2.12-2.8.0.tgz";
-                    "winsw"="https://github.com/winsw/winsw/releases/download/v2.11.0/WinSW.NET4.exe"} 
-
-$Names_hash_table = @{"nginx"="nginx-1.21.1.zip";
-                        "zookeeper"="apache-zookeeper-3.7.0-bin.tar.gz";
-                        "kafka"="kafka_2.12-2.8.0.tgz";
-                        "winsw"="WinSW.NET4new.exe"} 
-
-ForEach ($name in $names_array) {
-    $url = $URL_hash_table.$name
-    $output = "${pwd}\build\install\win\" + $Names_hash_table.$name
+ForEach ($key in $hash_table.keys) {
+    $url = $hash_table[$key]
+    $output = "${pwd}\build\install\win\" + $key
 
     if(![System.IO.File]::Exists($output)){
         [system.console]::WriteLine("Downloading $url")
