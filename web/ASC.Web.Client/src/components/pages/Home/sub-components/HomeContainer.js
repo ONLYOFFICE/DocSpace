0ab444b34eeb270323c60f69b52912bc0f83dac5
gectokot <dmitry.kulak11@gmail.com>
2021-12-08 15:06:32 +0300

    Web: Studio: Homepage: Moved sub components to sub-components folder

---
+++ web/ASC.Web.Client/src/components/pages/Home/sub-components/HomeContainer.js
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  margin: 110px auto;
+  max-width: 1000px;
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-between;
+
+  .home-modules-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+      font-weight: bold;
+      font-size: 23px;
+      line-height: 28px;
+      margin-bottom: 40px;
+    }
+
+    .home-modules {
+      display: grid;
+      grid-template-columns: repeat(3, 120px);
+      max-width: 500px;
+      grid-gap: 40px 25px;
+
+      @media (max-width: 1024px) {
+        grid-template-columns: repeat(3, 1fr);
+      }
+
+      @media (max-width: 500px) {
+        grid-gap: 20px 25px;
+      }
+
+      .home-module {
+        z-index: 42;
+      }
+    }
+
+    .home-error-text {
+      margin-top: 23px;
+      padding: 0 30px;
+      @media (min-width: 768px) {
+        margin-left: 25%;
+        flex: 0 0 50%;
+        max-width: 50%;
+      }
+      @media (min-width: 500px) {
+        flex: 0 0 100%;
+        max-width: 100%;
+      }
+    }
+  }
+
+  @media (max-width: 1024px) {
+    flex-direction: column;
+    margin: 100px auto;
+
+    .home-modules {
+      margin: 0 auto;
+    }
+  }
+`;
+
+export default HomeContainer;
