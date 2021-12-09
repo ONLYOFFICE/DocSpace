9d05a32351e909e1f9782e9e451123bb4597829f
Sergey Kirichenko <sk81.biz@gmail.com>
2021-12-09 15:11:57 +0300

    Modify default NGINX_WORKER_CONNECTIONS

--- build/install/docker/prepare-nginx-proxy.sh
+++ build/install/docker/prepare-nginx-proxy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export NGINX_WORKER_PROCESSES=${NGINX_WORKER_PROCESSES:-$(grep processor /proc/cpuinfo | wc -l)};
-export NGINX_WORKER_CONNECTIONS=${NGINX_WORKER_CONNECTIONS:-$(ulimit -n)};
+export NGINX_WORKER_CONNECTIONS=${NGINX_WORKER_CONNECTIONS:-4096};
 
 envsubst '$MAP_HASH_BUCKET_SIZE,$NGINX_WORKER_PROCESSES,$NGINX_WORKER_CONNECTIONS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
