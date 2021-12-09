b9e7700ce1b2e760201690bd339821095952868e
Sergey Kirichenko <sk81.biz@gmail.com>
2021-12-09 09:51:31 +0300

    Add processess, connections config

--- build/install/docker/prepare-nginx-proxy.sh
+++ build/install/docker/prepare-nginx-proxy.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-envsubst '$MAP_HASH_BUCKET_SIZE,$COUNT_WORKER_CONNECTIONS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+NGINX_WORKER_PROCESSES=${NGINX_WORKER_PROCESSES:-$(grep processor /proc/cpuinfo | wc -l)};
+NGINX_WORKER_CONNECTIONS=${NGINX_WORKER_CONNECTIONS:-$(ulimit -n)};
+
+envsubst '$MAP_HASH_BUCKET_SIZE,$NGINX_WORKER_PROCESSES,$NGINX_WORKER_CONNECTIONS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
