50a5a75f12e375c9f60e9b38a1f6ff190fd819e0
TimofeyBoyko <timofey.boyko@onlyoffice.com>
2021-12-17 16:17:33 +0800

    Web:People:Tests: add translation tests

---
+++ products/ASC.People/Client/tests/translation_tests.js
@@ -0,0 +1,79 @@
+const Endpoints = require('./mocking/endpoints.js');
+const json = require('./mocking/mock-data/settings/cultures.json');
+
+const cultures = json.response.map((item) => item.split('-')[1]);
+
+const isModel = !!process.env.MODEL;
+
+for (const culture of cultures) {
+  const featureName = isModel ? `People translation(model)` : `${culture}`;
+
+  Feature(featureName);
+
+  Before(async ({ I }) => {
+    I.mockData();
+  });
+
+  Scenario(`Main page tests ${culture}`, ({ I }) => {
+    I.mockEndpoint(Endpoints.filter, 'many');
+    I.mockEndpoint(Endpoints.group, 'many');
+
+    if (!isModel && culture !== 'US') {
+      I.mockEndpoint(Endpoints.self, `self${culture}`);
+      I.mockEndpoint(Endpoints.settings, `settings${culture}`);
+    }
+
+    I.openPage();
+
+    I.saveScreenshot(`${culture}-main-page.png`);
+    I.seeVisualDiff(`${culture}-main-page.png`, {
+      tolerance: 0.04,
+      prepareBaseImage: false,
+      // ignoredBox: { top: 0, left: 0, bottom: 0, right: 1720 },
+    });
+  });
+
+  Scenario(`Main button tests ${culture}`, ({ I }) => {
+    I.mockEndpoint(Endpoints.filter, 'many');
+    I.mockEndpoint(Endpoints.group, 'many');
+
+    if (!isModel && culture !== 'US') {
+      I.mockEndpoint(Endpoints.self, `self${culture}`);
+      I.mockEndpoint(Endpoints.settings, `settings${culture}`);
+    }
+
+    I.openPage();
+
+    I.seeElement({ react: 'ArticleMainButton' });
+    I.click({ react: 'ArticleMainButton' });
+
+    I.saveScreenshot(`${culture}-main-button.png`);
+    I.seeVisualDiff(`${culture}-main-button.png`, {
+      tolerance: 0,
+      prepareBaseImage: false,
+      ignoredBox: { top: 0, left: 256, bottom: 0, right: 0 },
+    });
+  });
+
+  Scenario(`Select fields tests ${culture}`, ({ I }) => {
+    I.mockEndpoint(Endpoints.filter, 'many');
+    I.mockEndpoint(Endpoints.group, 'many');
+
+    if (!isModel && culture !== 'US') {
+      I.mockEndpoint(Endpoints.self, `self${culture}`);
+      I.mockEndpoint(Endpoints.settings, `settings${culture}`);
+    }
+
+    I.openPage();
+
+    I.seeElement({ react: 'TableSettings' });
+    I.click({ react: 'TableSettings' });
+
+    I.saveScreenshot(`${culture}-table-settings.png`);
+    I.seeVisualDiff(`${culture}-table-settings.png`, {
+      tolerance: 0.07,
+      prepareBaseImage: false,
+      ignoredBox: { top: 0, left: 0, bottom: 0, right: 1770 },
+    });
+  });
+}
