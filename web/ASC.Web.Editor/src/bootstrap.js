7fc992c9f4d00aaece67d58d7032135b81ad6406
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-01-11 12:59:03 +0300

    Web: DocEditor: applied RectDOM.hydration() instead of ReactDOM.render()

--- web/ASC.Web.Editor/src/bootstrap.js
+++ web/ASC.Web.Editor/src/bootstrap.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { registerSW } from "@appserver/common/utils/sw-helper";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.hydrate(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 registerSW();
