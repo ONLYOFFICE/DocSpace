783bdd27f0d96d17d3d3b8e3521357ccfa215628
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2022-01-14 14:36:14 +0300

    WEb: Files: Change the password dialog due to modification of the password imitation component.

--- products/ASC.Files/Client/src/components/dialogs/ConvertPasswordDialog/index.js
+++ products/ASC.Files/Client/src/components/dialogs/ConvertPasswordDialog/index.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import ModalDialog from "@appserver/components/modal-dialog";
 import Button from "@appserver/components/button";
 import Text from "@appserver/components/text";
 import { withTranslation } from "react-i18next";
+import { inject, observer } from "mobx-react";
 import SimulatePassword from "../../SimulatePassword";
 import StyledComponent from "./StyledConvertPasswordDialog";
+
 const ConvertPasswordDialogComponent = (props) => {
-  const { t } = props;
+  const { t, visible, setConvertPasswordDialogVisible, isTabletView } = props;
 
-  const onConvert = () => {};
-  const onClose = () => {};
+  const [password, setPassword] = useState("");
+  const [passwordValid, setPasswordValid] = useState(true);
 
+  const onClose = () => {
+    setConvertPasswordDialogVisible(false);
+  };
+  const onConvert = () => {
+    let hasError = false;
+
+    const pass = password.trim();
+    if (!pass) {
+      hasError = true;
+      setPasswordValid(false);
+    }
+
+    if (hasError) return;
+  };
+  const onChangePassword = useCallback(
+    (password) => {
+      !passwordValid && setPasswordValid(true);
+      setPassword(password);
+    },
+    [onChangePassword, passwordValid]
+  );
+  console.log("ConvertPasswordDialogComponent", !passwordValid);
   return (
-    <ModalDialog visible={true}>
-      <ModalDialog.Header>{t("ConvertAndOpenTitle")}</ModalDialog.Header>
+    <ModalDialog visible={visible} onClose={onClose}>
+      <ModalDialog.Header>
+        {t("ConvertDialog:ConvertAndOpenTitle")}
+      </ModalDialog.Header>
       <ModalDialog.Body>
         <StyledComponent>
           <div className="convert-password-dialog_content">
@@ -24,16 +50,20 @@ const ConvertPasswordDialogComponent = (props) => {
               <SimulatePassword
                 inputMaxWidth={"512px"}
                 inputBlockMaxWidth={"536px"}
+                onChange={onChangePassword}
+                hasError={!passwordValid}
               />
             </div>
           </div>
         </StyledComponent>
       </ModalDialog.Body>
+
       <ModalDialog.Footer>
-        <StyledComponent>
-          <div className="convert_dialog_footer">
+        <StyledComponent isTabletView={isTabletView}>
+          <div className="convert-password_footer">
             <Button
-              className="convert-password-dialog_button-accept"
+              id="convert-password-dialog_button-accept"
+              className="convert-password-dialog_button"
               key="ContinueButton"
               label={t("Convert")}
               size="medium"
@@ -61,4 +91,19 @@ const ConvertPasswordDialog = withTranslation([
   "Common",
 ])(ConvertPasswordDialogComponent);
 
-export default ConvertPasswordDialog;
+export default inject(({ auth, dialogsStore, uploadDataStore }) => {
+  const {
+    convertPasswordDialogVisible: visible,
+    setConvertPasswordDialogVisible,
+  } = dialogsStore;
+  const { copyAsAction } = uploadDataStore;
+
+  const { settingsStore } = auth;
+  const { isTabletView } = settingsStore;
+
+  return {
+    visible,
+    setConvertPasswordDialogVisible,
+    isTabletView,
+  };
+})(observer(ConvertPasswordDialog));
