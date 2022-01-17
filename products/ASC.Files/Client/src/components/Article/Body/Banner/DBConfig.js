54356f842edb910670e23d433c8699ccacf2eaf6
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-01-17 20:34:34 +0300

    Web: Files: create db config

---
+++ products/ASC.Files/Client/src/components/Article/Body/Banner/DBConfig.js
@@ -0,0 +1,15 @@
+export const DBConfig = {
+  name: "AdsData",
+  version: 1,
+  objectStoresMeta: [
+    {
+      store: "ads",
+      storeConfig: { keyPath: "id", autoIncrement: true },
+      storeSchema: [
+        { name: "campaign", keypath: "campaign", options: { unique: true } },
+        { name: "image", keypath: "image", options: { unique: false } },
+        { name: "translate", keypath: "translate", options: { unique: false } },
+      ],
+    },
+  ],
+};
