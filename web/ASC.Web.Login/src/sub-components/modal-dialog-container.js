cbb58fdf097fb4df51523c7540df7fe20b445e3f
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-01-26 19:12:44 +0300

    Web: Login: fix width modal aside

--- web/ASC.Web.Login/src/sub-components/modal-dialog-container.js
+++ web/ASC.Web.Login/src/sub-components/modal-dialog-container.js
@@ -2,6 +2,29 @@ import styled from "styled-components";
 import ModalDialog from "@appserver/components/modal-dialog";
 
 const ModalDialogContainer = styled(ModalDialog)`
+  .modal-dialog-aside {
+    padding: 0;
+    transform: translateX(${(props) => (props.visible ? "0" : "480px")});
+    width: 480px;
+
+    @media (max-width: 550px) {
+      width: 320px;
+      transform: translateX(${(props) => (props.visible ? "0" : "320px")});
+    }
+  }
+
+  .header_aside-panel {
+    transition: unset;
+    transform: translateX(${(props) => (props.visible ? "0" : "480px")});
+    width: 480px;
+    overflow-y: hidden;
+
+    @media (max-width: 550px) {
+      width: 320px;
+      transform: translateX(${(props) => (props.visible ? "0" : "320px")});
+    }
+  }
+
   .modal-dialog-aside-footer {
     @media (max-width: 1024px) {
       width: 90%;
