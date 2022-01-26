ca0e8a83ade42a05019ab71b8aa6d9d0fb7266cf
SuhorukovAnton <anton.sukhorukov@onlyoffice.com>
2022-01-26 14:42:03 +0300

    refactoring: refactoring globalUsings

--- common/services/ASC.ElasticSearch/GlobalUsings.cs
+++ common/services/ASC.ElasticSearch/GlobalUsings.cs
@@ -1,7 +1,11 @@
 ﻿global using System;
+global using System.Collections;
+global using System.Collections.Concurrent;
 global using System.Collections.Generic;
 global using System.Data;
+global using System.Globalization;
 global using System.Linq;
+global using System.Linq.Expressions;
 global using System.Text;
 global using System.Threading;
 global using System.Threading.Tasks;
@@ -14,29 +18,20 @@ global using ASC.Core;
 global using ASC.Core.Common.EF;
 global using ASC.Core.Common.EF.Context;
 global using ASC.Core.Common.EF.Model;
+global using ASC.Core.Common.Settings;
 global using ASC.Core.Tenants;
+global using ASC.ElasticSearch.Core;
+global using ASC.ElasticSearch.Service;
 
 global using Autofac;
 
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Options;
-
-global using Newtonsoft.Json;
-global using Nest;
-
-global using System.Linq.Expressions;
-
-global using ASC.ElasticSearch.Core;
-
-global using ASC.Core.Common.Settings;
-global using System.Globalization;
-
-global using System.Collections;
-global using System.Collections.Concurrent;
-
-global using ASC.ElasticSearch.Service;
-
 global using Elasticsearch.Net;
 
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Options;
+
+global using Nest;
+
+global using Newtonsoft.Json;
