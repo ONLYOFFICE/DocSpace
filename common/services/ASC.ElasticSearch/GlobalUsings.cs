2ddb37dedf563588b48ba85f149ef769fb54445c
SuhorukovAnton <anton.sukhorukov@onlyoffice.com>
2022-01-26 14:41:26 +0300

    refactoring: add globalUsing: elasticSeach

---
+++ common/services/ASC.ElasticSearch/GlobalUsings.cs
@@ -0,0 +1,42 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Data;
+global using System.Linq;
+global using System.Text;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using ASC.Common;
+global using ASC.Common.Caching;
+global using ASC.Common.Logging;
+global using ASC.Common.Utils;
+global using ASC.Core;
+global using ASC.Core.Common.EF;
+global using ASC.Core.Common.EF.Context;
+global using ASC.Core.Common.EF.Model;
+global using ASC.Core.Tenants;
+
+global using Autofac;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Options;
+
+global using Newtonsoft.Json;
+global using Nest;
+
+global using System.Linq.Expressions;
+
+global using ASC.ElasticSearch.Core;
+
+global using ASC.Core.Common.Settings;
+global using System.Globalization;
+
+global using System.Collections;
+global using System.Collections.Concurrent;
+
+global using ASC.ElasticSearch.Service;
+
+global using Elasticsearch.Net;
+
+global using Microsoft.Extensions.Hosting;
