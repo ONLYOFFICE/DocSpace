96f15bc2a3dc1a4b0445f4b4ecb8d152e61fa140
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-01-27 16:53:41 +0300

    Web: Editor: added _app.js and _document.js (styled-components for ssr)

---
+++ web/ASC.Web.Editor/pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head title="SSR TEST">
+          {this.props.styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
