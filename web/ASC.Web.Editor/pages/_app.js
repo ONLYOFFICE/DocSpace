1e6d2f1e1c8fbfc249878cce13e810726dc0aaf5
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-01-28 13:51:23 +0300

    Web: Editor: added global styles

--- web/ASC.Web.Editor/pages/_app.js
+++ web/ASC.Web.Editor/pages/_app.js
@@ -1,3 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+}
+
+html, body {
+    height: 100%;
+}
+`;
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
