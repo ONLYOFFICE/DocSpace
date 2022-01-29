74bf6bf7a0106230244072180463155daeb2d1b8
SuhorukovAnton <anton.sukhorukov@onlyoffice.com>
2022-01-30 01:17:12 +0300

    refactoring: add globalusings: studio.notify

--- common/services/ASC.Studio.Notify/Program.cs
+++ common/services/ASC.Studio.Notify/Program.cs
@@ -1,28 +1,4 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
-
-using ASC.Api.Core;
-using ASC.Common;
-using ASC.Common.Caching;
-using ASC.Common.DependencyInjection;
-using ASC.Common.Utils;
-using ASC.Core.Notify;
-using ASC.Notify;
-using ASC.Web.Studio.Core.Notify;
-
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
-using StackExchange.Redis.Extensions.Core.Configuration;
-using StackExchange.Redis.Extensions.Newtonsoft;
-
-namespace ASC.Studio.Notify
+﻿namespace ASC.Studio.Notify
 {
     public class Program
     {
