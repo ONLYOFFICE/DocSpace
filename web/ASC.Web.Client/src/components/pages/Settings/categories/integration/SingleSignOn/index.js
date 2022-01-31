8d3c316ab5aa37000d1726d3bd95f80d58e82792
Dmitry Kulak <dmitry.kulak11@gmail.com>
2022-01-31 19:14:06 +0300

    Web: Studio: SSO: Wrapped text inputs into containers, refactoring

--- web/ASC.Web.Client/src/components/pages/Settings/categories/integration/SingleSignOn/index.js
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/integration/SingleSignOn/index.js
@@ -11,7 +11,13 @@ import Button from "@appserver/components/button";
 import RadioButtonGroup from "@appserver/components/radio-button-group";
 import ComboBox from "@appserver/components/combobox";
 import Checkbox from "@appserver/components/checkbox";
-import Label from "@appserver/components/label";
+import { ReactSVG } from "react-svg";
+import styled from "styled-components";
+
+const StyledInputWrapper = styled.div`
+  width: 100%;
+  max-width: ${(props) => props.maxWidth || "350px"};
+`;
 
 const SingleSignOn = () => {
   const { t } = useTranslation("SingleSignOn");
@@ -39,10 +45,10 @@ const SingleSignOn = () => {
   };
 
   return (
-    <Box displayProp="flex" flexDirection="column" widthProp="100%">
+    <Box widthProp="100%">
       <Box
         backgroundProp="#F8F9F9 "
-        borderProp={{ "border-radius": "4px" }}
+        borderProp={{ radius: "4px" }}
         displayProp="flex"
         flexDirection="row"
         paddingProp="12px"
@@ -70,19 +76,14 @@ const SingleSignOn = () => {
         </Box>
       </Box>
 
-      <Box as="form" displayProp="flex" flexDirection="column">
+      <Box as="form">
         <Box>
           <Box displayProp="flex" flexDirection="row" marginProp="24px 0">
             <Text as="h2" fontWeight={600}>
               {t("ServiceProviderSettings")}
             </Text>
 
-            <Link
-              isHovered
-              onClick={showServiceProviderClick}
-              style={{ margin: "0 16px" }}
-              type="action"
-            >
+            <Link isHovered onClick={showServiceProviderClick} type="action">
               {showServiceProvider ? t("Hide") : t("Show")}
             </Link>
           </Box>
@@ -95,13 +96,15 @@ const SingleSignOn = () => {
             place="top"
             tooltipContent="Paste you tooltip content here"
           >
-            <Box displayProp="flex" flexDirection="row">
-              <TextInput
-                className="field-input"
-                onChange={() => {}}
-                placeholder={t("UploadXMLPlaceholder")}
-                size="middle"
-              />
+            <Box alignItems="center" displayProp="flex" flexDirection="row">
+              <StyledInputWrapper maxWidth="300px">
+                <TextInput
+                  className="field-input"
+                  onChange={() => {}}
+                  placeholder={t("UploadXMLPlaceholder")}
+                  scale
+                />
+              </StyledInputWrapper>
 
               <Button label="icon" size="medium" />
               <Text>{t("Or")}</Text>
@@ -114,14 +117,16 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("CustomEntryButton")}
             place="top"
-            style={{ maxWidth: "350px" }}
             tooltipContent={t("CustomEntryTooltip")}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="Single Sign-on"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="Single Sign-on"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -129,14 +134,16 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("ProviderURL")}
             place="top"
-            style={{ maxWidth: "350px" }}
             tooltipContent={t("ProviderURLTooltip")}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="https://www.test.com"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="https://www.test.com"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -144,11 +151,10 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("EndpointURL")}
             place="top"
-            style={{ maxWidth: "350px" }}
             tooltipContent={t("EndpointURLTooltip")}
           >
             <Box displayProp="flex" flexDirection="row">
-              <Label text={t("Binding")} htmlFor="binding" />
+              <Text>{t("Binding")}</Text>
 
               <RadioButtonGroup
                 name="binding"
@@ -166,11 +172,14 @@ const SingleSignOn = () => {
               />
             </Box>
 
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="https://www.test.com/saml/login"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="https://www.test.com/saml/login"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -178,61 +187,69 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("NameIDFormat")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <ComboBox
-              selectedOption={{
-                key: 1,
-                label: "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
-              }}
-              options={[
-                {
+            <StyledInputWrapper>
+              <ComboBox
+                scaled
+                scaledOptions
+                textOverflow={false}
+                selectedOption={{
                   key: 1,
                   label:
                     "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
-                },
-                {
-                  key: 2,
-                  label:
-                    "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress",
-                },
-                {
-                  key: 3,
-                  label: "urn:oasis:names:tc:SAML:2.0:nameid-format:entity",
-                },
-                {
-                  key: 4,
-                  label: "urn:oasis:names:tc:SAML:2.0:nameid-format:transient",
-                },
-                {
-                  key: 5,
-                  label: "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent",
-                },
-                {
-                  key: 6,
-                  label: "urn:oasis:names:tc:SAML:2.0:nameid-format:encrypted",
-                },
-                {
-                  key: 7,
-                  label:
-                    "urn:oasis:names:tc:SAML:2.0:nameid-format:unspecified",
-                },
-                {
-                  key: 8,
-                  label:
-                    "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName",
-                },
-                {
-                  key: 9,
-                  label:
-                    "urn:oasis:names:tc:SAML:1.1:nameid-format:WindowsDomainQualifiedName",
-                },
-                {
-                  key: 10,
-                  label: "urn:oasis:names:tc:SAML:2.0:nameid-format:kerberos",
-                },
-              ]}
-            />
+                }}
+                options={[
+                  {
+                    key: 1,
+                    label:
+                      "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified",
+                  },
+                  {
+                    key: 2,
+                    label:
+                      "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress",
+                  },
+                  {
+                    key: 3,
+                    label: "urn:oasis:names:tc:SAML:2.0:nameid-format:entity",
+                  },
+                  {
+                    key: 4,
+                    label:
+                      "urn:oasis:names:tc:SAML:2.0:nameid-format:transient",
+                  },
+                  {
+                    key: 5,
+                    label:
+                      "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent",
+                  },
+                  {
+                    key: 6,
+                    label:
+                      "urn:oasis:names:tc:SAML:2.0:nameid-format:encrypted",
+                  },
+                  {
+                    key: 7,
+                    label:
+                      "urn:oasis:names:tc:SAML:2.0:nameid-format:unspecified",
+                  },
+                  {
+                    key: 8,
+                    label:
+                      "urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName",
+                  },
+                  {
+                    key: 9,
+                    label:
+                      "urn:oasis:names:tc:SAML:1.1:nameid-format:WindowsDomainQualifiedName",
+                  },
+                  {
+                    key: 10,
+                    label: "urn:oasis:names:tc:SAML:2.0:nameid-format:kerberos",
+                  },
+                ]}
+              />
+            </StyledInputWrapper>
           </FieldContainer>
         </Box>
 
@@ -249,7 +266,6 @@ const SingleSignOn = () => {
 
             <HelpButton
               offsetRight={0}
-              style={{ padding: "0 6px" }}
               tooltipContent={<Text>{t("TurnOnTooltip")}</Text>}
             />
           </Box>
@@ -260,7 +276,6 @@ const SingleSignOn = () => {
             <Link
               isHovered
               onClick={showAdditionalParametersClick}
-              style={{ margin: "0 16px" }}
               type="action"
             >
               {showAdditionalParameters
@@ -279,28 +294,29 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("SigningAlgorithm")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <ComboBox
-              selectedOption={{
-                key: 1,
-                label: "rsa-sha1",
-              }}
-              options={[
-                {
+            <StyledInputWrapper>
+              <ComboBox
+                selectedOption={{
                   key: 1,
                   label: "rsa-sha1",
-                },
-                {
-                  key: 2,
-                  label: "rsa-sha256",
-                },
-                {
-                  key: 3,
-                  label: "rsa-sha512",
-                },
-              ]}
-            />
+                }}
+                options={[
+                  {
+                    key: 1,
+                    label: "rsa-sha1",
+                  },
+                  {
+                    key: 2,
+                    label: "rsa-sha256",
+                  },
+                  {
+                    key: 3,
+                    label: "rsa-sha512",
+                  },
+                ]}
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -308,28 +324,29 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("StandardDecryptionAlgorithm")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <ComboBox
-              selectedOption={{
-                key: 1,
-                label: "aes128-cbc",
-              }}
-              options={[
-                {
+            <StyledInputWrapper>
+              <ComboBox
+                selectedOption={{
                   key: 1,
                   label: "aes128-cbc",
-                },
-                {
-                  key: 2,
-                  label: "aes256-cbc",
-                },
-                {
-                  key: 1,
-                  label: "tripledes-cbc",
-                },
-              ]}
-            />
+                }}
+                options={[
+                  {
+                    key: 1,
+                    label: "aes128-cbc",
+                  },
+                  {
+                    key: 2,
+                    label: "aes256-cbc",
+                  },
+                  {
+                    key: 1,
+                    label: "tripledes-cbc",
+                  },
+                ]}
+              />
+            </StyledInputWrapper>
           </FieldContainer>
         </Box>
 
@@ -342,25 +359,25 @@ const SingleSignOn = () => {
 
               <HelpButton
                 offsetRight={0}
-                style={{ padding: "0 6px" }}
                 tooltipContent={<Text>{t("AttributeMatchingTooltip")}</Text>}
               />
             </Box>
           </Box>
 
-        <Box>
           <FieldContainer
             helpButtonHeaderContent="Tooltip header"
             isVertical
             labelText={t("FirstName")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="givenName"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="givenName"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -368,13 +385,15 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("LastName")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="sn"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="sn"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -382,13 +401,15 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("Email")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="sn"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="sn"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -396,13 +417,15 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("Location")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="sn"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="sn"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -410,13 +433,15 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("Title")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="sn"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="sn"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -424,13 +449,15 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("Phone")}
             place="top"
-            style={{ maxWidth: "350px" }}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="sn"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="sn"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -438,7 +465,6 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("AdvancedSettings")}
             place="top"
-            style={{ maxWidth: "350px" }}
             tooltipContent={t("AdvancedSettingsTooltip")}
           >
             <Checkbox label={t("HideAuthPage")} />
@@ -457,31 +483,27 @@ const SingleSignOn = () => {
                 {t("SPMetadata")}
               </Text>
 
-              <Link
-                isHovered
-                onClick={showProviderMetadataClick}
-                style={{ margin: "0 16px" }}
-                type="action"
-              >
+              <Link isHovered onClick={showProviderMetadataClick} type="action">
                 {showProviderMetadata ? t("Hide") : t("Show")}
               </Link>
             </Box>
           </Box>
 
-        <Box displayProp="flex" flexDirection="column">
           <FieldContainer
             helpButtonHeaderContent="Tooltip header"
             isVertical
             labelText={t("SPEntityId")}
             place="top"
-            style={{ maxWidth: "350px" }}
             tooltipContent={t("SPEntityIdTooltip")}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="Line input"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="Line input"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -489,14 +511,16 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("SPAssertionConsumerURL")}
             place="top"
-            style={{ maxWidth: "350px" }}
             tooltipContent={t("SPAssertionConsumerURLTooltip")}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="Line input"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="Line input"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <FieldContainer
@@ -504,14 +528,16 @@ const SingleSignOn = () => {
             isVertical
             labelText={t("SPSingleLogoutURL")}
             place="top"
-            style={{ maxWidth: "350px" }}
             tooltipContent={t("SPSingleLogoutURLTooltip")}
           >
-            <TextInput
-              className="field-input"
-              onChange={() => {}}
-              placeholder="Line input"
-            />
+            <StyledInputWrapper>
+              <TextInput
+                className="field-input"
+                onChange={() => {}}
+                placeholder="Line input"
+                scale
+              />
+            </StyledInputWrapper>
           </FieldContainer>
 
           <Button label="DownloadMetadataXML" primary size="medium" />
