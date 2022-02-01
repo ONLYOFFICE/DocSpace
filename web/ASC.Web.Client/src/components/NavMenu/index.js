a40e8bf7556701ba54691c349a5555dff0bbbc20
Dmitry Sychugov <dmitry.sychugov@onlyoffice.com>
2022-02-01 12:49:38 +0500

    Web: Client: deleted checkedMaintenance

--- web/ASC.Web.Client/src/components/NavMenu/index.js
+++ web/ASC.Web.Client/src/components/NavMenu/index.js
@@ -37,7 +37,7 @@ const StyledContainer = styled.header`
           .navMenuHeader,
           .profileMenuIcon,
           .navMenuHeaderUnAuth {
-            position: ${(props) => (props.maintenanceExist ? "none" : "fixed")};
+            position: fixed;
             z-index: 160;
             top: ${(props) => (props.isVisible ? "0" : "-48px")};
 
@@ -149,7 +149,6 @@ class NavMenu extends React.Component {
       asideContent,
       history,
       isDesktop,
-      maintenanceExist,
     } = this.props;
 
     const isAsideAvailable = !!asideContent;
@@ -165,7 +164,6 @@ class NavMenu extends React.Component {
             isLoaded={isLoaded}
             isVisible={value.isVisible}
             isDesktop={hideHeader}
-            maintenanceExist={maintenanceExist}
           >
             <Toast />
 
@@ -234,10 +232,9 @@ NavMenu.defaultProps = {
 
 const NavMenuWrapper = inject(({ auth }) => {
   const { settingsStore, isAuthenticated, isLoaded, language } = auth;
-  const { isDesktopClient: isDesktop, maintenanceExist } = settingsStore;
+  const { isDesktopClient: isDesktop } = settingsStore;
   return {
     isAuthenticated,
-    maintenanceExist,
     isLoaded,
     isDesktop,
     language,
