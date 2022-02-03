25504f1d8b53a6e783d2f98043dd3fbbd2489559
SuhorukovAnton <anton.sukhorukov@onlyoffice.com>
2022-02-03 16:53:42 +0300

    refactoring: web.studio

--- web/ASC.Web.Studio/Startup.cs
+++ web/ASC.Web.Studio/Startup.cs
@@ -15,59 +15,57 @@ using Microsoft.Extensions.Hosting;
 using StackExchange.Redis.Extensions.Core.Configuration;
 using StackExchange.Redis.Extensions.Newtonsoft;
 
-namespace ASC.Web.Studio
+namespace ASC.Web.Studio;
+public class Startup : BaseStartup
 {
-    public class Startup : BaseStartup
+    public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment) : base(configuration, hostEnvironment)
     {
-        public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment) : base(configuration, hostEnvironment)
+    }
+
+    public override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors();
+
+        base.ConfigureServices(services);
+
+        services.AddMemoryCache();
+        DIHelper.TryAdd<Login>();
+        DIHelper.TryAdd<PathUtils>();
+        DIHelper.TryAdd<StorageHandlerScope>();
+        DIHelper.TryAdd<GoogleLoginProvider>();
+        DIHelper.TryAdd<FacebookLoginProvider>();
+        DIHelper.TryAdd<LinkedInLoginProvider>();
+    }
+
+    public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        base.Configure(app, env);
+
+        app.UseForwardedHeaders(new ForwardedHeadersOptions
         {
-        }
+            ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+        });
 
-        public override void ConfigureServices(IServiceCollection services)
+        app.UseRouting();
+
+        app.UseCors(builder =>
+            builder
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+        app.UseAuthentication();
+
+        app.UseEndpoints(endpoints =>
         {
-            services.AddCors();
+            endpoints.InitializeHttpHandlers();
+        });
 
-            base.ConfigureServices(services);
-
-            services.AddMemoryCache();
-            DIHelper.TryAdd<Login>();
-            DIHelper.TryAdd<PathUtils>();
-            DIHelper.TryAdd<StorageHandlerScope>();
-            DIHelper.TryAdd<GoogleLoginProvider>();
-            DIHelper.TryAdd<FacebookLoginProvider>();
-            DIHelper.TryAdd<LinkedInLoginProvider>();
-        }
-
-        public override void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            base.Configure(app, env);
-
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
+        app.MapWhen(
+            context => context.Request.Path.ToString().EndsWith("login.ashx"),
+            appBranch =>
             {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                appBranch.UseLoginHandler();
             });
-
-            app.UseRouting();
-
-            app.UseCors(builder =>
-                builder
-                    .AllowAnyOrigin()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod());
-
-            app.UseAuthentication();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.InitializeHttpHandlers();
-            });
-
-            app.MapWhen(
-               context => context.Request.Path.ToString().EndsWith("login.ashx"),
-               appBranch =>
-               {
-                   appBranch.UseLoginHandler();
-               });
-        }
     }
 }
