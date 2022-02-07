1908c170d42413a1a2bb9c49f20fc27ec9ab5a0d
mushka <nikitamushka@mail.ru>
2022-02-07 14:47:21 +0300

    fixed media

--- packages/asc-web-components/submenu/index.js
+++ packages/asc-web-components/submenu/index.js
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import React, { useState } from "react";
 
 import Text from "../text";
-import { isTablet } from "../utils/device";
 import {
   StyledSubmenu,
   StyledSubmenuBottomLine,
