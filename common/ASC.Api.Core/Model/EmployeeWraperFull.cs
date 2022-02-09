d27a33f08f7d7c7bafdd58e54fe631dc65ffe17f
pavelbannov <pavel.bannov@onlyoffice.com>
2022-02-09 14:05:21 +0300

    People: added simple wrapper for getbyid method fixed invite link

--- common/ASC.Api.Core/Model/EmployeeWraperFull.cs
+++ common/ASC.Api.Core/Model/EmployeeWraperFull.cs
@@ -173,6 +173,22 @@ namespace ASC.Web.Api.Models
             return lambda;
         }
 
+        public EmployeeWraperFull GetSimple(UserInfo userInfo)
+        {
+            var result = new EmployeeWraperFull
+            {
+                FirstName = userInfo.FirstName,
+                LastName = userInfo.LastName,
+            };
+
+            FillGroups(result, userInfo);
+
+            var userInfoLM = userInfo.LastModified.GetHashCode();
+            result.AvatarMax = UserPhotoManager.GetMaxPhotoURL(userInfo.ID, out var isdef) + (isdef ? "" : $"?_={userInfoLM}");
+
+            return result;
+        }
+
         public EmployeeWraperFull GetFull(UserInfo userInfo)
         {
             var result = new EmployeeWraperFull
@@ -223,23 +239,7 @@ namespace ASC.Web.Api.Models
             }
 
             FillConacts(result, userInfo);
-
-            if (Context.Check("groups") || Context.Check("department"))
-            {
-                var groups = UserManager.GetUserGroups(userInfo.ID)
-                    .Select(x => new GroupWrapperSummary(x, UserManager))
-                    .ToList();
-
-                if (groups.Count > 0)
-                {
-                    result.Groups = groups;
-                    result.Department = string.Join(", ", result.Groups.Select(d => d.Name.HtmlEncode()));
-                }
-                else
-                {
-                    result.Department = "";
-                }
-            }
+            FillGroups(result, userInfo);
 
             var userInfoLM = userInfo.LastModified.GetHashCode();
 
@@ -267,6 +267,28 @@ namespace ASC.Web.Api.Models
             }
 
             return result;
+        }
+
+        private void FillGroups(EmployeeWraperFull result, UserInfo userInfo)
+        {
+            if (!Context.Check("groups") && !Context.Check("department"))
+            {
+                return;
+            }
+
+            var groups = UserManager.GetUserGroups(userInfo.ID)
+                .Select(x => new GroupWrapperSummary(x, UserManager))
+                .ToList();
+
+            if (groups.Count > 0)
+            {
+                result.Groups = groups;
+                result.Department = string.Join(", ", result.Groups.Select(d => d.Name.HtmlEncode()));
+            }
+            else
+            {
+                result.Department = "";
+            }
         }
 
         private void FillConacts(EmployeeWraperFull employeeWraperFull, UserInfo userInfo)
