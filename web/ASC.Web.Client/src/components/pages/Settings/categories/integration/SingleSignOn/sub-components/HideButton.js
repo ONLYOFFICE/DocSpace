eee26cf4d0f19250c9777eafb03cfbcd9173f894
Dmitry Kulak <dmitry.kulak11@gmail.com>
2022-02-10 14:40:36 +0300

    Web: Studio: SSO: Made HideButton subcomponent

---
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/integration/SingleSignOn/sub-components/HideButton.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Box from "@appserver/components/box";
+import Link from "@appserver/components/link";
+import Text from "@appserver/components/text";
+import { addParameter } from "../../../../utils";
+import { observer } from "mobx-react";
+
+const HideButton = ({ FormStore, label, t, isAdditionalParameters }) => {
+  const hide = isAdditionalParameters ? "HideAdditionalParameters" : "Hide";
+  const show = isAdditionalParameters ? "ShowAdditionalParameters" : "Show";
+  const marginProp = isAdditionalParameters ? null : "24px 0";
+  const className = isAdditionalParameters
+    ? "hide-additional-button"
+    : "hide-button";
+
+  const onClick = addParameter(FormStore.onHideClick, label);
+
+  return (
+    <Box
+      alignItems="center"
+      displayProp="flex"
+      flexDirection="row"
+      marginProp={marginProp}
+    >
+      {!isAdditionalParameters && (
+        <Text as="h2" fontSize="16px" fontWeight={600}>
+          {t(label)}
+        </Text>
+      )}
+
+      <Link className={className} isHovered onClick={onClick} type="action">
+        {FormStore[label] ? t(hide) : t(show)}
+      </Link>
+    </Box>
+  );
+};
+
+export default observer(HideButton);
