c979d72430f05f97c618ca36deb3923f5ce82ec9
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2022-02-11 17:59:18 +0300

    FeedAggregator: refactor

--- common/services/ASC.Feed.Aggregator/GlobalUsings.cs
+++ common/services/ASC.Feed.Aggregator/GlobalUsings.cs
@@ -6,19 +6,19 @@ global using System.Threading;
 global using System.Threading.Tasks;
 
 global using ASC.Common;
-global using ASC.Common.Utils;
-global using ASC.Core;
-global using ASC.Feed.Data;
-global using ASC.Web.Core;
 global using ASC.Common.Caching;
 global using ASC.Common.Logging;
+global using ASC.Common.Utils;
+global using ASC.Core;
 global using ASC.Core.Common;
 global using ASC.Core.Notify.Signalr;
 global using ASC.Feed.Aggregator.Modules;
 global using ASC.Feed.Configuration;
+global using ASC.Feed.Data;
+global using ASC.Web.Core;
 
 global using Autofac;
 
-global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Options;
