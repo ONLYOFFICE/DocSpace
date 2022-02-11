608c271d83c53133f86812629a9623f3fe4592f4
Dmitry Kulak <dmitry.kulak11@gmail.com>
2022-02-11 11:00:00 +0300

    Web: Studio: SSO: Removed unused code

--- web/ASC.Web.Client/src/components/pages/Settings/categories/integration/SingleSignOn/index.js
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/integration/SingleSignOn/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import { observer } from "mobx-react";
 import { useTranslation } from "react-i18next";
 
@@ -12,146 +11,12 @@ import ProviderMetadata from "./ProviderMetadata";
 import StyledSsoPage from "./styled-containers/StyledSsoPageContainer";
 import ToggleSSO from "./sub-components/ToggleSSO";
 
-  .field-label-icon {
-    align-items: center;
-    margin-bottom: 4px;
-    max-width: 350px;
-  }
-
-  .field-label {
-    height: auto;
-    font-weight: 600;
-    line-height: 20px;
-    overflow: visible;
-    white-space: normal;
-  }
-
-  .xml-input {
-    .field-label-icon {
-      margin-bottom: 8px;
-      max-width: 350px;
-    }
-
-    .field-label {
-      font-weight: 400;
-    }
-  }
-
-  .or-text {
-    margin: 0 24px;
-  }
-
-  .radio-button-group {
-    margin-left: 25px;
-  }
-
-  .combo-button-label {
-    max-width: 100%;
-  }
-
-  .checkbox-input {
-    margin: 6px 8px 6px 0;
-  }
-
-  .upload-button {
-    margin-left: 9px;
-    overflow: inherit;
-
-    & > div {
-      margin-top: 2px;
-    }
-  }
-
-  .save-button {
-    margin-right: 8px;
-  }
-
-  .download-button {
-    max-width: 404px;
-    width: 100%;
-  }
-`;
-
-const StyledInputWrapper = styled.div`
-  width: 100%;
-  max-width: ${(props) => props.maxWidth || "350px"};
-`;
-
-const InputLabel = ({ text, tooltipContent }) => (
-  <Box
-    alignItems="center"
-    className="field-label-icon"
-    displayProp="flex"
-    flexDirection="row"
-  >
-    <span className="field-label">
-      {text}
-      {tooltipContent && (
-        <HelpButton
-          className="tooltip-button"
-          offsetRight={0}
-          tooltipContent={<Text>{tooltipContent}</Text>}
-          style={{ display: "inline-flex" }}
-        />
-      )}
-    </span>
-  </Box>
-);
-
 const SingleSignOn = () => {
   const { t } = useTranslation(["SingleSignOn", "Common"]);
 
   return (
     <StyledSsoPage>
       <ToggleSSO FormStore={FormStore} t={t} />
-
-          <FieldContainer isVertical labelText={t("Location")}>
-            <StyledInputWrapper>
-              <TextInput
-                className="field-input"
-                onChange={() => {}}
-                placeholder="sn"
-                scale
-                tabIndex={19}
-              />
-            </StyledInputWrapper>
-          </FieldContainer>
-
-          <FieldContainer isVertical labelText={t("Title")}>
-            <StyledInputWrapper>
-              <TextInput
-                className="field-input"
-                onChange={() => {}}
-                placeholder="sn"
-                scale
-                tabIndex={20}
-              />
-            </StyledInputWrapper>
-          </FieldContainer>
-
-          <FieldContainer isVertical labelText={t("Phone")}>
-            <StyledInputWrapper>
-              <TextInput
-                className="field-input"
-                onChange={() => {}}
-                placeholder="sn"
-                scale
-                tabIndex={21}
-              />
-            </StyledInputWrapper>
-          </FieldContainer>
-
-          <FieldContainer
-            isVertical
-            labelText={
-              <InputLabel
-                text={t("AdvancedSettings")}
-                tooltipContent={t("AdvancedSettingsTooltip")}
-              />
-            }
-          >
-            <Checkbox label={t("HideAuthPage")} tabIndex={22} />
-          </FieldContainer>
 
       <IdpSettings FormStore={FormStore} t={t} />
 
