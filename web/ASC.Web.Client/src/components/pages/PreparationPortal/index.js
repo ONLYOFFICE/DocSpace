4a6ad133b08411e1d098b06dd645689c536c1e48
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2022-02-14 17:46:27 +0300

    Web: files: Changed styles for preparation portal.

--- web/ASC.Web.Client/src/components/pages/PreparationPortal/index.js
+++ web/ASC.Web.Client/src/components/pages/PreparationPortal/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ErrorContainer from "@appserver/common/components/ErrorContainer";
 import { I18nextProvider, withTranslation } from "react-i18next";
 import i18n from "./i18n";
-import StyledPreparationPortal from "./styledPreparationPortal";
+import {
+  StyledPreparationPortal,
+  StyledBodyPreparationPortal,
+} from "./styledPreparationPortal";
 import Text from "@appserver/components/text";
 import { getRestoreProgress } from "../../../../../../packages/asc-web-common/api/portal";
 
@@ -16,31 +19,28 @@ class PreparationPortal extends React.Component {
     this.timerId = null;
   }
   componentDidMount() {
-    this.setState({ isLoadingData: true }, function () {
-      getRestoreProgress()
-        .then((response) => {
-          if (response) {
-            if (!response.error) {
-              if (response.progress === 100)
-                this.setState({
-                  percent: 100,
-                });
-              if (response.progress !== 100)
-                this.timerId = setInterval(() => this.getProgress(), 1000);
-            } else {
-              this.setState({
-                errorMessage: response.error,
-              });
-            }
+    getRestoreProgress()
+      .then((response) => {
+        if (response) {
+          const { error, progress } = response;
+          if (!error) {
+            this.setState({
+              percent: progress,
+            });
+            if (progress !== 100)
+              this.timerId = setInterval(() => this.getProgress(), 1000);
+          } else {
+            this.setState({
+              errorMessage: error,
+            });
           }
+        }
+      })
+      .catch((err) =>
+        this.setState({
+          errorMessage: err,
         })
-        .catch((err) =>
-          this.setState({
-            errorMessage: err,
-          })
-        )
-        .finally(() => this.setState({ isLoadingData: false }));
-    });
+      );
   }
   componentWillUnmount() {
     clearInterval(this.timerId);
@@ -48,36 +48,37 @@ class PreparationPortal extends React.Component {
   getProgress = () => {
     getRestoreProgress().then((response) => {
       if (response) {
-        if (!response.error) {
-          const percent = response.progress;
-
+        const { error, progress } = response;
+        if (!error) {
           this.setState({
-            percent: percent,
+            percent: progress,
           });
 
-          if (percent === 100) {
+          if (progress === 100) {
             clearInterval(this.timerId);
           }
         } else {
           clearInterval(this.timerId);
           this.setState({
-            errorMessage: response.error,
+            errorMessage: error,
           });
         }
+      } else {
+        clearInterval(this.timerId);
       }
     });
   };
   render() {
     const { t } = this.props;
-    const { percent, errorMessage, isLoadingData } = this.state;
-    console.log("percent", errorMessage);
+    const { percent, errorMessage } = this.state;
+
     return (
-      <ErrorContainer
-        headerText={t("PreparationPortalTitle")}
-        bodyText={t("PreparationPortalDescription")}
-      >
-        {!isLoadingData ? (
-          <StyledPreparationPortal
+      <StyledPreparationPortal>
+        <ErrorContainer
+          headerText={t("PreparationPortalTitle")}
+          bodyText={t("PreparationPortalDescription")}
+        >
+          <StyledBodyPreparationPortal
             percent={percent}
             errorMessage={errorMessage}
           >
@@ -91,17 +92,12 @@ class PreparationPortal extends React.Component {
                 <div className="preparation-portal_progress-bar">
                   <div className="preparation-portal_progress-line"></div>
                 </div>
-                <Text
-                  className="preparation-portal_percent"
-                  color="#a3a9ae"
-                >{`${percent}%`}</Text>
+                <Text className="preparation-portal_percent">{`${percent}%`}</Text>
               </>
             )}
-          </StyledPreparationPortal>
-        ) : (
-          <></>
-        )}
-      </ErrorContainer>
+          </StyledBodyPreparationPortal>
+        </ErrorContainer>
+      </StyledPreparationPortal>
     );
   }
 }
--- web/ASC.Web.Client/src/components/pages/PreparationPortal/styledPreparationPortal.js
+++ web/ASC.Web.Client/src/components/pages/PreparationPortal/styledPreparationPortal.js
@@ -1,59 +1,50 @@
 import styled from "styled-components";
 
-const StyledPreparationPortal = styled.div`
+const StyledBodyPreparationPortal = styled.div`
   margin-bottom: 24px;
   display: flex;
   width: 100%;
-  max-width: ${(props) => (props.errorMessage ? "560px" : "360px")};
+  max-width: ${(props) => (props.errorMessage ? "560px" : "480px")};
   padding: 0 24px;
   box-sizing: border-box;
   align-items: center;
+  position: relative;
 
   .preparation-portal_progress-bar {
     border-radius: 2px;
     margin-right: 8px;
     width: 100%;
 
-    height: 14px;
-    background-color: #a3a9ae;
+    height: 24px;
+    background-color: #f3f4f4;
   }
   .preparation-portal_progress-line {
     border-radius: inherit;
     width: ${(props) => props.percent}%;
     background: #439ccd;
     height: inherit;
-    //width: 0;
     transition-property: width;
     transition-duration: 0.9s;
-    background: linear-gradient(
-      top,
-      #57aedf,
-      #439ccd 50%,
-      #3b95c6 51%,
-      #237faf
-    );
-    background: -o-linear-gradient(
-      top,
-      #57aedf,
-      #439ccd 50%,
-      #3b95c6 51%,
-      #237faf
-    );
-    background: -moz-linear-gradient(
-      top,
-      #57aedf,
-      #439ccd 50%,
-      #3b95c6 51%,
-      #237faf
-    );
-    background: -webkit-linear-gradient(
-      top,
-      #57aedf,
-      #439ccd 50%,
-      #3b95c6 51%,
-      #237faf
-    );
-    -ms-filter: "progid:DXImageTransform.Microsoft.gradient(startColorStr='#57aedf', EndColorStr='#237faf')";
+    background: #1f97ca;
+  }
+  .preparation-portal_percent {
+    position: absolute;
+    right: 50%;
+    ${(props) => props.percent > 50 && "color: white"}
   }
 `;
-export default StyledPreparationPortal;
+
+const StyledPreparationPortal = styled.div`
+  width: 100%;
+
+  #header {
+    font-size: 23px;
+  }
+  #text {
+    color: #a3a9ae;
+    font-size: 13px;
+    line-height: 20px;
+    max-width: 480px;
+  }
+`;
+export { StyledBodyPreparationPortal, StyledPreparationPortal };
