dddfffbc9df351fabcfef782713f2be92e3e24c1
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2022-02-16 17:16:20 +0300

    Notify: used WebApplication.CreateBulder

--- common/services/ASC.Notify/Program.cs
+++ common/services/ASC.Notify/Program.cs
@@ -1,87 +1,88 @@
-﻿namespace ASC.Notify;
-
-public static class Program
+﻿var options = new WebApplicationOptions
 {
-    public async static Task Main(string[] args)
-    {
-        var host = CreateHostBuilder(args).Build();
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+};
 
-        await host.RunAsync();
+var builder = WebApplication.CreateBuilder(options);
+
+builder.Host.UseSystemd();
+builder.Host.UseWindowsService();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<BaseWorkerStartup>());
+builder.Host.ConfigureAppConfiguration((hostContext, config) =>
+{
+    var buildedConfig = config.Build();
+    var path = buildedConfig["pathToConf"];
+    if (!Path.IsPathRooted(path))
+    {
+        path = Path.GetFullPath(CrossPlatform.PathCombine(hostContext.HostingEnvironment.ContentRootPath, path));
     }
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseSystemd()
-            .UseWindowsService()
-            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<BaseWorkerStartup>())
-            .ConfigureAppConfiguration((hostContext, config) =>
-            {
-                var buided = config.Build();
-                var path = buided["pathToConf"];
-                if (!Path.IsPathRooted(path))
-                {
-                    path = Path.GetFullPath(CrossPlatform.PathCombine(hostContext.HostingEnvironment.ContentRootPath, path));
-                }
-                config.SetBasePath(path);
-                var env = hostContext.Configuration.GetValue("ENVIRONMENT", "Production");
-                config
-                    .AddJsonFile("appsettings.json")
-                    .AddJsonFile($"appsettings.{env}.json", true)
-                    .AddJsonFile($"appsettings.services.json", true)
-                    .AddJsonFile("storage.json")
-                    .AddJsonFile("notify.json")
-                    .AddJsonFile($"notify.{env}.json", true)
-                    .AddJsonFile("kafka.json")
-                    .AddJsonFile($"kafka.{env}.json", true)
-                    .AddJsonFile("redis.json")
-                    .AddJsonFile($"redis.{env}.json", true)
-                    .AddEnvironmentVariables()
-                    .AddCommandLine(args)
-                    .AddInMemoryCollection(new Dictionary<string, string>
-                        {
-                                {"pathToConf", path }
-                        }
-                    );
-            })
-            .ConfigureServices((hostContext, services) =>
-            {
-                services.AddMemoryCache();
-                var diHelper = new DIHelper(services);
+    config.SetBasePath(path);
 
-                var redisConfiguration = hostContext.Configuration.GetSection("Redis").Get<RedisConfiguration>();
-                var kafkaConfiguration = hostContext.Configuration.GetSection("kafka").Get<KafkaSettings>();
+    var env = hostContext.Configuration.GetValue("ENVIRONMENT", "Production");
 
-                if (kafkaConfiguration != null)
-                {
-                    diHelper.TryAdd(typeof(ICacheNotify<>), typeof(KafkaCacheNotify<>));
-                }
-                else if (redisConfiguration != null)
-                {
-                    diHelper.TryAdd(typeof(ICacheNotify<>), typeof(RedisCacheNotify<>));
+    config.AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{env}.json", true)
+    .AddJsonFile($"appsettings.services.json", true)
+    .AddJsonFile("storage.json")
+    .AddJsonFile("notify.json")
+    .AddJsonFile($"notify.{env}.json", true)
+    .AddJsonFile("kafka.json")
+    .AddJsonFile($"kafka.{env}.json", true)
+    .AddJsonFile("redis.json")
+    .AddJsonFile($"redis.{env}.json", true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .AddInMemoryCollection(new Dictionary<string, string>
+    {
+        {"pathToConf", path }
+    });
+});
 
-                    services.AddStackExchangeRedisExtensions<NewtonsoftSerializer>(redisConfiguration);
-                }
-                else
-                {
-                    diHelper.TryAdd(typeof(ICacheNotify<>), typeof(MemoryCacheNotify<>));
-                }
+builder.Host.ConfigureServices((hostContext, services) =>
+{
+    services.AddMemoryCache();
+    var diHelper = new DIHelper(services);
 
-                diHelper.RegisterProducts(hostContext.Configuration, hostContext.HostingEnvironment.ContentRootPath);
+    var redisConfiguration = hostContext.Configuration.GetSection("Redis").Get<RedisConfiguration>();
+    var kafkaConfiguration = hostContext.Configuration.GetSection("kafka").Get<KafkaSettings>();
 
-                services.Configure<NotifyServiceCfg>(hostContext.Configuration.GetSection("notify"));
+    if (kafkaConfiguration != null)
+    {
+        diHelper.TryAdd(typeof(ICacheNotify<>), typeof(KafkaCacheNotify<>));
+    }
+    else if (redisConfiguration != null)
+    {
+        diHelper.TryAdd(typeof(ICacheNotify<>), typeof(RedisCacheNotify<>));
 
-                diHelper.TryAdd<NotifyServiceLauncher>();
+        services.AddStackExchangeRedisExtensions<NewtonsoftSerializer>(redisConfiguration);
+    }
+    else
+    {
+        diHelper.TryAdd(typeof(ICacheNotify<>), typeof(MemoryCacheNotify<>));
+    }
 
-                diHelper.TryAdd<JabberSender>();
-                diHelper.TryAdd<SmtpSender>();
-                diHelper.TryAdd<AWSSender>(); // fix private
+    diHelper.RegisterProducts(hostContext.Configuration, hostContext.HostingEnvironment.ContentRootPath);
 
-                    services.AddHostedService<NotifyServiceLauncher>();
-            })
-            .ConfigureContainer<ContainerBuilder>((context, builder) =>
-            {
-                builder.Register(context.Configuration);
-            })
-        .ConfigureNLogLogging();
-}
+    services.Configure<NotifyServiceCfg>(hostContext.Configuration.GetSection("notify"));
+
+    diHelper.TryAdd<NotifyServiceLauncher>();
+    diHelper.TryAdd<JabberSender>();
+    diHelper.TryAdd<SmtpSender>();
+    diHelper.TryAdd<AWSSender>(); // fix private
+
+    services.AddHostedService<NotifyServiceLauncher>();
+});
+
+builder.Host.ConfigureContainer<ContainerBuilder>((context, builder) =>
+{
+    builder.Register(context.Configuration);
+});
+
+builder.Host.ConfigureNLogLogging();
+
+var app = builder.Build();
+
+await app.RunAsync();
