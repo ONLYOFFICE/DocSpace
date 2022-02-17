94f73cb1f8de8013ed5db24c7ee09b2adfc5d22f
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2022-02-18 00:44:31 +0300

    WebhookService: used BackgroundService

--- common/services/ASC.Webhooks.Service/Services/WorkerService.cs
+++ common/services/ASC.Webhooks.Service/Services/WorkerService.cs
@@ -1,14 +1,13 @@
 ﻿namespace ASC.Webhooks.Service.Services;
 
 [Singletone]
-public class WorkerService : IHostedService, IDisposable
+public class WorkerService : BackgroundService
 {
     private readonly ILog _logger;
     private readonly ConcurrentQueue<WebhookRequest> _queue;
     private readonly int? _threadCount = 10;
     private readonly WebhookSender _webhookSender;
-    private CancellationToken _cancellationToken;
-    private Timer _timer;
+    private readonly TimeSpan _waitingPeriod;
 
     public WorkerService(WebhookSender webhookSender,
         ILog logger,
@@ -17,79 +16,54 @@ public class WorkerService : IHostedService, IDisposable
     {
         _logger = logger;
         _webhookSender = webhookSender;
-        _queue = buildQueueService._queue;
+        _queue = buildQueueService.Queue;
         _threadCount = settings.ThreadCount;
+        _waitingPeriod = TimeSpan.FromSeconds(5);
     }
 
-    public void Dispose()
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _timer?.Dispose();
-    }
-
-    public Task StartAsync(CancellationToken cancellationToken)
-    {
-        _cancellationToken = cancellationToken;
-
-        _timer = new Timer(DoWork, null, 0, Timeout.Infinite);
-        return Task.CompletedTask;
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken)
-    {
-        Stop();
-        return Task.CompletedTask;
-    }
-
-    private void DoWork(object _)
-    {
-        if (_cancellationToken.IsCancellationRequested)
+        while (!stoppingToken.IsCancellationRequested)
         {
-            Stop();
-            return;
-        }
+            var queueSize = _queue.Count;
 
-        _timer.Change(Timeout.Infinite, Timeout.Infinite);
-        _logger.Debug("Procedure: Start.");
-        var queueSize = _queue.Count;
-
-        if (queueSize == 0)// change to "<= threadCount"
-        {
-            _logger.TraceFormat("Procedure: Waiting for data. Sleep {0}.", 5);
-            _timer.Change(TimeSpan.FromSeconds(5), TimeSpan.FromMilliseconds(-1));
-            return;
-        }
-
-        var tasks = new List<Task>();
-        var counter = 0;
-        for (int i = 0; i < queueSize; i++)
-        {
-            if (_cancellationToken.IsCancellationRequested)
+            if (queueSize == 0) // change to "<= threadCount"
             {
-                Stop();
-                return;
+                _logger.TraceFormat("Procedure: Waiting for data. Sleep {0}.", _waitingPeriod);
+
+                await Task.Delay(_waitingPeriod, stoppingToken);
+
+                continue;
             }
 
-            if (!_queue.TryDequeue(out var entry))
-                break;
+            var tasks = new List<Task>();
+            var counter = 0;
 
-            tasks.Add(_webhookSender.Send(entry, _cancellationToken));
-            counter++;
-
-            if (counter >= _threadCount)
+            for (int i = 0; i < queueSize; i++)
             {
-                Task.WaitAll(tasks.ToArray());
-                tasks.Clear();
-                counter = 0;
+                if (stoppingToken.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                if (!_queue.TryDequeue(out var entry))
+                {
+                    break;
+                }
+
+                tasks.Add(_webhookSender.Send(entry, stoppingToken));
+                counter++;
+
+                if (counter >= _threadCount)
+                {
+                    Task.WaitAll(tasks.ToArray());
+                    tasks.Clear();
+                    counter = 0;
+                }
             }
+
+            Task.WaitAll(tasks.ToArray());
+            _logger.Debug("Procedure: Finish.");
         }
-
-        Task.WaitAll(tasks.ToArray());
-        _logger.Debug("Procedure: Finish.");
-        _timer.Change(0, Timeout.Infinite);
-    }
-
-    private void Stop()
-    {
-        _timer?.Change(Timeout.Infinite, 0);
     }
 }
