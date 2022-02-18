f80310941987b66f9f0d72976f530fc89a91d9bb
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-02-18 14:03:09 +0300

    Web: Client: Tests: mock confirm data

---
+++ web/ASC.Web.Client/tests/mocking/endpoints.js
@@ -0,0 +1,7 @@
+module.exports = class Endpoints {
+  static confirm = {
+    url: ["http://localhost:8092/api/2.0/authentication/confirm.json"],
+    method: "POST",
+    baseDir: "user",
+  };
+};
