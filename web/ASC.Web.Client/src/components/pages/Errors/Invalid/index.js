c69ed2bc186858b365cd789bb94d19ad38352727
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-02-21 18:31:47 +0300

    Web: Client: add redirect

--- web/ASC.Web.Client/src/components/pages/Errors/Invalid/index.js
+++ web/ASC.Web.Client/src/components/pages/Errors/Invalid/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ErrorContainer from "@appserver/common/components/ErrorContainer";
 import { I18nextProvider, useTranslation } from "react-i18next";
+import { AppServerConfig } from "@appserver/common/constants";
+import config from "../../../../../package.json";
+import { combineUrl } from "@appserver/common/utils";
+
 import i18n from "./i18n";
 
 const InvalidError = () => {
+  const { proxyURL } = AppServerConfig;
+  const homepage = config.homepage;
+
+  const PROXY_HOMEPAGE_URL = combineUrl(proxyURL, homepage);
+
   const { t } = useTranslation();
+
+  setTimeout(() => (location.href = PROXY_HOMEPAGE_URL), 10000);
 
   return (
     <ErrorContainer
