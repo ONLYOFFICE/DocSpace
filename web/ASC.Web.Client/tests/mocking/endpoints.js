9e5253a81254fb994a1565992d92c5d974aee294
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-02-21 22:12:16 +0300

    Web: Client: Tests: mock data

--- web/ASC.Web.Client/tests/mocking/endpoints.js
+++ web/ASC.Web.Client/tests/mocking/endpoints.js
@@ -10,4 +10,22 @@ module.exports = class Endpoints {
     method: "GET",
     baseDir: "settings",
   };
+
+  static build = {
+    url: ["http://localhost:8092/api/2.0/settings/version/build.json"],
+    method: "GET",
+    baseDir: "settings",
+  };
+
+  static password = {
+    url: ["http://localhost:8092/api/2.0/settings/security/password"],
+    method: "GET",
+    baseDir: "settings",
+  };
+
+  static providers = {
+    url: ["http://localhost:8092/api/2.0/people/thirdparty/providers"],
+    method: "GET",
+    baseDir: "people",
+  };
 };
