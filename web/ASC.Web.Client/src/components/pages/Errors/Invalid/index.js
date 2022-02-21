c0c0188f2c2298d6d6499fa12db8c3d6149421ac
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-02-21 18:43:22 +0300

    Web: Client: fix by layout

--- web/ASC.Web.Client/src/components/pages/Errors/Invalid/index.js
+++ web/ASC.Web.Client/src/components/pages/Errors/Invalid/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ErrorContainer from "@appserver/common/components/ErrorContainer";
-import { I18nextProvider, useTranslation } from "react-i18next";
+import Text from "@appserver/components/text";
+import Link from "@appserver/components/link";
+import { I18nextProvider, useTranslation, Trans } from "react-i18next";
 import { AppServerConfig } from "@appserver/common/constants";
 import config from "../../../../../package.json";
 import { combineUrl } from "@appserver/common/utils";
@@ -18,10 +20,21 @@ const InvalidError = () => {
   setTimeout(() => (location.href = PROXY_HOMEPAGE_URL), 10000);
 
   return (
-    <ErrorContainer
-      headerText={t("ErrorInvalidHeader")}
-      bodyText={t("ErrorInvalidText")}
-    />
+    <ErrorContainer headerText={t("ErrorInvalidHeader")}>
+      <Text fontSize="13px" fontWeight="600">
+        <Trans t={t} i18nKey="ErrorInvalidText">
+          In 10 seconds you will be redirected to the
+          <Link
+            color="#2DA7DB"
+            fontSize="13px"
+            fontWeight="600"
+            href={PROXY_HOMEPAGE_URL}
+          >
+            login page
+          </Link>
+        </Trans>
+      </Text>
+    </ErrorContainer>
   );
 };
 
