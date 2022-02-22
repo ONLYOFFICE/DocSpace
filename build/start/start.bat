b8b23d266df6cad579b9299bbc048b74f064d768
SuhorukovAnton <anton.sukhorukov@onlyoffice.com>
2022-02-22 13:15:02 +0300

    winsw: update bats

--- build/start/start.bat
+++ build/start/start.bat
@@ -3,16 +3,15 @@
 PUSHD %~dp0..
 call runasadmin.bat "%~dpnx0"
 
-if %errorlevel% == 0 (
-	for /R "..\common\services\winswConfigs\" %%f in (*.xml) do (
-		call ..\thirdparty\winsw.exe start %%f
-	)
-)
 
 POPD
 
 if %errorlevel% == 0 (
 	pwsh  %~dp0/command.ps1 "start"
+	
+	for /R "%~dp0\..\..\common\services\winswConfigs\" %%f in (*.xml) do (
+		call %~dp0\..\..\thirdparty\winsw.exe start %%f
+	)
 )
 
 echo.
--- build/start/stop.bat
+++ build/start/stop.bat
@@ -3,16 +3,14 @@
 PUSHD %~dp0..
 call runasadmin.bat "%~dpnx0"
 
-if %errorlevel% == 0 (
-	for /R "..\common\services\winswConfigs\" %%f in (*.xml) do (
-		call ..\thirdparty\winsw.exe stop %%f
-	)
-)
-
 POPD
 
 if %errorlevel% == 0 (
 	pwsh  %~dp0/command.ps1 "stop"
+	
+	for /R "%~dp0\..\..\common\services\winswConfigs\" %%f in (*.xml) do (
+		call %~dp0\..\..\thirdparty\winsw.exe stop %%f
+	)
 )
 
 echo.
