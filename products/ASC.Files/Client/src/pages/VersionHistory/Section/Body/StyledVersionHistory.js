159dfe0028b52bf4041f6232fde7679206f264a1
gopienkonikita <nikita.gopienko@onlyoffice.com>
2022-02-24 19:13:40 +0300

    Web: Files: fixed VersionHistory page styles

--- products/ASC.Files/Client/src/pages/VersionHistory/Section/Body/StyledVersionHistory.js
+++ products/ASC.Files/Client/src/pages/VersionHistory/Section/Body/StyledVersionHistory.js
@@ -146,7 +146,7 @@ const StyledVersionRow = styled(Row)`
     width: 10px;
     height: 10px;
     margin-left: 9px;
-    margin-right: 16px;
+    margin-right: 32px;
     @media ${tablet} {
       margin-top: -1px;
     }
