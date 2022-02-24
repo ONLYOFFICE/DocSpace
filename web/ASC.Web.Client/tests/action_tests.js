b62f56f7e2e579ca862bdded5271a6cd8d5a3cdb
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-02-24 23:03:03 +0300

    Web: Client: Tests: Tfa activation success test

--- web/ASC.Web.Client/tests/action_tests.js
+++ web/ASC.Web.Client/tests/action_tests.js
@@ -28,6 +28,26 @@ Scenario("Tfa auth success", async ({ I }) => {
   I.see("Documents");
 });
 
+Scenario("Tfa activation success", async ({ I }) => {
+  I.mockEndpoint(Endpoints.confirm, "confirm");
+  I.mockEndpoint(Endpoints.setup, "setup");
+  I.mockEndpoint(Endpoints.validation, "validation");
+  I.mockEndpoint(Endpoints.settings, "settings");
+  I.mockEndpoint(Endpoints.build, "build");
+  I.mockEndpoint(Endpoints.info, "info");
+  I.mockEndpoint(Endpoints.self, "self");
+
+  I.amOnPage("/confirm/TfaActivation");
+
+  I.fillField("code", "123456");
+
+  I.click({
+    react: "Button",
+  });
+
+  I.see("Documents");
+});
+
 Scenario("Profile remove success", async ({ I }) => {
   I.mockEndpoint(Endpoints.confirm, "confirm");
 
