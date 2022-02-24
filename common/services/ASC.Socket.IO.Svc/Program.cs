6b7fe6461f52244a82d2a293612feb1a42368178
pavelbannov <pavel.bannov@onlyoffice.com>
2022-02-24 14:57:21 +0300

    fix socketio

--- common/services/ASC.Socket.IO.Svc/Program.cs
+++ common/services/ASC.Socket.IO.Svc/Program.cs
@@ -102,11 +102,11 @@ namespace ASC.Socket.IO.Svc
 
                     if (kafkaConfiguration != null)
                     {
-                        diHelper.TryAdd(typeof(ICacheNotify<>), typeof(KafkaCache<>));
+                        diHelper.TryAdd(typeof(ICacheNotify<>), typeof(KafkaCacheNotify<>));
                     }
                     else if (redisConfiguration != null)
                     {
-                        diHelper.TryAdd(typeof(ICacheNotify<>), typeof(RedisCache<>));
+                        diHelper.TryAdd(typeof(ICacheNotify<>), typeof(KafkaCacheNotify<>));
 
                         services.AddStackExchangeRedisExtensions<NewtonsoftSerializer>(redisConfiguration);
                     }
