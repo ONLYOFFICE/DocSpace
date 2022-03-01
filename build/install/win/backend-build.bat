9234045fd4fa5102b4ab9ff7f0c9d1adb9e1a277
eugene-kozyrev <eugene.kozyrev@onlyoffice.com>
2022-03-01 11:05:18 +0300

    Add ASC.SsoAuth build

--- build/install/win/backend-build.bat
+++ build/install/win/backend-build.bat
@@ -10,17 +10,22 @@ call dotnet build ASC.Web.sln
 
 echo "== Build ASC.Thumbnails =="
 pushd common\ASC.Thumbnails
-call yarn install --frozen-lockfile
+  call yarn install --frozen-lockfile
 popd
 
 echo "== Build ASC.UrlShortener =="
 pushd common\ASC.UrlShortener
-call yarn install --frozen-lockfile
+  call yarn install --frozen-lockfile
 popd
 
 echo "== Build ASC.Socket.IO =="
 pushd common\ASC.Socket.IO
-call yarn install --frozen-lockfile
+  call yarn install --frozen-lockfile
+popd
+
+echo "== Build ASC.SsoAuth =="
+pushd common\ASC.SsoAuth
+  call yarn install --frozen-lockfile
 popd
 
 popd
