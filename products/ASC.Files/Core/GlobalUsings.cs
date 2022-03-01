a73606f1f9036bfef0a93433cc908f6aaf422775
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2022-03-01 17:26:30 +0300

    Files: used implicit usings

--- products/ASC.Files/Core/GlobalUsings.cs
+++ products/ASC.Files/Core/GlobalUsings.cs
@@ -1,15 +1,10 @@
-﻿global using System;
-global using System.Collections;
+﻿global using System.Collections;
 global using System.Collections.Concurrent;
-global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Globalization;
-global using System.IO;
-global using System.Linq;
 global using System.Linq.Expressions;
 global using System.Net;
-global using System.Net.Http;
 global using System.Net.Http.Headers;
 global using System.Net.Mime;
 global using System.Runtime.Serialization;
@@ -20,8 +15,6 @@ global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Text.RegularExpressions;
-global using System.Threading;
-global using System.Threading.Tasks;
 global using System.Web;
 global using System.Xml;
 
