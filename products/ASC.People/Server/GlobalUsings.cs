2719149f3ded78054ec00e1c14997d82ff04df75
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2022-03-01 14:58:37 +0300

    People: used imlicit usings

--- products/ASC.People/Server/GlobalUsings.cs
+++ products/ASC.People/Server/GlobalUsings.cs
@@ -1,14 +1,7 @@
-﻿global using System;
-global using System.Collections.Generic;
-global using System.IO;
-global using System.Linq;
-global using System.Net.Http;
-global using System.Net.Mail;
+﻿global using System.Net.Mail;
 global using System.Runtime.InteropServices;
 global using System.Security;
 global using System.ServiceModel.Security;
-global using System.Threading;
-global using System.Threading.Tasks;
 global using System.Web;
 
 global using ASC.Api.Core;
@@ -51,14 +44,8 @@ global using Autofac;
 global using Autofac.Extensions.DependencyInjection;
 
 global using Microsoft.AspNetCore.Authorization;
-global using Microsoft.AspNetCore.Builder;
-global using Microsoft.AspNetCore.Hosting;
-global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Http.Extensions;
 global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.Extensions.Configuration;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Options;
 global using Microsoft.Extensions.Hosting.WindowsServices;
 
