8411255de027300bd8018309411788c24e2e5fd5
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-03-03 12:54:35 +0300

    Web: Components: add more tests

--- packages/asc-web-components/code-input/code-input.test.js
+++ packages/asc-web-components/code-input/code-input.test.js
@@ -8,4 +8,22 @@ describe("<CodeInput />", () => {
 
     expect(wrapper).toExist();
   });
+
+  it("accepts id", () => {
+    const wrapper = mount(<CodeInput id="testId" />);
+
+    expect(wrapper.prop("id")).toEqual("testId");
+  });
+
+  it("accepts className", () => {
+    const wrapper = mount(<CodeInput className="test" />);
+
+    expect(wrapper.prop("className")).toEqual("test");
+  });
+
+  it("accepts style", () => {
+    const wrapper = mount(<CodeInput style={{ color: "red" }} />);
+
+    expect(wrapper.getDOMNode().style).toHaveProperty("color", "red");
+  });
 });
