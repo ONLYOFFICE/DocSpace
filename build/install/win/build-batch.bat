fc4c5b6236a6f89bccee37b2fed4dc3eed0212f4
eugene-kozyrev <eugene.kozyrev@onlyoffice.com>
2022-03-03 08:52:27 +0300

    Edit .json sed substitutions

--- build/install/win/build-batch.bat
+++ build/install/win/build-batch.bat
@@ -40,11 +40,12 @@ REM echo ######## Edit nginx conf files ########
 %sed% -i "s!/var/www!..!g" build/install/win/Files/nginx/conf/includes/onlyoffice-*.conf
 
 REM echo ######## Edit json files ########
-%sed% -i "s!\(\"machinekey\":\).*!\1 \"1123askdasjklasbnd\",!g" build/install/win/Files/config/appsettings.test.json
-%sed% -i "s!\(\"folder\":\).*!\1 \"{APPDIRCONF}products\",!g" build/install/win/Files/config/appsettings.test.json
-%sed% -i "s!\(\"path\":\).*!\1 \"{APPDIRCONF}services\/ASC.Socket.IO\/service\",!g" build/install/win/Files/config/socket.json
-%sed% -i "s!\(\"path\":\).*!\1 \"{APPDIRCONF}services\/ASC.Thumbnails\/service\"!g" build/install/win/Files/config/thumb.test.json
-%sed% -i "s!\(\"path\":\).*!\1 \"{APPDIRCONF}services\/ASC.UrlShortener\/service\/index.js\"!g" build/install/win/Files/config/urlshortener.test.json
+%sed% -i "s!\(\"machinekey\":\).*!\1 \"1123askdasjklasbnd\",!g" build/install/win/Files/config/appsettings*.json
+%sed% -i "s!\(\"folder\":\).*!\1 \"{APPDIRCONF}products\",!g" build/install/win/Files/config/appsettings*.json
+%sed% -i "s!\(\"path\":\).*!\1 \"{APPDIRCONF}services\/ASC.Socket.IO\/service\",!g" build/install/win/Files/config/socket*.json
+%sed% -i "s!\(\"path\":\).*!\1 \"{APPDIRCONF}services\/ASC.Thumbnails\/service\"!g" build/install/win/Files/config/thumb*.json
+%sed% -i "s!\(\"path\":\).*!\1 \"{APPDIRCONF}services\/ASC.UrlShortener\/service\/index.js\"!g" build/install/win/Files/config/urlshortener*.json
+%sed% -i "s!\(\"path\":\).*!\1 \"{APPDIRCONF}services\/ASC.SsoAuth\/service\"!g" build/install/win/Files/config/ssoauth*.json
 
 REM echo ######## Delete sed temp files ########
 del /f /q build\install\win\Files\config\sed*
