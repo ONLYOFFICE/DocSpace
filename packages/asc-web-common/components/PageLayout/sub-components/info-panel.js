bb69417dbf9faeed9397514382a0f76904eacd1e
mushka <nikitamushka@mail.ru>
2022-03-03 16:22:27 +0300

    fix kafka code formatting and ContextMenu - InfoPanel layering interaction

--- packages/asc-web-common/components/PageLayout/sub-components/info-panel.js
+++ packages/asc-web-common/components/PageLayout/sub-components/info-panel.js
@@ -7,128 +7,128 @@ import React, { useEffect } from "react";
 import styled from "styled-components";
 
 const StyledInfoPanelWrapper = styled.div.attrs(({ title }) => ({
-    title: title,
+  title: title,
 }))`
-    height: auto;
-    width: auto;
-    background: rgba(6, 22, 38, 0.2);
-    backdrop-filter: blur(18px);
+  height: auto;
+  width: auto;
+  background: rgba(6, 22, 38, 0.2);
+  backdrop-filter: blur(18px);
 
-    @media ${tablet} {
-        z-index: 200;
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-    }
+  @media ${tablet} {
+    z-index: 1002;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
 `;
 
 const StyledInfoPanel = styled.div`
-    height: 100%;
-    width: 368px;
-    background-color: #ffffff;
-    border-left: 1px solid #eceef1;
-    display: flex;
-    flex-direction: column;
+  height: 100%;
+  width: 368px;
+  background-color: #ffffff;
+  border-left: 1px solid #eceef1;
+  display: flex;
+  flex-direction: column;
 
-    @media ${tablet} {
-        position: absolute;
-        border: none;
-        right: 0;
-        width: 448px;
-        max-width: calc(100vw - 69px);
-    }
+  @media ${tablet} {
+    position: absolute;
+    border: none;
+    right: 0;
+    width: 448px;
+    max-width: calc(100vw - 69px);
+  }
 
-    @media ${mobile} {
-        bottom: 0;
-        height: 80%;
-        width: 100vw;
-        max-width: 100vw;
-    }
+  @media ${mobile} {
+    bottom: 0;
+    height: 80%;
+    width: 100vw;
+    max-width: 100vw;
+  }
 `;
 
 const StyledCloseButtonWrapper = styled.div`
-    position: absolute;
-    display: none;
-    @media ${tablet} {
-        display: block;
-        top: 0;
-        left: 0;
-        margin-top: 18px;
-        margin-left: -27px;
-    }
-    @media ${mobile} {
-        right: 0;
-        left: auto;
-        margin-top: -27px;
-        margin-right: 10px;
-    }
+  position: absolute;
+  display: none;
+  @media ${tablet} {
+    display: block;
+    top: 0;
+    left: 0;
+    margin-top: 18px;
+    margin-left: -27px;
+  }
+  @media ${mobile} {
+    right: 0;
+    left: auto;
+    margin-top: -27px;
+    margin-right: 10px;
+  }
 `;
 
 const InfoPanel = ({ children, selectedItems, isVisible, setIsVisible }) => {
-    //if (selectedItems.length > 0) setIsVisible(true);
-    //else setIsVisible(false);
+  //if (selectedItems.length > 0) setIsVisible(true);
+  //else setIsVisible(false);
 
-    if (!isVisible) return null;
+  if (!isVisible) return null;
 
-    const closeInfoPanel = () => setIsVisible(false);
+  const closeInfoPanel = () => setIsVisible(false);
 
-    useEffect(() => {
-        const onMouseDown = (e) => {
-            if (e.target.title === "InfoPanelWrapper") closeInfoPanel();
-        };
+  useEffect(() => {
+    const onMouseDown = (e) => {
+      if (e.target.title === "InfoPanelWrapper") closeInfoPanel();
+    };
 
-        if (isTablet()) document.addEventListener("mousedown", onMouseDown);
-        return () => document.removeEventListener("mousedown", onMouseDown);
-    }, []);
+    if (isTablet()) document.addEventListener("mousedown", onMouseDown);
+    return () => document.removeEventListener("mousedown", onMouseDown);
+  }, []);
 
-    return (
-        <StyledInfoPanelWrapper className="info-panel" title="InfoPanelWrapper">
-            <StyledInfoPanel>
-                <StyledCloseButtonWrapper>
-                    <IconButton
-                        onClick={closeInfoPanel}
-                        iconName="/static/images/cross.react.svg"
-                        size="17"
-                        color="#ffffff"
-                        hoverColor="#657077"
-                        isFill={true}
-                        className="info-panel-button"
-                    />
-                </StyledCloseButtonWrapper>
-                {children}
-            </StyledInfoPanel>
-        </StyledInfoPanelWrapper>
-    );
+  return (
+    <StyledInfoPanelWrapper className="info-panel" title="InfoPanelWrapper">
+      <StyledInfoPanel>
+        <StyledCloseButtonWrapper>
+          <IconButton
+            onClick={closeInfoPanel}
+            iconName="/static/images/cross.react.svg"
+            size="17"
+            color="#ffffff"
+            hoverColor="#657077"
+            isFill={true}
+            className="info-panel-button"
+          />
+        </StyledCloseButtonWrapper>
+        {children}
+      </StyledInfoPanel>
+    </StyledInfoPanelWrapper>
+  );
 };
 
 InfoPanel.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node,
-        PropTypes.any,
-    ]),
-    isVisible: PropTypes.bool,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+    PropTypes.any,
+  ]),
+  isVisible: PropTypes.bool,
 };
 
 export default inject(({ infoPanelStore, filesStore }) => {
-    let selectedItems = [];
-    let isVisible = false;
-    let setIsVisible = () => {};
+  let selectedItems = [];
+  let isVisible = false;
+  let setIsVisible = () => {};
 
-    if (infoPanelStore) {
-        isVisible = infoPanelStore.isVisible;
-        setIsVisible = infoPanelStore.setIsVisible;
-    }
+  if (infoPanelStore) {
+    isVisible = infoPanelStore.isVisible;
+    setIsVisible = infoPanelStore.setIsVisible;
+  }
 
-    if (filesStore) {
-        selectedItems = JSON.parse(JSON.stringify(filesStore.selection));
-    }
+  if (filesStore) {
+    selectedItems = JSON.parse(JSON.stringify(filesStore.selection));
+  }
 
-    return {
-        selectedItems,
-        isVisible,
-        setIsVisible,
-    };
+  return {
+    selectedItems,
+    isVisible,
+    setIsVisible,
+  };
 })(InfoPanel);
