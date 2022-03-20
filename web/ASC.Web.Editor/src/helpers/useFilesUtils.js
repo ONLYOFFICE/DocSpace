18d40455835e5eaab6c498818fa471b79bae2555
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-03-20 20:32:09 +0300

    Web: Doceditor: added useFilesUtils hook

---
+++ web/ASC.Web.Editor/src/helpers/useFilesUtils.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { loadComponent } from "../components/dynamic";
+import { FILES_SCOPE } from "./constants";
+function useFilesUtils() {
+  React.useEffect(() => {
+    if (document.getElementById(FILES_SCOPE)) {
+      loadComponent(FILES_SCOPE, "./utils", "filesUtils")();
+    }
+  }, []);
+}
+
+export default useFilesUtils;
