ec215d8e93acdf24325b2e9e5488cbebcae6afcb
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-03-24 20:50:01 +0300

    Web:Studio:Refactoring the Customization component. Added to state heightSettingsBlock, heightScrollBody. Refactoring the checkHeightSettingsBlock function.

--- web/ASC.Web.Client/src/components/pages/Settings/categories/common/customization.js
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/common/customization.js
@@ -48,7 +48,7 @@ const StyledComponent = styled.div`
     margin-right: 4px;
   }
 
-  @media (min-width: 601px) {
+  @media (min-width: 600px) {
     .settings-block {
       max-width: 350px;
       height: auto;
