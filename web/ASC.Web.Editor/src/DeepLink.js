a8c9b1c2940b5cd92331b3760bf22343a0eee11a
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-03-25 14:59:03 +0300

    Web: Editor: init deeplink page

---
+++ web/ASC.Web.Editor/src/DeepLink.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Text from "@appserver/components/text";
+import Button from "@appserver/components/button";
+import Link from "@appserver/components/link";
+import Checkbox from "@appserver/components/checkbox";
+import { useTranslation } from "react-i18next";
+
+const StyledBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 32px;
+
+  .description {
+    margin-bottom: 32px;
+  }
+
+  .button {
+    margin-top: 32px;
+    margin-bottom: 24px;
+  }
+
+  .link {
+    text-align: center;
+  }
+`;
+
+const StyledFileTile = styled.div`
+  display: flex;
+  gap: 16px;
+  padding: 16px;
+  margin: 16px 0;
+  background: #f3f4f4;
+  border-radius: 3px;
+  align-items: center;
+`;
+
+const StyledHeader = styled.div`
+  display: flex;
+  height: 48px;
+  background-color: #0f4071;
+  align-items: center;
+
+  img {
+    padding-left: 32px;
+  }
+`;
+
+const SimpleHeader = () => {
+  return (
+    <StyledHeader>
+      <img alt="logo" src="/static/images/nav.logo.opened.react.svg" />
+    </StyledHeader>
+  );
+};
+
+const DeepLinkPage = (props) => {
+  console.log(props);
+  const { fileInfo } = props;
+  const [isChecked, setIsChecked] = useState(false);
+  const { t } = useTranslation(["Editor", "Common"]);
+
+  const onChangeCheckbox = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const getFileIcon = () => {
+    const fileExst = ".docx";
+    const iconPath = "/static/images/icons/32/";
+    return `${iconPath}${fileExst}.svg`;
+  };
+
+  return (
+    <>
+      <SimpleHeader />
+      <StyledBody>
+        <Text fontSize="23px" fontWeight="700">
+          {t("Common:OpeningDocument")}
+        </Text>
+        <StyledFileTile>
+          <img src={getFileIcon()} />
+          <Text fontSize="14px" fontWeight="600" truncate>
+            Hello
+          </Text>
+        </StyledFileTile>
+        <Text className="description" fontSize="13px" fontWeight="400">
+          {t("Common:DeepLinkDescription")}
+        </Text>
+        <Checkbox
+          label={t("Common:Remember")}
+          onChange={onChangeCheckbox}
+          isChecked={isChecked}
+        />
+        <Button
+          className="button"
+          label={t("Common:OpenInApp")}
+          //onClick={onOpenAppClick}
+          primary
+          scale
+          size="normal"
+        />
+
+        <Link
+          className="link"
+          color="#316DAA"
+          fontWeight="600"
+          //onClick={onStayBrowserClick}
+          target="_self"
+          type="action"
+        >
+          {t("Common:StayBrowser")}
+        </Link>
+      </StyledBody>
+    </>
+  );
+};
+
+export default DeepLinkPage;
