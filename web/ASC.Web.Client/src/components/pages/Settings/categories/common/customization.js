fe65e040cbdd6b407e1629c3e942ca5267a94cbd
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-03-28 22:22:29 +0300

    Web:Studio:Refactoring of the Customization component. Added SettingsPageLayout, SettingsPageMobileView components.

--- web/ASC.Web.Client/src/components/pages/Settings/categories/common/customization.js
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/common/customization.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { withTranslation } from "react-i18next";
 import styled from "styled-components";
 import withCultureNames from "@appserver/common/hoc/withCultureNames";
-import CustomizationNavbar from "./customization-navbar";
-import LanguageAndTimeZone from "./language-and-time-zone";
-import CustomTitles from "./custom-titles";
-import PortalRenaming from "./portal-renaming";
-import { Base } from "@appserver/components/themes";
-import { isSmallTablet } from "@appserver/components/utils/device";
+import LanguageAndTimeZone from "./settingsCustomization/language-and-time-zone";
+import CustomTitles from "./settingsCustomization/custom-titles";
+import PortalRenaming from "./settingsCustomization/portal-renaming";
+import SettingsPageLayout from "./SettingsPageLayout";
+import SettingsPageMobileView from "./SettingsPageMobileView";
 
-import commonSettingsStyles from "../../utils/commonSettingsStyles";
+import { Base } from "@appserver/components/themes";
+
 const StyledComponent = styled.div`
   .combo-button-label {
     max-width: 100%;
@@ -60,38 +60,25 @@ const StyledComponent = styled.div`
 StyledComponent.defaultProps = { theme: Base };
 
 const Customization = ({ t }) => {
-  const [mobileView, setMobileView] = useState();
-
-  const checkInnerWidth = () => {
-    if (window.innerWidth < 600) {
-      setMobileView(true);
-    } else {
-      setMobileView(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", checkInnerWidth);
-    return () => window.removeEventListener("resize", checkInnerWidth);
-  }, [checkInnerWidth]);
-
-  return isSmallTablet() || mobileView ? (
-    <CustomizationNavbar />
-  ) : (
-    <StyledComponent>
-      <div className="category-description">{`${t(
-        "Settings:CustomizationDescription"
-      )}`}</div>
-      <div className="category-item-wrapper">
-        <LanguageAndTimeZone />
-      </div>
-      {/* <div className="category-item-wrapper">
-              <CustomTitles />
-            </div>
-            <div className="category-item-wrapper">
-              <PortalRenaming />
-            </div> */}
-    </StyledComponent>
+  return (
+    <SettingsPageLayout>
+      {(isMobile) =>
+        isMobile ? (
+          <SettingsPageMobileView>
+            <LanguageAndTimeZone isMobileView={isMobile} />
+            {/* <CustomTitles /> */}
+          </SettingsPageMobileView>
+        ) : (
+          <StyledComponent>
+            <div className="category-description">{`${t(
+              "Settings:CustomizationDescription"
+            )}`}</div>
+            <LanguageAndTimeZone isMobileView={isMobile} />
+            {/* <CustomTitles /> */}
+          </StyledComponent>
+        )
+      }
+    </SettingsPageLayout>
   );
 };
 
