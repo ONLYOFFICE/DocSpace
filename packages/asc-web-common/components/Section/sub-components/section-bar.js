59becc4207adea647cd11e01096418d1df3f5d08
Dmitry Sychugov <dmitry.sychugov@onlyoffice.com>
2022-03-28 17:26:12 +0500

    Web: Common: delete should component update

--- packages/asc-web-common/components/Section/sub-components/section-bar.js
+++ packages/asc-web-common/components/Section/sub-components/section-bar.js
@@ -2,10 +2,6 @@ import React from "react";
 import equal from "fast-deep-equal/react";
 
 class SectionBar extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return !equal(this.props, nextProps);
-  }
-
   componentWillUnmount() {
     this.props.setMaintenanceExist && this.props.setMaintenanceExist(false);
   }
