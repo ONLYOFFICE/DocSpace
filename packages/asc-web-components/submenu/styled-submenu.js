4d980901d119b9efd778bffdec5d6bc8e2ae1e1b
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-03-30 19:26:14 +0300

    Web: Components: Submenu: delete justify-content

--- packages/asc-web-components/submenu/styled-submenu.js
+++ packages/asc-web-components/submenu/styled-submenu.js
@@ -29,7 +29,6 @@ export const StyledSubmenuContentWrapper = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
-  justify-content: center;
 `;
 
 export const StyledSubmenuItems = styled.div`
