95959317a255a231cc281fe90e2e0d758df31c72
SuhorukovAnton <anton.sukhorukov@onlyoffice.com>
2022-04-11 10:38:00 +0300

    fix

--- common/ASC.IPSecurity/IPRestrictionsService.cs
+++ common/ASC.IPSecurity/IPRestrictionsService.cs
@@ -75,7 +75,7 @@ namespace ASC.IPSecurity
         {
             var key = IPRestrictionsServiceCache.GetCacheKey(tenant);
             var restrictions = cache.Get<List<IPRestriction>>(key);
-            if (restrictions == null || restrictions.Count == 0)
+            if (restrictions == null)
             {
                 restrictions = IPRestrictionsRepository.Get(tenant);
                 cache.Insert(key, restrictions, timeout);
