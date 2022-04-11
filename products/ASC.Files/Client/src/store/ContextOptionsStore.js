0e7eec8221af0613779ea330e1ac42ce9d6ba3aa
gopienkonikita <nikita.gopienko@onlyoffice.com>
2022-04-11 16:22:23 +0300

    Web: Files: fixed "Duplicate" translation

--- products/ASC.Files/Client/src/store/ContextOptionsStore.js
+++ products/ASC.Files/Client/src/store/ContextOptionsStore.js
@@ -412,7 +412,7 @@ class ContextOptionsStore {
                 },
                 {
                   key: "copy",
-                  label: t("Duplicate"),
+                  label: t("Common:Duplicate"),
                   onClick: () => this.onDuplicate(item, t),
                   disabled: false,
                 },
@@ -436,7 +436,7 @@ class ContextOptionsStore {
             },
             {
               key: "copy",
-              label: t("Duplicate"),
+              label: t("Common:Duplicate"),
               icon: "/static/images/copy.react.svg",
               onClick: () => this.onDuplicate(item, t),
               disabled: false,
