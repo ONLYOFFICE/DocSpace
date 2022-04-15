3921acd43d33917f040a829e3f2328eaf8890c3d
Timofey Boyko <fognir742@gmail.com>
2022-04-15 14:22:29 +0300

    Web:Components:Themes: change menu container text color of dark-theme

--- packages/asc-web-components/themes/dark.js
+++ packages/asc-web-components/themes/dark.js
@@ -1931,7 +1931,7 @@ const Dark = {
 
   menuContainer: {
     background: "linear-gradient(226.22deg, #606060 0.24%, #1F1F1F 79.81%)",
-    color: "#22221f",
+    color: "rgba(255, 255, 255, 0.92)",
   },
 
   article: {
