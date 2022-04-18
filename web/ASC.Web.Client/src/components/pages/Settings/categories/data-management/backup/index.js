73cce7bd6cffd597c202dea1b03bdfc3a1f9d3e8
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-04-18 12:09:02 +0300

    Web: Client: Settings: fix backup layout

--- web/ASC.Web.Client/src/components/pages/Settings/categories/data-management/backup/index.js
+++ web/ASC.Web.Client/src/components/pages/Settings/categories/data-management/backup/index.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MobileView from "./MobileView";
 import DesktopView from "./DesktopView";
-import { isMobileOnly } from "react-device-detect";
 import { withRouter } from "react-router-dom";
+import { size } from "@appserver/components/utils/device";
 
 const Backup = (props) => {
-  return isMobileOnly ? <MobileView {...props} /> : <DesktopView {...props} />;
+  const [isMobileView, setIsMobileView] = useState(false);
+
+  useEffect(() => {
+    checkWidth();
+    window.addEventListener("resize", checkWidth);
+    return () => window.removeEventListener("resize", checkWidth);
+  }, []);
+
+  const checkWidth = () => {
+    window.innerWidth <= size.smallTablet
+      ? setIsMobileView(true)
+      : setIsMobileView(false);
+  };
+
+  return isMobileView ? <MobileView {...props} /> : <DesktopView {...props} />;
 };
 
 export default withRouter(Backup);
