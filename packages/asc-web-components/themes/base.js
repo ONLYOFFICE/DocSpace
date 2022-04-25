d00c143d7be575a8d12477ef3d0eab6dd5d2a49f
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-04-25 13:46:33 +0300

    Web: Components: Themes: fix group menu border

--- packages/asc-web-components/themes/base.js
+++ packages/asc-web-components/themes/base.js
@@ -2060,7 +2060,7 @@ const Base = {
 
     groupMenu: {
       background: white,
-      borderBottom: "none",
+      borderBottom: "1px solid transparent",
       borderRight: `1px solid ${grayMid}`,
       boxShadow: "0px 5px 20px rgba(4, 15, 27, 7%)",
     },
