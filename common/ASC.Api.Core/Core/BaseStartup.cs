fe39f4d3985dc08c38aaa8492ffd525c323c03a7
pavelbannov <pavel.bannov@onlyoffice.com>
2022-04-26 21:34:27 +0300

    fix namespaces

--- common/ASC.Api.Core/Core/BaseStartup.cs
+++ common/ASC.Api.Core/Core/BaseStartup.cs
@@ -24,12 +24,6 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-using ASC.Common.Logging;
-
-using NLog;
-using NLog.Config;
-using NLog.Extensions.Logging;
-
 using JsonConverter = System.Text.Json.Serialization.JsonConverter;
 
 namespace ASC.Api.Core;
