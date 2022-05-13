5f75774729727d5ffc18c76e810cccade9a59a67
SuhorukovAnton <anton.sukhorukov@onlyoffice.com>
2022-05-13 13:47:01 +0300

    add loggers

--- common/ASC.MessagingSystem/Core/MessageService.cs
+++ common/ASC.MessagingSystem/Core/MessageService.cs
@@ -192,7 +192,7 @@ public class MessageService
 
         if (_request == null)
         {
-            _logger.LogDebug("Empty Http Request for \"{action}\" type of event", action);
+            _logger.DebugEmptyHttpRequest(action);
 
             return;
         }
