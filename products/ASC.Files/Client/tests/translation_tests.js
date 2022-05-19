31dec5f6e9ad2538fa70ab040b90a78f523d0e70
Timofey Boyko <fognir742@gmail.com>
2022-05-19 17:27:14 +0300

    Web:Files:Tests: add new translation tests: sort button on tile view, add button, filter block and context menu

--- products/ASC.Files/Client/tests/translation_tests.js
+++ products/ASC.Files/Client/tests/translation_tests.js
@@ -31,22 +31,16 @@ Before(async ({ I }) => {
 
 for (const culture of cultures) {
   Scenario(`Main page test ${culture}`, { timeout: 30 }, ({ I }) => {
-    changeCulture(culture);
-
+    changeCulture(culture, isPersonal);
     const isException = ignoringCultures.mainPage.indexOf(culture) != -1;
-
     I.mockEndpoint(Endpoints.my, "default");
-
     if (!isException) {
       I.mockEndpoint(Endpoints.self, `selfTranslation`);
       I.mockEndpoint(Endpoints.settings, settingsTranslationFile);
     }
-
     I.amOnPage("/products/files");
     I.wait(3);
-
     I.see("My documents");
-
     I.saveScreenshot(`${culture}-main-page.png`);
     I.seeVisualDiff(`${culture}-main-page.png`, {
       tolerance: 0.025,
@@ -56,23 +50,17 @@ for (const culture of cultures) {
   });
 
   Scenario(`Profile menu test ${culture}`, { timeout: 30 }, ({ I }) => {
-    changeCulture(culture);
+    changeCulture(culture, isPersonal);
     const isException = ignoringCultures.profileMenu.indexOf(culture) != -1;
-
     I.mockEndpoint(Endpoints.my, "default");
-
     if (!isException) {
       I.mockEndpoint(Endpoints.self, `selfTranslation`);
       I.mockEndpoint(Endpoints.settings, settingsTranslationFile);
     }
-
     I.amOnPage("/products/files");
     I.wait(3);
-
     I.openProfileMenu();
-
     I.wait(3);
-
     I.saveScreenshot(`${culture}-profile-menu.png`);
     I.seeVisualDiff(`${culture}-profile-menu.png`, {
       tolerance: 0.05,
@@ -81,24 +69,18 @@ for (const culture of cultures) {
   });
 
   Scenario(`Main button test ${culture}`, { timeout: 30 }, ({ I }) => {
-    changeCulture(culture);
+    changeCulture(culture, isPersonal);
     const isException = ignoringCultures.mainButton.indexOf(culture) != -1;
-
     I.mockEndpoint(Endpoints.my, "default");
-
     if (!isException) {
       I.mockEndpoint(Endpoints.self, `selfTranslation`);
       I.mockEndpoint(Endpoints.settings, settingsTranslationFile);
     }
-
     I.amOnPage("/products/files");
     I.wait(3);
-
     I.seeElement({ react: "MainButton" });
     I.click({ react: "MainButton" });
-
     I.wait(3);
-
     I.saveScreenshot(`${culture}-main-button.png`);
     I.seeVisualDiff(`${culture}-main-button.png`, {
       tolerance: 0.01,
@@ -109,27 +91,109 @@ for (const culture of cultures) {
 
   Scenario(`Table settings test ${culture}`, { timeout: 30 }, ({ I }) => {
     const isException = ignoringCultures.tableSettings.indexOf(culture) != -1;
-
     I.mockEndpoint(Endpoints.my, "default");
-
     if (!isException) {
       I.mockEndpoint(Endpoints.self, `selfTranslation`);
       I.mockEndpoint(Endpoints.settings, settingsTranslationFile);
     }
-
     I.amOnPage("/products/files");
     I.wait(3);
-
     I.seeElement({ react: "TableSettings" });
     I.click({ react: "TableSettings" });
-
     I.wait(3);
-
     I.saveScreenshot(`${culture}-table-settings.png`);
     I.seeVisualDiff(`${culture}-table-settings.png`, {
       tolerance: 0.07,
       prepareBaseImage: false,
       ignoredBox: { top: 0, left: 0, bottom: 0, right: 1770 },
+    });
+  });
+
+  Scenario(`Add button test ${culture}`, { timeout: 30 }, ({ I }) => {
+    changeCulture(culture, isPersonal);
+    const isException = ignoringCultures.addButton.indexOf(culture) != -1;
+    I.mockEndpoint(Endpoints.my, "default");
+    if (!isException) {
+      I.mockEndpoint(Endpoints.self, `selfTranslation`);
+      I.mockEndpoint(Endpoints.settings, settingsTranslationFile);
+    }
+    I.amOnPage("/products/files");
+    I.wait(3);
+    I.seeElement({
+      react: "ContextMenuButton",
+      props: { className: "add-button" },
+    });
+    I.click({ react: "ContextMenuButton", props: { className: "add-button" } });
+    I.wait(3);
+    I.saveScreenshot(`${culture}-add-button.png`);
+    I.seeVisualDiff(`${culture}-add-button.png`, {
+      tolerance: 0.01,
+      prepareBaseImage: false,
+    });
+  });
+
+  Scenario(`Sort menu test ${culture}`, { timeout: 30 }, ({ I }) => {
+    changeCulture(culture, isPersonal);
+    const isException = ignoringCultures.sortMenu.indexOf(culture) != -1;
+    I.mockEndpoint(Endpoints.my, "default");
+    if (!isException) {
+      I.mockEndpoint(Endpoints.self, `selfTranslation`);
+      I.mockEndpoint(Endpoints.settings, settingsTranslationFile);
+    }
+    I.amOnPage("/products/files");
+    I.wait(3);
+    I.switchView("tile");
+    I.seeElement({
+      react: "IconButton",
+      props: { className: "sort-icon" },
+    });
+    I.click({ react: "IconButton", props: { className: "sort-icon" } });
+    I.wait(3);
+    I.saveScreenshot(`${culture}-sort-menu.png`);
+    I.seeVisualDiff(`${culture}-sort-menu.png`, {
+      tolerance: 0.01,
+      prepareBaseImage: false,
+    });
+  });
+
+  Scenario(`Filter block test ${culture}`, { timeout: 30 }, ({ I }) => {
+    changeCulture(culture, isPersonal);
+    const isException = ignoringCultures.filterBlock.indexOf(culture) != -1;
+    I.mockEndpoint(Endpoints.my, "default");
+    if (!isException) {
+      I.mockEndpoint(Endpoints.self, `selfTranslation`);
+      I.mockEndpoint(Endpoints.settings, settingsTranslationFile);
+    }
+    I.amOnPage("/products/files");
+    I.wait(3);
+    I.seeElement({
+      react: "FilterButton",
+    });
+    I.click({ react: "FilterButton" });
+    I.wait(3);
+    I.saveScreenshot(`${culture}-filter-block.png`);
+    I.seeVisualDiff(`${culture}-filter-block.png`, {
+      tolerance: 0.01,
+      prepareBaseImage: false,
+    });
+  });
+
+  Scenario(`Context menu test ${culture}`, { timeout: 30 }, ({ I }) => {
+    changeCulture(culture, isPersonal);
+    const isException = ignoringCultures.contextMenu.indexOf(culture) != -1;
+    I.mockEndpoint(Endpoints.my, "default");
+    if (!isException) {
+      I.mockEndpoint(Endpoints.self, `selfTranslation`);
+      I.mockEndpoint(Endpoints.settings, settingsTranslationFile);
+    }
+    I.amOnPage("/products/files");
+    I.wait(3);
+    I.openContextMenu();
+    I.wait(3);
+    I.saveScreenshot(`${culture}-context-menu.png`);
+    I.seeVisualDiff(`${culture}-context-menu.png`, {
+      tolerance: 0.01,
+      prepareBaseImage: false,
     });
   });
 }
