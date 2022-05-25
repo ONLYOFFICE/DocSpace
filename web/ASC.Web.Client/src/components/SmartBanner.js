59f1307223713a3a08d05665edd8980dc08b1c5c
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-05-25 22:07:50 +0300

    Web: Client: SmartBanner: fix show logic

--- web/ASC.Web.Client/src/components/SmartBanner.js
+++ web/ASC.Web.Client/src/components/SmartBanner.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { isMobile, isIOS } from "react-device-detect";
 import SmartBanner from "react-smartbanner";
 import "./smartbanner.css";
 
@@ -8,10 +9,42 @@ const Wrapper = styled.div`
 `;
 
 const ReactSmartBanner = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const force = isIOS ? "ios" : "android";
+
+  const getCookie = (name) => {
+    let matches = document.cookie.match(
+      new RegExp(
+        "(?:^|; )" +
+          name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+          "=([^;]*)"
+      )
+    );
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+  };
+
+  const hideBanner = () => {
+    setIsVisible(false);
+  };
+
+  useEffect(() => {
+    const cookie = getCookie("smartbanner-closed");
+    if (cookie) hideBanner();
+  }, []);
+
   return (
-    <Wrapper>
-      <SmartBanner title="Onlyoffice" author="Onlyoffice" />
-    </Wrapper>
+    isMobile &&
+    isVisible && (
+      <Wrapper>
+        <SmartBanner
+          title="Onlyoffice"
+          author="Onlyoffice"
+          force={force}
+          onClose={hideBanner}
+          onInstall={hideBanner}
+        />
+      </Wrapper>
+    )
   );
 };
 
