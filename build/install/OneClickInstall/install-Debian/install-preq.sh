9f84493c89265fc834173c2a7eb420df641aca9c
evgeniy-antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2022-05-30 19:43:55 +0300

    Add onlyoffice group creation

--- build/install/OneClickInstall/install-Debian/install-preq.sh
+++ build/install/OneClickInstall/install-Debian/install-preq.sh
@@ -58,7 +58,7 @@ PRODUCT_DIR="/var/www/${product}"
 if [ "$(ls "$PRODUCT_DIR/services/kafka" 2> /dev/null)" == "" ]; then
 	mkdir -p ${PRODUCT_DIR}/services/kafka/
 	if ! cat /etc/passwd | grep -q "onlyoffice"; then
-		adduser --quiet --home ${PRODUCT_DIR} --system onlyoffice
+		adduser --quiet --home ${PRODUCT_DIR} --system --group onlyoffice
 	fi
 	cd ${PRODUCT_DIR}/services/kafka
 	KAFKA_VERSION=$(curl https://downloads.apache.org/kafka/ | grep -Eo '3.1.[0-9]' | tail -1)
