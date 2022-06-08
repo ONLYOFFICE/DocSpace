0b6f5deac2f550473cc6a0dd530c694ac5d2a839
mushka <nikitamushka@mail.ru>
2022-06-08 15:12:24 +0300

    added new icons: person+ and email+

---
+++ products/ASC.Files/Client/src/pages/Home/InfoPanel/Body/views/VirtualRoom/Details/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VirtualRoomDetails = () => {
+  return <div></div>;
+};
+
+export default VirtualRoomDetails;
---
+++ products/ASC.Files/Client/src/pages/Home/InfoPanel/Body/views/VirtualRoom/mock_data.js
@@ -0,0 +1,32 @@
+const fillingFormsVR = {
+  title: "Filling forms room",
+  icon: "images/icons/32/folder.svg",
+  members: {
+    inRoom: [
+      {
+        name: "Nikita Mushka",
+        role: "Owner",
+        avatar: "",
+        isMe: true,
+      },
+      {
+        name: "Konstantinov Konstantin Konstantinopolsky",
+        role: "Room manager",
+        avatar: "",
+      },
+      {
+        name: "Rebecca Holt",
+        role: "Room manager",
+        avatar: "",
+      },
+    ],
+    expect: [
+      { name: "Johnnie Cunnigham", role: "Viewer", avatar: "" },
+      { email: "ycummerata@yahoo.com", role: "Viewer" },
+      { email: "yoshiko05@rohan.biz", role: "Viewer" },
+      { name: "Angela Garcia", email: "ycummerata@yahoo.com", role: "Viewer" },
+    ],
+  },
+};
+
+export { fillingFormsVR };
