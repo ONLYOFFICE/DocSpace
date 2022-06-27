1b405301076f2f26671fab052e5ce44c475a8e14
gopienkonikita <nikita.gopienko@onlyoffice.com>
2022-06-27 15:25:34 +0300

    Fixed Bug 57814 - Client.HotKeys. Fixed setting of hotkey caret

--- products/ASC.Files/Client/src/store/HotkeyStore.js
+++ products/ASC.Files/Client/src/store/HotkeyStore.js
@@ -49,7 +49,8 @@ class HotkeyStore {
       e.preventDefault();
     }
 
-    const { selection, hotkeyCaret, viewAs } = this.filesStore;
+    const { selection: s, hotkeyCaret, viewAs, filesList } = this.filesStore;
+    const selection = s.length ? s : filesList;
 
     if (!hotkeyCaret) {
       const scroll = document.getElementsByClassName("section-scroll");
