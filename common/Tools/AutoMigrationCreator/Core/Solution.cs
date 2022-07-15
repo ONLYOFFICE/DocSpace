317a72898a819a5c23e0356684e9fb0e96910110
pavelbannov <pavel.bannov@onlyoffice.com>
2022-07-15 12:48:15 +0300

    refactoring

--- common/Tools/AutoMigrationCreator/Core/Solution.cs
+++ common/Tools/AutoMigrationCreator/Core/Solution.cs
@@ -24,13 +24,6 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-
-using Microsoft.Build.Construction;
-
 namespace AutoMigrationCreator.Core;
 
 public static class Solution
--- common/Tools/AutoMigrationCreator/Utils/DbContextActivator.cs
+++ common/Tools/AutoMigrationCreator/Utils/DbContextActivator.cs
@@ -24,11 +24,6 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-using System;
-using System.Configuration;
-
-using ASC.Core.Common.EF;
-
 namespace AutoMigrationCreator;
 
 public class DbContextActivator
