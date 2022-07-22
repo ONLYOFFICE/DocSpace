9eec30a707ccaa6f863fb3090faad3e82f5f2f8d
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-07-22 11:11:27 +0300

    Web:Common:Added methods getAppearanceTheme, sendAppearanceTheme.

--- packages/asc-web-common/store/SettingsStore.js
+++ packages/asc-web-common/store/SettingsStore.js
@@ -127,7 +127,9 @@ class SettingsStore {
   helpLink = null;
   hotkeyPanelVisible = false;
 
-  colorScheme = 1;
+  appearanceTheme = [];
+  selectedThemeId = null;
+  currentColorScheme = null;
 
   constructor() {
     makeAutoObservable(this);
@@ -240,7 +242,7 @@ class SettingsStore {
     this.setIsLoading(true);
     const requests = [];
 
-    requests.push(this.getPortalSettings());
+    requests.push(this.getPortalSettings(), this.getAppearanceTheme());
 
     this.tenantStatus !== TenantStatus.PortalRestore &&
       requests.push(this.getBuildVersionInfo());
@@ -528,8 +530,32 @@ class SettingsStore {
     this.hotkeyPanelVisible = hotkeyPanelVisible;
   };
 
-  setColorScheme = (colorScheme) => {
-    this.colorScheme = colorScheme;
+  setAppearanceTheme = (theme) => {
+    this.appearanceTheme = theme;
+  };
+
+  setSelectThemeId = (selected) => {
+    this.selectedThemeId = selected;
+  };
+
+  setCurrentColorScheme = (currentColorScheme) => {
+    this.currentColorScheme = currentColorScheme;
+  };
+
+  getAppearanceTheme = async () => {
+    const res = await api.settings.getAppearanceTheme();
+
+    const currentColorScheme = res.themes.find((theme) => {
+      return res.selected === theme.id;
+    });
+
+    this.setAppearanceTheme(res.themes);
+    this.setSelectThemeId(res.selected);
+    this.setCurrentColorScheme(currentColorScheme);
+  };
+
+  sendAppearanceTheme = async (data) => {
+    const res = await api.settings.sendAppearanceTheme(data);
   };
 }
 
