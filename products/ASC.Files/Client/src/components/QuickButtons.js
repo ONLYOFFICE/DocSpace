72e8dd64a406aa0a2ec01de875cd5de986deb8d8
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-07-25 22:16:01 +0300

    Web:Components:Added theme changer for QuickButtons.

--- products/ASC.Files/Client/src/components/QuickButtons.js
+++ products/ASC.Files/Client/src/components/QuickButtons.js
@@ -5,24 +5,28 @@ import commonIconsStyles from "@appserver/components/utils/common-icons-style";
 import { isMobile, isTablet } from "react-device-detect";
 import { FileStatus } from "@appserver/common/constants";
 
+import { ColorTheme, ThemeType } from "@appserver/common/components/ColorTheme";
+
 export const StyledIcon = styled(IconButton)`
   ${commonIconsStyles}
 `;
 
-const QuickButtons = ({
-  item,
-  theme,
-  sectionWidth,
-  isTrashFolder,
-  accessToEdit,
-  showShare,
-  onClickLock,
-  isDisabled,
-  onClickFavorite,
-  onClickShare,
-  viewAs,
-  isCanWebEdit,
-}) => {
+const QuickButtons = (props) => {
+  const {
+    item,
+    theme,
+    sectionWidth,
+    isTrashFolder,
+    accessToEdit,
+    showShare,
+    onClickLock,
+    isDisabled,
+    onClickFavorite,
+    onClickShare,
+    viewAs,
+    isCanWebEdit,
+  } = props;
+
   const { id, locked, fileStatus, title, fileExst, shared } = item;
 
   const isFavorite =
@@ -34,25 +38,19 @@ const QuickButtons = ({
     ? "/static/images/shared.share.react.svg"
     : "/static/images/share.react.svg";
 
-  const colorShare = shared
-    ? theme.filesQuickButtons.sharedColor
-    : theme.filesQuickButtons.color;
+  const colorShare = theme.filesQuickButtons.color;
 
   const iconLock = locked
     ? "/static/images/file.actions.locked.react.svg"
     : "/static/images/locked.react.svg";
 
-  const colorLock = locked
-    ? theme.filesQuickButtons.sharedColor
-    : theme.filesQuickButtons.color;
+  const colorLock = theme.filesQuickButtons.color;
 
   const iconFavorite = isFavorite
     ? "/static/images/file.actions.favorite.react.svg"
     : "/static/images/favorite.react.svg";
 
-  const colorFavorite = isFavorite
-    ? theme.filesQuickButtons.sharedColor
-    : theme.filesQuickButtons.color;
+  const colorFavorite = theme.filesQuickButtons.color;
 
   const tabletViewQuickButton =
     (sectionWidth > 500 && sectionWidth <= 1024) || isTablet;
@@ -66,43 +64,46 @@ const QuickButtons = ({
   return (
     <div className="badges additional-badges">
       {item.canShare && showShare && displayBadges && (
-        <StyledIcon
+        <ColorTheme
+          type={ThemeType.IconButton}
+          shared={shared}
           iconName={iconShare}
           className="badge share-button-icon"
           size={sizeQuickButton}
-          onClick={onClickShare}
           color={colorShare}
-          hoverColor={theme.filesQuickButtons.sharedColor}
-        />
+          onClick={onClickShare}
+        ></ColorTheme>
       )}
       {fileExst &&
         accessToEdit &&
         !isTrashFolder &&
         displayBadges &&
         isCanWebEdit && (
-          <StyledIcon
+          <ColorTheme
+            type={ThemeType.IconButton}
             iconName={iconLock}
+            locked={locked}
             className="badge lock-file icons-group"
             size={sizeQuickButton}
             data-id={id}
             data-locked={locked ? true : false}
             onClick={onClickLock}
-            isDisabled={isDisabled}
             color={colorLock}
-            hoverColor={theme.filesQuickButtons.sharedColor}
-          />
+            isDisabled={isDisabled}
+          ></ColorTheme>
         )}
       {fileExst && !isTrashFolder && displayBadges && (
-        <StyledIcon
+        <ColorTheme
+          type={ThemeType.IconButton}
           iconName={iconFavorite}
+          isFavorite={isFavorite}
           className="favorite badge icons-group"
           size={sizeQuickButton}
           data-id={id}
           data-title={title}
-          onClick={setFavorite}
           color={colorFavorite}
-          hoverColor={theme.filesQuickButtons.hoverColor}
-        />
+          onClick={setFavorite}
+        ></ColorTheme>
       )}
     </div>
   );
