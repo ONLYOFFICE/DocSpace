0425c79018ac6ea121b0e9c581136a7c68b7b9c1
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-07-25 10:41:55 +0300

    Web:Common:Delete className.

--- packages/asc-web-common/components/ColorTheme/styled/badge.js
+++ packages/asc-web-common/components/ColorTheme/styled/badge.js
@@ -1,15 +1,19 @@
 import styled, { css } from "styled-components";
-import { StyledBadge } from "@appserver/components/badge/styled-badge";
+import {
+  StyledBadge,
+  StyledInner,
+} from "@appserver/components/badge/styled-badge";
 
 const getDefaultStyles = ({ currentColorScheme }) => css`
-  .color-theme-background {
+  ${StyledInner} {
     background-color: ${currentColorScheme.accentColor};
+
+    &:hover {
+      background-color: ${currentColorScheme.accentColor};
+    }
   }
 
   &:hover {
-    .color-theme-background {
-      background-color: ${currentColorScheme.accentColor};
-    }
     border-color: ${currentColorScheme.accentColor};
   }
 `;
