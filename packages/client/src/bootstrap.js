e0856c1503e612e028ab7e50500ed1672b4a8347
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2022-07-31 12:53:20 +0300

    Web: bootstrap: Commented registerSW

--- packages/client/src/bootstrap.js
+++ packages/client/src/bootstrap.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { registerSW } from "@docspace/common/sw/helper";
+//import { registerSW } from "@docspace/common/sw/helper";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-registerSW();
+//registerSW();
