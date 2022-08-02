177e415b83362778e80be9eb7e81bcb822bfc58d
TimofeyBoyko <fognir742@gmail.com>
2022-08-02 10:21:09 +0300

    Web:PortalSettings:Integration: add submenu for thirdparty and plugins

--- packages/client/src/pages/PortalSettings/categories/integration/index.js
+++ packages/client/src/pages/PortalSettings/categories/integration/index.js
@@ -1,35 +1,68 @@
-import React, { lazy, Suspense } from "react";
-import { Route, Switch } from "react-router-dom";
-import { withRouter } from "react-router";
-import Loader from "@docspace/components/loader";
-import { combineUrl } from "@docspace/common/utils";
-import AppServerConfig from "@docspace/common/constants/AppServerConfig";
+import React from "react";
+import { inject, observer } from "mobx-react";
 
-const ThirdPartyServices = lazy(() => import("./thirdPartyServicesSettings"));
+import { withRouter } from "react-router";
+import { withTranslation } from "react-i18next";
+import withLoading from "SRC_DIR/HOCs/withLoading";
+
+import AppServerConfig from "@docspace/common/constants/AppServerConfig";
+import { combineUrl } from "@docspace/common/utils";
+import config from "PACKAGE_FILE";
+
+import Submenu from "@docspace/components/submenu";
+
+import ThirdPartyServices from "./thirdPartyServicesSettings";
+import PortalPlugins from "./PortalPlugins";
+
+// const ThirdPartyServices = lazy(() => import("./thirdPartyServicesSettings"));
 
 const PROXY_BASE_URL = combineUrl(
   AppServerConfig.proxyURL,
   "/portal-settings/integration"
 );
 
-const Integration = ({ match }) => {
+const Integration = ({ t, history }) => {
+  const [currentTab, setCurrentTab] = React.useState(0);
+
+  React.useEffect(() => {
+    const path = location.pathname;
+    const currentTab = data.findIndex((item) => path.includes(item.id));
+    if (currentTab !== -1) {
+      setCurrentTab(currentTab);
+    }
+  }, []);
+
+  const data = [
+    {
+      id: "third-party-services",
+      name: "Third party services",
+      content: <ThirdPartyServices />,
+    },
+    {
+      id: "plugins",
+      name: "Plugins",
+      content: <PortalPlugins />,
+    },
+  ];
+
+  const onSelect = (e) => {
+    history.push(combineUrl(PROXY_BASE_URL, `/${e.id}`));
+  };
+
   return (
-    <Suspense
-      fallback={<Loader className="pageLoader" type="rombs" size="40px" />}
-    >
-      <Switch>
-        <Route
-          exact
-          path={[
-            combineUrl(PROXY_BASE_URL, "/third-party-services"),
-            combineUrl(AppServerConfig.proxyURL, "/integration"),
-            match.path,
-          ]}
-          component={ThirdPartyServices}
-        />
-      </Switch>
-    </Suspense>
+    <Submenu
+      data={data}
+      startSelect={currentTab}
+      onSelect={(e) => onSelect(e)}
+      isLoading={false}
+    />
   );
 };
 
-export default withRouter(Integration);
+export default inject(({ common }) => {
+  const { isLoaded, setIsLoadedSubmenu } = common;
+  return {
+    isLoaded,
+    setIsLoadedSubmenu,
+  };
+})(withLoading(withRouter(withTranslation("Settings")(observer(Integration)))));
