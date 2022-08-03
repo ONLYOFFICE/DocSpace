a373d0dcc85660a1f1e33029fd67c175e4296bea
mushka <nikitamushka@mail.ru>
2022-08-03 14:42:49 +0300

    created own component for help button

---
+++ packages/client/src/components/dialogs/CreateEditRoomDialog/sub-components/SecondaryInfoButton.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+import HelpButton from "@docspace/components/help-button";
+
+const StyledHelpButton = styled(HelpButton)`
+  border-radius: 50%;
+  background-color: #a3a9ae;
+  circle,
+  rect {
+    fill: #ffffff;
+  }
+`;
+
+const SecondaryInfoButton = ({ content }) => {
+  return (
+    <StyledHelpButton
+      displayType="auto"
+      className="set_room_params-info-title-help"
+      iconName="/static/images/info.react.svg"
+      tooltipProps={{ globalEventOff: "click" }}
+      tooltipContent={content}
+      size={12}
+    />
+  );
+};
+
+export default SecondaryInfoButton;
