52fd53a381aad16a2963464f62edb44ca88824f7
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-08-04 12:14:55 +0300

    Web: Login: added index.d.ts

---
+++ packages/login/index.d.ts
@@ -0,0 +1,21 @@
+import { Request } from "express";
+export {};
+
+declare global {
+  interface Window {
+    authCallback?: (profile: object) => {};
+    __ASC_INITIAL_LOGIN_STATE__?: object;
+    initialI18nStoreASC?: object;
+    initialLanguage?: string;
+  }
+
+  interface IInitLoginState {
+    error?: string | object;
+  }
+
+  interface DevRequest extends Request {
+    assets?: object;
+  }
+  var IS_DEVELOPMENT: boolean;
+  var PORT: number;
+}
