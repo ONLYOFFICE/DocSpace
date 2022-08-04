b8f32739e58f09cb9df77eede9de5ad4414e2292
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-08-04 12:14:35 +0300

    Web: Login: added base client

---
+++ packages/login/src/client/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App: React.FC = (props) => {
+  return <div>Test: {JSON.stringify(props)}</div>;
+};
+
+export default App;
---
+++ packages/login/src/client/bootstrap.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { hydrate } from "react-dom";
+import App from "./App";
+import { registerSW } from "@docspace/common/sw/helper";
+import pkg from "../../package.json";
+
+const propsObj = window.__ASC_INITIAL_LOGIN_STATE__;
+const initialI18nStoreASC = window.initialI18nStoreASC;
+const initialLanguage = window.initialLanguage;
+
+hydrate(
+  <React.Suspense fallback={<div></div>}>
+    <App
+      initialLanguage={initialLanguage}
+      initialI18nStoreASC={initialI18nStoreASC}
+      {...propsObj}
+    />
+  </React.Suspense>,
+  document.getElementById("root")
+);
+
+if (IS_DEVELOPMENT) {
+  const port = PORT || 5011;
+  const socketPath = pkg.socketPath;
+
+  const ws = new WebSocket(`ws://localhost:${port}${socketPath}`);
+  let isErrorConnection = false;
+
+  ws.onopen = (event) => {
+    console.log("[login-dev] Socket is connected. Live reload enabled");
+  };
+
+  ws.onmessage = function (event) {
+    if (event.data === "reload") {
+      console.log("[login-dev] App updated. Reloading...");
+      location.reload();
+    }
+  };
+
+  ws.onclose = function (event) {
+    console.log("[login-dev] Socket is disconnected! Reloading...");
+    setTimeout(() => {
+      !isErrorConnection && location.reload();
+    }, 1500);
+  };
+
+  ws.onerror = (event) => {
+    isErrorConnection = true;
+    console.log("[login-dev] Socket connect error!");
+  };
+}
+
+registerSW();
