72ea160e18b127fc38bfee736fa56f21193a45d5
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2022-08-05 18:50:39 +0300

    Web: Client: PortalSettings: Fixed portal integration page header

--- packages/client/src/pages/PortalSettings/categories/integration/index.js
+++ packages/client/src/pages/PortalSettings/categories/integration/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { inject, observer } from "mobx-react";
 import { withRouter } from "react-router";
 import { withTranslation } from "react-i18next";
+import { isMobile } from "react-device-detect";
 
 import { combineUrl } from "@docspace/common/utils";
 import AppServerConfig from "@docspace/common/constants/AppServerConfig";
@@ -19,18 +20,21 @@ const Integration = (props) => {
   const [currentTab, setCurrentTab] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
+  const integrationData = {
+    id: "portal-integration",
+    name: "Portal Integration",
+    content: <PortalIntegration />,
+  };
+
   const data = [
     {
       id: "third-party-services",
       name: t("ThirdPartyAuthorization"),
       content: <ThirdPartyServices />,
     },
-    {
-      id: "portal-integration",
-      name: "Portal Integration",
-      content: <PortalIntegration />,
-    },
   ];
+
+  if (!isMobile) data.push(integrationData);
 
   const load = async () => {
     const { loadBaseInfo } = props;
