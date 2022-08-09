a85c33db504fe9a11ffa1df4c948160f1eeb2eea
mushka <nikitamushka@mail.ru>
2022-08-09 19:05:36 +0300

    added isRoom method to InfoPanelStore

--- packages/common/store/InfoPanelStore.js
+++ packages/common/store/InfoPanelStore.js
@@ -1,5 +1,8 @@
 import { makeAutoObservable } from "mobx";
 
+import { getCategoryType } from "SRC_DIR/helpers/utils";
+import { CategoryType } from "SRC_DIR/helpers/constants";
+
 class InfoPanelStore {
   isVisible = false;
   roomState = "members";
@@ -7,6 +10,17 @@ class InfoPanelStore {
   constructor() {
     makeAutoObservable(this);
   }
+
+  isRoom = (selectedItem) => {
+    const categoryType = getCategoryType(location);
+    const isRoomCategory =
+      categoryType == CategoryType.Shared ||
+      categoryType == CategoryType.SharedRoom ||
+      categoryType == CategoryType.Archive ||
+      categoryType == CategoryType.ArchivedRoom;
+    const isRoomItem = selectedItem.isRoom;
+    return isRoomCategory && isRoomItem;
+  };
 
   toggleIsVisible = () => {
     this.isVisible = !this.isVisible;
