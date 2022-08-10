97aee7f8c2c0738f74e4de6173886048c3f4550b
Nikolay Rechkin <nikolay.rechkin@onlyoffice.com>
2022-08-10 15:33:02 +0300

    SSO: fix login

--- common/ASC.Common/Utils/Signature.cs
+++ common/ASC.Common/Utils/Signature.cs
@@ -58,14 +58,14 @@ public class Signature
     {
         try
         {
-            var mod4 = signature.Length % 4;
+            var lastSignChar = Int32.Parse(signature.Substring(signature.Length - 1));
+            signature = signature.Remove(signature.Length - 1);
 
-            while (mod4 > 0)
+            while(lastSignChar > 0)
             {
-                signature = signature.Remove(signature.Length - 1);
-                mod4 = signature.Length % 4;
+                signature = signature + "=";
+                lastSignChar--;
             }
-
             var payloadParts = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(signature)).Split('?');
 
             if (GetHashBase64(payloadParts[1].Trim() + secret) == payloadParts[0])
