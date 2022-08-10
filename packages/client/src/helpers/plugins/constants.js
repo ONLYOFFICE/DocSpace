10e20b9f1773c83ddd2045276d68da7d578615cf
TimofeyBoyko <fognir742@gmail.com>
2022-08-10 16:26:33 +0300

    Web:Helplers:Plugins: add plugin type for contstants

--- packages/client/src/helpers/plugins/constants.js
+++ packages/client/src/helpers/plugins/constants.js
@@ -4,3 +4,9 @@ export const PluginContextMenuItemType = Object.freeze({
   Rooms: "Rooms",
   All: "All",
 });
+
+export const PluginType = Object.freeze({
+  CONTEXT_MENU: "context menu",
+  ACTION_BUTTON: "action button",
+  PROFILE_MENU: "profile menu",
+});
