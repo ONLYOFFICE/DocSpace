db2967571c710c002667ade4962142c52cf55563
eugene-kozyrev <eugene.kozyrev@onlyoffice.com>
2022-08-12 13:45:16 +0300

    Add DocEditor service
    Update windows installation project on develop

--- build/install/win/frontend-copy.bat
+++ build/install/win/frontend-copy.bat
@@ -17,10 +17,8 @@ if defined SecondArg (
 )
 
 xcopy "%PathToRepository%\build\deploy\public" "%PathToAppFolder%\public" /s /y /b /i
-xcopy "%PathToRepository%\build\deploy\studio\client" "%PathToAppFolder%\studio\client" /s /y /b /i
-xcopy "%PathToRepository%\build\deploy\studio\login" "%PathToAppFolder%\studio\login" /s /y /b /i
-xcopy "%PathToRepository%\build\deploy\products\ASC.Files\client" "%PathToAppFolder%\products\ASC.Files\client" /s /y /b /i
-xcopy "%PathToRepository%\build\deploy\products\ASC.Files\editor" "%PathToAppFolder%\products\ASC.Files\editor" /s /y /b /i
-xcopy "%PathToRepository%\build\deploy\products\ASC.People\client" "%PathToAppFolder%\products\ASC.People\client" /s /y /b /i
+xcopy "%PathToRepository%\build\deploy\client" "%PathToAppFolder%\client" /s /y /b /i
+xcopy "%PathToRepository%\build\deploy\login" "%PathToAppFolder%\login" /s /y /b /i
+xcopy "%PathToRepository%\build\deploy\editor" "%PathToAppFolder%\products\ASC.Files\editor" /s /y /b /i
 xcopy "%PathToRepository%\config\nginx" "%PathToAppFolder%\nginx\conf" /s /y /b /i
 xcopy "%PathToRepository%\config\*" "%PathToAppFolder%\config" /y /b /i
