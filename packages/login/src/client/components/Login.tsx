885c92a5624c1fa03e05662c5e0504e7eb68e5d4
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-08-12 22:42:39 +0300

    Web: Login: added ssoButton

--- packages/login/src/client/components/Login.tsx
+++ packages/login/src/client/components/Login.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import {
   ButtonsWrapper,
   LoginContainer,
@@ -6,6 +7,8 @@ import {
 } from "./StyledLogin";
 import Logo from "../../../../../public/images/docspace.big.react.svg";
 import Text from "@docspace/components/text";
+import SocialButton from "@docspace/components/social-button";
+import { getProviderTranslation } from "@docspace/common/utils";
 
 const greetingTitle = "Web Office Applications"; // from PortalSettingsStore
 
@@ -24,7 +27,31 @@ const App: React.FC<ILoginProps> = ({
   isDesktopEditor,
   ...rest
 }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   const { enabledJoin } = portalSettings;
+  const { ssoLabel, ssoUrl } = capabilities;
+
+  const { t } = useTranslation(["Login", "Common"]);
+
+  const ssoExists = () => {
+    if (ssoUrl) return true;
+    else return false;
+  };
+
+  const ssoButton = () => {
+    return (
+      <div className="buttonWrapper">
+        <SocialButton
+          iconName="/static/images/sso.react.svg"
+          className="socialButton"
+          label={ssoLabel || getProviderTranslation("sso", t)}
+          onClick={() => (window.location.href = ssoUrl)}
+          isDisabled={isLoading}
+        />
+      </div>
+    );
+  };
 
   return (
     <LoginFormWrapper enabledJoin={enabledJoin} isDesktop={isDesktopEditor}>
@@ -38,6 +65,7 @@ const App: React.FC<ILoginProps> = ({
         >
           {greetingTitle}
         </Text>
+        {ssoExists() && <ButtonsWrapper>{ssoButton()}</ButtonsWrapper>}
       </LoginContainer>
     </LoginFormWrapper>
   );
