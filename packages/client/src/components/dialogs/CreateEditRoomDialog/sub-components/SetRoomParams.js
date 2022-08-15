ec286077c29a2e833b364b89da20535cafa97470
mushka <nikitamushka@mail.ru>
2022-08-15 15:57:33 +0300

    updated and fixed SetRoomParams

--- packages/client/src/components/dialogs/CreateEditRoomDialog/sub-components/SetRoomParams.js
+++ packages/client/src/components/dialogs/CreateEditRoomDialog/sub-components/SetRoomParams.js
@@ -1,40 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import RoomTypeDropdown from "./RoomTypeDropdown";
-import StorageLocation from "./StorageLocation";
+import ThirdPartyStorage from "./ThirdPartyStorage";
 
 import TextInput from "@docspace/components/text-input";
 import Label from "@docspace/components/label";
-import ToggleButton from "@docspace/components/toggle-button";
-import Text from "@docspace/components/text";
 import TagInput from "./TagInput";
-import { StyledParam } from "./StyledParam";
 import RoomType from "./RoomType";
 import { roomTypes } from "../data";
-import SecondaryInfoButton from "./SecondaryInfoButton";
 import IconEditor from "./IconEditor";
+import PermanentSettings from "./PermanentSettings";
+import ToggleParam from "./Params/ToggleParam";
+import InputParam from "./Params/InputParam";
+import IsPrivateParam from "./IsPrivateParam";
 
 const StyledSetRoomParams = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   gap: 20px;
-
-  .set_room_params-input {
-    display: flex;
-    flex-direction: column;
-    gap: 4px;
-  }
-`;
-
-const StyledIconEditorWrapper = styled.div`
-  .use_modal-avatar_editor_body {
-    margin: 0;
-  }
-
-  .use_modal-buttons_wrapper {
-    display: none;
-  }
 `;
 
 const SetRoomParams = ({
@@ -45,6 +29,7 @@ const SetRoomParams = ({
   tagHandler,
   setIsScrollLocked,
   isEdit,
+  providers,
 }) => {
   const onChangeName = (e) =>
     setRoomParams({ ...roomParams, title: e.target.value });
@@ -52,8 +37,22 @@ const SetRoomParams = ({
   const onChangeIsPrivate = () =>
     setRoomParams({ ...roomParams, isPrivate: !roomParams.isPrivate });
 
-  const onChangeThidpartyFolderName = (e) =>
-    setRoomParams({ ...roomParams, thirdpartyFolderName: e.target.value });
+  // const onChangeThidpartyFolderName = (e) =>
+  //   setRoomParams({ ...roomParams, thirdpartyFolderName: e.target.value });
+
+  const onChangeIcon = (icon) => setRoomParams({ ...roomParams, icon: icon });
+
+  const onChangeIsThirdparty = () =>
+    setRoomParams({ ...roomParams, isThirdparty: !roomParams.isThirdparty });
+
+  const setChangeStorageLocation = (storageLocation) =>
+    setRoomParams({ ...roomParams, storageLocation });
+
+  const onChangeRememberThirdpartyStorage = () =>
+    setRoomParams({
+      ...roomParams,
+      rememberThirdpartyStorage: !roomParams.rememberThirdpartyStorage,
+    });
 
   const [currentRoomTypeData] = roomTypes.filter(
     (room) => room.type === roomParams.type
@@ -71,22 +70,23 @@ const SetRoomParams = ({
         />
       )}
 
-      <div className="set_room_params-input">
-        <Label
-          display="display"
-          htmlFor="room-name"
-          text={`${t("Common:Name")}:`}
-          title="Fill the first name field"
+      {isEdit && (
+        <PermanentSettings
+          t={t}
+          title={roomParams.title}
+          isThirdparty={roomParams.isThirdparty}
+          storageLocation={roomParams.storageLocation}
+          isPrivate={roomParams.isPrivate}
         />
-        <TextInput
-          id="room-name"
-          value={roomParams.title}
-          onChange={onChangeName}
-          scale
-          placeholder={t("NamePlaceholder")}
-          tabIndex={1}
-        />
-      </div>
+      )}
+
+      <InputParam
+        id={"room-name"}
+        title={`${t("Common:Name")}:`}
+        placeholder={t("NamePlaceholder")}
+        value={roomParams.title}
+        onChange={onChangeName}
+      />
 
       <TagInput
         t={t}
@@ -94,61 +94,66 @@ const SetRoomParams = ({
         setIsScrollLocked={setIsScrollLocked}
       />
 
-      <StyledParam isPrivate>
-        <div className="set_room_params-info">
-          <div className="set_room_params-info-title">
-            <Text className="set_room_params-info-title-text">
-              {t("MakeRoomPrivateTitle")}
-            </Text>
-            <SecondaryInfoButton content={t("MakeRoomPrivateSecondaryInfo")} />
-          </div>
-          <div className="set_room_params-info-description">
-            {t("MakeRoomPrivateDescription")}
-          </div>
-        </div>
-        <ToggleButton
-          className="set_room_params-toggle"
-          isChecked={roomParams.isPrivate}
-          onChange={onChangeIsPrivate}
+      {!isEdit && (
+        <IsPrivateParam
+          t={t}
+          isPrivate={roomParams.isPrivate}
+          onChangeIsPrivate={onChangeIsPrivate}
         />
-      </StyledParam>
+      )}
 
-      <StorageLocation
+      {!isEdit && (
+        <ThirdPartyStorage
+          t={t}
+          providers={providers}
+          roomParams={roomParams}
+          isThirdparty={roomParams.isThirdparty}
+          onChangeIsThirdparty={onChangeIsThirdparty}
+          storageLocation={roomParams.storageLocation}
+          setChangeStorageLocation={setChangeStorageLocation}
+          rememberThirdpartyStorage={roomParams.rememberThirdpartyStorage}
+          onChangeRememberThirdpartyStorage={onChangeRememberThirdpartyStorage}
+          setIsScrollLocked={setIsScrollLocked}
+        />
+      )}
+
+      {/* {!isEdit && roomParams.isThirdparty && (
+        <StyledParam folderName>
+          <div className="set_room_params-info">
+            <div className="set_room_params-info-title">
+              <Text className="set_room_params-info-title-text">
+                {`${t("FolderNameTitle")}:`}
+              </Text>
+            </div>
+            <div className="set_room_params-info-description">
+              {t("FolderNameDescription")}
+            </div>
+          </div>
+
+          <div className="set_room_params-input">
+            <TextInput
+              id="room-folder-title"
+              scale
+              value={roomParams.thirdpartyFolderName}
+              onChange={onChangeThidpartyFolderName}
+              placeholder={`${
+                roomParams.storageLocation
+                  ? roomParams.storageLocation.title + " - "
+                  : ""
+              }${t("Files:NewRoom")}`}
+              tabIndex={1}
+            />
+          </div>
+        </StyledParam>
+      )} */}
+
+      <IconEditor
         t={t}
-        roomParams={roomParams}
-        setRoomParams={setRoomParams}
-        setIsScrollLocked={setIsScrollLocked}
+        title={roomParams.title}
+        tags={roomParams.tags}
+        icon={roomParams.icon}
+        onChangeIcon={onChangeIcon}
       />
-
-      <StyledParam folderName>
-        <div className="set_room_params-info">
-          <div className="set_room_params-info-title">
-            <Text className="set_room_params-info-title-text">
-              {`${t("FolderNameTitle")}:`}
-            </Text>
-          </div>
-          <div className="set_room_params-info-description">
-            {t("FolderNameDescription")}
-          </div>
-        </div>
-
-        <div className="set_room_params-input">
-          <TextInput
-            id="room-folder-title"
-            scale
-            value={roomParams.thirdpartyFolderName}
-            onChange={onChangeThidpartyFolderName}
-            placeholder={`${
-              roomParams.storageLocation
-                ? roomParams.storageLocation.title + " - "
-                : ""
-            }${t("Home:NewRoom")}`}
-            tabIndex={1}
-          />
-        </div>
-      </StyledParam>
-
-      <IconEditor />
     </StyledSetRoomParams>
   );
 };
