5aa2147bf396c3d3c7a8f967dc8472e50e97b5bc
mushka <nikitamushka@mail.ru>
2022-08-15 15:52:30 +0300

    added permanent settings

---
+++ packages/client/src/components/dialogs/CreateEditRoomDialog/sub-components/PermanentSettings/PermanentSetting.js
@@ -0,0 +1,94 @@
+import React from "react";
+import styled, { css } from "styled-components";
+import { ReactSVG } from "react-svg";
+
+import SecondaryInfoButton from "../SecondaryInfoButton";
+
+const StyledPermanentSetting = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: ${(props) => (props.isFull ? "column" : "row")};
+  align-items: ${(props) => (props.isFull ? "start" : "center")};
+  justify-content: ${(props) => (props.isFull ? "center" : "start")};
+  gap: 4px;
+
+  width: 100%;
+  max-width: 100%;
+  padding: 12px 16px;
+
+  background: #f8f9f9;
+  border-radius: 6px;
+
+  .permanent_setting-main_info {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+    gap: 8px;
+
+    &-icon {
+      width: 16px;
+      height: 16px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      svg {
+        max-width: 100%;
+        max-height: 100%;
+        width: 16px;
+        height: 16px;
+
+        ${(props) =>
+          props.type === "privacy" &&
+          css`
+            path {
+              fill: #35ad17;
+            }
+          `}
+      }
+    }
+
+    &-title {
+      font-weight: 600;
+      font-size: 12px;
+      line-height: 16px;
+    }
+  }
+
+  .permanent_setting-help_button {
+    margin-left: auto;
+  }
+
+  .permanent_setting-secondary-info {
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 16px;
+    color: #555f65;
+  }
+`;
+
+const PermanentSetting = ({ isFull, type, icon, title, content }) => {
+  return (
+    <StyledPermanentSetting
+      className="permanent_setting"
+      isFull={isFull}
+      type={type}
+    >
+      <div className="permanent_setting-main_info">
+        <ReactSVG className="permanent_setting-main_info-icon" src={icon} />
+        <div className="permanent_setting-main_info-title">{title}</div>
+      </div>
+
+      {isFull ? (
+        <div className="permanent_setting-secondary-info">{content}</div>
+      ) : (
+        <div className="permanent_setting-help_button">
+          <SecondaryInfoButton content={content} />
+        </div>
+      )}
+    </StyledPermanentSetting>
+  );
+};
+
+export default PermanentSetting;
---
+++ packages/client/src/components/dialogs/CreateEditRoomDialog/sub-components/PermanentSettings/index.js
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+import { Trans } from "react-i18next";
+
+import {
+  connectedCloudsTypeIcon as getProviderTypeIcon,
+  connectedCloudsTypeTitleTranslation as getProviderTypeTitle,
+} from "@docspace/client/src/helpers/filesUtils";
+
+import PermanentSetting from "./PermanentSetting";
+
+const StyledPermanentSettings = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  margin-top: -12px;
+`;
+
+const PermanentSettings = ({
+  t,
+  title,
+  isThirdparty,
+  storageLocation,
+  isPrivate,
+}) => {
+  const thirdpartyTitle = isThirdparty
+    ? getProviderTypeTitle(storageLocation.providerKey, t)
+    : "";
+
+  const thirdpartyIcon = isThirdparty
+    ? getProviderTypeIcon(storageLocation.providerKey)
+    : "";
+
+  const thirdpartyFolderName = isThirdparty ? storageLocation.title : "";
+
+  const createThirdpartyPath = () => {
+    const path = storageLocation.parentId.split("|");
+    path.shift();
+    path.unshift(thirdpartyTitle);
+    path.push(thirdpartyFolderName);
+    return `(${path.join("/")})`;
+  };
+
+  const thirdpartyPath = isThirdparty ? createThirdpartyPath() : "";
+
+  return (
+    <StyledPermanentSettings>
+      {isThirdparty && (
+        <PermanentSetting
+          type="storageLocation"
+          isFull={!isPrivate}
+          icon={thirdpartyIcon}
+          title={thirdpartyTitle}
+          content={
+            <Trans
+              i18nKey="ThirdPartyStoragePermanentSettingDescription"
+              ns="CreateEditRoomDialog"
+              t={t}
+            >
+              Files are stored in a third-party {{ thirdpartyTitle }} storage in
+              the \"{{ thirdpartyFolderName }}\" folder.{" "}
+              <strong>{{ thirdpartyPath }}</strong>"
+            </Trans>
+          }
+        />
+      )}
+      {isPrivate && (
+        <PermanentSetting
+          type="privacy"
+          isFull={!storageLocation}
+          icon={"images/security.svg"}
+          title={"Private room"}
+          content={`All files in this room will be encrypted`}
+        />
+      )}
+    </StyledPermanentSettings>
+  );
+};
+
+export default PermanentSettings;
