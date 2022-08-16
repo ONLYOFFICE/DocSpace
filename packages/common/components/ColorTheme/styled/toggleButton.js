bde4a1f0192d21f9429d3fa6bedd73ca0e9c83b2
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-16 13:42:31 +0300

    Web:Common:Fix ToggleButtonTheme.

--- packages/common/components/ColorTheme/styled/toggleButton.js
+++ packages/common/components/ColorTheme/styled/toggleButton.js
@@ -4,20 +4,22 @@ import {
   ToggleButtonContainer,
 } from "@docspace/components/toggle-button/styled-toggle-button";
 
-const getDefaultStyles = ({ currentColorScheme, isDisabled, isChecked }) => css`
-  ${ToggleButtonContainer} {
-    svg {
-      rect {
-        fill: ${isChecked && currentColorScheme.accentColor};
-        opacity: ${isDisabled && "0.6"};
-
-        &:hover {
+const getDefaultStyles = ({ currentColorScheme, isDisabled, isChecked }) =>
+  currentColorScheme &&
+  css`
+    ${ToggleButtonContainer} {
+      svg {
+        rect {
           fill: ${isChecked && currentColorScheme.accentColor};
           opacity: ${isDisabled && "0.6"};
+
+          &:hover {
+            fill: ${isChecked && currentColorScheme.accentColor};
+            opacity: ${isDisabled && "0.6"};
+          }
         }
       }
     }
-  }
-`;
+  `;
 
 export default styled(Container)(getDefaultStyles);
