e36ffa1f7513e0a3dcd2a74b70cb37886528299c
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-16 09:20:43 +0300

    Web:Common:Fix button styles.

--- packages/common/components/ColorTheme/styled/button.js
+++ packages/common/components/ColorTheme/styled/button.js
@@ -1,16 +1,55 @@
 import styled, { css } from "styled-components";
 import StyledButton from "@docspace/components/button/styled-button";
 
-const getDefaultStyles = ({ primary, currentColorScheme, isDisabled }) => css`
-  background: ${!!primary && currentColorScheme.buttonsMain};
-
-  opacity: ${!!primary && isDisabled && "0.6"};
-  border-color: ${!!primary && currentColorScheme.buttonsMain};
-
-  &:hover {
-    background: ${!!primary && currentColorScheme.buttonsMain};
-    border-color: ${currentColorScheme.buttonsMain};
-  }
+const activeCss = css`
+  border-color: ${(props) => props.currentColorScheme.buttonsMain};
+  background: ${(props) =>
+    !!props.primary && props.currentColorScheme.buttonsMain};
+  // filter: ${(props) => !!props.primary && "brightness(60%)"};
 `;
+
+const hoverCss = css`
+  border-color: ${(props) => props.currentColorScheme.buttonsMain};
+  background: ${(props) =>
+    !!props.primary && props.currentColorScheme.buttonsMain};
+  opacity: ${(props) => !!props.primary && "0.85"};
+`;
+
+const getDefaultStyles = ({
+  primary,
+  currentColorScheme,
+  isDisabled,
+  isLoading,
+  isClicked,
+  isHovered,
+  disableHover,
+}) =>
+  currentColorScheme.buttonsMain &&
+  css`
+    background: ${!!primary && currentColorScheme.buttonsMain};
+    opacity: ${!!primary && isDisabled && "0.6"};
+    border-color: ${!!primary && currentColorScheme.buttonsMain};
+
+    ${!isDisabled &&
+    !isLoading &&
+    (isHovered
+      ? hoverCss
+      : !disableHover &&
+        css`
+          &:hover {
+            ${hoverCss}
+          }
+        `)}
+
+    ${!isDisabled &&
+    !isLoading &&
+    (isClicked
+      ? activeCss
+      : css`
+          &:active {
+            ${activeCss}
+          }
+        `)}
+  `;
 
 export default styled(StyledButton)(getDefaultStyles);
