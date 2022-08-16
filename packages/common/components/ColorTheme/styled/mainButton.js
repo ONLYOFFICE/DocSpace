fb453b3582767d0cf3e7cde9651fa113d28b8f8b
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-16 09:21:12 +0300

    Web:Common:Fix mainButton styles.

--- packages/common/components/ColorTheme/styled/mainButton.js
+++ packages/common/components/ColorTheme/styled/mainButton.js
@@ -1,11 +1,18 @@
 import styled, { css } from "styled-components";
 import { StyledMainButton } from "@docspace/components/main-button/styled-main-button";
 
-const getDefaultStyles = ({ currentColorScheme }) => css`
+const getDefaultStyles = ({ currentColorScheme, isDisabled }) => css`
   background: ${currentColorScheme.accentColor};
 
   &:hover {
     background: ${currentColorScheme.accentColor};
+    cursor: pointer;
+  }
+
+  &:active {
+    background: ${currentColorScheme.accentColor};
+    opacity: ${!isDisabled && "0.6"};
+    cursor: pointer;
   }
 `;
 
