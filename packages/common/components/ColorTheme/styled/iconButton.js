60f15d8a1d3db051d024eeaf79de0dbdaf809c52
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-16 13:51:41 +0300

    Web:Common:Fix IconButtonTheme styles.

--- packages/common/components/ColorTheme/styled/iconButton.js
+++ packages/common/components/ColorTheme/styled/iconButton.js
@@ -2,29 +2,49 @@ import styled, { css } from "styled-components";
 import IconButton from "@docspace/components/icon-button";
 import commonIconsStyles from "@docspace/components/utils/common-icons-style";
 
+const StyledEditIcon = styled(IconButton)`
+  ${commonIconsStyles}
+
+  svg {
+    path {
+      fill: ${(props) => props.theme.filesSection.rowView.editingIconColor};
+    }
+  }
+`;
+
 const getDefaultStyles = ({
   currentColorScheme,
   shared,
   locked,
   isFavorite,
   isEditing,
-  isPin,
-}) => css`
-  ${commonIconsStyles}
-  svg {
-    path {
-      fill: ${(shared || locked || isFavorite || isEditing || isPin) &&
-      currentColorScheme.accentColor};
-    }
-  }
+}) =>
+  currentColorScheme
+    ? css`
+        ${commonIconsStyles}
+        svg {
+          path {
+            fill: ${(shared || locked || isFavorite || isEditing) &&
+            currentColorScheme.accentColor};
+          }
+        }
 
-  &:hover {
-    svg {
-      path {
-        fill: ${currentColorScheme.accentColor};
-      }
-    }
-  }
-`;
+        &:hover {
+          svg {
+            path {
+              fill: ${currentColorScheme.accentColor};
+            }
+          }
+        }
+      `
+    : isEditing
+    ? css`
+        ${StyledEditIcon}
+      `
+    : css`
+        ${IconButton} {
+          ${commonIconsStyles}
+        }
+      `;
 
 export default styled(IconButton)(getDefaultStyles);
