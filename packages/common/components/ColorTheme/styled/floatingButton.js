dd751ef7f622d5de73abe94a3636b6c545de5988
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-17 10:24:41 +0300

    Web:Common: Fix floatingButton styles.

--- packages/common/components/ColorTheme/styled/floatingButton.js
+++ packages/common/components/ColorTheme/styled/floatingButton.js
@@ -5,21 +5,30 @@ import {
 } from "@docspace/common/components/FloatingButton/StyledFloatingButton";
 import Base from "@docspace/components/themes/base";
 
-const getDefaultStyles = ({ currentColorScheme, color, theme }) => css`
-  background: ${color
-    ? color
-    : theme.isBase === true
-    ? currentColorScheme.accentColor
-    : theme.floatingButton.backgroundColor};
+const getDefaultStyles = ({ currentColorScheme, color, icon, theme }) =>
+  currentColorScheme &&
+  css`
+    background: ${
+      color
+        ? color
+        : theme.isBase
+        ? currentColorScheme.accentColor
+        : icon === "upload"
+        ? theme.floatingButton.backgroundColor
+        : currentColorScheme.accentColor
+    } !important;
 
-  ${StyledFloatingButton} {
-    background: ${color
-      ? color
-      : theme.isBase === true
-      ? currentColorScheme.accentColor
-      : theme.floatingButton.backgroundColor};
-  }
-`;
+    ${StyledFloatingButton} {
+      background: ${
+        color
+          ? color
+          : theme.isBase
+          ? currentColorScheme.accentColor
+          : icon === "upload"
+          ? theme.floatingButton.backgroundColor
+          : currentColorScheme.accentColor
+      } !important;
+  `;
 
 StyledCircleWrap.defaultProps = { theme: Base };
 
