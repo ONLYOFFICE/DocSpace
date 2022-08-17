f790f970af1fd0f2ad12ece624f53b8b05646e1b
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-17 11:54:01 +0300

    Web:Common: Returned a stylish VersionBadge.

--- packages/client/src/pages/VersionHistory/Section/Body/StyledVersionHistory.js
+++ packages/client/src/pages/VersionHistory/Section/Body/StyledVersionHistory.js
@@ -281,6 +281,19 @@ StyledVersionRow.defaultProps = { theme: Base };
 
 const StyledVersionSvg = styled(VersionSvg)`
   path {
+    fill: ${(props) =>
+      !props.isVersion
+        ? props.theme.filesVersionHistory.badge.defaultFill
+        : props.index === 0
+        ? props.theme.filesVersionHistory.badge.fill
+        : props.theme.filesVersionHistory.badge.badgeFill};
+    stroke: ${(props) =>
+      !props.isVersion
+        ? props.theme.filesVersionHistory.badge.stroke
+        : props.index === 0
+        ? props.theme.filesVersionHistory.badge.fill
+        : props.theme.filesVersionHistory.badge.badgeFill};
+
     stroke-dasharray: ${(props) => (props.isVersion ? "2 0" : "3 1")};
     stroke-linejoin: ${(props) => (props.isVersion ? "unset" : "round")};
 
