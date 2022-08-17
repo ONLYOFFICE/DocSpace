166d47c3596cb15ede1e3f6bf4c1c6fdf6b4b87f
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-17 15:51:14 +0300

    Web:Common:Fix SubmenuItemLabel theme.

--- packages/common/components/ColorTheme/styled/submenuItemLabel.js
+++ packages/common/components/ColorTheme/styled/submenuItemLabel.js
@@ -2,12 +2,14 @@ import styled, { css } from "styled-components";
 
 import { StyledSubmenuItemLabel } from "@docspace/components/submenu/styled-submenu";
 
-const getDefaultStyles = ({ currentColorScheme, isActive }) => css`
-  background-color: ${isActive ? currentColorScheme.accentColor : "none"};
+const getDefaultStyles = ({ currentColorScheme, isActive }) =>
+  currentColorScheme &&
+  css`
+    background-color: ${isActive ? currentColorScheme.accentColor : "none"};
 
-  &:hover {
-    background-color: ${isActive && currentColorScheme.accentColor};
-  }
-`;
+    &:hover {
+      background-color: ${isActive && currentColorScheme.accentColor};
+    }
+  `;
 
 export default styled(StyledSubmenuItemLabel)(getDefaultStyles);
