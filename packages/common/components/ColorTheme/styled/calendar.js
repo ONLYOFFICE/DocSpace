eead02f968b5b7ba42b31f5634ba3990f57b1c9f
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-17 11:14:30 +0300

    Web:Common: Fix calendar.

--- packages/common/components/ColorTheme/styled/calendar.js
+++ packages/common/components/ColorTheme/styled/calendar.js
@@ -1,13 +1,15 @@
 import styled, { css } from "styled-components";
 import { CalendarStyle } from "@docspace/components/calendar/styled-calendar";
 
-const getDefaultStyles = ({ currentColorScheme, color }) => css`
-  .calendar-month_selected-day {
-    background-color: ${color ? color : currentColorScheme.accentColor};
-    &:hover {
+const getDefaultStyles = ({ currentColorScheme, color }) =>
+  currentColorScheme &&
+  css`
+    .calendar-month_selected-day {
       background-color: ${color ? color : currentColorScheme.accentColor};
+      &:hover {
+        background-color: ${color ? color : currentColorScheme.accentColor};
+      }
     }
-  }
-`;
+  `;
 
 export default styled(CalendarStyle)(getDefaultStyles);
