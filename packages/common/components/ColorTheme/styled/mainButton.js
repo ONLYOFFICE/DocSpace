b5c8b1f95353a52341a1de3d069a94f30df6c26a
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-17 15:52:16 +0300

    Web:Common:Fix MainButton theme.

--- packages/common/components/ColorTheme/styled/mainButton.js
+++ packages/common/components/ColorTheme/styled/mainButton.js
@@ -1,19 +1,26 @@
 import styled, { css } from "styled-components";
 import { StyledMainButton } from "@docspace/components/main-button/styled-main-button";
+import Base from "@docspace/components/themes/base";
 
-const getDefaultStyles = ({ currentColorScheme, isDisabled }) => css`
-  background: ${currentColorScheme.accentColor};
+const getDefaultStyles = ({ currentColorScheme, isDisabled, theme }) =>
+  currentColorScheme &&
+  css`
+    background-color: ${currentColorScheme.accentColor};
 
-  &:hover {
-    background: ${currentColorScheme.accentColor};
-    cursor: pointer;
-  }
+    &:hover {
+      background-color: ${currentColorScheme.accentColor};
+      opacity: ${!isDisabled && "0.85"};
+      cursor: pointer;
+    }
 
-  &:active {
-    background: ${currentColorScheme.accentColor};
-    opacity: ${!isDisabled && "0.6"};
-    cursor: pointer;
-  }
-`;
+    &:active {
+      background-color: ${currentColorScheme.accentColor};
+      opacity: ${!isDisabled && "1"};
+      filter: ${theme.isBase ? "brightness(90%)" : "brightness(82%)"};
+      cursor: pointer;
+    }
+  `;
+
+StyledMainButton.defaultProps = { theme: Base };
 
 export default styled(StyledMainButton)(getDefaultStyles);
