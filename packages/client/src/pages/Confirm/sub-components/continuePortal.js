8a3b6e1284fb41f7e222f9dc343f0155a7e2bde3
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-08-19 23:05:16 +0300

    Confirm: fix portal continue

--- packages/client/src/pages/Confirm/sub-components/continuePortal.js
+++ packages/client/src/pages/Confirm/sub-components/continuePortal.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router";
-import { withTranslation } from "react-i18next";
+import { Trans, withTranslation } from "react-i18next";
 import { inject, observer } from "mobx-react";
 import Section from "@docspace/common/components/Section";
 import Text from "@docspace/components/text";
 import Button from "@docspace/components/button";
+import Link from "@docspace/components/link";
 import toastr from "@docspace/components/toast/toastr";
 import { continuePortal } from "@docspace/common/api/portal";
 import {
@@ -21,10 +22,13 @@ import DocspaceLogo from "../../../DocspaceLogo";
 
 const ContinuePortal = (props) => {
   const { t, greetingTitle, linkData, history } = props;
+  const [isReactivate, setIsReactivate] = useState(false);
 
   const onRestoreClick = async () => {
     try {
       await continuePortal(linkData.confirmHeader);
+      setIsReactivate(true);
+      setTimeout(() => (location.href = "/"), 10000);
     } catch (e) {
       toastr.error(e);
     }
@@ -45,24 +49,36 @@ const ContinuePortal = (props) => {
         </StyledHeader>
 
         <FormWrapper>
-          <Text className="subtitle">{t("PortalContinueTitle")}</Text>
-          <ButtonsWrapper>
-            <Button
-              primary
-              scale
-              size="medium"
-              label={t("Reactivate")}
-              tabIndex={1}
-              onClick={onRestoreClick}
-            />
-            <Button
-              scale
-              size="medium"
-              label={t("Common:Cancel")}
-              tabIndex={1}
-              onClick={onCancelClick}
-            />
-          </ButtonsWrapper>
+          {isReactivate ? (
+            <Trans t={t} i18nKey="SuccessReactivate" ns="Confirm">
+              Your account has been successfully reactivated. In 10 seconds you
+              will be redirected to the
+              <Link isHovered href="/">
+                portal
+              </Link>
+            </Trans>
+          ) : (
+            <>
+              <Text className="subtitle">{t("PortalContinueTitle")}</Text>
+              <ButtonsWrapper>
+                <Button
+                  primary
+                  scale
+                  size="medium"
+                  label={t("Reactivate")}
+                  tabIndex={1}
+                  onClick={onRestoreClick}
+                />
+                <Button
+                  scale
+                  size="medium"
+                  label={t("Common:Cancel")}
+                  tabIndex={1}
+                  onClick={onCancelClick}
+                />
+              </ButtonsWrapper>
+            </>
+          )}
         </FormWrapper>
       </StyledBody>
     </StyledPage>
