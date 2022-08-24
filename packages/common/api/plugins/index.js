051c9e207a1ecbb1e60bc5611e2bd5811da8ea57
TimofeyBoyko <fognir742@gmail.com>
2022-08-24 12:32:26 +0300

    Web:Common:API: move api for plugins from client to common

---
+++ packages/common/api/plugins/index.js
@@ -0,0 +1,37 @@
+export const getPlugins = async () => {
+  const plugins = await (
+    await fetch("http://localhost:8092/api/2.0/plugins")
+  ).json();
+
+  console.log(plugins);
+
+  return plugins;
+};
+
+export const activatePlugin = async (id) => {
+  const plugin = await (
+    await fetch(`http://localhost:8092/api/2.0/plugins/activate/${id}`, {
+      method: "PUT",
+    })
+  ).json();
+
+  return plugin;
+};
+
+export const uploadPlugin = async (formData) => {
+  const plugin = await (
+    await fetch("http://localhost:8092/api/2.0/plugins/upload", {
+      method: "POST",
+      body: formData,
+    })
+  ).json();
+
+  return plugin;
+};
+
+export const deletePlugin = async (id) => {
+  await fetch(`http://localhost:8092/api/2.0/plugins/delete/${id}`, {
+    method: "DELETE",
+    body: {},
+  });
+};
