ea113737d0ae9bd2bda8ae602c7a14838e82f19b
TimofeyBoyko <fognir742@gmail.com>
2022-08-24 12:36:12 +0300

    Common:ASC.WebPlugins: change port from 3000 to 5014

--- common/ASC.WebPlugins/src/main.ts
+++ common/ASC.WebPlugins/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(5014);
 }
 bootstrap();
