a4a0aa5d2bf017604ebd1b50baa47c84e09707dc
TimofeyBoyko <fognir742@gmail.com>
2022-08-25 10:53:14 +0300

    Plugins: disable CORS

--- common/ASC.WebPlugins/src/main.ts
+++ common/ASC.WebPlugins/src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: false });
+  // app.enableCors();
   await app.listen(5014);
 }
 bootstrap();
