44ad3d4d9091b01c3b03fb7ef58768105188e3f4
TimofeyBoyko <fognir742@gmail.com>
2022-08-25 12:17:05 +0300

    Plugins: add validate by uniq file name

--- common/ASC.WebPlugins/src/utils/file-filter.ts
+++ common/ASC.WebPlugins/src/utils/file-filter.ts
@@ -1,5 +1,17 @@
+import * as fs from "fs";
+import * as path from "path";
+
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "text/javascript") return cb(null, true);
+  const pluginsDir = path.join(
+    __dirname,
+    "../../../../../../../public/scripts"
+  );
+
+  var files = fs.readdirSync(pluginsDir);
+
+  const isUniqName = !files.includes(file.originalname);
+
+  if (file.mimetype === "text/javascript" && isUniqName) return cb(null, true);
 
   return cb(null, false);
 };
