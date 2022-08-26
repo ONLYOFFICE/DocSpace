146dafd96de1442c066c8f2c1e4079cb7a2d91c3
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-08-27 01:54:43 +0300

    Web: Login: modified render

--- packages/login/src/server/lib/helpers/render-app.tsx
+++ packages/login/src/server/lib/helpers/render-app.tsx
@@ -7,14 +7,15 @@ import { I18nextProvider, I18nextProviderProps } from "react-i18next";
 
 const renderApp = (
   i18n: I18nextProviderProps,
-  initialState: IInitialState
+  initialState: IInitialState,
+  Component: React.FC<IInitialState>
 ): { component: string; styleTags: string } => {
   const sheet = new ServerStyleSheet();
   const component = renderToString(
     sheet.collectStyles(
       <I18nextProvider i18n={i18n}>
         <GlobalStyle />
-        <Login {...initialState} />
+        <Component {...initialState} />
       </I18nextProvider>
     )
   );
