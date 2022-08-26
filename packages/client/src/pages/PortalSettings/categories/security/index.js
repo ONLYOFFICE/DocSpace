ac53f438d7c396f98a976e373da4eaf56b8613f2
Dmitry Sychugov <dmitry.sychugov@onlyoffice.com>
2022-08-26 19:13:13 +0500

    Web: Client: added login/audit history data

--- packages/client/src/pages/PortalSettings/categories/security/index.js
+++ packages/client/src/pages/PortalSettings/categories/security/index.js
@@ -10,8 +10,10 @@ import config from "PACKAGE_FILE";
 import AccessRights from "./access-rights/index.js";
 import AccessPortal from "./access-portal/index.js";
 import SecurityLoader from "./sub-components/loaders/security-loader";
+import LoginHistory from "./login-history/index.js";
 import MobileSecurityLoader from "./sub-components/loaders/mobile-security-loader";
 import AccessLoader from "./sub-components/loaders/access-loader";
+import AuditTrail from "./audit-trail/index.js";
 
 import { isMobile } from "react-device-detect";
 
@@ -30,6 +32,16 @@ const SecurityWrapper = (props) => {
       id: "access-rights",
       name: t("Common:AccessRights"),
       content: <AccessRights />,
+    },
+    {
+      id: "login-history",
+      name: t("LoginHistoryTitle"),
+      content: <LoginHistory />,
+    },
+    {
+      id: "audit-trail",
+      name: t("AuditTrailNav"),
+      content: <AuditTrail />,
     },
   ];
 
