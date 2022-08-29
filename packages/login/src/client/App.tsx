9bfc07250805d354f4eebb2b87cfc9c0aabf2563
Artem Tarasov <artem.tarasov@onlyoffice.com>
2022-08-30 00:01:08 +0300

    Web: Login: added routing

--- packages/login/src/client/App.tsx
+++ packages/login/src/client/App.tsx
@@ -1,49 +1,21 @@
 import React from "react";
-import { useSSR } from "react-i18next";
-import { fonts } from "@docspace/common/fonts";
-import GlobalStyle from "./components/GlobalStyle";
-import { inject, observer, Provider as MobxProvider } from "mobx-react";
-import ThemeProvider from "@docspace/components/theme-provider";
-import store from "client/store";
-import { I18nextProvider } from "react-i18next";
-import i18n from "./i18n";
 import Login from "./components/Login";
-import toastr from "@docspace/components/toast/toastr";
-import ErrorBoundary from "./components/ErrorBoundary";
-
-const ThemeProviderWrapper = inject(({ auth }) => {
-  const { settingsStore } = auth;
-  return { theme: settingsStore.theme };
-})(observer(ThemeProvider));
+import { Switch, Route } from "react-router-dom";
+import InvalidRoute from "./components/Invalid";
 
 interface ILoginProps extends IInitialState {
-  initialLanguage: string;
-  initialI18nStoreASC: any;
-  isDesktopEditor: boolean;
+  isDesktopEditor?: boolean;
 }
-const App: React.FC<ILoginProps> = ({
-  initialLanguage,
-  initialI18nStoreASC,
-  isDesktopEditor,
-  ...rest
-}) => {
-  useSSR(initialI18nStoreASC, initialLanguage);
-
-  const onError = (errorInfo: any) => {
-    toastr.error(errorInfo);
-  };
-
+const App: React.FC<ILoginProps> = (props) => {
   return (
-    <ErrorBoundary onError={onError}>
-      <MobxProvider {...store}>
-        <I18nextProvider i18n={i18n}>
-          <ThemeProviderWrapper>
-            <GlobalStyle fonts={fonts} />
-            <Login {...rest} isDesktopEditor={isDesktopEditor} />
-          </ThemeProviderWrapper>
-        </I18nextProvider>
-      </MobxProvider>
-    </ErrorBoundary>
+    <Switch>
+      <Route path="/login" exact>
+        <Login {...props} />
+      </Route>
+      <Route path="/login/error=:error">
+        <InvalidRoute />
+      </Route>
+    </Switch>
   );
 };
 
--- packages/login/src/server/lib/helpers/render-app.tsx
+++ packages/login/src/server/lib/helpers/render-app.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import { ServerStyleSheet } from "styled-components";
-import GlobalStyle from "../../../client/components/GlobalStyle";
-import Login from "../../../client/components/Login";
+// import GlobalStyle from "../../../client/components/GlobalStyle";
+// import Login from "../../../client/components/Login";
+import App from "../../../client/App";
 import { renderToString } from "react-dom/server";
 import { I18nextProvider, I18nextProviderProps } from "react-i18next";
-
+import { StaticRouter } from "react-router-dom";
+import GlobalStyle from "../../../client/components/GlobalStyle";
 const renderApp = (
   i18n: I18nextProviderProps,
   initialState: IInitialState,
-  Component: React.FC<IInitialState>
+  url: string
 ): { component: string; styleTags: string } => {
   const sheet = new ServerStyleSheet();
+
   const component = renderToString(
     sheet.collectStyles(
-      <I18nextProvider i18n={i18n}>
-        <GlobalStyle />
-        <Component {...initialState} />
-      </I18nextProvider>
+      <StaticRouter location={url} context={{}}>
+        <I18nextProvider i18n={i18n}>
+          <GlobalStyle />
+          <App {...initialState} />
+        </I18nextProvider>
+      </StaticRouter>
     )
   );
+
   const styleTags = sheet.getStyleTags();
 
   return { component, styleTags };
