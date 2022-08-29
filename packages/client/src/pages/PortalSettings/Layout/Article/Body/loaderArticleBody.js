67a6c0225ddd2c0a17d5972448c004f6764d8dca
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-08-29 13:48:38 +0300

    Web:Client:Fix styles loader ArticleBody.

--- packages/client/src/pages/PortalSettings/Layout/Article/Body/loaderArticleBody.js
+++ packages/client/src/pages/PortalSettings/Layout/Article/Body/loaderArticleBody.js
@@ -1,37 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Loaders from "@docspace/common/components/Loaders";
 import { isTablet as isTabletUtils } from "@docspace/components/utils/device";
 import { isTablet } from "react-device-detect";
 
 const StyledLoader = styled.div`
-  padding: 0px 16px 0px;
+  padding-left: 8px;
 
   .loader {
-    display: block;
+    padding-bottom: 12px;
+  }
+
+  .section-name-container {
+    padding: 1px 3px 29px;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  .section-name {
     width: 20px;
-    padding-bottom: 16px;
+    height: 1px;
+    line-height: 1px;
+    background: #d0d5da;
+    margin: 0px;
   }
 
   @media (min-width: 1024px) {
-    padding: 0px 20px 0px;
+    padding-left: 10px;
+    padding-top: 20px;
+    display: flex;
+    flex-direction: column;
+
+    .section-name-loader {
+      margin-bottom: 12px;
+    }
 
     .loader {
-      width: 216px;
-      padding-bottom: 12px;
+      padding-bottom: 16px;
     }
   }
 `;
 
 const LoaderArticleBody = () => {
-  const heightLoader = isTabletUtils() || isTablet ? "20px" : "24px";
+  const [isTabletView, setIsTabletView] = useState(false);
+
+  const checkInnerWidth = () => {
+    const isTabletView = window.innerWidth <= 1024 || isTablet;
+
+    if (isTabletView) {
+      setIsTabletView(true);
+    } else {
+      setIsTabletView(false);
+    }
+  };
+
+  useEffect(() => {
+    checkInnerWidth();
+    window.addEventListener("resize", checkInnerWidth);
+
+    return () => window.removeEventListener("resize", checkInnerWidth);
+  });
+
+  const height = isTabletView ? "28px" : "20px";
+  const width = isTabletView ? "28px" : "187px";
+
   return (
     <StyledLoader>
-      <Loaders.Rectangle height={heightLoader} className="loader" />
-      <Loaders.Rectangle height={heightLoader} className="loader" />
-      <Loaders.Rectangle height={heightLoader} className="loader" />
-      <Loaders.Rectangle height={heightLoader} className="loader" />
-      <Loaders.Rectangle height={heightLoader} className="loader" />
+      {isTabletView ? (
+        <div className="section-name-container">
+          <p className="section-name"></p>
+        </div>
+      ) : (
+        <Loaders.Rectangle
+          width={"42px"}
+          height={"12px"}
+          className="section-name-loader"
+        />
+      )}
+      <Loaders.Rectangle width={width} height={height} className="loader" />
+      <Loaders.Rectangle width={width} height={height} className="loader" />
+      <Loaders.Rectangle width={width} height={height} className="loader" />
+      <Loaders.Rectangle width={width} height={height} className="loader" />
+      <Loaders.Rectangle width={width} height={height} className="loader" />
+      <Loaders.Rectangle width={width} height={height} className="loader" />
+      <Loaders.Rectangle width={width} height={height} className="loader" />
     </StyledLoader>
   );
 };
