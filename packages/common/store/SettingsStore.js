1078c7f138c3ddf13430ad766eeaadfbaeb76b10
gopienkonikita <nikita.gopienko@onlyoffice.com>
2022-08-29 11:27:13 +0300

    Web: Common: fixed getOforms

--- packages/common/store/SettingsStore.js
+++ packages/common/store/SettingsStore.js
@@ -61,7 +61,7 @@ class SettingsStore {
   enabledJoin = false;
   urlLicense = "https://gnu.org/licenses/gpl-3.0.html";
   urlSupport = "https://helpdesk.onlyoffice.com/";
-  urlOforms = "https://cmsoforms.onlyoffice.com/api/oforms?populate=*&locale=";
+  urlOforms = "https://cmsoforms.onlyoffice.com/api/oforms";
 
   logoUrl = combineUrl(proxyURL, "/static/images/logo.docspace.react.svg");
   customNames = {
