58f4cf277451bd88911e716cb130b6a318f59d05
mushka <nikitamushka@mail.ru>
2022-09-01 04:34:31 +0300

    redo if body/room component

--- packages/client/src/pages/Home/InfoPanel/Body/views/Room/index.js
+++ packages/client/src/pages/Home/InfoPanel/Body/views/Room/index.js
@@ -4,8 +4,7 @@ import History from "./History";
 import Members from "./Members";
 
 const Room = ({
-  selectedItems,
-  selectedFolder,
+  selection,
   roomState,
 
   defaultProps,
@@ -13,19 +12,12 @@ const Room = ({
   historyProps,
   detailsProps,
 }) => {
-  const selectedItem =
-    selectedItems.length === 0
-      ? selectedFolder
-      : selectedItems.length === 1
-      ? selectedItems[0]
-      : selectedItems;
-
   return roomState === "members" ? (
-    <Members selectedItem={selectedItem} {...defaultProps} {...membersProps} />
+    <Members selectedItem={selection} {...defaultProps} {...membersProps} />
   ) : roomState === "history" ? (
-    <History selectedItem={selectedItem} {...defaultProps} {...historyProps} />
+    <History selectedItem={selection} {...defaultProps} {...historyProps} />
   ) : (
-    <Info selectedItem={selectedItem} {...defaultProps} {...detailsProps} />
+    <Info selection={selection} {...defaultProps} {...detailsProps} />
   );
 };
 
