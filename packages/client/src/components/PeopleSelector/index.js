3befde4df23c9e3e4c11141263c85d58321095bf
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2022-09-05 18:16:44 +0300

    Web: Client: PeopleSelector: Added base people selector with new selector body

--- packages/client/src/components/PeopleSelector/index.js
+++ packages/client/src/components/PeopleSelector/index.js
@@ -1,329 +1,178 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { inject, observer } from "mobx-react";
 import PropTypes from "prop-types";
 import { I18nextProvider, withTranslation } from "react-i18next";
 import i18n from "./i18n";
-import AdvancedSelector from "@docspace/common/components/AdvancedSelector";
-import { getUserList } from "@docspace/common/api/people";
-import { getGroupList } from "@docspace/common/api/groups";
+
+import Selector from "@docspace/components/selector";
 import Filter from "@docspace/common/api/people/filter";
+
 import UserTooltip from "./sub-components/UserTooltip";
 
-class PeopleSelector extends React.Component {
-  constructor(props) {
-    super(props);
+import { getUserList } from "@docspace/common/api/people";
+import Loaders from "@docspace/common/components/Loaders";
 
-    this.state = {
-      options: [],
-      groups: [],
-      total: 0,
-      page: 0,
-      hasNextPage: true,
-      isNextPageLoading: false,
-      isFirstLoad: true,
-    };
-  }
+const PeopleSelector = ({
+  acceptButtonLabel,
+  accessRights,
+  cancelButtonLabel,
+  className,
+  emptyScreenDescription,
+  emptyScreenHeader,
+  emptyScreenImage,
+  headerLabel,
+  id,
+  isMultiSelect,
+  items,
+  onAccept,
+  onAccessRightsChange,
+  onBackClick,
+  onCancel,
+  onSelect,
+  onSelectAll,
+  searchEmptyScreenDescription,
+  searchEmptyScreenHeader,
+  searchEmptyScreenImage,
+  searchPlaceholder,
+  selectAllIcon,
+  selectAllLabel,
+  selectedAccessRight,
+  selectedItems,
+  style,
+  t,
+  withAccessRights,
+  withCancelButton,
+  withSelectAll,
+  filter,
+}) => {
+  const [itemsList, setItemsList] = useState(items);
+  const [searchValue, setSearchValue] = useState("");
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(0);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [isNextPageLoading, setIsNextPageLoading] = useState(false);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
 
-  componentDidMount() {
-    const { groupList, useFake, t } = this.props;
+  useEffect(() => {
+    setIsFirstLoad(true);
+    loadNextPage(0);
+  }, []);
 
-    if (!groupList) {
-      getGroupList(useFake)
-        .then((groups) =>
-          this.setState({
-            groups: [
-              {
-                key: "all",
-                id: "all",
-                label: `${t("AllUsers")}`,
-                total: 0,
-                selectedCount: 0,
-              },
-            ].concat(this.convertGroups(groups)),
-          })
-        )
-        .catch((error) => console.log(error));
-    } else {
-      this.setState({
-        groups: [
-          {
-            key: "all",
-            id: "all",
-            label: `${t("AllUsers")}`,
-            total: 0,
-            selectedCount: 0,
-          },
-        ].concat(groupList),
-      });
-    }
-  }
-
-  convertGroups = (groups) => {
-    return groups
-      ? groups.map((g) => {
-          return {
-            key: g.id,
-            id: g.id,
-            label: g.name,
-            total: 0,
-            selectedCount: 0,
-          };
-        })
-      : [];
-  };
-
-  convertUser = (u) => {
+  const toListItem = (item) => {
     return {
-      key: u.id,
-      groups: u.groups && u.groups.length ? u.groups.map((g) => g.id) : [],
-      label: u.displayName,
-      email: u.email,
-      position: u.title,
-      avatarUrl: u.avatar,
+      id: item.id,
+      avatar: item.avatar,
+      icon: item.icon,
+      label: item.displayName,
     };
   };
 
-  convertUsers = (users) => {
-    return users ? users.map(this.convertUser) : [];
-  };
-
-  loadNextPage = ({ startIndex, searchValue, currentGroup }) => {
-    // console.log(
-    //   `loadNextPage(startIndex=${startIndex}, searchValue="${searchValue}", currentGroup="${currentGroup}")`
-    // );
-
+  const loadNextPage = (startIndex, search) => {
     const pageCount = 100;
 
-    this.setState(
-      { isNextPageLoading: true, isFirstLoad: startIndex === 0 },
-      () => {
-        const { role, employeeStatus, useFake } = this.props;
+    setIsNextPageLoading(true);
 
-        const filter = Filter.getDefault();
-        filter.page = startIndex / pageCount;
-        filter.pageCount = pageCount;
+    const filter = filter || Filter.getDefault();
+    filter.page = startIndex / pageCount;
+    filter.pageCount = pageCount;
 
-        if (searchValue) {
-          filter.search = searchValue;
-        }
+    if (search) {
+      filter.search = search;
+    }
 
-        if (employeeStatus) {
-          filter.employeeStatus = employeeStatus;
-        }
+    getUserList(filter)
+      .then((response) => {
+        let newItems = startIndex ? itemsList : [];
 
-        if (role) {
-          filter.role = role;
-        }
-        if (employeeStatus) {
-          filter.employeeStatus = employeeStatus;
-        }
+        const items = response.items.map((item) => toListItem(item));
 
-        if (currentGroup && currentGroup !== "all") filter.group = currentGroup;
+        newItems = [...newItems, ...items];
 
-        const { defaultOption, defaultOptionLabel } = this.props;
+        setHasNextPage(newItems.length < response.total);
+        setItemsList(newItems);
+        setTotal(response.total);
 
-        getUserList(filter, useFake)
-          .then((response) => {
-            let newOptions = startIndex ? [...this.state.options] : [];
+        setIsNextPageLoading(false);
+        isFirstLoad && setIsFirstLoad(false);
+      })
+      .catch((error) => console.log(error));
+  };
 
-            if (defaultOption) {
-              const inGroup =
-                !currentGroup ||
-                currentGroup === "all" ||
-                (defaultOption.groups &&
-                  defaultOption.groups.filter((g) => g.id === currentGroup)
-                    .length > 0);
+  const onSearch = (value) => {
+    setSearchValue(value);
+    loadNextPage(0, value);
+  };
 
-              if (searchValue) {
-                const exists = response.items.find(
-                  (item) => item.id === defaultOption.id
-                );
+  const setDefaultTranslations = () => {
+    headerLabel = headerLabel || t("ListAccounts");
+    searchPlaceholder = searchPlaceholder || t("Common:Search");
+    acceptButtonLabel = acceptButtonLabel || t("PeopleTranslations:AddMembers");
+    cancelButtonLabel = cancelButtonLabel || t("Common:CancelButton");
+    selectAllLabel = selectAllLabel || t("AllAccounts");
+    selectAllIcon =
+      selectAllIcon || "/static/images/catalog.accounts.react.svg";
+    emptyScreenImage =
+      emptyScreenImage || "/static/images/empty_screen_persons.png";
+    emptyScreenHeader = emptyScreenHeader || t("EmptyHeader");
+    emptyScreenDescription = emptyScreenDescription || t("EmptyDescription");
+    searchEmptyScreenImage =
+      searchEmptyScreenImage || "/static/images/empty_screen_persons.png";
+    searchEmptyScreenHeader = searchEmptyScreenHeader || t("SearchEmptyHeader");
+    searchEmptyScreenDescription =
+      searchEmptyScreenDescription || t("SearchEmptyDescription");
+  };
 
-                if (exists && inGroup) {
-                  newOptions.push(
-                    this.convertUser({
-                      ...defaultOption,
-                      displayName: defaultOptionLabel,
-                    })
-                  );
-                }
-              } else if (!startIndex && response.items.length > 0 && inGroup) {
-                newOptions.push(
-                  this.convertUser({
-                    ...defaultOption,
-                    displayName: defaultOptionLabel,
-                  })
-                );
-              }
+  setDefaultTranslations();
 
-              newOptions = newOptions.concat(
-                this.convertUsers(
-                  response.items.filter((item) => item.id !== defaultOption.id)
-                )
-              );
-            } else {
-              newOptions = newOptions.concat(this.convertUsers(response.items));
-            }
-
-            this.setState({
-              hasNextPage: newOptions.length < response.total,
-              isNextPageLoading: false,
-              options: newOptions,
-              total: response.total,
-            });
-          })
-          .catch((error) => console.log(error));
+  return (
+    <Selector
+      id={id}
+      className={className}
+      style={style}
+      headerLabel={headerLabel}
+      onBackClick={onBackClick}
+      searchPlaceholder={searchPlaceholder}
+      searchValue={searchValue}
+      onSearch={onSearch}
+      items={itemsList}
+      isMultiSelect={isMultiSelect}
+      selectedItems={selectedItems}
+      acceptButtonLabel={acceptButtonLabel}
+      onAccept={onAccept}
+      withSelectAll={withSelectAll}
+      selectAllLabel={selectAllLabel}
+      selectAllIcon={selectAllIcon}
+      withAccessRights={withAccessRights}
+      accessRights={accessRights}
+      selectedAccessRight={selectedAccessRight}
+      withCancelButton={withCancelButton}
+      cancelButtonLabel={cancelButtonLabel}
+      onCancel={onCancel}
+      emptyScreenImage={emptyScreenImage}
+      emptyScreenHeader={emptyScreenHeader}
+      emptyScreenDescription={emptyScreenDescription}
+      searchEmptyScreenImage={searchEmptyScreenImage}
+      searchEmptyScreenHeader={searchEmptyScreenHeader}
+      searchEmptyScreenDescription={searchEmptyScreenDescription}
+      hasNextPage={hasNextPage}
+      isNextPageLoading={isNextPageLoading}
+      loadNextPage={loadNextPage}
+      totalItems={total}
+      isLoading={isFirstLoad}
+      searchLoader={<Loaders.SelectorSearchLoader />}
+      rowLoader={
+        <Loaders.SelectorRowLoader
+          isMultiSelect={false}
+          isContainer={isFirstLoad}
+        />
       }
-    );
-  };
-
-  getOptionTooltipContent = (index) => {
-    if (!index) return null;
-
-    const { options } = this.state;
-
-    const user = options[+index];
-
-    if (!user) return null;
-
-    // console.log("onOptionTooltipShow", index, user);
-
-    const { defaultOption, theme } = this.props;
-
-    const label =
-      defaultOption && defaultOption.id === user.key
-        ? defaultOption.displayName
-        : user.label;
-
-    return (
-      <UserTooltip
-        theme={theme}
-        avatarUrl={user.avatarUrl}
-        label={label}
-        email={user.email}
-        position={user.position}
-      />
-    );
-  };
-
-  onSearchChanged = () => {
-    //console.log("onSearchChanged")(value);
-    this.setState({ options: [], hasNextPage: true, isFirstLoad: true });
-  };
-
-  onGroupChanged = () => {
-    //console.log("onGroupChanged")(group);
-    this.setState({ options: [], hasNextPage: true, isFirstLoad: true });
-  };
-
-  render() {
-    const {
-      options,
-      groups,
-      hasNextPage,
-      isNextPageLoading,
-      total,
-      isFirstLoad,
-    } = this.state;
-
-    const {
-      id,
-      className,
-      style,
-      isOpen,
-      isMultiSelect,
-      isDisabled,
-      onSelect,
-      size,
-      onCancel,
-      t,
-      searchPlaceHolderLabel,
-      withoutAside,
-      embeddedComponent,
-      selectedOptions,
-      showCounter,
-      smallSectionWidth,
-      theme,
-      onArrowClick,
-      headerLabel,
-    } = this.props;
-
-    // console.log("CustomAllGroups", t("CustomAllGroups", { groupsCaption }));
-
-    // console.log("PeopleSelector render");
-    return (
-      <AdvancedSelector
-        theme={theme}
-        id={id}
-        className={className}
-        style={style}
-        options={options}
-        groups={groups}
-        hasNextPage={hasNextPage}
-        isNextPageLoading={isNextPageLoading}
-        smallSectionWidth={smallSectionWidth}
-        loadNextPage={this.loadNextPage}
-        size={size}
-        selectedOptions={selectedOptions}
-        isOpen={isOpen}
-        isMultiSelect={isMultiSelect}
-        isDisabled={isDisabled}
-        searchPlaceHolderLabel={
-          searchPlaceHolderLabel || t("SearchUsersPlaceholder")
-        }
-        isDefaultDisplayDropDown={false}
-        selectButtonLabel={t("PeopleTranslations:AddMembers")}
-        emptySearchOptionsLabel={t("EmptySearchUsersResult")}
-        emptyOptionsLabel={t("EmptyUsers")}
-        onSelect={onSelect}
-        onSearchChanged={this.onSearchChanged}
-        onGroupChanged={this.onGroupChanged}
-        onCancel={onCancel}
-        withoutAside={withoutAside}
-        embeddedComponent={embeddedComponent}
-        showCounter={showCounter}
-        onArrowClick={onArrowClick}
-        headerLabel={headerLabel ? headerLabel : `${t("Common:AddUsers")}`}
-        total={total}
-        isFirstLoad={isFirstLoad}
-      />
-    );
-  }
-}
-
-PeopleSelector.propTypes = {
-  id: PropTypes.string,
-  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-  style: PropTypes.object,
-  isOpen: PropTypes.bool,
-  onSelect: PropTypes.func,
-  onCancel: PropTypes.func,
-  useFake: PropTypes.bool,
-  isMultiSelect: PropTypes.bool,
-  isDisabled: PropTypes.bool,
-  defaultOption: PropTypes.object,
-  defaultOptionLabel: PropTypes.string,
-  size: PropTypes.oneOf(["full", "compact"]),
-  language: PropTypes.string,
-  t: PropTypes.func,
-  groupsCaption: PropTypes.string,
-  searchPlaceHolderLabel: PropTypes.string,
-  role: PropTypes.oneOf(["admin", "user", "guest"]),
-  employeeStatus: PropTypes.any,
-  withoutAside: PropTypes.bool,
-  embeddedComponent: PropTypes.any,
+    />
+  );
 };
 
-PeopleSelector.defaultProps = {
-  useFake: false,
-  size: "full",
-  language: "en",
-  role: null,
-  employeeStatus: null,
-  defaultOption: null,
-  defaultOptionLabel: "Me",
-  withoutAside: false,
-};
+PeopleSelector.propTypes = {};
+
+PeopleSelector.defaultProps = {};
 
 const ExtendedPeopleSelector = inject(({ auth }) => {
   return { theme: auth.settingsStore.theme };
