52ed0d5607b035200f9f7ca66fb216f391342b20
mushka <nikitamushka@mail.ru>
2022-09-06 23:38:56 +0300

    updated InfoPanelMemberListLoader

--- packages/common/components/Loaders/InfoPanelBodyLoader/InfoPanelMemberListLoader.js
+++ packages/common/components/Loaders/InfoPanelBodyLoader/InfoPanelMemberListLoader.js
@@ -4,14 +4,26 @@ import styled from "styled-components";
 import RectangleLoader from "../RectangleLoader";
 
 const StyledInfoPanelMemberListLoader = styled.div`
+  width: 100%;
+
   display: flex;
   flex-direction: column;
-
   justify-content: center;
   align-items: start;
 `;
 
-const StyledInfoPanelMemberLoader = styled.div`
+const StyledSubtitleLoader = styled.div`
+  width: 100%;
+  padding: 8px 0 16px 0;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledMemberLoader = styled.div`
+  width: 100%;
   height: 48px;
 
   display: flex;
@@ -19,24 +31,56 @@ const StyledInfoPanelMemberLoader = styled.div`
   justify-content: start;
   align-items: center;
   gap: 8px;
+
+  .avatar {
+    min-height: 32px;
+    min-width: 32px;
+  }
 `;
 
 const InfoPanelMemberListLoader = () => {
   return (
     <StyledInfoPanelMemberListLoader>
-      {[...Array(5).keys()].map((i) => (
-        <StyledInfoPanelMemberLoader key={i}>
+      <StyledSubtitleLoader>
+        <RectangleLoader width={"100px"} height={"20px"} borderRadius={"3px"} />
+        <RectangleLoader width={"16px"} height={"16px"} borderRadius={"3px"} />
+      </StyledSubtitleLoader>
+
+      {[...Array(4).keys()].map((i) => (
+        <StyledMemberLoader key={i}>
           <RectangleLoader
+            className="avatar"
             width={"32px"}
             height={"32px"}
             borderRadius={"50%"}
           />
           <RectangleLoader
-            width={"128px"}
-            height={"16px"}
+            width={"100%"}
+            height={"20px"}
             borderRadius={"3px"}
           />
-        </StyledInfoPanelMemberLoader>
+        </StyledMemberLoader>
+      ))}
+
+      <StyledSubtitleLoader>
+        <RectangleLoader width={"100px"} height={"20px"} borderRadius={"3px"} />
+        <RectangleLoader width={"16px"} height={"16px"} borderRadius={"3px"} />
+      </StyledSubtitleLoader>
+
+      {[...Array(2).keys()].map((i) => (
+        <StyledMemberLoader key={i}>
+          <RectangleLoader
+            className="avatar"
+            width={"32px"}
+            height={"32px"}
+            borderRadius={"50%"}
+          />
+          <RectangleLoader
+            width={"100%"}
+            height={"20px"}
+            borderRadius={"3px"}
+          />
+        </StyledMemberLoader>
       ))}
     </StyledInfoPanelMemberListLoader>
   );
