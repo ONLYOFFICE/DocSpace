c82f4210b6b1501b1d2bcb47f87133ba6fe24448
mushka <nikitamushka@mail.ru>
2022-09-07 01:15:43 +0300

    implemented new info renaming changes

---
+++ packages/client/src/pages/Home/InfoPanel/Body/helpers/ViewHelper.js
@@ -0,0 +1,26 @@
+class ViewHelper {
+  constructor(props) {
+    this.t = props.t;
+    this.item = props.selectedItem;
+    this.getContextOptions = props.getContextOptions;
+    this.getContextOptionActions = props.getContextOptionActions;
+
+    this.fixItemContextOptions();
+  }
+
+  fixItemContextOptions = () => {
+    let newContextOptions = this.item.contextOptions;
+
+    if (!newContextOptions)
+      newContextOptions = this.getContextOptions(this.item, false);
+    newContextOptions = newContextOptions.filter((co) => co !== "show-info");
+
+    this.item.contextOptions = newContextOptions;
+  };
+
+  getItemContextOptions = () => {
+    return this.getContextOptionActions(this.item, this.t);
+  };
+}
+
+export default ViewHelper;
