e8d538618e3537927e0bc119b49bdc4e5eefe982
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-09-07 07:35:43 +0300

    Client: Dialogs: AvatarEditorDialog: init preview

---
+++ packages/client/src/components/dialogs/AvatarEditorDialog/editor/preview.js
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "styled-components";
+
+import Avatar from "@docspace/components/avatar";
+import Text from "@docspace/components/text";
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  .avatar {
+    height: 124px;
+    width: 124px;
+    align-self: center;
+  }
+
+  .name-preview {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    padding: 8px;
+    border: 1px solid #eceef1;
+    border-radius: 6px;
+  }
+`;
+
+const AvatarPreview = ({ avatar, userName }) => {
+  return (
+    <StyledWrapper>
+      <Avatar
+        className="avatar"
+        size="max"
+        role="user"
+        source={avatar}
+        userName={userName}
+        editing={false}
+      />
+      <div className="name-preview">
+        <Avatar
+          size="min"
+          role="user"
+          source={avatar}
+          userName={userName}
+          editing={false}
+        />
+        <Text fontWeight={600} fontSize="15px" isTruncate>
+          {userName}
+        </Text>
+      </div>
+    </StyledWrapper>
+  );
+};
+
+export default AvatarPreview;
