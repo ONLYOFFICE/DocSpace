ba7b9900e1c591df71e5a366e099033e3ebd957b
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-09-07 19:21:54 +0300

    Web:Client:Add companyInfoSettingsData in AboutContent.

--- packages/client/src/pages/About/AboutContent.js
+++ packages/client/src/pages/About/AboutContent.js
@@ -52,132 +52,149 @@ const StyledAboutBody = styled.div`
 `;
 
 const AboutContent = (props) => {
-  const { personal, buildVersionInfo, theme } = props;
+  const { personal, buildVersionInfo, theme, companyInfoSettingsData } = props;
   const { t } = useTranslation("About");
   const license = "AGPL-3.0";
   const linkAppServer = "https://github.com/ONLYOFFICE/AppServer";
   const linkDocs = "https://github.com/ONLYOFFICE/DocumentServer";
-  const phone = "+371 660-16425";
-  const email = "support@onlyoffice.com";
-  const address =
-    "20A-12 Ernesta Birznieka-Upisha street, Riga, Latvia, EU, LV-1050";
 
   return (
-    <StyledAboutBody>
-      <div className="avatar">
-        {personal ? (
-          <ReactSVG
-            src="/images/logo_personal_about.svg"
-            className="logo-theme no-select"
-          />
-        ) : (
-          <img
-            src={
-              theme.isBase
-                ? "/images/dark_general.png"
-                : "/images/white_general.png"
-            }
-            alt="Logo"
-            className="no-select"
-          />
-        )}
-      </div>
+    companyInfoSettingsData && (
+      <StyledAboutBody>
+        <div className="avatar">
+          {personal ? (
+            <ReactSVG
+              src="/images/logo_personal_about.svg"
+              className="logo-theme no-select"
+            />
+          ) : (
+            <img
+              src={
+                theme.isBase
+                  ? "/images/dark_general.png"
+                  : "/images/white_general.png"
+              }
+              alt="Logo"
+              className="no-select"
+            />
+          )}
+        </div>
 
-      <div className="row">
-        <Text className="row-el" fontSize="13px" noSelect>
-          {t("DocumentManagement")}:
-        </Text>
-        <ColorTheme
-          {...props}
-          tag="a"
-          themeId={ThemeType.Link}
-          className="row-el"
-          fontSize="13px"
-          fontWeight="600"
-          href={linkAppServer}
-          target="_blank"
-        >
-          &nbsp;ONLYOFFICE DocSpace&nbsp;
-        </ColorTheme>
+        <div className="row">
+          <Text className="row-el" fontSize="13px" noSelect>
+            {t("DocumentManagement")}:
+          </Text>
+          <ColorTheme
+            {...props}
+            tag="a"
+            themeId={ThemeType.Link}
+            className="row-el"
+            fontSize="13px"
+            fontWeight="600"
+            href={linkAppServer}
+            target="_blank"
+          >
+            &nbsp;ONLYOFFICE DocSpace&nbsp;
+          </ColorTheme>
 
-        <Text className="row-el select-el" fontSize="13px" fontWeight="600">
-          v.{buildVersionInfo.appServer}
-        </Text>
-      </div>
+          <Text className="row-el select-el" fontSize="13px" fontWeight="600">
+            v.{buildVersionInfo.appServer}
+          </Text>
+        </div>
 
-      <div className="row">
-        <Text className="row-el" fontSize="13px" noSelect>
-          {t("OnlineEditors")}:
-        </Text>
-        <ColorTheme
-          {...props}
-          tag="a"
-          themeId={ThemeType.Link}
-          className="row-el"
-          fontSize="13px"
-          fontWeight="600"
-          href={linkDocs}
-          target="_blank"
-        >
-          &nbsp;ONLYOFFICE Docs&nbsp;
-        </ColorTheme>
-        <Text className="row-el select-el" fontSize="13px" fontWeight="600">
-          v.{buildVersionInfo.documentServer}
-        </Text>
-      </div>
+        <div className="row">
+          <Text className="row-el" fontSize="13px" noSelect>
+            {t("OnlineEditors")}:
+          </Text>
+          <ColorTheme
+            {...props}
+            tag="a"
+            themeId={ThemeType.Link}
+            className="row-el"
+            fontSize="13px"
+            fontWeight="600"
+            href={linkDocs}
+            target="_blank"
+          >
+            &nbsp;ONLYOFFICE Docs&nbsp;
+          </ColorTheme>
+          <Text className="row-el select-el" fontSize="13px" fontWeight="600">
+            v.{buildVersionInfo.documentServer}
+          </Text>
+        </div>
 
-      <div className="row">
-        <Text className="row-el" fontSize="13px" noSelect>
-          {t("SoftwareLicense")}:{" "}
-        </Text>
-        <Text className="row-el" fontSize="13px" fontWeight="600" noSelect>
-          &nbsp;{license}
-        </Text>
-      </div>
+        <div className="row">
+          <Text className="row-el" fontSize="13px" noSelect>
+            {t("SoftwareLicense")}:{" "}
+          </Text>
+          <Text className="row-el" fontSize="13px" fontWeight="600" noSelect>
+            &nbsp;{license}
+          </Text>
+        </div>
 
-      <Text className="copyright" fontSize="14px" fontWeight="600" noSelect>
-        © Ascensio System SIA
-      </Text>
-
-      <div className="row">
-        <Text className="address-title" fontSize="13px" noSelect>
-          {t("AboutCompanyAddressTitle")}:{" "}
-        </Text>
-        <Text className="address-title select-el" fontSize="13px">
-          {address}
-        </Text>
-      </div>
-
-      <div className="row">
-        <Text className="tel-title" fontSize="13px" noSelect>
-          {t("AboutCompanyTelTitle")}:{" "}
-        </Text>
-        <Text className="tel-title select-el" fontSize="13px">
-          {phone}
-        </Text>
-      </div>
-
-      <div className="row">
-        <Text className="row-el" fontSize="13px" noSelect>
-          {t("AboutCompanyEmailTitle")}:
+        <Text className="copyright" fontSize="14px" fontWeight="600" noSelect>
+          © Ascensio System SIA
         </Text>
 
-        <ColorTheme
-          {...props}
-          tag="a"
-          themeId={ThemeType.Link}
-          className="row-el"
-          fontSize="13px"
-          fontWeight="600"
-          href={`mailto:${email}`}
-        >
-          &nbsp;{email}
-        </ColorTheme>
-      </div>
-    </StyledAboutBody>
+        <div className="row">
+          <Text className="address-title" fontSize="13px" noSelect>
+            {t("AboutCompanyAddressTitle")}:{" "}
+          </Text>
+          <Text className="address-title select-el" fontSize="13px">
+            {companyInfoSettingsData.address}
+          </Text>
+        </div>
+
+        <div className="row">
+          <Text className="tel-title" fontSize="13px" noSelect>
+            {t("AboutCompanyTelTitle")}:{" "}
+          </Text>
+          <Text className="tel-title select-el" fontSize="13px">
+            {companyInfoSettingsData.phone}
+          </Text>
+        </div>
+
+        <div className="row">
+          <Text className="row-el" fontSize="13px" noSelect>
+            {t("AboutCompanyEmailTitle")}:
+          </Text>
+
+          <ColorTheme
+            {...props}
+            tag="a"
+            themeId={ThemeType.Link}
+            className="row-el"
+            fontSize="13px"
+            fontWeight="600"
+            href={`mailto:${companyInfoSettingsData.email}`}
+          >
+            &nbsp;{companyInfoSettingsData.email}
+          </ColorTheme>
+        </div>
+
+        <div className="row">
+          <ColorTheme
+            {...props}
+            tag="a"
+            themeId={ThemeType.Link}
+            className="row-el"
+            fontSize="13px"
+            fontWeight="600"
+            target="_blank"
+            href={companyInfoSettingsData.site}
+          >
+            {companyInfoSettingsData.site.split("/").pop()}
+          </ColorTheme>
+        </div>
+      </StyledAboutBody>
+    )
   );
 };
 
 export default inject(({ auth }) => {
-  return { theme: auth.settingsStore.theme };
+  const { settingsStore } = auth;
+
+  const { theme, companyInfoSettingsData } = settingsStore;
+
+  return { theme, companyInfoSettingsData };
 })(observer(AboutContent));
