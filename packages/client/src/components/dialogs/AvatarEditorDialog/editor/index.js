72989939fb1f7e7e34c12e21181934227c666740
Viktor Fomin <viktor.fomin@onlyoffice.com>
2022-09-07 07:36:06 +0300

    Client: Dialogs: AvatarEditorDialog: init editor

---
+++ packages/client/src/components/dialogs/AvatarEditorDialog/editor/index.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import AvatarCropper from "./avatar-cropper";
+import AvatarPreview from "./preview";
+import Dropzone from "./dropzone";
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+
+  .avatar-editor {
+    display: flex;
+    gap: 16px;
+    align-items: center;
+  }
+`;
+
+const AvatarEditor = ({
+  t,
+  profile,
+  avatar,
+  onChangeAvatar,
+  preview,
+  setPreview,
+}) => {
+  const setUploadedFile = (file) =>
+    onChangeAvatar({ ...avatar, uploadedFile: file });
+
+  return (
+    <StyledWrapper>
+      <div className="avatar-editor">
+        <AvatarCropper
+          t={t}
+          avatar={avatar}
+          onChangeAvatar={onChangeAvatar}
+          uploadedFile={avatar.uploadedFile}
+          setUploadedFile={setUploadedFile}
+          setPreviewAvatar={setPreview}
+        />
+        <AvatarPreview avatar={preview} userName={profile.displayName} />
+      </div>
+      <Dropzone t={t} setUploadedFile={setUploadedFile} />
+    </StyledWrapper>
+  );
+};
+
+export default AvatarEditor;
--- packages/client/src/components/dialogs/AvatarEditorDialog/index.js
+++ packages/client/src/components/dialogs/AvatarEditorDialog/index.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useTranslation } from "react-i18next";
 
 import ModalDialog from "@docspace/components/modal-dialog";
 import Text from "@docspace/components/text";
 import Button from "@docspace/components/button";
 
+import AvatarEditor from "./editor";
+
 const StyledModalDialog = styled(ModalDialog)``;
 
 const AvatarEditorDialog = (props) => {
-  const { t, visible, onClose, onSave } = props;
+  const { t } = useTranslation(["Profile", "Common"]);
+  const { visible, onClose, profile } = props;
+  const [avatar, setAvatar] = useState({
+    uploadedFile: profile.avatarMax,
+    x: 0.5,
+    y: 0.5,
+    zoom: 1,
+  });
+  const [preview, setPreview] = useState(null);
+
+  const onChangeAvatar = (newAvatar) => setAvatar(newAvatar);
+
+  const onSaveClick = () => {
+    console.log("onSave");
+  };
 
   return (
     <StyledModalDialog
@@ -23,7 +40,16 @@ const AvatarEditorDialog = (props) => {
           {t("EditPhoto")}
         </Text>
       </ModalDialog.Header>
-      <ModalDialog.Body></ModalDialog.Body>
+      <ModalDialog.Body>
+        <AvatarEditor
+          t={t}
+          avatar={avatar}
+          onChangeAvatar={onChangeAvatar}
+          preview={preview}
+          setPreview={setPreview}
+          profile={profile}
+        />
+      </ModalDialog.Body>
       <ModalDialog.Footer>
         <Button
           key="AvatarEditorSaveBtn"
@@ -31,7 +57,7 @@ const AvatarEditorDialog = (props) => {
           size="normal"
           scale
           primary={true}
-          onClick={onSave}
+          onClick={onSaveClick}
         />
         <Button
           key="AvatarEditorCloseBtn"
