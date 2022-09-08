61d40ae745896ffc2140a75a50308c10f8f5c3b8
mushka <nikitamushka@mail.ru>
2022-09-08 04:31:53 +0300

    restuctured noItem view

--- packages/client/src/pages/Home/InfoPanel/Body/views/NoItem/index.js
+++ packages/client/src/pages/Home/InfoPanel/Body/views/NoItem/index.js
@@ -1,39 +1,12 @@
 import React from "react";
-import styled from "styled-components";
-import { withTranslation } from "react-i18next";
+import NoGalleryItem from "./NoGalleryItem";
+import NoRoomItem from "./NoRoomItem";
+import NoFileOrFolderItem from "./NoFileOrFolderItem";
 
-const EmptyScreenContainer = styled.div`
-  margin: 80px auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  gap: 32px;
-
-  .empty-screen-text {
-    font-family: "Open Sans";
-    font-style: normal;
-    font-weight: 600;
-    font-size: 14px;
-    line-height: 16px;
-    text-align: center;
-  }
-`;
-
-const NoItem = ({ t }) => {
-  return (
-    <EmptyScreenContainer>
-      <div className="no-thumbnail-img-wrapper">
-        <img
-          size="96px"
-          className="no-thumbnail-img"
-          src="images/empty_screen_info_panel.png"
-        />
-      </div>
-
-      <div className="empty-screen-text">{t("EmptyScreenText")}</div>
-    </EmptyScreenContainer>
-  );
+const NoItem = ({ t, selection, isGallery }) => {
+  if (isGallery) return <NoGalleryItem t={t} />;
+  if (selection.isRoom) return <NoRoomItem t={t} />;
+  return <NoFileOrFolderItem t={t} />;
 };
 
-export default withTranslation(["InfoPanel"])(NoItem);
+export default NoItem;
