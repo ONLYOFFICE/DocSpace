8c77b5a5c083013368894f94751dcd7f32f726d1
Elyor Djalilov <edjalilov.dev@gmail.com>
2022-09-08 11:48:40 +0500

    some fix with style

--- packages/components/input-phone/index.js
+++ packages/components/input-phone/index.js
@@ -1,35 +1,52 @@
 import { useState } from "react";
 import ReactPhoneInput from "react-phone-input-2";
+import styled from "styled-components";
 import "react-phone-input-2/lib/style.css";
 
 export const InputPhone = () => {
   const [phoneValue, setPhoneValue] = useState("");
 
+  const Wrapper = styled.div`
+    margin: 50px;
+    width: 330px;
+    border-radius: 3px;
+    &:focus-within {
+      outline: 1px solid #2da7db;
+    }
+  `;
+
   return (
-    <ReactPhoneInput
-      inputProps={{
-        name: "phone",
-        required: true,
-        autoFocus: true,
-      }}
-      value={phoneValue}
-      onChange={(phone) => setPhoneValue(phone)}
-      country="ru"
-      buttonStyle={{ background: "#fff", width: "50px" }}
-      inputStyle={{
-        width: "320px",
-        height: "44px",
-        marginLeft: "10px",
-        borderRadius: "3px",
-        fontSize: "16px",
-      }}
-      searchPlaceholder="Search"
-      containerStyle={{ marginTop: "15px" }}
-      searchStyle={{ margin: "0", width: "97%", height: "30px" }}
-      enableSearch
-      disableSearchIcon
-      placeholder="XXX XXX-XX-XX"
-      defaultErrorMessage="Сountry code is invalid"
-    />
+    <Wrapper>
+      <ReactPhoneInput
+        inputProps={{
+          name: "phone",
+          required: true,
+          autoFocus: true,
+        }}
+        value={phoneValue}
+        onChange={(phone) => setPhoneValue(phone)}
+        country={"ru"}
+        buttonStyle={{ background: "#fff", width: "50px" }}
+        inputStyle={{
+          width: "320px",
+          height: "44px",
+          marginLeft: "10px",
+          borderRadius: "3px",
+          fontSize: "16px",
+        }}
+        searchPlaceholder="Search"
+        containerStyle={{ marginTop: "15px" }}
+        searchStyle={{
+          margin: "0",
+          width: "97%",
+          height: "30px",
+          borderColor: "#2da7db",
+        }}
+        enableSearch
+        disableSearchIcon
+        placeholder="XXX XXX-XX-XX"
+        defaultErrorMessage="Сountry code is invalid"
+      />
+    </Wrapper>
   );
 };
