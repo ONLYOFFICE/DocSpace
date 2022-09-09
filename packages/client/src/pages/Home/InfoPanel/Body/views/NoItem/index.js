14df6e788b55a5fc53e436cffa56b6ec51de0d65
mushka <nikitamushka@mail.ru>
2022-09-10 00:29:26 +0300

    slight update on Noitem view

--- packages/client/src/pages/Home/InfoPanel/Body/views/NoItem/index.js
+++ packages/client/src/pages/Home/InfoPanel/Body/views/NoItem/index.js
@@ -3,10 +3,11 @@ import NoGalleryItem from "./NoGalleryItem";
 import NoRoomItem from "./NoRoomItem";
 import NoFileOrFolderItem from "./NoFileOrFolderItem";
 
-const NoItem = ({ t, selection, isGallery }) => {
+const NoItem = ({ t, isGallery, isRoomCategory, isFileCategory }) => {
+  if (isFileCategory) return <NoFileOrFolderItem t={t} />;
+  if (isRoomCategory) return <NoRoomItem t={t} />;
   if (isGallery) return <NoGalleryItem t={t} />;
-  if (selection.isRoom) return <NoRoomItem t={t} />;
-  return <NoFileOrFolderItem t={t} />;
+  return null;
 };
 
 export default NoItem;
