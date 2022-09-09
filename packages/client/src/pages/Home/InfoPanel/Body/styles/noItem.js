16e4fb6a2baa21198081199e4b2094209c9654e8
mushka <nikitamushka@mail.ru>
2022-09-10 00:23:14 +0300

    created new noItem file for no-item styled components

---
+++ packages/client/src/pages/Home/InfoPanel/Body/styles/noItem.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const StyledNoItemContainer = styled.div`
+  margin: 80px auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 32px;
+
+  .no-item-text {
+    font-family: "Open Sans";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 16px;
+    text-align: center;
+  }
+`;
+
+export { StyledNoItemContainer };
