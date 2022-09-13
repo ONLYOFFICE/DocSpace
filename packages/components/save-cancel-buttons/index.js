8cd48f12f4055ddcaabd63913cda12a02773505e
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-09-13 10:37:26 +0300

    Web:Components:Change props disableRestoreToDefault in SaveCancelButtons

--- packages/components/save-cancel-buttons/index.js
+++ packages/components/save-cancel-buttons/index.js
@@ -44,7 +44,7 @@ class SaveCancelButtons extends React.Component {
       saveButtonLabel,
       cancelButtonLabel,
       hasScroll,
-      isFirstRestoreToDefault,
+      disableRestoreToDefault,
       className,
       id,
       isSaving,
@@ -53,8 +53,8 @@ class SaveCancelButtons extends React.Component {
 
     const cancelButtonDisabled = cancelEnable
       ? false
-      : isFirstRestoreToDefault === "true"
-      ? false
+      : typeof disableRestoreToDefault === "boolean"
+      ? disableRestoreToDefault
       : !showReminder;
 
     return (
@@ -114,7 +114,7 @@ SaveCancelButtons.propTypes = {
   displaySettings: PropTypes.bool,
   hasScroll: PropTypes.bool,
   minwidth: PropTypes.string,
-  isFirstRestoreToDefault: PropTypes.string,
+  disableRestoreToDefault: PropTypes.bool,
   isSaving: PropTypes.bool,
   cancelEnable: PropTypes.bool,
 };
