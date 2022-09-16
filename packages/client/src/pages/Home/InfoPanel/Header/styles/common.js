2ad81335269107c0d8c264e09583c4489151f76b
mushka <nikitamushka@mail.ru>
2022-09-16 15:07:21 +0300

    changed styles in InfoPanel header

---
+++ packages/client/src/pages/Home/InfoPanel/Header/styles/common.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+import { Base } from "@docspace/components/themes";
+import { tablet } from "@docspace/components/utils/device";
+
+const getHeaderHeight = ({ withSubmenu, isTablet }) => {
+  let res = isTablet ? 53 : 69;
+  if (withSubmenu) res += 32;
+  return `${res}px`;
+};
+
+const getMainHeight = ({ withSubmenu, isTablet }) => {
+  let res = isTablet ? 52 : 68;
+  if (withSubmenu) res += 1;
+  return `${res}px`;
+};
+
+const StyledInfoPanelHeader = styled.div`
+  width: 100%;
+  max-width: 100%;
+
+  height: ${(props) => getHeaderHeight(props)};
+  min-height: ${(props) => getHeaderHeight(props)};
+  @media ${tablet} {
+    height: ${(props) => getHeaderHeight({ ...props, isTablet: true })};
+    min-height: ${(props) => getHeaderHeight({ ...props, isTablet: true })};
+  }
+
+  display: flex;
+  flex-direction: column;
+  border-bottom: ${(props) =>
+    props.withSubmenu
+      ? "none"
+      : `1px solid ${props.theme.infoPanel.borderColor}`};
+  .main {
+    height: ${(props) => getMainHeight(props)};
+    min-height: ${(props) => getMainHeight(props)};
+    @media ${tablet} {
+      height: ${(props) => getMainHeight({ ...props, isTablet: true })};
+      min-height: ${(props) => getMainHeight({ ...props, isTablet: true })};
+    }
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    .header-text {
+      margin-left: 20px;
+    }
+  }
+  .submenu {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    .sticky {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+`;
+
+const StyledInfoPanelToggleWrapper = styled.div`
+  display: flex;
+
+  @media ${tablet} {
+    display: none;
+  }
+
+  align-items: center;
+  justify-content: center;
+  padding-right: 20px;
+
+  .info-panel-toggle-bg {
+    height: 32px;
+    width: 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    background-color: ${(props) =>
+      props.theme.infoPanel.sectionHeaderToggleBgActive};
+
+    path {
+      fill: ${(props) => props.theme.infoPanel.sectionHeaderToggleIconActive};
+    }
+  }
+`;
+
+StyledInfoPanelHeader.defaultProps = { theme: Base };
+StyledInfoPanelToggleWrapper.defaultProps = { theme: Base };
+
+export { StyledInfoPanelHeader, StyledInfoPanelToggleWrapper };
