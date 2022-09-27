e4e6ce6232d63e9a21ec5ca40b1b084974c296f2
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2022-09-27 10:12:47 +0300

    Web: Fixed position of number.

--- packages/client/src/pages/PreparationPortal/StyledPreparationPortal.js
+++ packages/client/src/pages/PreparationPortal/StyledPreparationPortal.js
@@ -4,13 +4,11 @@ import { Base } from "@docspace/components/themes";
 
 const StyledBodyPreparationPortal = styled.div`
   margin-bottom: 24px;
-  // display: flex;
   width: 100%;
   max-width: ${(props) => (props.errorMessage ? "560px" : "480px")};
   padding: 0 24px;
   box-sizing: border-box;
   align-items: center;
-  position: relative;
 
   .preparation-portal_progress {
     display: flex;
@@ -18,9 +16,7 @@ const StyledBodyPreparationPortal = styled.div`
     position: relative;
     .preparation-portal_progress-bar {
       border-radius: 2px;
-   
       width: 100%;
-
       height: 24px;
       background-color: #f3f4f4;
     }
@@ -35,16 +31,14 @@ const StyledBodyPreparationPortal = styled.div`
     }
     .preparation-portal_percent {
       position: absolute;
-
       ${(props) => props.percent > 50 && "color: white"};
       top: 2px;
-      left: 50%;
+      left: calc(50% - 9px);
     }
   }
 
   .preparation-portal_text {
     text-align: center;
-
     color: ${(props) => props.theme.text.disableColor};
   }
 `;
