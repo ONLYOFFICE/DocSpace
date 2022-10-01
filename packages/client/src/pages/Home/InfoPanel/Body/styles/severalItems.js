ee91e430c9e26212040911b3d2af1eda13c23591
mushka <nikitamushka@mail.ru>
2022-10-02 02:29:28 +0300

    created new severalItems.js file for styles

---
+++ packages/client/src/pages/Home/InfoPanel/Body/styles/severalItems.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledSeveralItemsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  img {
+  }
+`;
+
+export { StyledSeveralItemsContainer };
