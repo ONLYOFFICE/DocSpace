419646f8fcb0fecd290374b828a2a4c51c807d12
mushka <nikitamushka@mail.ru>
2022-10-02 02:44:09 +0300

    complete overhaul for InfoPanelHeaderLoader

--- packages/common/components/Loaders/InfoPanelHeaderLoader/InfoPanelHeaderLoader.js
+++ packages/common/components/Loaders/InfoPanelHeaderLoader/InfoPanelHeaderLoader.js
@@ -1,36 +1,61 @@
 import React from "react";
-import {
-  StyledInfoPanelHeader,
-  StyledInfoPanelToggleWrapper,
-} from "./StyledInfoPanelHeaderLoader";
+import { StyledInfoPanelHeader } from "./StyledInfoPanelHeaderLoader";
 import RectangleLoader from "../RectangleLoader";
 
+import { isMobile as isMobileRDD } from "react-device-detect";
 import {
-  isTablet,
+  isDesktop as isDesktopUtils,
   isMobile as isMobileUtils,
-  isDesktop,
+  isTablet as isTabletUtils,
 } from "@docspace/components/utils/device";
-import { isMobile } from "react-device-detect";
 
 const InfoPanelHeaderLoader = () => {
-  const customRectangleLoader = (width, height, rounded) => (
-    <RectangleLoader
-      width={"" + width}
-      height={"" + height}
-      borderRadius={"" + rounded}
-    />
-  );
+  const isTablet =
+    isTabletUtils() || isMobileUtils() || isMobileRDD || !isDesktopUtils();
+
+  const pathname = window.location.pathname.toLowerCase();
+  const isGallery = pathname.indexOf("form-gallery") !== -1;
+  const isRooms = pathname.indexOf("rooms") !== -1;
+  const isAccounts =
+    pathname.indexOf("accounts") !== -1 && !(pathname.indexOf("view") !== -1);
+  const withSubmenu = !isGallery && !isAccounts;
 
   return (
-    <StyledInfoPanelHeader>
-      <div className="header-text">{customRectangleLoader(200, 28, 3)}</div>
-      <StyledInfoPanelToggleWrapper
-        isRootFolder={true}
-        isInfoPanelVisible={true}
-      >
-        {!(isTablet() || isMobile || isMobileUtils() || !isDesktop()) &&
-          customRectangleLoader(32, 32, "50%")}
-      </StyledInfoPanelToggleWrapper>
+    <StyledInfoPanelHeader isTablet={isTablet} withSubmenu={false}>
+      <div className="main">
+        <RectangleLoader width={"120px"} height={"24px"} borderRadius={"3px"} />
+        {!isTablet && (
+          <div className="info-panel-toggle-bg">
+            <RectangleLoader
+              width={"32px"}
+              height={"32px"}
+              borderRadius={"50%"}
+            />
+          </div>
+        )}
+      </div>
+
+      {/* {withSubmenu && (
+        <div className="submenu">
+          <RectangleLoader
+            width={"60px"}
+            height={"20px"}
+            borderRadius={"3px"}
+          />
+          <RectangleLoader
+            width={"60px"}
+            height={"20px"}
+            borderRadius={"3px"}
+          />
+          {isRooms && (
+            <RectangleLoader
+              width={"60px"}
+              height={"20px"}
+              borderRadius={"3px"}
+            />
+          )}
+        </div>
+      )} */}
     </StyledInfoPanelHeader>
   );
 };
