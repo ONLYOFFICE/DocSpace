9a8d8856e2337df4655d30bb112cc672864684f7
mushka <nikitamushka@mail.ru>
2022-10-02 02:37:06 +0300

    added accounts to noItem view and added own translation import

--- packages/client/src/pages/Home/InfoPanel/Body/views/NoItem/index.js
+++ packages/client/src/pages/Home/InfoPanel/Body/views/NoItem/index.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { withTranslation } from "react-i18next";
+
+import withLoader from "@docspace/client/src/HOCs/withLoader";
+import Loaders from "@docspace/common/components/Loaders/index.js";
+
 import NoGalleryItem from "./NoGalleryItem";
 import NoRoomItem from "./NoRoomItem";
 import NoFileOrFolderItem from "./NoFileOrFolderItem";
+import NoAccountsItem from "./NoAccountsItem";
 
-const NoItem = ({ t, isGallery, isRoomCategory, isFileCategory }) => {
+const NoItem = ({ t, isAccounts, isGallery, isRooms, isFiles }) => {
+  if (isAccounts) return <NoAccountsItem t={t} />;
   if (isGallery) return <NoGalleryItem t={t} />;
-  if (isFileCategory) return <NoFileOrFolderItem t={t} />;
-  if (isRoomCategory) return <NoRoomItem t={t} />;
+  if (isFiles) return <NoFileOrFolderItem t={t} />;
+  if (isRooms) return <NoRoomItem t={t} />;
   return null;
 };
 
-export default NoItem;
+export default withTranslation(["InfoPanel", "FormGallery"])(
+  withLoader(NoItem)(<Loaders.InfoPanelViewLoader view="noItem" />)
+);
