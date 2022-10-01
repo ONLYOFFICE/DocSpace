7755fdf012bfeb03b4cba4e09abd2ebfd27bf4c1
mushka <nikitamushka@mail.ru>
2022-10-02 02:35:21 +0300

    updated styling and fetching algorighm for history

--- packages/client/src/pages/Home/InfoPanel/Body/views/History/index.js
+++ packages/client/src/pages/Home/InfoPanel/Body/views/History/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { withTranslation } from "react-i18next";
 
+import withLoader from "@docspace/client/src/HOCs/withLoader";
 import {
   StyledHistoryBlock,
   StyledHistoryList,
@@ -8,11 +10,11 @@ import {
 
 import Avatar from "@docspace/components/avatar";
 import Text from "@docspace/components/text";
-import { getUser } from "@docspace/common/api/people";
 import { parseAndFormatDate } from "../../helpers/DetailsHelper";
 import HistoryBlockMessage from "./HistoryBlockMessage";
 import HistoryBlockItemList from "./HistoryBlockItemList";
 import Loaders from "@docspace/common/components/Loaders";
+import HistoryBlockUser from "./HistoryBlockUser";
 
 const History = ({
   t,
@@ -23,25 +25,26 @@ const History = ({
   getItemIcon,
 
   getHistory,
-  openFileAction,
+  openLocationAction,
 }) => {
   const [history, setHistory] = useState(null);
   const [showLoader, setShowLoader] = useState(false);
 
-  const parseHistoryJSON = async (fetchedHistory) => {
+  const parseHistoryJSON = (fetchedHistory) => {
     let feeds = fetchedHistory.feeds;
     let newFeeds = [];
     for (let i = 0; i < feeds.length; i++) {
       const feedsJSON = JSON.parse(feeds[i].json);
-      feedsJSON.author = await getUser(feedsJSON.AuthorId);
 
       let newGroupedFeeds = [];
       if (feeds[i].groupedFeeds) {
         let groupFeeds = feeds[i].groupedFeeds;
-        for (let j = 0; j < groupFeeds.length; j++) {
-          const groupFeedsJSON = JSON.parse(groupFeeds[j].json);
-          newGroupedFeeds.push(groupFeedsJSON);
-        }
+        for (let j = 0; j < groupFeeds.length; j++)
+          newGroupedFeeds.push(
+            !!groupFeeds[j].target
+              ? groupFeeds[j].target
+              : JSON.parse(groupFeeds[j].json)
+          );
       }
 
       newFeeds.push({
@@ -63,7 +66,7 @@ const History = ({
     if (history) timerId = setTimeout(() => setShowLoader(true), 1500);
 
     let fetchedHistory = await getHistory(module, itemId);
-    fetchedHistory = await parseHistoryJSON(fetchedHistory);
+    fetchedHistory = parseHistoryJSON(fetchedHistory);
 
     clearTimeout(timerId);
 
@@ -84,29 +87,33 @@ const History = ({
 
   if (!history || showLoader)
     return <Loaders.InfoPanelViewLoader view="history" />;
+
   return (
     <>
       <StyledHistoryList>
         <StyledHistorySubtitle>{t("RecentActivities")}</StyledHistorySubtitle>
 
         {history.feeds.map((feed) => (
-          <StyledHistoryBlock key={feed.json.Id}>
+          <StyledHistoryBlock
+            key={feed.json.Id}
+            isUserAction={feed.json.Item === "sharedRoom" && feed.target}
+          >
             <Avatar
               role="user"
               className="avatar"
               size="min"
               source={
-                feed.json.author.avatar ||
-                (feed.json.author.displayName
+                feed.initiator.avatarSmall ||
+                (feed.initiator.displayName
                   ? ""
-                  : feed.json.author.email && "/static/images/@.react.svg")
+                  : feed.initiator.email && "/static/images/@.react.svg")
               }
-              userName={feed.json.author.displayName}
+              userName={feed.initiator.displayName}
             />
             <div className="info">
               <div className="title">
-                <Text className="name">{feed.json.author.displayName}</Text>
-                {feed.json.author.isOwner && (
+                <Text className="name">{feed.initiator.displayName}</Text>
+                {feed.initiator.isOwner && (
                   <Text className="secondary-info">
                     {t("Common:Owner").toLowerCase()}
                   </Text>
@@ -122,16 +129,29 @@ const History = ({
 
               <HistoryBlockMessage
                 t={t}
+                className="message"
                 action={feed.json}
                 groupedActions={feed.groupedFeeds}
               />
 
-              <HistoryBlockItemList
-                t={t}
-                items={[feed.json, ...feed.groupedFeeds]}
-                getItemIcon={getItemIcon}
-                openFileAction={openFileAction}
-              />
+              {(feed.json.Item === "file" || feed.json.Item === "folder") && (
+                <HistoryBlockItemList
+                  t={t}
+                  items={[feed.json, ...feed.groupedFeeds]}
+                  getItemIcon={getItemIcon}
+                  openLocationAction={openLocationAction}
+                />
+              )}
+
+              {feed.json.Item === "sharedRoom" &&
+                feed.target &&
+                [feed.target, ...feed.groupedFeeds].map((user, i) => (
+                  <HistoryBlockUser
+                    key={user.id}
+                    user={user}
+                    withComma={i !== feed.groupedFeeds.length}
+                  />
+                ))}
             </div>
           </StyledHistoryBlock>
         ))}
@@ -140,4 +160,6 @@ const History = ({
   );
 };
 
-export default History;
+export default withTranslation(["InfoPanel", "Common", "Translations"])(
+  withLoader(History)(<Loaders.InfoPanelViewLoader view="history" />)
+);
