da596bdc72c79be50a1cca3b9ed3bb4ec9de29d7
mushka <nikitamushka@mail.ru>
2022-10-02 02:30:54 +0300

    created title component for files folders and rooms

---
+++ packages/client/src/pages/Home/InfoPanel/Body/sub-components/ItemTitle/FilesItemTitle.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { withTranslation } from "react-i18next";
+import { ReactSVG } from "react-svg";
+
+import { Text } from "@docspace/components";
+
+import ItemContextOptions from "./ItemContextOptions";
+
+import { StyledTitle } from "../../styles/common";
+
+const FilesItemTitle = ({
+  t,
+
+  selection,
+  isSeveralItems,
+
+  setBufferSelection,
+  getIcon,
+
+  getContextOptions,
+  getContextOptionActions,
+}) => {
+  if (isSeveralItems)
+    return (
+      <StyledTitle>
+        <ReactSVG className="icon" src={getIcon(32, ".file")} />
+        <Text className="text">
+          {`${t("InfoPanel:ItemsSelected")}: ${selection.length}`}
+        </Text>
+      </StyledTitle>
+    );
+
+  return (
+    <StyledTitle>
+      <img
+        className={`icon ${selection.isRoom && "is-room"}`}
+        src={selection.icon}
+        alt="thumbnail-icon"
+      />
+      <Text className="text">{selection.title}</Text>
+      {selection && (
+        <ItemContextOptions
+          t={t}
+          selection={selection}
+          setBufferSelection={setBufferSelection}
+          getContextOptions={getContextOptions}
+          getContextOptionActions={getContextOptionActions}
+        />
+      )}
+    </StyledTitle>
+  );
+};
+
+// isGallery ? (
+//     <StyledTitle>
+//       <ReactSVG className="icon" src={getIcon(32, ".docxf")} />
+//       <Text className="text">{selection?.attributes?.name_form}</Text>
+//     </StyledTitle>
+//   ) :
+
+export default withTranslation([
+  "Files",
+  "Common",
+  "Translations",
+  "InfoPanel",
+  "SharingPanel",
+])(FilesItemTitle);
