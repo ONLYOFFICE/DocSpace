4741de7c58d690002137b4739badfe315f403526
mushka <nikitamushka@mail.ru>
2022-10-02 03:11:48 +0300

    cleaned up code and fixed issue with visual bug on loaction change

--- packages/client/src/pages/Home/InfoPanel/Body/sub-components/ItemTitle/index.js
+++ packages/client/src/pages/Home/InfoPanel/Body/sub-components/ItemTitle/index.js
@@ -30,6 +30,8 @@ const ItemTitle = ({
   getContextOptionActions,
   getUserContextOptions,
 }) => {
+  if (!selection) return null;
+
   if (isAccounts)
     return (
       <AccountsItemTitle
@@ -61,40 +63,6 @@ const ItemTitle = ({
       getContextOptionActions={getContextOptionActions}
     />
   );
-
-  if (isSeveralItems)
-    return (
-      <StyledTitle>
-        <ReactSVG className="icon" src={getIcon(32, ".file")} />
-        <Text className="text">
-          {`${t("InfoPanel:ItemsSelected")}: ${displayedSelection.length}`}
-        </Text>
-      </StyledTitle>
-    );
-  return isGallery ? (
-    <StyledTitle>
-      <ReactSVG className="icon" src={getIcon(32, ".docxf")} />
-      <Text className="text">{displayedSelection?.attributes?.name_form}</Text>
-    </StyledTitle>
-  ) : (
-    <StyledTitle>
-      <img
-        className={`icon ${displayedSelection.isRoom && "is-room"}`}
-        src={displayedSelection.icon}
-        alt="thumbnail-icon"
-      />
-      <Text className="text">{displayedSelection.title}</Text>
-      {displayedSelection && (
-        <ItemContextOptions
-          t={t}
-          selection={displayedSelection}
-          setBufferSelection={setBufferSelection}
-          getContextOptions={getContextOptions}
-          getContextOptionActions={getContextOptionActions}
-        />
-      )}
-    </StyledTitle>
-  );
 };
 
 export default withTranslation([
@@ -104,57 +72,3 @@ export default withTranslation([
   "InfoPanel",
   "SharingPanel",
 ])(ItemTitle);
-
-// const ItemTitle = ({
-//   t,
-
-//   selection,
-//   selectionParentRoom,
-
-//   isGallery,
-//   isSeveralItems,
-//   setBufferSelection,
-//   getIcon,
-//   getContextOptions,
-//   getContextOptionActions,
-// }) => {
-//   return isSeveralItems ? (
-//     <StyledTitle>
-//       <ReactSVG className="icon" src={getIcon(32, ".file")} />
-//       <Text className="text">
-//         {`${t("InfoPanel:ItemsSelected")}: ${selection.length}`}
-//       </Text>
-//     </StyledTitle>
-//   ) : isGallery ? (
-//     <StyledTitle>
-//       <ReactSVG className="icon" src={getIcon(32, ".docxf")} />
-//       <Text className="text">{selection?.attributes?.name_form}</Text>
-//     </StyledTitle>
-//   ) : (
-//     <StyledTitle>
-//       <img
-//         className={`icon ${selection.isRoom && "is-room"}`}
-//         src={selection.icon}
-//         alt="thumbnail-icon"
-//       />
-//       <Text className="text">{selection.title}</Text>
-//       {selection && (
-//         <ItemContextOptions
-//           t={t}
-//           selection={selection}
-//           setBufferSelection={setBufferSelection}
-//           getContextOptions={getContextOptions}
-//           getContextOptionActions={getContextOptionActions}
-//         />
-//       )}
-//     </StyledTitle>
-//   );
-// };
-
-// export default withTranslation([
-//   "Files",
-//   "Common",
-//   "Translations",
-//   "InfoPanel",
-//   "SharingPanel",
-// ])(ItemTitle);
