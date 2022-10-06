d66e6b21e083bba2890b0a8b2e1e24228498a440
mushka <nikitamushka@mail.ru>
2022-10-06 20:06:05 +0300

    updated historyBlockUser componenet to InfoPanelStore update

--- packages/client/src/pages/Home/InfoPanel/Body/views/History/HistoryBlockUser.js
+++ packages/client/src/pages/Home/InfoPanel/Body/views/History/HistoryBlockUser.js
@@ -3,64 +3,20 @@ import { useHistory } from "react-router-dom";
 
 import Link from "@docspace/components/link";
 import { StyledUserNameLink } from "../../styles/history";
-import { getUserRole } from "@docspace/client/src/helpers/people-helpers";
-import { combineUrl } from "@docspace/common/utils";
-import { AppServerConfig } from "@docspace/common/constants";
-import config from "PACKAGE_FILE";
 
-const HistoryBlockUser = ({
-  user,
-  withComma,
-  selectUser,
-  selfId,
-  getUserById,
-  getUserContextOptions,
-  getStatusType,
-  setSelectedFolder,
-  setSelectedNode,
-}) => {
+const HistoryBlockUser = ({ user, withComma, openUser }) => {
   const username = user.displayName || user.email;
   const history = useHistory();
 
-  const goToAccounts = (openSelfProfile = false) => {
-    const path = [AppServerConfig.proxyURL, config.homepage, "/accounts"];
-    if (openSelfProfile) path.push("/@self");
-
-    setSelectedFolder(null);
-    setSelectedNode(["accounts", "filter"]);
-    history.push(combineUrl(...path));
-  };
-
-  const onUserClick = async () => {
-    if (user.id === selfId) {
-      goToAccounts(true);
-      return;
-    }
-
-    goToAccounts(false);
-    const fetchedUser = await getUserById(user.id);
-    fetchedUser.role = getUserRole(fetchedUser);
-    fetchedUser.statusType = getStatusType(fetchedUser);
-    fetchedUser.options = getUserContextOptions(
-      false,
-      fetchedUser.isOwner,
-      fetchedUser.statusType,
-      fetchedUser.status
-    );
-    selectUser(fetchedUser);
+  const onUserClick = () => {
+    openUser(user.id, history);
   };
 
   return (
     <StyledUserNameLink key={user.id} className="user">
-      {user.profileUrl ? (
-        <Link className="username link" onClick={onUserClick}>
-          {username}
-        </Link>
-      ) : (
-        <div className="username text" onClick={onUserClick}>
-          {username}
-        </div>
-      )}
+      <Link className="username link" onClick={onUserClick}>
+        {username}
+      </Link>
       {withComma ? "," : ""}
       {withComma && <div className="space"></div>}
     </StyledUserNameLink>
