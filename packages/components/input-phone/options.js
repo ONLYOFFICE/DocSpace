85332ffbb447aa8372cd704216d966524e7d5ac6
Elyor Djalilov <edjalilov.dev@gmail.com>
2022-10-06 15:25:25 +0500

    Web: InputPhone: bug with mask fixed

--- packages/components/input-phone/options.js
+++ packages/components/input-phone/options.js
@@ -540,7 +540,7 @@ export const options = [
     name: "Canada",
     dialCode: "1",
     code: "CA",
-    mask: [/\d/, " ", /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/],
+    mask: [/\d/, " ", /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/],
     flag: Canada
   },
   {
@@ -1905,7 +1905,7 @@ export const options = [
     name: "United States",
     dialCode: "1",
     code: "US",
-    mask: [/\d/, " ", /\d/, /\d/, /\d/, " ", /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/],
+    mask: [/\d/, " ", /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/],
     flag: UnitedStates
   },
   {
