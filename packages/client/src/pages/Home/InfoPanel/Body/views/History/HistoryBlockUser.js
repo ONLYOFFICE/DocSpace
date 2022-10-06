7557864d20de41d7304cd24f897c7b382a5f13eb
mushka <nikitamushka@mail.ru>
2022-10-06 15:51:42 +0300

    updated histoyBlockUser: added new functionality to onClick action

--- packages/client/src/pages/Home/InfoPanel/Body/views/History/HistoryBlockUser.js
+++ packages/client/src/pages/Home/InfoPanel/Body/views/History/HistoryBlockUser.js
@@ -1,18 +1,65 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import Link from "@docspace/components/link";
 import { StyledUserNameLink } from "../../styles/history";
+import { getUserRole } from "@docspace/client/src/helpers/people-helpers";
+import { combineUrl } from "@docspace/common/utils";
+import { AppServerConfig } from "@docspace/common/constants";
+import config from "PACKAGE_FILE";
 
-const HistoryBlockUser = ({ user, withComma }) => {
+const HistoryBlockUser = ({
+  user,
+  withComma,
+  selectUser,
+  selfId,
+  getUserById,
+  getUserContextOptions,
+  getStatusType,
+  setSelectedFolder,
+  setSelectedNode,
+}) => {
   const username = user.displayName || user.email;
+  const history = useHistory();
+
+  const goToAccounts = (openSelfProfile = false) => {
+    const path = [AppServerConfig.proxyURL, config.homepage, "/accounts"];
+    if (openSelfProfile) path.push("/@self");
+
+    setSelectedFolder(null);
+    setSelectedNode(["accounts", "filter"]);
+    history.push(combineUrl(...path));
+  };
+
+  const onUserClick = async () => {
+    if (user.id === selfId) {
+      goToAccounts(true);
+      return;
+    }
+
+    goToAccounts(false);
+    const fetchedUser = await getUserById(user.id);
+    fetchedUser.role = getUserRole(fetchedUser);
+    fetchedUser.statusType = getStatusType(fetchedUser);
+    fetchedUser.options = getUserContextOptions(
+      false,
+      fetchedUser.isOwner,
+      fetchedUser.statusType,
+      fetchedUser.status
+    );
+    selectUser(fetchedUser);
+  };
 
   return (
     <StyledUserNameLink key={user.id} className="user">
       {user.profileUrl ? (
-        // <Link className="username link" href={user.profileUrl}>
-        <Link className="username link">{username}</Link>
+        <Link className="username link" onClick={onUserClick}>
+          {username}
+        </Link>
       ) : (
-        <div className="username text">{username}</div>
+        <div className="username text" onClick={onUserClick}>
+          {username}
+        </div>
       )}
       {withComma ? "," : ""}
       {withComma && <div className="space"></div>}
