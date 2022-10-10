be3bb23144edb5310cd51cca8dcdfa5c3afb65e7
mushka <nikitamushka@mail.ru>
2022-10-10 17:19:19 +0300

    fixed loader error in accounts

--- packages/common/components/Loaders/InfoPanelBodyLoader/views/AccountsLoader.js
+++ packages/common/components/Loaders/InfoPanelBodyLoader/views/AccountsLoader.js
@@ -38,11 +38,36 @@ const StyledProperty = styled.div`
 `;
 
 const propertyDimensions = [
-  { propertyTitle: "50px", propertyContent: "39px" },
-  { propertyTitle: "29px", propertyContent: "64px" },
-  { propertyTitle: "38px", propertyContent: "28px" },
-  { propertyTitle: "36px", propertyContent: "120px" },
-  { propertyTitle: "0px", propertyContent: "160px" },
+  {
+    titleKey: 0,
+    contentKey: 1,
+    propertyTitle: "50px",
+    propertyContent: "39px",
+  },
+  {
+    titleKey: 2,
+    contentKey: 3,
+    propertyTitle: "29px",
+    propertyContent: "64px",
+  },
+  {
+    titleKey: 4,
+    contentKey: 5,
+    propertyTitle: "38px",
+    propertyContent: "28px",
+  },
+  {
+    titleKey: 6,
+    contentKey: 7,
+    propertyTitle: "36px",
+    propertyContent: "120px",
+  },
+  {
+    titleKey: 8,
+    contentKey: 9,
+    propertyTitle: "0px",
+    propertyContent: "160px",
+  },
 ];
 
 const AccountsLoader = () => {
@@ -55,12 +80,14 @@ const AccountsLoader = () => {
       <StyledProperty>
         {propertyDimensions.map((property) => [
           <RectangleLoader
+            key={property.titleKey}
             className="property-title"
             width={property.propertyTitle}
             height={"20px"}
             borderRadius={"3px"}
           />,
           <RectangleLoader
+            key={property.contentKey}
             className="property-content"
             width={property.propertyContent}
             height={"20px"}
