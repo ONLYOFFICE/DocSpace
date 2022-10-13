41994df60d77cfadc7e8b72ba147ca9e8c36bf1e
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-10-13 16:08:49 +0300

    Web:Client:Fix after merge.

--- packages/client/src/pages/PortalSettings/categories/common/branding.js
+++ packages/client/src/pages/PortalSettings/categories/common/branding.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { withTranslation } from "react-i18next";
 
 import { inject, observer } from "mobx-react";
-import { isMobile } from "react-device-detect";
+import { isMobile, isDesktop } from "react-device-detect";
 
 import withLoading from "SRC_DIR/HOCs/withLoading";
 import Whitelabel from "./Branding/whitelabel";
