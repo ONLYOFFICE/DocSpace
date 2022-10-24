1b0c161b159730f57aa04d8a64d04d6ad313169d
Viktor Fomin <enotcode@gmail.com>
2022-10-24 12:16:04 +0500

    Login: fix

--- packages/login/src/client/components/sub-components/LoginForm.tsx
+++ packages/login/src/client/components/sub-components/LoginForm.tsx
@@ -55,7 +55,7 @@ const LoginForm: React.FC<ILoginFormProps> = ({
 
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const { t, ready } = useTranslation(["Login", "Common"]);
+  const { t } = useTranslation(["Login", "Common"]);
 
   const { message, confirmedEmail } = match;
 
@@ -214,8 +214,6 @@ const LoginForm: React.FC<ILoginFormProps> = ({
     setIsDisabled(false);
     setIsLoading(false);
   };
-
-  if (!ready) return <></>;
 
   return (
     <form className="auth-form-container">
