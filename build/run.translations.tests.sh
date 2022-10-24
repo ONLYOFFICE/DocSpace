440943f81b2a6992aa9a954d77bcdec844fd039f
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2022-10-24 13:31:11 +0300

    Update run.translations.tests.sh

--- build/run.translations.tests.sh
+++ build/run.translations.tests.sh
@@ -5,4 +5,4 @@ dir=$(builtin cd $rd/../; pwd)
 
 echo "Root directory:" $dir
 
-dotnet test $dir/common/Tests/Frontend.Translations.Tests/Frontend.Translations.Tests.csproj --filter "TestCategory=FastRunning" -l:html -r TestsResults
+dotnet test $dir/common/Tests/Frontend.Translations.Tests/Frontend.Translations.Tests.csproj --filter "TestCategory=FastRunning" -l:html -r $dir/TestsResults
