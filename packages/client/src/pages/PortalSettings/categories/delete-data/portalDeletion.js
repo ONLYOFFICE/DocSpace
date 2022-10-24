8482dc20721ecc3e0b46fcc16ce9ee839c9eedb3
Viktor Fomin <enotcode@gmail.com>
2022-10-24 17:14:55 +0500

    Client: PortalSettings: DeleteData: get stripe url

--- packages/client/src/pages/PortalSettings/categories/delete-data/portalDeletion.js
+++ packages/client/src/pages/PortalSettings/categories/delete-data/portalDeletion.js
@@ -7,15 +7,42 @@ import Button from "@docspace/components/button";
 import { MainContainer } from "./StyledDeleteData";
 import { setDocumentTitle } from "../../../../helpers/utils";
 import { DeletePortalDialog } from "SRC_DIR/components/dialogs";
+import toastr from "@docspace/components/toast/toastr";
+import {
+  getPaymentAccount,
+  sendDeletePortalEmail,
+} from "@docspace/common/api/portal";
 
 const PortalDeletion = (props) => {
   const { t, getPortalOwner, owner } = props;
   const [isDialogVisible, setIsDialogVisible] = useState(false);
+  const [stripeUrl, setStripeUrl] = useState(null);
+
+  const fetchData = async () => {
+    const res = await getPaymentAccount();
+    setStripeUrl(res);
+  };
 
   useEffect(() => {
     setDocumentTitle(t("PortalDeletion"));
     getPortalOwner();
+    fetchData();
   }, []);
+
+  const onDeleteClick = async () => {
+    if (stripeUrl) {
+      setIsDialogVisible(true);
+    } else {
+      try {
+        await sendDeletePortalEmail();
+        toastr.success(
+          t("PortalDeletionEmailSended", { ownerEmail: owner.email })
+        );
+      } catch (error) {
+        toastr.error(error);
+      }
+    }
+  };
 
   return (
     <MainContainer>
@@ -31,13 +58,14 @@ const PortalDeletion = (props) => {
         label={t("Common:Delete")}
         primary
         size="normal"
-        onClick={() => setIsDialogVisible(true)}
+        onClick={onDeleteClick}
       />
 
       <DeletePortalDialog
         visible={isDialogVisible}
         onClose={() => setIsDialogVisible(false)}
         owner={owner}
+        stripeUrl={stripeUrl}
       />
     </MainContainer>
   );
