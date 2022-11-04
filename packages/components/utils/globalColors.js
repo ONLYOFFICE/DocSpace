9914d45468804f7fe3ba9b709e80b571e347bb31
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2022-11-04 12:42:47 +0500

    Web:Component:Utils:Added new colors for button in global Colors

--- packages/components/utils/globalColors.js
+++ packages/components/utils/globalColors.js
@@ -25,6 +25,12 @@ const globalColors = {
   blueLightMid: "#265A8F",
   strongBlue: "#316AC5",
 
+  buttonLightPrimaryDefault: "#5299E0",
+  buttonLightPrimaryHover: "rgba(82, 153, 224, 0.85)",
+  buttonLightPrimaryPressed:"#4A8ACA",
+  buttonLightPrimaryDisabled:"#96C1EC",
+  buttonLightPrimaryLoader:"#96C1EC",
+
   lightGrayishBlue: "#DEE2E6",
   lightGrayishStrongBlue: "#DFE2E3",
   cyanBlueDarkShade: "#555F65",
