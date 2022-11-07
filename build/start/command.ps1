bc358ae54cc091b6e11119cd80b7163c2b6f3a71
Alexey Bannov <alexey.bannov@onlyoffice.com>
2022-11-07 14:57:05 +0300

    build: added scipt for change service start timeout

--- build/start/command.ps1
+++ build/start/command.ps1
@@ -2,13 +2,28 @@ $PSversionMajor = $PSVersionTable.PSVersion | sort-object major | ForEach-Object
 $PSversionMinor = $PSVersionTable.PSVersion | sort-object minor | ForEach-Object {$_.minor}
 
 if ($PSversionMajor -lt 7 -or $PSversionMinor -lt 2) {
-    Write-Error "Powershell version must be greater than or equal to 7.2. Press Enter to exit."
-    Pause
+    Write-Error "Powershell version must be greater than or equal to 7.2."
     exit
 }
 
 $WorkDir = "$(Split-Path -Parent $PSScriptRoot)\run";
 $CommandName = "$($args[0])";
+
+if ( $CommandName -eq "increase-service-timeout" ) {	
+	$RegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control'
+	$Name         = 'ServicesPipeTimeout'
+	$Value        = '90000'
+	
+	Write-Output  "Changing service start timeout from default value (30000 ms ) to $($Value) ms"
+	
+	New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD -Force
+
+
+    Write-Warning  "Please, restart the computer for the changes to take effect."
+    exit		
+}
+
+
 
 #Write-Output  "Starting $($CommandName) services at time: $(Get-Date -Format HH:mm:ss)"
 #Write-Output  ""
