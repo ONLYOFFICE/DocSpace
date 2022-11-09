0e8e019b22d91d94aaf2c203091cc9d84d7fba85
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-11-09 14:33:42 +0300

    Web:Common:Add textColor in VersionBadge.

--- packages/common/components/ColorTheme/styled/versionBadge.js
+++ packages/common/components/ColorTheme/styled/versionBadge.js
@@ -1,21 +1,28 @@
 import styled, { css } from "styled-components";
 import { StyledVersionSvg } from "@docspace/client/src/pages/VersionHistory/Section/Body/StyledVersionHistory";
+import Box from "@docspace/components/box";
 const getDefaultStyles = ({ $currentColorScheme, $isVersion, theme, index }) =>
   $currentColorScheme &&
   css`
-    path {
-      fill: ${!$isVersion
-        ? theme.filesVersionHistory.badge.defaultFill
-        : index === 0
-        ? theme.filesVersionHistory.badge.fill
-        : $currentColorScheme.accentColor};
+    ${StyledVersionSvg} {
+      path {
+        fill: ${!$isVersion
+          ? theme.filesVersionHistory.badge.defaultFill
+          : index === 0
+          ? theme.filesVersionHistory.badge.fill
+          : $currentColorScheme.accentColor};
 
-      stroke: ${!$isVersion
-        ? theme.filesVersionHistory.badge.stroke
-        : index === 0
-        ? theme.filesVersionHistory.badge.fill
-        : $currentColorScheme.accentColor};
+        stroke: ${!$isVersion
+          ? theme.filesVersionHistory.badge.stroke
+          : index === 0
+          ? theme.filesVersionHistory.badge.fill
+          : $currentColorScheme.accentColor};
+      }
+    }
+
+    .version_badge-text {
+      color: ${$isVersion && index !== 0 && $currentColorScheme.textColor};
     }
   `;
 
-export default styled(StyledVersionSvg)(getDefaultStyles);
+export default styled(Box)(getDefaultStyles);
