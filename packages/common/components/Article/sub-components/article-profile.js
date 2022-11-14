94ea046a686af1a31a984709b8a89a847930db8f
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2022-11-14 15:15:51 +0500

    Web:Common:Components:Article:Sub-Components: Updated id

--- packages/common/components/Article/sub-components/article-profile.js
+++ packages/common/components/Article/sub-components/article-profile.js
@@ -51,6 +51,7 @@ const ArticleProfile = (props) => {
         <div ref={ref}>
           <Avatar
             className={"profile-avatar"}
+            id="user-avatar"
             size={avatarSize}
             role={"user"}
             source={user.avatar}
@@ -78,7 +79,7 @@ const ArticleProfile = (props) => {
               </Text>
             </StyledUserName>
             <ContextMenuButton
-              id="profile-option-button"
+              id="user-option-button"
               className="option-button"
               iconClassName="option-button-icon"
               zIndex={402}
