9dfcd2503311529c0be92947457259c44c26b316
Alexey Bannov <alexey.bannov@onlyoffice.com>
2022-11-14 14:11:10 +0300

    backend: update dependency dll

--- build/runMigrations.bat
+++ build/runMigrations.bat
@@ -5,6 +5,6 @@ cd /D "%~dp0"
 call start\stop.bat nopause
 dotnet build ..\asc.web.sln 
 dotnet build ..\ASC.Migrations.sln
-PUSHD %~dp0..\common\Tools\ASC.Migration.Runner\bin\Debug\net6.0
+PUSHD %~dp0..\common\Tools\ASC.Migration.Runner\bin\Debug\net7.0
 dotnet ASC.Migration.Runner.dll
 pause
