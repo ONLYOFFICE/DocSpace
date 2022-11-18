1bd4897199ccc2dc05a3cc65f6f3943c8dea754b
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-11-18 11:41:55 +0300

    Web:Client:Fix theme.

--- packages/common/components/ColorTheme/styled/indicatorLoader.js
+++ packages/common/components/ColorTheme/styled/indicatorLoader.js
@@ -1,16 +1,21 @@
 import styled, { css } from "styled-components";
 import StyledWrapper from "@docspace/client/src/components/IndicatorLoader/StyledWrapper";
+import Base from "@docspace/components/themes/base";
 
-const getDefaultStyles = ({ $currentColorScheme }) =>
+const getDefaultStyles = ({ $currentColorScheme, theme }) =>
   $currentColorScheme &&
   css`
     #ipl-progress-indicator {
-      background-color: ${$currentColorScheme.main.accent};
+      background-color: ${theme.isBase
+        ? $currentColorScheme.main.accent
+        : "#FFFFFF"};
 
       &:hover {
-        background-color: ${$currentColorScheme.main.accent};
+        background-color: ${theme.isBase
+          ? $currentColorScheme.main.accent
+          : "#FFFFFF"};
       }
     }
   `;
-
+StyledWrapper.defaultProps = { theme: Base };
 export default styled(StyledWrapper)(getDefaultStyles);
