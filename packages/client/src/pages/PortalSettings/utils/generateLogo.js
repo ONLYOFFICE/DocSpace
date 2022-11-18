5acd9298955e771fcac842503a1506bae6ac4504
Viktor Fomin <enotcode@gmail.com>
2022-11-18 07:00:46 +0500

    Client: PortalSettings: Utils: add logo generator

---
+++ packages/client/src/pages/PortalSettings/utils/generateLogo.js
@@ -0,0 +1,14 @@
+export const generateLogo = (width, height, text, fill = "#000") => {
+  const canvas = document.createElement("canvas");
+  canvas.width = width;
+  canvas.height = height;
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "transparent";
+  ctx.clearRect(0, 0, width, height);
+  ctx.fillStyle = fill;
+  ctx.textAlign = "start";
+  ctx.textBaseline = "top";
+  ctx.font = `${height}px Arial`;
+  ctx.fillText(text, 0, 0);
+  return canvas.toDataURL();
+};
