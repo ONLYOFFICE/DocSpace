5d6e0b17c916e3837026b3576b20e280491941bb
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2022-11-23 14:52:36 +0300

    Web:Common:Fix hover.

--- packages/common/components/ColorTheme/styled/infoPanelToggle.js
+++ packages/common/components/ColorTheme/styled/infoPanelToggle.js
@@ -2,12 +2,17 @@ import styled, { css } from "styled-components";
 import { StyledInfoPanelToggleWrapper } from "@docspace/client/src/pages/Home/InfoPanel/Header/styles/common";
 import Base from "@docspace/components/themes/base";
 
-const getDefaultStyles = ({ $currentColorScheme, theme }) =>
+const getDefaultStyles = ({ $currentColorScheme }) =>
   $currentColorScheme &&
   css`
     .info-panel-toggle-bg {
       path {
         fill: ${$currentColorScheme.main.accent};
+      }
+      &:hover {
+        path {
+          fill: ${$currentColorScheme.main.accent};
+        }
       }
     }
   `;
