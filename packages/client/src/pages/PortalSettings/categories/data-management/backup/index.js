b7de4a5ab0195b73a1d9c83eb9ba50db9bdaeda2
Elyor Djalilov <edjalilov.dev@gmail.com>
2022-11-24 19:40:19 +0500

    bugfix/156: tooltip style fixed

--- packages/client/src/pages/PortalSettings/categories/data-management/backup/index.js
+++ packages/client/src/pages/PortalSettings/categories/data-management/backup/index.js
@@ -21,6 +21,8 @@ const Backup = ({
     return (
       <>
         <HelpButton
+          displayType="auto"
+          place="bottom"
           iconName={"/static/images/help.react.svg"}
           tooltipContent={
             <>
