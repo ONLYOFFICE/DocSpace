0bd6621138c738fa77aa072c0ec49e4c37ee1e67
Viktor Fomin <enotcode@gmail.com>
2022-12-09 12:00:21 +0300

    Client: Confirm: fix auth redirect

--- packages/client/src/pages/Confirm/sub-components/auth.js
+++ packages/client/src/pages/Confirm/sub-components/auth.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router";
 import Loader from "@docspace/components/loader";
 import Section from "@docspace/common/components/Section";
 import { loginWithConfirmKey } from "@docspace/common/api/user";
+import toastr from "@docspace/components/toast/toastr";
 
 const Auth = (props) => {
   console.log("Auth render");
@@ -14,7 +15,13 @@ const Auth = (props) => {
         Email: linkData.email,
         Key: linkData.confirmHeader,
       },
-    });
+    })
+      .then((res) => {
+        console.log("Login with confirm key success", res);
+        if (typeof res === "string") window.location.replace(res);
+        else window.location.replace("/");
+      })
+      .catch((error) => toastr.error(error));
   });
 
   return <Loader className="pageLoader" type="rombs" size="40px" />;
