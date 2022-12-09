853240f18fbc3603240b87a048e873f6a3760e68
Viktor Fomin <enotcode@gmail.com>
2022-12-09 10:40:47 +0300

    Client: Confirm: fix

--- packages/client/src/pages/Confirm/sub-components/auth.js
+++ packages/client/src/pages/Confirm/sub-components/auth.js
@@ -2,15 +2,19 @@ import React, { useEffect } from "react";
 import { withRouter } from "react-router";
 import Loader from "@docspace/components/loader";
 import Section from "@docspace/common/components/Section";
-import { combineUrl } from "@docspace/common/utils";
-import tryRedirectTo from "@docspace/common/utils/tryRedirectTo";
-import { AppServerConfig } from "@docspace/common/constants";
+import { loginWithConfirmKey } from "@docspace/common/api/user";
 
 const Auth = (props) => {
   console.log("Auth render");
+  const { linkData } = props;
 
   useEffect(() => {
-    tryRedirectTo(combineUrl(AppServerConfig.proxyURL, `/login`));
+    loginWithConfirmKey({
+      ConfirmData: {
+        Email: linkData.email,
+        Key: linkData.confirmHeader,
+      },
+    });
   });
 
   return <Loader className="pageLoader" type="rombs" size="40px" />;
