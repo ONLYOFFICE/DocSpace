5f8f9dc29ed1b2850df458ff05f45a7f52d8d6ee
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2022-12-09 12:41:59 +0300

    Files: removed unnecessary

--- products/ASC.Files/Core/Services/WCFService/FileOperations/FileOperation.cs
+++ products/ASC.Files/Core/Services/WCFService/FileOperations/FileOperation.cs
@@ -222,9 +222,6 @@ abstract class FileOperation<T, TId> : FileOperation where T : FileOperationData
             var (tenantManager, daoFactory, fileSecurity, logger) = scopeClass;
             tenantManager.SetCurrentTenant(CurrentTenant);
 
-            var currentTenantManager = _serviceProvider.GetService<TenantManager>();
-            currentTenantManager.SetCurrentTenant(CurrentTenant);
-
             Thread.CurrentPrincipal = _principal;
             Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo(_culture);
             Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(_culture);
