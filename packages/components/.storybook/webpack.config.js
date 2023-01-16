eb4b8bc44d375d43aacd0dc2a05010538b43b6d2
Timofey Boyko <55255132+TimofeyBoyko@users.noreply.github.com>
2023-01-16 12:59:09 +0300

    Web:Components:Storybook: update storybook webpack config for work with asets

--- packages/components/.storybook/webpack.config.js
+++ packages/components/.storybook/webpack.config.js
@@ -1,13 +1,45 @@
 const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+const pathToAssets = path.resolve(__dirname, "../../../public/images");
 
 module.exports = ({ config }) => {
   const rules = config.module.rules;
 
   const fileLoaderRule = rules.find((rule) => rule.test.test(".svg"));
-  fileLoaderRule.exclude = /\.react.svg$/;
+  fileLoaderRule.exclude = /\.svg$/;
+
+  config.output.assetModuleFilename = (pathData) => {
+    //console.log({ pathData });
+
+    let result = pathData.filename
+      .substr(pathData.filename.indexOf("public/"))
+      .split("/")
+      .slice(1);
+
+    result.pop();
+
+    let folder = result.join("/");
+
+    console.log("call " + folder);
+
+    return `${folder}/[name].[contenthash][ext]`; //`${folder}/[name][ext]?hash=[contenthash]`;
+  };
+
+  // rules.push({
+  //   test: /\.(png|jpe?g|gif|ico)$/i,
+  //   type: "asset/resource",
+  // });
 
   rules.push({
-    test: /\.react.svg$/,
+    test: /\.svg$/i,
+    type: "asset/resource",
+    resourceQuery: /url/, // *.svg?url
+  });
+
+  rules.push({
+    test: /\.svg$/i,
+    resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
     use: [
       {
         loader: "@svgr/webpack",
@@ -20,18 +52,18 @@ module.exports = ({ config }) => {
     ],
   });
 
-  config.plugins.push(
-    new CopyPlugin({
-      patterns: [
-        {
-          from: "../../../public/images",
-          to: "./static/images",
-          toType: "dir",
-          context: "storybook-static",
-        },
-      ],
-    })
-  );
+  // config.plugins.push(
+  //   new CopyPlugin({
+  //     patterns: [
+  //       {
+  //         from: "../../../public/images",
+  //         to: "./images",
+  //         toType: "dir",
+  //         context: "storybook-static",
+  //       },
+  //     ],
+  //   })
+  // );
 
   return config;
 };
