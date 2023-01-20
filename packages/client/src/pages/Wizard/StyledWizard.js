aefaad2191dcf627f53b0c9cf6136090f648ff11
Viktor Fomin <enotcode@gmail.com>
2023-01-20 21:07:22 +0300

    Client: Wizard: fix crash

--- packages/client/src/pages/Wizard/StyledWizard.js
+++ packages/client/src/pages/Wizard/StyledWizard.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { tablet, hugeMobile } from "@docspace/components/utils/device";
 import { isIOS, isFirefox, isMobileOnly } from "react-device-detect";
 
