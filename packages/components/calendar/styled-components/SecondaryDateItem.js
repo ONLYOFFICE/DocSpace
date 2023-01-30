9122e6df7c5aa23c0cd8396502bdd18d16657dec
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-01-30 20:51:47 +0500

    style for disabled/past dates was created

---
+++ packages/components/calendar/styled-components/SecondaryDateItem.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { DateItem } from "./";
+
+export const SecondaryDateItem = styled(DateItem)`
+  color: #A3A9AE;
+
+  :hover{
+    cursor: ${props => props.disabled ? 'auto' : 'pointer'};
+  }
+
+  :focus{
+    
+  }
+`;
