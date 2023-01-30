f0b2461241ce5bc84db4170a2b87da7b6845174f
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-01-30 19:54:13 +0500

    calendar container was created

---
+++ packages/components/calendar/styled-components/Container.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 432px;
+  max-height: 446px;
+  box-sizing: border-box;
+  
+  padding: 30px 28px 28px 28px;
+
+  box-shadow: 0px 12px 40px rgba(4, 15, 27, 0.12);
+  border-radius: 6px; 
+`;
