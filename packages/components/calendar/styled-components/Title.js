b6f18520b0c311056f470f2025fe714cc8bdcd9e
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-01-30 20:53:14 +0500

    style for month year title representation was created

---
+++ packages/components/calendar/styled-components/Title.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-family: 'Open Sans', sans-serif, Arial;
+  font-weight: 700;
+  font-size: 21px;
+  line-height: 28px;
+  color: #555f65;
+  border-bottom: 1px dashed transparent;
+  margin: 0;
+
+  :hover {
+    border-bottom: 1px dashed #555f65;
+    cursor: pointer;
+  }
+`;
