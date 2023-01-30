eb861b83f48a9fc2faf3ac84b13ad9f39db2ba9d
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-01-30 20:53:37 +0500

    style for weekday was created

---
+++ packages/components/calendar/styled-components/Weekday.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Weekday = styled.span`
+  pointer-events: none;
+  font-family: "Open Sans";
+  font-weight: 400;
+  font-size: 16px;
+
+  color: #a3a9ae;
+  width: 40px;
+
+  text-align: center;
+  padding: 10.7px 0;
+`;
