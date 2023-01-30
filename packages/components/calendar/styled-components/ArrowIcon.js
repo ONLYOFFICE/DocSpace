4dbcdd324fd78b8c0391ac02a1095ce0bf678db5
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-01-30 19:29:19 +0500

    arrow icon was created from borders

---
+++ packages/components/calendar/styled-components/ArrowIcon.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ArrowIcon = styled.span`
+  position: absolute;
+  border-left: 2px solid #555f65;
+  border-bottom: 2px solid #555f65;
+  width: 5px;
+  height: 5px;
+`;
