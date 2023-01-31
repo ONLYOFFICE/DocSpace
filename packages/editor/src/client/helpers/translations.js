521a1658c62feb4a8c70b844e844d0571fe6c053
Timofey Boyko <55255132+TimofeyBoyko@users.noreply.github.com>
2023-01-31 17:10:19 +0300

    Web:Editor: disable sw and fix tranlsation hash

---
+++ packages/editor/src/client/helpers/translations.js
@@ -0,0 +1,192 @@
+import CommonazUrl from "PUBLIC_DIR/locales/az/Common.json?url";
+import EditorazUrl from "PUBLIC_DIR/locales/az/Editor.json?url";
+import CommonbgUrl from "PUBLIC_DIR/locales/bg/Common.json?url";
+import EditorbgUrl from "PUBLIC_DIR/locales/bg/Editor.json?url";
+import CommoncsUrl from "PUBLIC_DIR/locales/cs/Common.json?url";
+import EditorcsUrl from "PUBLIC_DIR/locales/cs/Editor.json?url";
+import CommondeUrl from "PUBLIC_DIR/locales/de/Common.json?url";
+import EditordeUrl from "PUBLIC_DIR/locales/de/Editor.json?url";
+import CommonelGRUrl from "PUBLIC_DIR/locales/el-GR/Common.json?url";
+import EditorelGRUrl from "PUBLIC_DIR/locales/el-GR/Editor.json?url";
+import CommonenUrl from "PUBLIC_DIR/locales/en/Common.json?url";
+import EditorenUrl from "PUBLIC_DIR/locales/en/Editor.json?url";
+import CommonesUrl from "PUBLIC_DIR/locales/es/Common.json?url";
+import EditoresUrl from "PUBLIC_DIR/locales/es/Editor.json?url";
+import CommonfiUrl from "PUBLIC_DIR/locales/fi/Common.json?url";
+import EditorfiUrl from "PUBLIC_DIR/locales/fi/Editor.json?url";
+import CommonfrUrl from "PUBLIC_DIR/locales/fr/Common.json?url";
+import EditorfrUrl from "PUBLIC_DIR/locales/fr/Editor.json?url";
+import CommonhyAMUrl from "PUBLIC_DIR/locales/hy-AM/Common.json?url";
+import EditorhyAMUrl from "PUBLIC_DIR/locales/hy-AM/Editor.json?url";
+import CommonitUrl from "PUBLIC_DIR/locales/it/Common.json?url";
+import EditoritUrl from "PUBLIC_DIR/locales/it/Editor.json?url";
+import CommonjaJPUrl from "PUBLIC_DIR/locales/ja-JP/Common.json?url";
+import EditorjaJPUrl from "PUBLIC_DIR/locales/ja-JP/Editor.json?url";
+import CommonkoKRUrl from "PUBLIC_DIR/locales/ko-KR/Common.json?url";
+import EditorkoKRUrl from "PUBLIC_DIR/locales/ko-KR/Editor.json?url";
+import CommonloLAUrl from "PUBLIC_DIR/locales/lo-LA/Common.json?url";
+import EditorloLAUrl from "PUBLIC_DIR/locales/lo-LA/Editor.json?url";
+import CommonlvUrl from "PUBLIC_DIR/locales/lv/Common.json?url";
+import EditorlvUrl from "PUBLIC_DIR/locales/lv/Editor.json?url";
+import CommonnlUrl from "PUBLIC_DIR/locales/nl/Common.json?url";
+import EditornlUrl from "PUBLIC_DIR/locales/nl/Editor.json?url";
+import CommonplUrl from "PUBLIC_DIR/locales/pl/Common.json?url";
+import EditorplUrl from "PUBLIC_DIR/locales/pl/Editor.json?url";
+import CommonptUrl from "PUBLIC_DIR/locales/pt/Common.json?url";
+import EditorptUrl from "PUBLIC_DIR/locales/pt/Editor.json?url";
+import CommonptBRUrl from "PUBLIC_DIR/locales/pt-BR/Common.json?url";
+import EditorptBRUrl from "PUBLIC_DIR/locales/pt-BR/Editor.json?url";
+import CommonroUrl from "PUBLIC_DIR/locales/ro/Common.json?url";
+import EditorroUrl from "PUBLIC_DIR/locales/ro/Editor.json?url";
+import CommonruUrl from "PUBLIC_DIR/locales/ru/Common.json?url";
+import EditorruUrl from "PUBLIC_DIR/locales/ru/Editor.json?url";
+import CommonskUrl from "PUBLIC_DIR/locales/sk/Common.json?url";
+import EditorskUrl from "PUBLIC_DIR/locales/sk/Editor.json?url";
+import CommonslUrl from "PUBLIC_DIR/locales/sl/Common.json?url";
+import EditorslUrl from "PUBLIC_DIR/locales/sl/Editor.json?url";
+import CommontrUrl from "PUBLIC_DIR/locales/tr/Common.json?url";
+import EditortrUrl from "PUBLIC_DIR/locales/tr/Editor.json?url";
+import CommonukUAUrl from "PUBLIC_DIR/locales/uk-UA/Common.json?url";
+import EditorukUAUrl from "PUBLIC_DIR/locales/uk-UA/Editor.json?url";
+import CommonviUrl from "PUBLIC_DIR/locales/vi/Common.json?url";
+import EditorviUrl from "PUBLIC_DIR/locales/vi/Editor.json?url";
+import CommonzhCNUrl from "PUBLIC_DIR/locales/zh-CN/Common.json?url";
+import EditorzhCNUrl from "PUBLIC_DIR/locales/zh-CN/Editor.json?url";
+
+const az = new Map([
+  ["Common", CommonazUrl],
+  ["Editor", EditorazUrl],
+]);
+const bg = new Map([
+  ["Common", CommonbgUrl],
+  ["Editor", EditorbgUrl],
+]);
+const cs = new Map([
+  ["Common", CommoncsUrl],
+  ["Editor", EditorcsUrl],
+]);
+const de = new Map([
+  ["Common", CommondeUrl],
+  ["Editor", EditordeUrl],
+]);
+const elGR = new Map([
+  ["Common", CommonelGRUrl],
+  ["Editor", EditorelGRUrl],
+]);
+const en = new Map([
+  ["Common", CommonenUrl],
+  ["Editor", EditorenUrl],
+]);
+const es = new Map([
+  ["Common", CommonesUrl],
+  ["Editor", EditoresUrl],
+]);
+const fi = new Map([
+  ["Common", CommonfiUrl],
+  ["Editor", EditorfiUrl],
+]);
+const fr = new Map([
+  ["Common", CommonfrUrl],
+  ["Editor", EditorfrUrl],
+]);
+const hyAM = new Map([
+  ["Common", CommonhyAMUrl],
+  ["Editor", EditorhyAMUrl],
+]);
+const it = new Map([
+  ["Common", CommonitUrl],
+  ["Editor", EditoritUrl],
+]);
+const jaJP = new Map([
+  ["Common", CommonjaJPUrl],
+  ["Editor", EditorjaJPUrl],
+]);
+const koKR = new Map([
+  ["Common", CommonkoKRUrl],
+  ["Editor", EditorkoKRUrl],
+]);
+const loLA = new Map([
+  ["Common", CommonloLAUrl],
+  ["Editor", EditorloLAUrl],
+]);
+const lv = new Map([
+  ["Common", CommonlvUrl],
+  ["Editor", EditorlvUrl],
+]);
+const nl = new Map([
+  ["Common", CommonnlUrl],
+  ["Editor", EditornlUrl],
+]);
+const pl = new Map([
+  ["Common", CommonplUrl],
+  ["Editor", EditorplUrl],
+]);
+const pt = new Map([
+  ["Common", CommonptUrl],
+  ["Editor", EditorptUrl],
+]);
+const ptBR = new Map([
+  ["Common", CommonptBRUrl],
+  ["Editor", EditorptBRUrl],
+]);
+const ro = new Map([
+  ["Common", CommonroUrl],
+  ["Editor", EditorroUrl],
+]);
+const ru = new Map([
+  ["Common", CommonruUrl],
+  ["Editor", EditorruUrl],
+]);
+const sk = new Map([
+  ["Common", CommonskUrl],
+  ["Editor", EditorskUrl],
+]);
+const sl = new Map([
+  ["Common", CommonslUrl],
+  ["Editor", EditorslUrl],
+]);
+const tr = new Map([
+  ["Common", CommontrUrl],
+  ["Editor", EditortrUrl],
+]);
+const ukUA = new Map([
+  ["Common", CommonukUAUrl],
+  ["Editor", EditorukUAUrl],
+]);
+const vi = new Map([
+  ["Common", CommonviUrl],
+  ["Editor", EditorviUrl],
+]);
+const zhCN = new Map([
+  ["Common", CommonzhCNUrl],
+  ["Editor", EditorzhCNUrl],
+]);
+export const translations = new Map([
+  ["az", az],
+  ["bg", bg],
+  ["cs", cs],
+  ["de", de],
+  ["el-GR", elGR],
+  ["en", en],
+  ["es", es],
+  ["fi", fi],
+  ["fr", fr],
+  ["hy-AM", hyAM],
+  ["it", it],
+  ["ja-JP", jaJP],
+  ["ko-KR", koKR],
+  ["lo-LA", loLA],
+  ["lv", lv],
+  ["nl", nl],
+  ["pl", pl],
+  ["pt", pt],
+  ["pt-BR", ptBR],
+  ["ro", ro],
+  ["ru", ru],
+  ["sk", sk],
+  ["sl", sl],
+  ["tr", tr],
+  ["uk-UA", ukUA],
+  ["vi", vi],
+  ["zh-CN", zhCN],
+]);
--- packages/editor/src/client/helpers/utils.js
+++ packages/editor/src/client/helpers/utils.js
@@ -1,27 +1,56 @@
 import pkg from "../../../package.json";
+import { translations } from "./translations";
+
+export function getLanguage(lng) {
+  try {
+    let language = lng == "en-US" || lng == "en-GB" ? "en" : lng;
+
+    const splitted = lng.split("-");
+
+    if (splitted.length == 2 && splitted[0] == splitted[1].toLowerCase()) {
+      language = splitted[0];
+    }
+
+    return language;
+  } catch (error) {
+    console.error(error);
+  }
+
+  return lng;
+}
+
+export function loadLanguagePath(homepage, fixedNS = null) {
+  return (lng, ns) => {
+    const language = getLanguage(lng instanceof Array ? lng[0] : lng);
+
+    const lngCollection = translations.get(language);
+
+    let path = "";
+
+    if (ns.length > 0 && ns[0] === "Common") {
+      path = lngCollection?.get("Common");
+    }
+    path = lngCollection?.get(`${fixedNS || ns}`);
+
+    return path;
+  };
+}
 
 export const initI18n = (initialI18nStoreASC) => {
   if (!initialI18nStoreASC || window.i18n) return;
-
-  const { homepage } = pkg;
 
   window.i18n = {};
   window.i18n.inLoad = [];
   window.i18n.loaded = {};
 
   for (let lng in initialI18nStoreASC) {
+    const collection = translations.get(lng);
+
     for (let ns in initialI18nStoreASC[lng]) {
-      if (ns === "Common") {
-        window.i18n.loaded[`/static/locales/${lng}/${ns}.json`] = {
-          namespaces: ns,
-          data: initialI18nStoreASC[lng][ns],
-        };
-      } else {
-        window.i18n.loaded[`${homepage}/locales/${lng}/${ns}.json`] = {
-          namespaces: ns,
-          data: initialI18nStoreASC[lng][ns],
-        };
-      }
+      window.i18n.loaded[`${collection?.get(ns)}`] = {
+        namespaces: ns,
+        data: initialI18nStoreASC[lng][ns],
+      };
     }
   }
 };
