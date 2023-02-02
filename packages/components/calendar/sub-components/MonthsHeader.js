75cb5be51ef11065cbbbf4e592684c31192a3fe6
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-02 14:42:45 +0500

    header for months selector was created

---
+++ packages/components/calendar/sub-components/MonthsHeader.js
@@ -0,0 +1,11 @@
+import { HeaderContainer, Title } from "../styled-components";
+import { HeaderButtons } from "./HeaderButtons";
+
+export const MonthsHeader = () => {
+  return (
+    <HeaderContainer>
+      <Title>2023</Title>
+      <HeaderButtons />
+    </HeaderContainer>
+  );
+};
