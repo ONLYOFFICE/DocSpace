3c204dfc462b3861cc4da2d910a710a5c2b56b51
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-02 14:48:18 +0500

    years component was created

---
+++ packages/components/calendar/sub-components/Years.js
@@ -0,0 +1,11 @@
+import { YearsBody } from "./YearsBody";
+import { YearsHeader } from "./YearsHeader";
+
+export const Years = () => {
+  return (
+    <>
+      <YearsHeader />
+      <YearsBody />
+    </>
+  );
+};
