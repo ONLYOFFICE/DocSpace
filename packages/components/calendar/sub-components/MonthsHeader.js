1a401229f195b241755d1d4c997f49142750db7c
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-02 16:40:27 +0500

    handlers were added

--- packages/components/calendar/sub-components/MonthsHeader.js
+++ packages/components/calendar/sub-components/MonthsHeader.js
@@ -1,11 +1,25 @@
 import { HeaderContainer, Title } from "../styled-components";
 import { HeaderButtons } from "./HeaderButtons";
 
-export const MonthsHeader = () => {
+const onLeftClick = (setSelectedDate) => {
+  setSelectedDate((prevSelectedDate) =>
+    prevSelectedDate.clone().subtract(1, "year")
+  );
+};
+const onRightClick = (setSelectedDate) => {
+  setSelectedDate((prevSelectedDate) =>
+    prevSelectedDate.clone().add(1, "year")
+  );
+};
+
+export const MonthsHeader = ({ selectedDate, setSelectedDate }) => {
   return (
     <HeaderContainer>
-      <Title>2023</Title>
-      <HeaderButtons />
+      <Title>{selectedDate.year()}</Title>
+      <HeaderButtons
+        onLeftClick={() => onLeftClick(setSelectedDate)}
+        onRightClick={() => onRightClick(setSelectedDate)}
+      />
     </HeaderContainer>
   );
 };
