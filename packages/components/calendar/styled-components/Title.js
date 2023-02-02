0e6aeeec70086b821aba9f5de202a62de0c611e5
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-02 14:32:35 +0500

    dissabled styles were added

--- packages/components/calendar/styled-components/Title.js
+++ packages/components/calendar/styled-components/Title.js
@@ -1,16 +1,18 @@
 import styled from "styled-components";
 
 export const Title = styled.h2`
-  font-family: 'Open Sans', sans-serif, Arial;
+  font-family: "Open Sans", sans-serif, Arial;
   font-weight: 700;
   font-size: 21px;
   line-height: 28px;
   color: #555f65;
   border-bottom: 1px dashed transparent;
   margin: 0;
+  display: inline-block;
 
   :hover {
-    border-bottom: 1px dashed #555f65;
-    cursor: pointer;
+    border-bottom: ${(props) =>
+      props.disabled ? "none" : "1px dashed #555f65"};
+    cursor: ${(props) => (props.disabled ? "auto" : "pointer")};
   }
 `;
