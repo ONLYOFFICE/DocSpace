946e2caad4fdc48c5f3245f5218730fa32617150
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-02 14:47:49 +0500

    years header was created

---
+++ packages/components/calendar/sub-components/YearsHeader.js
@@ -0,0 +1,11 @@
+import { HeaderContainer, Title } from "../styled-components";
+import { HeaderButtons } from "./HeaderButtons";
+
+export const YearsHeader = () => {
+  return (
+    <HeaderContainer>
+      <Title disabled>2020-2029</Title>
+      <HeaderButtons />
+    </HeaderContainer>
+  );
+};
