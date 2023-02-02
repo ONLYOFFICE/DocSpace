fe8664cc394a988d70c7f2d0e45e62c1bf1d936a
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-02 16:58:27 +0500

    getMonths functions were added

--- packages/components/calendar/utils/index.js
+++ packages/components/calendar/utils/index.js
@@ -1,3 +1,5 @@
 export { getCalendarDays } from "./getCalendarDays";
 export { getDayElements } from "./getDayElements";
-export {getWeekdayElements} from './getWeekdayElements'
+export { getWeekdayElements } from "./getWeekdayElements";
+export { getCalendarMonths } from "./getCalendarMonths";
+export { getMonthElements } from "./getMonthElements";
