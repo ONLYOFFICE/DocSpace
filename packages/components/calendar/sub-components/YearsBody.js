306ff158e12fed34944202a91757f1ae588fddc9
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-02 18:24:58 +0500

    logic was moved to separate function

--- packages/components/calendar/sub-components/YearsBody.js
+++ packages/components/calendar/sub-components/YearsBody.js
@@ -1,15 +1,13 @@
-import { CurrentDateItem, DateItem, SecondaryDateItem, YearsContainer } from "../styled-components"
+import moment from "moment";
 
-export const YearsBody = () => {
-  const years = [2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034];
+import {
+  YearsContainer,
+} from "../styled-components";
+import { getCalendarYears, getYearElements } from "../utils";
 
-  const yearsElements = years.map(year => <SecondaryDateItem big>{year}</SecondaryDateItem>);
+export const YearsBody = ({ selectedDate, setSelectedDate }) => {
+  const years = getCalendarYears(selectedDate);
+  const yearElements = getYearElements(years, setSelectedDate);
 
-  for(let i = 1; i < 11; i++){
-    yearsElements[i] = <DateItem big>{years[i]}</DateItem>;
-  }
-
-  yearsElements[3] = <CurrentDateItem big>{years[3]}</CurrentDateItem>
-
-  return <YearsContainer>{yearsElements}</YearsContainer>;
+  return <YearsContainer>{yearElements}</YearsContainer>;
 };
