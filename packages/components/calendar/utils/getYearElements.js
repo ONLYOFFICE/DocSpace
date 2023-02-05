175f7f542e15c7273786d21d27bc832b87767d93
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-05 15:45:28 +0500

    selected Date is focused/onClick is changing selected date

--- packages/components/calendar/utils/getYearElements.js
+++ packages/components/calendar/utils/getYearElements.js
@@ -6,42 +6,56 @@ import {
   SecondaryDateItem,
 } from "../styled-components";
 
-const onDateClick = (year, setSelectedDate) => {
-  setSelectedDate((prevSelectedDate) =>
-    prevSelectedDate.clone().set({ year: year })
+const onDateClick = (year, setObservedDate, setSelectedScene) => {
+  setObservedDate((prevObservedDate) =>
+    prevObservedDate.clone().set({ year: year })
   );
+  setSelectedScene((prevSelectedScene) => prevSelectedScene - 1);
 };
 
-export const getYearElements = (years, setSelectedDate) => {
+export const getYearElements = (
+  years,
+  setObservedDate,
+  setSelectedScene,
+  selectedDate
+) => {
+  const onClick = (year) =>
+    onDateClick(year, setObservedDate, setSelectedScene);
+
   const yearElements = years.map((year) => (
-    <SecondaryDateItem
-      big
-      key={year}
-      onClick={() => onDateClick(year, setSelectedDate)}
-    >
+    <SecondaryDateItem big key={year} onClick={() => onClick(year)}>
       {year}
     </SecondaryDateItem>
   ));
 
   for (let i = 1; i < 11; i++) {
     yearElements[i] = (
-      <DateItem
-        big
-        key={years[i]}
-        onClick={() => onDateClick(years[i], setSelectedDate)}
-      >
+      <DateItem big key={years[i]} onClick={() => onClick(years[i])}>
         {years[i]}
       </DateItem>
     );
   }
 
   const currentYearIndex = years.indexOf(moment().year());
+  const selectedYearIndex = years.indexOf(selectedDate.year());
+  if (selectedYearIndex !== -1) {
+    yearElements[selectedYearIndex] = (
+      <DateItem
+        big
+        focused
+        key={years[selectedYearIndex]}
+        onClick={() => onClick(years[selectedYearIndex])}
+      >
+        {years[selectedYearIndex]}
+      </DateItem>
+    );
+  }
   if (currentYearIndex !== -1) {
     yearElements[currentYearIndex] = (
       <CurrentDateItem
         big
         key={years[currentYearIndex]}
-        onClick={() => onDateClick(years[currentYearIndex], setSelectedDate)}
+        onClick={() => onClick(years[currentYearIndex])}
       >
         {years[currentYearIndex]}
       </CurrentDateItem>
