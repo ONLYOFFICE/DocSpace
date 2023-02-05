d2ec8afd20331523a260cc70c3318662e7526825
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-05 15:18:00 +0500

    observer state was added

--- packages/components/calendar/sub-components/DaysBody.js
+++ packages/components/calendar/sub-components/DaysBody.js
@@ -2,8 +2,12 @@ import { DaysContainer } from "../styled-components";
 
 import { getDayElements, getWeekdayElements } from "../utils";
 
-export const DaysBody = ({ selectedDate, setSelectedDate }) => {
-  const daysElements = getDayElements(selectedDate, setSelectedDate);
+export const DaysBody = ({ observedDate, setSelectedDate, selectedDate }) => {
+  const daysElements = getDayElements(
+    observedDate,
+    selectedDate,
+    setSelectedDate
+  );
   const weekdayElements = getWeekdayElements();
 
   return (
