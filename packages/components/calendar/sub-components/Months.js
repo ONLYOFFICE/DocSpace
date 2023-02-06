8452ba994d705e3e424eac48d08fa5502c900598
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-06 20:03:28 +0500

    min and max dates were added

--- packages/components/calendar/sub-components/Months.js
+++ packages/components/calendar/sub-components/Months.js
@@ -1,19 +1,30 @@
 import { MonthsBody } from "./MonthsBody";
 import { MonthsHeader } from "./MonthsHeader";
 
-export const Months = ({ observedDate, setObservedDate, selectedDate, setSelectedScene }) => {
+export const Months = ({
+  observedDate,
+  setObservedDate,
+  selectedDate,
+  setSelectedScene,
+  minDate,
+  maxDate,
+}) => {
   return (
     <>
       <MonthsHeader
         observedDate={observedDate}
         setObservedDate={setObservedDate}
         setSelectedScene={setSelectedScene}
+        minDate={minDate}
+        maxDate={maxDate}
       />
       <MonthsBody
         observedDate={observedDate}
         setObservedDate={setObservedDate}
         setSelectedScene={setSelectedScene}
         selectedDate={selectedDate}
+        minDate={minDate}
+        maxDate={maxDate}
       />
     </>
   );
