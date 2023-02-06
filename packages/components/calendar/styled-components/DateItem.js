30f7d0f61e7f1d676a4286b0806ab04940e63abf
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-06 20:00:09 +0500

    disabled style was added

--- packages/components/calendar/styled-components/DateItem.js
+++ packages/components/calendar/styled-components/DateItem.js
@@ -5,26 +5,27 @@ export const DateItem = styled.button`
   font-weight: 600;
   font-size: 16px;
   border-radius: 50%;
-  color: ${props => props.focused ? '#4781D1' : '#333'};
+  color: ${(props) =>
+    props.disabled ? "#A3A9AE" : props.focused ? "#4781D1" : "#333"};
 
   border: 2px solid;
-  border-color: ${props => props.focused ? '#4781D1' : 'transparent'};
+  border-color: ${(props) => (props.focused ? "#4781D1" : "transparent")};
   background-color: transparent;
 
-  width: ${props => props.big ? '60px' : '40px'};
-  height: ${props => props.big ? '60px' : '40px'};
+  width: ${(props) => (props.big ? "60px" : "40px")};
+  height: ${(props) => (props.big ? "60px" : "40px")};
 
   display: inline-flex;
   justify-content: center;
   align-items: center;
 
   :hover {
-    cursor: pointer;
-    background: #f3f4f4;
+    cursor: ${(props) => (props.disabled ? "default" : "pointer")};
+    background: ${(props) => (props.disabled ? "transparent" : "#f3f4f4")};
   }
 
-  :focus{
-    color: #4781D1;
-    border: 2px solid #4781D1;
+  :focus {
+    color: #4781d1;
+    border: 2px solid #4781d1;
   }
 `;
