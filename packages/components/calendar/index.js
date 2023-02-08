2ec77ebf76b8010a3ca03bce42aef9f60a6368bd
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-08 19:46:42 +0500

    props/default props/propTypes were added to component

--- packages/components/calendar/index.js
+++ packages/components/calendar/index.js
@@ -2,20 +2,27 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import moment from "moment";
 
-import { Container } from "./styled-components";
 import { Days, Months, Years } from "./sub-components";
 import { ColorTheme, ThemeType } from "@docspace/common/components/ColorTheme";
 
-const Calendar = () => {
-  moment.locale("en");
-  const [selectedDate, setSelectedDate] = useState(moment());
+const Calendar = ({
+  locale,
+  selectedDate,
+  onChange,
+  minDate,
+  maxDate,
+  id,
+  className,
+}) => {
+  moment.locale(locale);
+  const setSelectedDate = onChange;
   const [observedDate, setObservedDate] = useState(moment());
   const [selectedScene, setSelectedScene] = useState(0);
-  const minDate = moment("2020-12-31");
-  const maxDate = moment("2030-12-1");
+  minDate = moment(minDate);
+  maxDate = moment(maxDate);
 
   return (
-    <ColorTheme themeId={ThemeType.Calendar}>
+    <ColorTheme themeId={ThemeType.Calendar} id={id} className={className}>
       {selectedScene === 0 ? (
         <Days
           observedDate={observedDate}
@@ -47,6 +54,31 @@ const Calendar = () => {
       )}
     </ColorTheme>
   );
+};
+
+Calendar.PropTypes = {
+  /** Class name */
+  className: PropTypes.string,
+  /** Used as HTML `id` property  */
+  id: PropTypes.string,
+  /** Specifies the locale of calendar */
+  locale: PropTypes.string,
+  /** Value of selected date */
+  selectedDate: PropTypes.object,
+  /** Allow you to handle changing events of component */
+  onChange: PropTypes.func,
+  /** Specifies the minimum selectable date */
+  minDate: PropTypes.object,
+  /** Specifies the maximum selectable date */
+  maxDate: PropTypes.object,
+};
+
+Calendar.defaultProps = {
+  locale: "en",
+  minDate: new Date("1970/01/01"),
+  maxDate: new Date("2040/01/01"),
+  id: "",
+  className: "",
 };
 
 export default Calendar;
