938bcbd1c52b06545f2d1d442ded9f768dabcffa
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-10 16:05:24 +0500

    numeration was deleted

--- packages/components/textarea/utils/beautifyJSON.js
+++ packages/components/textarea/utils/beautifyJSON.js
@@ -1,12 +1,7 @@
 import json_beautifier from "csvjson-json_beautifier";
 
 export function beautifyJSON(jsonString) {
-  const beautifiedValue = json_beautifier(JSON.parse(jsonString), {
+  return json_beautifier(JSON.parse(jsonString), {
     inlineShortArrays: true,
   });
-
-  return beautifiedValue
-    .split("\n")
-    .map((value, index) => index + 1 + `\t` + value)
-    .join("\n");
 }
