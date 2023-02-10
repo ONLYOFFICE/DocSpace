ee1dfa04a838beb6a21d7b1213af96af1f38c7be
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-10 16:26:00 +0500

    initial commit of test file

---
+++ packages/components/calendar/calendar.test.js
@@ -0,0 +1,13 @@
+import { unmountComponentAtNode } from "react-dom";
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
