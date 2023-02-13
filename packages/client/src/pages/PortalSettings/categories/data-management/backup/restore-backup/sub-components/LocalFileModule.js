2e69d836833521a9f448cba004a04ef28be7dc3e
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2023-02-13 11:23:24 +0300

    Web: portalSettings: Restore: Deleted useless code.

--- packages/client/src/pages/PortalSettings/categories/data-management/backup/restore-backup/sub-components/LocalFileModule.js
+++ packages/client/src/pages/PortalSettings/categories/data-management/backup/restore-backup/sub-components/LocalFileModule.js
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { inject, observer } from "mobx-react";
 
 import FileInput from "@docspace/components/file-input";
-import TextInput from "@docspace/common/components/ColorTheme/styled/textInput";
 
-let timerId = null;
-const LocalFile = ({ standalone, setRestoreResource, isEnableRestore, t }) => {
-  const [value, setValue] = useState("");
-
+const LocalFile = ({ setRestoreResource, isEnableRestore, t }) => {
   const onClickInput = (file) => {
     let data = new FormData();
     data.append("file", file);
@@ -15,18 +11,7 @@ const LocalFile = ({ standalone, setRestoreResource, isEnableRestore, t }) => {
     setRestoreResource(data);
   };
 
-  const onChange = (e) => {
-    if (timerId) {
-      timerId = null;
-      clearTimeout(timerId);
-    }
-
-    const value = e.currentTarget.value;
-    setValue(value);
-    timerId = setTimeout(() => setRestoreResource(value), 1000);
-  };
-
-  const SaaSMode = (
+  return (
     <FileInput
       onInput={onClickInput}
       scale
@@ -34,32 +19,15 @@ const LocalFile = ({ standalone, setRestoreResource, isEnableRestore, t }) => {
       isDisabled={!isEnableRestore}
     />
   );
-
-  const EnterpriseMode = (
-    <TextInput
-      onChange={onChange}
-      value={value}
-      type="text"
-      size="base"
-      scale
-      className="restore-backup_input"
-      placeholder={t("EnterPath")}
-      isDisabled={!isEnableRestore}
-    />
-  );
-
-  return standalone ? EnterpriseMode : SaaSMode;
 };
 
 export default inject(({ auth, backup }) => {
-  const { settingsStore, currentQuotaStore } = auth;
-  const { standalone } = settingsStore;
+  const { currentQuotaStore } = auth;
   const { setRestoreResource } = backup;
   const { isRestoreAndAutoBackupAvailable } = currentQuotaStore;
 
   return {
     isEnableRestore: isRestoreAndAutoBackupAvailable,
-    standalone,
     setRestoreResource,
   };
 })(observer(LocalFile));
