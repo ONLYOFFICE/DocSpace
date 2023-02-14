d40afdc4c90ad64779de6c5add60ac9b9b1913bc
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-14 17:00:00 +0500

    first letter of weekday is transformed to uppercase letter

--- packages/components/calendar/utils/getWeekdayElements.js
+++ packages/components/calendar/utils/getWeekdayElements.js
@@ -2,6 +2,8 @@ import { Weekday } from "../styled-components";
 import moment from "moment";
 
 export const getWeekdayElements = () => {
-  const weekdays = moment.weekdaysMin(true);
+  const weekdays = moment
+    .weekdaysMin(true)
+    .map((weekday) => weekday.charAt(0).toUpperCase() + weekday.substring(1));
   return weekdays.map((day) => <Weekday key={day}>{day}</Weekday>);
 };
