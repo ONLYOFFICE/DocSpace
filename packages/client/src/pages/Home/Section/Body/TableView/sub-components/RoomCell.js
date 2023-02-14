d0fb62615de93f46479be4b0af2074614ee8e06e
Mushka Nikita <nikitamushka@mail.ru>
2023-02-14 23:38:57 +0300

    added tooltip

--- packages/client/src/pages/Home/Section/Body/TableView/sub-components/RoomCell.js
+++ packages/client/src/pages/Home/Section/Body/TableView/sub-components/RoomCell.js
@@ -1,20 +1,47 @@
+import { Tooltip } from "@docspace/components";
+import Text from "@docspace/components/text";
 import React from "react";
 import { StyledText } from "./CellStyles";
 
 const RoomCell = ({ t, sideColor, item }) => {
-  const { originTitle } = item;
-  return (
+  const { originRoomTitle, originTitle } = item;
+
+  const roomTitle = originRoomTitle || originTitle;
+  const path = originRoomTitle ? originTitle : null;
+
+  return [
     <StyledText
-      title={originTitle}
+      key="cell"
+      // title={roomTitle}
       fontSize="12px"
       fontWeight={600}
       color={sideColor}
       className="row_update-text"
       truncate
+      data-for={"" + item.id}
+      data-tip={""}
+      data-place={"bottom"}
     >
-      {originTitle}
-    </StyledText>
-  );
+      {roomTitle}
+    </StyledText>,
+
+    <Tooltip
+      id={"" + item.id}
+      key="tooltip"
+      getContent={() => (
+        <div>
+          <Text isBold isInline fontSize="12px">
+            {roomTitle}
+          </Text>
+          {path && (
+            <Text isInline fontSize="12px">
+              {`/${path}`}
+            </Text>
+          )}
+        </div>
+      )}
+    />,
+  ];
 };
 
 export default RoomCell;
