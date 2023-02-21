51eaf68effcdb8fe49a0915fc4b74a9104f9ee53
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-21 13:29:54 +0500

    success badge was created

---
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/SuccessBadge/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Badge } from '@docspace/components';
+
+export const SuccessBadge = ({ label }) => (
+  <Badge backgroundColor="#2DB4821A" color="#2DB482" isHovered label={label} noHover />
+);
