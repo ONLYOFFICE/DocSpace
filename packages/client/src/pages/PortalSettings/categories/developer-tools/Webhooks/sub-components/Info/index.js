0f6e26b92fbb3f3a6064867c4cc06b06c68dbdcb
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-21 13:29:10 +0500

    info component was created

---
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/Info/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Link, InfoText, InfoWrapper } from '../../styled-components';
+
+export const Info = () => {
+  return (
+    <InfoWrapper>
+      <InfoText>
+        Use webhooks to perform custom actions on the side of any application or website you are
+        using based on various events in ONLYOFFICE Docspace. <br />
+        Here, you can create and manage all your webhooks, configure them, and browse history of
+        every webhook to audit their performance.
+      </InfoText>
+      <Link href="https://api.onlyoffice.com/portals/basic">Webhooks Guide</Link>
+    </InfoWrapper>
+  );
+};
