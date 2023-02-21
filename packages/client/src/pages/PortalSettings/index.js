2f42dc3564cf470da7948c7045571762946e3159
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-21 13:24:49 +0500

    developer tools routes were added

--- packages/client/src/pages/PortalSettings/index.js
+++ packages/client/src/pages/PortalSettings/index.js
@@ -9,70 +9,47 @@ const SecuritySettings = lazy(() => import("./categories/security/index.js"));
 
 const TfaPage = lazy(() => import("./categories/security/access-portal/tfa"));
 const PasswordStrengthPage = lazy(() =>
-  import("./categories/security/access-portal/passwordStrength")
+  import("./categories/security/access-portal/passwordStrength"),
 );
-const TrustedMailPage = lazy(() =>
-  import("./categories/security/access-portal/trustedMail")
-);
-const IpSecurityPage = lazy(() =>
-  import("./categories/security/access-portal/ipSecurity")
-);
-const AdminMessagePage = lazy(() =>
-  import("./categories/security/access-portal/adminMessage")
-);
+const TrustedMailPage = lazy(() => import("./categories/security/access-portal/trustedMail"));
+const IpSecurityPage = lazy(() => import("./categories/security/access-portal/ipSecurity"));
+const AdminMessagePage = lazy(() => import("./categories/security/access-portal/adminMessage"));
 const SessionLifetimePage = lazy(() =>
-  import("./categories/security/access-portal/sessionLifetime")
+  import("./categories/security/access-portal/sessionLifetime"),
 );
 
 const CommonSettings = lazy(() => import("./categories/common/index.js"));
 
-const DeveloperTools = lazy(() =>
-  import("./categories/developer-tools/index.js")
-);
+const DeveloperTools = lazy(() => import("./categories/developer-tools/index.js"));
 
-const CustomizationSettings = lazy(() =>
-  import("./categories/common/customization")
-);
+const CustomizationSettings = lazy(() => import("./categories/common/customization"));
 const LanguageAndTimeZoneSettings = lazy(() =>
-  import("./categories/common/Customization/language-and-time-zone")
+  import("./categories/common/Customization/language-and-time-zone"),
 );
 const WelcomePageSettings = lazy(() =>
-  import("./categories/common/Customization/welcome-page-settings")
+  import("./categories/common/Customization/welcome-page-settings"),
 );
 
-const DNSSettings = lazy(() =>
-  import("./categories/common/Customization/dns-settings")
-);
+const DNSSettings = lazy(() => import("./categories/common/Customization/dns-settings"));
 
-const PortalRenaming = lazy(() =>
-  import("./categories/common/Customization/portal-renaming")
-);
+const PortalRenaming = lazy(() => import("./categories/common/Customization/portal-renaming"));
 
 const Integration = lazy(() => import("./categories/integration"));
 const Payments = lazy(() => import("./categories/payments"));
-const ThirdParty = lazy(() =>
-  import("./categories/integration/ThirdPartyServicesSettings")
-);
-const SingleSignOn = lazy(() =>
-  import("./categories/integration/SingleSignOn")
-);
+const ThirdParty = lazy(() => import("./categories/integration/ThirdPartyServicesSettings"));
+const SingleSignOn = lazy(() => import("./categories/integration/SingleSignOn"));
 
 const Backup = lazy(() => import("./categories/data-management/backup"));
 
 const RestoreBackup = lazy(() =>
-  import("./categories/data-management/backup/restore-backup/index")
+  import("./categories/data-management/backup/restore-backup/index"),
 );
 const DeleteDataPage = lazy(() => import("./categories/delete-data"));
 
-const WhiteLabel = lazy(() =>
-  import("./categories/common/Branding/whitelabel")
-);
+const WhiteLabel = lazy(() => import("./categories/common/Branding/whitelabel"));
 
 const Branding = lazy(() => import("./categories/common/branding"));
-const PROXY_BASE_URL = combineUrl(
-  window.DocSpaceConfig?.proxy?.url,
-  "/portal-settings"
-);
+const PROXY_BASE_URL = combineUrl(window.DocSpaceConfig?.proxy?.url, "/portal-settings");
 
 const COMMON_URLS = [
   PROXY_BASE_URL,
@@ -84,8 +61,9 @@ const COMMON_URLS = [
 
 const DEVELOPER_URLS = [
   PROXY_BASE_URL,
-  combineUrl(PROXY_BASE_URL, "/developer"),
   combineUrl(PROXY_BASE_URL, "/developer/tools"),
+  combineUrl(PROXY_BASE_URL, "/developer/tools/javascript-sdk"),
+  combineUrl(PROXY_BASE_URL, "/developer/tools/webhooks"),
 ];
 
 const CUSTOMIZATION_URLS = [
@@ -103,28 +81,16 @@ const BACKUP_URLS = [
 
 const RESTORE_DATA_URL = combineUrl(PROXY_BASE_URL, "/restore");
 
-const LTZ_URL = combineUrl(
-  PROXY_BASE_URL,
-  "/common/customization/language-and-time-zone"
-);
+const LTZ_URL = combineUrl(PROXY_BASE_URL, "/common/customization/language-and-time-zone");
 const WELCOME_PAGE_SETTINGS_URL = combineUrl(
   PROXY_BASE_URL,
-  "/common/customization/welcome-page-settings"
+  "/common/customization/welcome-page-settings",
 );
 
-const DNS_SETTINGS = combineUrl(
-  PROXY_BASE_URL,
-  "/common/customization/dns-settings"
-);
+const DNS_SETTINGS = combineUrl(PROXY_BASE_URL, "/common/customization/dns-settings");
 
-const PORTAL_RENAMING = combineUrl(
-  PROXY_BASE_URL,
-  "/common/customization/portal-renaming"
-);
-const TEAM_TEMPLATE_URL = combineUrl(
-  PROXY_BASE_URL,
-  "/common/customization/team-template"
-);
+const PORTAL_RENAMING = combineUrl(PROXY_BASE_URL, "/common/customization/portal-renaming");
+const TEAM_TEMPLATE_URL = combineUrl(PROXY_BASE_URL, "/common/customization/team-template");
 const WHITELABEL_URL = combineUrl(PROXY_BASE_URL, "/common/whitelabel");
 const SECURITY_URLS = [
   combineUrl(PROXY_BASE_URL, "/security/access-rights"),
@@ -133,26 +99,11 @@ const SECURITY_URLS = [
   combineUrl(PROXY_BASE_URL, "/security/audit-trail"),
 ];
 const TFA_PAGE_URL = combineUrl(PROXY_BASE_URL, "/security/access-portal/tfa");
-const PASSWORD_PAGE_URL = combineUrl(
-  PROXY_BASE_URL,
-  "/security/access-portal/password"
-);
-const TRUSTED_MAIL_PAGE_URL = combineUrl(
-  PROXY_BASE_URL,
-  "/security/access-portal/trusted-mail"
-);
-const IP_SECURITY_PAGE_URL = combineUrl(
-  PROXY_BASE_URL,
-  "/security/access-portal/ip"
-);
-const ADMIN_MESSAGE_PAGE_URL = combineUrl(
-  PROXY_BASE_URL,
-  "/security/access-portal/admin-message"
-);
-const SESSION_LIFETIME_PAGE_URL = combineUrl(
-  PROXY_BASE_URL,
-  "/security/access-portal/lifetime"
-);
+const PASSWORD_PAGE_URL = combineUrl(PROXY_BASE_URL, "/security/access-portal/password");
+const TRUSTED_MAIL_PAGE_URL = combineUrl(PROXY_BASE_URL, "/security/access-portal/trusted-mail");
+const IP_SECURITY_PAGE_URL = combineUrl(PROXY_BASE_URL, "/security/access-portal/ip");
+const ADMIN_MESSAGE_PAGE_URL = combineUrl(PROXY_BASE_URL, "/security/access-portal/admin-message");
+const SESSION_LIFETIME_PAGE_URL = combineUrl(PROXY_BASE_URL, "/security/access-portal/lifetime");
 
 const ADMINS_URL = combineUrl(PROXY_BASE_URL, "/security/access-rights/admins");
 
@@ -165,10 +116,7 @@ const INTEGRATION_URLS = [
 
 const PAYMENTS_URL = combineUrl(PROXY_BASE_URL, "/payments/portal-payments");
 
-const THIRD_PARTY_URL = combineUrl(
-  PROXY_BASE_URL,
-  "/integration/third-party-services"
-);
+const THIRD_PARTY_URL = combineUrl(PROXY_BASE_URL, "/integration/third-party-services");
 
 const SSO_URL = combineUrl(PROXY_BASE_URL, "/integration/single-sign-on");
 const BACKUP_URL = combineUrl(PROXY_BASE_URL, "/datamanagement/backup");
@@ -178,10 +126,7 @@ const DELETE_DATA_URLS = [
   combineUrl(PROXY_BASE_URL, "/delete-data/deactivation"),
 ];
 
-const ERROR_404_URL = combineUrl(
-  window.DocSpaceConfig?.proxy?.url,
-  "/error/404"
-);
+const ERROR_404_URL = combineUrl(window.DocSpaceConfig?.proxy?.url, "/error/404");
 
 const Settings = () => {
   return (
@@ -197,11 +142,7 @@ const Settings = () => {
             component={CustomizationSettings}
           /> */}
           <Route exact path={LTZ_URL} component={LanguageAndTimeZoneSettings} />
-          <Route
-            exact
-            path={WELCOME_PAGE_SETTINGS_URL}
-            component={WelcomePageSettings}
-          />
+          <Route exact path={WELCOME_PAGE_SETTINGS_URL} component={WelcomePageSettings} />
           <Route exact path={DNS_SETTINGS} component={DNSSettings} />
           <Route exact path={PORTAL_RENAMING} component={PortalRenaming} />
           <Route exact path={WHITELABEL_URL} component={WhiteLabel} />
@@ -209,32 +150,17 @@ const Settings = () => {
           <Route exact path={SECURITY_URLS} component={SecuritySettings} />
 
           <Route exact path={TFA_PAGE_URL} component={TfaPage} />
-          <Route
-            exact
-            path={PASSWORD_PAGE_URL}
-            component={PasswordStrengthPage}
-          />
-          <Route
-            exact
-            path={TRUSTED_MAIL_PAGE_URL}
-            component={TrustedMailPage}
-          />
+          <Route exact path={PASSWORD_PAGE_URL} component={PasswordStrengthPage} />
+          <Route exact path={TRUSTED_MAIL_PAGE_URL} component={TrustedMailPage} />
           <Route exact path={IP_SECURITY_PAGE_URL} component={IpSecurityPage} />
-          <Route
-            exact
-            path={ADMIN_MESSAGE_PAGE_URL}
-            component={AdminMessagePage}
-          />
-          <Route
-            exact
-            path={SESSION_LIFETIME_PAGE_URL}
-            component={SessionLifetimePage}
-          />
+          <Route exact path={ADMIN_MESSAGE_PAGE_URL} component={AdminMessagePage} />
+          <Route exact path={SESSION_LIFETIME_PAGE_URL} component={SessionLifetimePage} />
 
           <Route exact path={INTEGRATION_URLS} component={Integration} />
           <Route exact path={PAYMENTS_URL} component={Payments} />
           <Route exact path={THIRD_PARTY_URL} component={ThirdParty} />
           <Route exact path={SSO_URL} component={SingleSignOn} />
+
           <Route exact path={DEVELOPER_URLS} component={DeveloperTools} />
 
           <Route exact path={BACKUP_URLS} component={Backup} />
