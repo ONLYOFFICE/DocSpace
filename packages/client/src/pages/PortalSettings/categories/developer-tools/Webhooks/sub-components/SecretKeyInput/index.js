897bab3a4a8c28ab5f52b2a765af83b0e8fa9d80
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-23 22:44:43 +0500

    secret key input was created

---
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/SecretKeyInput/index.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import InfoIcon from "PUBLIC_DIR/images/info.react.svg?url";
+
+import { Hint } from "../../styled-components";
+
+import { PasswordInput } from "@docspace/components";
+
+const Header = styled.h1`
+  font-family: "Open Sans";
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 20px;
+
+  margin-top: 20px;
+
+  color: #333333;
+  display: flex;
+  align-items: center;
+
+  img {
+    margin-left: 4px;
+  }
+`;
+
+const StyledInfoIcon = styled.img`
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const InfoHint = styled(Hint)`
+  position: absolute;
+  z-index: 2;
+
+  width: 320px;
+`;
+
+const ReadMore = styled.a`
+  display: inline-block;
+  margin-top: 10px;
+  font-family: "Open Sans";
+  font-weight: 600;
+  font-size: 13px;
+  line-height: 15px;
+  color: #333333;
+`;
+
+export const SecretKeyInput = () => {
+  const [isHintVisible, setIsHintVisible] = useState(false);
+
+  const toggleHint = () => setIsHintVisible((prevIsHintVisible) => !prevIsHintVisible);
+  return (
+    <div>
+      <Header>
+        Secret key <StyledInfoIcon src={InfoIcon} alt="infoIcon" onClick={toggleHint} />
+      </Header>
+
+      <InfoHint hidden={!isHintVisible} onClick={toggleHint}>
+        Setting a webhook secret allows you to verify requests sent to the payload URL. <br />
+        <ReadMore href="">Read more</ReadMore>
+      </InfoHint>
+
+      <PasswordInput
+        id=""
+        mask={null}
+        name=""
+        onBlur={() => {}}
+        onChange={function noRefCheck() {}}
+        onFocus={function noRefCheck() {}}
+        placeholder="Enter secret key"
+        isDisableTooltip={true}
+        inputType="password"
+        isFullWidth={true}
+        scale={true}
+      />
+    </div>
+  );
+};
