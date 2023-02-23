3e26d843191f62022619230fccb4c3cd00411913
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-23 22:41:27 +0500

    hint styled component was created

---
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/styled-components/Hint.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Hint = styled.div`
+  padding: 8px 12px;
+  background: #f8f7bf;
+  border-radius: 6px;
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 16px;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
