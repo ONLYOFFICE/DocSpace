d2feb4bc29a5916f34641b6840791d0a665e3a02
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-28 00:48:12 +0500

    consumer context was added/table component was fixed

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/WebhooksList/index.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/WebhooksList/index.js
@@ -1,96 +1,75 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import TableContainer from '@docspace/components/table-container/TableContainer';
-import TableHeader from '@docspace/components/table-container/TableHeader';
-import TableBody from '@docspace/components/table-container/TableBody';
+import TableContainer from "@docspace/components/table-container/TableContainer";
+import TableHeader from "@docspace/components/table-container/TableHeader";
+import TableBody from "@docspace/components/table-container/TableBody";
 
-import { WebhooksListRow } from '../WebhooksListRow';
+import { WebhooksListRow } from "../WebhooksListRow";
 
-const Header = styled(TableHeader)`
-  display: flex;
-`;
+import { Consumer } from "@docspace/components/utils/context";
 
 const TableWrapper = styled(TableContainer)`
-  margin-top: 0;
+  margin-top: 16px;
 `;
 
-export const WebhooksList = () => {
-  const wrapperRef = useRef(null);
-  const headerRef = useRef(null);
+export const WebhooksList = ({ webhooks, setWebhooks }) => {
+  const ref = useRef(null);
 
   const [columns, setColumns] = useState([
     {
-      key: 'Name',
-      title: 'Name',
+      key: "Name",
+      title: "Name",
       enable: true,
       default: true,
       active: true,
       minWidth: 200,
     },
     {
-      key: 'URL',
-      title: 'URL',
+      key: "URL",
+      title: "URL",
       enable: true,
       resizable: true,
     },
     {
-      key: 'State',
-      title: 'State',
+      key: "State",
+      title: "State",
       enable: true,
       resizable: true,
     },
-  ]);
-
-  const [rows, setRows] = useState([
     {
-      name: {
-        text: 'Webhook 1',
-        badgeTitle: '200',
-        responseStatus: 'success',
-      },
-      url: 'https://webhook.site/3d9f41d8-30dc-4f55-8b78-1649f4118c56',
-      isEnabled: true,
-    },
-    {
-      name: {
-        text: 'Webhook 2',
-        badgeTitle: '404',
-        responseStatus: 'error',
-      },
-      url: 'https://webhook.site/3d9f41d8-30dc-4f55-8b78-16',
-      isEnabled: false,
+      key: "Settings",
+      title: "",
     },
   ]);
 
   return (
-    <TableWrapper forwardedRef={wrapperRef}>
-      <Header
-        columns={columns}
-        containerRef={headerRef}
-        sectionWidth={1000}
-        checkboxMargin="12px"
-      />
-      <TableBody>
-        {rows.map((row, index) => (
-          <WebhooksListRow key={row.url} row={row} index={index} setRows={setRows} />
-        ))}
-      </TableBody>
-    </TableWrapper>
+    <Consumer>
+      {(context) => {
+        return (
+          <TableWrapper forwardedRef={ref}>
+            <TableHeader
+              columns={columns}
+              containerRef={ref}
+              checkboxMargin="12px"
+              checkboxSize="48px"
+              sectionWidth={context.sectionWidth}
+              showSettings={false}
+            />
+            <TableBody>
+              {webhooks.map((webhook, index) => (
+                <WebhooksListRow
+                  key={webhook.url}
+                  webhook={webhook}
+                  index={index}
+                  setWebhooks={setWebhooks}
+                />
+              ))}
+            </TableBody>
+          </TableWrapper>
+        );
+      }}
+    </Consumer>
   );
 };
-
-// containerRef: PropTypes.shape({ current: PropTypes.any }).isRequired,
-// columns: PropTypes.array.isRequired,
-// sortBy: PropTypes.string,
-// sorted: PropTypes.bool,
-// columnStorageName: PropTypes.string,
-// sectionWidth: PropTypes.number,
-// onClick: PropTypes.func,
-// resetColumnsSize: PropTypes.bool,
-// isLengthenHeader: PropTypes.bool,
-// sortingVisible: PropTypes.bool,
-// infoPanelVisible: PropTypes.bool,
-// useReactWindow: PropTypes.bool,
-// showSettings: PropTypes.bool,
