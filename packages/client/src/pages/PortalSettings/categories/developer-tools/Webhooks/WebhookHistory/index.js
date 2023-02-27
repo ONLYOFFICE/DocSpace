17c5758237fae50c2affd593f2108ba04bad6daf
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-28 00:49:55 +0500

    html skelet of webhook history page was created

---
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const WebhookHistory = () => {
+  return (
+    <>
+      <header>
+        <a href="">arrow</a>
+        History
+        <span>Deliveries are automatically deleted after 15 days</span>
+      </header>
+      <body>
+        <header>
+          Webhook 32
+          <button>filter</button>
+        </header>
+      </body>
+    </>
+  );
+};
+
+export default WebhookHistory;
