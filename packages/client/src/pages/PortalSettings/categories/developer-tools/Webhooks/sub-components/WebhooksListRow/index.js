bcb663f3f50ceef4ad36a4b86d25466b326afa5f
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-02-28 00:49:17 +0500

    delete and setting webhook dialog windows were added/context items were passed correctly

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/WebhooksListRow/index.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/WebhooksListRow/index.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
-import TableRow from '@docspace/components/table-container/TableRow';
-import TableCell from '@docspace/components/table-container/TableCell';
-import styled from 'styled-components';
-import { FailBadge } from '../FailBadge';
-import { SuccessBadge } from '../SuccessBadge';
-import { ToggleButton } from '@docspace/components';
-import ContextMenuButton from '@docspace/components/context-menu-button';
-import DropDownItem from '@docspace/components/drop-down-item';
-import SettingsIcon from 'PUBLIC_DIR/images/settings.webhooks.react.svg?url';
-import HistoryIcon from 'PUBLIC_DIR/images/history.react.svg?url';
-import RetryIcon from 'PUBLIC_DIR/images/refresh.react.svg?url';
-import DeleteIcon from 'PUBLIC_DIR/images/delete.react.svg?url';
+import React, { useState } from "react";
+import TableRow from "@docspace/components/table-container/TableRow";
+import TableCell from "@docspace/components/table-container/TableCell";
+import styled from "styled-components";
+import { FailBadge } from "../FailBadge";
+import { SuccessBadge } from "../SuccessBadge";
+import { ToggleButton } from "@docspace/components";
+import ContextMenuButton from "@docspace/components/context-menu-button";
+import SettingsIcon from "PUBLIC_DIR/images/settings.webhooks.react.svg?url";
+import HistoryIcon from "PUBLIC_DIR/images/history.react.svg?url";
+import RetryIcon from "PUBLIC_DIR/images/refresh.react.svg?url";
+import DeleteIcon from "PUBLIC_DIR/images/delete.react.svg?url";
+import { WebhookDialog } from "../WebhookDialog";
+import { DeleteWebhookDialog } from "../DeleteWebhookDialog";
 
-const Row = styled(TableRow)`
-  display: flex;
-  flex-direction: row;
-`;
-
-export const WebhooksListRow = ({ row, index, setRows }) => {
-  const [isChecked, setIsChecked] = useState(row.isEnabled);
+export const WebhooksListRow = ({ webhook, index, setWebhooks }) => {
+  const [isChecked, setIsChecked] = useState(webhook.isEnabled);
 
   const [isOpen, setIsOpen] = useState(false);
+
+  const [isSettings, setIsSettings] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const onSettingsClose = () => {
+    setIsSettings(false);
+  };
+  const onDeleteClose = () => {
+    setIsDelete(false);
+  };
 
   const onClickHandler = () => {
     setIsOpen((prevIsOpen) => !prevIsOpen);
@@ -29,95 +34,67 @@ export const WebhooksListRow = ({ row, index, setRows }) => {
   const getData = () => {
     return [
       {
-        key: 'key1',
-        label: (
-          <DropDownItem
-            isSeparator={false}
-            isHeader={false}
-            label="Settings"
-            icon={SettingsIcon}
-            onClick={() => console.log('Button 1 clicked')}
-          />
-        ),
-        onClick: () => console.log('label1'),
+        key: "key1",
+        label: "Settings",
+        icon: SettingsIcon,
+        onClick: () => setIsSettings(true),
       },
       {
-        key: 'key2',
-        label: (
-          <DropDownItem
-            isSeparator={false}
-            isHeader={false}
-            label="Webhook history"
-            icon={HistoryIcon}
-            onClick={() => console.log('Button 1 clicked')}
-          />
-        ),
-        onClick: () => console.log('label2'),
+        key: "key2",
+        label: "Webhook history",
+        icon: HistoryIcon,
+        onClick: () => console.log("Button 1 clicked"),
       },
       {
-        key: 'key3',
-        label: (
-          <DropDownItem
-            isSeparator={false}
-            isHeader={false}
-            label="Retry"
-            icon={RetryIcon}
-            onClick={() => console.log('Button 1 clicked')}
-          />
-        ),
-        onClick: () => console.log('label2'),
+        key: "key3",
+        label: "Retry",
+        icon: RetryIcon,
+        onClick: () => console.log("Button 1 clicked"),
       },
       {
-        key: 'key4',
-        label: <DropDownItem isSeparator={true} />,
-        onClick: () => console.log('label2'),
+        key: "key4",
+        isSeparator: true,
+        onClick: () => console.log("label2"),
       },
       {
-        key: 'key5',
-        label: (
-          <DropDownItem
-            isSeparator={false}
-            isHeader={false}
-            label="Delete webhook"
-            icon={DeleteIcon}
-            onClick={() => console.log('Button 1 clicked')}
-          />
-        ),
-        onClick: () => console.log('label2'),
+        key: "key5",
+        label: "Delete webhook",
+        icon: DeleteIcon,
+        onClick: () => setIsDelete(true),
       },
     ];
   };
 
   return (
-    <Row>
-      <TableCell>
-        {row.name.text}{' '}
-        {row.name.responseStatus === 'success' ? (
-          <SuccessBadge label={row.name.badgeTitle} />
-        ) : row.name.responseStatus === 'error' ? (
-          <FailBadge label={row.name.badgeTitle} />
-        ) : (
-          <span></span>
-        )}
-      </TableCell>
-      <TableCell>{row.url}</TableCell>
-      <TableCell>
-        <ToggleButton
-          className="toggle className"
-          id="toggle id"
-          isChecked={isChecked}
-          onChange={() => {
-            setRows((prevRows) => {
-              prevRows[index].isEnabled = !prevRows[index].isEnabled;
-              return prevRows;
-            });
-            setIsChecked((prevIsChecked) => !prevIsChecked);
-          }}
-        />
-      </TableCell>
+    <>
+      <TableRow>
+        <TableCell>
+          {webhook.title}{" "}
+          {webhook.responseStatus === "success" ? (
+            <SuccessBadge label={webhook.badgeTitle} />
+          ) : webhook.responseStatus === "error" ? (
+            <FailBadge label={webhook.badgeTitle} />
+          ) : (
+            <span></span>
+          )}
+        </TableCell>
+        <TableCell>{webhook.url}</TableCell>
+        <TableCell>
+          <ToggleButton
+            className="toggle toggleButton"
+            id="toggle id"
+            isChecked={isChecked}
+            onChange={() => {
+              setWebhooks((prevWebhooks) => {
+                prevWebhooks[index].isEnabled = !prevWebhooks[index].isEnabled;
+                return prevWebhooks;
+              });
+              setIsChecked((prevIsChecked) => !prevIsChecked);
+            }}
+          />
+        </TableCell>
 
-      <TableCell>
-        <div>
+        <TableCell>
           <ContextMenuButton
             directionX="right"
             getData={getData}
@@ -126,8 +103,32 @@ export const WebhooksListRow = ({ row, index, setRows }) => {
             onClick={onClickHandler}
             title="Actions"
           />
-        </div>
-      </TableCell>
-    </Row>
+        </TableCell>
+      </TableRow>
+      <WebhookDialog
+        visible={isSettings}
+        onClose={onSettingsClose}
+        header="Setting webhook"
+        isSettingsModal={true}
+        webhook={webhook}
+        onSubmit={(webhookInfo) => {
+          setWebhooks((prevWebhooks) =>
+            prevWebhooks.map((prevWebhook) =>
+              prevWebhook.url === webhook.url ? webhookInfo : prevWebhook,
+            ),
+          );
+        }}
+      />
+      <DeleteWebhookDialog
+        visible={isDelete}
+        onClose={onDeleteClose}
+        header="Delete Webhook forever?"
+        handleSubmit={() => {
+          setWebhooks((prevWebhooks) =>
+            prevWebhooks.filter((webhookInfo) => webhookInfo.url !== webhook.url),
+          );
+        }}
+      />
+    </>
   );
 };
