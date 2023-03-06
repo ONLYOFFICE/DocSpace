5475c80893be2db6136676a5f67e3ce53ef4a8f2
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-03-06 15:57:22 +0500

    set document title to developer tools

--- packages/client/src/pages/PortalSettings/categories/developer-tools/index.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/index.js
@@ -1,31 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import Submenu from '@docspace/components/submenu';
-import { withRouter } from 'react-router';
-import { withTranslation } from 'react-i18next';
-import { inject, observer } from 'mobx-react';
-import { combineUrl } from '@docspace/common/utils';
-import config from 'PACKAGE_FILE';
+import React, { useEffect, useState } from "react";
+import Submenu from "@docspace/components/submenu";
+import { withRouter } from "react-router";
+import { withTranslation } from "react-i18next";
+import { inject, observer } from "mobx-react";
+import { combineUrl } from "@docspace/common/utils";
+import config from "PACKAGE_FILE";
 
-import JavascriptSDK from './JavascriptSDK';
-import Webhooks from './Webhooks';
+import JavascriptSDK from "./JavascriptSDK";
+import Webhooks from "./Webhooks";
 
-import AppLoader from '@docspace/common/components/AppLoader';
-import SSOLoader from './sub-components/ssoLoader';
+import AppLoader from "@docspace/common/components/AppLoader";
+import SSOLoader from "./sub-components/ssoLoader";
 
 const DeveloperToolsWrapper = (props) => {
-  const { t, tReady, history, loadBaseInfo } = props;
+  const { t, tReady, history, loadBaseInfo, setDocumentTitle } = props;
   const [currentTab, setCurrentTab] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
+  setDocumentTitle("Developer Tools");
+
   const data = [
     {
-      id: 'javascript-sdk',
-      name: t('Javascript sdk'),
+      id: "javascript-sdk",
+      name: t("Javascript sdk"),
       content: <JavascriptSDK />,
     },
     {
-      id: 'webhooks',
-      name: t('Webhooks'),
+      id: "webhooks",
+      name: t("Webhooks"),
       content: <Webhooks />,
     },
   ];
@@ -60,14 +62,16 @@ const DeveloperToolsWrapper = (props) => {
 
 export default inject(({ setup, auth }) => {
   const { initSettings } = setup;
+  const { setDocumentTitle } = auth;
 
   return {
     loadBaseInfo: async () => {
       await initSettings();
     },
+    setDocumentTitle
   };
 })(
-  withTranslation(['Settings', 'SingleSignOn', 'Translations'])(
+  withTranslation(["Settings", "SingleSignOn", "Translations"])(
     withRouter(observer(DeveloperToolsWrapper)),
   ),
 );
