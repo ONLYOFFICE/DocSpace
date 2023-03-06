dff264e97c5b71f8e8667a80f8c0984c41dbc925
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-03-06 15:58:28 +0500

    webhooks store was integrated / webhooks interaction functions were added

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/index.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/index.js
@@ -5,36 +5,31 @@ import { Info } from "./sub-components/Info";
 import { WebhooksList } from "./sub-components/WebhooksList";
 import { setDocumentTitle } from "SRC_DIR/helpers/utils";
 
+import { inject, observer } from "mobx-react";
+
 import styled from "styled-components";
 
 const MainWrapper = styled.div`
   width: 100%;
-  
+
   .toggleButton {
     display: flex;
     align-items: center;
   }
 `;
 
-const Webhooks = () => {
+const Webhooks = (props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const [webhooks, setWebhooks] = useState([
-    {
-      title: "Webhook 1",
-      responseCode: "200",
-      responseStatus: "success",
-      url: "https://webhook.site/3d9f41d8-30dc-4f55-8b78-1649f4118c56",
-      isEnabled: true,
-    },
-    {
-      title: "Webhook 2",
-      responseCode: "404",
-      responseStatus: "error",
-      url: "https://webhook.site/3d9f41d8-30dc-4f55-8b78-16",
-      isEnabled: false,
-    },
-  ]);
+  const {
+    webhooks,
+    addWebhook,
+    isWebhookExist,
+    isWebhooksEmpty,
+    toggleEnabled,
+    deleteWebhook,
+    editWebhook,
+  } = props;
 
   const closeModal = () => {
     setIsModalOpen(false);
@@ -48,14 +43,21 @@ const Webhooks = () => {
     <MainWrapper>
       <Info />
       <Button label="Create webhook" primary size="small" onClick={() => setIsModalOpen(true)} />
-      {webhooks.length > 0 && <WebhooksList webhooks={webhooks} setWebhooks={setWebhooks} />}
+      {!isWebhooksEmpty && (
+        <WebhooksList
+          webhooks={webhooks}
+          toggleEnabled={toggleEnabled}
+          deleteWebhook={deleteWebhook}
+          editWebhook={editWebhook}
+        />
+      )}
       <WebhookDialog
         visible={isModalOpen}
         onClose={closeModal}
         header="Create webhook"
         onSubmit={(webhookInfo) => {
-          if (!webhooks.find((webhook) => webhook.url === webhookInfo.url)) {
-            setWebhooks((prevWebhooks) => [webhookInfo, ...prevWebhooks]);
+          if (!isWebhookExist(webhookInfo)) {
+            addWebhook(webhookInfo);
             closeModal();
           }
         }}
@@ -64,4 +66,24 @@ const Webhooks = () => {
   );
 };
 
-export default Webhooks;
+export default inject(({ webhooksStore }) => {
+  const {
+    webhooks,
+    addWebhook,
+    isWebhookExist,
+    isWebhooksEmpty,
+    toggleEnabled,
+    deleteWebhook,
+    editWebhook,
+  } = webhooksStore;
+
+  return {
+    webhooks,
+    addWebhook,
+    isWebhookExist,
+    isWebhooksEmpty,
+    toggleEnabled,
+    deleteWebhook,
+    editWebhook,
+  };
+})(observer(Webhooks));
