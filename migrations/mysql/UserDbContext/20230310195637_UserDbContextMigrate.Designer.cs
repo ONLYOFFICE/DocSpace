7a5d007858d86f577fb85794ca524603f708c597
pavelbannov <pavel.bannov@onlyoffice.com>
2023-03-10 23:13:19 +0300

    fixed migration

--- migrations/mysql/UserDbContext/20230310195637_UserDbContextMigrate.Designer.cs
+++ migrations/mysql/UserDbContext/20230310195637_UserDbContextMigrate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ASC.Core.Common.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASC.Migrations.MySql.Migrations.UserDb
 {
     [DbContext(typeof(UserDbContext))]
-    partial class UserDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230310195637_UserDbContextMigrate")]
+    partial class UserDbContextMigrate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
