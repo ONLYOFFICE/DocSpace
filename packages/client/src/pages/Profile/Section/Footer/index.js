52c90296f196a673d28ec9ba1a12c9057001a092
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2023-03-15 12:07:38 +0300

    Web:Client:Add Loader for ActiveSessions.

--- packages/client/src/pages/Profile/Section/Footer/index.js
+++ packages/client/src/pages/Profile/Section/Footer/index.js
@@ -11,6 +11,8 @@ import Link from "@docspace/components/link";
 import Box from "@docspace/components/box";
 import HelpButton from "@docspace/components/help-button";
 import toastr from "@docspace/components/toast/toastr";
+import Loaders from "@docspace/common/components/Loaders";
+import withPeopleLoader from "../../../../HOCs/withPeopleLoader";
 
 import {
   LogoutConnectionDialog,
@@ -263,4 +265,12 @@ export default inject(({ auth, setup }) => {
     setLogoutAllVisible,
     removeAllExecptThis,
   };
-})(observer(withTranslation(["Profile", "Common"])(ActiveSessions)));
+})(
+  observer(
+    withTranslation(["Profile", "Common"])(
+      withPeopleLoader(ActiveSessions)(
+        <Loaders.ProfileFooter isProfileFooter />
+      )
+    )
+  )
+);
