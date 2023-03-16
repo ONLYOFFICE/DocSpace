989821a6a38c60e55a3f635c315cd8024fd07862
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2023-03-16 12:27:11 +0300

    Revert "Web:Client:For a list of 4 items, set disableMobileView to false otherwise the list of users in the invite panel changed styles incorrectly."
    This reverts commit 0845b1c0ebcba0bd66097c20c27dfb5c2b02bb77.

--- packages/components/combobox/index.js
+++ packages/components/combobox/index.js
@@ -159,7 +159,7 @@ class ComboBox extends React.Component {
         : 6;
     }
 
-    const disableMobileView = optionsCount < 4;
+    const disableMobileView = optionsCount < 5;
 
     return (
       <StyledComboBox
