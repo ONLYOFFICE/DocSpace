de2ef53b88fe7b8fd2643562be649f575817cc75
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-03-17 06:06:47 +0200

    state was added / all requests were restructured

--- packages/client/src/store/WebhooksStore.js
+++ packages/client/src/store/WebhooksStore.js
@@ -1,70 +1,95 @@
-import { makeAutoObservable } from "mobx";
+import { runInAction, action, makeAutoObservable } from "mobx";
 import {
   createWebhook,
   getAllWebhooks,
   updateWebhook,
   removeWebhook,
-  getJournal,
+  toggleEnabledWebhook,
+  getWebhooksJournal,
   retryWebhook,
   retryWebhooks,
 } from "@docspace/common/api/settings";
 
+// {
+//       id: 0,
+//       title: "Webhook 1",
+//       responseCode: "200",
+//       responseStatus: "success",
+//       url: "https://webhook.site/3d9f41d8-30dc-4f55-8b78-1649f4118c56",
+//       isEnabled: true,
+//     },
+//     {
+//       id: 1,
+//       title: "Webhook 2",
+//       responseCode: "404",
+//       responseStatus: "error",
+//       url: "https://webhook.site/3d9f41d8-30dc-4f55-8b78-16",
+//       isEnabled: false,
+//     },
+
 class WebhooksStore {
-  webhooks = [
-    {
-      id: 0,
-      title: "Webhook 1",
-      responseCode: "200",
-      responseStatus: "success",
-      url: "https://webhook.site/3d9f41d8-30dc-4f55-8b78-1649f4118c56",
-      isEnabled: true,
-    },
-    {
-      id: 1,
-      title: "Webhook 2",
-      responseCode: "404",
-      responseStatus: "error",
-      url: "https://webhook.site/3d9f41d8-30dc-4f55-8b78-16",
-      isEnabled: false,
-    },
-  ];
+  webhooks = [];
+  state = "pending"; // "pending", "done" or "error"
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  loadWebhookhs = async () => {
-    const webhooks = await getAllWebhooks();
-    this.webhooks = webhooks;
+  loadWebhooks = async () => {
+    try {
+      const webhooksData = await getAllWebhooks();
+      runInAction(() => {
+        this.webhooks = webhooksData.map((data) => ({
+          ...data,
+          url: data.uri,
+          title: data.name,
+          isEnabled: data.enabled,
+        }));
+        this.state = "success";
+      });
+    } catch (error) {
+      console.error(error);
+      this.state = "error";
+    }
   };
 
   addWebhook = async (webhook) => {
-    // const webhookData = await createWebhook(webhook.title, webhook.url, webhook.secretKey);
-    // this.webhooks.push({
-    //   id: webhookData.Id,
-    //   url: webhookData.Uri,
-    //   title: "default",
-    //   isEnabled: webhookData.Enabled,
-    // });
-    this.webhooks.push(webhook);
+    const webhookData = await createWebhook(webhook.title, webhook.url, webhook.secretKey);
+
+    this.webhooks = [
+      ...this.webhooks,
+      {
+        id: webhookData.id,
+        url: webhookData.uri,
+        title: webhookData.name,
+        isEnabled: webhookData.enabled,
+        secretKey: webhookData.secretKey,
+      },
+    ];
   };
 
   isWebhookExist = (desiredWebhook) => {
     return this.webhooks.some((webhook) => webhook.id === desiredWebhook.id);
   };
 
-  toggleEnabled = (desiredWebhook) => {
+  toggleEnabled = async (desiredWebhook) => {
+    await toggleEnabledWebhook(desiredWebhook);
     const index = this.webhooks.findIndex((webhook) => webhook.id === desiredWebhook.id);
     this.webhooks[index].isEnabled = !this.webhooks[index].isEnabled;
   };
 
   deleteWebhook = async (webhook) => {
-    // await removeWebhook(webhook.id);
+    await removeWebhook(webhook.id);
     this.webhooks = this.webhooks.filter((currentWebhook) => currentWebhook.id !== webhook.id);
   };
 
   editWebhook = async (prevWebhook, webhookInfo) => {
-    // await updateWebhook(webhookInfo.title, webhookInfo.url, webhookInfo.secretKey);
+    await updateWebhook(
+      prevWebhook.id,
+      webhookInfo.title,
+      webhookInfo.url,
+      webhookInfo.secretKey || prevWebhook.secretKey,
+    );
     this.webhooks = this.webhooks.map((webhook) =>
       webhook.id === prevWebhook.id ? { ...prevWebhook, ...webhookInfo } : webhook,
     );
@@ -79,7 +104,7 @@ class WebhooksStore {
   };
 
   getWebhookHistory = async (hookname, logCount) => {
-    return await getJournal(hookname, logCount);
+    return await getWebhooksJournal(hookname, logCount);
   };
 
   get isWebhooksEmpty() {
