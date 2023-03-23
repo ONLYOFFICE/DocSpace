68e3d036d7d42e78d9ea6e1c5160e531f7aaab83
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-03-23 14:08:41 +0500

    color and background color are recieved from props

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/styled-components/Hint.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/styled-components/Hint.js
@@ -2,15 +2,12 @@ import styled from "styled-components";
 
 export const Hint = styled.div`
   padding: 8px 12px;
-  background: #f8f7bf;
+  background: ${(props) => (props.backgroundColor ? props.backgroundColor : "#f8f7bf")};
+  color: ${(props) => (props.color ? props.color : "initial")};
   border-radius: 6px;
   font-family: "Open Sans";
   font-style: normal;
   font-weight: 400;
   font-size: 12px;
   line-height: 16px;
-
-  :hover {
-    cursor: pointer;
-  }
 `;
