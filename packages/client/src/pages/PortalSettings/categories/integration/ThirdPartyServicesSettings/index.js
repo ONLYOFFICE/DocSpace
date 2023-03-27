87ac1492477a240041edea1c250e1c118fcb7d52
Elyor Djalilov <edjalilov.dev@gmail.com>
2023-03-27 22:40:34 +0500

    Fix Bug 61785 - Settings: ThirdParty. Change links to helpcenter articles for docspace

--- packages/client/src/pages/PortalSettings/categories/integration/ThirdPartyServicesSettings/index.js
+++ packages/client/src/pages/PortalSettings/categories/integration/ThirdPartyServicesSettings/index.js
@@ -123,7 +123,7 @@ class ThirdPartyServices extends React.Component {
       i18n,
       consumers,
       updateConsumerProps,
-      urlAuthKeys,
+      integrationSettingsUrl,
       theme,
       currentColorScheme,
     } = this.props;
@@ -141,7 +141,7 @@ class ThirdPartyServices extends React.Component {
               color={currentColorScheme.main.accent}
               isHovered
               target="_blank"
-              href={urlAuthKeys}
+              href={integrationSettingsUrl}
             >
               {t("Common:LearnMore")}
             </Link>
@@ -187,7 +187,7 @@ ThirdPartyServices.propTypes = {
   t: PropTypes.func.isRequired,
   i18n: PropTypes.object.isRequired,
   consumers: PropTypes.arrayOf(PropTypes.object).isRequired,
-  urlAuthKeys: PropTypes.string,
+  integrationSettingsUrl: PropTypes.string,
   getConsumers: PropTypes.func.isRequired,
   updateConsumerProps: PropTypes.func.isRequired,
   setSelectedConsumer: PropTypes.func.isRequired,
@@ -195,7 +195,7 @@ ThirdPartyServices.propTypes = {
 
 export default inject(({ setup, auth }) => {
   const { settingsStore, setDocumentTitle } = auth;
-  const { urlAuthKeys, theme, currentColorScheme } = settingsStore;
+  const { integrationSettingsUrl, theme, currentColorScheme } = settingsStore;
   const {
     getConsumers,
     integration,
@@ -207,7 +207,7 @@ export default inject(({ setup, auth }) => {
   return {
     theme,
     consumers,
-    urlAuthKeys,
+    integrationSettingsUrl,
     getConsumers,
     updateConsumerProps,
     setSelectedConsumer,
