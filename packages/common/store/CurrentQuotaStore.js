7356bd617090c6ba9097beb23a34a28236b62500
Viktor Fomin <enotcode@gmail.com>
2023-03-29 13:56:58 +0300

    Common: add isThirdPartyAvailable

--- packages/common/store/CurrentQuotaStore.js
+++ packages/common/store/CurrentQuotaStore.js
@@ -116,6 +116,14 @@ class QuotasStore {
     return result?.value;
   }
 
+  get isThirdPartyAvailable() {
+    const result = this.currentPortalQuotaFeatures.find(
+      (obj) => obj.id === "thirdparty"
+    );
+
+    return result?.value;
+  }
+
   get isSSOAvailable() {
     const result = this.currentPortalQuotaFeatures.find(
       (obj) => obj.id === "sso"
