cbc64bb779de6bb1d550b1e1b1fa6eff6a32fb85
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-03-31 19:20:47 +0500

    filters were added

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/FilterDialog.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/FilterDialog.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import moment from "moment";
+
 import ModalDialog from "@docspace/components/modal-dialog";
-import Button from "@docspace/components/button";
 import styled from "styled-components";
 
 import { Text } from "@docspace/components";
@@ -8,6 +9,7 @@ import { SelectorAddButton } from "@docspace/components";
 import { SelectedItem } from "@docspace/components";
 
 import { Calendar } from "@docspace/components";
+import { Button } from "@docspace/components";
 
 const Footer = styled.div`
   width: 100%;
@@ -22,24 +24,30 @@ const Footer = styled.div`
 `;
 
 const Selectors = styled.div`
+  position: relative;
   margin-top: 12px;
   margin-bottom: 16px;
 `;
 
 const Separator = styled.hr`
   border-top: 1px solid #eceef1;
+  margin-bottom: 16px;
+`;
+
+const RoundedButton = styled(Button)`
+  font-size: 13px;
+  font-weight: 400;
+
+  border-radius: 16px;
+  margin-right: 8px;
 `;
 
 export const FilterDialog = (props) => {
   const { visible, closeModal, applyFilters } = props;
   const [isCalendarOpen, setIsCalendarOpen] = useState(false);
 
-  const openCalendar = () => setIsCalendarOpen(true);
+  const toggleCalendar = () => setIsCalendarOpen((prevIsCalendarOpen) => !prevIsCalendarOpen);
   const closeCalendar = () => setIsCalendarOpen(false);
-
-  const setDeliveryDate = (date) => {
-    setFilterSettings((prevFilterSetting) => ({ ...prevFilterSetting, deliveryDate: date }));
-  };
 
   const [filterSettings, setFilterSettings] = useState({
     deliveryDate: null,
@@ -47,7 +55,30 @@ export const FilterDialog = (props) => {
     status: [],
   });
 
-  console.log(filterSettings);
+  const setDeliveryDate = (date) =>
+    setFilterSettings((prevFilterSetting) => ({ ...prevFilterSetting, deliveryDate: date }));
+
+  const deleteSelectedDate = () =>
+    setFilterSettings((prevFilterSetting) => ({ ...prevFilterSetting, deliveryDate: null }));
+
+  const toggleStatus = (e) => {
+    const statusName = e.target.textContent;
+    if (filterSettings.status.includes(statusName)) {
+      setFilterSettings((prevFilterSetting) => ({
+        ...prevFilterSetting,
+        status: prevFilterSetting.status.filter((statusItem) => statusItem !== statusName),
+      }));
+    } else {
+      setFilterSettings((prevFilterSetting) => ({
+        ...prevFilterSetting,
+        status: [...prevFilterSetting.status, statusName],
+      }));
+    }
+  };
+
+  const isStatusSelected = (statusName) => {
+    return filterSettings.status.includes(statusName);
+  };
 
   return (
     <ModalDialog withFooterBorder visible={visible} onClose={closeModal} displayType="aside">
@@ -57,23 +88,64 @@ export const FilterDialog = (props) => {
           Delivery date
         </Text>
         <Selectors>
-          <span>
-            <SelectorAddButton title="add" onClick={openCalendar} style={{ marginRight: "8px" }} />
-            <Text isInline fontWeight={600} color="#A3A9AE">
-              Select date
-            </Text>
-            {isCalendarOpen ? (
-              <Calendar
-                selectedDate={filterSettings.deliveryDate}
-                setSelectedDate={setDeliveryDate}
-                onChange={closeCalendar}
+          {filterSettings.deliveryDate === null ? (
+            <span>
+              <SelectorAddButton
+                title="add"
+                onClick={toggleCalendar}
+                style={{ marginRight: "8px" }}
               />
-            ) : (
-              <></>
-            )}
-          </span>
-
-          {/* <SelectedItem onClose={() => {}} text="Selected item" /> */}
+              <Text isInline fontWeight={600} color="#A3A9AE">
+                Select date
+              </Text>
+              {isCalendarOpen ? (
+                <Calendar
+                  selectedDate={filterSettings.deliveryDate}
+                  setSelectedDate={setDeliveryDate}
+                  onChange={closeCalendar}
+                />
+              ) : (
+                <></>
+              )}
+            </span>
+          ) : (
+            <SelectedItem
+              onClose={deleteSelectedDate}
+              text={moment(filterSettings.deliveryDate).format("DD MMM YYYY")}
+            />
+          )}
+          {/* {filterSettings.deliveryDate !== null ? (
+            <span>
+              <SelectorAddButton
+                title="add"
+                onClick={toggleCalendar}
+                style={{ marginRight: "8px" }}
+              />
+              <Text isInline fontWeight={600} color="#A3A9AE">
+                Select Delivery time
+              </Text>
+            </span>
+          ) : (
+            <SelectedItem
+              onClose={deleteSelectedDate}
+              text={moment(filterSettings.deliveryDate).format("DD MMM YYYY")}
+            />
+          )} */}
+        </Selectors>
+        <Separator />
+        <Text fontWeight={600} fontSize="15px">
+          Status
+        </Text>
+        <Selectors>
+          <RoundedButton
+            label="Not sent"
+            onClick={toggleStatus}
+            primary={isStatusSelected("Not sent")}
+          />
+          <RoundedButton label="2XX" onClick={toggleStatus} primary={isStatusSelected("2XX")} />
+          <RoundedButton label="3XX" onClick={toggleStatus} primary={isStatusSelected("3XX")} />
+          <RoundedButton label="4XX" onClick={toggleStatus} primary={isStatusSelected("4XX")} />
+          <RoundedButton label="5XX" onClick={toggleStatus} primary={isStatusSelected("5XX")} />
         </Selectors>
         <Separator />
       </ModalDialog.Body>
