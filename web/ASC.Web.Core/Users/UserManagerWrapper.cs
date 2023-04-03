91606f1962a31ec5efb5ae5f457e9d31e78b7643
Vashchuk Nikita <nikita.vashchuk@onlyoffice.com>
2023-04-03 16:38:23 +0500

    small fix

--- web/ASC.Web.Core/Users/UserManagerWrapper.cs
+++ web/ASC.Web.Core/Users/UserManagerWrapper.cs
@@ -24,10 +24,6 @@
 // content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
 // International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
 
-
-
-using ASC.Core.Common.Quota;
-
 using Constants = ASC.Core.Users.Constants;
 
 namespace ASC.Web.Core.Users;
