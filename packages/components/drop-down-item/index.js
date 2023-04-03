54ce892d699275e6a202df025013c7b2e80f32d3
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2023-04-03 19:31:16 +0500

    Web:Client Added live chat toggle

--- packages/components/drop-down-item/index.js
+++ packages/components/drop-down-item/index.js
@@ -4,8 +4,12 @@ import { ReactSVG } from "react-svg";
 
 import RightArrowReactSvgUrl from "PUBLIC_DIR/images/right.arrow.react.svg?url";
 
-import { StyledDropdownItem, IconWrapper } from "./styled-drop-down-item";
-import { isNull } from "lodash";
+import {
+  StyledDropdownItem,
+  IconWrapper,
+  WrapperToggle,
+} from "./styled-drop-down-item";
+import ToggleButton from "../toggle-button";
 
 const DropDownItem = (props) => {
   //console.log("DropDownItem render");
@@ -15,7 +19,6 @@ const DropDownItem = (props) => {
     icon,
     children,
     disabled,
-    onClick,
     className,
     theme,
     fillIcon,
@@ -28,19 +31,31 @@ const DropDownItem = (props) => {
     isActiveDescendant,
   } = props;
 
+  const { withToggle, checked, onClick, ...rest } = props;
+
   const onClickAction = (e) => {
     onClick && !disabled && onClick(e);
   };
 
+  const stopPropagation = (event) => {
+    event.stopPropagation();
+  };
+
+  const onChange = (event) => {
+    stopPropagation(event);
+    if (!disabled) onClick?.();
+  };
+
   return (
     <StyledDropdownItem
-      {...props}
+      {...rest}
       noHover={noHover}
       className={className}
       onClick={onClickAction}
       disabled={disabled}
       isActive={isActive}
       isSelected={isSelected}
+      withToggle={withToggle}
       isActiveDescendant={isActiveDescendant}
     >
       {icon && (
@@ -67,6 +82,12 @@ const DropDownItem = (props) => {
             className="drop-down-item_icon"
           />
         </IconWrapper>
+      )}
+
+      {withToggle && (
+        <WrapperToggle onClick={stopPropagation}>
+          <ToggleButton isChecked={checked} onChange={onChange} />
+        </WrapperToggle>
       )}
     </StyledDropdownItem>
   );
