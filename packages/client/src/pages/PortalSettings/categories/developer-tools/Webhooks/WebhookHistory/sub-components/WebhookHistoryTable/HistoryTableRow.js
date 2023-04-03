16dbc6f56dec46fdde8ca07bf773f7cf4c53484e
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-04-03 16:31:10 +0500

    checkbox was added

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/WebhookHistoryTable/HistoryTableRow.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/WebhookHistoryTable/HistoryTableRow.js
@@ -5,6 +5,7 @@ import RetryIcon from "PUBLIC_DIR/images/refresh.react.svg?url";
 import InfoIcon from "PUBLIC_DIR/images/info.outline.react.svg?url";
 
 import Text from "@docspace/components/text";
+import Checkbox from "@docspace/components/checkbox";
 import { StatusBadge } from "../../../sub-components/StatusBadge";
 
 export const HistoryTableRow = ({ eventData }) => {
@@ -21,9 +22,25 @@ export const HistoryTableRow = ({ eventData }) => {
     },
   ];
 
+  const onContentSelect = (boolVal, obj) => {};
+
+  const onChange = (e) => {
+    onContentSelect && onContentSelect(e.target.checked, item);
+  };
+
   return (
     <TableRow contextOptions={contextOptions}>
       <TableCell>
+        <TableCell>
+          <Checkbox
+            className="table-container_row-checkbox"
+            onChange={onChange}
+            isChecked={false}
+            title="TitleSelectFile"
+            label="abc"
+          />
+        </TableCell>
+
         <Text fontWeight={600}>{eventData.id}</Text>
       </TableCell>
       <TableCell>
