ff30e6c2de7c8c2cf0fb8a23630d61d28bf2cda5
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-04-04 12:40:27 +0500

    z-index of submenu header were changed to maintain correct stack order

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookEventDetails/sub-components/MessagesDetails.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookEventDetails/sub-components/MessagesDetails.js
@@ -1,25 +1,33 @@
 import React from "react";
-
+import styled from "styled-components";
 import Submenu from "@docspace/components/submenu";
 
 import { RequestDetails } from "./RequestDetails";
 
+const SubmenuWrapper = styled.div`
+  .sticky {
+    z-index: 3;
+  }
+`;
+
 export const MessagesDetails = () => {
   return (
-    <Submenu
-      data={[
-        {
-          id: "webhookRequest",
-          name: "Request",
-          content: <RequestDetails />,
-        },
-        {
-          id: "webhookResponse",
-          name: "Response",
-          content: <RequestDetails />,
-        },
-      ]}
-      startSelect={0}
-    />
+    <SubmenuWrapper>
+      <Submenu
+        data={[
+          {
+            id: "webhookRequest",
+            name: "Request",
+            content: <RequestDetails />,
+          },
+          {
+            id: "webhookResponse",
+            name: "Response",
+            content: <RequestDetails />,
+          },
+        ]}
+        startSelect={0}
+      />
+    </SubmenuWrapper>
   );
 };
