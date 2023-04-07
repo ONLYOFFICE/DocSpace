2fbcc720290b38c4088153666122316d7f816379
gopienkonikita <nikita.gopienko@onlyoffice.com>
2023-04-07 11:57:39 +0300

    Web: Accounts: fixed profile menu styles

--- packages/client/src/pages/Profile/Section/Body/sub-components/main-profile/index.js
+++ packages/client/src/pages/Profile/Section/Body/sub-components/main-profile/index.js
@@ -1,18 +1,22 @@
 ﻿import SendClockReactSvgUrl from "PUBLIC_DIR/images/send.clock.react.svg?url";
 import PencilOutlineReactSvgUrl from "PUBLIC_DIR/images/pencil.outline.react.svg?url";
 import DefaultUserAvatarMax from "PUBLIC_DIR/images/default_user_photo_size_200-200.png";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ReactSVG } from "react-svg";
 import { useTranslation } from "react-i18next";
 import { inject, observer } from "mobx-react";
 
 import Avatar from "@docspace/components/avatar";
 import Text from "@docspace/components/text";
+import Link from "@docspace/components/link";
+import ComboBox from "@docspace/components/combobox";
 import IconButton from "@docspace/components/icon-button";
+import { isMobileOnly } from "react-device-detect";
+import toastr from "@docspace/components/toast/toastr";
 
-import { getUserRole } from "@docspace/common/utils";
+import { getUserRole, convertLanguage } from "@docspace/common/utils";
 
-import LanguagesCombo from "./languagesCombo";
+import { Trans } from "react-i18next";
 //import TimezoneCombo from "./timezoneCombo";
 
 import {
@@ -22,14 +26,20 @@ import {
   ChangeNameDialog,
 } from "SRC_DIR/components/dialogs";
 
-import { StyledWrapper, StyledInfo } from "./styled-main-profile";
+import { StyledWrapper, StyledInfo, StyledLabel } from "./styled-main-profile";
 import { HelpButton, Tooltip } from "@docspace/components";
+import withCultureNames from "@docspace/common/hoc/withCultureNames";
 
 const MainProfile = (props) => {
   const { t } = useTranslation(["Profile", "Common"]);
 
   const {
+    theme,
     profile,
+    culture,
+    helpLink,
+    cultureNames,
+    setIsLoading,
     changeEmailVisible,
     setChangeEmailVisible,
     changePasswordVisible,
@@ -41,7 +51,26 @@ const MainProfile = (props) => {
     withActivationBar,
     sendActivationLink,
     currentColorScheme,
+    updateProfileCulture,
   } = props;
+
+  const [horizontalOrientation, setHorizontalOrientation] = useState(false);
+
+  useEffect(() => {
+    checkWidth();
+    window.addEventListener("resize", checkWidth);
+    return () => window.removeEventListener("resize", checkWidth);
+  }, []);
+
+  const checkWidth = () => {
+    if (!isMobileOnly) return;
+
+    if (!isSmallTablet()) {
+      setHorizontalOrientation(true);
+    } else {
+      setHorizontalOrientation(false);
+    }
+  };
 
   const role = getUserRole(profile);
 
@@ -52,6 +81,57 @@ const MainProfile = (props) => {
   const userAvatar = profile.hasAvatar
     ? profile.avatarMax
     : DefaultUserAvatarMax;
+
+  const supportEmail = "documentation@onlyoffice.com";
+
+  const tooltipLanguage = (
+    <Text fontSize="13px">
+      <Trans t={t} i18nKey="NotFoundLanguage" ns="Common">
+        "In case you cannot find your language in the list of the available
+        ones, feel free to write to us at
+        <Link
+          href={`mailto:${supportEmail}`}
+          isHovered={true}
+          color={theme.profileInfo.tooltipLinkColor}
+        >
+          {{ supportEmail }}
+        </Link>
+        to take part in the translation and get up to 1 year free of charge."
+      </Trans>{" "}
+      <Link
+        color={theme.profileInfo.tooltipLinkColor}
+        isHovered={true}
+        href={`${helpLink}/guides/become-translator.aspx`}
+        target="_blank"
+      >
+        {t("Common:LearnMore")}
+      </Link>
+    </Text>
+  );
+
+  const isMobileHorizontalOrientation = isMobileOnly && horizontalOrientation;
+
+  const { cultureName, currentCulture } = profile;
+  const language = convertLanguage(cultureName || currentCulture || culture);
+
+  const selectedLanguage = cultureNames.find((item) => item.key === language) ||
+    cultureNames.find((item) => item.key === culture) || {
+      key: language,
+      label: "",
+    };
+
+  const onLanguageSelect = (language) => {
+    if (profile.cultureName === language.key) return;
+
+    setIsLoading(true);
+    updateProfileCulture(profile.id, language.key)
+      .then(() => setIsLoading(false))
+      .then(() => location.reload())
+      .catch((error) => {
+        toastr.error(error && error.message ? error.message : error);
+        setIsLoading(false);
+      });
+  };
 
   return (
     <StyledWrapper>
@@ -69,12 +149,132 @@ const MainProfile = (props) => {
         currentColorScheme={currentColorScheme}
       >
         <div className="rows-container">
-          <div className="row">
-            <div className="field">
-              <Text as="div" className="label">
+          <div className="profile-block">
+            <StyledLabel as="div">{t("Common:Name")}</StyledLabel>
+
+            <StyledLabel as="div" marginTopProp="16px">
+              {t("Common:Email")}
+            </StyledLabel>
+
+            <StyledLabel
+              as="div"
+              marginTopProp={withActivationBar ? "34px" : "16px"}
+            >
+              {t("Common:Password")}
+            </StyledLabel>
+
+            <StyledLabel as="div" marginTopProp="15px">
+              {t("Common:Language")}
+              <HelpButton
+                size={12}
+                offsetRight={0}
+                place="right"
+                tooltipContent={tooltipLanguage}
+              />
+            </StyledLabel>
+          </div>
+
+          <div className="profile-block">
+            <div className="profile-block-field">
+              <Text fontWeight={600} truncate>
+                {profile.displayName}
+              </Text>
+              <IconButton
+                className="edit-button"
+                iconName={PencilOutlineReactSvgUrl}
+                size="12"
+                onClick={() => setChangeNameVisible(true)}
+              />
+            </div>
+            <div className="email-container">
+              <div className="email-edit-container">
+                <Text
+                  data-for="emailTooltip"
+                  data-tip={t("EmailNotVerified")}
+                  as="div"
+                  className="email-text-container"
+                  fontWeight={600}
+                >
+                  {profile.email}
+                </Text>
+                {withActivationBar && (
+                  <Tooltip
+                    id="emailTooltip"
+                    getContent={(dataTip) => (
+                      <Text fontSize="12px">{dataTip}</Text>
+                    )}
+                    effect="float"
+                    place="bottom"
+                  />
+                )}
+                <IconButton
+                  className="edit-button email-edit-button"
+                  iconName={PencilOutlineReactSvgUrl}
+                  size="12"
+                  onClick={() => setChangeEmailVisible(true)}
+                />
+              </div>
+              {withActivationBar && (
+                <div
+                  className="send-again-container"
+                  onClick={sendActivationLinkAction}
+                >
+                  <ReactSVG
+                    className="send-again-icon"
+                    src={SendClockReactSvgUrl}
+                  />
+                  <Text className="send-again-text" fontWeight={600} noSelect>
+                    {t("SendAgain")}
+                  </Text>
+                </div>
+              )}
+            </div>
+            <div className="profile-block-field" style={{ marginTop: 16 }}>
+              <Text fontWeight={600}>********</Text>
+              <IconButton
+                className="edit-button"
+                iconName={PencilOutlineReactSvgUrl}
+                size="12"
+                onClick={() => setChangePasswordVisible(true)}
+              />
+            </div>
+            <div className="language-combo-box-wrapper">
+              <ComboBox
+                className="language-combo-box"
+                directionY={isMobileHorizontalOrientation ? "bottom" : "both"}
+                options={cultureNames}
+                selectedOption={selectedLanguage}
+                onSelect={onLanguageSelect}
+                isDisabled={false}
+                scaled={isMobileOnly}
+                scaledOptions={false}
+                size="content"
+                showDisabledItems={true}
+                dropDownMaxHeight={364}
+                manualWidth="250px"
+                isDefaultMode={
+                  isMobileHorizontalOrientation
+                    ? isMobileHorizontalOrientation
+                    : !isMobileOnly
+                }
+                withBlur={isMobileHorizontalOrientation ? false : isMobileOnly}
+                fillIcon={false}
+                modernView={!isMobileOnly}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mobile-profile-block">
+          <div className="mobile-profile-row">
+            <div className="mobile-profile-field">
+              <Text className="mobile-profile-label" as="div">
                 {t("Common:Name")}
               </Text>
-              <Text fontWeight={600} truncate>
+              <Text
+                className="mobile-profile-label-field"
+                fontWeight={600}
+                truncate
+              >
                 {profile.displayName}
               </Text>
             </div>
@@ -85,9 +285,9 @@ const MainProfile = (props) => {
               onClick={() => setChangeNameVisible(true)}
             />
           </div>
-          <div className="row">
-            <div className="field">
-              <Text as="div" className="label">
+          <div className="mobile-profile-row">
+            <div className="mobile-profile-field">
+              <Text className="mobile-profile-label" as="div">
                 {t("Common:Email")}
               </Text>
               <div className="email-container">
@@ -96,48 +296,26 @@ const MainProfile = (props) => {
                     data-for="emailTooltip"
                     data-tip={t("EmailNotVerified")}
                     as="div"
-                    className={"email-text-container"}
+                    className="email-text-container"
                     fontWeight={600}
                   >
                     {profile.email}
                   </Text>
-                  {withActivationBar && (
-                    <Tooltip
-                      id="emailTooltip"
-                      getContent={(dataTip) => (
-                        <Text fontSize="12px">{dataTip}</Text>
-                      )}
-                      effect="float"
-                      place="bottom"
-                    />
-                  )}
-
-                  <IconButton
-                    className="edit-button email-edit-button"
-                    iconName={PencilOutlineReactSvgUrl}
-                    size="12"
-                    onClick={() => setChangeEmailVisible(true)}
-                  />
                 </div>
                 {withActivationBar && (
-                  <div
-                    className="send-again-container send-again-desktop"
-                    onClick={sendActivationLinkAction}
-                  >
-                    <ReactSVG
-                      className="send-again-icon"
-                      src={SendClockReactSvgUrl}
-                    />
-                    <Text className="send-again-text" fontWeight={600} noSelect>
-                      {t("SendAgain")}
-                    </Text>
-                  </div>
+                  <Tooltip
+                    id="emailTooltip"
+                    getContent={(dataTip) => (
+                      <Text fontSize="12px">{dataTip}</Text>
+                    )}
+                    effect="float"
+                    place="bottom"
+                  />
                 )}
               </div>
-
               {withActivationBar && (
                 <div
-                  className="send-again-container send-again-mobile"
+                  className="send-again-container"
                   onClick={sendActivationLinkAction}
                 >
                   <ReactSVG
@@ -151,18 +329,20 @@ const MainProfile = (props) => {
               )}
             </div>
             <IconButton
-              className="edit-button email-edit-button-mobile"
+              className="edit-button"
               iconName={PencilOutlineReactSvgUrl}
               size="12"
               onClick={() => setChangeEmailVisible(true)}
             />
           </div>
-          <div className="row">
-            <div className="field">
-              <Text as="div" className="label">
+          <div className="mobile-profile-row">
+            <div className="mobile-profile-field">
+              <Text as="div" className="mobile-profile-label">
                 {t("Common:Password")}
               </Text>
-              <Text fontWeight={600}>********</Text>
+              <Text className="mobile-profile-password" fontWeight={600}>
+                ********
+              </Text>
             </div>
             <IconButton
               className="edit-button"
@@ -171,8 +351,41 @@ const MainProfile = (props) => {
               onClick={() => setChangePasswordVisible(true)}
             />
           </div>
+
+          <div className="mobile-language">
+            <Text as="div" fontWeight={600} className="mobile-profile-label">
+              {t("Common:Language")}
+              <HelpButton
+                size={12}
+                offsetRight={0}
+                place="right"
+                tooltipContent={tooltipLanguage}
+              />
+            </Text>
+            <ComboBox
+              className="language-combo-box"
+              directionY={isMobileHorizontalOrientation ? "bottom" : "both"}
+              options={cultureNames}
+              selectedOption={selectedLanguage}
+              onSelect={onLanguageSelect}
+              isDisabled={false}
+              scaled={isMobileOnly}
+              scaledOptions={false}
+              size="content"
+              showDisabledItems={true}
+              dropDownMaxHeight={364}
+              manualWidth="250px"
+              isDefaultMode={
+                isMobileHorizontalOrientation
+                  ? isMobileHorizontalOrientation
+                  : !isMobileOnly
+              }
+              withBlur={isMobileHorizontalOrientation ? false : isMobileOnly}
+              fillIcon={false}
+              modernView={!isMobileOnly}
+            />
+          </div>
         </div>
-        <LanguagesCombo t={t} />
         {/* <TimezoneCombo title={t("Common:ComingSoon")} /> */}
       </StyledInfo>
 
@@ -212,11 +425,9 @@ const MainProfile = (props) => {
 };
 
 export default inject(({ auth, peopleStore }) => {
-  const { targetUserStore } = peopleStore;
-
   const { withActivationBar, sendActivationLink } = auth.userStore;
-
-  const { currentColorScheme } = auth.settingsStore;
+  const { theme, helpLink, culture, currentColorScheme } = auth.settingsStore;
+  const { setIsLoading } = peopleStore.loadingStore;
 
   const {
     targetUser: profile,
@@ -228,10 +439,15 @@ export default inject(({ auth, peopleStore }) => {
     setChangeNameVisible,
     changeAvatarVisible,
     setChangeAvatarVisible,
-  } = targetUserStore;
+    updateProfileCulture,
+  } = peopleStore.targetUserStore;
 
   return {
+    theme,
     profile,
+    culture,
+    helpLink,
+    setIsLoading,
     changeEmailVisible,
     setChangeEmailVisible,
     changePasswordVisible,
@@ -243,5 +459,6 @@ export default inject(({ auth, peopleStore }) => {
     withActivationBar,
     sendActivationLink,
     currentColorScheme,
+    updateProfileCulture,
   };
-})(observer(MainProfile));
+})(withCultureNames(observer(MainProfile)));
