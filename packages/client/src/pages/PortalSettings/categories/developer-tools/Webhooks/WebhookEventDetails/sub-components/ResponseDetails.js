e83f23f5313d2dd5fe5e3b8418ca16dae472cc6b
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-04-16 21:27:01 +0500

    empty response message and empty response body cases were fixed

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookEventDetails/sub-components/ResponseDetails.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookEventDetails/sub-components/ResponseDetails.js
@@ -2,12 +2,39 @@ import React from "react";
 import styled from "styled-components";
 import { Text, Textarea } from "@docspace/components";
 
+import DangerIcon from "PUBLIC_DIR/images/danger.toast.react.svg?url";
+
 const DetailsWrapper = styled.div`
   width: 100%;
 `;
 
+const ErrorMessageTooltip = styled.div`
+  width: 100%;
+  padding: 8px 12px;
+  background: #f7cdbe;
+
+  box-shadow: 0px 5px 20px rgba(4, 15, 27, 0.07);
+  border-radius: 6px;
+  display: flex;
+  align-items: center;
+`;
+
 export const ResponseDetails = ({ webhookDetails }) => {
-  const responsePayload = webhookDetails.responsePayload.trim();
+  if (
+    !(
+      webhookDetails.hasOwnProperty("responseHeaders") ||
+      webhookDetails.hasOwnProperty("responsePayload")
+    )
+  ) {
+    return (
+      <ErrorMessageTooltip>
+        <img src={DangerIcon} alt="danger icon" style={{ marginRight: "8px" }} />
+        The SSL connection could not be established, see inner exception.
+      </ErrorMessageTooltip>
+    );
+  }
+
+  const responsePayload = webhookDetails.responsePayload?.trim();
 
   return (
     <DetailsWrapper>
@@ -24,7 +51,11 @@ export const ResponseDetails = ({ webhookDetails }) => {
       <Text as="h3" fontWeight={600} style={{ marginBottom: "4px", marginTop: "16px" }}>
         Response post body
       </Text>
-      <Textarea value={responsePayload} isJSONField enableCopy hasNumeration isFullHeight />
+      {responsePayload === "" ? (
+        <Textarea isDisabled />
+      ) : (
+        <Textarea value={responsePayload} isJSONField enableCopy hasNumeration isFullHeight />
+      )}
     </DetailsWrapper>
   );
 };
