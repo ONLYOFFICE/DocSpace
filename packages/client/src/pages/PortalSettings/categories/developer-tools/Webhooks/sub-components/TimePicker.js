c1fb0c2d0b3f492f2cc1e485a8beef09a3c8940f
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-04-17 22:13:59 +0500

    date, setDate, adn onChange props were added

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/TimePicker.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/TimePicker.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import moment from "moment";
 import styled from "styled-components";
 
 import { TextInput } from "@docspace/components";
@@ -29,52 +30,109 @@ const TimeInput = styled.div`
   }
 `;
 
-export const TimePicker = () => {
+export const TimePicker = ({ date, setDate, onChange }) => {
   const hoursInputRef = useRef(null);
   const minutesInputRef = useRef(null);
   const timeInputRef = useRef(null);
 
-  const [hours, setHours] = useState("00");
+  const [hours, setHours] = useState(moment(date, "HH:mm").format("HH"));
 
-  const [minutes, setMinutes] = useState("00");
+  const [minutes, setMinutes] = useState(moment(date, "HH:mm").format("mm"));
+
+  const handleHoursChange = (time) => {
+    setHours(time);
+    setDate((prevDate) => moment(prevDate.format("YYYY-MM-DD") + " " + time + ":" + minutes));
+    onChange(time);
+  };
+  const handleMinutesChange = (time) => {
+    setMinutes(time);
+    setDate((prevDate) => moment(prevDate.format("YYYY-MM-DD") + " " + hours + ":" + time));
+    onChange(time);
+  };
 
   const handleChangeHours = (e) => {
-    setHours(e.target.value);
+    const hours = e.target.value;
+
+    if (hours === "") {
+      handleHoursChange("00");
+      return;
+    }
+    if (!/^\d+$/.test(hours)) return;
+
+    if (hours > 23) {
+      focusMinutesInput();
+      hours.length === 2 && handleHoursChange("0" + hours[0]);
+      return;
+    }
+
+    if (hours.length === 1 && hours > 2) {
+      handleHoursChange("0" + hours);
+      focusMinutesInput();
+      return;
+    }
+
+    hours.length === 2 && focusMinutesInput();
+
+    handleHoursChange(hours);
   };
+
   const handleChangeMinutes = (e) => {
-    setMinutes(e.target.value);
-    console.log(e);
+    const minutes = e.target.value;
+
+    if (minutes === "") {
+      handleMinutesChange("00");
+      return;
+    }
+    if (!/^\d+$/.test(minutes)) return;
+
+    if (minutes > 59) return;
+
+    if (minutes.length === 1 && minutes > 5) {
+      handleMinutesChange("0" + minutes);
+      blurMinutesInput();
+      return;
+    }
+    minutes.length === 2 && blurMinutesInput();
+
+    handleMinutesChange(minutes);
   };
 
   const focusHoursInput = (e) => {
     const target = e.target;
-    console.log(minutesInputRef);
-    if (!minutesInputRef.current.contains(target)) hoursInputRef.current.focus();
+    if (!minutesInputRef.current.contains(target)) hoursInputRef.current.select();
+  };
+  const focusMinutesInput = () => {
+    minutesInputRef.current.select();
+  };
+  const blurMinutesInput = () => {
+    minutesInputRef.current.blur();
   };
 
-  const HoursInput = () => (
-    <TextInput
-      withBorder={false}
-      forwardedRef={hoursInputRef}
-      value={hours}
-      onChange={handleChangeHours}
-      // mask={[/[0-2]/, /\d/]}
-    />
-  );
-  const MinutesInput = () => (
-    <TextInput
-      withBorder={false}
-      forwardedRef={minutesInputRef}
-      value={minutes}
-      onChange={handleChangeMinutes}
-      // mask={[/[0-6]/, /\d/]}
-    />
-  );
+  const onHoursBlur = (e) => {
+    e.target.value.length === 1 && handleHoursChange("0" + e.target.value);
+  };
+  const onMinutesBlur = (e) => {
+    e.target.value.length === 1 && handleMinutesChange("0" + e.target.value);
+  };
 
   return (
     <TimeInput ref={timeInputRef} onClick={focusHoursInput}>
-      <HoursInput />:
-      <MinutesInput />
+      <TextInput
+        withBorder={false}
+        forwardedRef={hoursInputRef}
+        value={hours}
+        onChange={handleChangeHours}
+        onBlur={onHoursBlur}
+      />
+      :
+      <TextInput
+        withBorder={false}
+        forwardedRef={minutesInputRef}
+        value={minutes}
+        onChange={handleChangeMinutes}
+        onClick={focusMinutesInput}
+        onBlur={onMinutesBlur}
+      />
     </TimeInput>
   );
 };
