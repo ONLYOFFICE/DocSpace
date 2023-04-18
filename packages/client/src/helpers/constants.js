4013e1d9c39cbadc6b1407b7c5d9a7601aabdcd9
Viktor Fomin <enotcode@gmail.com>
2023-04-18 14:08:03 +0300

    Client: Helpers: add SSO_NAME_ID_FORMAT

--- packages/client/src/helpers/constants.js
+++ packages/client/src/helpers/constants.js
@@ -82,7 +82,8 @@ export const QuotaBarTypes = Object.freeze({
 export const BINDING_POST = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST";
 export const BINDING_REDIRECT =
   "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect";
-
+export const SSO_NAME_ID_FORMAT =
+  "urn:oasis:names:tc:SAML:2.0:nameid-format:transient";
 export const SSO_GIVEN_NAME = "givenName";
 export const SSO_SN = "sn";
 export const SSO_EMAIL = "email";
