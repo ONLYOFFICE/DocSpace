361f45f339d353c8022a836b5b5678748bf5b189
Viktor Fomin <enotcode@gmail.com>
2023-04-20 13:31:03 +0300

    Editor: Components: init deeplink layout

---
+++ packages/editor/src/client/components/DeepLink/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Text from "@docspace/components/text";
+import Checkbox from "@docspace/components/checkbox";
+import Button from "@docspace/components/button";
+import Link from "@docspace/components/link";
+
+import {
+  StyledSimpleNav,
+  StyledDeepLink,
+  StyledBodyWrapper,
+  StyledFileTile,
+  StyledActionsWrapper,
+} from "./StyledDeepLink";
+
+const DeepLink = () => {
+  return (
+    <>
+      <StyledSimpleNav>
+        <img src="/static/images/logo/lightsmall.svg" />
+      </StyledSimpleNav>
+      <StyledDeepLink>
+        <StyledBodyWrapper>
+          <Text fontSize="23px" fontWeight="700">
+            Opening a document
+          </Text>
+          <StyledFileTile>
+            <img src="/static/images/icons/32/xlsx.svg" />
+            <Text fontSize="14px" fontWeight="600" truncate>
+              Spreadsheet
+            </Text>
+          </StyledFileTile>
+          <Text>
+            You can open the document on the portal or in the mobile application
+          </Text>
+        </StyledBodyWrapper>
+        <StyledActionsWrapper>
+          <Checkbox label={"Remember"} isChecked={false} onChange={() => {}} />
+          <Button size="medium" primary label="Open in the app" />
+          <Link
+            className="stay-link"
+            type="action"
+            fontSize="13px"
+            fontWeight="600"
+            isHovered
+            color="#316DAA"
+          >
+            Stay in the browser
+          </Link>
+        </StyledActionsWrapper>
+      </StyledDeepLink>
+    </>
+  );
+};
+
+export default DeepLink;
