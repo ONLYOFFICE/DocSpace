e726c134e12a84d337108f002a149f7bce15e2ab
DmitrySychugov <dmitry.s.develop@mail.ru>
2023-04-20 19:01:48 +0500

    Web: Components: SearchInput - added description

--- packages/components/search-input/index.js
+++ packages/components/search-input/index.js
@@ -132,6 +132,7 @@ class SearchInput extends React.Component {
 SearchInput.propTypes = {
   /** Used as HTML `id` property */
   id: PropTypes.string,
+  /** Sets the unique element name */
   name: PropTypes.string,
   /** Accepts class */
   className: PropTypes.string,
@@ -145,11 +146,15 @@ SearchInput.propTypes = {
   placeholder: PropTypes.string,
   /** Sets a callback function that allows handling the component's changing events */
   onChange: PropTypes.func,
+  /** Sets a callback function that is triggered when the clear icon of the search input is clicked */
   onClearSearch: PropTypes.func,
   /** Indicates that the field cannot be used (e.g not authorized, or the changes have not been saved) */
   isDisabled: PropTypes.bool,
+  /** Displays the Clear Button */
   showClearButton: PropTypes.bool,
+  /** Sets the refresh timeout of the input  */
   refreshTimeout: PropTypes.number,
+  /** Sets the input to refresh automatically */
   autoRefresh: PropTypes.bool,
   /** Child elements */
   children: PropTypes.any,
