0116359bb156b7a0c5b5fc14ab024195aa25d665
DmitrySychugov <dmitry.s.develop@mail.ru>
2023-04-21 14:34:06 +0500

    Web: Components: SocialButton - added description

--- packages/components/social-button/index.js
+++ packages/components/social-button/index.js
@@ -13,13 +13,8 @@ class SocialButton extends React.Component {
   }
 
   render() {
-    const {
-      label,
-      iconName,
-      IconComponent,
-      isConnect,
-      ...otherProps
-    } = this.props;
+    const { label, iconName, IconComponent, isConnect, ...otherProps } =
+      this.props;
     return (
       <StyledSocialButton isConnect={isConnect} {...otherProps}>
         <div>
@@ -56,9 +51,13 @@ SocialButton.propTypes = {
   id: PropTypes.string,
   /** Accepts css style */
   style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  /** Sets a callback function that is triggered when the button is clicked */
   onClick: PropTypes.func,
+  /** Accepts the icon options  */
   $iconOptions: PropTypes.object,
+  /** Sets the image size. Contains the small and the basic size options */
   size: PropTypes.oneOf(["base", "small"]),
+  /** Changes the button style if the user is connected to the social network account */
   isConnect: PropTypes.bool,
 };
 
