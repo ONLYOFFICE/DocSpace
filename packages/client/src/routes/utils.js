e2984385b7b747069f4894f6f6368de2b0d97303
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2023-04-24 13:53:36 +0400

    Web: Added missing imports

--- packages/client/src/routes/utils.js
+++ packages/client/src/routes/utils.js
@@ -1,4 +1,7 @@
 import React from "react";
+import PrivateRoute from "@docspace/common/components/PrivateRoute";
+import ErrorBoundary from "@docspace/common/components/ErrorBoundary";
+import Error404 from "SRC_DIR/pages/Errors/404";
 
 export const NotFoundError = () => {
   return (
