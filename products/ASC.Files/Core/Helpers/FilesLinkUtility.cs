5f0cd6c53b086662d405b6b1736aa7aa774e8ed7
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2023-04-25 19:33:22 +0300

    Files: FilesLinkUtility: added new query key

--- products/ASC.Files/Core/Helpers/FilesLinkUtility.cs
+++ products/ASC.Files/Core/Helpers/FilesLinkUtility.cs
@@ -75,6 +75,7 @@ public class FilesLinkUtility
     public const string AuthKey = "stream_auth";
     public const string Anchor = "anchor";
     public const string Size = "size";
+    public const string FolderShareKey = "share";
 
     public string FileHandlerPath
     {
