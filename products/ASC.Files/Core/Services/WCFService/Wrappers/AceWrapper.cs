59efe26a96ced7a7174a1f339e83cc2c2a405323
MaksimChegulov <maksim.chegulov@onlyoffice.com>
2023-04-26 14:29:33 +0300

    Files: AceWrapper: added link template parameter

--- products/ASC.Files/Core/Services/WCFService/Wrappers/AceWrapper.cs
+++ products/ASC.Files/Core/Services/WCFService/Wrappers/AceWrapper.cs
@@ -42,6 +42,7 @@ public class AceWrapper : IMapFrom<RoomInvitation>
     public SubjectType SubjectType { get; set; }
     public FileShareOptions FileShareOptions { get; set; }
     public bool CanEditAccess { get; set; }
+    public bool IsTemplate { get; set; }
 
     [JsonPropertyName("title")]
     public string SubjectName { get; set; }
