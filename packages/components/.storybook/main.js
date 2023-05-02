a7c4427cf8e757be70b87248a0fffedfee1a9518
DmitrySychugov <dmitry.s.develop@mail.ru>
2023-05-02 16:20:19 +0500

    Web: Components: overwritten storybook main js after migration

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,33 +1,32 @@
 module.exports = {
-  core: {
-    builder: "webpack5",
-  },
-  stories: [
-    "../all/all.stories.js", // default page
-    "../**/*.stories.js", //"../**/*.stories.@(js|mdx)",
+  stories: ["../all/all.stories.js",
+  // default page
+  "../**/*.stories.js" //"../**/*.stories.@(js|mdx)",
   ],
+
   staticDirs: ["../../../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-actions",
-    "@storybook/addon-controls",
-    "@storybook/addon-viewport",
-    "@storybook/addon-contexts/register",
-    // {
-    //   name: "@storybook/addon-docs",
-    //   options: {
-    //     babelOptions: {
-    //       plugins: [
-    //         [
-    //           "@babel/plugin-proposal-private-property-in-object",
-    //           {
-    //             loose: true,
-    //           },
-    //         ],
-    //       ],
-    //     },
-    //   },
-    // },
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-actions", "@storybook/addon-controls", "@storybook/addon-viewport", "@storybook/addon-contexts/register"
+  // {
+  //   name: "@storybook/addon-docs",
+  //   options: {
+  //     babelOptions: {
+  //       plugins: [
+  //         [
+  //           "@babel/plugin-proposal-private-property-in-object",
+  //           {
+  //             loose: true,
+  //           },
+  //         ],
+  //       ],
+  //     },
+  //   },
+  // },
+  , "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
