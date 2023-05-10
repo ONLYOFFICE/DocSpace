218ea6ec5b9d0faa5c1c0cd703bf6085d14a980f
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-05-10 12:52:17 +0500

    unused componenets were substituted with new components

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/WebhookInfo.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/sub-components/WebhookInfo.js
@@ -1,6 +1,12 @@
 import React from "react";
+import styled from "styled-components";
+import { InfoText } from "../styled-components";
 
-import { Link, InfoText, InfoWrapper } from "../styled-components";
+import { Link } from "@docspace/components";
+
+const InfoWrapper = styled.div`
+  margin-bottom: 27px;
+`;
 
 export const WebhookInfo = () => {
   return (
@@ -11,7 +17,14 @@ export const WebhookInfo = () => {
         Here, you can create and manage all your webhooks, configure them, and browse history of
         every webhook to audit their performance.
       </InfoText>
-      <Link href="https://api.onlyoffice.com/portals/basic">Webhooks Guide</Link>
+      <Link
+        fontWeight={600}
+        color="#316DAA"
+        isHovered
+        type="page"
+        href="https://api.onlyoffice.com/portals/basic">
+        Webhooks Guide
+      </Link>
     </InfoWrapper>
   );
 };
