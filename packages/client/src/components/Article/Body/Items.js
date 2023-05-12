dc0fc261e2132c76cd87514a68331560252dbb75
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2023-05-12 17:54:59 +0400

    Fix Bug 62465 - Files.Trash. Ability to repeatedly press the Empty trash button.

--- packages/client/src/components/Article/Body/Items.js
+++ packages/client/src/components/Article/Body/Items.js
@@ -172,6 +172,7 @@ const Items = ({
   firstLoad,
   deleteAction,
   startDrag,
+  emptyTrashInProgress,
 }) => {
   useEffect(() => {
     data.forEach((elem) => {
@@ -340,7 +341,9 @@ const Items = ({
     (data) => {
       const items = data.map((item, index) => {
         const isTrash = item.rootFolderType === FolderType.TRASH;
-        const showBadge = item.newItems
+        const showBadge = emptyTrashInProgress
+          ? false
+          : item.newItems
           ? item.newItems > 0 && true
           : isTrash && !trashIsEmpty;
         const labelBadge = showBadge ? item.newItems : null;
@@ -399,6 +402,7 @@ const Items = ({
       isAdmin,
       isVisitor,
       firstLoad,
+      emptyTrashInProgress,
     ]
   );
 
@@ -449,6 +453,7 @@ export default inject(
       moveDragItems,
       uploadEmptyFolders,
       deleteAction,
+      emptyTrashInProgress,
     } = filesActionsStore;
     const { setEmptyTrashDialogVisible } = dialogsStore;
 
@@ -481,6 +486,7 @@ export default inject(
       rootFolderType,
       firstLoad,
       startDrag,
+      emptyTrashInProgress,
     };
   }
 )(withTranslation(["Files", "Common", "Translations"])(observer(Items)));
