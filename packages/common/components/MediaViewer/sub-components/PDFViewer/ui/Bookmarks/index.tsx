be9079cf4024cc473dfe679d06b90cd4ba27a538
Akmal Isomadinov <isomadinovakmal1997@gmail.com>
2023-05-16 15:05:27 +0500

    Web:Viewer Fixed style

--- packages/common/components/MediaViewer/sub-components/PDFViewer/ui/Bookmarks/index.tsx
+++ packages/common/components/MediaViewer/sub-components/PDFViewer/ui/Bookmarks/index.tsx
@@ -1,51 +1,20 @@
 import React from "react";
-import styled from "styled-components";
 import CustomScrollbarsVirtualList from "@docspace/components/scrollbar/custom-scrollbars-virtual-list";
 
 import BookmarksProps from "./Bookmarks.props";
-
-const List = styled.ul`
-  padding-left: 16px;
-  padding-right: 30px;
-
-  list-style: none;
-  margin-top: 0px;
-
-  display: flex;
-  flex-direction: column;
-`;
-
-const Item = styled.li`
-  color: #ffffff;
-  padding: 1px 16px 0 16px;
-  font-weight: 400;
-  font-size: 13px;
-  line-height: 20px;
-
-  cursor: pointer;
-
-  margin-top: -1px;
-
-  :hover {
-    background: #474747;
-  }
-`;
-
-const Text = styled.p`
-  margin: 0;
-  border-bottom: 1px solid #474747;
-  padding: 6px 0;
-`;
+import { Item, List, Text } from "./Bookmarks.styled";
 
 function Bookmarks({ bookmarks, navigate }: BookmarksProps) {
   return (
     <CustomScrollbarsVirtualList>
       <List>
-        {bookmarks.map((item, index) => (
-          <Item key={index}>
-            <Text onClick={() => navigate(index)}>{item.description}</Text>
-          </Item>
-        ))}
+        {bookmarks.map((item, index) => {
+          return (
+            <Item key={index}>
+              <Text onClick={() => navigate(index)}>{item.description}</Text>
+            </Item>
+          );
+        })}
       </List>
     </CustomScrollbarsVirtualList>
   );
