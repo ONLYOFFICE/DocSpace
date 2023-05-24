ce14056fc657650fa266bc66a558352dae009ffc
Viktor Fomin <enotcode@gmail.com>
2023-05-24 15:08:09 +0300

    Client: Helpers: ErrorsStats: add fields

--- packages/client/src/helpers/errorsStats.js
+++ packages/client/src/helpers/errorsStats.js
@@ -1,9 +1,17 @@
-export const getErrorsStats = (userId, version) => {
+export const getErrorsStats = (userId, version, language, error) => {
+  const reportTime = new Date();
+
   const errors = {
+    url: window.origin,
     id: userId,
     version: version,
     platform: navigator?.platform,
     userAgent: navigator?.userAgent,
+    language: language,
+    errorMessage: error?.message,
+    errorStack: error?.stack,
+    localStorage: window.localStorage,
+    reportTime: reportTime,
   };
   console.log("errors", errors);
 };
