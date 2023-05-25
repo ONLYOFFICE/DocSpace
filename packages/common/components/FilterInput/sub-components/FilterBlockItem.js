bcf9c9c65f28d081e7a52cc1f7e5e5d5b47a5fba
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2023-05-25 11:54:48 +0300

    Web:Moved StyledFilterBlockItemTag to separate file.

--- packages/common/components/FilterInput/sub-components/FilterBlockItem.js
+++ packages/common/components/FilterInput/sub-components/FilterBlockItem.js
@@ -11,7 +11,6 @@ import {
   StyledFilterBlockItemContent,
   StyledFilterBlockItemSelector,
   StyledFilterBlockItemSelectorText,
-  StyledFilterBlockItemTag,
   StyledFilterBlockItemTagText,
   StyledFilterBlockItemTagIcon,
   StyledFilterBlockItemToggle,
--- packages/components/ColorTheme/styled/filterBlockItemTag.js
+++ packages/components/ColorTheme/styled/filterBlockItemTag.js
@@ -1,31 +1,6 @@
 import styled, { css } from "styled-components";
 import Base from "@docspace/components/themes/base";
-
-const StyledFilterBlockItemTag = styled.div`
-  height: 28px;
-  max-height: 28px;
-
-  max-width: 100%;
-
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-
-  border: ${(props) => props.theme.filterInput.filter.border};
-  border-radius: 16px;
-
-  box-sizing: border-box;
-
-  padding: 4px 15px;
-
-  cursor: pointer;
-
-  ${(props) => props.isSelected && selectedItemTag}
-
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-`;
-
-StyledFilterBlockItemTag.defaultProps = { theme: Base };
+import StyledFilterBlockItemTag from "./sub-components/StyledFilterBlockItemTag";
 
 const getDefaultStyles = ({ $currentColorScheme, isSelected, theme }) =>
   $currentColorScheme &&
