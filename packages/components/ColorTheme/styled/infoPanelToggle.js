51500850aef7e7c592c60233e544e4d52b8d8b9c
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2023-05-25 11:59:09 +0300

    Web:Components:Moved StyledInfoPanelToggleWrapper to a separate file.

--- packages/components/ColorTheme/styled/infoPanelToggle.js
+++ packages/components/ColorTheme/styled/infoPanelToggle.js
@@ -1,35 +1,6 @@
 import styled, { css } from "styled-components";
 import Base from "@docspace/components/themes/base";
-import { tablet } from "@docspace/components/utils/device";
-
-const StyledInfoPanelToggleWrapper = styled.div`
-  display: flex;
-
-  @media ${tablet} {
-    display: none;
-  }
-
-  align-items: center;
-  justify-content: center;
-  padding-right: 20px;
-
-  .info-panel-toggle-bg {
-    height: 32px;
-    width: 32px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    background-color: ${(props) =>
-      !props.isInfoPanelVisible
-        ? "none"
-        : props.theme.infoPanel.sectionHeaderToggleBgActive};
-
-    path {
-      fill: ${(props) => props.theme.infoPanel.sectionHeaderToggleIconActive};
-    }
-  }
-`;
+import StyledInfoPanelToggleWrapper from "./sub-components/StyledWrapper";
 
 const getDefaultStyles = ({ $currentColorScheme, isInfoPanelVisible }) =>
   $currentColorScheme &&
