ee8288b16b1e9efd7fd6a8ce0c4bf5237898880c
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2023-05-25 11:57:37 +0300

    Web:Components:Moved StyledIconWrapper to a separate file.

--- packages/components/ColorTheme/styled/iconWrapper.js
+++ packages/components/ColorTheme/styled/iconWrapper.js
@@ -1,25 +1,5 @@
 import styled, { css } from "styled-components";
-const StyledIconWrapper = styled.div`
-  width: 17px;
-  display: flex;
-  align-items: ${(props) => (props.isRoot ? "center" : "flex-end")};
-  justify-content: center;
-
-  svg {
-    path {
-      fill: ${(props) => props.theme.navigation.icon.fill};
-    }
-
-    circle {
-      stroke: ${(props) => props.theme.navigation.icon.fill};
-    }
-
-    path:first-child {
-      fill: none !important;
-      stroke: ${(props) => props.theme.navigation.icon.stroke};
-    }
-  }
-`;
+import StyledIconWrapper from "./sub-components/StyledIconWrapper";
 
 const getDefaultStyles = ({ $currentColorScheme }) =>
   $currentColorScheme &&
