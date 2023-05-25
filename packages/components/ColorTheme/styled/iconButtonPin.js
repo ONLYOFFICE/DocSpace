5238bc5c5f516f71bf461dd04a0808605764405b
gazizova-vlada <vlada.gazizova@onlyoffice.com>
2023-05-25 11:56:48 +0300

    Web:Components:Moved StyledPinIcon to a separate file.

--- packages/components/ColorTheme/styled/iconButtonPin.js
+++ packages/components/ColorTheme/styled/iconButtonPin.js
@@ -1,26 +1,7 @@
 import styled, { css } from "styled-components";
 import commonIconsStyles from "@docspace/components/utils/common-icons-style";
 import Base from "@docspace/components/themes/base";
-
-import IconButton from "@docspace/components/icon-button";
-
-const StyledPinIcon = styled(IconButton)`
-  ${commonIconsStyles}
-
-  svg {
-    path {
-      fill: ${(props) => props.theme.filesSection.rowView.pinColor};
-    }
-  }
-
-  :hover {
-    svg {
-      path {
-        fill: ${(props) => props.theme.filesSection.rowView.pinColor};
-      }
-    }
-  }
-`;
+import StyledPinIcon from "./sub-components/StyledPinIcon";
 
 const getDefaultStyles = ({ $currentColorScheme, theme }) =>
   $currentColorScheme &&
