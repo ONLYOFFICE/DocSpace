3c1ef33f49ee86faa213d7edcd8408caa9b0821c
Viktor Fomin <enotcode@gmail.com>
2023-05-29 14:57:54 +0300

    Common: Components: ErrorBoundary: add error log state

--- packages/common/components/ErrorBoundary/ErrorBoundary.js
+++ packages/common/components/ErrorBoundary/ErrorBoundary.js
@@ -5,7 +5,7 @@ import Error520 from "client/Error520";
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: null };
   }
 
   // eslint-disable-next-line no-unused-vars
@@ -17,13 +17,14 @@ class ErrorBoundary extends React.Component {
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
     console.error(error, errorInfo);
+    this.setState({ error: error });
     this.props.onError && this.props.onError();
   }
 
   render() {
-    if (this.state.hasError) {
+    if (this.state.hasError && this.state.error) {
       // You can render any custom fallback UI
-      return <Error520 />;
+      return <Error520 errorLog={this.state.error} />;
     }
 
     return this.props.children;
