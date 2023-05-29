b9e65a84425ef7052dca2ef306c2c18cd81fc138
Viktor Fomin <enotcode@gmail.com>
2023-05-30 00:00:15 +0300

    Client: Helper: CrashReport: use file-saver

--- packages/client/src/helpers/crashReport.js
+++ packages/client/src/helpers/crashReport.js
@@ -1,3 +1,5 @@
+import saveAs from "file-saver";
+
 export const getCrashReport = (userId, version, language, error) => {
   const currentTime = new Date();
   const reportTime = currentTime.toTimeString();
@@ -23,9 +25,5 @@ export const downloadJson = (json, fileName) => {
   const cleanJson = JSON.stringify(json);
   const data = new Blob([cleanJson], { type: "application/json" });
   const url = window.URL.createObjectURL(data);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = `${fileName}.json`;
-  a.click();
-  a.remove();
+  saveAs(url, `${fileName}.json`);
 };
