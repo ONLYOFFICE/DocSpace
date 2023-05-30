aed4c789ad87a1c3f1fd5f0aef5d7b2d2a79ca19
Viktor Fomin <enotcode@gmail.com>
2023-05-30 12:22:45 +0300

    Client: Pages: Errors: add logo

--- packages/client/src/pages/Errors/520/index.js
+++ packages/client/src/pages/Errors/520/index.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import ErrorContainer from "@docspace/common/components/ErrorContainer";
 import { I18nextProvider, useTranslation } from "react-i18next";
 import i18n from "./i18n";
 
 import { ReportDialog } from "SRC_DIR/components/dialogs";
+import DocspaceLogo from "SRC_DIR/DocspaceLogo";
+
+const StyledWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 28px;
+  margin: 0 16px;
+`;
 
 const Error520 = ({ errorLog }) => {
   const { t } = useTranslation(["Common"]);
@@ -19,7 +31,8 @@ const Error520 = ({ errorLog }) => {
   };
 
   return (
-    <>
+    <StyledWrapper>
+      <DocspaceLogo />
       <ErrorContainer
         isPrimaryButton={false}
         headerText={t("SomethingWentWrong")}
@@ -32,7 +45,7 @@ const Error520 = ({ errorLog }) => {
         onClose={closeDialog}
         error={errorLog}
       />
-    </>
+    </StyledWrapper>
   );
 };
 
