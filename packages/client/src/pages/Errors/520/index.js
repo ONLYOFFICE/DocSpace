9121dc0411861edb92d68b5832bb87298ed98965
Viktor Fomin <enotcode@gmail.com>
2023-05-30 12:47:00 +0300

    Client: Pages: Errors: add reload page

--- packages/client/src/pages/Errors/520/index.js
+++ packages/client/src/pages/Errors/520/index.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { inject, observer } from "mobx-react";
 import styled from "styled-components";
 import ErrorContainer from "@docspace/common/components/ErrorContainer";
+import Link from "@docspace/components/link";
 import { I18nextProvider, useTranslation } from "react-i18next";
 import i18n from "./i18n";
 
@@ -13,11 +15,18 @@ const StyledWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 28px;
   margin: 0 16px;
+
+  .logo {
+    margin-bottom: 28px;
+  }
+
+  .link {
+    margin-top: 24px;
+  }
 `;
 
-const Error520 = ({ errorLog }) => {
+const Error520 = ({ errorLog, currentColorScheme }) => {
   const { t } = useTranslation(["Common"]);
 
   const [reportDialogVisible, setReportDialogVisible] = useState(false);
@@ -30,9 +39,13 @@ const Error520 = ({ errorLog }) => {
     setReportDialogVisible(false);
   };
 
+  const onReloadClick = () => {
+    window.location.reload();
+  };
+
   return (
     <StyledWrapper>
-      <DocspaceLogo />
+      <DocspaceLogo className="logo" />
       <ErrorContainer
         isPrimaryButton={false}
         headerText={t("SomethingWentWrong")}
@@ -40,6 +53,16 @@ const Error520 = ({ errorLog }) => {
         buttonText={t("SendReport")}
         onClickButton={showDialog}
       />
+      <Link
+        className="link"
+        type="action"
+        isHovered
+        fontWeight={600}
+        onClick={onReloadClick}
+        color={currentColorScheme?.main?.accent}
+      >
+        {t("ReloadPage")}
+      </Link>
       <ReportDialog
         visible={reportDialogVisible}
         onClose={closeDialog}
@@ -49,8 +72,16 @@ const Error520 = ({ errorLog }) => {
   );
 };
 
+const Error520Wrapper = inject(({ auth }) => {
+  const { currentColorScheme } = auth.settingsStore;
+
+  return {
+    currentColorScheme,
+  };
+})(observer(Error520));
+
 export default (props) => (
   <I18nextProvider i18n={i18n}>
-    <Error520 {...props} />
+    <Error520Wrapper {...props} />
   </I18nextProvider>
 );
