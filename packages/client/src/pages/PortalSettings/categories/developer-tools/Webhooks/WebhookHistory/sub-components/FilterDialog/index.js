6406fe140234d5739b3e853751497326bbfecfcd
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-05-31 15:53:17 +0500

    dark theme was added / traslations were corrected

--- packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/FilterDialog/index.js
+++ packages/client/src/pages/PortalSettings/categories/developer-tools/Webhooks/WebhookHistory/sub-components/FilterDialog/index.js
@@ -11,6 +11,12 @@ import StatusPicker from "./StatusPicker";
 
 import { useTranslation } from "react-i18next";
 
+import { Base } from "@docspace/components/themes";
+
+const DialogBodyWrapper = styled.div`
+  margin-top: -4px;
+`;
+
 const Footer = styled.div`
   width: 100%;
   display: flex;
@@ -25,18 +31,21 @@ const Footer = styled.div`
 
 const Selectors = styled.div`
   position: relative;
-  margin-top: 12px;
+  margin-top: 8px;
   margin-bottom: 16px;
 `;
 
 const Separator = styled.hr`
-  border-top: 1px solid #eceef1;
-  margin-bottom: 16px;
+  border-top: 1px solid;
+  border-color: ${(props) => (props.theme.isBase ? "#eceef1" : "#474747")};
+  margin-bottom: 14px;
 `;
+
+Separator.defaultProps = { theme: Base };
 
 const FilterDialog = (props) => {
   const { visible, closeModal, applyFilters, formatFilters, setHistoryFilters } = props;
-  const { t } = useTranslation(["Webhooks"]);
+  const { t } = useTranslation(["Webhooks", "Files", "Common"]);
 
   const [filters, setFilters] = useState({
     deliveryDate: null,
@@ -61,29 +70,31 @@ const FilterDialog = (props) => {
 
   return (
     <ModalDialog withFooterBorder visible={visible} onClose={closeModal} displayType="aside">
-      <ModalDialog.Header>{t("SearchOptions")}</ModalDialog.Header>
+      <ModalDialog.Header>{t("Files:Filter")}</ModalDialog.Header>
       <ModalDialog.Body>
-        <DeliveryDatePicker
-          Selectors={Selectors}
-          isApplied={isApplied}
-          setIsApplied={setIsApplied}
-          filters={filters}
-          setFilters={setFilters}
-        />
-        <Separator />
-        <StatusPicker Selectors={Selectors} filters={filters} setFilters={setFilters} />
-        <Separator />
+        <DialogBodyWrapper>
+          <DeliveryDatePicker
+            Selectors={Selectors}
+            isApplied={isApplied}
+            setIsApplied={setIsApplied}
+            filters={filters}
+            setFilters={setFilters}
+          />
+          <Separator />
+          <StatusPicker Selectors={Selectors} filters={filters} setFilters={setFilters} />
+          <Separator />
+        </DialogBodyWrapper>
       </ModalDialog.Body>
 
       <ModalDialog.Footer>
         <Footer>
           <Button
-            label={t("Apply")}
+            label={t("Common:ApplyButton")}
             size="normal"
             primary={true}
             onClick={handleApplyFilters}
           />
-          <Button label={t("Cancel")} size="normal" onClick={closeModal} />
+          <Button label={t("Common:CancelButton")} size="normal" onClick={closeModal} />
         </Footer>
       </ModalDialog.Footer>
     </ModalDialog>
