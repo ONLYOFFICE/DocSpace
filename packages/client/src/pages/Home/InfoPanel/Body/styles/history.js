931b7619aeb27110bdd440743af8d72f2b1c1bce
Mushka Nikita <nikitamushka@mail.ru>
2023-06-01 14:21:00 +0300

    added folder label text ellipsis

--- packages/client/src/pages/Home/InfoPanel/Body/styles/history.js
+++ packages/client/src/pages/Home/InfoPanel/Body/styles/history.js
@@ -108,12 +108,24 @@ const StyledHistoryBlockMessage = styled.div`
   font-size: 13px;
   line-height: 20px;
 
+  display: flex;
+  gap: 4px;
+
   strong {
     font-weight: 600;
   }
 
-  .folderLabel {
+  .main-message {
+    width: max-content;
+    white-space: nowrap;
+  }
+
+  .folder-label {
+    max-width: 100%;
     color: ${(props) => props.theme.infoPanel.history.locationIconColor};
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
   }
 `;
 
