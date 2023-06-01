4ab3461fdac7078db568e663cd0696796e48482b
Viktor Fomin <enotcode@gmail.com>
2023-06-01 12:05:06 +0300

    Common: Components: ErrorBoundary: fix

--- packages/common/components/ErrorBoundary/ErrorBoundary.js
+++ packages/common/components/ErrorBoundary/ErrorBoundary.js
@@ -5,24 +5,23 @@ import Error520 from "client/Error520";
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false, error: null };
+    this.state = { error: null };
   }
 
   // eslint-disable-next-line no-unused-vars
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
-    return { hasError: true };
+    return { error: error ?? "Unhandled exception" };
   }
 
   componentDidCatch(error, errorInfo) {
     // You can also log the error to an error reporting service
     console.error(error, errorInfo);
-    this.setState({ error: error });
     this.props.onError && this.props.onError();
   }
 
   render() {
-    if (this.state.hasError && this.state.error) {
+    if (this.state.error) {
       // You can render any custom fallback UI
       return <Error520 errorLog={this.state.error} />;
     }
