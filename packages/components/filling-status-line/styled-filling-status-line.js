bd6b6b40d256187104bc2e4d844d2f109785f706
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-06-12 12:17:15 +0500

    semicolon was added

--- packages/components/filling-status-line/styled-filling-status-line.js
+++ packages/components/filling-status-line/styled-filling-status-line.js
@@ -43,11 +43,12 @@ const AccordionItem = styled.div`
     cursor: pointer;
     height: 38px;
     padding: 18px 0;
-  
+
     .user-avatar {
-      padding 1px;
-      border: 2px solid #A3A9AE;
-      border-color: ${(props) => (props.isDone && "#4781D1") || (props.isInterrupted && "#F2675A")};
+      padding: 1px;
+      border: 2px solid #a3a9ae;
+      border-color: ${(props) =>
+        (props.isDone && "#4781D1") || (props.isInterrupted && "#F2675A")};
       border-radius: 50%;
     }
 
@@ -58,14 +59,15 @@ const AccordionItem = styled.div`
     .accordion-role {
       color: #657077;
     }
-  
+
     .arrow-icon {
       display: flex;
       align-items: center;
       justify-content: center;
       cursor: pointer;
-      transform: ${(props) => props.isOpen ? "rotate(270deg)" : "rotate(90deg)"};
-      path { 
+      transform: ${(props) =>
+        props.isOpen ? "rotate(270deg)" : "rotate(90deg)"};
+      path {
         fill: ${(props) => (props.isOpen ? "#4781d1" : "#A3A9AE")};
       }
     }
@@ -83,8 +85,11 @@ const AccordionItem = styled.div`
     align-items: center;
     min-height: 40px;
     margin: ${(props) => (props.isDone || props.isInterrupted ? "0" : "2px 0")};
-    border-left: 2px ${(props) => props.isDone || props.isInterrupted ? "solid" : "dashed"} #A3A9AE;
-    border-color: ${(props) => (props.isDone && "#4781D1") || (props.isInterrupted && "#F2675A")};
+    border-left: 2px
+      ${(props) => (props.isDone || props.isInterrupted ? "solid" : "dashed")}
+      #a3a9ae;
+    border-color: ${(props) =>
+      (props.isDone && "#4781D1") || (props.isInterrupted && "#F2675A")};
 
     .status-text {
       margin-left: 15px;
