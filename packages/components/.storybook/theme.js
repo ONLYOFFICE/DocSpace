2c2c67d4861fd6acfdac5ef4b47441613910eb40
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-06-15 13:50:19 +0500

    onlyoffcie logo for storybook was added

---
+++ packages/components/.storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+import Logo from "./lightsmall.svg";
+
+export default create({
+  brandTitle: "OnlyOffice",
+  brandUrl: "https://www.onlyoffice.com/docspace.aspx",
+  brandImage: Logo,
+  brandTarget: "_self",
+});
