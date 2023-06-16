c2fa18856dcb8791d2b85a919ebf0a145f2911ff
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-06-16 18:00:45 +0500

    dark theme background color was changed to #333

--- packages/components/.storybook/DocsContainer.js
+++ packages/components/.storybook/DocsContainer.js
@@ -3,12 +3,17 @@ import { DocsContainer as BaseContainer } from "@storybook/blocks";
 import { themes } from "@storybook/theming";
 
 export const DocsContainer = ({ children, context }) => {
+  const modifiedDarkTheme = {
+    ...themes.dark,
+    appBg: "#333", // Replace 'new-color' with the desired background color
+  };
+
   return (
     <BaseContainer
       context={context}
       theme={
         context.store?.globals.globals.theme === "Dark"
-          ? themes.dark
+          ? modifiedDarkTheme
           : themes.light
       }
     >
