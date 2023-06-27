85a4cb5cc5359c03ef83d4a49bf62a0bdc413ca0
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2023-06-27 18:00:55 +0500

    Add priority use of the microsoft repository to fix dependency issues

--- build/install/OneClickInstall/install-Debian/install-preq.sh
+++ build/install/OneClickInstall/install-Debian/install-preq.sh
@@ -52,6 +52,7 @@ if [ "$DIST" = "debian" ] && [ "$DISTRIB_CODENAME" = "stretch" ]; then
 else
 	curl https://packages.microsoft.com/config/$DIST/$REV/packages-microsoft-prod.deb -O
 fi
+echo -e "Package: *\nPin: origin \"packages.microsoft.com\"\nPin-Priority: 1002" | tee /etc/apt/preferences.d/99microsoft-prod.pref
 dpkg -i packages-microsoft-prod.deb && rm packages-microsoft-prod.deb
 
 MYSQL_REPO_VERSION="$(curl https://repo.mysql.com | grep -oP 'mysql-apt-config_\K.*' | grep -o '^[^_]*' | sort --version-sort --field-separator=. | tail -n1)"
