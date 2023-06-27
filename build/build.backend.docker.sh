bec597b4337fcf9b342114eb3e31aed6c1e0b0e8
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2023-06-27 13:20:15 +0400

    build: fix dev docker sh script

--- build/build.backend.docker.sh
+++ build/build.backend.docker.sh
@@ -48,12 +48,6 @@ echo "SERVICE_CLIENT: $client"
 # Stop all backend services"
 $dir/build/start/stop.backend.docker.sh
 
-# echo "Remove all backend containers"
-# docker rm -f $(docker ps -a | egrep "onlyoffice" | egrep -v "mysql|rabbitmq|redis|elasticsearch|documentserver" | awk 'NR>0 {print $1}')
-
-# echo "Remove all docker images except 'mysql, rabbitmq, redis, elasticsearch, documentserver'"
-# docker rmi -f $(docker images -a | egrep "onlyoffice" | egrep -v "mysql|rabbitmq|redis|elasticsearch|documentserver" | awk 'NR>0 {print $3}')
-
 echo "Run MySQL"
 
 arch_name="$(uname -m)"
@@ -83,32 +77,29 @@ bash $dir/build/install/common/build-services.sh -pb backend-publish -pc Debug -
 
 cd $dir/build/install/docker/
 
-echo "Build backend services images"
-DOCKERFILE=$docker_file \
-DOCKER_TAG="v9.9.9" \
-RELEASE_DATE=$build_date \
-GIT_BRANCH=$branch \
-SERVICE_DOCEDITOR=$doceditor \
-SERVICE_LOGIN=$login \
-SERVICE_CLIENT=$client \
-APP_CORE_BASE_DOMAIN=$core_base_domain \
-ROOT_DIR=$dir \
-ENV_EXTENSION=$env_extension \
-docker compose -f build.dev.yml --profile backend-dev build --build-arg GIT_BRANCH=$branch --build-arg RELEASE_DATE=$build_date
+# echo "Build backend services images"
+# DOCKERFILE=$docker_file \
+# DOCKER_TAG="v9.9.9" \
+# RELEASE_DATE=$build_date \
+# GIT_BRANCH=$branch \
+# SERVICE_DOCEDITOR=$doceditor \
+# SERVICE_LOGIN=$login \
+# SERVICE_CLIENT=$client \
+# APP_CORE_BASE_DOMAIN=$core_base_domain \
+# ROOT_DIR=$dir \
+# ENV_EXTENSION=$env_extension \
+# docker compose -f build.dev.yml --profile backend-dev build --build-arg GIT_BRANCH=$branch --build-arg RELEASE_DATE=$build_date
 
 echo "Run migration"
-DOCKERFILE=$docker_file \
-DOCKER_TAG="v9.9.9" \
+Baseimage_Dotnet_Run="sk81biz/baseimage-dotnet-run:v1.0.0" \
 BUILD_PATH="/var/www" \
 SRC_PATH="$dir/publish/services" \
-ROOT_DIR=$dir \
-DATA_DIR="$dir\Data" \
-ENV_EXTENSION=$env_extension \
 docker-compose -f docspace.yml -f docspace.overcome.yml --profile migration-runner up -d
 
 echo "Run backend services"
-DOCKERFILE=$docker_file \
-DOCKER_TAG="v9.9.9" \
+Baseimage_Dotnet_Run="sk81biz/baseimage-dotnet-run:v1.0.0" \
+Baseimage_Nodejs_Run="sk81biz/baseimage-nodejs-run:v1.0.0" \
+Baseimage_Proxy_Run="sk81biz/baseimage-proxy-run:v1.0.0" \
 BUILD_PATH="/var/www" \
 SRC_PATH="$dir/publish/services" \
 SERVICE_DOCEDITOR=$doceditor \
