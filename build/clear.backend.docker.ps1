1b196f59a4022647ce0cab968afa9341eb32e430
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2023-07-03 12:32:51 +0300

    Build: Fixed clear.backend.docker.ps1

--- build/clear.backend.docker.ps1
+++ build/clear.backend.docker.ps1
@@ -1,21 +1,36 @@
 $Containers = docker ps -aqf "name=^onlyoffice"
 $Images = docker images onlyoffice/docspace* -q
+$RootDir = Split-Path -Parent $PSScriptRoot
+$DockerDir = ($RootDir + "\build\install\docker")
+$LocalIp = (Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration | Where-Object { $_.DHCPEnabled -ne $null -and $_.DefaultIPGateway -ne $null }).IPAddress | Select-Object -First 1
 
-if ($Containers) {
-  Write-Host "Stop all backend containers" -ForegroundColor Blue
-  docker stop $Containers
+Write-Host "Clean up containers, images, volumes or networks" -ForegroundColor Green
 
+if ($Containers -or $Images) {
   Write-Host "Remove all backend containers" -ForegroundColor Blue
-  docker rm -f $Containers
+
+  $Env:DOCUMENT_SERVER_IMAGE_NAME="onlyoffice/documentserver-de:latest"
+  $Env:Baseimage_Dotnet_Run="onlyoffice/4testing-docspace-dotnet-runtime:v1.0.0"
+  $Env:Baseimage_Nodejs_Run="onlyoffice/4testing-docspace-nodejs-runtime:v1.0.0"
+  $Env:Baseimage_Proxy_Run="onlyoffice/4testing-docspace-proxy-runtime:v1.0.0"
+  $Env:SERVICE_CLIENT=($LocalIp + ":5001")
+  $Env:BUILD_PATH="/var/www"
+  $Env:SRC_PATH="$RootDir\publish\services"
+  $Env:ROOT_DIR=$RootDir
+  $Env:DATA_DIR="$RootDir\Data"
+
+  docker compose -f "$DockerDir\docspace.profiles.yml" -f "$DockerDir\docspace.overcome.yml" --profile "migration-runner" down --volumes
+  docker compose -f "$DockerDir\docspace.profiles.yml" -f "$DockerDir\docspace.overcome.yml" --profile "backend-local" down --volumes
+
+  Write-Host "Remove docker contatiners 'mysql'" -ForegroundColor Blue
+  docker compose -f "$DockerDir\db.yml" down --volumes
+
+  Write-Host "Remove docker volumes" -ForegroundColor Blue
+  docker volume prune -f -a
+
+  Write-Host "Remove docker networks" -ForegroundColor Blue
+  docker network prune -f
 }
-
-if ($Images) {
-  Write-Host "Remove all docker images except 'mysql, rabbitmq, redis, elasticsearch, documentserver'" -ForegroundColor Blue
-  docker rmi -f $Images
+else { 
+  Write-Host "No containers, images, volumes or networks to clean up" -ForegroundColor Green
 }
-
-Write-Host "Remove unused volumes." -ForegroundColor Blue
-docker volume prune -f
-
-Write-Host "Remove unused networks." -ForegroundColor Blue
-docker network prune -f
