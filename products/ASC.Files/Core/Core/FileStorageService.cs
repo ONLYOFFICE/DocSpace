047c942bbf9a06b941cf4ccb8bd35eb6bd29dc6b
pavelbannov <pavel.bannov@onlyoffice.com>
2023-07-04 15:52:52 +0300

    bug 63232

--- products/ASC.Files/Core/Core/FileStorageService.cs
+++ products/ASC.Files/Core/Core/FileStorageService.cs
@@ -893,7 +893,7 @@ public class FileStorageService<T> //: IFileStorageService
                     file = await fileDao.SaveFileAsync(file, null);
                 }
 
-                int counter = 0;
+                var counter = 0;
 
                 foreach (var size in _thumbnailSettings.Sizes)
                 {
@@ -2163,7 +2163,7 @@ public class FileStorageService<T> //: IFileStorageService
                 continue;
             }
 
-            ErrorIf(!await _fileSecurity.CanReadAsync(file), FilesCommonResource.ErrorMassage_SecurityException_ReadFile);
+            ErrorIf(!await _fileSecurity.CanConvert(file), FilesCommonResource.ErrorMassage_SecurityException_ReadFile);
 
             if (fileInfo.StartConvert && _fileConverter.MustConvert(file))
             {
