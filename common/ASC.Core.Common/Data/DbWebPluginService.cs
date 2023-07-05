c8672b444b568d085cd8e1c5a4e9165b5b80fe87
Andrey Savihin <andrey.savihin@onlyoffice.com>
2023-07-05 12:20:40 +0300

    ASC.Core.Common: EF: added model, dbcontext and dbservice for web plugins

---
+++ common/ASC.Core.Common/Data/DbWebPluginService.cs
@@ -0,0 +1,84 @@
+﻿// (c) Copyright Ascensio System SIA 2010-2022
+//
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+//
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+//
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+//
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+//
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+//
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+namespace ASC.Core.Data;
+
+[Scope]
+public class DbWebPluginService
+{
+    private readonly IDbContextFactory<WebPluginDbContext> _dbContextFactory;
+
+    public DbWebPluginService(IDbContextFactory<WebPluginDbContext> dbContextFactory)
+    {
+        _dbContextFactory = dbContextFactory;
+    }
+
+    public async Task<DbWebPlugin> SaveAsync(DbWebPlugin webPlugin)
+    {
+        ArgumentNullException.ThrowIfNull(webPlugin);
+
+        using var dbContext = _dbContextFactory.CreateDbContext();
+        await dbContext.AddOrUpdateAsync(q => q.WebPlugins, webPlugin);
+        await dbContext.SaveChangesAsync();
+
+        return webPlugin;
+    }
+
+    public async Task<DbWebPlugin> GetByIdAsync(int id)
+    {
+        using var dbContext = _dbContextFactory.CreateDbContext();
+
+        return await dbContext.WebPlugins.FindAsync(id);
+    }
+
+    public async Task<List<DbWebPlugin>> GetAsync(int tenantId, bool? enabled = null)
+    {
+        using var dbContext = _dbContextFactory.CreateDbContext();
+
+        return await dbContext.WebPlugins
+            .Where(r => r.TenantId == tenantId && (!enabled.HasValue || r.Enabled == enabled.Value))
+            .OrderByDescending(r => r.Id)
+            .ToListAsync();
+    }
+
+    public async Task UpdateAsync(int id, bool enabled)
+    {
+        using var dbContext = _dbContextFactory.CreateDbContext();
+
+        await dbContext.WebPlugins
+           .Where(r => r.Id == id)
+           .ExecuteUpdateAsync(r => r.SetProperty(p => p.Enabled, enabled));
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        using var dbContext = _dbContextFactory.CreateDbContext();
+
+        await dbContext.WebPlugins
+            .Where(r => r.Id == id)
+            .ExecuteDeleteAsync();
+    }
+}
---
+++ common/ASC.Core.Common/EF/Model/DbWebPlugin.cs
@@ -0,0 +1,105 @@
+﻿// (c) Copyright Ascensio System SIA 2010-2022
+//
+// This program is a free software product.
+// You can redistribute it and/or modify it under the terms
+// of the GNU Affero General Public License (AGPL) version 3 as published by the Free Software
+// Foundation. In accordance with Section 7(a) of the GNU AGPL its Section 15 shall be amended
+// to the effect that Ascensio System SIA expressly excludes the warranty of non-infringement of
+// any third-party rights.
+//
+// This program is distributed WITHOUT ANY WARRANTY, without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE. For details, see
+// the GNU AGPL at: http://www.gnu.org/licenses/agpl-3.0.html
+//
+// You can contact Ascensio System SIA at Lubanas st. 125a-25, Riga, Latvia, EU, LV-1021.
+//
+// The  interactive user interfaces in modified source and object code versions of the Program must
+// display Appropriate Legal Notices, as required under Section 5 of the GNU AGPL version 3.
+//
+// Pursuant to Section 7(b) of the License you must retain the original Product logo when
+// distributing the program. Pursuant to Section 7(e) we decline to grant you any rights under
+// trademark law for use of our trademarks.
+//
+// All the Product's GUI elements, including illustrations and icon sets, as well as technical writing
+// content are licensed under the terms of the Creative Commons Attribution-ShareAlike 4.0
+// International. See the License terms at http://creativecommons.org/licenses/by-sa/4.0/legalcode
+
+namespace ASC.Core.Common.EF.Model;
+
+public class DbWebPlugin : BaseEntity
+{
+    public int Id { get; set; }
+    public int TenantId { get; set; }
+    public string Name { get; set; }
+    public bool Enabled { get; set; }
+
+    public DbTenant Tenant { get; set; }
+
+    public override object[] GetKeys()
+    {
+        return new object[] { Id };
+    }
+}
+
+public static class WebPluginExtension
+{
+    public static ModelBuilderWrapper AddDbWebPlugins(this ModelBuilderWrapper modelBuilder)
+    {
+        modelBuilder.Entity<DbWebPlugin>().Navigation(e => e.Tenant).AutoInclude(false);
+
+        modelBuilder
+            .Add(MySqlAddDbWebPlugins, Provider.MySql)
+            .Add(PgSqlAddDbWebPlugins, Provider.PostgreSql);
+
+        return modelBuilder;
+    }
+
+    public static void MySqlAddDbWebPlugins(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DbWebPlugin>(entity =>
+        {
+            entity.HasKey(e => new { e.Id })
+                .HasName("PRIMARY");
+
+            entity.ToTable("webplugins")
+                .HasCharSet("utf8");
+
+            entity.HasIndex(e => e.TenantId)
+                .HasDatabaseName("tenant");
+
+            entity.Property(e => e.TenantId).HasColumnName("tenant_id");
+
+            entity.Property(e => e.Name)
+                .HasColumnName("name")
+                .HasColumnType("varchar(255)")
+                .HasCharSet("utf8")
+                .UseCollation("utf8_general_ci");
+
+            entity.Property(e => e.Enabled)
+                .HasColumnName("enabled")
+                .HasColumnType("tinyint(1)")
+                .HasDefaultValueSql("'0'");
+        });
+    }
+    public static void PgSqlAddDbWebPlugins(this ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DbWebPlugin>(entity =>
+        {
+            entity.HasKey(e => new { e.Id })
+                .HasName("webplugins_pkey");
+
+            entity.ToTable("webplugins", "onlyoffice");
+
+            entity.HasIndex(e => e.TenantId)
+                .HasDatabaseName("tenant_webplugins");
+
+            entity.Property(e => e.TenantId).HasColumnName("tenant_id");
+
+            entity.Property(e => e.Name)
+                .HasColumnName("name")
+                .HasMaxLength(255);
+
+            entity.Property(e => e.Enabled).HasColumnName("enabled");
+        });
+    }
+}
