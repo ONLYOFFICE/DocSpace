f59de04ebceda8e5b7555496c42c29dd4bb068bf
Viktor Fomin <enotcode@gmail.com>
2023-07-07 14:43:19 +0300

    Management: init App

--- packages/management/src/App.tsx
+++ packages/management/src/App.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { observer, Provider as MobxProvider } from "mobx-react";
 
-const App = () => {
-  return <h1>Management</h1>;
-};
+import ThemeProvider from "@docspace/components/theme-provider";
+import "@docspace/common/custom.scss";
 
-export default App;
+import { RootStoreContext, RootStore, useStore } from "./store";
+import Client from "./categories";
+
+import store from "client/store";
+
+const App = observer(() => {
+  const { authStore } = useStore();
+  const { init, settingsStore, userStore } = authStore;
+  const { theme, setTheme } = settingsStore;
+
+  const userTheme = userStore?.user?.theme ? userStore?.user?.theme : "Dark";
+
+  useEffect(() => {
+    const initData = async () => {
+      await init();
+    };
+
+    initData();
+  }, []);
+
+  useEffect(() => {
+    if (userTheme) setTheme(userTheme);
+  }, [userTheme]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Client />
+    </ThemeProvider>
+  );
+});
+
+export default (props: any) => (
+  <MobxProvider {...store}>
+    <RootStoreContext.Provider value={new RootStore()}>
+      <App {...props} />
+    </RootStoreContext.Provider>
+  </MobxProvider>
+);
