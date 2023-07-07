82a2efbf73e081d89537c2030f7e6495d4df19ba
Vladimir Khvan <vladimirkhvan23@gmail.com>
2023-07-07 16:19:12 +0500

    className was added

--- packages/components/date-picker/index.js
+++ packages/components/date-picker/index.js
@@ -126,7 +126,7 @@ const DatePicker = (props) => {
           <DateSelector onClick={toggleCalendar} ref={selectorRef}>
             <SelectorAddButton
               title={t("Select")}
-              className="mr-8"
+              className="mr-8 add-delivery-date-button"
               iconName={CalendarIconUrl}
             />
             <Text isInline fontWeight={600} color="#A3A9AE">
