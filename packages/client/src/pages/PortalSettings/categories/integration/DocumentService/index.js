1aaa7643bb725d54385d88b16124a7aa069c60f0
Mushka Nikita <nikitamushka@mail.ru>
2023-07-07 15:19:27 +0300

    added regex validation to inputs

--- packages/client/src/pages/PortalSettings/categories/integration/DocumentService/index.js
+++ packages/client/src/pages/PortalSettings/categories/integration/DocumentService/index.js
@@ -10,48 +10,64 @@ import {
   Label,
 } from "@docspace/components";
 
-const DocumentService = ({}) => {
+const URL_REGEX = /^https?:\/\/[-a-zA-Z0-9@:%._\+~#=]{1,256}$/;
+
+const DocumentService = ({ changeDocumentServiceLocation }) => {
   const { t } = useTranslation(["Settings", "Common"]);
 
-  const [editingAdress, setEditingAdress] = useState("");
-  const onChangeEditingAdress = (e) => setEditingAdress(e.target.value);
+  const [apiUrl, setApiUrl] = useState("");
+  const [apiUrlIsValid, setApiUrlIsValid] = useState(true);
+  const onChangeApiUrl = (e) => {
+    setApiUrl(e.target.value);
+    if (!e.target.value) setApiUrlIsValid(true);
+    else setApiUrlIsValid(URL_REGEX.test(e.target.value));
+  };
 
-  const [fromDocspaceAdress, setFromDocspaceAdress] = useState("");
-  const onChangeFromDocspaceAdress = (e) =>
-    setFromDocspaceAdress(e.target.value);
+  const [internalUrl, setInternalUrl] = useState("");
+  const [internalUrlIsValid, setInternalUrlIsValid] = useState(true);
+  const onChangeInternalUrl = (e) => {
+    setInternalUrl(e.target.value);
+    if (!e.target.value) setInternalUrlIsValid(true);
+    else setInternalUrlIsValid(URL_REGEX.test(e.target.value));
+  };
 
-  const [fromDocServiceAdress, setFromDocServiceAdress] = useState("");
-  const onChangeFromDocServiceAdress = (e) =>
-    setFromDocServiceAdress(e.target.value);
+  const [portalUrl, setPortalUrl] = useState("");
+  const [portalUrlIsValid, setPortalUrlIsValid] = useState(true);
+  const onChangePortalUrl = (e) => {
+    setPortalUrl(e.target.value);
+    if (!e.target.value) setPortalUrlIsValid(true);
+    else setPortalUrlIsValid(URL_REGEX.test(e.target.value));
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
+    changeDocumentServiceLocation(apiUrl, internalUrl, portalUrl);
   };
 
-  const onReset = () => {};
+  const onReset = () => {
+    setApiUrl("");
+    setInternalUrl("");
+    setPortalUrl("");
+    setApiUrlIsValid(true);
+    setInternalUrlIsValid(true);
+    setPortalUrlIsValid(true);
+  };
 
   return (
     <Styled.Location>
       <Styled.LocationHeader>
         <div className="main">
           <Heading className={"heading"} isInline level={3}>
-            {/* Document Service Location */}
             {t("Settings:DocumentServiceLocationHeader")}
           </Heading>
           <div className="help-button-wrapper">
             <HelpButton
-              tooltipContent={
-                t("Settings:DocumentServiceLocationHeaderHelp")
-                // "Document Service is the server service which allows to perform the document editing and allows to convert the document file into the appropriate OfficeOpen XML format."
-              }
+              tooltipContent={t("Settings:DocumentServiceLocationHeaderHelp")}
             />
           </div>
         </div>
         <div className="secondary">
-          {
-            t("Settings:DocumentServiceLocationHeaderInfo")
-            // " Document service location specifies the address of the server with the document services installed. Please change the '<editors-dns-name>' for the server address in the below lines leaving the rest of the line exactly as it is."
-          }
+          {t("Settings:DocumentServiceLocationHeaderInfo")}
         </div>
       </Styled.LocationHeader>
 
@@ -60,52 +76,46 @@ const DocumentService = ({}) => {
           <div className="input-wrapper">
             <Label
               htmlFor="editingAdress"
-              text={
-                // "Document Editing Service Address"
-                t("Settings:DocumentServiceLocationUrlApi")
-              }
+              text={t("Settings:DocumentServiceLocationUrlApi")}
             />
             <InputBlock
               id="editingAdress"
               scale
               iconButtonClassName={"icon-button"}
-              value={editingAdress}
-              onChange={onChangeEditingAdress}
+              value={apiUrl}
+              onChange={onChangeApiUrl}
               placeholder={"http://<editors-dns-name>/"}
+              hasError={!apiUrlIsValid}
             />
           </div>
           <div className="input-wrapper">
             <Label
               htmlFor="fromDocspaceAdress"
-              text={
-                t("Settings:DocumentServiceLocationUrlInternal")
-                // "Document Service address for requests from the DocSpace"
-              }
+              text={t("Settings:DocumentServiceLocationUrlInternal")}
             />
             <InputBlock
               id="fromDocspaceAdress"
               scale
               iconButtonClassName={"icon-button"}
-              value={fromDocspaceAdress}
-              onChange={onChangeFromDocspaceAdress}
+              value={internalUrl}
+              onChange={onChangeInternalUrl}
               placeholder={"http://<editors-dns-name>/"}
+              hasError={!internalUrlIsValid}
             />
           </div>
           <div className="input-wrapper">
             <Label
               htmlFor="fromDocServiceAdress"
-              text={
-                t("Settings:DocumentServiceLocationUrlPortal")
-                // "DocSpace address for requests from the Document Service"
-              }
+              text={t("Settings:DocumentServiceLocationUrlPortal")}
             />
             <InputBlock
               id="fromDocServiceAdress"
               scale
               iconButtonClassName={"icon-button"}
-              value={fromDocServiceAdress}
-              onChange={onChangeFromDocServiceAdress}
+              value={portalUrl}
+              onChange={onChangePortalUrl}
               placeholder={"http://<win-nvplrl2avjo/"}
+              hasError={!portalUrlIsValid}
             />
           </div>
         </div>
@@ -129,6 +139,8 @@ const DocumentService = ({}) => {
   );
 };
 
-export default inject(({}) => {
-  return {};
+export default inject(({ settingsStore }) => {
+  return {
+    changeDocumentServiceLocation: settingsStore.changeDocumentServiceLocation,
+  };
 })(observer(DocumentService));
