6e9e3e07f89b60cfc9b447633316f1d95e69df14
gazizova-vlada <vladamuzyka@yandex.ru>
2023-07-11 23:05:25 +0300

    Web:Changed content, translations.

--- packages/client/src/components/dialogs/DeleteProfileEverDialog/index.js
+++ packages/client/src/components/dialogs/DeleteProfileEverDialog/index.js
@@ -10,12 +10,43 @@ import { withTranslation, Trans } from "react-i18next";
 import api from "@docspace/common/api";
 import toastr from "@docspace/components/toast/toastr";
 import ModalDialogContainer from "../ModalDialogContainer";
+import Link from "@docspace/components/link";
 import { inject, observer } from "mobx-react";
 import config from "PACKAGE_FILE";
 import { combineUrl } from "@docspace/common/utils";
+import styled from "styled-components";
 
 const { deleteUser } = api.people; //TODO: Move to action
 const { Filter } = api;
+
+const StyledModalDialogContainer = styled(ModalDialogContainer)`
+  #modal-dialog {
+    width: auto;
+    max-width: 520px;
+    max-height: none;
+  }
+
+  .user-delete {
+    line-height: 20px;
+    padding-bottom: 16px;
+  }
+
+  .text-warning {
+    color: #f24724;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 22px;
+  }
+
+  .text-delete-description {
+    line-height: 20px;
+    padding: 8px 0;
+  }
+
+  .reassign-data {
+    line-height: 15px;
+  }
+`;
 
 const DeleteProfileEverDialogComponent = (props) => {
   const {
@@ -28,6 +59,7 @@ const DeleteProfileEverDialogComponent = (props) => {
     visible,
     setDataReassignmentDialogVisible,
     setDeleteProfileDialogVisible,
+    userPerformedDeletion,
   } = props;
   const [isRequestRunning, setIsRequestRunning] = React.useState(false);
 
@@ -51,27 +83,54 @@ const DeleteProfileEverDialogComponent = (props) => {
       .finally(() => onClose());
   };
 
-  const onClickDataReassignmentDialogVisible = () => {
+  const onClickDataReassignmentDialog = () => {
     setDataReassignmentDialogVisible(true);
     setDeleteProfileDialogVisible(false);
   };
   return (
-    <ModalDialogContainer
+    <StyledModalDialogContainer
       isLoading={!tReady}
       visible={visible}
       onClose={onClose}
     >
       <ModalDialog.Header>{t("DeleteUser")}</ModalDialog.Header>
       <ModalDialog.Body>
-        <Text>
+        <Text className="user-delete">
           <Trans i18nKey="DeleteUserMessage" ns="DeleteProfileEverDialog" t={t}>
-            {{ userCaption: t("Common:User") }}{" "}
+            {{ userCaption: t("Common:User") }}
             <strong>{{ user: user.displayName }}</strong>
-            will be deleted. User personal documents which are available to
-            others will be deleted.
+            will be deleted. This action cannot be undone.
           </Trans>
         </Text>
-        <div onClick={onClickDataReassignmentDialogVisible}>Reassign data</div>
+        <Text className="text-warning">{t("Common:Warning")}!</Text>
+        <Text className="text-delete-description">
+          <Trans
+            i18nKey="DeleteReassignDescription"
+            ns="DeleteProfileEverDialog"
+            t={t}
+          >
+            All personal files and folders in My documents section of this user
+            will be permanently deleted. Rooms created by this user and
+            documents stored in these rooms will be reassigned automatically to
+            an admin performing the deletion:
+            <strong>
+              {{ userPerformedDeletion }} ({{ userYou: t("Common:You") }})
+            </strong>
+            Reassign data manually to choose another destination user for
+            reassignment
+          </Trans>
+        </Text>
+
+        <Link
+          className="reassign-data"
+          type="action"
+          fontSize="13px"
+          fontWeight={600}
+          isHovered={true}
+          onClick={onClickDataReassignmentDialog}
+        >
+          {t("DataReassignmentDialog:ReassignData")}
+        </Link>
       </ModalDialog.Body>
       <ModalDialog.Footer>
         <Button
@@ -90,12 +149,13 @@ const DeleteProfileEverDialogComponent = (props) => {
           onClick={onClose}
         />
       </ModalDialog.Footer>
-    </ModalDialogContainer>
+    </StyledModalDialogContainer>
   );
 };
 
 const DeleteProfileEverDialog = withTranslation([
   "DeleteProfileEverDialog",
+  "DataReassignmentDialog",
   "Common",
   "PeopleTranslations",
 ])(DeleteProfileEverDialogComponent);
@@ -113,4 +173,5 @@ export default inject(({ peopleStore }) => ({
     peopleStore.dialogStore.setDataReassignmentDialogVisible,
   setDeleteProfileDialogVisible:
     peopleStore.dialogStore.setDeleteProfileDialogVisible,
+  userPerformedDeletion: peopleStore.authStore.userStore.user.displayName,
 }))(observer(DeleteProfileEverDialog));
