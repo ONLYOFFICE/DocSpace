2fa1a878d055af953c14df5810cffd496c14e57b
Viktor Fomin <enotcode@gmail.com>
2023-07-11 15:45:59 +0300

    Client: PortalSettings: CommonWhiteLabel: fix tooltip

--- packages/client/src/pages/PortalSettings/categories/common/Branding/CommonWhiteLabel.js
+++ packages/client/src/pages/PortalSettings/categories/common/Branding/CommonWhiteLabel.js
@@ -53,7 +53,7 @@ const CommonWhiteLabel = ({
       <div className="wl-helper">
         <Text className="settings_unavailable">{t("WhiteLabelHelper")}</Text>
         <HelpButton
-          tooltipContent={t("WhiteLabelTooltip")}
+          tooltipContent={<Text fontSize="12px">{t("WhiteLabelTooltip")}</Text>}
           place="right"
           offsetRight={0}
           className="settings_unavailable"
