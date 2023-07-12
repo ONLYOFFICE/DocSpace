00c40ef442ef6147be43a7b5e1c818f9c3e91123
Evgeniy Antonyuk <antonyuk.evgenyiy@onlyoffice.com>
2023-07-12 10:56:58 +0500

    Optimize tag sorting in OCI docker

--- build/install/OneClickInstall/install-Docker.sh
+++ build/install/OneClickInstall/install-Docker.sh
@@ -829,13 +829,13 @@ get_available_version () {
 		TAGS_RESP=$(echo $TAGS_RESP | jq -r '.results[].name')
 	fi
 
-	VERSION_REGEX_1="[0-9]+\.[0-9]+\.[0-9]+$"
-	VERSION_REGEX_2="[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$"
+	VERSION_REGEX="[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?$"
+	
 	TAG_LIST=""
 
 	for item in $TAGS_RESP
 	do
-		if [[ $item =~ $VERSION_REGEX_1 ]] || [[ $item =~ $VERSION_REGEX_2 ]]; then
+		if [[ $item =~ $VERSION_REGEX ]]; then
 			TAG_LIST="$item,$TAG_LIST"
 		fi
 	done
