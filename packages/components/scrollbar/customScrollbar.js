b34f10ddff41b1f688b56ca985918feea8a8524e
Subhonbek <basubhonbek@gmail.com>
2023-07-13 15:01:15 +0300

    web component rlt fix id in scrollbar

--- packages/components/scrollbar/customScrollbar.js
+++ packages/components/scrollbar/customScrollbar.js
@@ -2,20 +2,22 @@ import React from "react";
 import StyledScrollbar from "./styled-customScrollbar";
 import { useTheme } from "styled-components";
 
-const CustomScrollbars = React.forwardRef(({ className, ...props }, ref) => {
-  const theme = useTheme();
-  return (
-    <StyledScrollbar className={className} {...props}>
-      <div
-        className={`container  scroll-body ${props.scrollclass}`}
-        {...props}
-        style={{ overflow: "scroll", direction: theme.interfaceDirection }}
-        ref={ref}
-      >
-        {props.children}
-      </div>
-    </StyledScrollbar>
-  );
-});
+const CustomScrollbars = React.forwardRef(
+  ({ className, id, ...props }, ref) => {
+    const theme = useTheme();
+    return (
+      <StyledScrollbar className={className} {...props} id={id}>
+        <div
+          className={`container  scroll-body ${props.scrollclass}`}
+          {...props}
+          style={{ overflow: "scroll", direction: theme.interfaceDirection }}
+          ref={ref}
+        >
+          {props.children}
+        </div>
+      </StyledScrollbar>
+    );
+  }
+);
 
 export default CustomScrollbars;
