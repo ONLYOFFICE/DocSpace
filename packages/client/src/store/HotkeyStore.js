9c1b9387031a40215c312dfb5a6bf7288802a243
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2023-07-24 12:28:55 +0200

    Web: Components: Fix scroll when file element is not visible

--- packages/client/src/store/HotkeyStore.js
+++ packages/client/src/store/HotkeyStore.js
@@ -34,7 +34,7 @@ class HotkeyStore {
 
   scrollToCaret = () => {
     const { offsetTop, item } = this.getItemOffset();
-    const scroll = document.getElementsByClassName("section-scroll")[0];
+    const scroll = document.getElementById("sectionScroll");
     const scrollRect = scroll?.getBoundingClientRect();
 
     if (item && item[0]) {
