e84e228f814ffa909f4d62f9bb0cc7a758a9d484
DmitrySychugov <dmitry.s.develop@mail.ru>
2023-07-25 16:38:36 +0500

    Web: Management: create configuration page

---
+++ packages/management/src/categories/spaces/sub-components/ConfigurationSection.tsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { inject, observer } from "mobx-react";
+import Button from "@docspace/components/button";
+import TextInput from "@docspace/components/text-input";
+import Text from "@docspace/components/text";
+import { ConfigurationWrapper } from "../StyledSpaces";
+import { parseAddress } from "@docspace/components/utils/email";
+import { useStore } from "SRC_DIR/store";
+
+const ConfigurationSection = () => {
+  const [domain, setDomain] = React.useState<string>("");
+  const [name, setName] = React.useState<string>("");
+
+  const { spacesStore } = useStore();
+
+  const { validatePortalName, setPortalSettings } = spacesStore;
+
+  const onHandleDomain = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setDomain(e.target.value);
+
+  const onHandleName = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setName(e.target.value);
+
+  let parsed = parseAddress("test@" + domain);
+  const isDomainError = domain.length > 0 && !parsed.isValid();
+  const isNameError = name.length > 0 && (name.length > 100 || name.length < 6);
+
+  return (
+    <ConfigurationWrapper>
+      <div className="spaces-configuration-header">
+        <div className="spaces-configuration-title">
+          <Text fontSize="16px" fontWeight={700}>
+            Current space configuration
+          </Text>
+        </div>
+        <Text>
+          You need to connect your domain to your current IP to change the
+          address settings. Please make sure you have access to the domain
+          settings to be able to do that. After connecting the domain, create a
+          DocSpace name. You can change the created DocSpace name later using
+          the space settings.
+        </Text>
+      </div>
+      <div className="spaces-input-wrapper">
+        <div className="spaces-input-block">
+          <div className="spaces-text-wrapper">
+            <Text
+              color="#333"
+              fontSize="13px"
+              fontWeight="600"
+              className="spaces-domain-text"
+            >
+              Domain
+            </Text>
+            <Text color="#A3A9AE">(example.com)</Text>
+          </div>
+
+          <TextInput
+            hasError={isDomainError}
+            onChange={onHandleDomain}
+            value={domain}
+            placeholder="Enter domain name"
+            className="spaces-input"
+          />
+        </div>
+        <div className="spaces-input-block">
+          <Text color="#333" fontSize="13px" fontWeight="600">
+            DocSpace name
+          </Text>
+          <TextInput
+            hasError={isNameError}
+            onChange={onHandleName}
+            value={name}
+            placeholder="Enter name"
+            className="spaces-input"
+          />
+        </div>
+      </div>
+
+      <Button
+        isDisabled={!(name && domain) || isDomainError || isNameError}
+        size="normal"
+        className="spaces-button"
+        label="Connect"
+        onClick={() => setPortalSettings(domain, name)}
+        primary={true}
+      />
+    </ConfigurationWrapper>
+  );
+};
+
+export default observer(ConfigurationSection);
