0beeb368becd32d2a01096199393e6d43ec6c80c
Viktor Fomin <enotcode@gmail.com>
2023-07-26 14:22:22 +0300

    Common: init ArticleWrapper

---
+++ packages/common/components/Article/ArticleWrapper.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { Provider as MobxProvider } from "mobx-react";
+import store from "client/store";
+import Article from "./";
+
+const { auth: authStore } = store;
+
+const ArticleWrapper = (props) => {
+  useEffect(() => {
+    authStore.init(true);
+  }, []);
+
+  return (
+    <MobxProvider {...store}>
+      <Article {...props} />
+    </MobxProvider>
+  );
+};
+
+export default ArticleWrapper;
