7f4b1b57e4cfdbaafc508368d2db7dfde6d2f0a9
Viktor Fomin <enotcode@gmail.com>
2023-07-26 15:55:39 +0300

    Management: Layout: Section: Header: get translated header

--- packages/management/src/Layout/Section/Header/index.tsx
+++ packages/management/src/Layout/Section/Header/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 import Headline from "@docspace/common/components/Headline";
 
+import { getItemByLink } from "SRC_DIR/utils";
+
 const SectionHeaderContent = () => {
+  const { t } = useTranslation("Settings");
+
+  const location = useLocation();
+  const path = location.pathname;
+  const item = getItemByLink(path);
+
   return (
     <Headline type="content" truncate={true}>
-      <div className="header">Header</div>
+      <div className="header">{t(item.tKey)}</div>
     </Headline>
   );
 };
