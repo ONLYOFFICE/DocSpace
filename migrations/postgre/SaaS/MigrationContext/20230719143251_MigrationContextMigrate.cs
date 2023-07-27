a081ec02242069ccac56a19aa60267edc371efe0
Andrey Savihin <andrey.savihin@onlyoffice.com>
2023-07-27 11:55:51 +0300

    Migrations: new table webplugins

--- migrations/postgre/SaaS/MigrationContext/20230719143251_MigrationContextMigrate.cs
+++ migrations/postgre/SaaS/MigrationContext/20230719143251_MigrationContextMigrate.cs
@@ -1296,6 +1296,40 @@ namespace ASC.Migrations.PostgreSql.SaaS.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "webplugins",
+                schema: "onlyoffice",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    tenantid = table.Column<int>(name: "tenant_id", type: "integer", nullable: false),
+                    name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    version = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    license = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    author = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    homepage = table.Column<string>(name: "home_page", type: "character varying(255)", maxLength: 255, nullable: true),
+                    pluginname = table.Column<string>(name: "plugin_name", type: "character varying(255)", maxLength: 255, nullable: false),
+                    scopes = table.Column<string>(type: "text", nullable: true),
+                    image = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    createby = table.Column<Guid>(name: "create_by", type: "uuid", fixedLength: true, maxLength: 36, nullable: false),
+                    createon = table.Column<DateTime>(name: "create_on", type: "timestamp with time zone", nullable: false),
+                    enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    system = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("webplugins_pkey", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_webplugins_tenants_tenants_tenant_id",
+                        column: x => x.tenantid,
+                        principalSchema: "onlyoffice",
+                        principalTable: "tenants_tenants",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
             migrationBuilder.InsertData(
                 schema: "onlyoffice",
                 table: "files_converts",
@@ -2420,6 +2454,12 @@ namespace ASC.Migrations.PostgreSql.SaaS.Migrations
                 schema: "onlyoffice",
                 table: "webstudio_uservisit",
                 column: "visitdate");
+
+            migrationBuilder.CreateIndex(
+                name: "tenant_webplugins",
+                schema: "onlyoffice",
+                table: "webplugins",
+                column: "tenant_id");
         }
 
         /// <inheritdoc />
@@ -2633,6 +2673,10 @@ namespace ASC.Migrations.PostgreSql.SaaS.Migrations
 
             migrationBuilder.DropTable(
                 name: "tenants_tenants",
+                schema: "onlyoffice");
+
+            migrationBuilder.DropTable(
+                name: "webplugins",
                 schema: "onlyoffice");
         }
     }
