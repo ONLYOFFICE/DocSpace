30f7af4683cf7c08ef7c407a1b442b39010db0d3
Subhonbek <basubhonbek@gmail.com>
2023-07-27 16:38:44 +0300

    web client fix rtl cell style

--- packages/client/src/pages/Home/Section/Body/TableView/sub-components/CellStyles.js
+++ packages/client/src/pages/Home/Section/Body/TableView/sub-components/CellStyles.js
@@ -22,7 +22,14 @@ const StyledAuthorCell = styled.div`
     width: 16px;
     min-width: 16px;
     height: 16px;
-    margin-right: 8px;
+    ${props =>
+      props.theme.interfaceDirection === "rtl"
+        ? css`
+            margin-left: 8px;
+          `
+        : css`
+            margin-right: 8px;
+          `}
   }
 `;
 
