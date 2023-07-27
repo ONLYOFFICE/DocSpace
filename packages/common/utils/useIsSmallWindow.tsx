e7bb8f05ee9a45455e63ba68bddfeb67f8218985
Viktor Fomin <enotcode@gmail.com>
2023-07-28 01:34:43 +0300

    Common: Utils: init useIsSmallWindow

---
+++ packages/common/utils/useIsSmallWindow.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { isDesktop } from "react-device-detect";
+
+export const useIsSmallWindow = (windowWidth: number): boolean => {
+  const [isSmallWindow, setIsSmallWindow] = useState(false);
+
+  const onCheckView = () => {
+    if (isDesktop && window.innerWidth < windowWidth) {
+      setIsSmallWindow(true);
+    } else {
+      setIsSmallWindow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onCheckView);
+
+    return () => window.removeEventListener("resize", onCheckView);
+  }, []);
+
+  return isSmallWindow;
+};
