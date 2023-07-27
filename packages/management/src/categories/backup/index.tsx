fc1108cf19b66ff961ba102d0a7465d50ab17f15
Viktor Fomin <enotcode@gmail.com>
2023-07-27 12:18:56 +0300

    Management: add routes

---
+++ packages/management/src/categories/backup/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Backup = () => {
+  return <h1>Backup</h1>;
+};
+
+export default Backup;
---
+++ packages/management/src/categories/payments/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Payments = () => {
+  return <h1>Payments</h1>;
+};
+
+export default Payments;
