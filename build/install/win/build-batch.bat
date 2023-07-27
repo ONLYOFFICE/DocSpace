02dd068a503b906a9bfead565fa3f9b6e667179e
Nasrullo Nurullaev <nasrullo.nurullaev@onlyoffice.com>
2023-07-27 09:24:08 +0500

    Fix issue with parsing data from nlog.config

--- build/install/win/build-batch.bat
+++ build/install/win/build-batch.bat
@@ -42,7 +42,7 @@ del /f /q build\install\win\Files\config\*.test.json
 del /f /q build\install\win\Files\config\*.dev.json
 
 ::default logging to warning
-%sed% "s_\(minlevel=\)".*"_\1\"Warn\"_g" -i build\install\win\Files\config\nlog.config
+%sed% -i "s/minlevel=\"Debug\""/minlevel=\"Warn\""/g" build\install\win\Files\config\nlog.config
 %sed% "s_\(\"Default\":\).*,_\1 \"Warning\",_g" -i build\install\win\Files\config\appsettings.json
 %sed% "s_\(\"logLevel\":\).*_\1 \"warning\"_g" -i build\install\win\Files\config\appsettings.services.json
 %sed% "/\"debug-info\": {/,/}/ s/\(\"enabled\": \)\".*\"/\1\"false\"/" -i build\install\win\Files\config\appsettings.json
