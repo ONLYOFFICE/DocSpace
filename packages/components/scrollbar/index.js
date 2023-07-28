8e4c7d96d11f472907c1335292407966ea4791c5
Aleksandr Lushkin <aleksandr.lushkin@onlyoffice.com>
2023-07-28 10:05:53 +0200

    Web: Components: Refactor scrollbar

--- packages/components/scrollbar/index.js
+++ packages/components/scrollbar/index.js
@@ -2,9 +2,96 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 
 import { isMobile } from "@docspace/components/utils/device";
-import { classNames } from "../utils/classNames";
 import StyledScrollbar from "./styled-scrollbar";
 import { useTheme } from "styled-components";
+
+const scrollbarTypes = {
+  smallWhite: {
+    thumbV: {
+      width: "2px",
+      marginLeft: "2px",
+      borderRadius: "inherit",
+    },
+    thumbH: {
+      height: "2px",
+      marginTop: "2px",
+      borderRadius: "inherit",
+    },
+    trackV: {
+      width: "2px",
+      background: "transparent",
+    },
+    trackH: {
+      height: "2px",
+      background: "transparent",
+    },
+    content: { outline: "none", WebkitOverflowScrolling: "auto" },
+  },
+  smallBlack: {
+    thumbV: {
+      width: "3px",
+      marginLeft: "2px",
+      borderRadius: "inherit",
+    },
+    thumbH: {
+      height: "3px",
+      marginTop: "2px",
+      borderRadius: "inherit",
+    },
+    trackV: {
+      width: "3px",
+      background: "transparent",
+    },
+    trackH: {
+      height: "3px",
+      background: "transparent",
+    },
+    content: { outline: "none", WebkitOverflowScrolling: "auto" },
+  },
+  mediumBlack: {
+    thumbV: {
+      width: "8px",
+      borderRadius: "inherit",
+    },
+    thumbH: {
+      height: "8px",
+      borderRadius: "inherit",
+    },
+    trackV: {
+      width: "8px",
+      background: "transparent",
+    },
+    trackH: {
+      height: "8px",
+      background: "transparent",
+    },
+    content: {
+      outline: "none",
+      WebkitOverflowScrolling: "auto",
+    },
+  },
+  preMediumBlack: {
+    thumbV: {
+      width: "5px",
+      borderRadius: "inherit",
+      cursor: "default",
+    },
+    thumbH: {
+      height: "5px",
+      borderRadius: "inherit",
+      cursor: "default",
+    },
+    trackV: {
+      width: "5px",
+      background: "transparent",
+    },
+    trackH: {
+      height: "5px",
+      background: "transparent",
+    },
+    content: { outline: "none", WebkitOverflowScrolling: "auto" },
+  },
+};
 
 const Scrollbar = React.forwardRef((props, ref) => {
   const {
@@ -24,117 +111,12 @@ const Scrollbar = React.forwardRef((props, ref) => {
 
   const isRtl = interfaceDirection === "rtl";
 
-  const scrollbarTypes = {
-    smallWhite: {
-      thumbV: {
-        width: "2px",
-        marginLeft: "2px",
-        borderRadius: "inherit",
-      },
-      thumbH: {
-        height: "2px",
-        marginTop: "2px",
-        borderRadius: "inherit",
-      },
-      trackV: {
-        width: "2px",
-        background: "transparent",
-      },
-      trackH: {
-        height: "2px",
-        background: "transparent",
-      },
-      content: { outline: "none", WebkitOverflowScrolling: "auto" },
-    },
-    smallBlack: {
-      thumbV: {
-        width: "3px",
-        marginLeft: "2px",
-        borderRadius: "inherit",
-      },
-      thumbH: {
-        height: "3px",
-        marginTop: "2px",
-        borderRadius: "inherit",
-      },
-      trackV: {
-        width: "3px",
-        background: "transparent",
-      },
-      trackH: {
-        height: "3px",
-        background: "transparent",
-      },
-      content: { outline: "none", WebkitOverflowScrolling: "auto" },
-    },
-    mediumBlack: {
-      thumbV: {
-        width: "8px",
-        borderRadius: "inherit",
-      },
-      thumbH: {
-        height: "8px",
-        borderRadius: "inherit",
-      },
-      trackV: {
-        width: "8px",
-        background: "transparent",
-      },
-      trackH: {
-        height: "8px",
-        background: "transparent",
-      },
-      content: {
-        paddingRight: !isRtl && (isMobile() ? "8px" : "17px"),
-        paddingLeft: isRtl && (isMobile() ? "8px" : "17px"),
-        outline: "none",
-        WebkitOverflowScrolling: "auto",
-      },
-    },
-    preMediumBlack: {
-      thumbV: {
-        width: "5px",
-        borderRadius: "inherit",
-        cursor: "default",
-      },
-      thumbH: {
-        height: "5px",
-        borderRadius: "inherit",
-        cursor: "default",
-      },
-      trackV: {
-        width: "5px",
-        background: "transparent",
-      },
-      trackH: {
-        height: "5px",
-        background: "transparent",
-      },
-      content: { outline: "none", WebkitOverflowScrolling: "auto" },
-    },
-  };
+  const scrollbarType = scrollbarTypes[stype] ?? {};
 
-  const scrollbarType = scrollbarTypes[stype];
-
-  const thumbVStyles = scrollbarType ? scrollbarType.thumbV : {};
-  const thumbHStyles = scrollbarType ? scrollbarType.thumbH : {};
-
-  // Add 1px margin to vertical track to avoid scrollbar lib crashing when event.clientX equals 0
-  const trackVCommonStyles = {
-    marginLeft: isRtl ? "1px" : "0",
-    marginRight: isRtl ? "0" : "1px",
-  };
   const tracksCommonStyles = {
     opacity: autoHide && !isScrolling ? 0 : 1,
     transition: "opacity 0.4s ease-in-out",
   };
-  const trackVStyles = scrollbarType
-    ? { ...scrollbarType.trackV, ...tracksCommonStyles, ...trackVCommonStyles }
-    : { ...tracksCommonStyles, ...trackVCommonStyles };
-  const trackHStyles = scrollbarType
-    ? { ...scrollbarType.trackH, ...tracksCommonStyles }
-    : { ...tracksCommonStyles };
-  const contentStyles = scrollbarType ? scrollbarType.content : {};
 
   const showTrack = useCallback(() => {
     clearTimeout(timerId.current);
@@ -174,18 +156,21 @@ const Scrollbar = React.forwardRef((props, ref) => {
   }, [autoHide, hideTrack]);
 
   // onScroll handler placed here on Scroller element to get native event instead of parameters that library put
-  const renderScroller = (libProps) => {
-    const { elementRef, ...restLibProps } = libProps;
-    return (
-      <div
-        {...restLibProps}
-        id={id}
-        className={scrollclass}
-        ref={elementRef}
-        onScroll={onScroll}
-      />
-    );
-  };
+  const renderScroller = useCallback(
+    (libProps) => {
+      const { elementRef, ...restLibProps } = libProps;
+      return (
+        <div
+          {...restLibProps}
+          id={id}
+          className={scrollclass}
+          ref={elementRef}
+          onScroll={onScroll}
+        />
+      );
+    },
+    [id, onScroll, scrollclass]
+  );
 
   useEffect(() => {
     return () => clearTimeout(timerId.current);
@@ -201,26 +186,37 @@ const Scrollbar = React.forwardRef((props, ref) => {
       onScrollStart={onScrollStart}
       scrollerProps={{ renderer: renderScroller }}
       contentProps={{
-        style: contentStyles,
         tabIndex: -1,
-        className: classNames("scroll-body"),
+        className: "scroll-body",
+        style: {
+          ...scrollbarType.content,
+          paddingRight: !isRtl && (isMobile() ? "8px" : "17px"),
+          paddingLeft: isRtl && (isMobile() ? "8px" : "17px"),
+        },
       }}
       thumbYProps={{
         className: "nav-thumb-vertical",
-        style: thumbVStyles,
+        style: scrollbarType.thumbV,
       }}
       thumbXProps={{
         className: "nav-thumb-horizontal",
-        style: thumbHStyles,
+        style: scrollbarType.thumbH,
       }}
+      // Add 1px margin to vertical track to avoid scrollbar lib crashing when event.clientX equals 0
       trackYProps={{
-        style: trackVStyles,
+        style: {
+          ...scrollbarType.trackV,
+          ...tracksCommonStyles,
+          marginLeft: isRtl ? "1px" : "0",
+          marginRight: isRtl ? "0" : "1px",
+        },
         onMouseEnter,
         onMouseLeave,
       }}
       trackXProps={{
         style: {
-          ...trackHStyles,
+          ...scrollbarType.trackH,
+          ...tracksCommonStyles,
           direction: "ltr",
         },
         onMouseEnter,
