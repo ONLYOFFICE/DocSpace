e55a914e1db644e4f2394b72e2cbe3e39b97ff7c
DmitrySychugov <dmitry.s.develop@mail.ru>
2023-07-31 19:25:43 +0500

    Web: Management: added logic for displaying the portal icon, fixed styles

--- packages/management/src/categories/spaces/sub-components/RowView/SpacesRoomRow.tsx
+++ packages/management/src/categories/spaces/sub-components/RowView/SpacesRoomRow.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import Row from "@docspace/components/row";
 import { RoomContent } from "./RoomContent";
+import { observer } from "mobx-react";
 import styled from "styled-components";
 import CatalogSettingsReactSvgUrl from "PUBLIC_DIR/images/catalog.settings.react.svg?url";
 import DeleteReactSvgUrl from "PUBLIC_DIR/images/delete.react.svg?url";
 import ExternalLinkIcon from "PUBLIC_DIR/images/external.link.react.svg?url";
-import { ReactSVG } from "react-svg";
+import DefaultLogoUrl from "PUBLIC_DIR/images/logo/dark_lightsmall.svg?url";
 
-import TestIcon from "PUBLIC_DIR/images/logo/dark_lightsmall.svg?url";
+import { ReactSVG } from "react-svg";
+import { useStore } from "SRC_DIR/store";
 
 const StyledRoomRow = styled(Row)`
-  padding: 6px 0;
+  padding: 4px 0;
 
   .styled-element {
-    width: 42px;
+    width: 32px;
     margin-left: 20px;
   }
 
@@ -21,9 +23,16 @@ const StyledRoomRow = styled(Row)`
     margin-right: 8px;
   }
 `;
+const SpacesRoomRow = ({ item }) => {
+  const { spacesStore } = useStore();
 
-export const SpacesRoomRow = ({ item, deletePortal }) => {
-  const element = <ReactSVG id={item.key} src={TestIcon} />; // change icon
+  const { deletePortal, faviconLogo } = spacesStore;
+
+  const logoElement = faviconLogo ? (
+    <img id={item.key} width={"32px"} height={"32px"} src={faviconLogo} />
+  ) : (
+    <ReactSVG id={item.key} src={DefaultLogoUrl} />
+  );
 
   const contextOptionsProps = [
     {
@@ -51,7 +60,7 @@ export const SpacesRoomRow = ({ item, deletePortal }) => {
   return (
     <StyledRoomRow
       contextOptions={contextOptionsProps}
-      element={element}
+      element={logoElement}
       key={item.id}
       data={item}
     >
@@ -59,3 +68,5 @@ export const SpacesRoomRow = ({ item, deletePortal }) => {
     </StyledRoomRow>
   );
 };
+
+export default observer(SpacesRoomRow);
