346241d1daf3d0cda5a49a4425792f31fec01dc9
DmitrySychugov <dmitry.s.develop@mail.ru>
2023-07-31 19:20:50 +0500

    Web: Management: created a file with typing for spaces

---
+++ packages/management/src/types/spaces.ts
@@ -0,0 +1,19 @@
+export type TPortals = {
+  created: string;
+  domain: string;
+  industry: number;
+  language: string;
+  name: string;
+  ownerId: string;
+  portalName: string;
+  status: string;
+  tenantId: number;
+  timeZoneName: string;
+};
+
+export type TNewPortalData = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  portalName: string;
+};
