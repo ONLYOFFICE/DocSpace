9d0cde9c5c81c321760935dcb577dec77c4ff35c
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2023-08-02 13:31:20 +0400

    Web: Fix start socket

--- packages/common/store/SettingsStore.js
+++ packages/common/store/SettingsStore.js
@@ -8,7 +8,12 @@ import {
   getSystemTheme,
 } from "../utils";
 import FirebaseHelper from "../utils/firebase";
-import { ThemeKeys, COOKIE_EXPIRATION_YEAR, LANGUAGE, TenantStatus } from "../constants";
+import {
+  ThemeKeys,
+  COOKIE_EXPIRATION_YEAR,
+  LANGUAGE,
+  TenantStatus,
+} from "../constants";
 import { version } from "../package.json";
 import SocketIOHelper from "../utils/socket";
 import { Dark, Base } from "@docspace/components/themes";
@@ -38,7 +43,8 @@ class SettingsStore {
     ? window.RendererProcessVariable?.theme?.type === "dark"
       ? Dark
       : Base
-    : window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
+    : window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
     ? Dark
     : Base;
   trustedDomains = [];
@@ -351,7 +357,10 @@ class SettingsStore {
     else newSettings = await api.settings.getSettings(true);
 
     if (window["AscDesktopEditor"] !== undefined || this.personal) {
-      const dp = combineUrl(window.DocSpaceConfig?.proxy?.url, "/products/files/");
+      const dp = combineUrl(
+        window.DocSpaceConfig?.proxy?.url,
+        "/products/files/"
+      );
       this.setDefaultPage(dp);
     }
 
@@ -361,7 +370,7 @@ class SettingsStore {
           key,
           key === "defaultPage"
             ? combineUrl(window.DocSpaceConfig?.proxy?.url, newSettings[key])
-            : newSettings[key],
+            : newSettings[key]
         );
         if (key === "culture") {
           if (newSettings.wizardToken) return;
@@ -421,7 +430,7 @@ class SettingsStore {
       this.getPortalSettings(),
       this.getAppearanceTheme(),
       this.getWhiteLabelLogoUrls(),
-      this.getBuildVersionInfo(),
+      this.getBuildVersionInfo()
     );
 
     await Promise.all(requests);
@@ -457,12 +466,12 @@ class SettingsStore {
   setAdditionalResources = async (
     feedbackAndSupportEnabled,
     videoGuidesEnabled,
-    helpCenterEnabled,
+    helpCenterEnabled
   ) => {
     return await api.settings.setAdditionalResources(
       feedbackAndSupportEnabled,
       videoGuidesEnabled,
-      helpCenterEnabled,
+      helpCenterEnabled
     );
   };
 
@@ -509,7 +518,13 @@ class SettingsStore {
   };
 
   setCompanyInfoSettings = async (address, companyName, email, phone, site) => {
-    return api.settings.setCompanyInfoSettings(address, companyName, email, phone, site);
+    return api.settings.setCompanyInfoSettings(
+      address,
+      companyName,
+      email,
+      phone,
+      site
+    );
   };
 
   setLogoUrl = (url) => {
@@ -568,11 +583,15 @@ class SettingsStore {
   };
 
   getLoginLink = (token, code) => {
-    return combineUrl(window.DocSpaceConfig?.proxy?.url, `/login.ashx?p=${token}&code=${code}`);
+    return combineUrl(
+      window.DocSpaceConfig?.proxy?.url,
+      `/login.ashx?p=${token}&code=${code}`
+    );
   };
 
   setModuleInfo = (homepage, productId) => {
-    if (this.homepage === homepage || this.currentProductId === productId) return;
+    if (this.homepage === homepage || this.currentProductId === productId)
+      return;
 
     console.log(`setModuleInfo('${homepage}', '${productId}')`);
 
@@ -618,12 +637,17 @@ class SettingsStore {
     this.setPasswordSettings(settings);
   };
 
-  setPortalPasswordSettings = async (minLength, upperCase, digits, specSymbols) => {
+  setPortalPasswordSettings = async (
+    minLength,
+    upperCase,
+    digits,
+    specSymbols
+  ) => {
     const settings = await api.settings.setPortalPasswordSettings(
       minLength,
       upperCase,
       digits,
-      specSymbols,
+      specSymbols
     );
     this.setPasswordSettings(settings);
   };
@@ -681,7 +705,7 @@ class SettingsStore {
 
   get socketHelper() {
     const socketUrl =
-      this.isPublicRoom && this.publicRoomKey ? this.socketUrl : null;
+      this.isPublicRoom && !this.publicRoomKey ? null : this.socketUrl;
 
     return new SocketIOHelper(socketUrl, this.publicRoomKey);
   }
@@ -701,7 +725,8 @@ class SettingsStore {
       ...versionInfo,
     };
 
-    if (!this.buildVersionInfo.documentServer) this.buildVersionInfo.documentServer = "6.4.1";
+    if (!this.buildVersionInfo.documentServer)
+      this.buildVersionInfo.documentServer = "6.4.1";
   };
 
   setTheme = (key) => {
