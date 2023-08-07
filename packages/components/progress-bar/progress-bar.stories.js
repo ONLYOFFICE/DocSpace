f4f4ae28dba1a20f9e543cd6cfb0123dbce21e0f
Alexey Safronov <Alexey.Safronov@onlyoffice.com>
2023-08-07 16:01:38 +0400

    Web: Components: Added link to figma

--- packages/components/progress-bar/progress-bar.stories.js
+++ packages/components/progress-bar/progress-bar.stories.js
@@ -11,8 +11,14 @@ export default {
           "A container that displays a process or operation as a progress bar",
       },
     },
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/ZiW5KSwb4t7Tj6Nz5TducC/UI-Kit-DocSpace-1.0.0?node-id=991%3A43382&mode=dev",
+    },
   },
 };
+
+//
 
 const Template = (args) => {
   return <ProgressBar {...args} />;
