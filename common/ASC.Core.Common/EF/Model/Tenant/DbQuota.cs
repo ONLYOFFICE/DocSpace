194d743e223d8783c9b2a61e041c5f7780c66d83
Andrey Savihin <andrey.savihin@onlyoffice.com>
2023-08-16 17:36:34 +0300

    ASC.Core.Common: DbQuota: added new data

--- common/ASC.Core.Common/EF/Model/Tenant/DbQuota.cs
+++ common/ASC.Core.Common/EF/Model/Tenant/DbQuota.cs
@@ -85,6 +85,46 @@ public static class DbQuotaExtension
                     Price = 0,
                     ProductId = null,
                     Visible = false
+                },
+                new DbQuota
+                {
+                    Tenant = -4,
+                    Name = "disk",
+                    Description = null,
+                    Features = "total_size:1073741824",
+                    Price = 0,
+                    ProductId = "1004",
+                    Visible = false
+                },
+                new DbQuota
+                {
+                    Tenant = -5,
+                    Name = "admin1",
+                    Description = null,
+                    Features = "manager:1",
+                    Price = 0,
+                    ProductId = "1005",
+                    Visible = false
+                },
+                new DbQuota
+                {
+                    Tenant = -6,
+                    Name = "subscription",
+                    Description = null,
+                    Features = "audit,ldap,sso,whitelabel,thirdparty,restore,oauth,contentsearch,file_size:1024",
+                    Price = 0,
+                    ProductId = "1001",
+                    Visible = false
+                },
+                new DbQuota
+                {
+                    Tenant = -7,
+                    Name = "nonprofit",
+                    Description = null,
+                    Features = "non-profit,audit,ldap,sso,thirdparty,restore,oauth,total_size:2147483648,file_size:1024,manager:20",
+                    Price = 0,
+                    ProductId = null,
+                    Visible = false
                 }
                 );
 
