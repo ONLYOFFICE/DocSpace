3b874b1fb4ab663dbdbd03ec1ec508d1e40b1902
Sergey Kirichenko <Sergey.Kirichenko@onlyoffice.com>
2023-08-18 13:47:40 +0300

    Fix sed action

--- build/install/docker/prepare-nginx-proxy.sh
+++ build/install/docker/prepare-nginx-proxy.sh
@@ -2,4 +2,4 @@
 REDIS_HOST=${REDIS_HOST:-"onlyoffice-redis"}
 
 envsubst '$MAP_HASH_BUCKET_SIZE,$COUNT_WORKER_CONNECTIONS' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-sed -i 's/redis_host = \"127.0.0.1\"/redis_host = \"${REDIS_HOST}\"/' /etc/nginx/conf.d/onlyoffice.conf
+sed -i "s!redis_host = \"127.0.0.1\"!redis_host = \"${REDIS_HOST}\"!g"  /etc/nginx/conf.d/onlyoffice.conf
