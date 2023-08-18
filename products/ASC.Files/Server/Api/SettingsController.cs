01cd7b3a629441dda2619d3bf076808569970097
pavelbannov <pavel.bannov@onlyoffice.com>
2023-08-18 13:57:15 +0300

    Files: hidden favorites methods

--- products/ASC.Files/Server/Api/SettingsController.cs
+++ products/ASC.Files/Server/Api/SettingsController.cs
@@ -116,6 +116,7 @@ public class SettingsController : ApiControllerBase
     /// <returns type="System.Boolean, System">Boolean value: true if the parameter is enabled</returns>
     /// <path>api/2.0/files/settings/favorites</path>
     /// <httpMethod>PUT</httpMethod>
+    /// <visible>false</visible>
     [HttpPut("settings/favorites")]
     public bool DisplayFavorite(DisplayRequestDto inDto)
     {
