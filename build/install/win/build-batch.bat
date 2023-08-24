d935ec81ee9440adfcfd0b5358b11d2883e60b32
Nasrullo Nurullaev <nasrullo.nurullaev@onlyoffice.com>
2023-08-24 16:18:29 +0500

    Add OpenResty to DocSpace as prerequisite

--- build/install/win/build-batch.bat
+++ build/install/win/build-batch.bat
@@ -1,15 +1,10 @@
 REM echo ######## Set variables ########
 set "publisher="Ascensio System SIA""
 set "nuget="%cd%\thirdparty\SimpleRestServices\src\.nuget\NuGet.exe""
-set "nginx_version=1.21.1"
 set "environment=production"
 
 REM echo ######## Extracting and preparing files to build ########
-%sevenzip% x build\install\win\nginx-%nginx_version%.zip -o"build\install\win\Files" -y
-xcopy "build\install\win\Files\nginx-%nginx_version%" "build\install\win\Files\nginx" /s /y /b /i
-rmdir build\install\win\Files\nginx-%nginx_version% /s /q
-md build\install\win\Files\nginx\temp
-md build\install\win\Files\nginx\logs
+md build\install\win\OpenResty\tools
 md build\install\win\Files\tools
 md build\install\win\Files\Logs
 md build\install\win\Files\Data
@@ -24,8 +19,8 @@ md build\install\win\Files\services\ASC.ClearEvents\service\temp
 md build\install\win\Files\services\ASC.Web.Api\service\temp
 md build\install\win\Files\services\ASC.Web.Studio\service\temp
 md build\install\win\Files\services\ASC.Web.HealthChecks.UI\service\temp
-copy build\install\win\WinSW.NET4.exe "build\install\win\Files\tools\Proxy.exe" /y
-copy build\install\win\tools\Proxy.xml "build\install\win\Files\tools\Proxy.xml" /y
+copy build\install\win\WinSW.NET4.exe "build\install\win\OpenResty\tools\OpenResty.exe" /y
+copy build\install\win\tools\OpenResty.xml "build\install\win\OpenResty\tools\OpenResty.xml" /y
 copy build\install\win\WinSW3.0.0.exe "build\install\win\Files\tools\Socket.IO.exe" /y
 copy build\install\win\tools\Socket.IO.xml "build\install\win\Files\tools\Socket.IO.xml" /y
 copy build\install\win\WinSW3.0.0.exe "build\install\win\Files\tools\SsoAuth.exe" /y
@@ -73,6 +68,13 @@ del /f /q build\install\win\*.back.*
 
 REM echo ######## Build MySQL Server Installer ########
 iscc /Qp /S"byparam="signtool" sign /a /n "%publisher%" /t http://timestamp.digicert.com $f" "build\install\win\MySQL Server Installer Runner.iss"
+
+REM echo ######## Build OpenResty ########
+IF "%SignBuild%"=="true" (
+%AdvancedInstaller% /edit build\install\win\OpenResty.aip /SetSig
+%AdvancedInstaller% /edit build\install\win\OpenResty.aip /SetDigitalCertificateFile -file %onlyoffice_codesign_path% -password "%onlyoffice_codesign_password%"
+)
+%AdvancedInstaller% /rebuild build\install\win\OpenResty.aip
 
 REM echo ######## Build DocSpace package ########
 %AdvancedInstaller% /edit build\install\win\DocSpace.aip /SetVersion %BUILD_VERSION%.%BUILD_NUMBER%
