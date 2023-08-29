48852756bf6f590ab36a38de254b5078daf16533
gopienkonikita <nikita.gopienko@onlyoffice.com>
2023-08-29 17:44:50 +0300

    Web: Files: removed useless

--- packages/components/infinite-loader/InfiniteLoader.js
+++ packages/components/infinite-loader/InfiniteLoader.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { isMobileOnly } from "react-device-detect";
 import ListComponent from "./List";
