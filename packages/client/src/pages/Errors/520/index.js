58696b41e9c6be9440d6a5dc66bfda8ddb27f372
Viktor Fomin <enotcode@gmail.com>
2023-08-30 12:43:48 +0300

    Client: Errors: 520: auto send report

--- packages/client/src/pages/Errors/520/index.js
+++ packages/client/src/pages/Errors/520/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { inject, observer } from "mobx-react";
 import { I18nextProvider, useTranslation } from "react-i18next";
@@ -8,6 +8,11 @@ import { ZendeskAPI } from "@docspace/common/components/Zendesk";
 
 import { ReportDialog } from "SRC_DIR/components/dialogs";
 import DocspaceLogo from "SRC_DIR/DocspaceLogo";
+import {
+  getCrashReport,
+  downloadJson,
+  getCurrentDate,
+} from "SRC_DIR/helpers/crashReport";
 
 import i18n from "./i18n";
 
@@ -33,10 +38,28 @@ const StyledWrapper = styled.div`
   }
 `;
 
-const Error520 = ({ errorLog, currentColorScheme, FirebaseHelper }) => {
+const Error520 = ({
+  errorLog,
+  currentColorScheme,
+  FirebaseHelper,
+  user,
+  version,
+}) => {
   const { t } = useTranslation(["Common"]);
 
   const [reportDialogVisible, setReportDialogVisible] = useState(false);
+
+  const autoSendReport = async () => {
+    const report = getCrashReport(user.id, version, user.cultureName, errorLog);
+    const reportWithDescription = Object.assign(report, {
+      description: "AUTO SEND",
+    });
+    await FirebaseHelper.sendCrashReport(reportWithDescription);
+  };
+
+  useEffect(() => {
+    FirebaseHelper.isEnabledDB && autoSendReport();
+  }, []);
 
   const showDialog = () => {
     setReportDialogVisible(true);
@@ -61,9 +84,9 @@ const Error520 = ({ errorLog, currentColorScheme, FirebaseHelper }) => {
       <ErrorContainer
         isPrimaryButton={false}
         headerText={t("SomethingWentWrong")}
-        customizedBodyText={t("SomethingWentWrongDescription")}
-        buttonText={t("SendReport")}
-        onClickButton={showDialog}
+        //customizedBodyText={t("SomethingWentWrongDescription")}
+        //buttonText={t("SendReport")}
+        //onClickButton={showDialog}
       />
       <Link
         className="link"
@@ -86,10 +109,13 @@ const Error520 = ({ errorLog, currentColorScheme, FirebaseHelper }) => {
 
 const Error520Wrapper = inject(({ auth }) => {
   const { currentColorScheme, firebaseHelper } = auth.settingsStore;
+  const { user } = auth.userStore;
 
   return {
     currentColorScheme,
     FirebaseHelper: firebaseHelper,
+    user,
+    version: auth.version,
   };
 })(observer(Error520));
 
