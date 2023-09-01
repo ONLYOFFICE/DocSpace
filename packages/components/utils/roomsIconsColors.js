2ada1a62223609201ee6849ca0e5a1527a9d231c
gazizova-vlada <vladamuzyka@yandex.ru>
2023-09-01 10:37:27 +0300

    Web:Components:Add roomsIconsColors.

---
+++ packages/components/utils/roomsIconsColors.js
@@ -0,0 +1,15 @@
+const roomsIconsColors = {
+  salmon: "#FF6680",
+  orange: "#FF8F40",
+  yellow: "#F2D230",
+  green: "#61C059",
+  lightGreen: "#70E096",
+  turquoise: "#1FCECB",
+  blue: "#5CC3F7",
+  darkBlue: "#6191F2",
+  purple: "#7757D9",
+  violet: "#B679F2",
+  pink: "#FF7FD4",
+};
+
+export default roomsIconsColors;
