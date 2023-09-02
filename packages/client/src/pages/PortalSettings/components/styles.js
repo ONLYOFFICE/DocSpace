cd75d9c4d977a171bdf3568e1d28becf599c9b29
Viktor Fomin <enotcode@gmail.com>
2023-09-02 15:41:50 +0300

    Client: PortalSettings: fix

--- packages/client/src/pages/PortalSettings/components/styles.js
+++ packages/client/src/pages/PortalSettings/components/styles.js
@@ -9,32 +9,44 @@ export const StyledArrowRightIcon = styled(ArrowRightIcon)`
   path {
     fill: ${(props) => props.theme.client.settings.security.arrowFill};
   }
+
+  ${({ theme }) =>
+    theme.interfaceDirection === "rtl" && "transform: scaleX(-1);"}
 `;
 
 StyledArrowRightIcon.defaultProps = { theme: Base };
 
 export const StyledMobileCategoryWrapper = styled.div`
   margin-bottom: 20px;
+
   .category-item-heading {
     display: flex;
     align-items: center;
-    margin-bottom: 5px;
+    margin-bottom: 8px;
   }
+
   .category-item-subheader {
     font-size: 13px;
     font-weight: 600;
     margin-bottom: 5px;
   }
+
   .category-item-description {
     color: ${(props) => props.theme.client.settings.security.descriptionColor};
-    font-size: 12px;
+    font-size: 13px;
     max-width: 1024px;
+    line-height: 20px;
   }
+
   .inherit-title-link {
-    margin-right: 7px;
+    ${({ theme }) =>
+      theme.interfaceDirection === "rtl"
+        ? `margin-left: 7px;`
+        : `margin-right: 7px;`}
     font-size: 16px;
-    font-weight: 600;
+    font-weight: 700;
   }
+
   .link-text {
     margin: 0;
   }
