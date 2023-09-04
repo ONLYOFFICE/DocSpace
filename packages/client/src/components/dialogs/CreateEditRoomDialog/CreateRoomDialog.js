e2bf5997f94a7826227c98eb80f2073183cf89cb
Viktor Fomin <enotcode@gmail.com>
2023-09-04 12:06:02 +0300

    Client: Dialogs: CreateRoomDialog: delete useless

--- packages/client/src/components/dialogs/CreateEditRoomDialog/CreateRoomDialog.js
+++ packages/client/src/components/dialogs/CreateEditRoomDialog/CreateRoomDialog.js
@@ -11,9 +11,6 @@ import RoomTypeList from "./sub-components/RoomTypeList";
 import DialogHeader from "./sub-components/DialogHeader";
 
 const StyledModalDialog = styled(ModalDialog)`
-  #modal-dialog {
-    margin-bottom: -64px !important;
-  }
   .header-with-button {
     display: flex;
     align-items: center;
