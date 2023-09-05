1378fb919583da323ed05dbc4d52e1dd6130e7f5
Tatiana Lopaeva <tatiana.lopaeva@onlyoffice.com>
2023-09-05 14:16:45 +0300

    Web: Added styles.

--- packages/client/src/components/FilesSelectorInput/StyledComponents.js
+++ packages/client/src/components/FilesSelectorInput/StyledComponents.js
@@ -1,6 +1,7 @@
 import styled, { css } from "styled-components";
 
 const StyledBodyWrapper = styled.div`
+  margin: 16px 0;
   max-width: ${(props) => (props.maxWidth ? props.maxWidth : "350px")};
 `;
 
