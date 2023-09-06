0313e835b08fc45267d6ff9b7c69fee1d3de26e5
Nasrullo Nurullaev <61620246+nasrullonurullaev@users.noreply.github.com>
2023-09-06 13:06:27 +0500

    Add the ability to automatically switch to SSL (#1705)

--- build/install/win/build-batch.bat
+++ build/install/win/build-batch.bat
@@ -8,6 +8,7 @@ md build\install\win\OpenResty\tools
 md build\install\win\Files\tools
 md build\install\win\Files\Logs
 md build\install\win\Files\Data
+md build\install\win\Files\sbin
 md build\install\win\Files\products\ASC.Files\server\temp
 md build\install\win\Files\products\ASC.People\server\temp
 md build\install\win\Files\services\ASC.Data.Backup\service\temp
@@ -30,7 +31,17 @@ copy build\install\win\tools\DocEditor.xml "build\install\win\Files\tools\DocEdi
 copy build\install\win\WinSW3.0.0.exe "build\install\win\Files\tools\Login.exe" /y
 copy build\install\win\tools\Login.xml "build\install\win\Files\tools\Login.xml" /y
 copy "build\install\win\nginx.conf" "build\install\win\Files\nginx\conf\nginx.conf" /y
+copy "build\install\docker\config\nginx\onlyoffice-proxy.conf" "build\install\win\Files\nginx\conf\onlyoffice-proxy.conf" /y
+copy "build\install\docker\config\nginx\onlyoffice-proxy-ssl.conf" "build\install\win\Files\nginx\conf\onlyoffice-proxy-ssl.conf.tmpl" /y
+copy "build\install\docker\config\nginx\letsencrypt.conf" "build\install\win\Files\nginx\conf\includes\letsencrypt.conf" /y
+copy "build\install\win\sbin\docspace-ssl-setup.ps1" "build\install\win\Files\sbin\docspace-ssl-setup.ps1" /y
 rmdir build\install\win\publish /s /q
+
+REM echo ######## SSL configs ########
+%sed% -i "s/the_host/host/g" build\install\win\Files\nginx\conf\onlyoffice-proxy.conf build\install\win\Files\nginx\conf\onlyoffice-proxy-ssl.conf.tmpl
+%sed% -i "s/the_scheme/scheme/g" build\install\win\Files\nginx\conf\onlyoffice-proxy.conf build\install\win\Files\nginx\conf\onlyoffice-proxy-ssl.conf.tmpl
+%sed% -i "s/ssl_dhparam \/etc\/ssl\/certs\/dhparam.pem;/#ssl_dhparam \/etc\/ssl\/certs\/dhparam.pem;/" build\install\win\Files\nginx\conf\onlyoffice-proxy-ssl.conf.tmpl
+%sed% -i "s/# include includes\/letsencrypt.conf/include includes\/letsencrypt.conf/" build\install\win\Files\nginx\conf\onlyoffice-proxy.conf build\install\win\Files\nginx\conf\onlyoffice-proxy-ssl.conf.tmpl
 
 REM echo ######## Delete test and dev configs ########
 del /f /q build\install\win\Files\config\*.test.json
