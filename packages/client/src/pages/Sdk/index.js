c00645554b8bfcf49c9e38044439e79dd35e89e4
Ilya Oleshko <ilya.oleshko@onlyoffice.com>
2023-09-07 15:32:54 +0300

    Web: SDK: Fixed setting base locale

--- packages/client/src/pages/Sdk/index.js
+++ packages/client/src/pages/Sdk/index.js
@@ -75,7 +75,9 @@ const Sdk = ({
             {
               const requests = await Promise.all([
                 setFrameConfig(data),
-                user && updateProfileCulture(user.id, data.locale),
+                user &&
+                  data.locale &&
+                  updateProfileCulture(user.id, data.locale),
               ]);
               res = requests[0];
             }
