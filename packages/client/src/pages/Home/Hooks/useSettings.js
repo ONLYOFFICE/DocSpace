3ceccbba29bcc0f4950bc213f43179dd470fbf58
Timofey Boyko <timofey.boyko@onlyoffice.com>
2023-09-13 16:29:14 +0300

    Web:Client:Home: delete plugins list

--- packages/client/src/pages/Home/Hooks/useSettings.js
+++ packages/client/src/pages/Home/Hooks/useSettings.js
@@ -5,21 +5,9 @@ import { setDocumentTitle } from "SRC_DIR/helpers/utils";
 const useSettings = ({
   t,
   isSettingsPage,
-  isPluginsSettingsPage,
-  enablePlugins,
-  navigate,
+
   setIsLoading,
 }) => {
-  React.useEffect(() => {
-    if (!enablePlugins && isPluginsSettingsPage) {
-      return navigate("/settings", { replace: true });
-    }
-
-    setDocumentTitle(t("Common:Settings"));
-
-    setIsLoading(false);
-  }, [enablePlugins, isPluginsSettingsPage]);
-
   React.useEffect(() => {
     if (!isSettingsPage) return;
 
