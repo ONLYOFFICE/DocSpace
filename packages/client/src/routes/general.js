555c70fd450d73108b873109b6084d1e093129da
Viktor Fomin <enotcode@gmail.com>
2023-09-26 23:00:48 +0300

    Client: add new profile routes

--- packages/client/src/routes/general.js
+++ packages/client/src/routes/general.js
@@ -1,15 +1,19 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 import loadable from "@loadable/component";
 
 import PrivateRoute from "@docspace/common/components/PrivateRoute";
 
 const Profile = loadable(() => import("../pages/Profile"));
-const NotificationComponent = loadable(() => import("../pages/Notifications"));
 
 const generalRoutes = [
   {
     path: "profile",
+    element: <Navigate to="/profile/login" />,
+  },
+  {
+    path: "profile/login",
     element: (
       <PrivateRoute>
         <Profile />
@@ -17,10 +21,26 @@ const generalRoutes = [
     ),
   },
   {
-    path: "profile/notification",
+    path: "profile/notifications",
     element: (
       <PrivateRoute>
-        <NotificationComponent />
+        <Profile />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "profile/file-management",
+    element: (
+      <PrivateRoute>
+        <Profile />
+      </PrivateRoute>
+    ),
+  },
+  {
+    path: "profile/interface-theme",
+    element: (
+      <PrivateRoute>
+        <Profile />
       </PrivateRoute>
     ),
   },
